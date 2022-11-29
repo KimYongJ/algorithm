17702dcc1a120e11ccde09bbce82d1553e4c7f81
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-11-29 22:25:09 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -2,14 +2,12 @@ import java.io.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr= new int[31];
-        for(int i=0;i<28;i++){
-            int data = Integer.parseInt(in.readLine());
-            arr[data]=1;
-        }
-        for(int i=1;i<31;i++){
-            if(arr[i]!=1) System.out.println(i);
-        }
+        boolean[] arr= new boolean[31];
+        for(int i=0;i<28;i++) 
+            arr[Integer.parseInt(in.readLine())]=true;
+        for(int i=1;i<31;i++)
+            if(!arr[i]) System.out.println(i);
+
         
     }
 }
