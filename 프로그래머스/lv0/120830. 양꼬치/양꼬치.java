c336d54eba6577ee45c92b08e9a0da220d4de4b3
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-11-29 21:33:42 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int n, int k) {
-        int service = n/10;
-        k-=service;
-        return n*12000+k*2000;
+        return n*12000+k*2000-n/10*2000;
         
     }
 }
