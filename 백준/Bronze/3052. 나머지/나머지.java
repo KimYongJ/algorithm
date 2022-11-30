3d5906a929b7c3127e37f6c132478ee380b1f8e7
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-11-30 22:08:35 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -3,12 +3,10 @@ import java.io.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<String> hs = new HashSet<String>();
+        HashSet<Integer> hs = new HashSet<Integer>();
         
-        for(int i=0;i<10;i++){
-            int data = Integer.parseInt(in.readLine());
-            hs.add(String.valueOf(data%42));
-        }
+        for(int i=0;i<10;i++) hs.add(Integer.parseInt(in.readLine())%42);
+
         System.out.println(hs.size());
     }
 }
