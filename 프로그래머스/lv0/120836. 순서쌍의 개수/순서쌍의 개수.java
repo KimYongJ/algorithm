41002b612b07307dcbd895a387489b62a271d6fa
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-04 16:14:40 +0900

    [level 0] Title: 순서쌍의 개수, Time: 3.37 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int result = 0;
-        for(int i=1;i<n+1;i++){
-            if(n%i==0) result+=1;
+        int result = 1;
+        for(int i=1;i<n/2+1;i++){
+            if(n%i==0) ++result;
         }
         return result;
     }
