ad693f258ac720bb4a5510a7cbc9fd183b62de10
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-04 22:07:26 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        return my_string.replace("a","").replace("e","").replace("i","").replace("o","").replace("u","");
-    }
+        return my_string.replaceAll("[aeiou]","");
+        }
 }
