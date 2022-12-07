2e98d3ff62740a2b1c01373a3c70ba3591113766
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-07 21:18:21 +0900

    [level 0] Title: 암호 해독, Time: 11.18 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -2,8 +2,7 @@ class Solution {
     public String solution(String cipher, int code) {
         char[] c = cipher.toCharArray();
         String r = "";
-        for(int i=code;i<=c.length;i++)
-            if(i%code==0)
+        for(int i=code;i<=c.length;i+=code)
                 r=r+c[i-1]+"";
 
         return r;
