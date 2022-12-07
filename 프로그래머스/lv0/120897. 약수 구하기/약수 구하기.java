40ea2975f53259aef02bc2ecbf2b22b091559f03
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-07 21:08:14 +0900

    [level 0] Title: 약수 구하기, Time: 2.42 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -3,12 +3,9 @@ import java.util.List;
 class Solution {
     public int[] solution(int n) {
         List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=(n+1)/2;i++){
-            if(n%i==0){
-                list.add(i);
-            }
-        }
-        if(n!=1)
+        for(int i=1;i<=n/2;i++)
+            if(n%i==0) list.add(i);
+        
         list.add(n);
         
         return list.stream().mapToInt(Integer::intValue).toArray();
