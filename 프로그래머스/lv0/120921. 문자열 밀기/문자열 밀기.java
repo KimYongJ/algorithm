6fa292db2d94abd378f9970bfb71b5273e3f1b7e
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-09 11:01:20 +0000

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(String a, String b) {
-        int cnt = 0;
-        int l = a.length()-1;
-        while(cnt<l+1){
-            if(a.equals(b))
-                return cnt;
-            a = a.charAt(l)+a.substring(0,l);
-            cnt++;
-        }
-        return -1;
+        return b.repeat(2).indexOf(a);
     }
 }
