32608cc41309232e9fde441a8cc4bf51fce92264
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-14 08:12:19 +0900

    [level 0] Title: k의 개수, Time: 372.46 ms, Memory: 382 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int x, int y, int k) {
-        int cnt=0;
-        char param = Character.forDigit(k,10);
+        String str="";
         for(int i=x;i<y+1;i++){
-            char[] c = String.valueOf(i).toCharArray();
-            for(char a : c){
-                if(a==param) cnt++;
-            }
+            str+=Integer.toString(i);
         }
-        return cnt;
+        String param = Integer.toString(k);
+        return str.length() - str.replace(param,"").length();
     }
 }
