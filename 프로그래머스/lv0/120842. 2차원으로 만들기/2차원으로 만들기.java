7e54c1db1d2b1dc97ffdfb6fa59050aa57563671
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-13 22:32:31 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int len = num_list.length/n;
-        int[][] arr = new int[len][n];
-        int cnt=0;
-        for(int j=0;j<len;j++){
-            for(int i=0;i<n;i++){
-                arr[j][i]=num_list[cnt++];
-            }
+        int[][] arr = new int[num_list.length/n][n];
+        
+        for(int i=0;i<num_list.length;i++){
+            arr[i/n][i%n]=num_list[i];
         }
+        
         return arr; 
     }
 }
