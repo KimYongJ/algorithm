7763c4be14e1cea749f87bc2cef7d5686817bef2
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-15 21:24:42 +0900

    [Silver V] Title: 셀프 넘버, Time: 124 ms, Memory: 13916 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -4,26 +4,28 @@ import java.io.*;
 class Main{
     public static void main(String[] args){
         StringBuilder sb = new StringBuilder();
-        List<Integer> dlist = new ArrayList<Integer>();
-        for(int i=0;i<=10000;i++){
-            if(i<10){
-                dlist.add(i+i);
-            }else{
-                int d = 0;
-                d+=i;
-                char[] cl = Integer.toString(i).toCharArray();
-                for(char c : cl){
-                    d+=c-'0';
-                }
-                dlist.add(d);                
+        boolean[] check = new boolean[10001];
+        for(int i=1;i<10001;i++){
+            int a = d(i);
+            if(a<10001){
+                check[a]=true;
             }
         }
-        // 다시반복문을해서 dlist에 없는 값만 sb에 넣어주면된다.
-        for(int j=1;j<=10000;j++){
-            if(!dlist.contains(j)){
+        
+        for(int j=1;j<10001;j++){
+            if(!check[j])
                 sb.append(j).append("\n");
-            }
         }
+       
         System.out.println(sb);
     }
+    private static int d(int num){
+        int sum = num;
+        while(num!=0){
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+    
 }
