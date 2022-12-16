9087f8bd6d847eb7d1e7330e65a8e0d142f2cfa6
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-16 22:22:47 +0900

    [level 0] Title: 가까운 수, Time: 0.41 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -1,34 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
-        if(same(array,n)) return n;
-        int len = array.length+1;
-        int[] narray = new int[len];
-        int index =0;
-        System.arraycopy(array,0,narray,0,len-1);
-        narray[len-1]=n;
-        Arrays.sort(narray);
-        for(int i=0;i<len;i++){
-            if(n==narray[i]){
-                index = i;
-                break;
-            }
-        }
-        if(index==0){ return narray[1]; }
-        else if(index==len-1) {return narray[len-2];}
-        else{
-            if(narray[index]-narray[index-1]<=narray[index+1]-narray[index]){
-                return narray[index-1];}
-            else{
-                return narray[index+1];}
-        }
+        Arrays.sort(array);
         
-    }
-
-    private static boolean same(int[] array, int n){
         for(int i=0;i<array.length;i++){
-            if(n==array[i]) return true;
+            if(Math.abs(n-array[0])>Math.abs(n-array[i])){
+                array[0]=array[i];
+            }
+            
         }
-        return false;
+        return array[0];
     }
 }
