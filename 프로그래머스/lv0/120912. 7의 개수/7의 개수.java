36c86a2d6294fe4989dcc6a69415d7bb3945699c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-16 23:34:29 +0900

    [level 0] Title: 7의 개수, Time: 1.38 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] array) {
+        String str ="";
+        
+        for(int x : array)
+            str+= Integer.toString(x);
+        
+        return str.length() - str.replaceAll("7","").length();
+        
+    }
+}
