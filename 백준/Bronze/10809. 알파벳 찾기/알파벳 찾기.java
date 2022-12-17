c659ac44146d3b63964180e3fcb5b564c36c63cf
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 22:57:24 +0900

    Update 알파벳 찾기.java

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -3,20 +3,11 @@ import java.io.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        char[] cl = br.readLine().toCharArray();
-        int[] arr = new int[26];
+        String str = br.readLine();
         
-        for(int i=0;i<cl.length;i++){
-            if(arr[cl[i]-'a']==0){
-                arr[cl[i]-'a'] = i+1;
-                continue;
-            }
+        for(int x ='a';x<='z';x++){
+            System.out.print(str.indexOf(x)+" ");
         }
-        for(int j=0;j<26;j++){
-            sb.append(--arr[j]).append(" ");
-        }
-        System.out.println(sb);
         
     }
 }
