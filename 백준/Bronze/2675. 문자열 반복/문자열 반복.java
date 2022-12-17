7654335bcc50c200b71b253803e7e72186804e0b
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 23:12:32 +0900

    Update 문자열 반복.java

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -8,11 +8,9 @@ class Main{
         for(int i=0;i<len;i++){
             StringTokenizer st = new StringTokenizer(br.readLine()," ");
             int repeat = Integer.parseInt(st.nextToken());
-            String str = st.nextToken();
-            
-            for(char c : str.toCharArray()){
-                for(int j=0;j<repeat;j++) sb.append(c);
-            }
+            String str = st.nextToken();           
+            for(char c : str.toCharArray())
+                sb.append((c+"").repeat(repeat));
             sb.append('\n');
         }
         System.out.println(sb);
