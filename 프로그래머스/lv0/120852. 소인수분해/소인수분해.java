f23f278f79f70aadaa908f15fa7febcfedf074a7
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 15:20:11 +0900

    Update 소인수분해.java

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -3,7 +3,7 @@ class Solution {
     public int[] solution(int n) {
         LinkedHashSet<Integer> list = new LinkedHashSet<Integer>();
         int div =2;
-        while(n!=0 && n != 1){
+        while(n>1){
             if(n%div==0){
                 list.add(div);
                 n /= div;
