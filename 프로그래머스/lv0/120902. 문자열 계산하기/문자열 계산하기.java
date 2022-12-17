455916e4cc7b56e6fe18460507f66676d45c559c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 17:40:21 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String s) {
         String[] sl = s.split(" ");
         int result =Integer.parseInt(sl[0]) ;
-        for(int i=1;i<sl.length;i++){
+        for(int i=1;i<sl.length;i+=2){
             if(sl[i].equals("+"))
                 result += Integer.parseInt(sl[i+1]);
             else if(sl[i].equals("-"))
