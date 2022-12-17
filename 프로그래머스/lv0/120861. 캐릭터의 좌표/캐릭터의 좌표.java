6cac7f3fcbc28a7a92209a462d5caf1506da9042
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 21:56:57 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -3,15 +3,10 @@ class Solution {
         int x = 0;
         int y = 0;
         for(String k : keyinput){
-            if(k.contains("u") && board[1]/2>y){
-                ++y;
-            }else if(k.contains("d") && board[1]/2*-1<y){
-                --y;
-            }else if(k.contains("r") && board[0]/2>x){
-                ++x;
-            }else if(k.contains("l") && board[0]/2*-1<x){
-                --x;
-            }
+            if(k.contains("u") && board[1]/2>y) ++y;
+            else if(k.contains("d") && board[1]/2*-1<y) --y;
+            else if(k.contains("r") && board[0]/2>x) ++x;
+            else if(k.contains("l") && board[0]/2*-1<x) --x;
         }
         board[0]=x;
         board[1]=y;
