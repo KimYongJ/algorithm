334c8ed2a0262f422acdfca3167b5b185a9335d3
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 13:57:08 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,20 +1,12 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int idx = 0;
-        int maxIdx = numbers.length-1;
         for(int i=1;i<k;i++){
             idx+=2;
-            if(idx-maxIdx==1){
-                idx=0;
-                continue;
-            }
-            else if(idx-maxIdx==2){
-                idx=1;
-                continue;
+            if(idx>numbers.length-1){
+                idx = idx-numbers.length;
             }
         }
-        
-
         return numbers[idx];
     }
 }
