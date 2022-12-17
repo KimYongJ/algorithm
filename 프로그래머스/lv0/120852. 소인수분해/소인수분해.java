08e329bdfd3b0a6f370965b12eef5629ba5d1b06
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 15:18:26 +0900

    [level 0] Title: 소인수분해, Time: 2.82 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -1,25 +1,15 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        HashSet<Integer> list = new HashSet<Integer>();
+        LinkedHashSet<Integer> list = new LinkedHashSet<Integer>();
         int div =2;
-        while(n!=0){
+        while(n!=0 && n != 1){
             if(n%div==0){
                 list.add(div);
-                n = div(div,n);
-                if(n==1) break;
+                n /= div;
             }else
                 ++div;
         }
-        int[] result = list.stream().mapToInt(i->i).toArray();
-        Arrays.sort(result);
-        return result;
+        return list.stream().mapToInt(i->i).toArray();
     }
-    
-    private static int div(int div,int n){
-        while(n%div==0)
-            n = n/div;
-        return n;
-    }
-    
 }
