b97c0fc4acc5c4848fd0e3158515c1fa01839d2f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-17 21:55:41 +0900

    Update 캐릭터의 좌표.java

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-        int g = board[0]/2;
-        int s = board[1]/2;
-        int gm = g*-1;
-        int sm = s*-1;
         int x = 0;
         int y = 0;
         for(String k : keyinput){
-            if(k.contains("u") && s>y){
+            if(k.contains("u") && board[1]/2>y){
                 ++y;
-            }else if(k.contains("d") && sm<y){
+            }else if(k.contains("d") && board[1]/2*-1<y){
                 --y;
-            }else if(k.contains("r") && g>x){
+            }else if(k.contains("r") && board[0]/2>x){
                 ++x;
-            }else if(k.contains("l") && gm<x){
+            }else if(k.contains("l") && board[0]/2*-1<x){
                 --x;
             }
         }
-        int[] ans = {x,y};
-        return ans;
-        
+        board[0]=x;
+        board[1]=y;
+        return board;
     }
 }
