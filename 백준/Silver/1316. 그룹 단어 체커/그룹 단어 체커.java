d3d8a024fd4409cfc9f8a320cf14daadb37300e1
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-18 21:57:19 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -13,12 +13,9 @@ class Main{
     public static int comp(String str){
         // 전달 받은게 합성 문자열인지 아닌지만 판별해서 맞으면 1, 아니면 0반환
         // 소문자만 나온다 97~122
-        // true가 나오면 return 0 반복문이 잘 끝나면 return 1
+        // 배열에 1이상이면  return 0 반복문이 잘 끝나면 return 1
         boolean[] arr = new boolean[26];
-        int len = str.length();
-        if(len==1)
-            return 1;
-      
+        int len = str.length();      
         for(int i=0; i<len; i++){
             int c = str.charAt(i)-'a';
             if(len !=  i+1){
