eb2b688bc12503df71d636fcdabc0976215b4b3f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-18 11:27:22 +0900

    Update 단어 공부.java

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -5,22 +5,16 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine().toLowerCase();
         int[] cl = new int[26];
-        int max=0, cnt=0;
-        char result =' ';
+        int max = 0;
+        int result =' ';
         
         for(char c : str.toCharArray()){
             if(max<++cl[c-'a']){
                 max = cl[c-'a'];
-                result = c;
-                cnt=32;
+                result = c-32;
             }else if(max == cl[c-'a'])
-                cnt ++;
+                result = 63;
         }
-        if(cnt==32)
-            System.out.println((char)(result-cnt));
-        else
-            System.out.println("?");
-        
-        
+            System.out.println((char)result);
     }
 }
