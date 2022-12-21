17cd1d127d63063c87088d24f86aeb06632cc3dc
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-21 22:10:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,17 +4,17 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(br.readLine());
-        int cnt = 0 ;
+        int three = 0 ;
         while(true){
-            if(k%5 == 0){
-                System.out.println(k/5+cnt);
-                break;
+            if(k%5==0){
+                System.out.println(k/5+three);
+                return;
             }else if(k<0){
                 System.out.println(-1);
-                break;
+                return;
             }
             k-=3;
-            cnt++;
+            three++;          
         }
     }
 }
