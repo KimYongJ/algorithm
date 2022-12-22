c50cf64ba99e9ffb4c3dd549ecc8cbe1b22aaca5
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-22 20:47:36 +0900

    [Silver V] Title: 소수 찾기, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        String[] str = br.readLine().split(" ");
+        int cnt = 0;
+        for(String s : str)
+            if(check(s))
+                cnt++;
+        
+        System.out.println(cnt);
+    }
+    public static boolean check(String s){
+        int num = Integer.parseInt(s);
+        int cnt = 0;
+        if(num==1)
+        	return false;
+        for(int i=2; i<num; i++)
+            if(num%i==0)
+                return false;
+
+        return true;
+    }
+}
