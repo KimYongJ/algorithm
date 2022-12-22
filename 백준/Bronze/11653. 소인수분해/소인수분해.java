2f113922eadbb84b5f537c26272d7e34e6d4b779
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-22 22:44:52 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,36 +3,15 @@ import java.io.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int num = Integer.parseInt(br.readLine()),i=2;
-        if(num==1)return;
-        if(check(num)){
-            System.out.println(num);
-            return;
-        }
+        int num = Integer.parseInt(br.readLine());
         
-        while(true){
-            if(!check(i)){
-                i++;
-                continue;
-            }
-            if(num/i==1 && num%i==0){
-                sb.append(i).append("\n");
-                break;
-            }
-            if(num%i==0){
-                num /= i;
-                sb.append(i).append("\n");
-            }else
-                i++;
-        }
-        System.out.println(sb);
-    }
-    public static boolean check(int num){
         for(int i=2; i*i<=num; i++){
-            if(num%i==0)
-                return false;
+            while(num%i==0){
+                System.out.println(i);
+                num /= i;
+            }
         }
-        return true;
+        if(num!=1)
+            System.out.println(num);        
     }
 }
