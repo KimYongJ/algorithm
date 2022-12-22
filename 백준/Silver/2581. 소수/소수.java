bf1fb0f34723b7652a30b37082062ad346b89208
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-22 20:59:27 +0900

    [Silver V] Title: 소수, Time: 144 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int start = Integer.parseInt(br.readLine());
+        int end = Integer.parseInt(br.readLine());
+        int min = 10000;
+        int max = 0;
+        for(int i=start; i<=end; i++){
+            if(check(i)){
+                max += i;
+                if(min>i) min = i;
+            }
+        }
+        if(max==0){
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+    public static boolean check(int num){
+        if(num==1) return false;
+        for(int i=2; i<num; i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+}
