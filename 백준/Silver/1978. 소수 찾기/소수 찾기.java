2a4c0b87fd29db1f65f869ed1f14763449a278ce
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-22 21:10:59 +0900

    [Silver V] Title: 소수 찾기, Time: 120 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -17,7 +17,7 @@ class Main{
         int cnt = 0;
         if(num==1)
         	return false;
-        for(int i=2; i<num; i++)
+        for(int i=2; i*i<=num; i++)
             if(num%i==0)
                 return false;
 
