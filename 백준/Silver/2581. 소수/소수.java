972c997a704e8105c268b79151c38442d2150b1d
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-22 21:11:28 +0900

    [Silver V] Title: 소수, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.java
+++ 백준/Silver/2581. 소수/소수.java
@@ -22,7 +22,7 @@ class Main{
     }
     public static boolean check(int num){
         if(num==1) return false;
-        for(int i=2; i<num; i++){
+        for(int i=2; i*i<=num; i++){
             if(num%i==0) return false;
         }
         return true;
