aca703420f84a7a4a88a43e90e75f402010acb38
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 02:13:33 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 252 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+class Main{
+    public static boolean[] check = new boolean[10001];
+    public static void main(String[] args)throws Exception{
+        checked();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int len = Integer.parseInt(br.readLine());
+        for(int i=0; i<len; i++){
+            int n = Integer.parseInt(br.readLine());
+            int a=0,b=10000;
+            for(int j=2;j<=n/2;j++){
+                if(check[j] && check[n-j]){
+                    if(b-a>Math.abs(n-2*j)){
+                        if(n-j > j){
+                            a = j;
+                            b = n-j;
+                        }else{
+                            a = n-j;
+                            b = j;
+                        }
+                    }
+                }      
+            }
+            sb.append(a).append(" ").append(b).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void checked(){
+        for(int x=2; x<10001; x++){
+            boolean c = true;
+            for(int i=2; i*i<=x; i++){
+                if(x%i==0){ 
+                    c = false;
+                    break;
+                }
+            }
+            check[x] = c;
+        }
+    }
+}
