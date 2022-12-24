8646b7cf06dfa868ca7a6b3548bf38fbfdffd8c0
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 01:16:21 +0900

    [Silver II] Title: 베르트랑 공준, Time: 180 ms, Memory: 15168 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,22 +1,29 @@
 import java.io.*;
 import java.util.*;
-
 class Main{
+    public static int[] count = new int[246913];
     public static void main(String[] args)throws Exception{
+        countFunction(); // 시작부터 소수판별해서 저장함
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = 1;
         while((n = Integer.parseInt(br.readLine()))!=0){
-            int cnt = 0;
-            for(int i=n+1; i<=2*n; i++)
-                if(check(i)) cnt++;
-            sb.append(cnt).append("\n");
+            sb.append(count[2*n] - count[n]).append("\n");
         }
         System.out.println(sb);
     }
+    public static void countFunction(){
+        int countNum = 0;
+        for(int i=2; i<=246912; i++){
+            if(check(i))
+                countNum++;            
+            count[i] = countNum;
+        }
+    }
     public static boolean check(int n){
-        for(int i=2; i*i<=n; i++)
+        for(int i=2; i*i<=n; i++){
             if(n%i==0) return false;
+        }
         return true;
     }
 }
