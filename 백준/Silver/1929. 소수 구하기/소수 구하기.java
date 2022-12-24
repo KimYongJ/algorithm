19ba3af69f2b2b94bdfdb22f49273f53e5c0545d
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-24 10:19:47 +0900

    [Silver III] Title: 소수 구하기, Time: 468 ms, Memory: 20332 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,14 +1,16 @@
 import java.io.*;
 import java.util.*;
 class Main{
+    static boolean[] tf;
     public static void main(String[] args)throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
+        tf = new boolean[y+1];
         for(int i=x; i<=y; i++)
-            if(check(i))
+            if(check(i) || tf[i])
                 sb.append(i).append("\n");
         System.out.println(sb);
     }
@@ -17,6 +19,7 @@ class Main{
         for(int i=2; i*i<=n; i++)
             if(n%i==0)
                 return false;
+        tf[n]=true;
         return true;
     }
 }
