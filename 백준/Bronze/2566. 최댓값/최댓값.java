273cbdeab21766bbc43d7cda509ce52a0ad1f996
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 12:27:00 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 16112 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int max=0, n=0, r=0, h=0;
+        for(int i=0; i<9; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<9; j++){
+                n = Integer.parseInt(st.nextToken());
+                if(max<n){
+                    max = n;
+                    r = i;
+                    h = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(Integer.toString(r+1)+" "+Integer.toString(h+1));  
+    }
+}
