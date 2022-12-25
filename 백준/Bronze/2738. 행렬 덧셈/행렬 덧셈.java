f2ef7d4ca8db9e2e6338b9f497152348bfe34784
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 12:11:21 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 168 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -9,17 +9,17 @@ class Main{
         int m = Integer.parseInt(st.nextToken());
         int[][] result = new int[n][m];
         StringBuilder sb = new StringBuilder();
+        
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<m; j++)
                 result[i][j] = Integer.parseInt(st.nextToken());
         }
+        
         for(int y=0; y<n; y++){
             st = new StringTokenizer(br.readLine());
-            for(int z=0; z<m; z++){
-                result[y][z] += Integer.parseInt(st.nextToken());
-                sb.append(result[y][z]).append(" ");
-            }
+            for(int z=0; z<m; z++)
+                sb.append(result[y][z]+Integer.parseInt(st.nextToken())).append(" ");
             sb.append("\n");
         }
         System.out.println(sb);
