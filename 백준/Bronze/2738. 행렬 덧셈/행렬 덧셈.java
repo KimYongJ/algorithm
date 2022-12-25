a0faa38986c89d06081416f4a567fbd4d850d794
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 12:05:27 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 176 ms, Memory: 15704 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,21 +1,23 @@
 import java.io.*;
+import java.util.*;
 import java.util.stream.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int[] arr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[]arr = {Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())};
         int[][] result = new int[arr[0]][arr[1]];
-        for(int x=0; x<2;x++){
-            for(int i=0; i<arr[0]; i++){
-                int[] plus = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                for(int j=0; j<arr[1]; j++){
-                    result[i][j] +=plus[j];
-                }
+        for(int i=0; i<arr[0]; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<arr[1]; j++){
+                result[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         for(int y=0; y<arr[0]; y++){
+            st = new StringTokenizer(br.readLine());
             for(int z=0; z<arr[1]; z++){
+                result[y][z] += Integer.parseInt(st.nextToken());
                 sb.append(result[y][z]).append(" ");
             }
             sb.append("\n");
