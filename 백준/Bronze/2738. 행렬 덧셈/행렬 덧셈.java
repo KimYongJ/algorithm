b07f345aa5891a55e86b8accfbb42000ebb32499
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 12:07:14 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 172 ms, Memory: 15812 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -6,17 +6,18 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[]arr = {Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())};
-        int[][] result = new int[arr[0]][arr[1]];
-        for(int i=0; i<arr[0]; i++){
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] result = new int[n][m];
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<arr[1]; j++){
+            for(int j=0; j<m; j++){
                 result[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int y=0; y<arr[0]; y++){
+        for(int y=0; y<n; y++){
             st = new StringTokenizer(br.readLine());
-            for(int z=0; z<arr[1]; z++){
+            for(int z=0; z<m; z++){
                 result[y][z] += Integer.parseInt(st.nextToken());
                 sb.append(result[y][z]).append(" ");
             }
