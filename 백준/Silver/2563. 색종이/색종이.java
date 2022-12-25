e44ffe9db16e08a734c48442388b384983146591
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 21:42:13 +0900

    Update 색종이.java

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,30 +1,24 @@
 import java.io.*;
 import java.util.*;
 class Main{
+    public static boolean[][] s = new boolean[100][100];
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int len = Integer.parseInt(br.readLine());
-        boolean[][] s = new boolean[100][100];
         for(int l=0; l<len; l++){
             st = new StringTokenizer(br.readLine());
-            s = check(st.nextToken(),st.nextToken(),s);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for(int i=x;i<x+10;i++)
+                for(int j=y; j<y+10;j++)
+                    s[i][j]=true;
         }
-        System.out.println(count(s));
-    }
-    public static boolean[][] check(String a,String b,boolean[][] s){
-        int x = Integer.parseInt(a);
-        int y = Integer.parseInt(b);
-        for(int i=x;i<x+10;i++)
-            for(int j=y; j<y+10;j++)
-                s[i][j]=true;
-        return s;
-    }
-    public static int count(boolean[][] s){
+        
         int cnt = 0; 
         for(int i=0;i<100;i++)
             for(int j=0; j<100;j++)
                 if(s[i][j]) cnt++;
-        return cnt;
+        System.out.println(cnt);
     }
 }
