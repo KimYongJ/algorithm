c0fca9fe5c66dd65f51d57f916dac2dd7c1d6602
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-25 22:06:39 +0900

    Update 색종이.java

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -5,20 +5,18 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int len = Integer.parseInt(br.readLine());
+        int len = Integer.parseInt(br.readLine()), cnt = 0;
         for(int l=0; l<len; l++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             for(int i=x;i<x+10;i++)
                 for(int j=y; j<y+10;j++)
-                    s[i][j]=true;
+                    if(!s[i][j]){
+                        s[i][j]=true;
+                        cnt++;
+                    }
         }
-        
-        int cnt = 0; 
-        for(int i=0;i<100;i++)
-            for(int j=0; j<100;j++)
-                if(s[i][j]) cnt++;
         System.out.println(cnt);
     }
 }
