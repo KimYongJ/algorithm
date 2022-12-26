80a081ff54d9054073f915f3e7e3bec9d8517edb
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-26 19:42:52 +0900

    [Bronze II] Title: 대표값2, Time: 148 ms, Memory: 16080 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum=0;
+        short[] r = new short[5];
+        for(int i=0;i<5;i++){
+            short data = Short.parseShort(br.readLine());
+            sum+=data;
+            r[i] = data;
+        }
+        Arrays.sort(r);
+        System.out.println(sum/5+"\n"+r[2]);
+    }
+}
