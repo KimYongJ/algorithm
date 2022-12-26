11a7d0b1ce4c3ac9f15ae09968d2de3f1f710dc4
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-26 19:59:10 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -4,9 +4,12 @@ import java.util.stream.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] n = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String a = br.readLine();
+        int x = Integer.parseInt(a.split(" ")[0]);
+        int y = Integer.parseInt(a.split(" ")[1]);
+        //int[] n = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] score = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         Arrays.sort(score);
-        System.out.println(score[n[0]-n[1]]);        
+        System.out.println(score[x-y]);        
     }
 }
