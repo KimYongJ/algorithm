16ab48d0d4208082a7c291f1aa56c9d13ac34900
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-29 23:34:55 +0900

    [Silver V] Title: 나이순 정렬, Time: 464 ms, Memory: 41704 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -5,22 +5,22 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int l = Integer.parseInt(br.readLine());
-        String[][] str = new String[l][2];
-        for(int q=0; q<l; q++){
-            String[] s = br.readLine().split(" ");
-            str[q][0]=s[0];
-            str[q][1]=s[1];
+        StringBuilder[] sbArray = new StringBuilder[201];
+        for(int i=0; i<sbArray.length; i++)
+            sbArray[i] = new StringBuilder();
+        
+        StringTokenizer st;
+        for(int j=0; j<l; j++){
+            st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            sbArray[age].append(age).append(" ").append(st.nextToken()).append("\n");
         }
         
-        Arrays.sort(str,new Comparator<String[]>(){
-            public int compare(String[] a,String[] b){
-					return Integer.parseInt(a[0]) - Integer.parseInt(b[0]);
-            }
-        });
-
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<l; i++)
-            sb.append(str[i][0]+" "+str[i][1]+"\n");
-        System.out.println(sb);          
+        for(StringBuilder s : sbArray)
+            sb.append(s);
+        
+        System.out.println(sb);
+        
     }
 }
