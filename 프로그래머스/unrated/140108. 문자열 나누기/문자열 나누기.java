f594b0ede4abb19c143cf4a9844a7f2c252805a6
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-30 10:28:02 +0900

    [unrated] Title: 문자열 나누기, Time: 0.44 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,23 +1,23 @@
 class Solution {
     public int solution(String s) {
         char[] cl = s.toCharArray();
-        int cnt=0,left=1, right=0;
+        int cnt=0,dum=1, l= cl.length;
         char comp=cl[0];
         
-        for(int i=1; i<cl.length; i++){
+        for(int i=1; i<l; i++){
             if(comp==cl[i]){
-                left++;
+                dum++;
             }else{
-                right++;
+                dum--;
             }
-            if(left==right){
-                cnt++;left=0;right=0;
-                if(i<cl.length-1){
+            if(dum==0){
+                cnt++;
+                if(i<l-1){
                     comp=cl[i+1];
                 }
             }
         }
-        if(!(left==right))
+        if(dum!=0)
             cnt++;
         
         return cnt;
