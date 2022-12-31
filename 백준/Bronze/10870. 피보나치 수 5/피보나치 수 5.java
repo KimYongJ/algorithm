53f4bfca4f958feb456e2e5b6a41dfa026c26cf4
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-31 22:28:14 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(p(Integer.parseInt(br.readLine())));
+    }
+    public static int p(int n){
+        int r1 = 0;
+        int r2 = 1;
+        for(int i=0; i<n; i++){
+            int x = r2;
+            r2+=r1;
+            r1 = x;
+        }
+        return r1;
+    }
+}
