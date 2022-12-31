31a7db39976e551289d5cf3b915a5e343db337a7
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-31 15:49:20 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.49 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] s) {
         int[] result = new int[s.length];
-        int[] rank = new int[k];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int i=0;i< s.length; i++){
-            if(i<k)
-                rank[0]=s[i];
-            else
-                if(rank[0]<s[i])
-                    rank[0] = s[i];
-            Arrays.sort(rank);
-            result[i] = i<k ? rank[k-1-i] : rank[0];
+        for(int i=0; i<s.length; i++){
+            pq.add(s[i]);
+            if(pq.size()>k)
+                pq.poll();
+            result[i] = pq.peek();
         }
-        
         
         return result;
     }
