9ef8aec3f5b5b664f9f58027257933f8c8236d67
김용주 <106525587+KimYongJ@users.noreply.github.com>
2022-12-31 22:16:52 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -3,15 +3,10 @@ import java.io.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(fac(n));
+        System.out.println(fac(Integer.parseInt(br.readLine())));
     }
     public static int fac(int n){
-        if(n==1 || n==0){
-            return 1;
-        }
-        else{
-            return fac(n-1)*n; 
-        }
+        if(n==1 || n==0) return 1;
+        else return fac(n-1)*n;
     }
 }
