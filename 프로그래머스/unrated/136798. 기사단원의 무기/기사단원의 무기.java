f462c6332ba73f4caa7b2a6d6c804d969ab466ad
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-02 22:47:47 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.44 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,22 +1,14 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int c = 0, s = 0;
-        for(int i=0; i<number; i++){
-            c = measure(i+1);
-            if(c>limit)
-                c=power;
-            s+=c;
+        int s = 0;
+        for(int i=1; i<=number; i++){
+            int c = 0;
+            for(int j=1; j*j<=i; j++){
+                 if(j*j==i) c++;
+                 else if(i%j==0) c+=2;
+            }
+            s+= c>limit ? power : c;
         }        
         return s;
-    }
-    public int measure(int m){
-        int c = 0;
-        for(int i=1; i*i<=m; i++)
-            if(m%i==0){
-                c++;
-                if(i*i<m)
-                    c++;
-            }
-        return c;
     }
 }
