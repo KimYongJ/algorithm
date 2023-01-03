4845ca11dba6ce5f82ffa2fdbf96f1225a10ea30
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-04 00:00:56 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+    }
+}
