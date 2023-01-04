5eb51d420aaec26038b8abfc679852c05f097cea
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-04 23:27:02 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int s) {
+        int rock = 0;
+        while(s>0){
+            int m = s%10;
+            if(m == 5){
+                int x = check(s/10);
+                if(x>=5){
+                    rock += 10-m;
+                    s/=10;
+                    s++;
+                }else{
+                    rock += m;
+                    s/=10;
+                }
+            }else if(m >= 5){
+                rock += 10-m;
+                s/=10;
+                s++;
+            }
+            else{
+                rock += m;
+                s/=10;
+            }
+        }
+        return rock;
+    }
+    public int check (int x){
+        return x%10;
+    }
+}
