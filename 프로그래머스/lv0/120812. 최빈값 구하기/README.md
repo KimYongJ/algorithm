cb289eab8acb86af939aafccecff189ba06fff8c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-07 15:54:38 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.10 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/README.md
+++ 프로그래머스/lv0/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
 
 ### 성능 요약
 
-메모리: 73.2 MB, 시간: 0.05 ms
+메모리: 73.6 MB, 시간: 0.10 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,20 +1,40 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array) {
-        int[] countingSort = new int[1001];
-        for(int x : array)
-            countingSort[x]++;
+        int result = 0, maxCount = 0;
         
-        int max = 0, cnt = 0, result = 0;
-        for(int i=0; i<1001; i++)
-            if(max<countingSort[i]){ 
-                max = countingSort[i];
-                result = i;
-            }
+        HashMap<Integer, Integer> hm = new HashMap<>();
         
-        for(int j=0; j<1001; j++){
-            if(max == countingSort[j]) cnt++;
-            if(cnt>1) return -1;
+        for(int x : array){
+            int count = hm.getOrDefault(x,0)+1;
+            if(maxCount < count){
+                maxCount = count;
+                result = x;
+            }else if(count == maxCount)
+                result = -1;
+            hm.put(x,count);
         }
         return result;
     }
 }
+// class Solution {
+//     public int solution(int[] array) {
+//         int[] countingSort = new int[1001];
+//         for(int x : array)
+//             countingSort[x]++;
+        
+//         int max = 0, cnt = 0, result = 0;
+//         for(int i=0; i<1001; i++)
+//             if(max<countingSort[i]){ 
+//                 max = countingSort[i];
+//                 result = i;
+//             }
+        
+//         for(int j=0; j<1001; j++){
+//             if(max == countingSort[j]) cnt++;
+//             if(cnt>1) return -1;
+//         }
+//         return result;
+//     }
+// }
