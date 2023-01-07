602dc5527b2e4b4b942c043c2ea134055fc4bbb6
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-07 13:11:11 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,20 +1,40 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
-        int[] check = new int[201];
-        int[] result = new int[score.length];
-        int cnt = 1;
-        for(int[] a : score) check[a[0]+a[1]] += 1;            
+        int l = score.length;
+        List<Integer> list = new ArrayList<>();       
+        for(int i=0; i<l; i++)
+            list.add(score[i][0] + score[i][1]);
         
-        for(int i=200; i>=0; i--)
-            if(check[i] >= 1){
-                int tmp = check[i];
-                check[i] = cnt;
-                cnt += tmp;
-            }
+        Arrays.sort(list, Collections.reverseOrder());
         
-        for(int j=0; j<score.length; j++)
-            result[j] = check[score[j][0]+score[j][1]];
+        int[] result = new int[l];
+        
+        for(int j=0; j<l; j++)
+            result[i] = list.indexOf(score[i][0]+score[i][1]);
         
         return result;
     }
 }
+
+// 다른 답
+// class Solution {
+//     public int[] solution(int[][] score) {
+//         int[] check = new int[201];
+//         int[] result = new int[score.length];
+//         int cnt = 1;
+//         for(int[] a : score) check[a[0]+a[1]] += 1;            
+        
+//         for(int i=200; i>=0; i--)
+//             if(check[i] >= 1){
+//                 int tmp = check[i];
+//                 check[i] = cnt;
+//                 cnt += tmp;
+//             }
+        
+//         for(int j=0; j<score.length; j++)
+//             result[j] = check[score[j][0]+score[j][1]];
+        
+//         return result;
+//     }
+// }
