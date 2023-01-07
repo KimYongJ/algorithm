2fad46ca72e3157fa518df8f15e731de451cfa7d
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-07 22:47:41 +0900

    [Bronze I] Title: 2007년, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -6,27 +6,13 @@ class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
-        int day = y,cnt = 1;
-        while(cnt<x){
-            if(cnt==1||cnt==3||cnt==5||cnt==7||cnt==8||cnt==10) day += 31;
-            else if(cnt==4||cnt==6||cnt==9||cnt==11) day += 30;
-            else day += 28; cnt++;
-        }
-        switch(day%7){
-            case 1:
-                System.out.println("MON"); break;
-            case 2:
-                System.out.println("TUE"); break;
-            case 3:
-                System.out.println("WED"); break;
-            case 4:
-                System.out.println("THU"); break;
-            case 5:
-                System.out.println("FRI"); break;
-            case 6:
-                System.out.println("SAT"); break;
-            case 0:
-                System.out.println("SUN"); break;
-        }
+        int day = y;
+        int[] cnt = {31,28,31,30,31,30,31,31,30,31,30};
+        String[] str = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+       
+        for(int i=1; i<x; i++)
+            day += cnt[i-1];
+        
+        System.out.println(str[day%7]);
     }
 }
