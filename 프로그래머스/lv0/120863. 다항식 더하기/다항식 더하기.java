61a4183a0ecf19e7bc675e1f14c8a4446677dfc2
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-07 14:42:27 +0900

    [level 0] Title: 다항식 더하기, Time: 11.21 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -4,18 +4,18 @@ class Solution {
         int x = 0, n = 0;
         for(String s : str){
             if(s.contains("x"))
-                n += s.matches(".*[0-9].*") ?  Integer.parseInt(s.replace("x","")) : 1;
+                x += s.matches(".*[0-9].*") ?  Integer.parseInt(s.replace("x","")) : 1;
             else
-                x += Integer.parseInt(s);
+                n += Integer.parseInt(s);
         }
         String result="";
-        if(n>1){
-           result += String.valueOf(n)+"x"; 
-        }else if(n==1)
+        if(x>1){
+           result += String.valueOf(x)+"x"; 
+        }else if(x==1)
             result = "x";
         else
-            return String.valueOf(x);
-        if(x!=0) result+=" + " +String.valueOf(x);
+            return String.valueOf(n);
+        if(n!=0) result+=" + " +String.valueOf(n);
         
         return result;
     }
