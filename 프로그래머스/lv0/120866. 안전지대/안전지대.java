c0be889d4d646afa6a2f0d05e823939245eecdd8
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-07 20:49:32 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -2,26 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board) {
         int l = board.length;
-        boolean[][] check = new boolean[l][l];
         for(int i=0; i<board.length; i++)
             for(int j=0; j<board[i].length; j++)
-                if(board[i][j]==1) check[i][j] = true;
+                if(board[i][j]==1) board[i][j]=2;
         
         for(int i=0; i<board.length; i++)
             for(int j=0; j<board[i].length; j++)
-                if(check[i][j]) change(board,i,j);
-     
+                if(board[i][j]==2) 
+                    for(int x=-1;x<2; x++)
+                        for(int y=-1; y<2; y++)
+                            if(j+y>=0 && j+y<l && i+x>=0 && i+x<l && board[i+x][j+y]!=2)
+                                board[i+x][j+y] = 1;
         int r = 0;
         for(int[] n : board) for(int m : n)
-                if(m!=1) r++;
+                if(m<1) r++;
         
         return r;
-    }
-    void change(int[][] board ,int i,int j){
-        int l = board.length;
-        for(int x=-1;x<2; x++)
-            for(int y=-1; y<2; y++)
-                if(j+y>=0 && j+y<l && i+x>=0 && i+x<l)
-                    board[i+x][j+y] = 1;
     }
 }
