1db83ced4ea54c08500514e24196eac9766d0832
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-08 15:19:49 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -3,25 +3,19 @@ import java.io.*;
 class Main{
     public static void main(String... args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long[] p = new long[101];
         StringBuilder sb = new StringBuilder();
         int l = Integer.parseInt(br.readLine());
+        p[0] = 1;
+        p[1] = 1;
+        p[2] = 1;
+        for(int i = 3; i<101; i++)
+            p[i] = p[i-2]+p[i-3];
+        
         for(int i=0; i<l; i++){
-            int x = Integer.parseInt(br.readLine());
-            sb.append(find(x)).append("\n");
+            int n = Integer.parseInt(br.readLine());
+            sb.append(p[n-1]).append("\n");
         }
         System.out.println(sb);
     }
-    public static long find(int x){
-        if(x<=3) return 1;
-        long[] p = new long[] {1,1,1,2};
-        for(int i=0; i<x-4; i++){
-            long tmp = p[1]+p[2];
-            p[0]=p[1];
-            p[1]=p[2];
-            p[2]=p[3];
-            p[3]=tmp;
-        }
-        return p[3];
-    }
-    
 }
