b8b4d199effcc4a74c9d828f3319a64916d707f4
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-08 21:31:00 +0900

    [Silver V] Title: 다리 놓기, Time: 132 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int l = Integer.parseInt(st.nextToken());
+        
+        int[][] dp = new int[31][31];
+
+        for(int i=1; i<= 30; i++)
+            dp[i][1] = i;
+        
+        for(int i=2; i<=30; i++)
+            for(int j=2; j<=30; j++)
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+
+        
+        while(l-->0){
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(dp[n][m]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
