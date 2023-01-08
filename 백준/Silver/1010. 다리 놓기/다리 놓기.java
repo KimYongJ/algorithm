16f1e92391ceec35126672a44e40159b4a56faf6
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-08 22:31:52 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,27 +2,26 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    public static void main(String[] args)throws Exception{
+    static final int MAX = 31;
+    public static void main(String... args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int l = Integer.parseInt(st.nextToken());
-        
-        int[][] dp = new int[31][31];
-
-        for(int i=1; i<= 30; i++)
-            dp[i][1] = dp[i][0] = i;
-        
-        for(int i=2; i<=30; i++)
-            for(int j=2; j<=30; j++)
+        int[][] dp = new int[MAX][MAX];
+        for(int i=1; i<MAX; i++)
+            dp[i][1] = i;
+            
+        for(int i=2; i<MAX; i++)
+            for(int j=2; j<MAX; j++)
                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-
+        
         
         while(l-->0){
             st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
-            sb.append(dp[n][m]).append("\n");
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(dp[m][n]).append("\n");
         }
         System.out.println(sb);
     }
