70d0c9b5332a2c7dc18058795adc86ab04d16bca
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-08 23:31:09 +0900

    [Silver IV] Title: 보물, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -3,22 +3,24 @@ import java.util.*;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        
         int l = Integer.parseInt(br.readLine());
-        int[] a = new int[l] , b = new int[l];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<l; i++)
-            a[i] = Integer.parseInt(st.nextToken());
+            a.add(Integer.parseInt(st.nextToken()));
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<l; i++)
-            b[i] = Integer.parseInt(st.nextToken());
+            b.add(Integer.parseInt(st.nextToken()));
         
-        Arrays.sort(a);
-        b = Arrays.stream(b).boxed().sorted(Collections.reverseOrder())
-                            .mapToInt(Integer::intValue).toArray();
+        Collections.sort(a);
+        Collections.sort(b,Collections.reverseOrder());
         
         int sum = 0;
         for(int i=0; i<l; i++)
-            sum += a[i]*b[i];
+            sum += a.get(i)*b.get(i);
         
         System.out.println(sum);
         
