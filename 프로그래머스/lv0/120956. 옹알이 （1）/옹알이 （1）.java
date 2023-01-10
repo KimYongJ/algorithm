dcad221aee1b16f8e88ebfbaff2f09e0b56bb219
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-10 22:12:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.07 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -7,24 +7,12 @@ class Solution {
     public boolean check(String s){
         if(s.length()>10) return false;
         s = s.replace("woo","1").replace("aya","2").replace("ma","3").replace("ye","4");
-        // 모두 숫자여야 하며 1,2,3,4가 한개씩만 있어야한다.
+        // 모두 숫자여야 함
         try{
             Integer.parseInt(s); //숫자가 아닐경우 Exception으로 return false;
-            for(int i=1; i<5; i++){
-               int x = find(i,s);
-                if(x>=2)
-                    return false;
-            }
             return true;
         }catch(Exception e){
             return false;
         }
-    }
-    public int find(int i,String s){
-        int cnt  = 0;
-        for(char c : s.toCharArray()){
-            if(c==i-'0') cnt++;
-        }
-        return cnt;
     }
 }
