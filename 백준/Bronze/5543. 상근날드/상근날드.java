500ae0503422ebe750caa180005ad512f4017983
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-11 22:18:14 +0900

    [Bronze IV] Title: 상근날드, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main{
+    public static void main(String... args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        x = x < y ? x : y;
+        y = Integer.parseInt(br.readLine());
+        x = x < y ? x : y;
+        y = Integer.parseInt(br.readLine());
+        int z = Integer.parseInt(br.readLine());
+        System.out.println(x-50+(y<z?y:z));
+    }
+}
