753629f83f1a91201b6c19636725fdd2218cdd9e
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-11 21:53:24 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub
    [level 0] Title: 평행, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,7 +1,5 @@
 class Solution {
         public int solution(int[][] dots) {
-            int answer = 0;
-
             // 1,2 번째 점
             if((dots[0][1] - dots[1][1]) * (dots[2][0] - dots[3][0]) ==
             (dots[2][1] - dots[3][1]) * (dots[0][0] - dots[1][0]))
@@ -12,11 +10,11 @@ class Solution {
             (dots[1][1] - dots[3][1]) * (dots[0][0] - dots[2][0]))
             return 1;
 
-            // 1,4 번째 점
-            if((float)(dots[0][1] - dots[3][1]) * (dots[2][0] - dots[1][0])==
-            (float)(dots[2][1] - dots[1][1]) * (dots[0][0] - dots[3][0]))
+            // 2,4 번째 점
+            if((dots[0][1] - dots[3][1]) * (dots[2][0] - dots[1][0])==
+            (dots[2][1] - dots[1][1]) * (dots[0][0] - dots[3][0]))
             return 1;
 
-            return answer;
+            return 0;
         }
     }
