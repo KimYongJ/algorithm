61db5b9789b8d57dcdad9b05535d70c87c6b8864
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-15 16:02:50 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 22716 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,34 +2,33 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static int n,m;
-    static int[] arr = new int[9];
-    static boolean[] check = new boolean[9];
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args)throws Exception{
+    static int n,m;
+    static int[] arr;
+    static boolean[] check;
+    public static void main(String... args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        func(0);
+        arr = new int[m];
+        check = new boolean[n+1];
+        back(0);
         System.out.println(sb);
     }
-    public static void func(int k){ // 현재 k개까지 수를 택함
+    public static void back(int k){
         if(k==m){
-            for(int i=0; i<m; i++)
-                sb.append(arr[i]).append(" ");
-            
+            for(int x:arr)
+                sb.append(x).append(" ");
             sb.append("\n");
             return;
         }
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n; i++)
             if(!check[i]){
                 arr[k]=i;
                 check[i] = true;
-                func(k+1);
-                check[i]=false;
+                back(k+1);
+                check[i] = false;
             }
-        }
-        
     }
 }
