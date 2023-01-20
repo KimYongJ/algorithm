e166774547cac0819b9bce236f203df22e938250
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-20 21:43:09 +0900

    [unrated] Title: 옹알이 (2), Time: 3.25 ms, Memory: 74.2 MB -BaekjoonHub
    [unrated] Title: 옹알이 (2), Time: 3.25 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,19 +1,16 @@
 class Solution {
     public int solution(String[] b) {
         int cnt = 0;
-        String[] except = {"ayaaya","yeye","woowoo","mama"};
         String[] com = {"aya","ye","woo","ma"};
         for(String str : b){
             if(str.contains("ayaaya") || str.contains("yeye") || str.contains("woowoo") || str.contains("mama") )
                 continue;
             
             for(String x: com)
-                str = str.replaceAll(x,"2");
+                str = str.replaceAll(x," ");
             
-            boolean check = true;
-            for(char c: str.toCharArray())
-                if('2' != c) check = false;
-            if(check) cnt++;
+            if(str.replaceAll(" ","").length() == 0) 
+                cnt++;
         }
         return cnt;
     }
