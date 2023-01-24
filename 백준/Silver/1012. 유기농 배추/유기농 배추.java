67b5772fe4d7cbbaeacb6b70a7efc7bdac3757c3
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-25 00:41:00 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,56 +2,46 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
+    static int n,m;
     static int[][] arr;
-    static boolean[][] visit;
-    static int cnt,n,m;
     public static void main(String[] args)throws Exception{
-        st = new StringTokenizer(br.readLine());
-        int l = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt, l = Integer.parseInt(st.nextToken());
         while(l-->0){
             cnt = 0;
-            inputData();
-            countBeaChu();
+            // 이하 데이터 넣는 코드 
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            arr = new int[n][m];
+            int k = Integer.parseInt(st.nextToken());
+            for(int i=0; i<k; i++){
+                st = new StringTokenizer(br.readLine());
+                arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]=1;
+            }
+            // 이하 값이 1인것에 대해 dfs 실행 
+            for(int i=0; i<arr.length; i++)
+                for(int j=0; j<arr[0].length; j++)
+                    if(arr[i][j] == 1){
+                        cnt++;
+                        DFS(i,j); // 주변을 탐색해 상하좌우 인접한것 체크
+                    }
             sb.append(cnt).append("\n");
         }
         System.out.println(sb);
     }
     public static void DFS(int x, int y){
-        visit[x][y] = true;
+        arr[x][y] = 0;
         int[] dx = {0,0,-1,1};
         int[] dy = {-1,1,0,0};
         
         for(int i=0; i<4; i++){
-            int x1 = x + dx[i];
-            int y1 = y + dy[i];
-            if(x1<0 || x1>=n || y1<0 || y1>=m)
-                continue;
-            
-            if(arr[x1][y1]==1 && !visit[x1][y1])
+            int x1 = x+dx[i];
+            int y1 = y+dy[i];
+            if(!(x1<0) && !(x1>=n) && !(y1<0) && !(y1>=m) && arr[x1][y1]==1)
                 DFS(x1,y1);
-        }
-    }
-    public static void countBeaChu(){
-        for(int i=0; i<arr.length; i++)
-            for(int j=0; j<arr[0].length; j++)
-                if(arr[i][j] == 1 && !visit[i][j]){
-                    cnt++;
-                    DFS(i,j); // 주변을 탐색해 상하좌우 인접한것 체크
-                }
-    }
-    public static void inputData() throws Exception{
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        visit = new boolean[n][m];
-        int k = Integer.parseInt(st.nextToken());
-        for(int i=0; i<k; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]=1;
         }
     }
 }
