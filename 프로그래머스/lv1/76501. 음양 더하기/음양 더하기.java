b4d061070b341cc084ac66b9c2b60ef8ff9b42e1
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-26 22:42:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] a, boolean[] s) {
         int r = 0;
         for(int i=0; i<a.length; i++)
-            r += s[i] ? a[i] : a[i]*-1;        
+            r += s[i] ? a[i] : -a[i];        
         return r;
     }
 }
