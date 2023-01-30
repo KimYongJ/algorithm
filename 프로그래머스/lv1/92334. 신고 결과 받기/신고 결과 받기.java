dc5ccecdb46bce2e82a277372263e97d258b994d
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-30 22:45:04 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.20 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] list, String[] report, int k) {
+        HashMap<String,Integer> reported = new HashMap<>(); // 신고당한 사람 : 횟수
+        HashMap<String,Set<String>> reporter = new HashMap<>(); // 신고당한 사람 : 신고한사람list
+
+        int[] result = new int[list.length];
+
+        for(String item : report){
+            String a = item.split(" ")[0];
+            String b = item.split(" ")[1];
+            
+            Set<String> set = new HashSet<>();
+            if(reporter.containsKey(b)){
+                set = reporter.get(b);
+                if(set.contains(a)) continue;
+            }
+            set.add(a);
+            reporter.put(b,set);
+            
+            if(reported.containsKey(b)){
+                reported.put(b,reported.get(b)+1);
+            }else{
+                reported.put(b,1);
+            }
+        }
+        for(String item : reporter.keySet())
+            if(reported.get(item)>=k)
+                for(String name : reporter.get(item))
+                    for(int i=0; i<list.length; i++)
+                        if(name.equals(list[i])) result[i]++;
+        
+        return result;
+    }
+
+}
