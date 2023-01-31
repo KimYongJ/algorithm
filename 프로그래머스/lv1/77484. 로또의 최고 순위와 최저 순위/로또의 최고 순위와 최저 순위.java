964a77628a35ddaf6f0f071af7b6ef73f9405cb3
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-31 23:18:11 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] solution(int[] l, int[] w) {
         int[] arr = new int[2];
         
-        for(int i=0; i<6; i++){
-            if(l[i]==0) arr[1]++;
-            for(int j=0; j<6; j++){
-                if(l[i]==w[j])
+        for(int a : l){
+            if(a==0) arr[1]++;
+            for(int b : w)
+                if(a==b)
                     arr[0]++;
-            }
         }
+        
         
         int max = 7-(arr[0]+arr[1]);
         int min = 7-(arr[0]);
