18a0d9878f3b7825d5d1831640fc701099f6665f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-01-31 22:10:15 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.27 ms, Memory: 78.2 MB -BaekjoonHub
    [level 1] Title: 3진법 뒤집기, Time: 9.27 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,11 @@
 class Solution {
-    static String three = "";
     public int solution(int n) {
-        if(n<3) return n; // 3보다 작으면 바로 리턴
-        while(n>=3){
+        String three = "";
+        
+        while(n>0){
             three += n%3;
             n /= 3;
         }
-        three += n;
 
         return Integer.parseInt(three,3);
     }
