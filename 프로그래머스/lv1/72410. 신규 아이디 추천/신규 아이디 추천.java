ae41402470a6bfa37d0a4e16f3a62f659c791322
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-01 21:46:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.27 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,19 +1,55 @@
 class Solution {
     public String solution(String id) {
-        id = id.toLowerCase()
-               .replaceAll("[^a-z0-9-_.]","")
-               .replaceAll("[.]{2,}",".")
-               .replaceAll("^[.]|[.]$","");
-        if(id.length()==0)
-            id = "a";
-        else if(id.length()>15)
-            id = id.substring(0,15).replaceAll("^[.]|[.]$","");
-
-        if(id.length()<3){
-            while(id.length()<3)
-                id += id.charAt(id.length()-1);
-        }
+        return new KAKAO(id)
+                            .level_1()
+                            .level_2()
+                            .level_3()
+                            .level_4()
+                            .level_5()
+                            .level_6()
+                            .level_7()
+                            .toString();
+    }
+    public class KAKAO{
+        private String id;
         
-        return id;
+        public KAKAO(String id){
+            this.id = id;
+        }
+        public KAKAO level_1(){
+            id = id.toLowerCase();
+            return this;
+        }
+        public KAKAO level_2(){
+            id = id.replaceAll("[^a-z0-9-_.]","");
+            return this;
+        }
+        public KAKAO level_3(){
+            id = id.replaceAll("[.]{2,}",".");
+            return this;
+        }
+        public KAKAO level_4(){
+            id = id.replaceAll("^[.]|[.]$","");
+            return this;
+        }
+       public KAKAO level_5(){
+            if(id.length()==0)
+                    id = "a";
+            return this;
+        }
+       public KAKAO level_6(){
+            if(id.length()>15)
+                    id = id.substring(0,15).replaceAll("^[.]|[.]$","");
+            return this;
+        }
+        public KAKAO level_7(){
+            if(id.length()<3)
+                while(id.length()<3)
+                    id += id.charAt(id.length()-1);
+            return this;
+        }
+        public String toString(){
+            return id;
+        }
     }
 }
