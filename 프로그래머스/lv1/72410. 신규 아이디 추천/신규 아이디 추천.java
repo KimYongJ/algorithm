0edba7ff1b8e0dd6682c6afae50a30b016c8f2e7
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-01 21:32:21 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.98 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,25 +1,19 @@
 class Solution {
     public String solution(String id) {
-        id = id.toLowerCase().replaceAll("[^a-z0-9-_.]","").replaceAll("[.]{2,}",".");
-        id = deleteDot(id);
+        id = id.toLowerCase()
+               .replaceAll("[^a-z0-9-_.]","")
+               .replaceAll("[.]{2,}",".")
+               .replaceAll("^[.]|[.]$","");
         if(id.length()==0)
             id = "a";
         else if(id.length()>15)
-            id = deleteDot(id.substring(0,15));
+            id = id.substring(0,15).replaceAll("^[.]|[.]$","");
 
-        
         if(id.length()<3){
             while(id.length()<3)
                 id += id.charAt(id.length()-1);
         }
         
-        return id;
-    }
-    public String deleteDot(String id){
-        if(id.length()>0 && id.charAt(0)=='.')
-            id = id.substring(1);
-        if(id.length()>0 && id.charAt(id.length()-1)=='.')
-            id = id.substring(0,id.length()-1);
         return id;
     }
 }
