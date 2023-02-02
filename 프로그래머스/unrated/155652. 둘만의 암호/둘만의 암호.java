0ede1e42ebc8b8aa250fe5e809098e739b1217eb
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-02 21:28:15 +0900

    [unrated] Title: 둘만의 암호, Time: 13.06 ms, Memory: 79.5 MB -BaekjoonHub
    [unrated] Title: 둘만의 암호, Time: 13.06 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,28 +1,13 @@
 class Solution {
     public String solution(String s, String skip, int idx) {
         StringBuilder sb = new StringBuilder();
-        String str = "abcdefghijklmnopqrstuvwxyz";
-        for(char c : skip.toCharArray())
-            str = str.replace(c+"","");
-        int arrlen = str.length();
-        char[] arr = new char[arrlen];
-        for(int i=0; i<arrlen; i++)
-            arr[i] = str.charAt(i);
-        
         for(char c : s.toCharArray()){
-            int p = 0;
-            
-            for(int i=0; i<arrlen; i++)
-                if(arr[i]==c){
-                    p=i+(idx%arrlen);
-                    break;
-                }                
-            if(arrlen<=p){
-                p -= arrlen;
+            for(int i=0; i<idx; i++){
+                if(++c>122) c = 'a';
+                if(skip.contains(c+"")) --i;
             }
-            sb.append(arr[p]);
+            sb.append(c);
         }
-        
         return sb.toString();
     }
 }
