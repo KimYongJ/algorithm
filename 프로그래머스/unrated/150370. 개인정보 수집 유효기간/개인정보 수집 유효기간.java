168e227633515ba4b43daf69d37e2bb656363740
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-03 00:01:46 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.80 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    static HashMap<String,Integer> map = new HashMap<>();
+    public int[] solution(String t, String[] terms, String[] privacies) {
+        List<Integer> list =  new ArrayList<>();
+        for(String s : terms){
+            String[] x = s.split(" ");
+            map.put(x[0],Integer.parseInt(x[1]));
+        }
+        for(int i=0; i<privacies.length; i++){
+            int a = getDay(t);
+            int b = getDay(privacies[i].split(" ")[0]) + map.get(privacies[i].split(" ")[1])*28;
+            if(a>=b)
+                list.add(i+1);
+        }
+
+        
+        int[] result = new int[list.size()];
+        Iterator itor = list.iterator();
+        int idx = 0;
+        while(itor.hasNext()){
+            result[idx++] = (int)itor.next();
+        }
+        
+        return result;
+    }
+    public int getDay(String s){
+        StringTokenizer st = new StringTokenizer(s,"\\.");
+        int year = Integer.parseInt(st.nextToken());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+        
+        return year*12*28+month*28+day;
+    }
+}
