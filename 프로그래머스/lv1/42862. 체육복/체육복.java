1a9328734931db5a6ca83c0a0488c1561b0c71e5
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-06 21:58:01 +0900

    [level 1] Title: 체육복, Time: 0.53 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] r) {
+        Arrays.sort(lost);
+
+        boolean[] tf = new boolean[n+1];
+        for(int x : r)
+            tf[x] = true;
+
+        List<Integer> lost_list = new ArrayList<>();
+        for(int i=0; i<lost.length; i++){
+           if(tf[lost[i]]){
+               tf[lost[i]] = false;
+               continue;
+           }
+            lost_list.add(lost[i]);
+        }
+
+        int len = lost_list.size();
+        for(int i=0; i<lost_list.size(); i++){
+            int x = lost_list.get(i);
+            if(tf[x-1]){
+                tf[x-1]=false;
+                len--;
+            }else if(x+1<=n && tf[x+1]){
+                tf[x+1]=false;
+                len--;
+            }
+        }
+        return n-len;
+        
+    }
+}
