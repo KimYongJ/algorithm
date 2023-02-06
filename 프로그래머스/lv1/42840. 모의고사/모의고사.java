e1b6186feb79856d6f13e99259966eea4e0ba689
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-06 23:14:52 +0900

    [level 1] Title: 모의고사, Time: 0.72 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -2,17 +2,16 @@ import java.util.*;
 class Solution {
     static int count = 0;
     public int[] solution(int[] answers) {
-        int[] p1 = {1,2,3,4,5};
-        int[] p2 = {2,1,2,3,2,4,2,5};
-        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
-        
-        int a1 = find(p1,answers,0);    count = 0;
-        int a2 = find(p2,answers,0);    count = 0;
-        int a3 = find(p3,answers,0);
-         
-        return result(new int[] {a1,a2,a3});
-    }
-    public int[] result(int[] arr){
+        int[][] p = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
+        int[] arr = new int[3];
+        for(int i=0; i<3; i++){
+            arr[i] = find(p[i],answers,0);
+            count = 0;
+        }
         int max = 0;
         for(int i=0; i<3; i++)
             if(max<arr[i]) max = arr[i];
@@ -21,9 +20,7 @@ class Solution {
         for(int i=0; i<3; i++) if(max==arr[i]) list.add(i+1);
         
         int[] result = new int[list.size()];
-        int cnt = 0;
-        for(int x : list) result[cnt++] = x;
-
+        for(int x : list) result[count++] = x;
         return result;
     }
     public int find(int[] p,int[] answers, int idx){
