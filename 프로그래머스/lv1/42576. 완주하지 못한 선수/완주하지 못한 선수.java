f1f80b34febff960091d2ef1c57d8a2a083ca385
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-07 22:46:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.44 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String[] all, String[] str) {
-        Arrays.sort(all);
-        Arrays.sort(str);
+        String result = "";
+        HashMap<String, Integer> m = new HashMap<>();
         
-        for(int i=0; i<str.length; i++)
-            if(!all[i].equals(str[i]))
-                return all[i];
+        for(String s : all) m.put(s,m.getOrDefault(s,0)+1);
+        for(String s : str) m.put(s,m.get(s)-1);
         
-        return all[all.length-1];
+        
+        for(String s : all)
+            if(0!=m.get(s)){
+                result = s;
+                break;
+        }
+        return result;
     }
 }
