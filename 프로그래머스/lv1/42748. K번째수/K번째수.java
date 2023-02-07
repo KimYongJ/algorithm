467b22bcfc527857444545f712b6af10ebbf22ed
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-07 21:28:50 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -3,17 +3,12 @@ class Solution {
     public int[] solution(int[] arr, int[][] c) {
         int[] result = new int[c.length];
         
-        for(int i=0; i<c.length; i++)
-            result[i] = find(arr,c[i]);
+        for(int i=0; i<c.length; i++){
+            int[] copy = Arrays.copyOfRange(arr,c[i][0]-1,c[i][1]);
+            Arrays.sort(copy);
+            result[i] = copy[c[i][2]-1];
+        }
         
         return result;
-    }
-    public int find(int[] arr, int[] c){
-        int[] x = new int[c[1]-c[0]+1];
-        int idx=0;
-        for(int i=c[0]; i<=c[1]; i++)
-            x[idx++] = arr[i-1];
-        Arrays.sort(x);
-        return x[c[2]-1];
     }
 }
