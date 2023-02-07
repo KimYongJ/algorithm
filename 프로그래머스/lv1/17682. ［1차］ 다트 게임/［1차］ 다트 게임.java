5ddf7cbd4208b58bd4669020945e0ec580e4c692
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-08 01:03:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 2.20 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,45 +1,36 @@
-import java.util.*;
 class Solution {
-    public int solution(String dart) {
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        dart = dart.replaceAll("10",":")+' ';
-        for(int i=0; i<dart.length()-1; i++){
-            sb.append(dart.charAt(i));
-            char c = dart.charAt(i+1);
-            if(47<=c && c<=58 || c==' '){
-                list.add(sb.toString());
-                sb = new StringBuilder();
+  public int solution(String dartResult) {
+      String numTemp="";
+        int Answer=0;
+
+        int lastScore=0;
+        int num=0;
+
+        char next=' ';
+
+        for(int i=0;i<dartResult.length();i++){
+            next=dartResult.charAt(i);
+            if(next=='S'||next=='D'||next=='T'){
+                lastScore=num;
+                num=Integer.parseInt(numTemp);
+                if(next=='D'){
+                    num*=num;
+                }else if(next=='T'){
+                    num=num*num*num;
+                }
+                Answer+=num;
+                numTemp="";
+            }else if(next=='#'){
+                num=-num;
+                Answer+=2*num;
+            }else if(next=='*'){
+                Answer+=num+lastScore;
+                num=2*(num);
+            }else{
+                numTemp=numTemp+next;
             }
         }
-        
-        int sum = 0;
-        for(int i=list.size()-1; i>=0; i--)
-            sum += get(list.get(i));
-        
-        return sum;
-    }
-    
-    static boolean double_yn = false;
-    public int get(String str){
-        boolean check = false;
-        int num = str.charAt(0)-48;
-        for(char c : str.substring(1).toCharArray()){
-            if(c=='D')
-                num *= num;
-            else if(c=='T')
-                num *= num*num;
-            else if(c=='#')
-                num = -num;
-            else if(c=='*'){
-                num *= 2;
-                check = true;
-            }
-        }
-        if(double_yn)
-            num *= 2;
-        double_yn = check ? true : false;
-        
-        return num;
-    }
+
+      return Answer;
+  }
 }
