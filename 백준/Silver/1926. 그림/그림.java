0d9315cb5a61f17d6ca94832c1559120bcfc1901
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-14 23:26:36 +0900

    [Silver I] Title: 그림, Time: 308 ms, Memory: 32404 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -11,32 +11,32 @@ class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[x+2][y+2];
+        char[][] arr = new char[x+2][y+2];
         Queue<int[]> q = new LinkedList<>();
         for(int i=1; i<=x; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=y; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            String s = br.readLine();
+            for(int j=1, index = 0; j<=y; j++,index+=2){
+                arr[i][j] = s.charAt(index);
             }
         }
         for(int a=1; a<=x; a++)
             for(int b=1; b<=y; b++){
-                if(arr[a][b]==1){
+                if(arr[a][b]=='1'){
                 	int max1 = 1;
                     cnt++;
                     q.add(new int[]{a,b});
-                    arr[a][b]=0;
+                    arr[a][b]='0';
                     while(!q.isEmpty()){ 
                         int[] c = q.poll();
                         for(int i=0; i<4; i++){
                             int x1 = c[0]+dx[i];
                             int y1 = c[1]+dy[i];         
                             if(x1<1 || y1<1 || x1>x || y1>y || 
-                                arr[x1][y1]==0){
+                                arr[x1][y1]=='0'){
                                 continue;
                             }
                             q.add(new int[]{x1,y1});
-                            arr[x1][y1]=0;
+                            arr[x1][y1]='0';
                             max1++;
                         }
                     }
