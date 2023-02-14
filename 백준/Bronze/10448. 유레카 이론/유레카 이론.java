fb159b91fa30e776ac07ccd2860a3f57536336ca
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-14 21:19:47 +0900

    [Bronze I] Title: 유레카 이론, Time: 152 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 class Main{
     static int num,result;
+    static int[] arr = new int[3];
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -14,7 +15,7 @@ class Main{
         }
         System.out.print(sb);
     }
-    public static void search(int depth, int[] arr,int max){
+    public static void search(int depth, int max){
         if(depth==3){
             if(arr[0]+arr[1]+arr[2]==num)
                 result = 1;
@@ -22,14 +23,15 @@ class Main{
         }
         for(int i=1; i<=max;i++){
             arr[depth] = eureka(i);
-            search(depth+1,arr,max);
+            search(depth+1,max);
+            if(result==1)
+                return;
         }
     }
     public static void check(int x){
-        int[] arr = new int[3];
         for(int i=1; i<x; i++)
             if(eureka(i)>=x){
-                search(0,arr,i-1); 
+                search(0,i-1); 
                 break;
             }
     }
