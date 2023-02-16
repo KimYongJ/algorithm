9b258afb9ecb5edfdd0a3b2a93a3b30af923685c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-16 22:11:59 +0900

    [Gold V] Title: 토마토, Time: 388 ms, Memory: 56872 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,49 +2,61 @@ import java.io.*;
 import java.util.*;
 
 class Main{
+    static BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
     static ArrayDeque<int[]> q = new ArrayDeque<>();
     static int[][] dxy = {{0,1},{0,-1},{1,0},{-1,0}};
     static char[][] arr;
     static int x,y , result = 0 , noneTomato = 0;
 
     public static void main(String[] args)throws Exception{
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        x = Integer.parseInt(st.nextToken());
         y = Integer.parseInt(st.nextToken());
-        arr = new char[y][x];
-        for(int i=0; i<y; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<x; j++){
-                arr[i][j] = st.nextToken().charAt(0);
+        x = Integer.parseInt(st.nextToken());
+        arr = new char[x][y];
+        for(int i=0; i<x; i++){
+            for(int j=0; j<y; j++){
+                arr[i][j] = read();
                 if(arr[i][j]=='1'){
-                    result++;
                     q.add(new int[]{i,j,0});
-                }else if(arr[i][j]=='0') noneTomato++;
+                }
             }
         }
         if(result==x*y){
             System.out.println(0);
             return;
         }
-        // i = y , j = x 이다. {y,x,dist}
         while(!q.isEmpty()){
             int[] qData = q.poll();
             for(int[] d : dxy){
-                int y1 = d[0]+qData[0];
-                int x1 = d[1]+qData[1];
+                int x1 = d[0]+qData[0];
+                int y1 = d[1]+qData[1];
                 int dist = qData[2]+1;
                 
                 if(y1<0 || x1<0 || y1>=y || x1>=x ||
-                   arr[y1][x1]=='1' || arr[y1][x1]=='-')
+                   arr[x1][y1]=='1' || arr[x1][y1]=='-')
                     continue;
-                arr[y1][x1]='1';
+                arr[x1][y1]='1';
                 noneTomato--;
-                q.add(new int[]{y1,x1,dist});
+                q.add(new int[]{x1,y1,dist});
                 
             }
             result = qData[2];
         }
         System.out.println(noneTomato==0 ? result : -1);
     }
+    private static char read() throws IOException {
+		while(true) {
+			int now = br.read();
+            if(now=='-'){
+                br.read();
+                return (char)now;
+            }else if(now=='0'){
+                noneTomato++;
+                return (char)now;
+            }else if(now=='1'){
+                result++;
+                return (char)now;
+            }
+		}
+	}
 }
