999d045a3d06471419b4f045ae78fcd2074d2c3f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-02-19 20:16:16 +0900

    [Gold IV] Title: 불!, Time: 604 ms, Memory: 69972 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,68 +1,110 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Time;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args)throws Exception{
-        ArrayDeque<int[]> j_q = new ArrayDeque<>();
-        ArrayDeque<int[]> f_q = new ArrayDeque<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[][] dxy = {{0,1},{0,-1},{1,0},{-1,0}};
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        char[][]arr = new char[x][y];
-        for(int i=0; i<x;i++)
-            for(int j=0; j<y; j++){
-                arr[i][j] = read();
-                if(arr[i][j]=='J'){
-                    j_q.add(new int[]{i,j,0});
-                }else if(arr[i][j]=='F'){
-                    f_q.add(new int[]{i,j});
-                }
-            }
-        
-        while(!j_q.isEmpty()){
-            int fire_q_len = f_q.size();
-            for(int i=0; i<fire_q_len; i++){
-                int[] f_qData = f_q.poll();
-                for(int[] xy : dxy){
-                    int nx = f_qData[0] + xy[0];
-                    int ny = f_qData[1] + xy[1];
-					if(0 <= nx && nx < x && 0 <= ny && ny < y) {
-						if(arr[nx][ny] != '#' && arr[nx][ny] !='F') {
-						    arr[nx][ny]='F';
-                            f_q.add(new int[]{nx,ny});
+public class Main {
+	public static int n, m, ans;
+	public static int[] dx = {-1, 1, 0, 0};
+	public static int[] dy = {0, 0, -1, 1};
+	public static char[][] map;
+	public static class Node {
+		int x;
+		int y;
+		int time;
+		public Node(int x, int y, int time) {
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
+	}
+	public static Queue<Node> jh, fire;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		jh = new LinkedList<>();
+		fire = new LinkedList<>();
+		
+		
+		map = new char[n][m];
+		for(int i=0;i<n;i++) {
+			map[i] = br.readLine().toCharArray();
+			for(int j=0;j<m;j++) {
+				if(map[i][j] == 'J') {
+					jh.add(new Node(i, j, 0));
+				}
+				
+				if(map[i][j] == 'F') {
+					fire.add(new Node(i, j, 0));
+				}
+			}
+		}
+		
+		ans = 0;
+		
+		if(bfs()) {
+			System.out.println("IMPOSSIBLE");
+		} else {
+			System.out.println(ans);
+		}
+	
+		
+		
+		
+	}
+	
+	public static boolean bfs() {
+		// 불 먼저
+		while(!jh.isEmpty()) {
+			int f_size = fire.size();
+			for(int i=0;i<f_size;i++) {
+				Node node = fire.poll();
+				
+				for(int d=0;d<4;d++) {
+					int nx = node.x + dx[d];
+					int ny = node.y + dy[d];
+					if(0 <= nx && nx < n && 0 <= ny && ny < m) {
+						if(map[nx][ny] != '#' && map[nx][ny] !='F') {
+							map[nx][ny] = 'F';
+							fire.add(new Node(nx, ny, node.time+1));
 						}
 					} 
-                }
-            }
-            int jihun_q_len = j_q.size();
-            for(int i=0; i<jihun_q_len; i++){
-                int[] j_qData = j_q.poll();
-                for(int[] xy : dxy){
-                    int nx = j_qData[0] + xy[0];
-                    int ny = j_qData[1] + xy[1];
-                    int dist = j_qData[2] + 1;
-
-                    if(nx < 0 || nx >= x || ny < 0 || ny >= y) {
-                        System.out.println(dist);
-                        return;
+				}
+				
+			}
+			
+			int j_size = jh.size();
+			
+			for(int i=0;i<j_size;i++) {
+				Node node = jh.poll();
+				
+				for(int d=0;d<4;d++) {
+					int nx = node.x + dx[d];
+					int ny = node.y + dy[d];
+					if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+						ans = node.time+1;
+						return false;
 					}
-					if(arr[nx][ny] == '.') {
-							j_q.add(new int[]{nx,ny,dist});
-							arr[nx][ny] = 'J';
+					
+					
+					if(map[nx][ny] != '#' && map[nx][ny] !='F' && map[nx][ny] != 'J') {
+							jh.add(new Node(nx, ny, node.time+1)); 
+							map[nx][ny] = 'J';
 					}
-                }
-            }
-        }
-        System.out.println("IMPOSSIBLE");
-    }
+					
+				}
+				
+			}
+			
+		}
+		return true;
 
-    public static char read()throws Exception{
-        while(true){
-            int data = br.read();
-            if(data=='#' || data=='.' || data=='J' || data=='F')
-                return (char)data;
-        }
-    }
+	}
+
 }
