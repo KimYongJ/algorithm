a4226329e501adeb5e914c73ba3df5bedd3410bd
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-03-03 10:31:25 +0000

    [D1] Title: N줄덧셈, Time: 123 ms, Memory: 20,540 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println((x*(x+1))/2);
+    }
+}
