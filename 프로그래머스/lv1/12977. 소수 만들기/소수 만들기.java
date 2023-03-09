51738d0573b9595c983702665a58841e07d3d435
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-03-09 22:59:45 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -3,24 +3,16 @@ class Solution {
     static boolean[] bool = new boolean[1001];
     static int result;
     public int solution(int[] nums) {
-        
-        check(nums,0,new int[3]);
-
-        return result/6;
-    }
-    public void check(int[] nums,int depth,int[] arr){
-        if(depth==3){
-            if(prime(arr[0]+arr[1]+arr[2])) result++;
-            return;
-        }
-        for(int i=0; i<nums.length; i++){
-            if(!bool[nums[i]]){
-                arr[depth] = nums[i];
-                bool[nums[i]] = true;
-                check(nums,depth+1,arr);
-                bool[nums[i]] = false;
+        int len = nums.length;
+        for(int a=0; a<len-2; a++){
+            for(int b=a+1; b<len-1; b++){
+                for(int c=b+1; c<len; c++){
+                    if(prime(nums[a]+nums[b]+nums[c]))
+                        result++;
+                }
             }
         }
+        return result;
     }
     public boolean prime(int n){
         if(n<2) return false;
