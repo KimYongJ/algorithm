99c163c913b21e4fd75bfe50a2be438dec6b85f8
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-03-10 23:29:08 +0900

    [D1] Title: 아주 간단한 계산기, Time: 99 ms, Memory: 17,684 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Solution{
+ 	public static void main(String... args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        
+    }
+}
