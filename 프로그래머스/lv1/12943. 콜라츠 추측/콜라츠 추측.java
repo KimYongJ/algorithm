03032575c54c76413214ce76b890ebffe66043ec
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-03-12 21:14:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num) {
+        if(num==1) return 0;
+        return find(0,num);
+    }
+    public int find(int depth, long num){
+        if(depth==500) return -1;
+        else if(num==1) return depth;
+
+        num = num%2==0 ? num/2 : num*3+1;
+        
+        return find(depth+1,num);
+    }
+}
