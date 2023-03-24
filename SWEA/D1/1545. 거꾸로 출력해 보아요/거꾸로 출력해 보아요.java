1f6d96d4135d6ed5a6892164d08177d2ddc62a49
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-03-25 00:37:26 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 129 ms, Memory: 20,916 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution{
+ 	public static void main(String[] args){
+     	Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        for(int i=x; i>=0; i--)
+            System.out.print(i+" ");
+        
+    }
+}
