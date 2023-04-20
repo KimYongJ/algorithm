0907dcba3f8683b176b941bd54d8c3e0bd76ee5f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-04-20 23:21:59 +0900

    [unrated] Title: l로 만들기, Time: 20.02 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,10 +1,5 @@
 class Solution {
     public String solution(String str) {
-        StringBuilder sb = new StringBuilder();
-        
-        for(char c : str.toCharArray())
-            sb = c<'l' ? sb.append('l') : sb.append(c);
-        
-        return sb.toString();
+        return str.replaceAll("[^l-z]", "l");
     }
 }
