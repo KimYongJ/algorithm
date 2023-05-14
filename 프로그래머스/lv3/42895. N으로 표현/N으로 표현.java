c31ef55557d1a16ef67301ecf2e3c766a9d83949
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-05-14 15:16:28 +0900

    [level 3] Title: N으로 표현, Time: 0.09 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int solution(int N, int number) {
+        Set<Integer>[] set = new HashSet[9];
+        int n = 0;
+        for(int i=1; i<9; i++){
+            n = (n*10)+N;
+            set[i] = new HashSet<>();
+            set[i].add(n);
+        }
+        
+        for(int i=1; i<9; i++){
+            for(int j=1; j<i; j++){
+                for(int a : set[j]){
+                    for(int b: set[i-j]){
+                        set[i].add(a+b);
+                        set[i].add(a-b);
+                        set[i].add(a*b);
+                        if(a!=0)
+                            set[i].add(b/a);
+                        if(b!=0)
+                            set[i].add(a/b);
+                    }
+                }
+            }
+            if(set[i].contains(number))
+                return i;
+        }
+        
+        return -1;
+    }
+}
