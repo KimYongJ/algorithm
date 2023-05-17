c736ebb99e9234bf671e6512c07b5f25b10f6a00
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-05-17 22:38:37 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean check = true;
+        for(char c : s.toCharArray()){
+            if(c == ' '){
+                check = true;
+                sb.append(" ");
+                continue;
+            }
+            if(check){
+                c = Character.toUpperCase(c);
+                check = false;
+            }else{
+                c = Character.toLowerCase(c);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
