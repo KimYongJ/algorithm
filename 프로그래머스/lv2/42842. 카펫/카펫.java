cd6ba7081aec40eeb80ca9fc925952860bbfd8ad
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-06-18 20:50:12 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int row = 0;
+        int[] result = new int[2];
         for(int i=yellow; i>0; i--){
-            row = yellow/i;
-            if(yellow%i!=0) continue;
-            if(brown/2 == i+2+row){
-                return new int[]{i+2, row+2};
+            int row = yellow/i;
+            if(yellow%i!=0) 
+                continue;
+            else if(brown/2 == i+2+row){
+                result = new int[]{i+2, row+2};
+                break;
             }
             
         } 
-        return new int[]{0,0};
+        return result;
     }
 }
