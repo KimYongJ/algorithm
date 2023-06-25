0bcad5e161fa3f94673e228bd80c1c333b121def
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-06-25 21:16:24 +0900

    [level 2] Title: H-Index, Time: 0.44 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,15 +1,15 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] arr) {
-        int[] cnt = new int[10001];
-        int max = 0;
+        Arrays.sort(arr);
+        int max = arr[arr.length-1];
+        int[] cnt = new int[max+1];
         for(int a : arr){
-            if(max<a) max = a;
             cnt[a]++; // 카운팅 정렬, idx번 이상 인용된 논문의 갯수
         }
-        int before = max;
         for(int h=max-1; h>=0; h--){
-            cnt[h] += cnt[before]; //cnt[i] 값을 dp처럼사용해서 인용 횟수에 따른 논문 갯수를 cnt[i]안에 저장
-            before = h;
+            cnt[h] += cnt[max]; //cnt[i] 값을 dp처럼사용해서 인용 횟수에 따른 논문 갯수를 cnt[i]안에 저장
+            max = h;
             if(h<=cnt[h] && arr.length-cnt[h] <=h)
                 return h;
         }
