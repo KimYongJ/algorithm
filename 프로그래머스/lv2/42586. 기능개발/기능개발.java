2f6699dcc615aa070ae3dc295cb59bc64003fb17
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-07-23 23:25:39 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,33 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 class Solution {
+    static int len;
     public List<Integer> solution(int[] p, int[] s) {
+        len = p.length;
         // 몇일남았는지 프로그래스별로 일자를 구해 리스트에 담는다.
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<p.length; i++){
-            double num1 = 100-p[i];
-            int num2 = (int)Math.ceil(num1/s[i]);
-            list.add(num2);
+        for(int i=0; i<len; i++){
+            p[i] = (int)Math.ceil((100-p[i])/(double)s[i]);
         }
         // 리스트에서 자기보다 뒤에있는데 숫자가 작은것들은 +1해준다.
         List<Integer> result = new ArrayList<>();
-        int base = list.get(0);
+        int base = p[0];
         int num = 1;
-        for(int i=1; i<list.size(); i++){
-            if(base>=list.get(i)){
+        for(int i=1; i<len; i++){
+            if(base>=p[i]){
                 num++;
-                if(list.size()==i+1){
-                    result.add(num);
-                    break;
-                }
             }else{
                 result.add(num);
-                if(list.size()==i+1) {
-                    result.add(1);
-                    break;
-                }
                 num = 1;
-                base = list.get(i);
+                base = p[i];
+            }
+            if(len==i+1){
+                result.add(num);
+                break;
             }
         }
         return result;
