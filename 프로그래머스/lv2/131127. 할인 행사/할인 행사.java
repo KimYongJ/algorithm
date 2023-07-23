a5b65b879c565ea4738efa5c3a1ff4431e483917
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-07-23 22:39:09 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+class Solution {
+    static int result,idx,len;
+    public int solution(String[] want, int[] number, String[] discount) {
+        HashMap<String,Integer> hm = new HashMap<>();
+        // 반복할 횟수 10일이니까 discount의 배열에서 10을 빼고 +1
+        len = discount.length-9;
+        for(int i=0; i<len; i++){
+            hm.clear();
+            for(int x = idx; x<idx+10; x++){
+                hm.put(discount[x],hm.getOrDefault(discount[x],0)+1);
+            }
+            idx++;
+            // 해쉬맵의 값과 비교한다.
+            boolean bool = true;
+            for(int x = 0; x<want.length; x++){
+                String baseString = want[x];
+                int baseNumber = number[x];
+                if(!hm.containsKey(baseString) ||baseNumber != hm.get(baseString) ){
+                    bool = false;
+                    break;
+                }
+            }
+            if(bool) result++;
+            
+        }
+        
+        
+        return result;
+    }
+}
