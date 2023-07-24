efec442f7f43dfd793d9ddb3aceee8f6389e1dd4
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-07-24 22:38:40 +0900

    [level 2] Title: 프로세스, Time: 0.85 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+class Solution {
+    static int max,idx,result,len;
+    public int solution(int[] list, int location) {
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int x : list) q.add(x);
+        
+        max = q.poll();
+        len = list.length;
+        while(true){
+            if(idx==location && max == list[idx])
+                break;
+            else if(max==list[idx]){
+                list[idx] = -1;
+                max = q.poll();
+                result++;
+            }
+            idx = idx+1==len ? 0 : idx+1;
+        }
+        return result+1;   
+    }
+}
