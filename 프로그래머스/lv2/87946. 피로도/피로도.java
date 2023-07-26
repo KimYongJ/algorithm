7aa974ba4e632531bbaef9f743f451e02611fd75
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-07-26 22:49:38 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int result,len;
+    static boolean[] bool;
+    public int solution(int k, int[][] dun) {
+        len = dun.length;
+        bool = new boolean[len];
+        DFS(0,0,k,dun);
+        return result;
+    }
+    public void DFS(int depth,int cnt,int k, int[][] dun){
+        if(depth==len){
+            if(result<cnt)
+                result = cnt;
+            
+            return;
+        }
+        for(int i=0; i<len; i++)
+            if(k>=dun[i][0]){
+                if(!bool[i]){
+                    bool[i] = true;
+                    DFS(depth+1,cnt+1,k-dun[i][1],dun);
+                    bool[i] = false;
+                }
+            }else if(result<cnt){
+                    result = cnt;
+            }
+    }
+}
