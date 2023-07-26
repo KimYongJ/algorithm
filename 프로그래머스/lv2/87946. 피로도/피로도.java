95d6f6dc0471a5521a97b95783fddc3e05241d26
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-07-26 23:14:13 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,22 +1,18 @@
 class Solution {
-    static int result,len;
+    static int result;
     static boolean[] bool;
     public int solution(int k, int[][] dun) {
-        len = dun.length;
-        bool = new boolean[len];
+        bool = new boolean[dun.length];
         DFS(0,k,dun);
         return result;
     }
     public void DFS(int cnt,int k, int[][] dun){
-        for(int i=0; i<len; i++)
-            if(k>=dun[i][0]){
-                if(!bool[i]){
-                    bool[i] = true;
-                    DFS(cnt+1,k-dun[i][1],dun);
-                    bool[i] = false;
-                }
-            }else if(result<cnt){
-                result = cnt;
+        for(int i=0; i<dun.length; i++)
+            if(k>=dun[i][0] && !bool[i]){
+                bool[i] = true;
+                DFS(cnt+1,k-dun[i][1],dun);
+                bool[i] = false;
             }
+        result = Math.max(cnt,result);
     }
 }
