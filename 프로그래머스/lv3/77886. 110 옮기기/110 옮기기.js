41566c07a148a2e4badd6d16c5d4bf7ca2ac2b72
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-07-31 00:12:10 +0900

    Update 110 옮기기.js

--- 프로그래머스/lv3/77886. 110 옮기기/110 옮기기.js
+++ 프로그래머스/lv3/77886. 110 옮기기/110 옮기기.js
@@ -27,3 +27,56 @@ function solution(s) {
     
     return answer
 }
+
+//////////////////////////////////////////////// 다른 버전 ///////////////////////////////////////
+function solution(s) {
+    let answer = []
+    
+    s.forEach(str => {
+        let idx = -1
+        let stack = 0
+        let STR_LEN = str.length
+        
+        // '110' 찾아서 꺼내기
+        let oneCnt = 0
+        let newStr = []
+        for(let j=0; j<STR_LEN; j++) {
+            if(str[j] === '1') {
+                oneCnt++
+                newStr.push(str[j])
+            } else {
+                if(oneCnt >= 2) {
+                    oneCnt -= 2
+                    newStr.pop();
+                    newStr.pop();
+                    stack++
+                } else {
+                    newStr.push(str[j])
+                    oneCnt = 0
+                }
+            }
+        }
+
+        // 110 넣기
+        let lastZero = newStr.lastIndexOf('0')+1; // +1을 해주면 -1인 경우를 생각할 필요가 없게 됨
+        
+        
+//         if(stack && lastZero === -1) {
+//             newStr = '110' + newStr
+//             lastZero = 2
+//             stack--
+//         }
+        
+//         while(stack) {
+//             newStr = newStr.slice(0, lastZero+1) + '110' + newStr.slice(lastZero+1, newStr.length)
+//             lastZero += 3
+//             stack--
+//         }
+        newStr = newStr.join('')
+        newStr = newStr.slice(0, lastZero) + '110'.repeat(stack) + newStr.slice(lastZero, newStr.length)
+        
+        answer.push(newStr)
+    })
+    
+    return answer
+}
