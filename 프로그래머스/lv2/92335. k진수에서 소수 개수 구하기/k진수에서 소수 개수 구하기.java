f3432c6c3acad54f6221a1950f8b7f0e12a19199
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-01 21:08:24 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.15 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -2,12 +2,12 @@
 class Solution {
     static private int result;
     public int solution(int n, int k) {
-        Loop : for(String s : Integer.toString(n,k).split("0"))
+        MadeByKYJ : for(String s : Integer.toString(n,k).split("0"))
             if(s.length()>0){
                 long num = Long.parseLong(s);
                 if(num<2) continue;
                 for(long i=2; i*i<=num; i++){
-                    if(num%i==0) continue Loop;
+                    if(num%i==0) continue MadeByKYJ;
                 }
                 result++;
             }
