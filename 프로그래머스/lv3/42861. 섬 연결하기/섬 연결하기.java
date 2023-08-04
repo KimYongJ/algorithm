27b9c2eda8d7bf43a2e1c3373ea2ca24d02fb129
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-04 23:29:15 +0900

    Update 섬 연결하기.java

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,23 +1,19 @@
 import java.util.Arrays;
-import java.util.Comparator;
 class Solution {
-    static int [] parent;
-    static int result,max;
+    static int[] parent;
     public int solution(int n, int[][] costs) {
         parent = new int[n];
-        for(int i=0; i<n; i++) parent[i] = i;
-        Arrays.sort(costs,new Comparator<int[]>(){
-            public int compare(int[] o1, int[] o2){
-                return o1[2]-o2[2];
-            }
-        }); 
+        int result = 0;
+        for(int i=0; i<n; i++)
+            parent[i] = i;
+        Arrays.sort(costs,(int[] o1, int[] o2) -> o1[2]-o2[2]);
+        
         for(int[] x : costs){
             int aParent = getParent(x[0]);
             int bParent = getParent(x[1]);
             if(aParent == bParent) continue;
             
             parent[aParent] = bParent;
-            
             result+=x[2];
         }
         
@@ -26,18 +22,5 @@ class Solution {
     public int getParent(int x){
         if(parent[x]==x) return x;
         return getParent(parent[x]);
-    }
-    public void union(int a, int b){
-        int p1 = getParent(a);
-        int p2 = getParent(b);
-        if(p1<p2) parent[b] = p1;
-        else parent[a] = p2;
-    }
-    public boolean CheckSameParent(int a, int b){
-        int p1 = getParent(a);
-        int p2 = getParent(b);
-        if(p1==p2)return true;
-        return false;
-    }
-   
+    }   
 }
