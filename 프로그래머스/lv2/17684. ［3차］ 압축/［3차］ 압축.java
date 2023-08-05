49128db2e50bbb7a9ceb13dcf1f631a7863e70d5
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-05 22:40:13 +0900

    [level 2] Title: [3차] 압축, Time: 8.01 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -6,14 +6,14 @@ class Solution {
     static HashMap<String,Integer> hm = new HashMap<>();
     
     public List<Integer> solution(String msg) {
-        int maxValue = 27 , index = 0;
+        int maxValue = 27;
         StringBuilder sb = new StringBuilder();
         
         for(int i=65; i<91; i++){
             hm.put((char)(i)+"",i-64);// A-Z까지 초기화한다.
         }
         
-        while(index!=msg.length()){
+        for(int index = 0; index<msg.length(); index++){
             String before = sb.toString();
             sb.append(msg.charAt(index));
             String after = sb.toString();
@@ -23,11 +23,8 @@ class Solution {
                 sb.setLength(0); // 스트링 초기화
                 sb.append(msg.charAt(index)); // 다음 연산을 위해 추가해준다.
             }
-            if(++index==msg.length()){
-                list.add(hm.get(sb.toString())); // 연산 마지막 부분이면 값을 추가해준다.
-            }
         }
-        
+        list.add(hm.get(sb.toString())); // 연산 마지막에 남은 string을 추가 출력해준다.
         return list;
     }
 }
