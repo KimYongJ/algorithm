c8c2fbe664ae774b6c330829be0befdc996ec345
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-07 20:21:51 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,10 +1,6 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
-    public List<Integer> solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<n; i++)
-            list.add(num_list[i]);
-        return list;
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list,0,n);
     }
 }
