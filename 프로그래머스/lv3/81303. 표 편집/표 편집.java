7bea636d6d77b515e17fc464c282d1f485071940
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-20 14:03:58 +0900

    [level 3] Title: 표 편집, Time: 220.86 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/lv3/81303. 표 편집/표 편집.java
+++ 프로그래머스/lv3/81303. 표 편집/표 편집.java
@@ -5,58 +5,58 @@ class Solution {
         Node next = null;
         boolean isDelete = false;
     }
-    
     public String solution(int n, int k, String[] cmd) {
-        Stack<Node> trashCan = new Stack<>();
+        Stack<Node> st = new Stack<>();
         Node[] nodeArr = new Node[n];
         nodeArr[0] = new Node();
         for(int i=1; i<n; i++){
             nodeArr[i] = new Node();
-            nodeArr[i].prev = nodeArr[i-1];
             nodeArr[i-1].next = nodeArr[i];
+            nodeArr[i].prev = nodeArr[i-1];
         }
         Node now = nodeArr[k];
-        
-        for(String c : cmd){
-            char condition = c.charAt(0);
-            if(condition=='U'){
-                int cnt = Integer.parseInt(c.substring(2));
+        for(String com : cmd){
+            char c = com.charAt(0);
+            if(c=='U'){
+                int cnt = Integer.parseInt(com.substring(2));
                 for(int i=0; i<cnt; i++) now = now.prev;
-            }else if(condition=='D'){
-                int cnt = Integer.parseInt(c.substring(2));
+            }else if(c=='D'){
+                int cnt = Integer.parseInt(com.substring(2));
                 for(int i=0; i<cnt; i++) now = now.next;
-            }else if(condition=='C'){
-                    now.isDelete = true;
-                    trashCan.push(now);
-                    Node prev = now.prev;
-                    Node next = now.next;
-                    if(prev!=null){
-                        prev.next = next;
-                    }
-                    if(next!=null){
-                        next.prev = prev;
-                        now = next;
-                    }else{
-                        now = prev;
-                    }
-            }else{
-                    Node node = trashCan.pop();
-                    Node prev1 = node.prev;
-                    Node next1 = node.next;
-                    node.isDelete = false;
-                    if(prev1 != null){
-                        prev1.next = node;
-                    }
-                    if(next1 != null){
-                        next1.prev = node;
-                    }
+            }else if(c=='C'){
+                st.push(now);
+                now.isDelete = true;
+                Node prev = now.prev;
+                Node next = now.next;
+                if(prev != null){
+                   prev.next = next; 
+                }
+                if(next != null){
+                    next.prev = prev;
+                    now = next;
+                }else{
+                    now = prev;
+                }
+            }else if(c=='Z'){
+                Node node = st.pop();
+                node.isDelete = false;
+                Node prev = node.prev;
+                Node next = node.next;
+                if(prev != null){
+                    prev.next = node;
+                }
+                if(next != null){
+                    next.prev = node;
+                }
+                
             }
         }
-        StringBuilder sb =new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++){
             if(nodeArr[i].isDelete) sb.append('X');
-            else    sb.append('O');
+            else sb.append('O');
         }
+        
         return sb.toString();
     }
 }
