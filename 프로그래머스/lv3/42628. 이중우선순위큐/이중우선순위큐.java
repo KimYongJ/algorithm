1ad559e58f9b6634453c0421e9d55f639794386f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-22 10:13:51 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.59 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,30 +1,48 @@
 import java.util.PriorityQueue;
 import java.util.Collections;
 class Solution {
-    static PriorityQueue<Integer> q = new PriorityQueue<>();
-    static PriorityQueue<Integer> mq = new PriorityQueue<>(Collections.reverseOrder());
+    class DualPriority_Q{
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        PriorityQueue<Integer> mq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        public void add(int num){ // 큐에 데이터 추가
+                q.add(num);
+                mq.add(num);
+        }
+        public int size(){
+            return q.size();
+        }
+        public void remove(int num){ // 큐 데이터 삭제
+            if(num==1){ // 최댓값삭제
+                q.remove(mq.poll());
+            }else if(num==-1){// 최솟값삭제
+                mq.remove(q.poll());
+            }
+        }
+        public int[] print(){ // 최대 최솟값 반환
+            int max = 0;
+            int min = 0;
+            if(q.size()!=0){
+                max = mq.poll();
+                min = q.poll();
+            }        
+            return new int[]{max,min};
+        }
+    }
+
     public int[] solution(String[] operations) {
+        DualPriority_Q q = new DualPriority_Q();
         for(String s : operations){
             char cmd = s.charAt(0);
             int num = Integer.parseInt(s.substring(2));
             if(cmd=='I'){// 큐에 데이터 Insert
                 q.add(num);
-                mq.add(num);
             }else if(cmd=='D'){
                 if(q.size()==0) continue;// 큐가 비어있다면 삭제 연산 무시
-                if(num==1){ // 최댓값삭제
-                    q.remove(mq.poll());
-                }else if(num==-1){// 최솟값삭제
-                    mq.remove(q.poll());
-                }
+                q.remove(num);
             }
         }
-        int max = 0;
-        int min = 0;
-        if(q.size()!=0){
-            max = mq.poll();
-            min = q.poll();
-        }        
-        return new int[]{max,min};// 최대, 최소 값 리턴
+        return q.print();
+
     }
 }
