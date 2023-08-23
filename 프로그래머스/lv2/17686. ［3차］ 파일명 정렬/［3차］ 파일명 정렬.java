7ad89fe3e7197a146605241eadca27f69fa9b675
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-23 23:11:33 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 48.33 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -7,13 +7,12 @@ public class Solution {
             B = B.toUpperCase();
             String head1 = A.split("[0-9]")[0];
             String head2 = B.split("[0-9]")[0];
-            int numb1 = Integer.parseInt(A.replace(head1,"").split("[^0-9]")[0]);
-            int numb2 = Integer.parseInt(B.replace(head2,"").split("[^0-9]")[0]);
-            if(head1.compareTo(head2)==0){
-                return numb1-numb2;
-            }else{
-                return head1.compareTo(head2);
-            }
+            int numb1 = Integer.parseInt(A.split("[^0-9]+")[1]);// 숫자를 제외한 모든 문자가 한번이상 반복됨을의미(+를붙였기 때문)
+            int numb2 = Integer.parseInt(B.split("[^0-9]+")[1]);
+            int result = head1.compareTo(head2);
+            if(result==0) 
+                result = numb1-numb2;
+            return result;
         });
         return files;
     }
