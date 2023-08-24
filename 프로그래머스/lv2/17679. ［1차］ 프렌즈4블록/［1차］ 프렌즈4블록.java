f967bada08237d5de251ee5c8357b7e4641b901a
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-24 15:37:46 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.89 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,61 @@
+class Solution {
+    char[][] arr;           // 처음에 주어진 board를 char형 배열로 바꿔 저장한다.
+    boolean[][] deleteArr;  // 원배열에서 지울 정보를 담는 배열
+    int m,n,result = 0;
+
+    public boolean cntCheck(){ // 지울 데이터를 실제로 지워주는 로직
+        int base = result;
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                if(deleteArr[i][j]){
+                    result++;
+                    arr[i][j] = 0;
+                    deleteArr[i][j] = false;
+                }
+        return base==result ? false : true; // 지워진게 없다면 false, 있다면 true
+    }
+    
+    public void sort(){// 지운 후 데이터를 정렬하는 로직
+        for(int i=m-1; i>0; i--)
+          Loop:for(int j=0; j<n; j++)
+            if(arr[i][j]==0)
+                for(int x=i-1;x>=0; x--)
+                    if(arr[x][j]!=0){
+                        arr[i][j] = arr[x][j];
+                        arr[x][j] = 0;
+                        continue Loop;
+                    }
+    }
+    
+    public void findValue(){ //지울 데이터를 추가해주는 로직
+        for(int i=0; i<m-1; i++)
+            for(int j=0; j<n-1; j++){
+                char base = arr[i][j];
+                if(base !=0 && base == arr[i+1][j] && base == arr[i][j+1] && base == arr[i+1][j+1]){
+                    deleteArr[i][j]   = deleteArr[i+1][j] = 
+                    deleteArr[i][j+1] = deleteArr[i+1][j+1] = true;
+                }
+            }
+    }
+    
+    public boolean excute(char[][] arr){
+        findValue();        // 지울 데이터를 추가해주는 로직
+        if(!cntCheck()){    // 지울 데이터를 실제로 지워주는 로직 지울게 없다면 false반환
+            return false;
+        }
+        sort();             // 지운 후 데이터를 정렬하는 로직
+        return true;
+    }
+    
+    public int solution(int m, int n, String[] board) {
+        this.m = m; 
+        this.n =n;
+        deleteArr = new boolean[m][n];
+        arr = new char[m][n];
+        for(int i=0; i<m; i++) arr[i] = board[i].toCharArray();
+        
+        while(excute(arr));
+        
+        return result;
+    }
+}
