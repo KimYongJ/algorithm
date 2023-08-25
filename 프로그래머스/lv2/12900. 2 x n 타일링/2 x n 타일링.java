fe841d4cd1c0d5021ee3436567be2314e7322ae0
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-25 11:08:31 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.86 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,12 +1,13 @@
 // https://github.com/KimYongJ/algorithm
 class Solution {
     public int solution(int n) {
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
+        int a = 1;
+        int b = 2;
         for(int i=3;i<=n; i++){
-            dp[i] =(dp[i-1]+dp[i-2])%1000000007;
+            int c = (a+b) % 1_000_000_007;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return b;
     }
 }
