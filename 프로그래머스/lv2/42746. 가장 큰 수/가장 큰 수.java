fbae82b0ce79950cb2a2d932d61b51c6485bafac
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-30 21:53:03 +0900

    [level 2] Title: 가장 큰 수, Time: 0.49 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,18 +1,34 @@
 // https://github.com/KimYongJ/algorithm
 import java.util.Arrays;
+import java.util.Comparator;
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
+        Integer[] arr = new Integer[numbers.length];
+        for(int i=0; i<numbers.length; i++)
+            arr[i] = numbers[i];
+        Arrays.sort(arr, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                if(a==0) return 1;
+                if(b==0) return -1;
+                if(a==b) return 0;
+                int alen = (int)(Math.log10(a)+1);
+                int blen = (int)(Math.log10(b)+1);
+              //  System.out.println(alen +" "+blen);
+                int sum1 = ((int)a*(int)Math.pow(10,blen)) + b; // a+b
+                int sum2 = ((int)b*(int)Math.pow(10,alen)) + a; // b+a
+                return sum2-sum1;
+               // return 0;
+            }
+        }); 
         
-        for(int i=0; i<numbers.length; i++) arr[i] = String.valueOf(numbers[i]);
         
-        Arrays.sort(arr,(o1,o2) -> (o2.concat(o1)).compareTo(o1.concat(o2))); // 내림차순 정렬을 해야 하기 때문에 o2+o1부터 해준다.
-        if(arr[0].equals("0"))  // numbers에 0이 여러개 있을 경우
+        if(arr[0]==0)  // numbers에 0만 여러개 있을 경우
             return "0";
         
         StringBuilder sb = new StringBuilder();
         
-        for(String s : arr){
+        for(int s : arr){
             sb.append(s);
         }
         return sb.toString();
