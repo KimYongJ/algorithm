4d12400817586ce9c0456ccce24047faafc62860
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-08-31 22:46:05 +0900

    [level 2] Title: 소수 찾기, Time: 10.10 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+class Solution {
+    
+    private int len;
+    private int result;
+    private char[] list;
+    private boolean[] visit;
+    private ArrayList<Integer> makeList = new ArrayList<>();
+    
+    public int solution(String numbers) {
+        len = numbers.length();
+        list = numbers.toCharArray();
+        visit = new boolean[len];
+        
+        DFS(0,"0");
+        
+        return result;
+    }
+    public void DFS(int depth,String str){
+        int num = Integer.parseInt(str);
+        if(makeList.contains(num)) return;
+        makeList.add(num);
+        if(depth==len){
+            if(isPrime(num)) result++;
+            return;
+        }
+        if(isPrime(num)) result++;
+        for(int i=0; i<len; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                DFS(depth+1,str+list[i]);
+                visit[i] = false;
+            }
+        }
+        
+    }
+    public boolean isPrime(int x){
+        if(x<=1) return false;
+        for(int i=2; i*i<=x; i++)
+            if(x%i==0)return false;
+        return true;
+    }
+}
