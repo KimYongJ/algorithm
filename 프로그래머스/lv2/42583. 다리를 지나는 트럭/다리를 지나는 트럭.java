6fdae9688e5139a97de11d4df228ebbfaa34c092
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-04 00:12:06 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,9 +1,7 @@
-//https://github.com/KimYongJ/algorithm
-
 import java.util.ArrayDeque;
 class Solution {
     
-    public int result = 0;
+    public int result = 0; // 최종 시간을 반환할 변수 
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         ArrayDeque<Integer> q = new ArrayDeque<>(); // 다리를 건너는 중인 트럭을 담을 큐
@@ -14,12 +12,12 @@ class Solution {
             q.add(0); // 다리 길이 만큼 큐에 0을 넣는다.
         }
         
-        while(idx<truck_weights.length){ // 대기트럭의 길이보다 인덱스가 작을 때만 반복
+        while(idx<truck_weights.length){ // 대기트럭의 길이보다 순회할 인덱스가 작을 때만 반복
 
             int truck = q.poll(); // 큐에서 데이터를 하나 뽑아온다.
             
-            if(truck != 0){ // 트럭의 무게 측정 0이 아닌 경우
-                weightSum-= truck; // 다리를 건너는 중인 트럭에서 큐에서 뽑은 트럭의 무게를 뺀다
+            if(truck != 0){ // 트럭의 무게 측정, 0이 아닌 경우 이하 코드 실행
+                weightSum-= truck; // 다리를 건너는 중인 트럭무게 합에서 방금 큐에서 뽑은 트럭 무게를 뺀다
             }
             
             if(weightSum+truck_weights[idx] <= weight){// 다리를 건너는 중인 트럭 무게의 총합과 그다음 대기 중 트럭의 합이 다리가 견딜 수 있는 무게를 넘지 않는다면 이하 실행
