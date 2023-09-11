dcf7cfb0e679e168bcf98e46370ce467edd272d7
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-12 00:50:17 +0900

    [Bronze II] Title: 블랙잭, Time: 140 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+    private static int max, sum,result,arr[],len;
+    private static boolean[] visit;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        len = Integer.parseInt(st.nextToken());
+        result = 0;
+        arr = new int[len];
+        visit = new boolean[len];
+        max = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<len; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        BRUT(0,0);
+        System.out.println(result);
+    }
+    public static void BRUT(int depth,int sum){
+        if(depth==3){
+            if(max>=sum){
+                result = result<sum ? sum : result;
+            }
+            return;
+        }
+        for(int i=0; i<len;i++){
+            if(!visit[i]){
+                visit[i] = true;
+                BRUT(depth+1,sum+arr[i]);
+                visit[i] = false;
+            }
+        }
+        
+    }
+}
