8f47653f442742012b5b14de33ed0b4d87d1c923
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-12 17:43:59 +0900

    [level 3] Title: 야근 지수, Time: 111.12 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -1,3 +1,4 @@
+//https://github.com/KimYongJ/algorithm/
 import java.util.PriorityQueue;
 class Solution {
     
@@ -10,15 +11,15 @@ class Solution {
             q.add(w);// 큐에 모든 값 insert
         
         for(int i=0; i<n; i++){
-            q.add(q.poll()-1); // 큰 수 들을 뽑아서 -1 한 후 다시 insert
+            int qData = q.poll();
+            if(--qData<0)
+                return 0;
+            q.add(qData); // 큰 수 들을 뽑아서 -1 한 후 다시 insert
         }
-        if(q.peek()<1)// 가장 우선순위가 높은것이 1보다 작으면 return 0;
-            return 0;
         
-        int len = q.size(); // 큐의 크기대로 반복문 실행
-        for(int i=0; i<len; i++){
+        while(!q.isEmpty())
             result += Math.pow(q.poll(),2);
-        }
+        
         
         return result;
     }
