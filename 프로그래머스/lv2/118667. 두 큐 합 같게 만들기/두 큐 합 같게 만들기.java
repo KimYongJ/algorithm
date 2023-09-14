ca423b3c5cd826e1e465073ecfb09ae36a55b39a
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-14 14:15:28 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 36.43 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -4,20 +4,22 @@ class Solution {
     private int result = 0;
     
     public int solution(int[] qu1, int[] qu2) {
+        ArrayDeque<Integer> q1 = new ArrayDeque<>();
+        ArrayDeque<Integer> q2 = new ArrayDeque<>();
         int max = qu1.length*3;// 종료조건
         long sum1=0; // 큐 1의 합
         long sum2=0; // 큐 2의 합
-        ArrayDeque<Integer> q1 = new ArrayDeque<>();
-        ArrayDeque<Integer> q2 = new ArrayDeque<>();
         for(int i=0; i<qu1.length; i++){
-            sum1+=qu1[i];
-            sum2+=qu2[i];
             q1.add(qu1[i]); // 배열을 큐로 변환
             q2.add(qu2[i]); // 배열을 큐로 변환
+            sum1+=qu1[i];
+            sum2+=qu2[i];
         }
 
         long base = (sum1+sum2) / 2; // 기준 값
-        while(base!=sum1){
+        while(--max>0){
+            if(base==sum1) 
+                break;
             if(base>sum1){ // 기준 값보다 합계가 큰 큐의 값을 빼서 작은 큐에 넣는다.
                 int num = q2.poll();
                 sum1+=num;
@@ -27,11 +29,8 @@ class Solution {
                 sum1-=num;
                 q2.add(num);
             }
-            if(max<++result){
-                result = -1;
-                break;
-            }
+            result++;
         }
-        return result;        
+        return max==0 ? -1 : result;        
     }
 }
