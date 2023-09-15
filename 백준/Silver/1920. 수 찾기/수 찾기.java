d29bfb0e35bd9cc8dba4db3566144ddf513bc51b
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-16 08:30:18 +0900

    [Silver IV] Title: 수 찾기, Time: 616 ms, Memory: 44648 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,16 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        HashSet<Integer> hs = new HashSet<>();
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         for(int i=0; i<n; i++){
-            hs.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        
+        Arrays.sort(arr);
         
         int m = Integer.parseInt(br.readLine());
         
@@ -19,11 +22,25 @@ class Main{
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<m; i++){
             int x = Integer.parseInt(st.nextToken());
-            if(hs.contains(x)){
-                sb.append("1").append("\n");
-            }else{
-                sb.append("0").append("\n");
+            // 2분탐색 시작
+            int left = 0;
+            int right = n-1;
+            while(true){
+                int base = (left+right)/2;
+                if(x==arr[base]){
+                    sb.append(1).append("\n");
+                    break;
+                }else if(left>=right){
+                    sb.append(0).append("\n");
+                    break;
+                }
+                if(x<arr[base]){
+                    right = base-1;
+                }else{
+                    left = base+1;
+                }
             }
+            
         }
         
         System.out.println(sb.toString());
