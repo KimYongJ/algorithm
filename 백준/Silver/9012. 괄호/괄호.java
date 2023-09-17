c42c32132c0c8737c7efc222583f7e71cb2f18ec
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-17 23:46:04 +0900

    [Silver IV] Title: 괄호, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -8,22 +7,19 @@ class Main{
         int n = Integer.parseInt(br.readLine());
         for(int i=0; i<n; i++){
             String str = br.readLine();
-            sb.append(excute(str)).append("\n");
+            int cnt = 0;
+            for(char c : str.toCharArray()){
+                if(c=='('){
+                    cnt++;
+                }else{
+                    cnt--;
+                }
+                if(cnt<0){
+                    break;
+                }
+            }
+            sb.append(cnt != 0 ? "NO" : "YES").append("\n");
         }
         System.out.println(sb.toString());
-    }
-    public static String excute(String str){
-        int cnt = 0;
-        for(char c : str.toCharArray()){
-            if(c=='('){
-                cnt++;
-            }else{
-                cnt--;
-            }
-            if(cnt<0){
-                return "NO";
-            }
-        }
-        return cnt != 0 ? "NO" : "YES";
     }
 }
