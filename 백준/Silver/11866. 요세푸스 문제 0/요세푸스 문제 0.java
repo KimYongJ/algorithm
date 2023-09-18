61396cd2a5612e98846345efa06dfe83228d7c40
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-18 20:28:01 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,27 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.ArrayList;
 class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         ArrayList<Integer> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int k = Integer.parseInt(str[1]);
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
         int idx = k-1;
         
         for(int i=0; i<n; i++){
             list.add(i+1);
         }
+        
         sb.append("<");
+        
         while(list.size()!=1){
             int num = list.get(idx);
             list.remove(idx);
             sb.append(num).append(", ");
             idx = (idx+k-1)%list.size();
         }
+        
         sb.append(list.get(0)).append(">");
+        
         System.out.println(sb.toString());
     }
 }
