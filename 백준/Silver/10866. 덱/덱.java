0e591552f6fafaf268d0f2ebcb86a11675184fc0
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-18 19:45:23 +0900

    [Silver IV] Title: 덱, Time: 200 ms, Memory: 18800 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<n; i++){
+            String[] cmd = br.readLine().split(" ");
+            switch(cmd[0]){
+                case "push_front":
+                    q.addFirst(Integer.parseInt(cmd[1]));
+                    break;
+                case "push_back":
+                    q.addLast(Integer.parseInt(cmd[1]));
+                    break;
+                case "pop_front":
+                    sb.append(q.isEmpty() ? -1 : q.pollFirst()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(q.isEmpty() ? -1 : q.pollLast()).append("\n");
+                    break;
+                case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(q.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(q.isEmpty() ? -1 : q.peek()).append("\n");
+                    break;
+                case "back":
+                    sb.append(q.isEmpty() ? -1 : q.peekLast()).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
