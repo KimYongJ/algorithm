14b698cce3196733312f316c95ec0e9175a06b6f
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-19 20:44:46 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -17,8 +17,6 @@ class Main{
         for(int i=0; i<n; i++){
             int cnt = 1;
             for(int j=0; j<n; j++){
-                if(i==j)continue;
-                
                 if(arr[i][0]<arr[j][0]&&arr[i][1]<arr[j][1])
                     cnt++;
             }
