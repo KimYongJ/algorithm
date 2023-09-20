15f4cdada239094647451634d521bdde9ec2f330
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-20 21:36:14 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,14 @@
+
+SELECT DISTINCT CART_ID
+FROM CART_PRODUCTS
+WHERE   CART_ID IN (
+                    SELECT CART_ID
+                    FROM CART_PRODUCTS
+                    WHERE NAME = 'Milk'
+                    )
+AND     CART_ID IN (
+                    SELECT CART_ID
+                    FROM CART_PRODUCTS
+                    WHERE NAME = 'Yogurt'
+                    )
+ORDER BY CART_ID
