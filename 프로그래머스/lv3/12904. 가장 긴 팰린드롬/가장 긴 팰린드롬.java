9cf3130087a2f4165ccd51434ff08bc76d57d9fd
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-24 13:36:54 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.15 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -3,14 +3,13 @@ class Solution{
     private char[] arr;
     public int solution(String s){
         arr = s.toCharArray(); // 빠른 연산을 위해 배열로 전환
-        int len = arr.length; //
-        int maxchar = 1;
-        // 가장긴 글자 팰린드롬 부터 확인, 총 글자가 5개면 5개,4개,3개 글자순으로 차례로 비교한다.
-        for(int i=len;i>1; i--){//n개 글자부터 2개 글자까지 확인
-            // 최대 글자가 몇개인지 전달
+        int len = arr.length;
+        int maxchar = 1; // 이변수는 최대 X개일 때 몇번 반복할지를 체크하는 변수이다.
+        // 가장긴 글자 팰린드롬 부터 확인, 총 글자가 5개면 5개부터,4개,3개 글자순으로 차례로 비교한다.
+        Loop : for(int i=len;i>1; i--){//n개 글자부터 2개 글자까지 확인
             for(int a=0;a<maxchar; a++){
-                if(palindrome(a,a+i-1))
-                    return result;
+                if(palindrome(a,a+i-1)) // 팰린드롬 함수에 시작 인덱스와 종료 인덱스를 보낸다.
+                    break Loop; // 처음 찾은 팰린드롬이 가장 긴 문자열이므로 바로 반환한다.
             }
             maxchar++;
         }
