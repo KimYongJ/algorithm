ee5ab2c67cd1c883b2709e775f245809a75d0f69
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-24 23:57:03 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 24136 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,24 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 class Main{
-    public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<>();
-        int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args)throws Exception{        
+        int T = read();
+        int[] arr = new int[T];
+        int idx = 0;
         while(T-->0){
-            int data = Integer.parseInt(br.readLine());
+            int data = read();
             
-            if(data==0){
-                list.remove(list.size()-1);
+            if(data!=0){
+                arr[idx++] = data;
             }else{
-                list.add(data);
+                arr[--idx] = 0;
             }
+            
         }
+        
         int result = 0;
-        for(int data : list){
+        for(int data : arr){
             result += data;
+            if(data==0) 
+                break;
         }
         System.out.println(result);
+        
+    }
+    static int read() throws Exception { // 숫자 입력된 것 
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
