2f34945f4bee3fdb3d9643f358877059047d8ce2
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-24 13:55:52 +0900

    [unrated] Title: 롤케이크 자르기, Time: 103.24 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265#) 
 
 ### 성능 요약
 
-메모리: 144 MB, 시간: 95.49 ms
+메모리: 145 MB, 시간: 103.24 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,31 +3,23 @@ import java.util.HashSet;
 class Solution {
     public int solution(int[] topping) {
         int cnt = 0;
-        int[] a = dp(topping, true);
-        int[] b = dp(topping, false);
+        int[] a = new int[topping.length];
+        int[] b = new int[topping.length];
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0; i<topping.length; i++){
+            hs.add(topping[i]);
+            a[i] = hs.size();
+        }
+        hs.clear();
+        for(int i=topping.length-1; i>=0; i--){
+            hs.add(topping[i]);
+            b[i] = hs.size();
+        }
         for(int i=0; i<topping.length-1; i++)
             if(a[i]==b[i+1])
                 cnt++;
         
         return cnt;
-    }
-    public int[] dp(int[] topping, boolean tf){
-        int len = topping.length-1;
-        
-        int[] dp = new int[len+1];
-        
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i=0; i<topping.length; i++){
-            if(tf){
-                hs.add(topping[i]);
-                dp[i] = hs.size();
-            }
-            else{
-                hs.add(topping[len]);
-                dp[len] = hs.size();
-                len--;
-            }
-        }
-        return dp;
     }
 }
