56aec095eab658f6444a671533fb46c98538a15c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-25 10:36:39 +0900

    [Silver IV] Title: solved.ac, Time: 296 ms, Memory: 19448 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,4 +1,3 @@
-// https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
@@ -6,31 +5,35 @@ class Main{
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PriorityQueue<Integer> asc = new PriorityQueue<>();
-        PriorityQueue<Integer> des = new PriorityQueue<>((a,b)->b-a);
         
-        int T = Integer.parseInt(br.readLine());
+        int T = readInt();
         int per = (int)Math.round(T*0.15); // 15프로의 명수
-
+        int len = T-per-per; // 평균을 구할 명수
         
-        for(int i=0; i<T; i++){
-            int num = Integer.parseInt(br.readLine());
-            asc.add(num);
-            des.add(num);
-        }
+        for(int i=0; i<T; i++) // 우선순위 큐에 데이터를 넣는다.
+            asc.add(readInt());
         
-        // 상,하위 15% 제외
-        for(int i=0; i<per; i++){
+        // 하위 15% 제외
+        for(int i=0; i<per; i++)
             asc.poll();
-            des.poll();
-        }
         
-        double sum = 0;
+        float sum = 0;
         
-        for(int i=0; i<T-per*2; i++){
-            sum += (asc.poll() + des.poll());
-        }
+        for(int i=0; i<len; i++)
+            sum += asc.poll();
         
-        System.out.println(T==0 ? 0 : (int)Math.round((sum/2)/(T-per*2)));
-        
+        System.out.println(T==0 ? 0 : (int)Math.round(sum/len));
     }
+    
+    static int readInt() throws Exception {
+        int n = 0;
+        while (true) {
+            int input = System.in.read();
+            if (input <= 32)
+                return n;
+            else
+                n = (n << 3) + (n << 1) + (input & 15);
+        }
+    }
+    
 }
