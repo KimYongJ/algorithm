530f7dc07f7386a9e2c8f362b81d2a6698604f7e
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-25 17:57:52 +0900

    [Silver IV] Title: 듣보잡, Time: 352 ms, Memory: 28912 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,48 @@
+// https://github.com/KimYongJ/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.PriorityQueue;
+import java.util.HashSet;
+class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        HashSet<String> nSet = new HashSet<>(); // n개의 string담을 Set선언
+        HashSet<String> mSet = new HashSet<>(); // m개의 string담을 Set선언
+        
+        for(int i=0; i<n; i++)
+            nSet.add(br.readLine()); // set에 문자열을 담는다.
+        
+        for(int i=0; i<m; i++)
+            mSet.add(br.readLine()); // set에 문자열을 담는다.
+        
+        PriorityQueue<String> q = new PriorityQueue<>(); // 사전순 자동 정렬을 위한 우선순위 큐
+        if(n<m){ // n이 더작을 경우 nSet의 값을 mSet에서 찾는다.
+            for(String str : nSet){
+                if(mSet.contains(str)){
+                    q.add(str);
+                }
+            }
+        }else{ // m이 더작을 경우 mSet의 값을 nSet에서 찾는다.
+           for(String str : mSet){
+                if(nSet.contains(str)){
+                    q.add(str);
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(q.size()).append("\n"); // 큐의 사이즈 즉, 듣보잡의 숫자 
+        while(!q.isEmpty()){
+            sb.append(q.poll()).append("\n"); // 오름차순 정렬된 string을 차례로 담는다. 
+        }
+        
+        System.out.println(sb.toString());
+       
+    }
+}
