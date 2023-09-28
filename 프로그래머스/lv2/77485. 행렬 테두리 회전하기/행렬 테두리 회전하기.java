8aff07465bd27c0bcb71a5e88f77c40f9bd4e2bd
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-28 11:16:13 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 24.29 ms, Memory: 106 MB -Baekjoon

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,5 +1,4 @@
 // https://github.com/KimYongJ/algorithm
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 class Solution {
     
@@ -19,29 +18,33 @@ class Solution {
         return result;
     }
     public void rotation(int[] q,int[][] arr, ArrayList<Integer> result){
-        ArrayDeque<Integer> deq = new ArrayDeque<>();
         int min = Integer.MAX_VALUE; // 테두리에서 가장 최솟값 담을 변수
-        deq.add(arr[q[0]+1][q[1]]);
-        for(int i = q[1]; i<=q[3]; i++){// 사각형의 윗변
-            deq.add(arr[q[0]][i]);
-            arr[q[0]][i] = deq.poll();
-            min = Math.min(min,arr[q[0]][i]);
+        int before = arr[q[0]+1][q[1]];
+        int now = 0;
+        for(int x = q[1]; x<=q[3]; x++){// 사각형의 윗변
+            now = arr[q[0]][x];
+            arr[q[0]][x] = before;
+            before = now;
+            min = Math.min(min,arr[q[0]][x]);
         }
         for(int y=q[0]+1; y<=q[2]; y++){ // 사각형의 우변
-            deq.add(arr[y][q[3]]);
-            arr[y][q[3]] = deq.poll();
+            now = arr[y][q[3]];
+            arr[y][q[3]] = before;
+            before = now;
             min = Math.min(min,arr[y][q[3]]);
         }
         for(int x=q[3]-1;x>=q[1]; x--){ // 사각형의 아랫변
-            deq.add(arr[q[2]][x]);
-            arr[q[2]][x] = deq.poll();
+            now = arr[q[2]][x];
+            arr[q[2]][x] = before;
+            before = now;
             min = Math.min(min,arr[q[2]][x]);
         }
         for(int y=q[2]-1; y>q[0]; y--){ // 사각형의 좌변
-            deq.add(arr[y][q[1]]);
-            arr[y][q[1]] = deq.poll();
+            now = arr[y][q[1]];
+            arr[y][q[1]] = before;
+            before = now;
             min = Math.min(min,arr[y][q[1]]);
         }
-        result.add(Math.min(min,deq.poll()));
+        result.add(Math.min(min,before));
     }
 }
