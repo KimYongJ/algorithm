a5ea8063d849d972c41bfd32f11df54614cc5060
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-28 11:06:06 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 24.29 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,12 +1,11 @@
 // https://github.com/KimYongJ/algorithm
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 class Solution {
     
-    private ArrayList<Integer> result;
-    
     public ArrayList<Integer> solution(int rows, int columns, int[][] queries) {
         int[][] arr = new int[rows+1][columns+1];
-        result = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
         int idx = 1;
         for(int i=1; i<arr.length; i++){
             for(int j=1; j<arr[0].length; j++){
@@ -15,46 +14,34 @@ class Solution {
         }
         
         for(int[] q: queries)
-            rotation(q,arr);
+            rotation(q,arr,result);
         
         return result;
     }
-    public void rotation(int[] q,int[][] arr){
-        ArrayList<int[]> coordinate = new ArrayList<>();// 테두리 좌표 값들을 순차적으로 담는다.
-        ArrayList<Integer> nlist = new ArrayList<>(); // 테두리 값들을 순차적으로 담는다.
+    public void rotation(int[] q,int[][] arr, ArrayList<Integer> result){
+        ArrayDeque<Integer> deq = new ArrayDeque<>();
         int min = Integer.MAX_VALUE; // 테두리에서 가장 최솟값 담을 변수
-        int x = q[1];
-        int y = q[0];
-        for(; x<=q[3]; x++){
-            coordinate.add(new int[]{y,x}); // 사각형의 윗변 좌표를 담는다.
-            nlist.add(arr[y][x]);
-            min = Math.min(min,arr[y][x]);
+        deq.add(arr[q[0]+1][q[1]]);
+        for(int i = q[1]; i<=q[3]; i++){// 사각형의 윗변
+            deq.add(arr[q[0]][i]);
+            arr[q[0]][i] = deq.poll();
+            min = Math.min(min,arr[q[0]][i]);
         }
-        x--;
-        for(y=y+1; y<=q[2]; y++){
-            coordinate.add(new int[]{y,x}); // 우변 좌표를 담는다.
-            nlist.add(arr[y][x]);
-            min = Math.min(min,arr[y][x]);
+        for(int y=q[0]+1; y<=q[2]; y++){ // 사각형의 우변
+            deq.add(arr[y][q[3]]);
+            arr[y][q[3]] = deq.poll();
+            min = Math.min(min,arr[y][q[3]]);
         }
-        y--;
-        for(x=x-1;x>=q[1]; x--){
-            coordinate.add(new int[]{y,x}); // 하변 좌표를 담는다.
-            nlist.add(arr[y][x]);
-            min = Math.min(min,arr[y][x]);
+        for(int x=q[3]-1;x>=q[1]; x--){ // 사각형의 아랫변
+            deq.add(arr[q[2]][x]);
+            arr[q[2]][x] = deq.poll();
+            min = Math.min(min,arr[q[2]][x]);
         }
-        x++;
-        for(y=y-1; y>q[0]; y--){
-            coordinate.add(new int[]{y,x}); // 좌변 좌표를 담는다.
-            nlist.add(arr[y][x]);
-            min = Math.min(min,arr[y][x]);
+        for(int y=q[2]-1; y>q[0]; y--){ // 사각형의 좌변
+            deq.add(arr[y][q[1]]);
+            arr[y][q[1]] = deq.poll();
+            min = Math.min(min,arr[y][q[1]]);
         }
-        for(int i=1; i<coordinate.size(); i++){
-            int[] p = coordinate.get(i);
-            arr[p[0]][p[1]] = nlist.get(i-1);
-        }
-        int[] p = coordinate.get(0);
-        arr[p[0]][p[1]] = nlist.get(nlist.size()-1);
-        
-        result.add(min);
+        result.add(Math.min(min,deq.poll()));
     }
 }
