e080969673ee5d2925bac56a2b392f1d29097094
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-09-30 14:03:07 +0900

    [level 2] Title: 배달, Time: 0.55 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.java
+++ 프로그래머스/lv2/12978. 배달/배달.java
@@ -1,25 +1,26 @@
 // https://github.com/KimYongJ/algorithm
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+class Node{
+    int node, dist;
+    Node(int node, int dist){
+        this.node = node; // 인접 노드 번호
+        this.dist = dist; // 거리 
+    }
+}
 class Solution {
     public int solution(int N, int[][] road, int K) {
         
         int result = 1; // 1은 무조건 방문하기 때문에 기본 값은 1이다.
         
-        int[][] weight = new int[N+1][N+1];// 가중치를 담을 배열
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();// 인접 노드 담을 리스트
+        ArrayList<ArrayList<Node>> list = new ArrayList<>();// 인접 노드 담을 리스트
         
         for(int i=0; i<N+1;i++) // 리스트 초기화
             list.add(new ArrayList<>());
         
         for(int[] r: road){
-            if(weight[r[0]][r[1]] !=0){ // 같은 노드들의 가중치가 여러개 주어질 수 있으므로 해당 로직 생성
-                weight[r[0]][r[1]] = weight[r[1]][r[0]] = Math.min(r[2],weight[r[0]][r[1]]);// 가중치 세팅
-            }else{
-                weight[r[0]][r[1]] =  weight[r[1]][r[0]] = r[2];// 가중치 세팅
-            }
-            list.get(r[0]).add(r[1]); // 인접 리스트 생성
-            list.get(r[1]).add(r[0]); // 인접 리스트 생성
+            list.get(r[0]).add(new Node(r[1],r[2])); // 인접 리스트 생성
+            list.get(r[1]).add(new Node(r[0],r[2])); // 인접 리스트 생성
         }
         
         int[] dist = new int[N+1]; // 1로 부터의 거리를 담는 배열
@@ -34,15 +35,15 @@ class Solution {
             
             int node = q.poll(); // 노드를 꺼낸다
             
-            ArrayList<Integer> ad_List = list.get(node); // 해당 노드의 인접 리스트를 가져온다.
+            ArrayList<Node> ad_List = list.get(node); // 해당 노드의 인접 리스트를 가져온다.
             
             for(int i=0; i<ad_List.size(); i++){ // 인접 리스트 수만큼 반복
                 
-                int ad_Node = ad_List.get(i); // 인접 노드를 하나 꺼낸다
+                Node ad_Node = ad_List.get(i); // 인접 노드를 하나 꺼낸다
                 
-                if(dist[ad_Node] > dist[node] + weight[node][ad_Node]){ //  거리가 갱신될 수 있는 모든 상황에 대해서만 큐에 해당 노드를 넣는다.그래야 dist에 최단거리만 담기게 된다.
-                    q.add(ad_Node); // 큐에 해당 노드를 담아 다른 인접리스트 탐색 토록함
-                    dist[ad_Node] = dist[node] + weight[node][ad_Node];
+                if(dist[ad_Node.node] > dist[node] + ad_Node.dist){ //  거리가 갱신될 수 있는 모든 상황에 대해서만 큐에 해당 노드를 넣는다.그래야 dist에 최단거리만 담기게 된다.
+                    q.add(ad_Node.node); // 큐에 해당 노드를 담아 다른 인접리스트 탐색 토록함
+                    dist[ad_Node.node] = dist[node] + ad_Node.dist;
                 }
             }
         }
