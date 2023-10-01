9c4f6a22e28604971686632f3ca6e668012ebba4
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-02 01:37:25 +0900

    [level 3] Title: 베스트앨범, Time: 1.82 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -1,5 +1,4 @@
 // https://github.com/KimYongJ/algorithm
-import java.util.PriorityQueue;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,32 +17,24 @@ class Solution {
         
         for(int i=0; i<genres.length; i++){
             
-            genKeyString.put(genres[i],genKeyString.getOrDefault(genres[i],0)+plays[i]);
+            genKeyString.put(genres[i],genKeyString.getOrDefault(genres[i],0)+plays[i]);// 장르당 총 재생 횟수 담는 코드
             
+            // 이하 장르당 고유번호 저장 코드
             ArrayList<Node> list = hmlist.getOrDefault(genres[i],new ArrayList<>());
-            
             list.add(new Node(i,plays[i]));
-            
             hmlist.put(genres[i],list);
         }
-        
-        
-        PriorityQueue<Integer> q = new PriorityQueue<>((a,b)->b-a); // 재생횟수 담을 우선순위큐(내림차순)
-        HashMap<Integer,String> genKeyInteger = new HashMap<>();// key: 재생횟수 , value : 장르
-        
-        for(Map.Entry<String,Integer> set : genKeyString.entrySet()){// 키 벨류 반전 및 재생 횟수 내림차순 정렬
-            q.add(set.getValue());
-            genKeyInteger.put(set.getValue(),set.getKey());
-        }
+        // 이하 장르당 총 재생횟수 저장 및, 재생횟수기준 내림차순 정렬
+        ArrayList<Map.Entry<String,Integer>> maplist = new ArrayList<>(genKeyString.entrySet());
+        Collections.sort(maplist,(a,b)->b.getValue()-a.getValue());
         
         ArrayList<Integer> result = new ArrayList<>();// 결과
 
-        while(!q.isEmpty()){
-            int qData = q.poll();
-            String genre = genKeyInteger.get(qData); // 가장 많이 재생된 장르
+        for(int i=0; i<maplist.size(); i++){
+            String genre = maplist.get(i).getKey(); // 가장 많이 재생된 장르
             ArrayList<Node> list = hmlist.get(genre);// 가장 많이 재생된 장르의 고유번호 리스트
             
-            Collections.sort(list,(a,b)->{
+            Collections.sort(list,(a,b)->{// 장르당 고유번호 내림차순 , 같을 경우 고유번호기준 오름차순 정렬
                 if(b.play==a.play){
                     return a.idx - b.idx;
                 }else{
@@ -51,8 +42,8 @@ class Solution {
                 }
             });
             
-            result.add(list.get(0).idx);
-            if(list.size()>1)
+            result.add(list.get(0).idx);// 결과 추가
+            if(list.size()>1)// 값이 2개이상일 경우 결과추가 한번더함
                 result.add(list.get(1).idx);
         }
         
