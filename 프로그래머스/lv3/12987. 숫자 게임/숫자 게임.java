7de6d3b8a2c88bbcde9c1991d49364cf34cead60
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-01 22:33:25 +0900

    [level 3] Title: 숫자 게임, Time: 111.96 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -1,21 +1,18 @@
 // https://github.com/KimYongJ/algorithm
-import java.util.PriorityQueue;
+import java.util.Arrays;
 class Solution {
     public int solution(int[] A, int[] B) {
         int result = 0;
-        PriorityQueue<Integer> Aq = new PriorityQueue<>();
-        PriorityQueue<Integer> Bq = new PriorityQueue<>();
-        for(int i=0; i<A.length; i++){
-            Aq.add(A[i]);
-            Bq.add(B[i]);
-        }
-        while(!Bq.isEmpty()){
-            int bNum = Bq.poll();
-            if(bNum>Aq.peek()){ // B에서 가장 첫번째로 A보다 큰 숫자일 경우 결과에 +1;
-                Aq.poll();
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i=0;i<B.length; i++){
+            if(B[i]>A[result]){
                 result++;
             }
         }
+        
         return result;
     }
 }
