8a3b5d6426644f98583fa3466938beb05086612c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-07 23:23:31 +0900

    [Silver II] Title: 최소 힙, Time: 228 ms, Memory: 18548 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,29 +1,38 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 class Main{
     public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         PriorityQueue<Integer> q = new PriorityQueue<>();
         
         StringBuilder sb = new StringBuilder();
         
-        int T = Integer.parseInt(br.readLine());
+        int T = read();
         
         while(T-->0){
             
-            int n = Integer.parseInt(br.readLine());
+            int n = read();
             
             if(n==0){
-                if(!q.isEmpty()) n = q.poll();
-                sb.append(n).append('\n');
+                sb.append(q.isEmpty() ? n : q.poll()).append('\n');
             }else q.add(n);
             
         }
         
         System.out.print(sb);
     }
+    static int read() throws Exception {
+        int n = 0;
+        boolean isNegative = false;
+        while (true) {
+            int input = System.in.read();
+            if (input <= 32) {
+                return isNegative ? n * -1 : n;
+            } else if (input == '-')
+                isNegative = true;
+            else
+                n = (n << 3) + (n << 1) + (input & 15);
+        }
+     }
 }
