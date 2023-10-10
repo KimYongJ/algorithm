83d60004844083606e69d8a67dac3221bbcd1280
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-10 17:17:28 +0900

    [Silver II] Title: 색종이 만들기, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -15,7 +15,7 @@ class Main{
         DivdeAndConquer(arr,n,0,0);
         
         System.out.println(white);
-        System.out.println(blue);
+        System.out.print(blue);
     }
     /*
      * 모두 같은지 체크 후 다르다면 1~4분면으로 나눠서 재귀적으로 함수 실행
