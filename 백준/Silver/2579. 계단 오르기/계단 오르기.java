006e67b08b150cfaa7ad52d0af95f7e4468c3f1a
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-11 17:23:13 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,20 +1,21 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
     public static void main(String[] args)throws Exception{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	int N = Integer.parseInt(br.readLine());
+    	int N = read();
     	int[] arr = new int[N+3];
     	int[] dp  = new int[N+3];
     	for(int i=3; i<N+3; i++)
-    		arr[i] = Integer.parseInt(br.readLine());
+    		arr[i] = read();
     	
     	// 점화식 dp[i] = Math.max(arr[i-1]+dp[i-3],dp[i-2]) + arr[i];
     	for(int i=3; i<N+3; i++) {
     		dp[i] = Math.max(arr[i-1]+dp[i-3],dp[i-2]) + arr[i];
     	}
     	System.out.println(dp[N+2]);
+    }
+    private static int read() throws Exception {
+        int c, n = System.in.read() - 48;
+        while ((c = System.in.read()) > 32) n = 10 * n + c - 48;
+        return n;
     }
 }
