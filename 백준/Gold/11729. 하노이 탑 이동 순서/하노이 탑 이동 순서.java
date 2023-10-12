59cd58eae744d8462576a6cb2f77090e93598e7e
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-12 22:38:12 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 392 ms, Memory: 21228 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,23 +1,29 @@
-// https://github.com/KimYongJ/algorithm
+//https://github.com/KimYongJ/algorithm
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 class Main{
-	static StringBuilder sb = new StringBuilder();
+ static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static int count;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        hanoi(n,1,3,2); // 1번에서 3번으로 간다.
-        
-        System.out.println(count);
-        System.out.println(sb);
-    }
-    public static void hanoi(int n,int one, int three, int two) {
-    	if(n>0) {
-    		hanoi(n-1,one,two,three); // 1번을 2번으로 먼저 옮긴다.
-    		count++;
-    		sb.append(one).append(" ").append(three).append("\n");
-    		hanoi(n-1,two,three,one); // 2번을 3번으로 옮긴다.
-    	}
-    }
+ public static void main(String[] args)throws Exception{
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     bw.write(((1<<n)-1)+"");
+     hanoi(n,'1','3','2'); // 1번에서 3번으로 간다.
+     bw.flush();
+     bw.close();
+ }
+ public static void hanoi(int n,char one, char three, char two)throws Exception {
+ 	if(n>0) {
+ 		hanoi(n-1,one,two,three); // 1번을 2번으로 먼저 옮긴다.
+ 		print(one,three);
+ 		hanoi(n-1,two,three,one); // 2번을 3번으로 옮긴다.
+ 	}
+ }
+ public static void print(char from, char to)throws Exception{
+     bw.write('\n');
+     bw.write(from);
+     bw.write(' ');
+     bw.write(to);
+ }
 }
