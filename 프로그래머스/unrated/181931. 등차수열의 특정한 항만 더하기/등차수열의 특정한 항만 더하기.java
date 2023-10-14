da581e910d1f2e869c8ec7ed791531cf292411fd
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-14 12:07:57 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,16 @@
+// https://github.com/KimYongJ/algorithm
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        
+        for(int i=0; i<included.length; i++){
+            if(included[i]){
+                answer += a;
+            }
+            a+=d;
+        }
+        
+        
+        return answer;
+    }
+}
