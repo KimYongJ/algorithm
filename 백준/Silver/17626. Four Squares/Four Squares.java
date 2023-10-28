699fc7047305a5f3c26033270ae4f66760dc2448
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-28 21:48:35 +0900

    [Silver III] Title: Four Squares, Time: 256 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,21 @@
+// https://github.com/KimYongJ/algorithm
+import java.util.Scanner;
+class Main{
+    
+    public static void main(String[] args)throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1];
+            for(int j=1; j*j<=i; j++){
+                // dp의 이전 값과 임의의 j의 제곱에서 i를 뺀 값중 작은 것을 저장한다.
+                dp[i] = Math.min(dp[i],dp[i-j*j]);
+            }
+            dp[i] += 1;
+        }
+        System.out.println(dp[n]);
+    }
+}
