1d97f5150fc2ac9e470cf9e27c789e36d280ff05
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-28 21:52:16 +0900

    [Silver III] Title: Four Squares, Time: 168 ms, Memory: 14740 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,10 +1,11 @@
 // https://github.com/KimYongJ/algorithm
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 class Main{
     
     public static void main(String[] args)throws Exception{
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int dp[] = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
