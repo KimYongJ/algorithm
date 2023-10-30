dc31dd68e98348b6c96f6efa9ded60e6f38da872
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-10-30 22:40:07 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1038.33 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,27 @@
+//https://github.com/KimYongJ/algorithm
+class Solution {
+    public long solution(int cap, int n, int[] d, int[] p) {
+        long answer = 0;
+        for(int i=n-1; i>=0; i--){
+            if(d[i]!=0 || p[i]!=0){
+                answer += (i+1)*2;
+                minus(d,i,cap);
+                minus(p,i,cap);
+                if(d[i]!=0 ||p[i]!=0) i++;
+            }
+        }
+        return answer;   
+    }
+    public void minus(int[] arr, int idx,int cap){
+        while(idx>-1){
+            if(arr[idx]>=cap){
+                arr[idx] -= cap;
+                break;
+            }else{
+                cap -= arr[idx];
+                arr[idx] = 0;
+            }
+            idx--;
+        }
+    }
+}
