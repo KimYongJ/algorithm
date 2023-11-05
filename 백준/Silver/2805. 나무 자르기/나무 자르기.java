a18c2fe3d7c29e355770226bc8aefab3421a5cbf
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-05 11:17:25 +0900

    [Silver II] Title: 나무 자르기, Time: 312 ms, Memory: 18308 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,31 +1,25 @@
 // https://github.com/KimYongJ/algorithm
 class Main{
-
     public static void main(String[] args)throws Exception{
-    	int n = read();
-    	int total = read();
+    	int n = read(), m = read() , left = 0, right = 0;
     	int[] arr = new int[n];
-    	int left = 0, right = 0, mid = 0;
-    	for(int i=0; i<n; i++){
+        
+    	for(int i=0; i<n; i++)
             if(right<(arr[i] = read()))
             	right = arr[i];
-    	}
+    	
 
         while(left<right){
-        	mid = (left+right)/2;
+        	int mid = (left+right)/2;
             long sum = 0;
             for(int a: arr){
                 if(a-mid>0)
                     sum += a-mid;
-                
-                if(total<=sum){ // 자른 나무의 갯수가 구하려는 값보다 크거나 같다면 left값을 증가시킨다.
-                    left = mid+1;
-                    continue;
-                }
             }
-            if(sum<total)// 자른 나무의 갯수가 구하려는 값보다 작으면 right값을 감소시킨다.
+            if(m<=sum){ // 자른 나무의 갯수가 구하려는 값보다 크거나 같다면 left값을 증가시킨다.
+                left = mid+1;
+            }else if(sum<m)// 자른 나무의 갯수가 구하려는 값보다 작으면 right값을 감소시킨다.
                 right = mid;
-            
         }
         System.out.println(left-1);
     }
