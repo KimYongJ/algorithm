40324b1f9e4ef09b09c1dda55ca6c09434020e70
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-06 20:27:20 +0900

    [Gold IV] Title: 공유기 설치, Time: 208 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,26 +1,19 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.Arrays;
 class Main{
     public static void main(String[] args)throws Exception{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int N = Integer.parseInt(st.nextToken()); // 도현이의 집 개수
-    	int C = Integer.parseInt(st.nextToken()); // 공유기 개수
+    	int N = read(); // 도현이의 집 개수
+    	int C = read(); // 공유기 개수
     	int right = 0;
     	int left = 0;
     	int[] arr = new int[N]; // 집의 위치를 담을 배열
-    	for(int i=0; i<N; i++) {
-    		arr[i] = Integer.parseInt(br.readLine());
-    		if(right<arr[i]) right = arr[i]; // 가장큰 값을 저장 
-    	}
+    	for(int i=0; i<N; i++)
+    		arr[i] = read();
     	
     	Arrays.sort(arr); // 집 위치를 오름차순 정렬
-    	
+    	right = arr[N-1]; // 가장 큰 값을 저장 
     	while(left<=right) {
-    		int mid = right + left + 1 >> 1;
+    		int mid = right + left >> 1;
     		int cnt = 1;
     		int housePosition = arr[0];
     		// 공유기 갯수 카운트 로직
@@ -39,5 +32,16 @@ class Main{
     	} 
 
     	System.out.println(left - 1);
+    }
+    private static int read() throws Exception {
+        int value = 0;
+        while (true) {
+            int input = System.in.read();
+            if (input == ' ' || input == '\n') {
+                return value;
+            } else {
+                value = value * 10 + (input - 48);
+            }
+        }
     }
 }
