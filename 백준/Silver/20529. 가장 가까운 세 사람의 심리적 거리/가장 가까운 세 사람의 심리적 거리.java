0c5c3505e243eb9ccea61b43d412ccb0cd04db16
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-08 22:19:41 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 176 ms, Memory: 18424 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.java
@@ -1,23 +1,29 @@
 //https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 class Main{
-  static int min,N;
-  static String[] mbti;
-  static String[] comb = new String[3];
+    static int min,N;
+    static String[] mbti;
+    static String[] comb = new String[3];
+    static StringTokenizer st;
   public static void main(String[] args)throws Exception{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       StringBuilder sb = new StringBuilder();
       int T = Integer.parseInt(br.readLine());
       while(T-->0){
           N = Integer.parseInt(br.readLine());
-          mbti = br.readLine().split(" ");
-          if(N<33){
-              min = Integer.MAX_VALUE;
-              combination(0,0); // N개 중 3개를 뽑는 조합을 구현한다.
-          }else{
-              min = 0;
+          if(N>33){
+              br.readLine();
+              sb.append(0).append('\n');
+              continue;
           }
+          mbti = new String[N];
+          st = new StringTokenizer(br.readLine());
+          for(int i=0; i<N; i++)
+              mbti[i] = st.nextToken();
+          min = Integer.MAX_VALUE;
+          combination(0,0); // N개 중 3개를 뽑는 조합을 구현한다
           sb.append(min).append('\n');
       }
       System.out.println(sb.toString());
