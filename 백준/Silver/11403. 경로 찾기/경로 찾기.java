12a9823e1035832891a665e8b2732bb96e8de450
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-12 22:08:29 +0900

    [Silver I] Title: 경로 찾기, Time: 464 ms, Memory: 150184 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,26 +1,18 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.ArrayDeque;
 class Main{
 
     public static void main(String[] args)throws Exception{
     	ArrayList<ArrayList<Integer>> list = new ArrayList<>(); // 인접 리스트를 담을 리스트
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine());
-        
+        int N = read();
         int[][] result = new int[N][N];
         
         // 입력된 인접 리스트를 담는 코드
         for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
             ArrayList<Integer> part = new ArrayList<>();
             for(int j=0; j<N; j++){
-                int num = Integer.parseInt(st.nextToken());
+                int num = read();
                 if(num==1) // 방문 가능 노드만 넣는다.
                     part.add(j);
             }
@@ -63,5 +55,10 @@ class Main{
             }
         }
         
+    }
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
