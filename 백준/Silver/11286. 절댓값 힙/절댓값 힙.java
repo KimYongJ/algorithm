b72fadefc96300da707cf4f2790c9204d12065aa
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-13 21:58:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 252 ms, Memory: 18860 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,39 +1,22 @@
 // https://github.com/KimYongJ/algorithm
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Main{
     public static void main(String[] args)throws Exception{
-        PriorityQueue<Integer> p = new PriorityQueue<>();// 양수를 담을 것
-        PriorityQueue<Integer> n = new PriorityQueue<>((a,b)->b-a);// 음수를 담을 것 음수는 내림차순
-        
+        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>(){
+            public int compare(Integer a1, Integer a2){
+                if(Math.abs(a1) == Math.abs(a2)) return a1-a2;
+                return Math.abs(a1)-Math.abs(a2);
+            }
+        });
         StringBuilder sb = new StringBuilder();
         int N = read();
         while(N-->0){
             int num = read();
-            if(num==0){
-                Integer num1 = p.peek();
-                Integer num2 = n.peek();
-                if(num1==null && num2==null) sb.append(0).append('\n');
-                else if( num1==null && num2!=null ){
-                    sb.append(num2).append('\n');
-                    n.poll();
-                }else if(num1!=null && num2==null){
-                    sb.append(num1).append('\n');
-                    p.poll();
-                }else{
-                    if(Math.abs(num2)<=num1){
-                        sb.append(num2).append('\n');
-                        n.poll();
-                    }else{
-                        sb.append(num1).append('\n');
-                        p.poll();
-                    }
-                }
-            }else if(num<0){
-                n.add(num);
-            }else{
-                p.add(num);
-            }
+            if(num==0) 
+                sb.append(q.isEmpty() ? 0 : q.poll()).append('\n');
+            else q.add(num);
         }
         System.out.println(sb.toString());
     }
@@ -44,4 +27,5 @@ class Main{
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return isNegative ? ~n + 1 : n;
     }
+
 }
