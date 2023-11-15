a4d044d62cab9b78a8c35d573e3d06607c2e1949
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-15 22:47:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -8,67 +8,57 @@ import java.util.Collections;
 
 class Main{
     
-    static int cnt,N;
-    static char[][] arr;
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
-    static ArrayDeque<Node> q = new ArrayDeque<>();
-    static ArrayList<Integer> list = new ArrayList<>();
+    static int dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
     
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new char[N][N];
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        char[][] arr = new char[N][N];
+        int cnt = 0;
         
         for(int i=0; i<N; i++)
             arr[i] = br.readLine().toCharArray();
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
+        for(int i=0; i<N; i++)
+            for(int j=0; j<N; j++)
                 if(arr[i][j] != '0'){
                     cnt++;
-                    q.add(new Node(i,j,arr[i][j]));
+                    q.add(new int[] {i,j,arr[i][j]});
                     arr[i][j] = '0';
-                    list.add( BFS() );
+                    list.add( BFS(q,arr,N) );
                 }
-            }
-        }
         
-        StringBuilder sb = new StringBuilder();
         sb.append(cnt).append('\n');
+        
         Collections.sort(list);
+        
         for(int i=0; i< list.size() ; i++)
             sb.append( list.get(i) ).append('\n');
         
         System.out.print(sb);
     }
 
-    public static int BFS(){
+    public static int BFS(ArrayDeque<int[]> q,char[][] arr,int N){
         int area = 1;
         while(!q.isEmpty()){
-            Node node = q.poll();
+            int[] node = q.poll();
             for(int i=0; i<4; i++){
-                int x1 = node.x + dx[i];
-                int y1 = node.y + dy[i];
+                int x1 = node[0] + dx[i];
+                int y1 = node[1] + dy[i];
                 
-                if(x1<0 || y1<0 || x1>=N || y1>=N || arr[x1][y1] != node.c)
+                if(x1<0 || y1<0 || x1>=N || y1>=N || arr[x1][y1] != node[2])
                     continue;
                 
                 arr[x1][y1] = '0';
-                q.add(new Node(x1,y1,node.c));
+                q.add(new int[] {x1,y1,node[2]});
                 area++;
             }
         }
         return area;
     }
 
-}
-class Node{
-    int x,y;
-    char c;
-    Node(int x,int y, char c){
-        this.c = c;
-        this.x = x;
-        this.y = y;
-    }
 }
