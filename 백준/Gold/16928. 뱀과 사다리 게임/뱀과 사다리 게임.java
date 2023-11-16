9890599fd0a858fadb682c81a515438e27a8c53c
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-16 14:08:56 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,31 +1,20 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 class Main{
 	
-	static int dist=Integer.MAX_VALUE,transport[];
-	static boolean[] visit;
     public static void main(String[] args)throws Exception{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	int N = Integer.parseInt(st.nextToken());
-    	int M = Integer.parseInt(st.nextToken());
-    	transport = new int[101];
-    	visit = new boolean[101];
+    	int N = read();
+    	int M = read();
+    	int[] transport = new int[101];
+    	boolean[] visit = new boolean[101];
     	for(int i=0; i<N+M; i++) {
-    		st =  new StringTokenizer(br.readLine());
-    		int idx = Integer.parseInt(st.nextToken());
-    		int value = Integer.parseInt(st.nextToken());
+    		int idx = read();
+    		int value = read();
     		transport[idx] = value;
     	}   	
-    	BFS(1,0); // 순서 : 시작인덱스, 한번에이동한 칸의 수  
-    }
-    public static void BFS(int startIndex, int dist) {
     	ArrayDeque<int[]> q = new ArrayDeque<>();
-    	q.add(new int[] {startIndex,dist});
-    	visit[startIndex] = true;
+    	q.add(new int[] {1,0});
+    	visit[1] = true;
     	
     	while(!q.isEmpty()) {
     		int[] node = q.poll();
@@ -48,5 +37,10 @@ class Main{
     		}
     		
     	}
+    }
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
