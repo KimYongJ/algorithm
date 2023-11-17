a4952b2831e1b30049a8403671d22bb6c84d8d26
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-17 14:38:56 +0900

    [Gold V] Title: 리모컨, Time: 344 ms, Memory: 88292 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -4,7 +4,7 @@
 // DFS로 숫자 c를 만들며, 만들 때 자릿수 하나씩을 만들어간다.
 // 최종 만들어진 숫자 c를 set에 담는다.(이 때 100은 default로 들어가있다.)
 // 숫자 c의 길이 + c와 goal의 차이의 절댓값을 구한다(이 때 100은 길이가 0이다.)
-// DFS의 인자 : 깊이, 만들어진 자릿수들의정보(string) , 가능한숫자배열(arr), 최대 깊이
+// DFS의 인자 : 깊이, 만들어진 자릿수들의정보 , 가능한숫자배열(arr), 최대 깊이
 // DFS 주의점 : 숫자 c를 만들 때 goal과 길이가 같은 것만 만들 경우 답이 틀리게 된다.goal길이 낮거나 높게 잡아 주어야 한다. 
 // 반례1) 입력 1555 , 3 , 0 1 9 답 670나와야함 반례2) 99999 , 9 , 0 2 3 4 5 6 7 8 9 답 11118 나와야 함
 // [빠른 처리를 위한 특별 분기]
@@ -13,18 +13,13 @@
 // 3) 리모컨의 모든 숫자가 입력 가능이면 gaol의 길이 반환
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 class Main{
 	
+	static String goal;    				// 목표 문자열
+	static int goalLength, goalInt; 	// 목표 문자열의 길이
+	static int min = Integer.MAX_VALUE; // 최소 움직임 값
 	
-	static String goal;    // 목표 문자열
-	static int goalLength, goalInt; // 목표 문자열의 길이
-	static int min = Integer.MAX_VALUE;
-	static Set<Integer> nList = new HashSet<Integer>() {{
-			add(0);add(1);add(2);add(3);add(4);add(5);add(6);add(7);add(8);add(9);
-		   }};
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		goal = br.readLine();
@@ -37,42 +32,47 @@ class Main{
 			min = Math.min(goalLength, Math.abs(100-goalInt));
 			System.out.print(min);return;
 		}
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while(t-->0) {
-			nList.remove( Integer.parseInt(st.nextToken()) );
+		boolean[] nList = new boolean[10];
+		for(int i=0; i<t; i++) {
+			nList[ Integer.parseInt(st.nextToken()) ] = true;
 		}
 		
-		int arr[] = new int[nList.size()], idx = 0;
+		int arr[] = new int[10-t], idx = 0;
 	
-		for(int n : nList) arr[idx++] = n;
-		
-		
+		for(int i=0; i<10; i++) {
+			if(!nList[i]) {
+				arr[idx++] = i;
+			}
+		}
 		// 특별 분기
 		if(goalInt == 100) { // 목표가 100이면 100출력
-			System.out.print(0); return;
+			System.out.print(0); 
+			return;
 		}else if(arr.length == 10) {
-			System.out.print(Math.abs( goalInt - 100 ));return;
+			System.out.print(Math.abs( goalInt - 100 ));
+			return;
 		}
 		int i = goalLength == 1 ? goalLength : goalLength-1;
 		for(; i<=goalLength+1; i++)
-			DFS(0,"",arr,i); // 숫자 c 만들기 
+			DFS(0,0,arr,i); // 숫자 c 만들기 
 
         min = Math.min(min, Math.abs(goalInt-100));
         
 		System.out.println(min);
 	}
-	public static void DFS(int depth,String make,int[] arr,int max) {
+	public static void DFS(int depth,int make,int[] arr,int max) {
 		if(depth==max) {
 			// 최소 이동 값을 바로 min으로 저장.
-			int len = make.length(); // 해당 숫자를 누르는데까지 걸리는 클릭수이며 100일 경우 길이는 0
-			int value = Math.abs(Integer.parseInt(make)-goalInt) + len;// 만든숫자와 목표숫자를 빼고 길이를 더해서 이동횟수를 구한다.
+			int len = String.valueOf(make).length(); // 해당 숫자를 누르는데까지 걸리는 클릭수이며 100일 경우 길이는 0
+			int value = Math.abs(make-goalInt) + len;// 만든숫자와 목표숫자를 빼고 길이를 더해서 이동횟수를 구한다.
 			if(value<min)
 				min = value;
 			return;
 		}
 		for(int i=0; i<arr.length; i++) {
-			DFS(depth+1, make+arr[i],arr,max);
+			DFS(depth+1, make*10+arr[i],arr,max);
 		}
-	}   
-   
+	}  
 }
