74d21d3998c2e5e95cce3a7a81697a19674e7f25
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-21 13:41:09 +0900

    [Silver I] Title: 정수 삼각형, Time: 160 ms, Memory: 15472 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,26 +1,26 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
-	
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args)throws Exception{		
+		int N = read();
 		
 		int[][]arr = new int[N][N];
 		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
 			for(int j=0; j<=i; j++)
-				arr[i][j] = Integer.parseInt(st.nextToken());
-		}
+				arr[i][j] = read();
+		
 		
 		for(int i=N-2; i>=0; i--)
 			for(int j=0; j<=i; j++) 
 				arr[i][j] += Math.max(arr[i+1][j],arr[i+1][j+1]);
 	
 		System.out.println(arr[0][0]);
+	}
+    private static int read() throws Exception
+    {
+	    int c, n = System.in.read() & 15;
+	    while ((c = System.in.read()) > 32)
+	    	n = (n << 3) + (n << 1) + (c & 15);
+	    return n;
 	}
 }
