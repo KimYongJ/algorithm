275ca2666b1cf326962a13313015904f9b2c2170
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-21 16:20:39 +0900

    [Silver I] Title: 스티커, Time: 316 ms, Memory: 27604 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,25 +1,17 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
-	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0) {
-			int N = Integer.parseInt(br.readLine());
+			int N = read();
+			
 			int[][] arr = new int[2][N+2];
 			int[][] DP  = new int[2][N+2];
 			
-			st = new StringTokenizer(br.readLine());
-			for(int i=2; i<N+2;i++) 
-				arr[0][i] = Integer.parseInt(st.nextToken());
-			st = new StringTokenizer(br.readLine());
-			for(int i=2; i<N+2; i++)
-				arr[1][i] = Integer.parseInt(st.nextToken());
+			for(int i=2; i<N+2;i++) arr[0][i] = read();
+
+			for(int i=2; i<N+2; i++) arr[1][i] = read();
 			
 			for(int i=2; i<N+2; i++) {
 				// DP 시작
@@ -27,9 +19,13 @@ class Main{
 				DP[1][i] = Math.max(DP[0][i-1], DP[0][i-2])+arr[1][i];
 			}
 			sb.append(Math.max(DP[0][N+1], DP[1][N+1])).append('\n');
-
 		}
 		System.out.println(sb);
 	}	
-
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
 }
