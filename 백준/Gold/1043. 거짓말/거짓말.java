534bb37087e4c8938638512a55741637e9218d3e
김용주 <106525587+KimYongJ@users.noreply.github.com>
2023-11-22 16:22:44 +0900

    [Gold IV] Title: 거짓말, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,33 +1,27 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int[] parent;
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 사람수
-		int M = Integer.parseInt(st.nextToken()); // 파티 수
+		int N = read(); // 사람수
+		int M = read(); // 파티 수
 		
 		ArrayList<int[]> partyList = new ArrayList<>();
 		parent = new int[N+1]; // 부모노드를 담을 배열 선언
 		for(int i=1; i<N+1; i++)
 			parent[i] = i;
 		
-		st = new StringTokenizer(br.readLine());
-		int r = Integer.parseInt(st.nextToken());
+
+		int r = read();
 		for(int i=0; i<r; i++) { 
-			parent[Integer.parseInt(st.nextToken())] = 0; // 진실을 알고 있다면 부모가 -1값임 
+			parent[read()] = 0; // 진실을 알고 있다면 부모가 -1값임 
 		}
 		
 		for(int i=0; i<M; i++) {// 파티에 따른 입장 사람들
-			st = new StringTokenizer(br.readLine());
-			int[] arr = new int[Integer.parseInt(st.nextToken())];
+			int[] arr = new int[read()];
 			for(int j=0; j<arr.length; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
+				arr[j] = read();
 			}
 
 			partyList.add(arr); // 파티 리스트를 담는다.
@@ -67,5 +61,11 @@ class Main{
 	public static int getParent(int x) {
 		if(parent[x]==x) return x;
 		return getParent(parent[x]);
+	}
+    private static int read() throws Exception{
+	    int c, n = System.in.read() & 15;
+	    while ((c = System.in.read()) > 32)
+	    	n = (n << 3) + (n << 1) + (c & 15);
+	    return n;
 	}
 }
