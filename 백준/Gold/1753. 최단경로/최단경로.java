12188658d57591be46a3d92ae2b56dd7f6f0fa23
YongJu Kim <kyjdummy@gmail.com>
2023-11-24 13:41:19 +0900

    [Gold IV] Title: 최단경로, Time: 432 ms, Memory: 31512 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,50 +1,54 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
 		final int INF = 200_001;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(br.readLine());
+		int N = read();
+		int M = read();
+		int start = read();
 		
 		ArrayList<Node>[] list = new ArrayList[N+1];
 		for(int i=1; i<=N; i++)
 			list[i] = new ArrayList<Node>();
 		
 		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
+			int d = read();
 			list[a].add(new Node(b,d));	
 		}
 		
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.dist-b.dist);
+		boolean[] visit = new boolean[N+1];
 		int[] dist = new int[N+1];
 		Arrays.fill(dist, INF);
 		
 		dist[start] = 0;
 		pq.add(new Node(start,0));
 		while(!pq.isEmpty()) {
-			Node node = pq.poll();
-			int nowNode = node.node;
-			int nowDist = node.dist;
+			Node nowNodeObj = pq.poll();
+			int nowNode = nowNodeObj.node;
+			int nowDist = nowNodeObj.dist;
+			
+			if(visit[nowNode])  // 방문한노드라면 스킵
+				continue;
+			
+			visit[nowNode] = true; // 방문처리
+			
 			for(int i=0; i<list[nowNode].size(); i++) {
-				int nextNode = list[nowNode].get(i).node;
-				int nextDist = list[nowNode].get(i).dist;
+				Node nextNodeObj = list[nowNode].get(i);
+				int nextNode = nextNodeObj.node; // 방금 큐에서 꺼낸 nowNodeObj의 인접노드
+				int nextDist = nextNodeObj.dist; // 방금 큐에서 꺼낸 nowNodeObj의 인접노드의 거리
 				
-				if(dist[nextNode] > nowDist + nextDist) {
-					dist[nextNode] = nowDist + nextDist;
-					pq.add(new Node(nextNode,nowDist + nextDist));
+				int distSum = nowDist + nextDist;// 방금 큐에서 꺼낸 노드까지의 거리와 꺼낸 노드의 인접리스트 까지 거리
+				if(dist[nextNode] > distSum) {// 최단거리 저장배열 dist값보다 작다면 값 갱신 및 큐에 데이터 삽입 
+					dist[nextNode] = distSum;
+					pq.add(new Node(nextNode, distSum));
 				}
 			}
 		}
@@ -55,6 +59,12 @@ class Main{
 		}
 		System.out.print(sb);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13) System.in.read();
+        return n;
+    }
 }
 class Node{
 	int node, dist;
