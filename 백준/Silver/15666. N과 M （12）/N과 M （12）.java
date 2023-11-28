64a7912ef582ec01e1d7820d40285211140133c9
YongJu Kim <kyjdummy@gmail.com>
2023-11-28 16:04:55 +0900

    [Silver II] Title: N과 M (12), Time: 132 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,8 +1,5 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -10,16 +7,14 @@ class Main{
 	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken()); // N개의 숫자 중
-		M 		= Integer.parseInt(st.nextToken()); // M개를 고른다.
+		N 		= read(); // N개의 숫자 중
+		M 		= read(); // M개를 고른다.
 		arr		= new int[N];
 		result	= new int[N];
-		st 		= new StringTokenizer(br.readLine());
+
 		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
