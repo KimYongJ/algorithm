ac6bb8e643a207c0e9e3311e247ee1e16a79428f
YongJu Kim <kyjdummy@gmail.com>
2023-11-28 22:13:19 +0900

    [Silver V] Title: 피보나치 수 4, Time: 184 ms, Memory: 21328 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -2,7 +2,7 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 class Main{
     public static void main(String[] args)throws Exception{
@@ -13,9 +13,9 @@ class Main{
         	return;
         }
         
-        BigDecimal a = new BigDecimal("0");
-        BigDecimal b = new BigDecimal("1");
-        BigDecimal c = new BigDecimal("0");
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = BigInteger.ONE;
+        BigInteger c = BigInteger.ZERO;
 
         for(int i=2; i<=N; i++){
             c = a.add(b);
