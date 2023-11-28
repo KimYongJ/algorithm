556765dbf0c863ea1ea11fbdf61a1e63c5293e73
YongJu Kim <kyjdummy@gmail.com>
2023-11-28 17:57:07 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 272 ms, Memory: 25964 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,35 +1,48 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 class Main{
     public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+    	StringBuilder sb = new StringBuilder();
+    	Reader r = new Reader();
         
-        st = new StringTokenizer(br.readLine());
+        int n = r.read();
+        int m = r.read();
         
         int[] dp = new int[n+1];
-        dp[1] = Integer.parseInt(st.nextToken());
-        for(int i=2; i<=n; i++){
-            dp[i] = Integer.parseInt(st.nextToken()) + dp[i-1];
-        }
+        
+        for(int i=1; i<=n; i++)
+            dp[i] = r.read() + dp[i-1];
         
         for(int t=0; t<m; t++){
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int sum = 0;
-            if(i==1) {
-            	sum = dp[j];
-            }else {
-            	sum = dp[j] - dp[i-1];
-            }
-            sb.append(sum).append("\n");
+            int i = r.read();
+            int j = r.read();
+            sb.append( dp[j] - dp[i-1] ).append("\n");
         }
         System.out.println(sb.toString());
+    }
+}
+
+// 빠른 입력을 위해 만듦
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    int read() throws Exception {
+        int n = 0;
+        byte c;
+        while ((c = get()) <= 32);
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = get()));
+        return n;
+    }
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+    byte get() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
