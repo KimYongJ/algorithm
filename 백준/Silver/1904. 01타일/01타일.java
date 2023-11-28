ffd2fdc92ca9c63a388d3d39bf5de7c468550182
YongJu Kim <kyjdummy@gmail.com>
2023-11-28 22:43:13 +0900

    [Silver III] Title: 01타일, Time: 144 ms, Memory: 18172 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -8,12 +8,16 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        int dp[] = new int[1000001];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3; i<=n; i++){
-            dp[i] = (dp[i-1] + dp[i-2])% 15746;
-        }
-        System.out.println(dp[n]);
+        if(n>2){
+            int dp[] = new int[n+1];
+            dp[1] = 1;
+            dp[2] = 2;
+        
+            for(int i=3; i<=n; i++)
+                dp[i] = (dp[i-1] + dp[i-2])% 15746;
+        
+            System.out.println(dp[n]);
+        }else 
+            System.out.println(n);
     }
 }
