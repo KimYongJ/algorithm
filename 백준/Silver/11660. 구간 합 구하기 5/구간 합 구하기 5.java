5458cfe7d6089ef432db14ffbf9543653f7f0903
YongJu Kim <kyjdummy@gmail.com>
2023-11-28 17:39:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 324 ms, Memory: 30148 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,50 +3,56 @@
 class Main{
 	
 	public static void main(String[] args)throws Exception{
+		Reader r = new Reader();
 		StringBuilder   sb 	= new StringBuilder();
-		int N 				= read();
-		int M 				= read();
-		int arr[][] 		= new int[N+1][N+1];
+		int x1, y1, x2, y2, total;
+		int N 				= r.read();
+		int M 				= r.read();
 		int sum[][]			= new int[N+1][N+1];
-		int find[][]		= new int[M][4];
 		
 		// 배열 insert
-		for(int i=1; i<=N; i++) {
+		for(int i=1; i<=N; i++) 
 			// arr배열에 원 값을 넣음 과 동시에 구간합을 구합니다. 
-			for(int j=1; j<=N; j++) {
-				arr[i][j] = read();
-				sum[i][j] = arr[i][j] + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1];// 해당 좌표의 구간합 구하는 공식
-			}
-		}
-		// 구해야하는 좌표 값 insert
-		for(int i=0; i<M; i++) {
-			find[i][0] = read();
-			find[i][1] = read();
-			find[i][2] = read();
-			find[i][3] = read();
-		}
-		
-
-		for(int i=0; i<M; i++) {
-			int total	= 0;
-			int x1 		= find[i][0];
-			int y1 		= find[i][1];
-			int x2 		= find[i][2];
-			int y2 		= find[i][3];
+			for(int j=1; j<=N; j++) 
+				sum[i][j] = r.read() + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1];// 해당 좌표의 구간합 구하는 공식
 			
+		// 좌표를 입력 받음과 동시에 구간합을 계산해 저장합니다.
+		for(int i=0; i<M; i++) {
+			x1 = r.read();
+			y1 = r.read();
+			x2 = r.read();
+			y2 = r.read();
 			// 왼쪽 사각형 : x2 , y1-1
 			// 위쪽 사각형 : x1-1 , y2
 			// 겹치는 사각형 : x1-1, y1-1
-			total += sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1];
-			
-			sb.append(total).append('\n');
+			total = sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1];
+			sb.append( total ).append('\n');
 		}
 		System.out.println(sb);
 	}
-	// 빠른 입력을 위해 만듦
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+}
+// 빠른 입력을 위해 만듦
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    int read() throws Exception {
+        int n = 0;
+        byte c;
+        while ((c = get()) <= 32);
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = get()));
         return n;
+    }
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+    byte get() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
