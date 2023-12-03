9cca4025401f88fb25a2714af565d66147f0e5d7
YongJu Kim <kyjdummy@gmail.com>
2023-12-03 23:06:19 +0900

    [Gold II] Title: 네트워크 복구, Time: 612 ms, Memory: 92192 KB -BaekjoonHub

---
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.java
@@ -0,0 +1,97 @@
+// https://github.com/KimYongJ/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+class Main{
+    
+    static int N, M; 
+    static DIST dist[]; // 1번까지 최단거리와 연결과정에서 노드들을 담을 DIST객체 배열
+    static ArrayList<Node>[] list;// 인접리스트
+    static final int INF = 9_999;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken())+1;
+        M = Integer.parseInt(st.nextToken());
+        list = new ArrayList[N];
+        dist = new DIST[N];
+        for(int i=1; i<N; i++){
+            list[i] = new ArrayList<Node>();
+            dist[i] = new DIST(INF,null);
+        }
+        
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            list[a].add(new Node(b,d));
+            list[b].add(new Node(a,d));
+        }
+        if(N==2) {// 노드가 1개일 경우
+        	sb.append(1).append('\n');
+        	sb.append(1).append(' ').append(1);
+        }else {
+        	Dijkstra();
+        	sb.append(N-2).append('\n');
+        	for(int i=2; i<N; i++) {
+        		int end = dist[i].list.get(0);
+        		if(end==1) {
+        			sb.append(1).append(' ').append(i);
+        		}else {
+        			sb.append(i).append(' ').append(end);
+        		}
+        		sb.append('\n');
+        	}
+        }
+        System.out.println(sb);
+    }
+    
+    public static void Dijkstra(){
+        PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.dist-b.dist);
+        dist[1].dist = 0;
+        pq.add(new Node(1,0));
+        
+        while(!pq.isEmpty()){
+            Node now = pq.poll();
+            int nowNode = now.node; // 현재 노드
+            int until_now_dist = now.dist;// 현재 노드까지 오는데 걸리는 거리
+            
+            for(int i=0; i<list[nowNode].size(); i++){
+                Node next = list[nowNode].get(i);
+                int nextNode = next.node;
+                int nextDist = next.dist;
+                
+                int distSum = until_now_dist + nextDist;
+                
+                if(dist[nextNode].dist > distSum){
+                    dist[nextNode].dist = distSum;
+                    pq.add(new Node(nextNode, distSum));
+                    dist[nextNode].list = new ArrayList<Integer>();
+                    dist[nextNode].list.add(nowNode);
+                    
+                }
+            }
+            
+        }
+        
+    }
+}
+class DIST{
+    int dist;
+    ArrayList<Integer> list;
+    DIST(int dist, ArrayList<Integer> list){
+        this.dist = dist;
+        this.list = list;
+    }
+}
+class Node{
+    int node, dist;
+    Node(int node, int dist){
+        this.node = node;
+        this.dist = dist;
+    }
+}
