66e24ea5dba60989f3ba54707c2690ff8e3d3a96
YongJu Kim <kyjdummy@gmail.com>
2023-12-03 20:20:02 +0900

    [Gold II] Title: 등산, Time: 128 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.java
+++ 백준/Gold/1486. 등산/등산.java
@@ -7,103 +7,101 @@ import java.util.StringTokenizer;
 
 class Main{
 	static final int INF = 100_000_000;
-	static int N, M, T, D, minTime, arr[][];
+	static int N, M, T, D, arr[][];
 	static int dxy[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-	static int dist[][][][] = new int[26][26][26][26];
-	static boolean visit[][];
 	
-	static PriorityQueue<Node> pq; // 높은 순으로 내림 차순 정렬
-	static PriorityQueue<Node> q; // 다익스트라에서 사용할 우선순위 큐, dist기준 오름 차순 정렬
+	static PriorityQueue<Node> pq; // 다익스트라에서 사용할 우선순위 큐, time 기준 오름차순 정렬
 	
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken())+1; // 세로
         M = Integer.parseInt(st.nextToken())+1; // 가로
-        T = Integer.parseInt(st.nextToken()); // 52이하, 높이차가 T보다 작아야 함
-        D = Integer.parseInt(st.nextToken()); // 백만이하, 어두워 지는 시간 
+        T = Integer.parseInt(st.nextToken());   // 최대 52이하, 높이차가 T보다 작아야 함
+        D = Integer.parseInt(st.nextToken());   // 최대 백만이하, 어두워 지는데 까지 시간
         arr = new int[N][M];
-        pq = new PriorityQueue<Node>((a,b)->b.dist-a.dist); // 높이 내림 차순 정렬
-        
-        // 최단거리를 담을 dist배열 초기화
-        for(int k=1; k<26; k++) 
-        	for(int i=1; i<26; i++) 
-        		for(int j=1; j<26; j++) 
-        			Arrays.fill(dist[k][i][j], INF);
-        		
         	
         // 입력되는 높이 값을 int로 변환 후 arr배열에 담음
         for(int i=1; i<N; i++){
             String str = br.readLine();
             for(int j=1; j<M; j++){
             	char c = str.charAt(j-1);
-            	arr[i][j] = c -= c<='Z' ? 65 : 71;
-            	
-            	pq.add(new Node(i,j,c));// 높이가 높은 순으로 정렬
+            	arr[i][j] = c<='Z' ? c-65 : c-71;
             }
         }
         
-        Dijkstra(1,1); // 1,1에서 모든 정점으로 가는 최단거리를 구해 놓습니다. 
+        int[][] forward = Dijkstra(true); // 1,1에서 모든 정점으로 가는 최단거리를 구해 놓습니다. 
+        int[][] backward = Dijkstra(false);// 모든 정점에서 1,1까지 오는 거리를 구합니다. 
         
-        while(!pq.isEmpty()) {
-        	Node asc = pq.poll(); // 높이가 높은것을 우선순위에서 큐에서 하나 꺼냅니다. 
-        	int Y = asc.y;
-        	int X = asc.x;
+        int max = 0; // 가장 높은 값
+        
+        for(int i=1; i<N; i++) 
+        	for(int j=1; j<M; j++) 
+        		if(forward[i][j] + backward[i][j] <= D) // 특정 점으로 갔다 오는 시간의 합이 D보다 작을 때 
+        			max = Math.max(max, arr[i][j]);// 큰 높이를 max에 저장
+        		
         	
-        	Dijkstra(Y, X); // 꺼낸 Y, X를 시작점으로 하여 모든 정점에 대한 최단거리를 저장합니다. 
-        	
-        	if( dist[1][1][Y][X] + dist[Y][X][1][1] <= D ) {
-        		System.out.println(arr[Y][X]);
-        		return;
-        	}
-        }
-        System.out.println(0);
+        
+        System.out.println(max);
     }
-    public static void Dijkstra(int startY, int startX) {
-    	q = new PriorityQueue<Node>((a,b)->a.dist-b.dist);
-    	dist[startY][startX][startY][startX] = 0;
+    public static int[][] Dijkstra(boolean type) {
+    	int dist[][] = new int[N][M]; // 1,1에서 모든 정점으로 혹은, 모든 정점에서 1,1로의 최단거리를 담을 배열
+    	for(int i=1; i<N; i++) // 이동 거리 초기화 
+    		Arrays.fill(dist[i], INF);
     	
-    	q.add(new Node(startY, startX, 0));
+    	pq = new PriorityQueue<Node>((a,b)->a.time-b.time); // 시간 기준 오름차순 정렬
     	
-    	while(!q.isEmpty()) {
-    		Node now = q.poll();
-    		
-    		if(dist[startY][startX][now.y][now.x] < now.dist)
-    			continue;
+    	dist[1][1] = 0; // 1,1까지 걸리는 시간은 0
+    	pq.add(new Node(1,1,0));// 초기 값 삽입
+    	
+    	while(!pq.isEmpty()) {
+    		Node now = pq.poll();
+    		int nowY = now.y;// 현재 y좌표
+    		int nowX = now.x;// 현재 x좌표
+    		int until_now_time = now.time;// 현재 y,x좌표까지 오는데 걸린 시간
     		
     		for(int xy[] : dxy) {
-    			int y = now.y + xy[0];
-    			int x = now.x + xy[1];
-    			int time;
-    			if(y<1 || x<1 || y>=N || x>=M)
-    				continue;
+    			int newY = nowY + xy[0];
+    			int newX = nowX + xy[1];
+    			int newTime = until_now_time;
     			
-    			if(Math.abs(arr[now.y][now.x]- arr[y][x]) > T) // 높이 차이가 T보다 크다면 연산을 하지 않는다.
-    				continue;
+    			if(newY<1 || newX<1 || newY>=N || newX>=M) continue; // 새로 만든 좌표가 범위를 벗어날 경우 연산 스킵
+    			if(Math.abs(arr[nowY][nowX] - arr[newY][newX]) > T) continue;// 새로만든 좌표와 현재 좌표의 높이 차가 T보다 크면 연산 스킵
+    			
+    			if(type) { // 정방향일 경우 newTime 연산
+	    			if(arr[nowY][nowX] >= arr[newY][newX]) { // 이동하려는 곳이 낮거나 같은 곳이면
+	    				newTime += 1;
+	    			}else {// 이동하려는 곳이 높은 곳일 경우 
+	    				newTime += (int)Math.pow(arr[newY][newX]-arr[nowY][nowX],2);
+	    			}
+    			}else {// 역방향일 경우 newTime 연산
+	    			if(arr[nowY][nowX] <= arr[newY][newX]) { // 이동하려는 곳이 높은 곳이면
+	    				newTime += 1;
+	    			}else {// 이동하려는 곳이 낮은 곳일 경우
+	    				newTime += (int)Math.pow(arr[newY][newX]-arr[nowY][nowX],2);
+	    			}
+    			}
     			
     			
-    			if(arr[now.y][now.x] >= arr[y][x]) { // 가려는 곳이 더 낮다면
-    				time = now.dist + 1; // 지금까지 걸린 시간에+1 처리
-    			}else // 가려는 곳이 더 높다면
-    				time = (int)Math.pow(arr[now.y][now.x]- arr[y][x],2) + now.dist;
-
-    			if(dist[startY][startX][y][x] > time && time <= D) { // startY,startX에서 시작해서 y,x좌표로 가는 것을 구해놓은 것보다, now까지 와서 y,x좌표로 가는 시간이 더 작을 때 갱신
-    				
-    				dist[startY][startX][y][x] = time; // 최단거리 갱신 후 큐에 삽입
-    				q.add(new Node(y,x,time));
+    			
+    			if(newTime <= D && dist[newY][newX] > newTime) {
+    				dist[newY][newX] = newTime;
+    				pq.add(new Node(newY, newX, newTime));
     			}
     			
     		}
     	}
     	
+    	
+    	return dist;
     }
 }
 
 class Node{
-	int y, x, dist;
-	Node(int y, int x, int dist){
+	int y, x, time;
+	Node(int y, int x, int time){
 		this.y = y;
 		this.x = x;
-		this.dist = dist;
+		this.time = time;
 	}
 }
