f8be738497af7a40abb38aa57180fe410ce3e414
YongJu Kim <kyjdummy@gmail.com>
2023-12-07 00:45:01 +0900

    [Silver IV] Title: 반복수열, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -1,16 +1,11 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 class Main{
     
     public static void main(String[] args)throws Exception{
     	ArrayList<Integer> list = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
+        int n = read();
+        int r = read();
         DFS(n, r, list);
     }
     public static void DFS(int n, int r, ArrayList<Integer> list){
@@ -25,5 +20,10 @@ class Main{
             n/=10;
         }
         DFS(result, r, list);
+    }
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
