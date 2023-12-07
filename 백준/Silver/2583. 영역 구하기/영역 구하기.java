9da1822d5566fb123c0c42bb2df52ce5a13004e9
YongJu Kim <kyjdummy@gmail.com>
2023-12-08 00:12:54 +0900

    [Silver I] Title: 영역 구하기, Time: 128 ms, Memory: 15112 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,29 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 class Main{
 	
 	static int dxy[][] = {{0,1},{1,0},{-1,0},{0,-1}};
 	static int Y, X, K;
 	static boolean visit[][];
-	
+	static int cnt, area;
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		Y = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		Y = read();
+		X = read();
+		K = read();
 		visit = new boolean[X][Y];
 		for(int i=0; i<K; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Y-Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Y-Integer.parseInt(st.nextToken());
+
+			int x1 = read();
+			int y1 = Y-read();
+			int x2 = read();
+			int y2 = Y-read();
 			
 			for(int x=x1; x< x2; x++)
 				for(int y=y2; y<y1; y++)
@@ -34,8 +29,10 @@ class Main{
 		for(int x=0; x<X; x++)
 			for(int y=0; y<Y; y++)
 				if(!visit[x][y]) {
-					cnt++;
-					list.add( DFS(x, y) );
+					area = 0;
+					cnt +=1;
+					DFS(x, y);
+					list.add( area );
 				}
 
 		
@@ -47,16 +44,23 @@ class Main{
 		
 		System.out.println(sb);
 	}
-	public static int DFS(int x, int y) {
-		visit[x][y] = true;
-		int cnt = 0;
+	// DFS 함수 실행
+	public static void DFS(int x, int y) {
+		visit[x][y] = true; // 전달된 노드 방문처리
+		area += 1;
 		for(int xy[] : dxy) {
 			int newX = x+xy[0];
 			int newY = y+xy[1];
 			if( newX>=0 && newY>=0 && newX<X && newY<Y && !visit[newX][newY])
-				cnt += DFS(newX, newY);
-
+				DFS(newX, newY);
 		}
-		return cnt+1;
 	}
+	
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
 }
