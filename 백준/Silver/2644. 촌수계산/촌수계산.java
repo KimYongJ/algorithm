cb524e9cae57633ad330cc285fcae0621fc09c36
YongJu Kim <kyjdummy@gmail.com>
2023-12-07 22:08:02 +0900

    [Silver II] Title: 촌수계산, Time: 116 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -28,14 +28,15 @@ class Main{
         System.out.println(result);
     }
     public static void DFS(int nowNode,int dist){
-        if(visit[nowNode] || nowNode == B){
-            if(nowNode == B)result = dist;
-            return;
+        if(visit[nowNode]) return;
+        if(nowNode == B){
+            result = dist;return;
         }
         visit[nowNode] = true;
         for(int i=0; i<adList[nowNode].size(); i++)
             DFS( adList[nowNode].get(i) , dist+1);
     }
+    // 빠른 입력을 위한 함수
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
