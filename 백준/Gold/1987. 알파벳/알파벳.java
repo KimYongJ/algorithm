5890daaf964066ba7ad36c46f146d0c46ff18572
YongJu Kim <kyjdummy@gmail.com>
2023-12-10 15:57:20 +0900

    [Gold IV] Title: 알파벳, Time: 840 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,50 +1,45 @@
+// github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main{
-	
-	static int dxy[][] = {{0,1},{1,0},{-1,0},{0,-1}};
-	static boolean visit_alpha[] = new boolean[26];
-	static int Y, X, result = 1;
-	static char arr[][];
+    
+    static int R, C, arr[][];
+    static int result = 1;
+    static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Y = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        arr = new char[Y][X];
         
-        for(int i=0; i<Y; i++)
-            arr[i] = br.readLine().toCharArray();
-         
-        DFS(0,0,0);
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        arr = new int[R][C];
+        
+        for(int i=0; i<R; i++){
+            String str = br.readLine();
+            for(int j=0; j<C; j++)
+                arr[i][j] = str.charAt(j)-'A'; // 숫자로 해당 문자를 변환해 넣음
+        }
+        
+        DFS(0,0,0,0); // 방문한 문자를 bit로 연산한다. A부터 Z까지 26개이므로 26비트로 계산가능
         
         System.out.println(result);
     }
-    public static void DFS(int y, int x, int depth) {
-    	if(visit_alpha[arr[y][x]-'A']) {
-    		if(result < depth)
-    			result = depth;
-    		return;
+    public static void DFS(int i, int j, int visit_bit, int cnt){
+    	if((visit_bit | (1<<arr[i][j])) == visit_bit || result == 26) { // 방문한 비트면 종결
+            if(result < cnt) {
+            	result = cnt;
+            }
+            return;
     	}
-    	visit_alpha[arr[y][x]-'A'] = true;
-    	for(int xy[] : dxy) {
-    		int newY = y + xy[0];
-    		int newX = x + xy[1];
-    		if(newY>=0 && newX>=0 && newY<Y && newX<X)
-    			DFS(newY, newX, depth+1);
-    	}
-    	visit_alpha[arr[y][x]-'A'] = false;
-    }
-}
-class Node{
-    int y, x, dist;
-    boolean visit[];
-    Node(int y, int x, int dist, boolean visit[]){
-        this.y = y;
-        this.x = x;
-        this.dist = dist;
-        this.visit = visit;
+    	visit_bit = (visit_bit | (1<<arr[i][j])); // 해당 비트 방문처리
+        for(int xy[] : dxy){
+            int newI = i + xy[0]; // 새로운 좌표 I 생성
+            int newJ = j + xy[1]; // 새로운 좌표 J 생성
+            if(newI>=0 && newJ >=0 && newI <R && newJ <C) { // 좌표 유효성 검사
+                DFS(newI, newJ, visit_bit, cnt+1);// 새로 만든 좌표 전달
+            }
+        }
     }
 }
