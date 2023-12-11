4058bd2d6e5e2afdfbf7dfc4b1ff5a6d3d934482
YongJu Kim <kyjdummy@gmail.com>
2023-12-12 00:05:23 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 520 ms, Memory: 81812 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 class Main{
     
-    static int Y, X, result = -1;
+    static int Y, X;
     static int dx[] = {0,0,1,-1};
     static int dy[] = {-1,1,0,0};
     static char arr[][];
@@ -26,33 +26,34 @@ class Main{
         
         ArrayDeque<Node> q = new ArrayDeque<>();
         q.add(new Node(0,0,1,0));
-        
-        Loop:
+        visit[0][0][0] = true;
+
         while(!q.isEmpty()){
             Node now = q.poll();
-            
-            if(visit[now.usebreak][now.y][now.x])continue; 
-            visit[now.usebreak][now.y][now.x] = true;
-
-            
+                        
             if(now.y == Y-1 && now.x == X-1){			// 종료 지점 도달시 종료
-                result = now.dist;
-                break Loop;
+                System.out.println(now.dist); return;
             }
+            
             for(int i=0; i<4; i++){
                 int y = now.y + dy[i];
                 int x = now.x + dx[i];
-                int dist = now.dist + 1;
-                if(position_validate(y,x)){				// 새로 만든 좌표의 유효성 검증
-                    if(arr[y][x]=='1' && now.usebreak==0){ // 새로 만든 좌표가 벽이고 해당 노드가 벽을 깨부순적이 없고, 벽을 부순 노드가 지나간 적이 없으면
-                        q.add(new Node(y,x,dist,1));
-                    }else if(arr[y][x]=='0'){			// 새로 만든 좌표가 갈 수 있으면 그대로 진행
-                        q.add(new Node(y,x,dist,now.usebreak));
+
+                if(position_validate(y,x) && !(arr[y][x]=='1' && now.usebreak==1)){	// 새로 만든 좌표의 유효성 검증 및 새좌표가 벽인데 못부실때 스킵
+                	int usebreak = now.usebreak;
+                	
+                    if(arr[y][x]=='1'){// 새로 만든 좌표가 벽일때,
+                    	usebreak = 1;
                     }
+                    
+                    if(visit[usebreak][y][x]) continue;
+                    
+                    q.add(new Node(y,x,now.dist+1,usebreak));
+                    visit[usebreak][y][x] = true;
                 }
             }
         }
-        System.out.println( result );
+        System.out.println( -1 );
     }
     public static boolean position_validate(int y, int x) {
     	return y>=0 && x>=0 && y<Y && x<X;
