aee4e680e02c92d93df75b2b24dfe95360aea1ed
YongJu Kim <kyjdummy@gmail.com>
2023-12-11 15:10:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 336 ms, Memory: 112544 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -8,7 +8,6 @@ class Main{
 }
 class Solution{
  
-	int N, startY, startX, endY, endX;
 	int dy[] = {-1,-1,-2,-2,1,1,2,2};
 	int dx[] = {-2,2,-1,1,-2,2,-1,1};
 	StringBuilder sb = new StringBuilder();
@@ -23,7 +22,7 @@ class Solution{
 		return negative?~n+1:n;
 	}
 	
-	void BFS() {
+	void BFS(int N,int startY, int startX, int endY, int endX) {
 		ArrayDeque<Node> q = new ArrayDeque<>();
 		boolean visit[][] = new boolean[N][N];
 		q.add(new Node(startY,startX,0));
@@ -49,12 +48,12 @@ class Solution{
 	void solution() throws Exception{
 		int T 				= read();
 		while(T-->0) {
-			N 				= read();
-			startY 			= read(); 	// 시작점 입력
-			startX 			= read(); 	// 시작점 입력
-			endY 			= read();	// 도착점 입력
-			endX 			= read();	// 도착점 입력
-			BFS();
+			int N 			= read();
+			int startY 		= read(); 	// 시작점 입력
+			int startX 		= read(); 	// 시작점 입력
+			int endY 		= read();	// 도착점 입력
+			int endX 		= read();	// 도착점 입력
+			BFS(N, startY, startX, endY, endX);
 		}
 		System.out.println(sb);
 	}	
