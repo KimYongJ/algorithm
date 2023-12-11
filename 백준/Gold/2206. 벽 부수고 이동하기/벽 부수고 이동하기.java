583b9de72ec1364b39d3b3644a441af13af09ff1
YongJu Kim <kyjdummy@gmail.com>
2023-12-12 00:07:50 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 408 ms, Memory: 78288 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -3,26 +3,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 
 class Main{
     
     static int Y, X;
     static int dx[] = {0,0,1,-1};
     static int dy[] = {-1,1,0,0};
-    static char arr[][];
+    static int arr[][];
     static boolean visit[][][];
     
     public static void main(String[] args)throws Exception{
     	BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st 	= new StringTokenizer(br.readLine());
-        Y                   = Integer.parseInt(st.nextToken());
-        X                   = Integer.parseInt(st.nextToken());
-        arr                 = new char[Y][X];
+        Y                   = read();
+        X                   = read();
+        arr                 = new int[Y][X];
         visit               = new boolean[2][Y][X];
         
-        for(int y=0; y<Y; y++) 
-        	arr[y] 			= br.readLine().toCharArray();
+        for(int y=0; y<Y; y++) {
+        	for(int x=0; x<X; x++) {
+        		arr[y][x] = System.in.read() & 15;
+        	}
+        	System.in.read();
+        }
+        	
         
         ArrayDeque<Node> q = new ArrayDeque<>();
         q.add(new Node(0,0,1,0));
@@ -39,10 +42,10 @@ class Main{
                 int y = now.y + dy[i];
                 int x = now.x + dx[i];
 
-                if(position_validate(y,x) && !(arr[y][x]=='1' && now.usebreak==1)){	// 새로 만든 좌표의 유효성 검증 및 새좌표가 벽인데 못부실때 스킵
+                if(position_validate(y,x) && !(arr[y][x]==1 && now.usebreak==1)){	// 새로 만든 좌표의 유효성 검증 및 새좌표가 벽인데 못부실때 스킵
                 	int usebreak = now.usebreak;
                 	
-                    if(arr[y][x]=='1'){// 새로 만든 좌표가 벽일때,
+                    if(arr[y][x]==1){// 새로 만든 좌표가 벽일때,
                     	usebreak = 1;
                     }
                     
@@ -58,6 +61,11 @@ class Main{
     public static boolean position_validate(int y, int x) {
     	return y>=0 && x>=0 && y<Y && x<X;
     }
+    static int read() throws Exception{
+		int c, n = System.in.read() & 15;
+		while((c = System.in.read()) > 32) n = (n<<3) + (n<<1) + (c & 15);
+		return n;
+	}
 }
 class Node{
     int y, x, dist, usebreak; // 벽을 부심 1, 부신적 없으면 0
