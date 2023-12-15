e4d8ea19839e3c5d931df40965f88ce281f8378e
YongJu Kim <kyjdummy@gmail.com>
2023-12-16 01:05:17 +0900

    [Gold IV] Title: 네트워크 연결, Time: 236 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,7 +1,5 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 import java.util.PriorityQueue;
 
 class Node{
@@ -19,29 +17,33 @@ class Main{
 	static int a, b, d;
 	static int parent[];
 	
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
+
 	public static void changeParent(int a, int b, int dist) {
-		parent[a] = b;									// 부모 노드를 변경
-		result += dist;									// 결과 추가
+		parent[a] 		= b;							// 부모 노드를 변경
+		result 			+= dist;						// 결과 추가
 	}
     public static int getParent(int x, int[] parent){	// 부모 노드를 찾는 함수
         if(parent[x] == x) return x; 					// 자기 자신이 부모노드이면 반환 후 종료
         return getParent(parent[x], parent); 			// 자기 자신이 부모노드가 아니면 부모님 찾아 재귀 실행
     }
     public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        parent = new int[N+1]; // 부모 노드를 담을 배열 
+        N 				= read();
+        M 				= read();
+        parent 			= new int[N+1]; // 부모 노드를 담을 배열 
 
         for(int i=1; i<=N; i++)
-            parent[i] = i; // 부모 노드를 자기 자신으로 초기화
+            parent[i] 	= i; // 부모 노드를 자기 자신으로 초기화
         
         for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            d = Integer.parseInt(st.nextToken());
+            a 			= read();
+            b 			= read();
+            d 			= read();
             pq.add(new Node(a,b,d));
         }
         while(cnt < N){
