0330710beb204067fe17f2783b5d3606de3be82b
YongJu Kim <kyjdummy@gmail.com>
2023-12-17 14:47:40 +0900

    [Platinum V] Title: KCM Travel, Time: 1324 ms, Memory: 51664 KB -BaekjoonHub

--- 백준/Platinum/10217. KCM Travel/KCM Travel.java
+++ 백준/Platinum/10217. KCM Travel/KCM Travel.java
@@ -1,61 +1,65 @@
-import java.util.*;
-import java.io.*;
-class Node 
-{
-	int v;//정점
-	int c;//가격
-	int d;//시간
-	Node(int v,int c,int d)
-	{
-		this.v=v;
-		this.c=c;
-		this.d=d;
+//https://github.com/KimYongJ/algorithm
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Node{
+	int node, cost, time;
+	Node(int node, int cost, int time){
+		this.node = node;
+		this.cost = cost;
+		this.time = time;
 	}
 }
-public class Main {
 
-	public static int INF=100001;
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int T=Integer.parseInt(br.readLine());
-		String[] s=br.readLine().split(" ");
-		int N=Integer.parseInt(s[0]);
-		int M=Integer.parseInt(s[1]);
-		int K=Integer.parseInt(s[2]);
-		ArrayList<Node>[] arr=new ArrayList[N+1];
-		for(int i=1;i<=N;i++)
-		{
-			arr[i]=new ArrayList<Node>();
-		}
-		for(int i=0;i<K;i++)
-		{
-			s=br.readLine().split(" ");
-			int u=Integer.parseInt(s[0]);
-			int v=Integer.parseInt(s[1]);
-			int c=Integer.parseInt(s[2]);//비용
-			int d=Integer.parseInt(s[3]);//시간
-			arr[u].add(new Node(v,c,d));//양방향이 아니다
-		}
-		int[][] time=new int[N+1][M+1];
-		for(int i=0;i<=N;i++)//
-		{
+class Main{
+	static final int INF = 100_000;
+	static int N, M, K;
+	static int a, b, c, t;
+	static int time[][];
+	static ArrayList<Node> edge[];
+	
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+	public static void main(String[] args)throws Exception{
+		read();
+		N = read(); // 노드 갯수
+		M = read(); // 총 지원 비용
+		K = read(); // 티켓 정보 수
+		edge = new ArrayList[N+1];
+		time = new int[N+1][M+1]; //최단 시간을 저장하며 노드번호와 드는 비용당 최단 시간을 저장
+		
+		for(int i=0; i<=N; i++) {
+			edge[i] = new ArrayList<>();
 			Arrays.fill(time[i], INF);
 		}
-		time[1][0]=0;//시작점은 초기화
-		for(int i=0;i<=M;i++)
-		{
-			for(int j=1;j<=N;j++)
-			{
-				for(Node temp : arr[j])
-				{
-					if(i+temp.c<=M)
-					{
-						time[temp.v][i+temp.c]=Math.min(time[temp.v][i+temp.c], time[j][i]+temp.d);
+		
+		for(int i=0; i<K; i++) {
+			a = read(); // 시작 노드
+			b = read(); // 종료 노드
+			c = read(); // 드는 비용
+			t = read(); // 소요 시간
+			edge[a].add(new Node(b,c,t)); // 단방향 연결
+		}
+		time[1][0] = 0; //시작점 초기화
+		for(int m=0; m<=M; m++) {
+			for(int n=1; n<=N; n++) {
+				for(Node now : edge[n]) {
+					if(m + now.cost <= M) {
+						time[now.node][m+now.cost] = 
+								Math.min(
+										time[now.node][m+now.cost],
+										time[n][m] + now.time
+										);
 					}
 				}
 			}
 		}
+		
 		int result=INF;
 		for(int i=0;i<=M;i++)
 		{
