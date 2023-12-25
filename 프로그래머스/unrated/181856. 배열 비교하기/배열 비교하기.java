cae554a513f435b67ca09abc1a55b9c109bef633
YongJu Kim <kyjdummy@gmail.com>
2023-12-25 15:49:09 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.java
@@ -1,23 +1,11 @@
+import java.util.stream;
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        int ans = 0;
-        if(arr1.length > arr2.length){
-            ans = 1;
-        }else if(arr1.length < arr2.length){
-            ans = -1;
-        }else{
-            int sum = 0;
-            for(int i=0; i<arr1.length; i++){
-                sum += arr1[i];
-                sum -= arr2[i];
-            }
-            if(sum>0){
-                ans = 1;
-            }else if(sum < 0){
-                ans = -1;
-            }
-        }
+        int answer = Integer.compare(arr1.length, arr2.length);
         
-        return ans;
+        if(answer == 0){
+            answer = Integer.compare(arr1.stream().sum() , arr2.stream().sum());
+        }
+        return answer;
     }
 }
