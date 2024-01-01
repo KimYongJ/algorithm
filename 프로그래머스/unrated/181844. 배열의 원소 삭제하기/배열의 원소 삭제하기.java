986c44a67f18b17a4b0b3cc26348bf59976ec90f
YongJu Kim <kyjdummy@gmail.com>
2024-01-01 13:31:03 +0900

    [level unrated] Title: 배열의 원소 삭제하기, Time: 0.06 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,16 +1,13 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 class Solution {
     public ArrayList<Integer> solution(int[] arr, int[] delete_list) {
         ArrayList<Integer> result = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        for(int d : delete_list)
-            set.add(d);
         
-        for(int a : arr){
-            if(!set.contains(a))
-                result.add(a);
-        }
+        for(int a : arr)
+            result.add(a);
+        
+        for(int d : delete_list)
+            result.remove((Integer)d);
         
         return result;
     }
