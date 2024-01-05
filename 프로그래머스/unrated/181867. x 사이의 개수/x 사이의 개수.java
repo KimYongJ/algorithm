1137793d48c82ef4347ab49204568efa6d2b96c9
YongJu Kim <kyjdummy@gmail.com>
2024-01-05 18:54:00 +0900

    [level unrated] Title: x 사이의 개수, Time: 2.97 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -2,12 +2,11 @@ import java.util.ArrayList;
 class Solution {
     public ArrayList<Integer> solution(String myString) {
         ArrayList<Integer> result = new ArrayList<>();
-        String[] token = myString.split("x");
-        
-        for(String t : token)
+
+        for(String t : myString.split("x"))
             result.add( t.length() );
         
-        if(myString.charAt(myString.length()-1) == 'x' )
+        if(myString.endsWith("x"))
             result.add(0);
         
         return result;
