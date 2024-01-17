347de1ed371f0f1bf0f3a031b18a2bcfe64ea6e1
YongJu Kim <kyjdummy@gmail.com>
2024-01-17 21:24:52 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.11 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int[] q : queries)
+            for(int i=q[0]; i<=q[1]; i++)
+                arr[i]++;
+        return arr;
+    }
+}
