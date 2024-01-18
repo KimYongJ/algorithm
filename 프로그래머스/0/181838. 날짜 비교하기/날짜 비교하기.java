fa0954bf2ffb14d6560244c5b32b6212c0896110
YongJu Kim <kyjdummy@gmail.com>
2024-01-18 22:15:07 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.73 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,8 +1,8 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        LocalDateTime t1 = LocalDateTime.of(date1[0], date1[1], date1[2],0,0,0,0);
-        LocalDateTime t2 = LocalDateTime.of(date2[0], date2[1], date2[2],0,0,0,0);
+        LocalDate t1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate t2 = LocalDate.of(date2[0], date2[1], date2[2]);
         return t1.isBefore(t2) ? 1 : 0;
     }
 }
