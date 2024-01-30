05be9f205285f14e4c8e241afcd3dcb4938ccda8
YongJu Kim <kyjdummy@gmail.com>
2024-01-31 00:27:03 +0900

    [level 0] Title: 문자열 묶기, Time: 8.37 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,16 +1,32 @@
-import java.util.HashMap;
+
 class Solution {
     public int solution(String[] strArr) {
-        HashMap<Integer, Integer> m = new HashMap<>();
-        int answer = 0;
+        int result      = 0
+            , len       = 0
+            , count[]   = new int[31];
         
         for(String str : strArr)
         {
-            int len = str.length();
-            int cnt = m.getOrDefault(len,0)+1;
-            m.put(len, cnt);
-            answer = Math.max(answer, cnt);
+            len = str.length();
+            count[len]++;
+            result = Math.max(result, count[len]);
         }
-        return answer;
+        return result;
     }
 }
+// import java.util.HashMap;
+// class Solution {
+//     public int solution(String[] strArr) {
+//         HashMap<Integer, Integer> m = new HashMap<>();
+//         int answer = 0;
+        
+//         for(String str : strArr)
+//         {
+//             int len = str.length();
+//             int cnt = m.getOrDefault(len,0)+1;
+//             m.put(len, cnt);
+//             answer = Math.max(answer, cnt);
+//         }
+//         return answer;
+//     }
+// }
