c6372451482dca32c62124790f126b2dd2dfedff
YongJu Kim <kyjdummy@gmail.com>
2024-02-06 17:07:36 +0900

    [Gold IV] Title: 치즈, Time: 144 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,8 +1,4 @@
-import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -11,24 +7,31 @@ class Main{
 	static int[] dx = {1,0,-1,0};
 	static int[] dy = {0,1,0,-1};
 	static boolean visit[][];
-	static ArrayDeque<Point> q = new ArrayDeque<>();
+	static ArrayDeque<int[]> q = new ArrayDeque<>();
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13) System.in.read();
+        return n;
+    }
+    // BFS를 돌 때 유효성 검사
 	public static boolean validate(int x, int y) {
 		return x>=0 && y>=0 && x<X && y<Y && !visit[y][x];
 	}
 	public static void BFS() {
-		q = new ArrayDeque<Point>() {{add(new Point(0,0));}};
+		q = new ArrayDeque<int[]>() {{add(new int[] {0,0});}};
 		visit = new boolean[Y][X];
 		cnt = 0;
-		
 		while(!q.isEmpty()) {
-			Point now = q.poll();
+			int[] now = q.poll();
 			for(int i=0; i<4; i++) {
-				nextX = now.x + dx[i];
-				nextY = now.y + dy[i];
+				nextX = now[0] + dx[i];
+				nextY = now[1] + dy[i];
 				if(validate(nextX, nextY)) {
-					visit[nextY][nextX] = true;
+					visit[nextY][nextX] = true; // 방문처리
 					if(arr[nextY][nextX] == 0) {
-						q.add(new Point(nextX,nextY)); // 0인 경우만 큐에 넣는다.
+						q.add(new int[] {nextX,nextY}); // 0인 경우만 큐에 넣는다.
 					}else if(arr[nextY][nextX] == 1) {
 						arr[nextY][nextX] = 0;// 1인 경우 0으로 바꾸고 
 						cnt ++; // 1의 갯수 카운팅
@@ -40,27 +43,24 @@ class Main{
 		cnt_one = cnt; // 이전 1의 갯수를 구하기 위한 코드 
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Y = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
+		Y 	= read();
+		X 	= read();
 		arr = new int[Y][X];
 		
-		for(int i=0; i<Y; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i=0; i<Y; i++) 
 			for(int j=0; j<X; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if(arr[i][j] == 1)
-					cnt_one++;
+				arr[i][j] = read();
+				if(arr[i][j] == 1) cnt_one++;
 			}
-		}
-		
+
 		while(cnt_one != 0) {
-			result1++;
-			result2 = cnt_one;
+			result1++; // 총 카운팅 갯수
+			result2 = cnt_one; // 종료직전의 1의 갯수
 			BFS();
 		}
-		System.out.println(result1-1);
-		System.out.println(result2);
+		StringBuilder sb = new StringBuilder();
+		sb.append(result1-1).append('\n').append(result2);
+		System.out.print(sb);
+
 	}
 }
