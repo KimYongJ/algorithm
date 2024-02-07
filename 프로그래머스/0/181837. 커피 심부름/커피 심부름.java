fca22be4f8f550d3cb481d281154cf10b9b26710
YongJu Kim <kyjdummy@gmail.com>
2024-02-07 20:58:50 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,13 +1,8 @@
-import java.util.HashSet;
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        HashSet<String> cf = new HashSet<String>(){{
-            add("icecafelatte");add("cafelatteice");
-            add("hotcafelatte");add("cafelattehot");add("cafelatte");
-        }};
         for(String str : order)
-            answer += cf.contains(str) ? 5000 : 4500;
+            answer += str.contains("cafelatte") ? 5000 : 4500;
         return answer;
     }
 }
