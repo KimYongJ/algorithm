b926237df651d7c7bdc16217ecc595ec54b1938b
YongJu Kim <kyjdummy@gmail.com>
2024-02-10 01:23:22 +0900

    [Gold IV] Title: 인구 이동, Time: 736 ms, Memory: 297932 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,9 +1,6 @@
-// https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+//https://github.com/KimYongJ/algorithm
 import java.util.ArrayDeque;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 class Point{
 	int i, j;
@@ -20,39 +17,56 @@ class Main{
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
 	static boolean change, visit[][];
 	static HashMap<Integer, Integer> flag_map;
-	
-	public static void changeArr() {
+	static ArrayDeque<Point> q;
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception 
+    {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+	public static void changeArr() 
+	{
 		for(int i=1; i<=N; i++)
-			for(int j=1; j<=N; j++) {
+			for(int j=1; j<=N; j++) 
+			{
 				int key = flag_arr[i][j];
-				arr[i][j] = flag_map.getOrDefault(key,arr[i][j]);
+				if(key != 0)
+					arr[i][j] = flag_map.getOrDefault(key,arr[i][j]);
 			}
 		
 	}
-	public static void BFS(int i, int j) {
+	public static void BFS(int i, int j) 
+	{
 		boolean isVisit = false;
-		population = 0;
-		areacnt = 0;
-		flag += 1;
-		ArrayDeque<Point> q = new ArrayDeque<>();
+		population 		= 0;
+		areacnt 		= 0;
+		flag 			+= 1;
+		q 				= new ArrayDeque<>();
+		
 		q.add(new Point(i,j));
 		
-		while(!q.isEmpty()) {
+		while(!q.isEmpty()) 
+		{
 			Point now = q.poll();
 			
 			if(visit[now.i][now.j]) continue; // 방문했던 곳은 연산 스킵
-			visit[now.i][now.j] = true;
-			flag_arr[now.i][now.j] = flag; 
-			population += arr[now.i][now.j];
+			visit[now.i][now.j] 	= true;
+			flag_arr[now.i][now.j] 	= flag; 
+			population 				+= arr[now.i][now.j];
 			areacnt++;
 			
-			for(int xy[] : dxy) {
+			for(int xy[] : dxy) 
+			{
 				int nextI = now.i + xy[0];
 				int nextJ = now.j + xy[1];
 				
-				if(!visit[nextI][nextJ]) {
+				if(!visit[nextI][nextJ]) 
+				{
 					int diff = Math.abs(arr[now.i][now.j] - arr[nextI][nextJ]);
-					if(L<=diff && diff<=R) {
+					if(L<=diff && diff<=R) 
+					{
 						isVisit = true;
 						q.add(new Point(nextI, nextJ));
 					}
@@ -63,54 +77,49 @@ class Main{
 		}
 		
 		
-		if(isVisit) { // 국경을 합친 지역이 하나라도 있다면
-			change = true; // 연산 계속
-			int value = population / areacnt;// 덮어써야할 인원
-			flag_map.put(flag, value);
+		if(isVisit) { 							// 국경을 합친 지역이 하나라도 있다면
+			change = true; 						// 국경을 합친적이 한번이라도 있다는 뜻
+			int value = population / areacnt;	// 덮어써야할 인원
+			flag_map.put(flag, value);			// 키 값으로 덮어써야할 인원을 저장해 놓는다.
 		}
 	}
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		
+		N 	= read();
+		L 	= read();
+		R 	= read();
 		arr = new int[N+2][N+2];
-		
-		for(int i=0; i<=N+1; i++) {
+		// 겉에 패딩 넣기
+		for(int i=0; i<=N+1; i++) 
 			arr[0][i] = arr[N+1][i] = 
 			arr[i][0] = arr[i][N+1] = BLOCK;
-		}
-		
-		for(int i=1; i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
+		// 갑 잆력 받기
+		for(int i=1; i<=N; i++)
+			for(int j=1; j<=N; j++)
+				arr[i][j] = read();
+			
 		change = true;
-		while(change) {
-			change = false;
-			visit = new boolean[N+2][N+2];
-			flag = 0;
-			flag_map = new HashMap<>();
-			flag_arr = new int[N+2][N+2];
-			for(int i=1; i<=N; i++) {
-				for(int j=1; j<=N; j++) {
+		
+		while(change) 
+		{
+			flag 		= 0;
+			change 		= false;
+			flag_map 	= new HashMap<>();
+			flag_arr 	= new int[N+2][N+2];
+			visit 		= new boolean[N+2][N+2];
+			
+			for(int i=1; i<=N; i++)
+				for(int j=1; j<=N; j++)
 					if(!visit[i][j])
 						BFS(i,j);
-				}
-			}
-			if(change) {
+				
+			
+			if(change) 
+			{
 				cnt++;
 				changeArr();
 			}
 		}
-		
 		System.out.println(cnt);
 	}
 }
