950980f45bbb5f1321a731b94e4a84950410ab36
YongJu Kim <kyjdummy@gmail.com>
2024-02-09 16:23:46 +0900

    [level 0] Title: qr code, Time: 0.10 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(int q, int r, String code) {
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<code.length(); i++)
-            if(i%q == r)sb.append(code.charAt(i));
+        for(int i=r; i<code.length(); i+=q)
+            sb.append(code.charAt(i));
         return sb.toString();
     }
 }
