bee8c3adb41f76b58668133df291121fc248c111
YongJu Kim <kyjdummy@gmail.com>
2024-02-11 20:08:39 +0900

    [Gold V] Title: 트리, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,50 @@
+// https://github.com/KimYongJ/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int N, root, del, leaf;
+	static ArrayList<Integer>[] list;
+	public static void main(String[] args)throws Exception
+	{
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st 	= new StringTokenizer(br.readLine());
+		N 					= Integer.parseInt(st.nextToken());
+		list 				= new ArrayList[N];
+		st 					= new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++)
+			list[i] = new ArrayList<>(); // 자식 노드 정보를 담을 리스트 초기화  
+		
+		del = Integer.parseInt( new StringTokenizer(br.readLine()).nextToken()); // 없앨 노드를 입력받음
+		
+		for(int i=0; i<N; i++) 
+		{
+			int node = Integer.parseInt(st.nextToken());
+			if(node == -1) root = i;
+			else if(node !=del && i != del) 
+			{
+				if(node != -1 && i != del) list[node].add(i);
+			}
+		}
+		
+		if(root != del) 
+			DFS(root); // 루트노드가 삭제되지 않은 경우만 DFS로 leaf노드를 센다
+		
+		System.out.println(leaf);
+
+	}
+	public static void DFS(int node) 
+	{
+		if(list[node].size() == 0)  // 리프노드일 때는 자식노드가 없어서 size가 0이다.
+		{ 
+			leaf++; // 리프노드면 +1
+			return;
+		}
+		for(Integer l : list[node])
+			DFS(l);
+	}
+}
