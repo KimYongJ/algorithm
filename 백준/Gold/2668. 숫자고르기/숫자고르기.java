14e14f916a7866fedb55bbdcc7588e671906d904
YongJu Kim <kyjdummy@gmail.com>
2024-02-13 00:39:21 +0900

    [Gold V] Title: 숫자고르기, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -7,41 +7,40 @@ import java.util.TreeSet;
 
 class Main{
 	
-	static int N, arr[][];
+	static int N, arr[];
 	static StringBuilder sb;
 	static boolean visit[];
 	static Set<Integer> set, sub_set;
 	public static void DFS(int start, int idx) {
-		if(visit[idx]) {
-			if(arr[0][idx] != start)
+		if(visit[idx] || set.contains(idx)) {
+			if(idx != start)
 				sub_set.clear();
 			return;
 		}
 		visit[idx] = true;
 		sub_set.add(idx);
-		
-		
-		DFS(start, arr[1][idx]);
-		
+
+		DFS(start, arr[idx]);
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N 			= Integer.parseInt(br.readLine())+1;
-		arr 		= new int[2][N];
+		arr 		= new int[N];
 		sb			= new StringBuilder();
 		set			= new TreeSet<>();
 		sub_set 	= new TreeSet<>();
 		for(int i=1; i<N; i++) {
-			arr[0][i] = i;
-			arr[1][i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
 		for(int i=1; i<N; i++) {
 			visit = new boolean[N];
 			DFS(i,i); // 방문하지 않은 경우만 반복
-			set.addAll(sub_set);
-			sub_set.clear();
+			if(sub_set.size() != 0) {
+				set.addAll(sub_set);
+				sub_set.clear();
+			}
 		}
 		
 		sb.append(set.size()).append('\n');
