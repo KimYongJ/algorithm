0394ed2924f12e2ad4ea310af2dab70494f2d1f4
YongJu Kim <kyjdummy@gmail.com>
2024-02-16 00:40:01 +0900

    [Gold IV] Title: 이모티콘, Time: 140 ms, Memory: 15528 KB -BaekjoonHub

--- 백준/Gold IV/14226. 이모티콘/이모티콘.java
+++ 백준/Gold IV/14226. 이모티콘/이모티콘.java
@@ -1,13 +1,11 @@
 // github.com/KimYongJ/algorithm
 
-// 특징 : 어떤 점에 최적보다 더 늦게 도착해도 클립보드에 원소가 더 많으면 그게 나중에 답이될 수도 있어서 2차원 배열로 해야한다.
-
 import java.util.ArrayDeque;
-import java.util.Arrays;
 
 class Main{
 	static final int MAX = 100_000_000;
-	static int goal, MIN, time, LENG, emtOfTime[][];
+	static int goal, MIN, time;
+	static boolean visit[][];
 	
 	// 빠른 입력을 위한 함수
     static int read() throws Exception 
@@ -20,57 +18,56 @@ class Main{
 	public static void BFS() 
 	{
 		ArrayDeque<Point> q = new ArrayDeque<Point>() {{add(new Point(1,0,0));}};
-
+		visit[1][0] = true;
 		int nEmt, nTime;
 		while(!q.isEmpty()) 
 		{
-			Point now = q.poll(); // 큐에서 하나를 꺼낸다.
-			nTime = now.time + 1;
-			
-			if(now.emt == goal) 
-			{   // 종료조건
-				MIN = Math.min(MIN, emtOfTime[now.emt][now.clip]);
-				continue;
-			}
-			
-
-			// 클립보드에 복사
-			if(emtOfTime[now.emt][now.emt] > nTime) 
-			{
-				emtOfTime[now.emt][now.emt] = nTime;
-				q.add(new Point(now.emt, nTime, now.emt));
-			}
-			
-			// 클립보드있는 것을 화면에 붙여 넣기
-			if(now.clip > 0) 
-			{
-				nEmt = now.emt + now.clip;
-				if(0<nEmt && nEmt <LENG && emtOfTime[nEmt][now.clip] > nTime) 
+			int size = q.size();
+			for(int i=0; i<size; i++) {
+				Point now = q.poll(); // 큐에서 하나를 꺼낸다.
+				if(now.emt == goal) 
+				{   // 종료조건
+					MIN = now.time;
+					return;
+				}
+				
+				nTime = now.time + 1;
+	
+				// 클립보드에 복사
+				if(!visit[now.emt][now.emt]) 
 				{
-					emtOfTime[nEmt][now.clip] = nTime;
+					visit[now.emt][now.emt] = true;
+					q.add(new Point(now.emt, nTime, now.emt));
+				}
+				
+				// 클립보드있는 것을 화면에 붙여 넣기
+				if(now.clip > 0) 
+				{
+					nEmt = now.emt + now.clip;
+					if(1<nEmt && nEmt <1001 && !visit[nEmt][now.clip]) 
+					{
+						visit[nEmt][now.clip] = true;
+						q.add(new Point(nEmt, nTime, now.clip));
+					}
+				}
+				
+				// 이모티콘 삭제 
+				nEmt = now.emt-1;
+				if(1<nEmt && nEmt <1001 && !visit[nEmt][now.clip]) 
+				{
+					visit[nEmt][now.clip] = true;
 					q.add(new Point(nEmt, nTime, now.clip));
 				}
 			}
-			
-			// 이모티콘 삭제 
-			nEmt = now.emt-1;
-			if(0<nEmt && nEmt <LENG && emtOfTime[nEmt][now.clip] > nTime) 
-			{
-				emtOfTime[nEmt][now.clip] = nTime;
-				q.add(new Point(nEmt, nTime, now.clip));
-			}
-			
 		}
+			
+		
 	}
 	public static void main(String[] args)throws Exception
 	{
 		goal 		= read();
 		MIN 		= Integer.MAX_VALUE;
-		LENG		= (int) (goal*1.5);
-		emtOfTime 	= new int[LENG][LENG]; // 이모티콘 갯수당 걸린 시간
-		
-		for(int i=0; i<LENG; i++)
-			Arrays.fill(emtOfTime[i], MAX);
+		visit 		= new boolean[1001][1001]; // 이모티콘 갯수당 걸린 시간
 		
 		BFS();
 		
