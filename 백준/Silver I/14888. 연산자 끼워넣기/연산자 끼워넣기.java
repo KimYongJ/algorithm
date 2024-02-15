e40b26e4af91a018ddf7f3c9ef740f5a587810e2
YongJu Kim <kyjdummy@gmail.com>
2024-02-15 18:01:23 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 15556 KB -BaekjoonHub

--- 백준/Silver I/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver I/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,7 +1,4 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -10,38 +7,19 @@ class Main{
 	static int N, N_1, arr[];
 	static boolean visit[];
 
-	public static int calculator(int flag, int a, int b) {
+	// 빠른 입력을 위한 함수
+	static int read() throws Exception 
+	{
+	        int c, n = System.in.read() & 15;
+	        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+	        return n;
+	}
+	public static int calculator(int flag, int a, int b) 
+	{
 		if(flag == 0)		return a+b;
 		else if(flag == 1)	return a-b;
 		else if(flag == 2)	return a*b;
 		else				return a/b;
-	}
-
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N 			= Integer.parseInt(br.readLine());
-		N_1 		= N-1;
-		arr 		= new int[N];  		  		// 숫자들을 받을 배열
-		visit 		= new boolean[N_1]; 		// 연산자들에 대해 DFS진행시 방문 체크할 배열
-		MAX 		= Integer.MIN_VALUE;
-		MIN 		= Integer.MAX_VALUE;
-		st 			= new StringTokenizer(br.readLine());
-		
-		for(int i=0; i<N; i++)
-			arr[i] 	= Integer.parseInt(st.nextToken());
-		
-		st 			= new StringTokenizer(br.readLine());
-
-		maxplus 	= Integer.parseInt(st.nextToken());
-		maxminus 	= Integer.parseInt(st.nextToken());
-		maxmul 		= Integer.parseInt(st.nextToken());
-		maxdiv 		= Integer.parseInt(st.nextToken());
-	
-		DFS(0,0,0,0,arr[0],0);
-		
-		System.out.println(MAX);
-		System.out.println(MIN);
 	}
 	public static void DFS(int plus, int minus, int mul,int div, int sum, int depth) {
 		if(depth == N_1) {
@@ -72,12 +50,26 @@ class Main{
 		}
 	}
 
-	// 빠른 입력을 위한 함수
-//	static int read() throws Exception 
-//	{
-//	        int c, n = System.in.read() & 15;
-//	        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-//	        return n;
-//	} 
-
+	public static void main(String[] args)throws Exception
+	{
+		N 			= read();
+		N_1 		= N-1;
+		arr 		= new int[N];  		  		// 숫자들을 받을 배열
+		visit 		= new boolean[N_1]; 		// 연산자들에 대해 DFS진행시 방문 체크할 배열
+		MAX 		= Integer.MIN_VALUE;
+		MIN 		= Integer.MAX_VALUE;
+		
+		for(int i=0; i<N; i++)
+			arr[i] 	= read();
+		
+		maxplus 	= read();
+		maxminus 	= read();
+		maxmul 		= read();
+		maxdiv 		= read();
+	
+		DFS(0,0,0,0,arr[0],0);
+		
+		System.out.println(MAX);
+		System.out.println(MIN);
+	}
 }
