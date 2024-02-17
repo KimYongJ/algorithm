c62c064bdf235578a8b06538cc18721651d8b1d5
YongJu Kim <kyjdummy@gmail.com>
2024-02-17 11:25:04 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for(int i=0; i<my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if('Z' >= c && c>='A')
+                answer[c-'A']++;
+            else
+                answer[c-'a'+26]++;
+        }
+        return answer;
+    }
+}
