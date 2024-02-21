686a5fd6016df6adb4062ddbf420b1a9d227080b
YongJu Kim <kyjdummy@gmail.com>
2024-02-21 20:42:56 +0900

    [Gold III] Title: 양 구출 작전, Time: 748 ms, Memory: 101524 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -3,20 +3,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 class Main{
 	
 	static int N, animalCnt[];
 	static boolean isSheep[];
-	static ArrayList<Integer>[] list;
+	static HashMap<Integer , ArrayList<Integer>> hm;
 	
-	public static long DFS( int node ) {
+	public static long DFS( int node )
+	{
 		long sum = 0;
 		
-		for(int nextNode : list[node]) 
-			sum += DFS(nextNode);
-		
+		if(hm.containsKey(node)) 
+		{
+			ArrayList<Integer> list = hm.get(node);
+			for(int nextNode : list) 
+				sum += DFS(nextNode);
+		}
 		return isSheep[node] 
 				? 
 				sum + animalCnt[node]
@@ -29,19 +34,19 @@ class Main{
 		N 			= Integer.parseInt(br.readLine());
 		isSheep 	= new boolean[N+1]; 	// 양인지 아닌지 체크
 		animalCnt 	= new int[N+1]; 		// 양과 늑대가 몇마리인지 체크
-		list 		= new ArrayList[N+1];	// 1노드 부터 아래 노드로 연결
-		
-		for(int i=1; i<=N; i++) 
-			list[i] = new ArrayList<>();
-		
+		hm 			= new HashMap<>();		// 1번 노드부터 다음노드까지 연결을 담을 map
+		int node;
 		for(int i=2; i<=N; i++) 
 		{
 			st = new StringTokenizer(br.readLine());
 			isSheep[i] = "S".equals(st.nextToken()); // 양이면 true
 			animalCnt[i] = Integer.parseInt(st.nextToken()); // 양과 늑대의 마리수를 입력
-			list[Integer.parseInt(st.nextToken())].add(i); // 입력되는 숫자가 인덱스로 갈 수 있게 연결( 즉 1이 나머지들로 갈 수 있도록 연결 )
+			
+			node = Integer.parseInt(st.nextToken());
+			ArrayList<Integer> list = hm.getOrDefault(node, new ArrayList<Integer>());
+			list.add(i);
+			hm.put(node, list);// 입력되는 숫자가 인덱스로 갈 수 있게 연결( 즉 1이 나머지들로 갈 수 있도록 연결 )
 		}
-		
 		System.out.println(DFS(1));
 	}
 }
