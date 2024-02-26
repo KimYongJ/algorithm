9f4a45a3776bfdbd0a9ed9501b8383e9cba2ae4c
YongJu Kim <kyjdummy@gmail.com>
2024-02-26 21:11:55 +0900

    [Gold II] Title: 경비행기, Time: 280 ms, Memory: 17440 KB -BaekjoonHub

--- 백준/Gold/2585. 경비행기/경비행기.java
+++ 백준/Gold/2585. 경비행기/경비행기.java
@@ -1,32 +1,27 @@
 // https://github.com/KimYongJ/algorithm
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
-class Node
-{
-	int idx, k;
-	Node(int idx, int k)
-	{
-		this.idx = idx;
-		this.k = k;
-	}
-}
+
+
 class Main
 {
 	
-	static int N, K, fvalue, endIdx, point[][];
-	static int mid;
+	static int N, K, mid, endIdx, point[][];
 	static boolean visit[];
 	static ArrayDeque<Node> q;
-	public static int getFuel(int idx1, int idx2) 
-	{
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception 
+    {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
+	public static int getFuel(int idx1, int idx2) {
 		return (int)(Math.pow(point[idx1][0]-point[idx2][0], 2) + Math.pow(point[idx1][1]-point[idx2][1], 2));
 	}
 	public static boolean BFS() {
-		visit	= new boolean[N];
-		q 		= new ArrayDeque<>();
+		visit		= new boolean[N];
+		q 			= new ArrayDeque<>();
+		visit[0] 	= true;
 		q.add(new Node(0,0));
 		
 		while(!q.isEmpty()) 
@@ -52,21 +47,21 @@ class Main
 		return false;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st 	= new StringTokenizer(br.readLine());
-		N 					= Integer.parseInt(st.nextToken())+2;
-		K 					= Integer.parseInt(st.nextToken());
+		N 					= read()+2;
+		K 					= read();
 		endIdx				= N-1;
 		point 				= new int[N][2];
 		point[0][0] 		= point[0][1] 		= 0; 		// 초기좌표 셋팅
-		point[endIdx][0] 	= point[endIdx][1] 	= 10000; 	// 마지막 좌표 셋팅
+		point[endIdx][0]	= point[endIdx][1] 	= 10000; 	// 마지막 좌표 셋팅
 		for(int i=1; i<endIdx; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			point[i][0] = Integer.parseInt(st.nextToken());
-			point[i][1] = Integer.parseInt(st.nextToken());
+			point[i][0] = read();
+			point[i][1] = read();
 		}
-		int left = 0, right = getFuel(0, endIdx);
+		
+		int left 	= 0, 
+			right 	= getFuel(0, endIdx);
+		
 		while(left < right) {
 			mid = (right+left) / 2;
 			if( BFS() )
@@ -74,9 +69,16 @@ class Main
 			else 	
 				left = mid+1;
 		}
-		
 		System.out.println((int)Math.ceil(Math.sqrt(left)/10.0));
 		
 	}
-	
+}
+class Node
+{
+	int idx, k;
+	Node(int idx, int k)
+	{
+		this.idx = idx;
+		this.k = k;
+	}
 }
