4806ddbfa26efbc47b1c01b50b5e8915168e6c17
YongJu Kim <kyjdummy@gmail.com>
2024-02-29 20:38:31 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 1204 ms, Memory: 174332 KB -BaekjoonHub

---
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -0,0 +1,81 @@
+// https://github.com/KimYongJ/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int Y, X, nextY, nextX, flag, map[][];
+	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+	static HashMap<Integer, Integer> hm;
+	static HashSet<Integer> set;
+	
+	// DFS를 돌면서 0인 구간의 갯수를 세고 반환한다.
+	public static int DFS(int y, int x) 
+	{
+		if(map[y][x] !=0 ) return 0;
+		map[y][x] = flag;
+		int cnt = 1;
+		
+		for(int xy[] : dxy) 
+		{
+			nextY = y + xy[0];
+			nextX = x + xy[1];
+			if(nextX>=0 && nextY>=0 && nextX<X && nextY<Y && map[nextY][nextX] == 0)
+				cnt += DFS(nextY, nextX);
+		}
+		
+		return cnt;
+	}
+	public static void main(String[] args)throws Exception
+	{
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st 	= new StringTokenizer(br.readLine());
+		StringBuilder sb 	= new StringBuilder();
+		flag 				= 1;
+		Y 					= Integer.parseInt(st.nextToken());
+		X 					= Integer.parseInt(st.nextToken());
+		map 				= new int[Y][X];
+		hm 					= new HashMap<>();
+		
+		for(int y=0; y<Y; y++) 
+		{
+			String str = br.readLine();
+			for(int x=0; x<X; x++)
+				map[y][x] = str.charAt(x) - '0';
+		}
+		
+		for(int y=0; y<Y; y++)
+			for(int x=0; x<X; x++)
+				if(map[y][x] == 0)
+					hm.put(++flag , DFS(y,x));
+
+		for(int y=0; y<Y; y++) 
+		{
+			for(int x=0; x<X; x++) 
+			{
+				int sum = 0;
+				if(map[y][x] == 1) 
+				{
+					set = new HashSet<Integer>();
+					sum = 1;
+					for(int xy[] : dxy) 
+					{
+						nextY = y + xy[0];
+						nextX = x + xy[1];
+						if(nextX>=0 && nextY>=0 && nextX<X && nextY<Y && map[nextY][nextX] >= 2)
+							set.add(map[nextY][nextX]);
+					}
+					for(int i : set)
+						sum += hm.get(i);
+				}
+				sb.append(sum%10);
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);		
+	}
+}
