40efad3b67ec040ba006d93f64f65ab5bbf7b31f
YongJu Kim <kyjdummy@gmail.com>
2024-03-01 19:56:32 +0900

    [Gold II] Title: 내 선물을 받아줘, Time: 344 ms, Memory: 28772 KB -BaekjoonHub

---
+++ 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.java
@@ -0,0 +1,54 @@
+// https://github.com/KimYongJ/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args)throws Exception{new Main().solution();}
+	
+	
+	char map[][];
+	int area[][];
+	int Y, X, flag, result;
+	ArrayList<HashSet<Integer>> list;
+	public void DFS(int flag, int y, int x) 
+	{
+		if(area[y][x] > 0) 
+		{
+			if(area[y][x] == flag) 		// 사이클이 발생한 공간이 자기 자신이 아닌 경우
+				result++;
+			return;
+		}
+		area[y][x] = flag;
+		switch(map[y][x]) 
+		{
+			case 'N': DFS(flag,y-1,x);break;
+			case 'S': DFS(flag,y+1,x);break;
+			case 'W': DFS(flag,y,x-1);break;
+			case 'E': DFS(flag,y,x+1);
+		}
+	}
+	public void solution()throws Exception
+	{
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st 	= new StringTokenizer(br.readLine());
+		Y 					= Integer.parseInt(st.nextToken());
+		X 					= Integer.parseInt(st.nextToken());
+		flag 				= 0;
+		area 				= new int[Y][X];
+		map 				= new char[Y][X];
+		list				= new ArrayList<>();
+		for(int y=0; y<Y; y++)
+			map[y] = br.readLine().toCharArray();
+		
+		for(int y=0; y<Y; y++)
+			for(int x=0; x<X; x++)
+				if(area[y][x] == 0) 
+					DFS(++flag,y,x);
+
+		System.out.println(result);
+	}
+}
