54de849f4bcb704b38d263683f94114cc5164530
YongJu Kim <kyjdummy@gmail.com>
2024-03-01 23:37:18 +0900

    [Gold II] Title: 물통, Time: 2020 ms, Memory: 223640 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14867. 물통/물통.java
+++ 백준/Gold/14867. 물통/물통.java
@@ -3,13 +3,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{new Main().solution();}
 	
 	int a,b,c,d;
-	boolean visit[][];
+	HashSet<String> visit;
 	ArrayDeque<Point> q;
 	public void solution()throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,13 +20,14 @@ class Main{
 		b = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 		d = Integer.parseInt(st.nextToken());
-		visit = new boolean[a+1][b+1];
+		visit = new HashSet<>();
 		q = new ArrayDeque<>();
 		
 		q.add(new Point(0,0,0));
-		visit[0][0] = true;
+		visit.add(0+" "+0);
 		
 		int nextA, nextB, nextCnt;
+		String key;
 		while(!q.isEmpty()) {
 			Point now = q.poll();
 			
@@ -37,25 +39,29 @@ class Main{
 			// 왼쪽에 가득 채운다.
 			nextA = a;
 			nextB = now.b;
-			if(!visit[nextA][nextB]) {
-				visit[nextA][nextB] = true;
+			key = nextA+" "+nextB;
+			if(!visit.contains(key)) {
+				visit.add(key);
 				q.add(new Point(nextA, nextB, nextCnt));
 			}
 			// 오른쪽에 가득 채운다. 
 			nextA = now.a;
 			nextB = b;
-			if(!visit[nextA][nextB]) {
-				visit[nextA][nextB] = true;
+			key = nextA+" "+nextB;
+			if(!visit.contains(key)) {
+				visit.add(key);
 				q.add(new Point(nextA, nextB, nextCnt));
 			}
 			// 왼쪽을 비운다.
-			if(!visit[0][now.b]) {
-				visit[0][now.b] = true;
+			key = "0 "+now.b;
+			if(!visit.contains(key)) {
+				visit.add(key);
 				q.add(new Point(0, now.b, nextCnt));
 			}
 			// 오른쪽을 비운다.
-			if(!visit[now.a][0]) {
-				visit[now.a][0] = true;
+			key = now.a+" 0";
+			if(!visit.contains(key)) {
+				visit.add(key);
 				q.add(new Point(now.a, 0, nextCnt));
 			}
 			
@@ -63,14 +69,16 @@ class Main{
 			if(now.b+now.a <= b) // 물을 전부 비울 수 있는 경우
 			{
 				nextB = now.b + now.a;
-				if(!visit[0][nextB]) {
-					visit[0][nextB] = true;
+				key = "0 "+nextB;
+				if(!visit.contains(key)) {
+					visit.add(key);
 					q.add(new Point(0, nextB, nextCnt));
 				}
 			}else {
 				nextA = now.a + now.b - b;
-				if(!visit[nextA][b]) {
-					visit[nextA][b] = true;
+				key = nextA+" "+b;
+				if(!visit.contains(key)) {
+					visit.add(key);
 					q.add(new Point(nextA, b, nextCnt));
 				}
 			}
@@ -78,14 +86,16 @@ class Main{
 			// 오른쪽물을 왼쪽으로 비운다. 
 			if(a >= now.a+now.b) { // 물을 전부 비울 수 있는 경우
 				nextA = now.a+now.b;
-				if(!visit[nextA][0]) {
-					visit[nextA][0] = true;
+				key = nextA+" 0";
+				if(!visit.contains(key)) {
+					visit.add(key);
 					q.add(new Point(nextA, 0, nextCnt));
 				}
 			}else {
 				nextB = now.a + now.b - a;
-				if(!visit[a][nextB]) {
-					visit[a][nextB] = true;
+				key = a+" "+nextB;
+				if(!visit.contains(key)) {
+					visit.add(key);
 					q.add(new Point(a,nextB, nextCnt));
 				}
 			}
