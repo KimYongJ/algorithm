14103d783a8e49af30e6ac1210417509c4985e50
YongJu Kim <kyjdummy@gmail.com>
2024-03-01 15:07:34 +0900

    [Gold II] Title: 환승, Time: 572 ms, Memory: 52732 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.java
+++ 백준/Gold/5214. 환승/환승.java
@@ -1,19 +1,24 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 
 class Main{
 	
 	static int N, K, M, map[][];
-	static boolean visit[];
+	static boolean visit[], visit_station[];
 	static ArrayDeque<Node> q;
 	static ArrayList<Integer>[] list;
 	
+    // 빠른 입력을 위해 만든 함수
+    public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void BFS() 
 	{
 		q.add(new Node(1,1));
@@ -32,30 +37,32 @@ class Main{
 			
 			nextDist = now.dist+1;
 			for(int index : list[now.node]) 
-			{
-				for(int i=0; i<K; i++) 
+				if(!visit_station[index])
 				{
-					nextNode = map[index][i];
-					
-					if(!visit[nextNode]) 
+					visit_station[index] = true;
+					for(int i=0; i<K; i++) 
 					{
-						visit[nextNode] = true;
-						q.add(new Node(nextNode, nextDist));
+						nextNode = map[index][i];
+						
+						if(!visit[nextNode]) 
+						{
+							visit[nextNode] = true;
+							q.add(new Node(nextNode, nextDist));
+						}
 					}
 				}
-			}
 		}
 		
 	}
 	public static void main(String[] args)throws Exception{
-		InputReader read = new InputReader();
-		N 		= read.in(); 			// 역의 수 
-		K 		= read.in(); 			// 하나의 튜브가 연결하는 최대 역의 수 
-		M 		= read.in(); 			// 하이퍼튜브의 개수
+		N 		= read(); 				// 역의 수 
+		K 		= read(); 				// 하나의 튜브가 연결하는 최대 역의 수 
+		M 		= read(); 				// 하이퍼튜브의 개수
 		map 	= new int[M][K];		// 연결을 담을 좌표
-		visit 	= new boolean[N+1];		// 방문한 노드를 체크할 배열
 		list 	= new ArrayList[N+1];	// 역의 번호마다 연결된 하이퍼튜브의 idx
 		q 		= new ArrayDeque<>();	// 현재 역의 정보와 현재 역까지 오는데 걸린 갯수
+		visit 	= new boolean[N+1];		// 방문한 노드를 체크할 배열
+		visit_station = new boolean[M]; // 방문한 역은 2번이상 방문할 필요가 없음
 		
 		for(int i=0; i<=N; i++)
 			list[i] = new ArrayList<>();
@@ -63,66 +70,12 @@ class Main{
 		for(int i=0; i<M; i++) 
 			for(int j=0; j<K; j++) 
 			{
-				map[i][j] = read.in();
+				map[i][j] = read();
 				list[map[i][j]].add(i);
 			}
-
+	
 		BFS();
 		System.out.println(-1);
-	}
-	
-
-	// INPUT 속도 증가
-	private static class InputReader {
-		private InputStream stream = System.in;
-		private byte[] buf = new byte[1024];
-		private int curChar;
-		private int numChars;
-		
-		public int read() {
-			if (numChars == -1) {
-				throw new InputMismatchException();
-			}
-			if (curChar >= numChars) {
-				curChar = 0;
-				try {
-					numChars = stream.read(buf);
-				} catch (IOException e) {
-					throw new InputMismatchException();
-				}
-				if (numChars <= 0) {
-					return -1;
-				}
-			}
-			return buf[curChar++];
-		}
-
-		public int in() {
-			int c = read();
-			while (isSpaceChar(c)) {
-				c = read();
-			}
-			int sgn = 1;
-			if (c == '-') {
-				sgn = -1;
-				c = read();
-			}
-			int res = 0;
-			do {
-				if (c < '0' || c > '9') {
-					throw new InputMismatchException();
-				}
-				res *= 10;
-				res += c - '0';
-				c = read();
-			} while (!isSpaceChar(c));
-			return res * sgn;
-		}
-
-		public boolean isSpaceChar(int c) {
-			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-		}
-
 	}
 }
 class Node{
