22699b3c4bf5a3e755d22253f06f11770f162507
YongJu Kim <kyjdummy@gmail.com>
2024-03-01 22:32:01 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 14820 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,64 @@
+//https://github.com/KimYongJ/algorithm
+
+class Main{
+	public static void main(String[] args)throws Exception{new Main().solution();}
+	
+	int X,Y, nextX, nextY, land, map[][];
+	int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1},  {-1,-1},{-1,1},{1,-1},{1,1}};
+	StringBuilder sb = new StringBuilder();
+	
+    // 빠른 입력을 위해 만든 함수
+    public static int read() throws Exception 
+    {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
+
+	public void DFS(int y, int x) 
+	{
+		if(map[y][x] == 0) return;
+		map[y][x] = 0;
+		
+		for(int xy[] : dxy) 
+		{
+			nextY = y + xy[0];
+			nextX = x + xy[1];
+			if(map[nextY][nextX] == 1)
+				DFS(nextY, nextX);
+		}
+	}
+	public void solution() throws Exception
+	{
+
+		while(true) 
+		{
+			X = read();
+			Y = read();
+			land = 0;
+			
+			if(X==0 && Y==0)
+				break;
+			
+			map = new int[Y+2][X+2];
+			
+			for(int y=1; y<=Y; y++) 
+				for(int x=1; x<=X; x++) 
+					map[y][x] = read();
+			
+			for(int y=1; y<=Y; y++)
+				for(int x=1; x<=X; x++)
+					if(map[y][x] == 1) {
+						land++;
+						DFS(y,x);
+					}
+
+			sb.append(land).append('\n');
+			
+		}
+		System.out.println(sb);
+	}
+}
