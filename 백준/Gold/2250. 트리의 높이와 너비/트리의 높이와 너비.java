704c45c0b914d741ea4c9b8e68a15f8999a45a0b
YongJu Kim <kyjdummy@gmail.com>
2024-03-02 21:23:51 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 152 ms, Memory: 15384 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
@@ -1,23 +1,18 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{new Main().solution();}
 	
 	int N, X, p, l, r, parentNode;
-	int rlevel, width;
-	int minX[], maxX[];
+	int rlevel, width, minX[], maxX[];
 	boolean parent[];
 	Node nlist[];
+	
 	void solution()throws Exception {
-		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb	= new StringBuilder();
-		StringTokenizer st;
 		X					= 1;
-		N 					= Integer.parseInt(br.readLine());
+		N 					= read();
 		minX 				= new int[N+1];				// 레벨 별 가장 작은 X값
 		maxX 				= new int[N+1];				// 레벨 별 가장 큰 X값
 		nlist 				= new Node[N+1]; 			// 노드 정보를 담을 리스트 생성
@@ -25,10 +20,9 @@ class Main{
 		
 		for(int i=0; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			p = Integer.parseInt(st.nextToken());		// 부모노드
-			l = Integer.parseInt(st.nextToken());		// 왼쪽노드	
-			r = Integer.parseInt(st.nextToken());		// 오른쪽노드
+			p = read();									// 부모노드
+			l = read();									// 왼쪽노드	
+			r = read();									// 오른쪽노드
 			nlist[p] = new Node(l,r);					// 부모노드의 자식정보 셋팅
 			if(l >0 ) parent[l] = true;					// 루트노드가 아닌 것들 체크
 			if(r >0 ) parent[r] = true;					// 루트노드가 아닌 것들 체크 
@@ -42,19 +36,26 @@ class Main{
 		}
 		
 		DFS(parentNode, 1);								// 순서 : 부모노드 , level
+		// 결과 출력
 		for(int i=1; i<=N; i++) 
 		{
 			if(minX[i] == 10001) break;
-			if(width < maxX[i]-minX[i] +1) {
+			if(width < maxX[i]-minX[i] +1) 
+			{
 				width = maxX[i]-minX[i] +1;
 				rlevel = i;
 			}
 		}
-		sb.append(rlevel).append(' ').append(width);
+		
+		sb.append(rlevel)
+			.append(' ')
+			.append(width);
+		
 		System.out.println(sb);
 	}
 	// 중위 순회 진행 하며 x값을 저장 : 왼쪽 -> 루트 -> 오른쪽
-	public void DFS(int parent, int level) {
+	public void DFS(int parent, int level) 
+	{
 		int lNode = nlist[parent].left;
 		int rNode = nlist[parent].right;
 		if(lNode > 0) 					// 왼쪽 노드가 있다면
@@ -67,6 +68,15 @@ class Main{
 		
 		if(rNode > 0)					// 오른쪽 노드가 있다면
 			DFS(rNode, level + 1);		// 오른쪽 노드 탐색 시작
+	}
+    // 빠른 입력을 위해 만든 함수
+    public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
 	}
 }
 
