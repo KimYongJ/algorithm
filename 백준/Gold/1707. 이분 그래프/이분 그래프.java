911b00038ae06adf0b9c4ed629fb89cc16c1185a
YongJu Kim <kyjdummy@gmail.com>
2024-03-04 21:15:12 +0900

    [Gold IV] Title: 이분 그래프, Time: 948 ms, Memory: 270996 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -0,0 +1,78 @@
+// https://github.com/KimYongJ/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int T, V, E, node1, node2, flag[];
+	static ArrayList<Integer>[] list;
+	static ArrayDeque<Integer> q;
+	public static boolean BFS(int i) {
+		int nextFlag;
+		flag[i] = 1;
+		q.add(i);
+		while(!q.isEmpty()) 
+		{
+			int now = q.poll();
+			nextFlag = flag[now] * -1;
+			for(int next : list[now]) 
+			{
+				if(flag[next] == flag[now])
+					return false;
+				else if(flag[next] == 0) {
+					flag[next] = nextFlag;
+					q.add(next);
+				}
+			}
+			
+		}
+		
+		
+		return true;
+	}
+
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		
+		T = Integer.parseInt(br.readLine());
+		while(T-->0) {
+			st = new StringTokenizer(br.readLine());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			flag = new int[V+1];
+			list = new ArrayList[V+1];
+			q = new ArrayDeque<>();
+			
+			for(int i=1; i<=V; i++)
+				list[i] = new ArrayList<>();
+			
+			for(int i=0; i<E; i++) 
+			{
+				st = new StringTokenizer(br.readLine());
+				node1 = Integer.parseInt(st.nextToken());
+				node2 = Integer.parseInt(st.nextToken());
+				list[node1].add(node2);
+				list[node2].add(node1);
+			}
+			
+			boolean result = false;
+			for(int i=1; i<=V; i++) 
+			{
+				if(flag[i] == 0)
+					result = BFS(i);
+				if(!result) break;
+			}
+			
+			sb.append(result  ? "YES" : "NO")
+				.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
