25c7033b1be071546a9deb22c69f38cf7a10351b
YongJu Kim <kyjdummy@gmail.com>
2024-03-04 21:26:56 +0900

    [Gold IV] Title: 이분 그래프, Time: 556 ms, Memory: 70972 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,26 +1,31 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 class Main{
 	
 	static int T, V, E, node1, node2, flag[];
+	static boolean result;
 	static ArrayList<Integer>[] list;
+
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 
 	public static boolean DFS(int node, int flagValue) 
 	{
-		boolean f = false;
-		int nflagValue = flagValue * -1;
-		flag[node] = flagValue;
+		boolean f 		= false;
+		int nflagValue 	= flagValue * -1;
+		flag[node] 		= flagValue;
 		for(int nextNode : list[node]) 
 		{
-			if(flag[nextNode] == 0) {
+			if(flag[nextNode] == 0)
+			{
 				f =  DFS(nextNode, nflagValue);
-				if(!f)
-					return f;
+				if(!f) return f;
 			}
 			if(flag[nextNode] == flagValue) 
 				return false;
@@ -29,33 +34,27 @@ class Main{
 	}
 
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		
-		T = Integer.parseInt(br.readLine());
-		while(T-->0) {
-			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
-			flag = new int[V+1];
-			list = new ArrayList[V+1];
+
+		T = read();
+		while(T-->0) 
+		{
+			V 		= read();
+			E 		= read();
+			flag 	= new int[V+1];
+			list 	= new ArrayList[V+1];
 			
 			for(int i=1; i<=V; i++)
 				list[i] = new ArrayList<>();
 			
 			for(int i=0; i<E; i++) 
 			{
-				st = new StringTokenizer(br.readLine());
-				node1 = Integer.parseInt(st.nextToken());
-				node2 = Integer.parseInt(st.nextToken());
+				node1 = read();
+				node2 = read();
 				list[node1].add(node2);
 				list[node2].add(node1);
 			}
-			
-			boolean result = false;
-			
+
 			for(int i=1; i<=V; i++) 
 			{
 				if(flag[i] == 0)
