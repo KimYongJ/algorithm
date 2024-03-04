bab610caf5659f2dfcaf43d105bc9c86e43c10b9
YongJu Kim <kyjdummy@gmail.com>
2024-03-04 21:23:24 +0900

    [Gold IV] Title: 이분 그래프, Time: 924 ms, Memory: 269996 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -2,7 +2,6 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -10,28 +9,21 @@ class Main{
 	
 	static int T, V, E, node1, node2, flag[];
 	static ArrayList<Integer>[] list;
-	static ArrayDeque<Integer> q;
-	public static boolean BFS(int i) {
-		int nextFlag;
-		flag[i] = 1;
-		q.add(i);
-		while(!q.isEmpty()) 
+
+	public static boolean DFS(int node, int flagValue) 
+	{
+		boolean f = false;
+		flag[node] = flagValue;
+		for(int nextNode : list[node]) 
 		{
-			int now = q.poll();
-			nextFlag = flag[now] * -1;
-			for(int next : list[now]) 
-			{
-				if(flag[next] == flag[now])
-					return false;
-				else if(flag[next] == 0) {
-					flag[next] = nextFlag;
-					q.add(next);
-				}
+			if(flag[nextNode] == 0) {
+				f =  DFS(nextNode, flag[node] * -1);
+				if(!f)
+					return f;
 			}
-			
+			if(flag[nextNode] == flag[node]) 
+				return false;
 		}
-		
-		
 		return true;
 	}
 
@@ -48,7 +40,6 @@ class Main{
 			E = Integer.parseInt(st.nextToken());
 			flag = new int[V+1];
 			list = new ArrayList[V+1];
-			q = new ArrayDeque<>();
 			
 			for(int i=1; i<=V; i++)
 				list[i] = new ArrayList<>();
@@ -63,11 +54,13 @@ class Main{
 			}
 			
 			boolean result = false;
+			
 			for(int i=1; i<=V; i++) 
 			{
 				if(flag[i] == 0)
-					result = BFS(i);
-				if(!result) break;
+					result = DFS(i,1);
+				if(!result) 
+					break;
 			}
 			
 			sb.append(result  ? "YES" : "NO")
@@ -76,3 +69,85 @@ class Main{
 		System.out.println(sb);
 	}
 }
+
+
+///////////////////////// 이하 BFS 방법
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.ArrayDeque;
+//import java.util.ArrayList;
+//import java.util.StringTokenizer;
+//
+//class Main{
+//	
+//	static int T, V, E, node1, node2, flag[];
+//	static ArrayList<Integer>[] list;
+//	static ArrayDeque<Integer> q;
+//	public static boolean BFS(int i) {
+//		int nextFlag;
+//		flag[i] = 1;
+//		q.add(i);
+//		
+//		
+//		while(!q.isEmpty()) 
+//		{
+//			int now = q.poll();
+//			nextFlag = flag[now] * -1;
+//			for(int next : list[now]) 
+//			{
+//				if(flag[next] == flag[now])
+//					return false;
+//				else if(flag[next] == 0) {
+//					flag[next] = nextFlag;
+//					q.add(next);
+//				}
+//			}
+//			
+//		}
+//		
+//		
+//		return true;
+//	}
+//
+//	public static void main(String[] args)throws Exception{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		
+//		T = Integer.parseInt(br.readLine());
+//		while(T-->0) {
+//			st = new StringTokenizer(br.readLine());
+//			V = Integer.parseInt(st.nextToken());
+//			E = Integer.parseInt(st.nextToken());
+//			flag = new int[V+1];
+//			list = new ArrayList[V+1];
+//			q = new ArrayDeque<>();
+//			
+//			for(int i=1; i<=V; i++)
+//				list[i] = new ArrayList<>();
+//			
+//			for(int i=0; i<E; i++) 
+//			{
+//				st = new StringTokenizer(br.readLine());
+//				node1 = Integer.parseInt(st.nextToken());
+//				node2 = Integer.parseInt(st.nextToken());
+//				list[node1].add(node2);
+//				list[node2].add(node1);
+//			}
+//			
+//			boolean result = false;
+//			
+//			for(int i=1; i<=V; i++) 
+//			{
+//				if(flag[i] == 0)
+//					result = BFS(i);
+//				if(!result) break;
+//			}
+//			
+//			sb.append(result  ? "YES" : "NO")
+//				.append('\n');
+//		}
+//		System.out.println(sb);
+//	}
+//}
