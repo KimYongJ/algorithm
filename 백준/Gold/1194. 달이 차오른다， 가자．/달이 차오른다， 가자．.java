726fbe12b5470686112535c04dc97ffa435098ef
YongJu Kim <kyjdummy@gmail.com>
2024-03-05 21:01:49 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 144 ms, Memory: 15740 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -16,8 +16,8 @@ class Main{
 	
 	static char map[][];
 	static boolean isContinue, visit[][][];
+	static int Y, X, nextY, nextX, nextBitmask;
 	static int dxy[][] = {{0,1},{1,0},{-1,0},{0,-1}};
-	static int Y, X, nextY, nextX, nextDist, nextBitmask;
 	static ArrayDeque<Point> q;
 	
 	public static void BFS() {
@@ -34,10 +34,9 @@ class Main{
 			{
 				nextY 		= now.y + xy[0];
 				nextX 		= now.x + xy[1];
-				nextDist 	= now.dist + 1;
 				nextBitmask = now.bitmask;
 				isContinue	= true;
-				if(nextY>=0 && nextX>=0 && nextY<Y && nextX<X && map[nextY][nextX] != '#') 
+				if(map[nextY][nextX] != '#') 
 				{
 					if('a' <= map[nextY][nextX] && map[nextY][nextX] <='f' )		// 가려는 곳이 열쇠인 경우
 						nextBitmask |= (1 << (map[nextY][nextX]-'a'));				// 열쇠를 넣는다.
@@ -49,7 +48,7 @@ class Main{
 					if(isContinue && !visit[nextBitmask][nextY][nextX])
 					{
 						visit[nextBitmask][nextY][nextX] = true;
-						q.add(new Point(nextY, nextX, nextDist, nextBitmask));
+						q.add(new Point(nextY, nextX, now.dist + 1, nextBitmask));
 					}
 				}
 			}
@@ -61,18 +60,23 @@ class Main{
 		StringTokenizer st 	= new StringTokenizer(br.readLine());
 		Y 					= Integer.parseInt(st.nextToken());
 		X 					= Integer.parseInt(st.nextToken());
-		map 				= new char[Y][X];
-		visit 				= new boolean[64][Y][X];
+		map 				= new char[Y+2][X+2];
+		visit 				= new boolean[64][Y+2][X+2];
 		q 					= new ArrayDeque<>();
-		String str; char c;
-		for(int y=0; y<Y; y++) 
+		String str;
+		
+		for(int y=0; y<Y+2; y++)
+			map[y][0] = map[y][X+1] = '#';	// 패딩 넣기
+		for(int x=0; x<X+2; x++)
+			map[0][x] = map[Y+1][x] = '#';	// 패딩 넣기
+		
+		for(int y=1; y<=Y; y++) 
 		{
 			str = br.readLine();
-			for(int x=0; x<X; x++) 
+			for(int x=1; x<=X; x++) 
 			{
-				c = str.charAt(x);
-				map[y][x] = c;
-				if(c=='0') 
+				map[y][x] = str.charAt(x-1);
+				if(map[y][x]=='0') 
 				{
 					q.add(new Point(y,x,0,0));
 					visit[0][y][x] = true;
