9e80d6d064cb72bdcee683ae43a4a2396064caa3
YongJu Kim <kyjdummy@gmail.com>
2024-03-11 16:58:00 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1780 ms, Memory: 330204 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -14,14 +14,14 @@ class Node{
 }
 class Main
 {
-	static int Y, X, K, nextY, nextX, nextFlag, nextK, nextCnt, map[][];
+	static int Y, X, K, nextY, nextX, nextK, nextCnt, map[][];
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static boolean visit[][][][];
+	static boolean visit[][][];
 	static ArrayDeque<Node> q;
 	public static void BFS() {
 		q = new ArrayDeque<>();
 		q.add(new Node(1,1,1,0,true));
-		visit[0][0][1][1] = true; // 방문 처리
+		visit[0][1][1] = true; // 방문 처리
 		
 		while(!q.isEmpty()) {
 			Node now = q.poll();
@@ -31,41 +31,29 @@ class Main
 				System.out.println(now.cnt);
 				return;
 			}
-			
 			nextCnt 	= now.cnt 	+ 1;
 			nextK 		= now.k 	+ 1;
-			nextFlag 	= now.flag ? 0 : 1;
 			for(int xy[] : dxy) 
 			{
 				nextY = now.y + xy[0];
 				nextX = now.x + xy[1];
-				if(map[nextY][nextX] == 0)		// 가려는 곳이 그냥 갈 수 있는 경우
+				if(map[nextY][nextX] == 0 && !visit[now.k][nextY][nextX])	// 가려는 곳이 그냥 갈 수 있는 경우
 				{
-					if(!visit[nextFlag][now.k][nextY][nextX]) 
-					{
-						visit[nextFlag][now.k][nextY][nextX] = true;
-						q.add(new Node(nextY, nextX, nextCnt, now.k, !now.flag));
-					}
+					visit[now.k][nextY][nextX] = true;
+					q.add(new Node(nextY, nextX, nextCnt, now.k, !now.flag));
 				}
-				else if(map[nextY][nextX] == 1) // 가려는 곳이 벽일 경우
+				else if(map[nextY][nextX] == 1 && now.k < K) // 가려는 곳이 벽일 경우 + 벽을 부술 수 있을 때 
 				{
-					if(now.k < K) 				// 벽을 부술 수 있고
+					if(now.flag) 			// 낮인 경우
 					{
-						if(now.flag) 			// 낮인 경우
+						if(!visit[nextK][nextY][nextX]) 
 						{
-							if(!visit[nextFlag][nextK][nextY][nextX]) {
-								visit[nextFlag][nextK][nextY][nextX] = true;
-								q.add(new Node(nextY, nextX, nextCnt, nextK, !now.flag));
-							}
-						}
-						else 					// 갈 수 있는데 밤인 경우
-						{
-							if(!visit[nextFlag][now.k][now.y][now.x]) {
-								visit[nextFlag][now.k][now.y][now.x] = true;
-								q.add(new Node(now.y, now.x, nextCnt, now.k, !now.flag));
-							}
+							visit[nextK][nextY][nextX] = true;
+							q.add(new Node(nextY, nextX, nextCnt, nextK, !now.flag));
 						}
 					}
+					else 					// 갈 수 있는데 밤인 경우
+						q.add(new Node(now.y, now.x, nextCnt, now.k, !now.flag));
 				}
 			}
 		}
@@ -78,7 +66,7 @@ class Main
 		X 		= Integer.parseInt(st.nextToken());
 		K 		= Integer.parseInt(st.nextToken());
 		map 	= new int[Y+2][X+2];
-		visit 	= new boolean[2][K+1][Y+2][X+2];
+		visit 	= new boolean[K+1][Y+2][X+2];
 		// 패딩 넣기
 		for(int y=0; y<Y+2; y++)map[y][0] = map[y][X+1] = -1;
 		for(int x=0; x<X+2; x++)map[0][x] = map[Y+1][x] = -1;
@@ -86,8 +74,9 @@ class Main
 		for(int y=1; y<=Y; y++)
 		{
 			String str = br.readLine();
-			for(int x=1; x<=X; x++)
+			for(int x=1; x<=X; x++) {
 				map[y][x] = str.charAt(x-1)-'0';
+			}
 		}
 		BFS();
 	}
