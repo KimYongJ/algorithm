3e1e5e1231d9db3e0cfdd1f8b57840066052d184
YongJu Kim <kyjdummy@gmail.com>
2024-03-12 00:14:52 +0900

    [Silver II] Title: 숫자판 점프, Time: 128 ms, Memory: 15368 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -1,49 +1,48 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
-
 class Main{
-	static int nextY, nextX, map[][], dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static boolean visit[][];
-	static HashSet<String> set;
-	public static void BACK(int depth, int y , int x, String str) {
+	static int nextY, nextX, cnt, map[][], dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+	static boolean visit[];
+	// 빠른 입력을 위한 함수
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
+	public static void BACK(int depth, int y , int x, int sum) {
 		if(depth == 5) 
 		{
-			set.add(str);
+			if(!visit[sum]) 
+			{
+				visit[sum] = true;
+				cnt++;
+			}
 			return;
 		}
 		for(int xy[] : dxy) 
 		{
 			nextY = y + xy[0];
 			nextX = x + xy[1];
-			if(map[nextY][nextX] != -1 && !visit[nextY][nextX]) 
-				BACK(depth + 1, nextY, nextX, str + map[nextY][nextX]);
+			if(map[nextY][nextX] != -1)
+				BACK(depth + 1, nextY, nextX, sum*10 + map[nextY][nextX]);
 		}
 		
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		map = new int[7][7];
-		visit = new boolean[7][7];
-		set = new HashSet<>();
+		visit = new boolean[1000000];
 		for(int i=0; i<7; i++)			// 패딩 넣기
 			map[0][i] = map[i][0] = 
 			map[i][6] = map[6][i] = -1;
 		
 		for(int y=1; y<=5; y++) 
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=5; x++)
-				map[y][x] = Integer.parseInt(st.nextToken());
-		}
+				map[y][x] = read();
+		
 
 		for(int y=1; y<=5; y++)
 			for(int x=1; x<=5; x++) 
-				BACK(0,y,x, map[y][x]+"");
-		System.out.println(set.size());
+				BACK(0,y,x, map[y][x]);
+		System.out.println(cnt);
 	}
 }
