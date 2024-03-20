652718411f07684ea31ab3f4799d33d2bacdecc1
YongJu Kim <kyjdummy@gmail.com>
2024-03-20 20:07:28 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,22 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st 	= new StringTokenizer(br.readLine());
 		StringBuilder sb 	= new StringBuilder();
-		int N 				= Integer.parseInt(st.nextToken());
-		int M 				= Integer.parseInt(st.nextToken());
+		int N 				= read();
+		int M 				= read();
 		int arr[] 			= new int[N+1];
 		int a,b,c;
 		for(int i=1; i<=N; i++)
 			arr[i] = i;
 		for(int i=0; i<M; i++) {
-			st 	= new StringTokenizer(br.readLine());
-			a 	= Integer.parseInt(st.nextToken());
-			b 	= Integer.parseInt(st.nextToken());
+			a 	= read();
+			b 	= read();
 			c 	= arr[a];
 			arr[a] = arr[b];
 			arr[b] = c;
