996b9415ab8ee0bb959a44af91cb2f3b718ffa87
YongJu Kim <kyjdummy@gmail.com>
2024-03-20 21:44:26 +0900

    [Platinum V] Title: 개미, Time: 784 ms, Memory: 86000 KB -BaekjoonHub

---
+++ 백준/Platinum/14942. 개미/개미.java
@@ -0,0 +1,99 @@
+// https://github.com/KimYongJ/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Node{
+	int node, dist;
+	Node(int node, int dist){
+		this.node = node;
+		this.dist = dist;
+	}
+}
+class Main{
+	
+	static int N, energy[], parent[][], parentDist[][];
+	static boolean visit[];
+	static ArrayList<Node>[] adlist;
+	public static void DFS(int now) {
+		for(Node next : adlist[now]) 
+		{
+			if(!visit[next.node]) 
+			{
+				visit[next.node]		= true;		// 방문처리
+				// parent는 부모노드를 저장하는 배열이다. [노드번호][0]은 현재 노드의 바로 위 부모노드, [노드번호][1]은 2단계 위 부모노드, [2]는 4단계 위 부모노드 [3]은 8단계 위 부모노드
+				parent[next.node][0] 	= now;
+				// parentDist는 부모노드로의 거리를 저장하는 배열이다. [노드번호][0]은 현재 노드의 바로위 부모노드까지 거리, [노드번호][1]은 2단계 위 부모노드까지 거리 [2]는 4단계 위 부모까지거리 [3]은 8단계 위 부모노드까지 거리
+				parentDist[next.node][0]= next.dist;
+				DFS(next.node);
+			}
+			
+		}
+	}
+	public static void setting() {
+		for(int i=1; i<19; i++)
+		{
+			for(int node=1; node<=N; node++)
+			{
+				// 현재 node의 부모노드의 부모노드들을 세팅, i는 2^i번째 부모노드이다. i가 0이면 바로 위 부모노드, i가 1이면 2번째 위 부모노드, i가 2이면 4번째 3이면 8번째 부모노드
+				parent[node][i] 	= parent[      parent[node][i-1]      ][i-1];
+				// 현재 node에서 2^i 단계 위의 부모노드(parentDist[node][i])까지의 거리를 계산한다.
+				// node에서 2^(i-1) 단계 위의 부모노드까지의 거리(parentDist[node][i-1]) 
+				//	+ 그 부모노드에서 다시 2^(i-1) 단계 위의 부모노드까지의 거리(parentDist[  parent[node][i-1]  ][i-1])
+				parentDist[node][i] = parentDist[  parent[node][i-1]  ][i-1] + parentDist[node][i-1];
+			}
+			
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		N 			= Integer.parseInt(br.readLine());
+		energy 		= new int[N+1];
+		parent 		= new int[N+1][19];
+		parentDist 	= new int[N+1][19];
+		visit 		= new boolean[N+1];
+		adlist 		= new ArrayList[N+1];
+		for(int i=0; i<N; i++)	// 에너지를 입력 받는다.
+		{
+			energy[i+1]	= Integer.parseInt(br.readLine());
+			adlist[i+1] = new ArrayList<>();
+		}
+		
+		int a,b,c;
+		for(int i=1; i<N; i++) 
+		{
+			st	= new StringTokenizer(br.readLine());
+			a 	= Integer.parseInt(st.nextToken());
+			b 	= Integer.parseInt(st.nextToken());
+			c 	= Integer.parseInt(st.nextToken());
+			adlist[a].add(new Node(b,c));
+			adlist[b].add(new Node(a,c));
+		}
+		
+		visit[1] = true; 		// 1방문 체크
+		DFS(1);					// DFS 함수 안에서 희소배열의 최초 값세팅(자기 바로 위 부모노드까지의 정보만(거리던 노드번호던)세팅) 
+		setting();				// DFS 함수에서 세팅한 자기 부모노드의 기본정보로 전체를 세팅한다.
+		
+		int now;
+		for(int n=1; n<=N; n++) // 모든 노드에 대해서 탐색 
+		{
+			now = n;
+			for(int m=18; m>=0; m--) // 최상위 부터 탐색하면서 내려옴 
+			{
+				if(energy[n] - parentDist[now][m] >= 0) 
+				{
+					energy[n] -= parentDist[now][m];
+					now = parent[now][m];
+					//break;
+				}
+			}
+			sb.append(now == 0 ? 1 : now)
+				.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
