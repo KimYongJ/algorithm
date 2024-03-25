7fa8ec7d117eb7f7c4be3b858946be29b9ef7d12
YongJu Kim <kyjdummy@gmail.com>
2024-03-26 00:06:23 +0900

    [Gold V] Title: 암호 만들기, Time: 140 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -8,49 +8,47 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int MAX_DEPTH, C;
-	static char arr[];
+	static char arr[], string[];
 	static boolean visit[];
 	static StringBuilder sb;
-	public static boolean checkString(String str) {
-		for(int i=1; i<MAX_DEPTH; i++)
-			if(str.charAt(i-1) > str.charAt(i))
-				return false;
-		return true;
-	}
-	public static void BackTracking(String str,int idx, int depth, int VowelsCnt, int ConsonantsCnt) // 순서 : 깊이 , 모음개수, 자음 개수
+	public static void BackTracking(int idx, int depth, int VowelsCnt, int ConsonantsCnt) // 순서 : 깊이 , 모음개수, 자음 개수
 	{
-		if(depth == MAX_DEPTH) {
-			if(VowelsCnt >=1 && ConsonantsCnt>=2 && checkString(str)) {
-				sb.append(str).append('\n');
-			}
-				
+		if(depth == MAX_DEPTH) 
+		{
+			if(VowelsCnt >=1 && ConsonantsCnt>=2)
+				sb.append(new String(string)).append('\n');
 			return;
 		}
-		for(int i=idx; i<C; i++) {
-			if(!visit[i]) {
+		for(int i=idx; i<C; i++) 
+		{
+			if(!visit[i]) 
+			{
 				visit[i] = true;
+				string[depth] = arr[i];
 				if(arr[i]=='a' || arr[i]=='e' || arr[i]=='i' || arr[i]=='o' || arr[i]=='u')
-					BackTracking(str+arr[i],i, depth+1, VowelsCnt + 1, ConsonantsCnt);
+					BackTracking(i, depth+1, VowelsCnt + 1, ConsonantsCnt);
 				else
-					BackTracking(str+arr[i],i, depth+1, VowelsCnt, ConsonantsCnt + 1);
+					BackTracking(i, depth+1, VowelsCnt, ConsonantsCnt + 1);
 				visit[i] = false;
 			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		MAX_DEPTH = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		arr = new char[C];
-		visit = new boolean[C];
-		sb = new StringBuilder();
-		st = new StringTokenizer(br.readLine());
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st 	= new StringTokenizer(br.readLine());
+		MAX_DEPTH 			= Integer.parseInt(st.nextToken());
+		C 					= Integer.parseInt(st.nextToken());
+		arr 				= new char[C];
+		string				= new char[MAX_DEPTH];
+		visit 				= new boolean[C];
+		sb 					= new StringBuilder();
+		st 					= new StringTokenizer(br.readLine());
 		for(int i=0; i<C; i++)
 			arr[i] = st.nextToken().charAt(0);
+		
 		Arrays.sort(arr);
 		
-		BackTracking("",0,0,0,0);
+		BackTracking(0,0,0,0);
 		
 		System.out.println(sb);
 	}
