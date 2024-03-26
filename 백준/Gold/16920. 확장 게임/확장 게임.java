875940d0772230900284400bfe363978c3907c5a
YongJu Kim <kyjdummy@gmail.com>
2024-03-26 20:57:36 +0900

    [Gold II] Title: 확장 게임, Time: 504 ms, Memory: 57820 KB -BaekjoonHub

---
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -0,0 +1,100 @@
+// https://github.com/KimYongJ/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+
+class Point{
+	int y,x, dist;
+	Point(int y, int x, int dist){
+		this.y=y; this.x=x; this.dist=dist;
+	}
+}
+class Main
+{	
+	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+	static int Y, X, P, nextY, nextX, nextDist, castleCnt, maxDist[], castle[];
+	static char map[][];
+	static ArrayDeque<Point> q[];
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
+	public static boolean BFS(int idx, ArrayDeque<Point> q) {
+		boolean flag = false;
+		int size;
+		for(int r=0; r<maxDist[idx]; r++) {
+			size = q.size();
+			if(size == 0) 
+				break;
+			for(int i=0; i<size; i++) {
+				Point now = q.poll();
+				nextDist = now.dist + 1;
+				for(int xy[] : dxy) {
+					nextY = now.y + xy[0];
+					nextX = now.x + xy[1];
+					if(map[nextY][nextX] == '.') {
+						flag = true;
+						castleCnt++;
+						map[nextY][nextX] =(char)('0' + idx);
+						castle[map[nextY][nextX]-'0']++;
+						q.add(new Point(nextY, nextX, nextDist));
+					}
+				}
+			}
+		}
+		return flag;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st 	= new StringTokenizer(br.readLine());
+		StringBuilder sb 	= new StringBuilder();
+		Y 		= Integer.parseInt(st.nextToken());
+		X 		= Integer.parseInt(st.nextToken());
+		P 		= Integer.parseInt(st.nextToken());	// 성을 갖는 인원
+		maxDist = new int[P+1];
+		castle 	= new int[P+1];
+		map		= new char[Y+2][X+2];
+		q		= new ArrayDeque[P+1];
+		st 	= new StringTokenizer(br.readLine());
+		for(int i=1; i<=P; i++) 
+		{
+			q[i]		= new ArrayDeque<>();
+			maxDist[i] 	= Integer.parseInt(st.nextToken());
+		}
+		String str;
+		for(int y=1; y<=Y; y++) 
+		{
+			str = br.readLine();
+			for(int x=1; x<=X; x++) 
+			{
+				map[y][x] = str.charAt(x-1);
+				if(map[y][x] != '.')			// 빈 공간이 아닐 때
+				{
+					castleCnt++;				// 반복문 탈출때 사용할 이동 불가능한 공간(나중에 이동 가능한 공간을 구히기위한)
+					if(map[y][x] != '#') 
+					{
+						q[map[y][x]-'0'].add(new Point(y,x,0));// 숫자일 경우 해당 좌표를 큐에 넣는다.
+						castle[map[y][x]-'0']++;
+					}
+				}
+			}
+		}
+		boolean flag;
+		while(Y*X != castleCnt)
+		{
+			flag = false;
+			for(int i=1; i<=P; i++)
+				if( BFS(i, q[i]) )
+					flag = true;
+			if(!flag)
+				break;
+		}
+		for(int i=1; i<=P; i++)
+			sb.append(castle[i]).append(' ');
+		System.out.println(sb);
+	}
+}
+
