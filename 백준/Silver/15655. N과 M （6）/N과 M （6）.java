058b838d2b1bd840d0dbf2b49a56837214ef57ac
YongJu Kim <kyjdummy@gmail.com>
2024-03-29 17:14:41 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -8,6 +8,11 @@ public class Main {
 	static int N, M, arr[], result[];
 	static boolean visit[];
 	static StringBuilder sb;
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static void back(int depth,int now) {
 		if(depth == M) {
 			for(int i=0; i<M; i++)
@@ -19,23 +24,20 @@ public class Main {
 			if(!visit[i]) {
 				visit[i] = true;
 				result[depth] = arr[i];
-				back(depth+1,i);
+				back(depth+1,i+1);
 				visit[i] = false;
 			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		M 		= Integer.parseInt(st.nextToken());
+		N 		= read();
+		M 		= read();
 		arr 	= new int[N];
 		result 	= new int[M];
 		visit 	= new boolean[N];
 		sb 		= new StringBuilder();
-		st 		= new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 
 		Arrays.sort(arr);
 		
