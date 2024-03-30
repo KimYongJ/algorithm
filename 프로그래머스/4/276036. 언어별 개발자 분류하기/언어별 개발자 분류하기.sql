ddd8257f547f1c67fe309d938dd736a968513f3b
YongJu Kim <kyjdummy@gmail.com>
2024-03-31 01:38:34 +0900

    언어별 개발자 분류하기.sql

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,34 +1,18 @@
-WITH    FRONT_SKILL AS (SELECT  CODE FROM SKILLCODES WHERE CATEGORY = 'Front End')
-,       PYTHON_SKILL AS(SELECT  CODE FROM SKILLCODES WHERE NAME = 'Python')
-,       C_SKILL AS(SELECT  CODE FROM SKILLCODES WHERE NAME = 'C#')
-,       A_GRADE AS(
-                    SELECT  'A' AS GRADE
-                    ,       ID
-                    ,       EMAIL
-                    FROM DEVELOPERS  D
-                    WHERE EXISTS(SELECT 1 FROM FRONT_SKILL F WHERE F.CODE | D.SKILL_CODE = D.SKILL_CODE)
-                    AND EXISTS(SELECT 1 FROM PYTHON_SKILL P WHERE P.CODE | D.SKILL_CODE = D.SKILL_CODE)
+WITH TEMP AS(
+    SELECT
+            CASE WHEN EXISTS(SELECT 1 FROM SKILLCODES S WHERE S.CATEGORY = 'Front End' AND S.CODE | D.SKILL_CODE = D.SKILL_CODE)
+                 AND  EXISTS(SELECT 1 FROM SKILLCODES S WHERE S.NAME = 'Python' AND S.CODE | D.SKILL_CODE = D.SKILL_CODE)
+                 THEN 'A'
+                 WHEN EXISTS(SELECT 1 FROM SKILLCODES S WHERE S.NAME = 'C#' AND S.CODE | D.SKILL_CODE = D.SKILL_CODE)
+                 THEN 'B'
+                 WHEN EXISTS(SELECT 1 FROM SKILLCODES S WHERE S.CATEGORY = 'Front End' AND S.CODE | D.SKILL_CODE = D.SKILL_CODE)
+                 THEN 'C'
+                 ELSE NULL END AS GRADE
+    ,       D.ID
+    ,       D.EMAIL
+    FROM DEVELOPERS D 
 )
-,       B_GRADE AS(
-                    SELECT  'B' AS GRADE
-                    ,       ID
-                    ,       EMAIL
-                    FROM DEVELOPERS  D
-                    WHERE EXISTS(SELECT 1 FROM C_SKILL C WHERE C.CODE | D.SKILL_CODE = D.SKILL_CODE)
-                    AND NOT EXISTS(SELECT 1 FROM A_GRADE A WHERE A.ID = D.ID)
-)
-,       C_GRADE AS(
-                    SELECT  'C' AS GRADE
-                    ,       ID
-                    ,       EMAIL
-                    FROM DEVELOPERS  D
-                    WHERE EXISTS(SELECT 1 FROM FRONT_SKILL F WHERE F.CODE | D.SKILL_CODE = D.SKILL_CODE)
-                    AND NOT EXISTS(SELECT 1 FROM A_GRADE A WHERE A.ID = D.ID)
-                    AND NOT EXISTS(SELECT 1 FROM B_GRADE B WHERE B.ID = D.ID)
-)
-,       RESULT AS(
-                    SELECT * FROM A_GRADE  UNION ALL
-                    SELECT * FROM B_GRADE  UNION ALL
-                    SELECT * FROM C_GRADE 
-)
-SELECT * FROM RESULT ORDER BY GRADE,ID
+SELECT *
+FROM TEMP
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID
