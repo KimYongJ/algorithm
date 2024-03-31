7ebeb67d68a02ff2dd1483bfe409b664c5a0d9f7
YongJu Kim <kyjdummy@gmail.com>
2024-03-31 22:13:48 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 224 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -7,41 +7,43 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int N, MAX, nextCnt, arr[][];
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static void Back(int depth, int cnt) {
 		if(depth == N) {
 			MAX = Math.max(MAX, cnt);
 			return;
 		}
+		if(arr[depth][0] <= 0 ||cnt==N-1) {
+			Back(depth+1, cnt);
+			return;
+		}
 		
 		for(int i=0; i<N; i++) {
-			if(i==depth)  
+			if(i==depth || arr[i][0] <= 0)  
 				continue;
+			arr[i][0] 		-= arr[depth][1];
+			arr[depth][0] 	-= arr[i][1];
+			nextCnt 		=  0;
+			if(arr[i][0]     <= 0)	nextCnt += 1;
+			if(arr[depth][0] <= 0) 	nextCnt += 1;
 			
-			if(arr[i][0] > 0 && arr[depth][0] > 0) {
-				arr[i][0] 		-= arr[depth][1];
-				arr[depth][0] 	-= arr[i][1];
-				nextCnt 		=  cnt;
-				if(arr[i][0]     <= 0)	nextCnt += 1;
-				if(arr[depth][0] <= 0) 	nextCnt += 1;
-				
-				Back(depth+1, nextCnt);
-				
-				arr[i][0] 		+= arr[depth][1];
-				arr[depth][0] 	+= arr[i][1];
-			}else
-				Back(depth+1, cnt);
+			Back(depth+1, nextCnt+cnt);
+			
+			arr[i][0] 		+= arr[depth][1];
+			arr[depth][0] 	+= arr[i][1];
 		}
 		
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
+		N = read();
 		arr = new int[N][2]; // [N][0] 내구도, [N][1] 무게
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = read();
+			arr[i][1] = read();
 		}
 		
 		Back(0,0);
