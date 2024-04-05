b989b67a729b7f54936a390693c51723f54df84a
YongJu Kim <kyjdummy@gmail.com>
2024-04-05 20:17:27 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 952 ms, Memory: 203876 KB, Score: 23 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.java
@@ -14,13 +14,13 @@ class Node{
 }
 public class Main {
 	static long MAX = 10_000_000_000L;
-	static long money, cost[];
+	static long money, maxCost[], cost[];
 	static int N, M, start, end;
 	static boolean visit[];
 	static PriorityQueue<Node> pq;
 	static ArrayList<Node>[] adlist;
 	public static void Dijkstra() {
-		cost[start] = 0;					// start까지 가는데 든 지불비용 0
+		maxCost[start] = 0;					// start까지 가는데 든 지불비용 0
 		pq.add(new Node(start, money,0));	// start 노드에서 money 만큼 갖고 시작
 		while(!pq.isEmpty()) {
 			Node now = pq.poll();
@@ -32,12 +32,20 @@ public class Main {
 			{
 				if(visit[next.node])continue;
 
-				if(now.money >= next.money) 			// 돈을 지불해야 하기에 현재 갖고있는 돈이 지불할 비용(next.money)보다 커야함
+				long nextMaxMoney = Math.max(now.maxMoney,next.money); // 해당 노드까지 방문하는데 든 가장 큰 지불비용 삽입
+				long nextCost = now.money - next.money;
+				if(nextCost >= 0) 										// 돈을 지불해야 하기에 현재 갖고있는 돈이 지불할 비용(next.money)보다 커야함
 				{
-					if(cost[next.node] > next.money) 	// 가려는 노드까지 도달할 때 드는 지불비용이 최소여야 다음 노드로 갈 수 있음
+					if(maxCost[next.node]== nextMaxMoney ) {
+						if(cost[next.node] < nextCost) {
+							cost[next.node]= nextCost;
+							pq.add(new Node(next.node, cost[next.node], nextMaxMoney));
+						}
+					}else if(maxCost[next.node] > nextMaxMoney) 	// 가려는 노드까지 도달할 때 드는 지불비용이 최소여야 다음 노드로 갈 수 있음
 					{
-						cost[next.node]= Math.max(now.maxMoney,next.money); // 해당 노드까지 방문하는데 든 가장 큰 지불비용 삽입
-						pq.add(new Node(next.node, now.money - next.money, cost[next.node]));
+						maxCost[next.node]= nextMaxMoney;
+						cost[next.node] =  nextCost;
+						pq.add(new Node(next.node, cost[next.node], nextMaxMoney));
 					}
 				}
 				
@@ -52,12 +60,13 @@ public class Main {
 		start 	= Integer.parseInt(st.nextToken());			// 시작 노드
 		end 	= Integer.parseInt(st.nextToken());			// 종료 노드
 		money 	= Long.parseLong(st.nextToken());			// 처음 주어지는 돈
-		cost	= new long[N+1];							// 해당 노드 까지 가는데 든 가장 적은 지불비용
+		cost	= new long[N+1];							// 노드를 index로 하여 각 노드당 남은 돈을 기록한다.
+		maxCost	= new long[N+1];							// 해당 노드 까지 가는데 든 가장 적은 지불비용
 		visit	= new boolean[N+1];							// 다익스트라 실행시 방문한 노드 체킹
 		adlist	= new ArrayList[N+1];
 		pq		= new PriorityQueue<Node>((a,b)->Long.compare(a.money, b.money));
 		for(int i=0; i<=N; i++) {
-			cost[i] 	= MAX;
+			maxCost[i] 	= MAX;
 			adlist[i]	= new ArrayList<>();
 		}
 		
@@ -73,7 +82,7 @@ public class Main {
 		
 		Dijkstra();
 		
-		long result = cost[end];
+		long result = maxCost[end];
 		if(result == MAX)
 			result = -1;
 		System.out.println(result);
