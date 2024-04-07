5d94638bb9eb8b5e679b08d7bf6930b539c92c40
YongJu Kim <kyjdummy@gmail.com>
2024-04-07 21:20:48 +0900

    [Gold III] Title: 순회강연, Time: 168 ms, Memory: 14928 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -31,7 +31,7 @@ class Main{
 			if(parentNode != 0) 					 	// 가장 가까운 날이 0이 아닌 경우
 			{
 				money += now.money;						// 강연 가능하므로 강연비 플러스
-				setParent(parentNode - 1, parentNode);	// 가장 가까운 날을 바로 전날로 세팅,
+				parent[parentNode] = parentNode-1;		// 가장 가까운 날을 바로 전날로 세팅,
 			}
 		}
 		System.out.println(money);
@@ -39,8 +39,5 @@ class Main{
 	public static int getParent(int idx) {
 		if(parent[idx] == idx) return idx;				// 특정 날의 강연 가능한 일자를 구해온다.
 		return parent[idx] = getParent(parent[idx]);	// 경로 압축 기법, union find에서 find 효율을 높이기 위해 사용, 검색 경로상 모든 노드들이 직접 최종 루트를 가리키도록 업데이트함으로 속도 향상
-	}
-	public static void setParent(int beforeNode, int nowNode) {
-		parent[nowNode] = beforeNode;					 
 	}
 }
