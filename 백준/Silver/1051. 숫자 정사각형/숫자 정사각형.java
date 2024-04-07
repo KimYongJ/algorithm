0888f02bf21afc963a411b7259b0dcbfac59456e
YongJu Kim <kyjdummy@gmail.com>
2024-04-07 10:02:38 +0900

    [Silver III] Title: 숫자 정사각형, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -1,39 +1,37 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
-	static int Y, X, max;
-	static char[][] arr;
-
+	static int Y, X, max, arr[][];
+    private static int read() throws Exception{
+	    int c, n = System.in.read() & 15;
+	    while ((c = System.in.read()) > 32)
+	    	n = (n << 3) + (n << 1) + (c & 15);
+	    return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st 	= new StringTokenizer(br.readLine());
-		Y 	= Integer.parseInt(st.nextToken());
-		X 	= Integer.parseInt(st.nextToken());
+		Y 	= read();
+		X 	= read();
 		max = Math.min(Y, X);
-		arr = new char[Y][X];
-		for(int y=0; y<Y; y++)
-			arr[y] = br.readLine().toCharArray();
-		char a,b,c;
-		while(max-- > 0) {
-			for(int y=0; y<Y-max; y++) {
-				for(int x=0; x<X-max; x++) {
-					if(y+max < Y && x+max < X) {
-						a = arr[y+max][x];
-						b = arr[y][x+max];
-						c = arr[y+max][x+max];
-						if( arr[y][x] == a && a== b && b== c) {
-							max+=1;
-							System.out.println(max*max);
-                            return;
-						}
-					}
-				}
-			}
+		arr = new int[Y][X];
+		for(int y=0; y<Y; y++) {
+			for(int x=0; x<X; x++)
+				arr[y][x] = (int)System.in.read();
+			System.in.read();
 		}
-		System.out.println(1);
+		int a,b,c;
+		Loop:
+		while(max-- > 0)
+			for(int y=0; y<Y-max; y++)
+				for(int x=0; x<X-max; x++) {
+					a = arr[y+max][x];
+					b = arr[y][x+max];
+					c = arr[y+max][x+max];
+					if( arr[y][x] == a && a== b && b== c)
+						break Loop;
+				}
+	
+		max++;
+		System.out.println(max * max);
 	}
 }
