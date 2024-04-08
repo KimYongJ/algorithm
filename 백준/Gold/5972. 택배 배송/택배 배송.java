bfeea77505c0dc3b7c58ea9d491a24a0825bbb51
YongJu Kim <kyjdummy@gmail.com>
2024-04-08 16:20:11 +0900

    [Gold V] Title: 택배 배송, Time: 304 ms, Memory: 27016 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -1,10 +1,6 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 class Node{
 	int node, dist;
 	Node(int node, int dist){this.node=node; this.dist=dist;}
@@ -15,6 +11,11 @@ class Main{
 	static boolean visit[];
 	static ArrayList<Node>[] adlist;
 	static PriorityQueue<Node> pq;
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static void Dijkstra() {
 		dist[1] = 0;
 		pq.add(new Node(1,0)); // 1번째 노드까지 0만큼 든다.
@@ -22,12 +23,16 @@ class Main{
 		while(!pq.isEmpty()) {
 			Node now = pq.poll();
 			
-			if(!visit[now.node]) {
+			if(!visit[now.node]) 
+			{
 				visit[now.node] = true;		//큐에서 꺼낸 노드는 최단거리임이 보장된다. 
-				for(Node next : adlist[now.node]) {
-					if(!visit[next.node]) {
+				for(Node next : adlist[now.node]) 
+				{
+					if(!visit[next.node]) 
+					{
 						nextDist = now.dist + next.dist;
-						if(dist[next.node] > nextDist) {
+						if(dist[next.node] > nextDist) 
+						{
 							dist[next.node]= nextDist;
 							pq.add(new Node(next.node, nextDist));
 						}
@@ -37,24 +42,23 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		M 		= Integer.parseInt(st.nextToken());
+		N 		= read();
+		M 		= read();
 		dist 	= new int[N+1];
 		visit 	= new boolean[N+1];
 		adlist	= new ArrayList[N+1];
 		pq 		= new PriorityQueue<Node>((a,b)->a.dist-b.dist);
-		for(int i=0; i<=N; i++) {
+		for(int i=0; i<=N; i++) 
+		{
 			dist[i] 	= MAX;
 			adlist[i] 	= new ArrayList<>();
 		}
 		int a,b,c;
-		for(int i=0; i<M; i++) {
-			st 	= new StringTokenizer(br.readLine());
-			a 	= Integer.parseInt(st.nextToken());
-			b 	= Integer.parseInt(st.nextToken());
-			c 	= Integer.parseInt(st.nextToken());
+		for(int i=0; i<M; i++) 
+		{
+			a 	= read();
+			b 	= read();
+			c 	= read();
 			adlist[a].add(new Node(b,c));
 			adlist[b].add(new Node(a,c));
 		}
