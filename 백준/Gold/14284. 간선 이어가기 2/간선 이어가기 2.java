ae2b4e62bd71942212a0bc49ffc7f791ddd56c12
YongJu Kim <kyjdummy@gmail.com>
2024-04-09 16:53:12 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 228 ms, Memory: 22072 KB -BaekjoonHub

--- 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
@@ -1,9 +1,6 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int node, dist;
 	Node(int node, int dist){
@@ -16,18 +13,22 @@ class Main{
 	static boolean visit[];
 	static ArrayList<Node>[] adlist;
 	static PriorityQueue<Node> pq;
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static void Dijkstra() {
 		dist[s] = 0;
 		pq.add(new Node(s,0));
 		int nextDist;
-		while(!pq.isEmpty()) {
+		while(!pq.isEmpty())
+		{
 			Node now = pq.poll();
 			
+			if(now.node == e)break;
 			if(visit[now.node])continue;
 			visit[now.node] = true;
-			if(now.node == e) {
-				break;
-			}
 			
 			for(Node next : adlist[now.node]) {
 				if(!visit[next.node]) {
@@ -42,30 +43,31 @@ class Main{
 		System.out.println(dist[e]);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		M 		= Integer.parseInt(st.nextToken());
+		N 		= read();
+		M 		= read();
 		dist 	= new int[N+1];
 		visit 	= new boolean[N+1];
 		adlist 	= new ArrayList[N+1];
 		pq 		= new PriorityQueue<Node>((a,b)->a.dist-b.dist);
-		for(int i=1; i<=N; i++) {
+		
+		for(int i=1; i<=N; i++) 
+		{
 			dist[i] 	= MAX;
 			adlist[i] 	= new ArrayList<>();
 		}
+		
 		int a,b,c;
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
+		for(int i=0; i<M; i++) 
+		{
+			a = read();
+			b = read();
+			c = read();
 			adlist[a].add(new Node(b,c));
 			adlist[b].add(new Node(a,c));
 		}
-		st	= new StringTokenizer(br.readLine());
-		s = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
+		
+		s = read();
+		e = read();
 		Dijkstra();
 	}
 }
