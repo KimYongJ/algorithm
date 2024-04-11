313d5b876fd14f8ba665965fa951641f7c54fc85
YongJu Kim <kyjdummy@gmail.com>
2024-04-11 17:41:20 +0900

    [Gold II] Title: 소가 길을 건너간 이유 7, Time: 200 ms, Memory: 16124 KB -BaekjoonHub

--- 백준/Gold/14461. 소가 길을 건너간 이유 7/소가 길을 건너간 이유 7.java
+++ 백준/Gold/14461. 소가 길을 건너간 이유 7/소가 길을 건너간 이유 7.java
@@ -1,9 +1,5 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 class Node{
 	int y, x, cnt, time;
 	Node(int y, int x, int cnt, int time){
@@ -17,6 +13,11 @@ class Main{
 	static int N, ctime, nextY, nextX, nextTime, nextCnt, map[][], time[][][];
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
 	static PriorityQueue<Node> pq;
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static void Dijkstra() {
 		time[0][0][0] = 0;
 		pq.add(new Node(0,0,0,0));
@@ -46,24 +47,26 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		ctime 	= Integer.parseInt(st.nextToken());
+		N 		= read();
+		ctime 	= read();
 		map		= new int[N][N];
 		time	= new int[4][N][N];
 		pq		= new PriorityQueue<Node>((a,b)->a.time-b.time);
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j]	  = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++) 
+			{
+				map[i][j]	  = read();
 				time[0][i][j] = 
 				time[1][i][j] = 
 				time[2][i][j] = MAX;
 			}
-		}
 		Dijkstra();
 		N--;
-		System.out.println(Math.min(Math.min(time[0][N][N], time[1][N][N]),time[2][N][N]));
+		System.out.println(
+							Math.min(
+										Math.min(time[0][N][N], time[1][N][N]),time[2][N][N]
+									)
+						);
 	}
 }
