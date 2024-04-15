1d523d8036138be65dfac0494c785ac3b5f9a188
YongJu Kim <kyjdummy@gmail.com>
2024-04-15 15:09:41 +0900

    [Silver III] Title: 게임, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,30 +1,30 @@
 // https://github.com/kimyongj/algorithm
 
 class Main{
-	static long total, win, main, left, right, mid, ans;
-    static long read() throws Exception {// 빠른 입력을 위한 함수
-        long c, n = System.in.read() & 15;
+	static int total, win, main, left, right, mid, ans;
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
         return n;
     }
-    public static long get(long cnt) {
-    	return ((win+cnt)*100) / (total+cnt);
+    public static int get(long cnt) {
+    	return (int) ((((long)win+cnt)*100) / (total+cnt));
     }
 	public static void main(String[] args)throws Exception{
-		total 	= read();
-		win   	= read();
-		main 	= get(0);
-		ans 	= -1;
-		left 	= 0;
-		right 	= 1_000_000_000;
+		total = read();
+		win   = read();
+		main  = get(0);
+		ans	  = -1;
+		left  = 1;
+		right = 1_000_000_000;
 		while(left <= right) {
 			mid = (left + right) / 2;
 			if(main != get(mid)) {
 				right = mid-1;
-				ans = mid;
+				ans   = mid;
+			}else { 
+				left  = mid+1;
 			}
-			else 
-				left = mid+1;
 		}
 		System.out.print(ans);
 	}
