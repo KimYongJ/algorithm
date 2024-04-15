83574fa545f2c829c9f05dd4f588141d45e0cc1c
YongJu Kim <kyjdummy@gmail.com>
2024-04-15 13:28:46 +0900

    [Silver I] Title: 보석 상자, Time: 252 ms, Memory: 15716 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -1,19 +1,14 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
 	static int N, M, jewel[], left, mid, right;
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		M 		= Integer.parseInt(st.nextToken());
+		N 		= read();
+		M 		= read();
 		jewel 	= new int[M];
 		for(int i=0; i<M; i++) {
-			jewel[i] = Integer.parseInt(br.readLine());
+			jewel[i] = read();
 			right = Math.max(right, jewel[i]);
 		}
 		left = 1;
@@ -37,4 +32,9 @@ class Main{
 		}
 		return true;
 	}
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 }
