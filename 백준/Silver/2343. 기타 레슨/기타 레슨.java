7c64cf2d5abb6847d8083c30eda702e441658fc4
YongJu Kim <kyjdummy@gmail.com>
2024-04-16 16:51:24 +0900

    [Silver I] Title: 기타 레슨, Time: 184 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,33 +1,12 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N, M, lec[], left, mid, right, ans;
-	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 	= Integer.parseInt(st.nextToken());	// 강의 개수
-		M 	= Integer.parseInt(st.nextToken());	// 블루레이 개수
-		lec	= new int[N];
-		st	= new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			lec[i] = Integer.parseInt(st.nextToken());
-			right += lec[i];
-		}
-		left = 1;
-		while(left <= right) {
-			mid = (left + right) / 2; // 블루레이에 들어갈 강의 시간
-			if(check(mid)) {
-				ans = mid;
-				right = mid-1;
-			}else {
-				left = mid+1;
-			}
-		}
-		System.out.println(ans);
-	}
+    static int read() throws Exception {// 빠른 입력을 위한 함수
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
+    }
 	public static boolean check(int mid) {
 		int cnt = 0, sum = 0;
 		// mid시간에 M개 안에 다 넣을 수 있느냐 ?
@@ -42,5 +21,25 @@ class Main{
 				return false;
 		}
 		return true;
+	}
+	public static void main(String[] args)throws Exception{
+		N 	= read();	// 강의 개수
+		M 	= read();	// 블루레이 개수
+		lec	= new int[N];
+		for(int i=0; i<N; i++) {
+			lec[i] = read();
+			right += lec[i];
+			left   = Math.max(left, lec[i]);
+		}
+		while(left <= right) {
+			mid = (left + right) / 2; // 블루레이에 들어갈 강의 시간
+			if(check(mid)) {
+				ans = mid;
+				right = mid-1;
+			}else {
+				left = mid+1;
+			}
+		}
+		System.out.println(ans);
 	}
 }
