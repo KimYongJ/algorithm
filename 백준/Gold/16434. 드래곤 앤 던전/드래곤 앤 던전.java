0b811f6892f57c17a1e50031bbb1680ce2a24bd7
YongJu Kim <kyjdummy@gmail.com>
2024-04-17 19:41:49 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 588 ms, Memory: 55380 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 class Main{
 	
-	static long N, at, left, mid, right, ans, map[][];
+	static long N, at, left, mid, right, map[][];
 	public static boolean check(long MAX) {
 		long energy = MAX;	// 현재 체력
 		long power = at;	// 현재 공력력
@@ -16,8 +16,8 @@ class Main{
 				energy = Math.min(MAX, energy + m[2]); 	// 에너지 추가 
 				power += m[1];							// 공격력 업그레이드
 			}else {
-				cntWin  = (m[2] / power) + (m[2] % power == 0 ? 0 : 1);
-				cntLose = (energy  / m[1])  + (energy  % m[1]  == 0 ? 0 : 1); 
+				cntWin  = (long)Math.ceil((double)m[2] 		/ power);
+				cntLose = (long)Math.ceil((double)energy  	/ m[1]); 
 				if(cntWin <= cntLose) {
 					energy -=  --cntWin * m[1];
 				}else {
@@ -39,17 +39,15 @@ class Main{
 			map[i][1] = Integer.parseInt(st.nextToken());
 			map[i][2] = Integer.parseInt(st.nextToken());
 		}
-		right = 1000000000000000000L;
+		right = 100000000000000000L;
 
 		while(left<=right) {
 			mid = (left + right)/2;
-			if(check(mid)) {
-				ans = mid;
+			if(check(mid))
 				right = mid-1;
-			}else {
+			else
 				left = mid+1;
-			}
 		}
-		System.out.println(ans);
+		System.out.println(left);
 	}
 }
