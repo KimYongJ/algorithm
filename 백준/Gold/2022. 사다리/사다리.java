669645e3c1058e2e319fe0b0b4ccb6835a76dedb
YongJu Kim <kyjdummy@gmail.com>
2024-04-19 21:56:28 +0900

    [Gold IV] Title: 사다리, Time: 132 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Gold/2022. 사다리/사다리.java
+++ 백준/Gold/2022. 사다리/사다리.java
@@ -12,15 +12,16 @@ class Main{
 				Y 		= Double.parseDouble(st.nextToken()),
 				C 		= Double.parseDouble(st.nextToken()),
 				left 	= 0, 
-				right 	= Math.min(X, Y);
+				right 	= Math.min(X, Y),
+				mid, h1, h2, c;
 
 		X = Math.pow(X, 2);
 		Y = Math.pow(Y, 2);
 		while(right - left >= 0.001) {
-			double mid  = (left + right) / 2;
-			double h1 	= Math.sqrt(X - Math.pow(mid,2));
-			double h2 	= Math.sqrt(Y - Math.pow(mid,2));
-			double c 	= (h1*h2) / (h1+h2);
+			mid = (left + right) / 2;
+			h1 	= Math.sqrt(X - Math.pow(mid,2));
+			h2 	= Math.sqrt(Y - Math.pow(mid,2));
+			c 	= (h1*h2) / (h1+h2);
 			if(c < C)
 				right = mid;
 			else
