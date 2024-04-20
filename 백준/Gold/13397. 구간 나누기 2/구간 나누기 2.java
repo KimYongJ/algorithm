45862a5dda7a89bde519a971236ad0691914785a
YongJu Kim <kyjdummy@gmail.com>
2024-04-21 01:30:51 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 136 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -1,21 +1,20 @@
 // https://github.com/kimyongj/algorithm
 
 class Main{
-	static final int MAX_VALUE = 10_001;
-	static int N, M, num[], left , mid, right, min, max, cnt;
+	static int N, M, num[], left , mid, right, ans, min, max, cnt;
 	static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
 	public static boolean check(int mid) {
-		max = -MAX_VALUE;
-		min = MAX_VALUE;
+		max = num[0];
+		min = num[0];
 		cnt = 1;
-		for(int i=0; i<N; i++) 
+		for(int i=1; i<N; i++) 
 		{
-			min = Math.min(min, num[i]);
-			max = Math.max(max, num[i]);
+			if(min > num[i]) min = num[i];
+			if(max < num[i]) max = num[i];
 			if(Math.abs(max - min) > mid) 
 			{
 				cnt ++;
@@ -24,22 +23,29 @@ class Main{
 		}
 		return cnt <= M;
 	}
-	public static void main(String[] args)throws Exception{
+	public static void main(String[] args)throws Exception{ 
 		N 	= read();
 		M 	= read();
 		num = new int[N];
 
-		for(int i=0; i<N; i++)
+		for(int i=0; i<N; i++) {
 			num[i] = read();
+			if(right < num[i])
+				right = num[i];
+		}
 		
-		right = MAX_VALUE;
 		
-		while(left < right) 
+		while(left <= right) 
 		{
 			mid = (left + right)/2;
-			if(check(mid))	right	= mid;
-			else			left	= mid + 1;
+			if(check(mid))	{
+				ans	  = mid;
+				right = mid-1;
+			}
+			else{
+				left = mid + 1;
+			}
 		}
-		System.out.print(right);
+		System.out.print(ans);
 	}
 }
