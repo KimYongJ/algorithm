af192c83721df6eccdd764cd3dacdccc071fcab6
YongJu Kim <kyjdummy@gmail.com>
2024-04-21 13:49:30 +0900

    [Gold IV] Title: 좋다, Time: 172 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,30 +1,25 @@
 // https://github.com/kimyongj/algorithm
 import java.util.Arrays;
 class Main{
-	
-	
-    static int read() throws Exception {
-        int n = 0;
-        boolean isNegative = false;
-        while (true) {
-            int input = System.in.read();
-            if (input <= 32) {
-                return isNegative ? n * -1 : n;
-            } else if (input == '-')
-                isNegative = true;
-            else
-                n = (n << 3) + (n << 1) + (input & 15);
-        }
-    }
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
+		int left, right, num, cnt=0;
 		int N	  = read();
 		int arr[] = new int[N];
-		int left, right, num, cnt=0;
+		
 		for(int i=0; i<N; i++)
 			arr[i] = read();
 		
 		Arrays.sort(arr);
 		N--;
+		
 		for(int i=0; i<=N; i++)
 		{
 			left  = 0;
