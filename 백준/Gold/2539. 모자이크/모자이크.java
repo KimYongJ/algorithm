64dab1a117aa92246f0d3f03d25351a9e0c79383
YongJu Kim <kyjdummy@gmail.com>
2024-04-22 13:59:23 +0900

    [Gold IV] Title: 모자이크, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Gold/2539. 모자이크/모자이크.java
+++ 백준/Gold/2539. 모자이크/모자이크.java
@@ -7,38 +7,39 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int Y, X, M, paper, maxY, maxX, xrr[], left, right, mid, ans;
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static boolean check(int size) {
 		int cnt = 0, hide = 0;
-		for(int x : xrr) {
+		for(int x : xrr)
 			if(hide < x) {
 				hide = x + size - 1;
 				cnt++;
 			}
-		}
 		return cnt <= paper;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Y 		= Integer.parseInt(st.nextToken());
-		X 		= Integer.parseInt(st.nextToken());
-		paper 	= Integer.parseInt(br.readLine());
-		M 		= Integer.parseInt(br.readLine());
+		Y 		= read();
+		X 		= read();
+		paper 	= read();
+		M 		= read();
 		xrr		= new int[M];
 		int y, x;
 		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			y = Integer.parseInt(st.nextToken());
-			x = Integer.parseInt(st.nextToken());
-			maxY = Math.max(maxY, y);
-			maxX = Math.max(maxX, x);
-			xrr[i] = x;
+			y 	 	= read();
+			x	 	= read();
+			maxY 	= Math.max(maxY, y);
+			maxX 	= Math.max(maxX, x);
+			xrr[i] 	= x;
 		}
 		
 		Arrays.sort(xrr);
 		
-		left = maxY;
-		right = 1_000_002;
+		left 	= maxY;
+		right 	= 1_000_002;
 		while(left <= right) {
 			mid = (left + right) / 2;
 			if(check(mid)) {
