2547a23ff11b59a691bd0b2148391130baa6d1ce
YongJu Kim <kyjdummy@gmail.com>
2024-04-23 16:23:12 +0900

    [Gold V] Title: ABCDE, Time: 236 ms, Memory: 20120 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,15 +1,15 @@
 // https://github.com/kimyongj/algorithm
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N, M;
 	static ArrayList<Integer>[] adlist;
 	static boolean visit[];
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void DFS(int idx, int cnt) {
 		if(cnt == 5) {
 			System.out.println(1);
@@ -24,10 +24,8 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());	// 사람수
-		M 		= Integer.parseInt(st.nextToken());	// 친구 관계 수
+		N 		= read();	// 사람수
+		M 		= read();	// 친구 관계 수
 		visit	= new boolean[N];
 		adlist 	= new ArrayList[N];
 		
@@ -37,9 +35,8 @@ class Main{
 		
 		int a,b;
 		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			a = read();
+			b = read();
 			adlist[a].add(b);
 			adlist[b].add(a);
 		}
