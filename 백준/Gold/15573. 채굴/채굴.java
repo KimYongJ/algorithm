3bca160d2acfcdbb871591871be5462e96e8153a
YongJu Kim <kyjdummy@gmail.com>
2024-04-24 18:01:11 +0900

    [Gold III] Title: 채굴, Time: 1404 ms, Memory: 82296 KB -BaekjoonHub

--- 백준/Gold/15573. 채굴/채굴.java
+++ 백준/Gold/15573. 채굴/채굴.java
@@ -1,79 +1,59 @@
 // https://github.com/kimyongj/algorithm
-import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 class Node{
-	int y, x;
-	Node(int y, int x){this.y=y; this.x=x;}
+	int y, x, z;
+	Node(int y, int x, int z){
+		this.y=y; this.x=x; this.z=z;
+	}
 }
 class Main{
 	static final int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int Y, X, K, map[][], nextY, nextX;
+	static int Y, X, K, map[][], nextY, nextX, ans, cnt;
 	static boolean visit[][];
-	static ArrayDeque<Node> q;
+	static PriorityQueue<Node> pq;
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
-	public static boolean BFS(int mid) { // mid 힘으로 몇개를 캘 수 있는지
-		int cnt = 0;
-		visit 	= new boolean[Y][X];
-		q 		= new ArrayDeque<>();
-		for(int y=0; y<Y; y++) {
-			if(map[y][0] <= mid) {
-				q.add(new Node(y,0));
-				visit[y][0] = true;
-				cnt++;
-			}
-			if(map[y][X-1] <= mid) {
-				q.add(new Node(y,X-1));
-				visit[y][X-1] = true;
-				cnt++;
-			} 
+	public static void main(String[] args)throws Exception{
+		Y 		= read();
+		X 		= read();
+		K		= read();
+		map 	= new int[Y][X];
+		visit	= new boolean[Y][X];
+		pq 		= new PriorityQueue<Node>((a,b)->a.z-b.z);
+		for(int y=0; y<Y; y++)
+			for(int x=0; x<X; x++) 
+				map[y][x] = read();
+		
+		for(int y=0; y<Y; y++) 
+		{
+			pq.add(new Node(y,	0,	map[y][0]));
+			pq.add(new Node(y,	X-1,map[y][X-1]));
 		}
-		for(int x=1; x<X-1; x++) {
-			if(map[0][x] <= mid) {
-				q.add(new Node(0,x));
-				visit[0][x] = true;
-				cnt++;
-			}
-		}
-		while(!q.isEmpty()) {
-			Node now = q.poll();
+		for(int x=1; x<X-1; x++)
+			pq.add(new Node(0,	x, 	map[0][x]));
+		
+		while(true){
+			Node now = pq.poll(); 
+			if(visit[now.y][now.x]) continue;;
+			visit[now.y][now.x]= true; 
+			ans = Math.max(ans, now.z);
+			if(++cnt >= K) 
+				break;
+			
 			for(int xy[] : dxy) {
 				nextY = now.y + xy[0];
 				nextX = now.x + xy[1];
 				if(nextY >=0 && nextX>=0 && nextY<Y && nextX<X 
-					&& map[nextY][nextX] <= mid && !visit[nextY][nextX]) {
-					if(++cnt >= K) 
-						return true;
-					visit[nextY][nextX] = true;
-					q.add(new Node(nextY,nextX));
+					&& !visit[nextY][nextX]) {
+					pq.add(new Node(nextY,nextX, map[nextY][nextX]));
 				}
 			}
 		}
-		return cnt >= K;
-	}
-	public static void main(String[] args)throws Exception{
-		Y = read();
-		X = read();
-		K = read();
-		map = new int[Y][X];
-		int left = 0, right = 0, mid;
-		for(int y=0; y<Y; y++)
-			for(int x=0; x<X; x++) 
-			{
-				map[y][x] = read();
-				right = Math.max(right, map[y][x]);
-			}
+		
 
-		while(left < right) 
-		{
-			mid = (left + right) / 2; // 채굴기의 힘
-			if(BFS(mid))
-				right = mid;
-			else
-				left = mid + 1;
-		}
-		System.out.println(right);
+		System.out.println(ans);
 	}
 }
