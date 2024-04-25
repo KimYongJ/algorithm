ecf1e88c84996952f5c38a9e4ac774810d5fd418
YongJu Kim <kyjdummy@gmail.com>
2024-04-25 16:50:37 +0900

    [Gold II] Title: 숫자구슬, Time: 120 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Gold/2613. 숫자구슬/숫자구슬.java
+++ 백준/Gold/2613. 숫자구슬/숫자구슬.java
@@ -1,18 +1,17 @@
 // https://github.com/kimyongj/algorithm
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int 	N, M, marble[], marbleCnt[], resultMarbleCnt[], 
 				left, right, mid, ans;
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static boolean check(int mid) { 
 		int cnt = 1; // 그룹의 수 
 		int sum = 0; // 각 그룹당 더해진 수 mid보다 크면 안된다.
 		marbleCnt = new int[M]; // 각 그룹에 속한 구슬의 개수
-		
 		for(int i=0; i<N; i++) {
 			sum += marble[i];
 			if(sum > mid || ( N-i == M-cnt )) {
@@ -23,29 +22,33 @@ class Main{
 				return false;
 			marbleCnt[cnt-1]++;
 		}
-		return cnt <= M;
+		return cnt == M;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st 	= new StringTokenizer(br.readLine());
-		N 			= Integer.parseInt(st.nextToken());
-		M 			= Integer.parseInt(st.nextToken());
-		marble		= new int[N];
-		marbleCnt	= new int[M];
+		N 				= read();
+		M 				= read();
+		marble			= new int[N];
+		marbleCnt		= new int[M];
 		resultMarbleCnt = new int[M];
-		st = new StringTokenizer(br.readLine());
-		for(int n=0; n<N; n++) {
-			marble[n] = Integer.parseInt(st.nextToken());
+
+		for(int n=0; n<N; n++) 
+		{
+			marble[n] = read();
 			left = Math.max(marble[n], left);
 		}
+		
 		right = 30_001;
-		while( left <= right ) {
+		while( left <= right ) 
+		{
 			mid = (left + right) / 2; // mid는 각 그룹당 나올 수 있는 합의 상한이다.
-			if(check(mid)) {
+			if(check(mid)) 
+			{
 				ans = mid;
 				right = mid - 1;
 				resultMarbleCnt = marbleCnt.clone();
-			}else {
+			}
+			else 
+			{
 				left = mid + 1;
 			}
 		}
