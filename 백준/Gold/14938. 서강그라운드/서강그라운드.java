1d667b0ad3ec2c5357c9de3eb840b99d8c720608
YongJu Kim <kyjdummy@gmail.com>
2024-04-26 23:14:05 +0900

    [Gold IV] Title: 서강그라운드, Time: 144 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -1,58 +1,56 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
-	static final int INF = 3_001;
-	static int N, M, R, item[], map[][];
+    private static int read() throws Exception {
+        int c, N = System.in.read() - 48;
+        while ((c = System.in.read()) > 32) N = 10 * N + c - 48;
+        return N;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken()); // 지역 개수
-		M = Integer.parseInt(st.nextToken()); // 예으니 수색범위
-		R = Integer.parseInt(st.nextToken()); // 길의 개수
-		item = new int[N]; // 지역당 아이템 개수
-		map = new int[N][N]; // 맵
-		st = new StringTokenizer(br.readLine());
+        final int INF = 3_001;
+		int N 		= read();         // 지역 개수
+		int M 		= read();         // 예으니 수색범위
+		int R 		= read();         // 길의 개수
+		int item[] 	= new int[N];     // 지역당 아이템 개수
+		int map[][]	= new int[N][N];  // 맵
 		for(int i=0; i<N; i++)
-			item[i] = Integer.parseInt(st.nextToken());
+			item[i] = read();
 		
 		for(int i=0; i<N; i++)
-			for(int j=0; j<N; j++) {
+			for(int j=0; j<N; j++) 
+			{
 				if(i==j) continue;
 				map[i][j] = INF;
 			}
 		
 		int a,b,c;
-		for(int i=0; i<R; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken())-1;
-			b = Integer.parseInt(st.nextToken())-1;
-			c = Integer.parseInt(st.nextToken());
+		for(int i=0; i<R; i++) 
+		{
+			a = read()-1;
+			b = read()-1;
+			c = read();
 			map[a][b] = c;
 			map[b][a] = c;
 		}
-		for(int k=0; k<N; k++) {
-			for(int i=0; i<N; i++) {
+		for(int k=0; k<N; k++)
+			for(int i=0; i<N; i++) 
+			{
 				if(k==i) continue;
-				for(int j=0; j<N; j++) {
+				for(int j=0; j<N; j++) 
+				{
 					if(i==k || j==k) continue;
-					if(map[i][j] > map[i][k]+map[k][j]) {
+					if(map[i][j] > map[i][k]+map[k][j])
 						map[i][j] = map[i][k]+map[k][j];
-					}
 				}
 			}
-		}
+		
 		int result = 0, sum;
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<N; i++) 
+		{
 			sum = 0;
-			for(int j=0; j<N; j++) {
-				if(map[i][j] <= M) {
+			for(int j=0; j<N; j++)
+				if(map[i][j] <= M)
 					sum += item[j];
-				}
-			}
 			result = Math.max(result, sum);
 		}
 		System.out.println(result);
