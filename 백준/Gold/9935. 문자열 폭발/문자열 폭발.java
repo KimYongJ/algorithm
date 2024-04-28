bbdb563e875cdb13ecf1a707258cf52476095598
YongJu Kim <kyjdummy@gmail.com>
2024-04-28 20:22:29 +0900

    [Gold IV] Title: 문자열 폭발, Time: 492 ms, Memory: 48964 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,40 +1,62 @@
 // https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
+class Stack{
+	char c;
+	int size=0;
+	Stack child;
+	public Stack() {}
+	public Stack(char c, Stack child) {
+		this.c=c; this.child=child;
+		if(child != null)
+			this.size=child.size + 1;
+	}
+	public char get(int idx) {
+		if(idx==0)return c;
+		int idx1 = 0;
+		for(Stack now=child; now!=null; now= now.child)
+			if(++idx1 == idx)
+				return now.c;
+		return ' ';
+	}
+	public boolean isEmpty() {return size == 0;}
+	public Stack delete() {return child;}
+	public char pop() {return c;}
+}
 class Main{
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Stack<Character> stack = new Stack<>();
+		Stack stack = new Stack();
 		String base = br.readLine();
 		char[] key = br.readLine().toCharArray();
 		int keySize = key.length;
-		int stSize =0;
-		for(char c : base.toCharArray()) {
-			stack.add(c);
-			stSize = stack.size();
-			if(stack.size() >= keySize) {
+		for(char c : base.toCharArray()) 
+		{
+			stack = new Stack(c, stack);
+			if(stack.size >= keySize) 
+			{
 				boolean flag = true;
 				
-				for(int i=0; i<keySize; i++) {
-					if(key[i] != stack.get(stSize-keySize+i)) {
+				for(int i=0; i<keySize; i++) 
+					if(key[i] != stack.get(keySize-i-1)) 
+					{
 						flag = false;
 						break;
 					}
-				}
 				
-				if(flag) {
+				
+				if(flag)
 					for(int i=0; i<keySize; i++)
-						stack.pop();
-				}
+						stack = stack.delete();
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		while(!stack.isEmpty()) {
 			sb.append(stack.pop());
+			stack = stack.delete();
 		}
 		String result = sb.reverse().toString();
 		if("".equals(result))
