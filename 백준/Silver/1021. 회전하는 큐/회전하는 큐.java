f56e66debea75288397cbb29bedf493df5322671
YongJu Kim <kyjdummy@gmail.com>
2024-04-28 17:45:43 +0900

    [Silver III] Title: 회전하는 큐, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,9 +1,5 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Queue{
 	int lastIdx;
 	int arr[];
@@ -16,7 +12,6 @@ class Queue{
 		return arr[++lastIdx] = num;
 	}
 	public int getCnt(int num) {
-		int cnt = 0;
 		int idx = 0;
 		for(; idx<=lastIdx; idx++) {
 			if(arr[idx] == num)
@@ -24,33 +19,33 @@ class Queue{
 		}
 		int newArr[] = new int[MAX];
 		int newIdx = 0;
-		for(int i=idx+1; i<=lastIdx; i++) {
+		for(int i=idx+1; i<=lastIdx; i++)
 			newArr[newIdx++] = arr[i]; 
-		}
-		for(int i=0; i<idx; i++) {
+		
+		for(int i=0; i<idx; i++)
 			newArr[newIdx++] = arr[i];
-		}
+		
 		arr = newArr;
 		return Math.min(idx, lastIdx-- -idx+1);
 	}
 }
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		Queue q = new Queue();
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int N 	= read();
+		int M 	= read();
+		int cnt = 0;
 		
 		for(int i=1; i<=N; i++)
 			q.add(i);
 		
-		int cnt = 0;
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			cnt += q.getCnt(Integer.parseInt(st.nextToken()));
-		}
+		for(int i=0; i<M; i++)
+			cnt += q.getCnt(read());
 		
 		System.out.println(cnt);
 	}
