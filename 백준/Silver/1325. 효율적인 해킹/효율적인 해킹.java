21602f7dd304d3319ee1fd90ab7203c4369578a8
YongJu Kim <kyjdummy@gmail.com>
2024-04-28 23:43:32 +0900

    [Silver I] Title: 효율적인 해킹, Time: 7584 ms, Memory: 124916 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,56 +1,47 @@
 // https://github.com/KimYongJ/algorithm
-
 import java.util.ArrayList;
-
-class Node{
-	int node;
-	Node child;
-	Node(int node, Node child){this.node = node; this.child = child;}
-}
-
 class Main{
 	static int N, M, max, arr[];
-	static boolean visit[];
 	static ArrayList<Integer> adlist[];
+	static boolean visit[];
 	static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
-	public static void DFS(int node) {
-		visit[node] = true;
-		for(int next : adlist[node])
-			if(!visit[next]) 
-			{
-				arr[next]++;
-				DFS(next);				
+	public static int DFS(int node) {
+		int sum = 1;
+		for(int next : adlist[node]) {
+			if(!visit[next]) {
+				visit[next] = true;
+				sum += DFS(next);
 			}
+		}
+		return sum;
 	}
 	public static void main(String[] args)throws Exception{
 		N 		= read();
 		M 		= read();
 		arr 	= new int[N+1];
 		adlist 	= new ArrayList[N+1];
-		
-		for(int i=0; i<=N; i++) adlist[i] = new ArrayList<>();
+		for(int i=0; i<=N; i++) 
+			adlist[i] = new ArrayList<>();
 		int a,b;
 		for(int i=0; i<M; i++) 
 		{
 			a = read();
 			b = read();
-			adlist[a].add(b);
+			adlist[b].add(a);
 		}
 		
 		
 		for(int i=1; i<=N; i++) {
-			visit = new boolean[N+1];
-			DFS(i);
-		}
-		for(int i=1; i<=N; i++) {
+			visit 		= new boolean[N+1];
+			visit[i] 	= true;
+			arr[i] 		= DFS(i);
 			if(arr[i] > max)
 				max = arr[i];
 		}
-		
 		StringBuilder sb = new StringBuilder();
 		for(int i=1; i<=N; i++)
 			if(max == arr[i])
