559c8abc6f08c2bf01449c58a70135482e9c17dc
YongJu Kim <kyjdummy@gmail.com>
2024-04-29 20:45:38 +0900

    [Gold IV] Title: 팀배분, Time: 292 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/1953. 팀배분/팀배분.java
+++ 백준/Gold/1953. 팀배분/팀배분.java
@@ -2,65 +2,91 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 class Main{
 	
 	static int N;
 	static StringBuilder sb;
-	static ArrayList<Integer> map[], blue,white;
-	public static void set(ArrayList<Integer> list) {
-		Collections.sort(list);
-		sb.append(list.size()).append('\n');
-		for(int i=0; i<list.size(); i++)
-			sb.append(list.get(i)).append(' ');
-		sb.append('\n');
+	static boolean[] blue, white;
+	static ArrayList<Integer> map[];
+	public static void print() {
+		StringBuilder blueSb 	= new StringBuilder();
+		StringBuilder whiteSb 	= new StringBuilder();
+		int blueCnt=0, 
+			whiteCnt=0;
+		for(int i=1; i<N; i++) 
+		{
+			if(blue[i]) 
+			{
+				blueCnt++;
+				blueSb.append(i).append(' ');
+			}
+			if(white[i]) 
+			{
+				whiteCnt++;
+				whiteSb.append(i).append(' ');
+			}
+		}
+		StringBuilder result = new StringBuilder();
+		result.append(blueCnt).append('\n').append(blueSb.toString()).append('\n')
+			.append(whiteCnt).append('\n').append(whiteSb.toString());
+		System.out.println(result);
+		System.exit(0);
+	}
+	public static boolean check() {
+		boolean bflag = false, wflag = false;
+		for(int i=1; i<N; i++) 
+		{
+			if(blue[i])bflag 	= true;
+			if(white[i])wflag 	= true;
+			if(bflag && wflag) 
+				return true;
+		}
+		return false;
 	}
 	public static void DFS(int node) {
-		if(node == N) {
-			if(blue.size() !=0 && white.size() !=0) {
-				set(blue);
-				set(white);
-				System.out.print(sb);
-				System.exit(0);
-			}
+		if(node == N) 
+		{
+			if(check())
+				print();
 			return;
 		}
 		boolean flag = true;
 		for(int hate : map[node]) {
-			if(blue.contains(hate)) // blue에 싫어 하는 사람이 없다면
+			if(blue[hate]) // blue에 싫어 하는 사람이 있다면
 			{
 				flag = false;
 				break;
 			}
 		}
 		if(flag) {
-			blue.add(node);
+			blue[node] = true;
 			DFS(node+1);
-			blue.remove(blue.size()-1);
+			blue[node] = false;
 		}
 		flag = true;
 		for(int hate : map[node]) {
-			if(white.contains(hate)) // white에 싫어 하는 사람이 없다면
+			if(white[hate]) // white에 싫어 하는 사람이 있다면
 			{
 				flag = false;
 				break;
 			}
 		}
 		if(flag) {
-			white.add(node);
+			white[node] = true;
 			DFS(node+1);
-			white.remove(white.size()-1);
+			white[node] = false;
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N 		= Integer.parseInt(br.readLine())+1;
 		map 	= new ArrayList[N];
-		blue 	= new ArrayList<>();
-		white 	= new ArrayList<>();
+		blue 	= new boolean[N];
+		white 	= new boolean[N];
 		sb 		= new StringBuilder();
+		
 		for(int i=0; i<N; i++)
 			map[i] = new ArrayList<>();
 		
