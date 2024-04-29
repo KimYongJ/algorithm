0b4f1950932c3a33f98cb30080efa4036833cc3b
YongJu Kim <kyjdummy@gmail.com>
2024-04-29 17:05:21 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 172 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.java
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.java
@@ -1,27 +1,30 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
-	static int N, result[];
-	static int adlist[][];
+	static int N, result[], adlist[][];
 	static boolean visit[][];
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void print() {
 		StringBuilder sb = new StringBuilder();
 		for(int i=1; i<N; i++)
 			sb.append(result[i]).append('\n');
 		System.out.print(sb);
+		System.exit(0);
 	}
 	public static void DFS(int depth) {
-		if(depth == N) {
+		if(depth == N) 
 			print();
-			System.exit(0);
-		}
-		for(int next : adlist[depth]) {
-			if(next == 0) break;
-			if(!visit[depth][next] && next != result[depth-1]) {
+		
+		for(int next : adlist[depth])
+		{
+			if(next == 0) 
+				break;
+			if(!visit[depth][next] && next != result[depth-1]) 
+			{
 				visit[depth][next] = true;
 				result[depth] = next;
 				DFS(depth + 1);
@@ -29,24 +32,17 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken())+1; 	// 날수 = 깊이
-		result = new int[N];					// 날수(깊이) 에 따라 줄 떡
-		adlist = new int[N][9];					// 해당 날에 가져갈 떡 종류
-		visit  = new boolean[N][10];				// 방문 체크
+		N 		= read()+1; 			// 날수 = 깊이
+		result 	= new int[N];			// 날수(깊이) 에 따라 줄 떡
+		adlist 	= new int[N][9];		// 해당 날에 가져갈 떡 종류
+		visit  	= new boolean[N][10];	// 방문 체크
 		for(int i=1; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			int J = Integer.parseInt(st.nextToken());
+			int J = read();
 			for(int j=0; j<J; j++) 
-			{
-				adlist[i][j] = Integer.parseInt(st.nextToken());
-			}
+				adlist[i][j] = read();
 		}
-		
 		DFS(1);
-		
 		System.out.println(-1);
 	}
 }
