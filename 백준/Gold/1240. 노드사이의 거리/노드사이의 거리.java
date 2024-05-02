83808b44550dd0a11c94d3abb32c0a5b2ca2dde2
YongJu Kim <kyjdummy@gmail.com>
2024-05-02 16:23:32 +0900

    [Gold V] Title: 노드사이의 거리, Time: 176 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -2,50 +2,59 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
+class Node{
+	int node, dist;
+	Node before;
+	Node(int node, int dist, Node before){
+		this.node = node;
+		this.dist = dist;
+		this.before = before;
+	}
+}
 class Main{
-	static final long MAX = 2000000000;
-	static long N, M, map[][];
+	static int N, M, start, end, dist;
+	static Node adNode[];
+	static boolean visit[];
+	public static void DFS(int node, int sum) {
+		if(dist != 0) return;
+		if(node == end) {
+			dist = sum;
+			return;
+		}
+		for(Node now=adNode[node]; now!=null; now=now.before) {
+			if(!visit[now.node]) {
+				visit[now.node] = true;
+				DFS(now.node, sum + now.dist);
+			}
+		}
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new long[(int) (N+1)][(int) (N+1)];
-	
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N;j++) {
-				if(i==j) continue;
-				map[i][j] = MAX;
-			}
-		}
+		adNode = new Node[N+1];
 		int a,b,c;
 		for(int n=1; n<N; n++) {
 			st = new StringTokenizer(br.readLine());
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			c = Integer.parseInt(st.nextToken());
-			if(map[a][b] > c)
-				map[a][b] = map[b][a] = c;
+			adNode[a] = new Node(b,c,adNode[a]);
+			adNode[b] = new Node(a,c,adNode[b]);
 		}
-		for(int k=1; k<=N; k++) {
-			for(int i=1; i<=N; i++) {
-				if(k==i) continue;
-				for(int j=1; j<=N; j++) {
-					if(i==j || j==k) continue;
-					if(map[i][j] > map[i][k] + map[k][j]) {
-						map[i][j] = map[j][i] = map[i][k] + map[k][j];
-					}
-				}
-			}
-		}
-
 		StringBuilder sb = new StringBuilder();
 		for(int m=0; m<M; m++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			sb.append(map[a][b]).append('\n');
+			st 		= new StringTokenizer(br.readLine());
+			start 	= Integer.parseInt(st.nextToken());
+			end 	= Integer.parseInt(st.nextToken());
+			dist	= 0;
+			visit 	= new boolean[N+1];
+			visit[start] = true;
+			DFS(start,0);
+			sb.append(dist).append('\n');
 		}
 		System.out.println(sb);
 	}
