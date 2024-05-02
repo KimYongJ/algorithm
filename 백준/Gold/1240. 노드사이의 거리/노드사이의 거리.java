c5aab261e7c6feab76577534a9b6e5a917f40099
YongJu Kim <kyjdummy@gmail.com>
2024-05-02 16:26:09 +0900

    [Gold V] Title: 노드사이의 거리, Time: 148 ms, Memory: 15016 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,8 +1,4 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node, dist;
 	Node before;
@@ -16,43 +12,46 @@ class Main{
 	static int N, M, start, end, dist;
 	static Node adNode[];
 	static boolean visit[];
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void DFS(int node, int sum) {
 		if(dist != 0) return;
-		if(node == end) {
+		if(node == end) 
+		{
 			dist = sum;
 			return;
 		}
-		for(Node now=adNode[node]; now!=null; now=now.before) {
-			if(!visit[now.node]) {
+		for(Node now=adNode[node]; now!=null; now=now.before) 
+		{
+			if(!visit[now.node]) 
+			{
 				visit[now.node] = true;
 				DFS(now.node, sum + now.dist);
 			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		adNode = new Node[N+1];
+		N 		= read();
+		M 		= read();
+		adNode 	= new Node[N+1];
 		int a,b,c;
 		for(int n=1; n<N; n++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
-			adNode[a] = new Node(b,c,adNode[a]);
-			adNode[b] = new Node(a,c,adNode[b]);
+			a 			= read();
+			b 			= read();
+			c 			= read();
+			adNode[a] 	= new Node(b,c,adNode[a]); // 양방향 연결
+			adNode[b] 	= new Node(a,c,adNode[b]);
 		}
 		StringBuilder sb = new StringBuilder();
-		for(int m=0; m<M; m++) {
-			st 		= new StringTokenizer(br.readLine());
-			start 	= Integer.parseInt(st.nextToken());
-			end 	= Integer.parseInt(st.nextToken());
-			dist	= 0;
-			visit 	= new boolean[N+1];
-			visit[start] = true;
+		for(int m=0; m<M; m++) { // 입력 받을 때마다 DFS를 돌아도 충분
+			start 			= read();
+			end 			= read();
+			dist			= 0;
+			visit 			= new boolean[N+1];
+			visit[start] 	= true;
 			DFS(start,0);
 			sb.append(dist).append('\n');
 		}
