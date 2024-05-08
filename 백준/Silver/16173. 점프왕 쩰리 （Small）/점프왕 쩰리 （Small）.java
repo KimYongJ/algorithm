a49e46a205b67130f95c22f921273b48fd1a6f45
YongJu Kim <kyjdummy@gmail.com>
2024-05-08 17:58:27 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 124 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -2,7 +2,6 @@
 
 class Main{
 	static int N, map[][];
-	static boolean visit[][];
 	public static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		boolean isNegative = n == 13;
@@ -11,21 +10,18 @@ class Main{
 		if (c == 13) System.in.read();
 		return isNegative ? ~n + 1 : n;
 	}
-
 	public static boolean DFS(int y, int x) {
-		if(y >= N || x >= N || visit[y][x]) return false;
-		visit[y][x] = true;
-		if(map[y][x] == -1)
-			return true;
+		if(y >= N || x >= N || map[y][x] == 0) return false;
+		if(map[y][x] == -1)	return true;
 		return DFS(y + map[y][x], x) || DFS(y, x + map[y][x]);
 	}
 	public static void main(String[] args)throws Exception{
 		N 		= read();
 		map 	= new int[N][N];
-		visit 	= new boolean[N][N];
 		for(int y=0; y<N; y++)
 			for(int x=0; x<N; x++)
 				map[y][x] = read();
+		
 		System.out.println(DFS(0,0) ? "HaruHaru" : "Hing");
 	}
 }
