13b3b622fb3596672ca56ebc62c874be8e507e52
YongJu Kim <kyjdummy@gmail.com>
2024-05-08 17:47:42 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 152 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -1,42 +1,37 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
-	static int dxy[][] = {{0,1},{1,0}};
-	static int N, map[][], nextY, nextX;
+	static int N, map[][];
 	static boolean visit[][];
-	public static void endCondition() {
-		if(map[nextY][nextX]==-1) {
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
+	public static void endCondition(int y, int x) {
+		if(map[y][x]==-1) {
 			System.out.println("HaruHaru");
 			System.exit(0);
 		}
 	}
 	public static void DFS(int y, int x) {
-		for(int xy[] : dxy) {
-			nextY = y + (xy[0] * map[y][x]);
-			nextX = x + (xy[1] * map[y][x]);
-			if(nextY<N && nextX<N && !visit[nextY][nextX]) {
-				endCondition();
-				visit[nextY][nextX] = true;
-				DFS(nextY, nextX);
-			}
-		}
+		if(y >= N || x >= N || visit[y][x]) return;
+		visit[y][x] = true;
+		endCondition(y,x);
+		DFS(y + map[y][x], x);
+		DFS(y, x + map[y][x]);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
+		N 		= read();
 		map 	= new int[N][N];
 		visit 	= new boolean[N][N];
-		for(int y=0; y<N; y++) {
-			st = new StringTokenizer(br.readLine());
-			for(int x=0; x<N; x++) {
-				map[y][x] = Integer.parseInt(st.nextToken());
-			}
-		}
-		visit[0][0] = true;
+		for(int y=0; y<N; y++)
+			for(int x=0; x<N; x++)
+				map[y][x] = read();
+
 		DFS(0,0);
 		System.out.println("Hing");
 	}
