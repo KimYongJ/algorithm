b517e4a28f5c7717e2eba83083753d13599bcf9e
YongJu Kim <kyjdummy@gmail.com>
2024-05-10 15:24:59 +0900

    [Gold IV] Title: 구슬 찾기, Time: 156 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
@@ -1,20 +1,20 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node;
 	Node next;
 	Node(int node, Node next){
-		this.node = node;
-		this.next = next;
+		this.node = node; this.next = next;
 	}
 }
 class Main{
 	
 	static int N, M, cnt, result;
 	static boolean visit[];
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void DFS(int node, Node[] adNode) {
 		if(cnt >= M) return;
 		for(Node now=adNode[node]; now!=null && cnt < M; now=now.next) 
@@ -28,30 +28,30 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		N = read();
+		M = read();
 		
 		Node[] forward = new Node[N+1];
 		Node[] reverse = new Node[N+1];
 		int a,b;
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+		for(int i=0; i<M; i++) 
+		{
+			a = read();
+			b = read();
 			forward[a] = new Node(b, forward[a]); // 정방향
 			reverse[b] = new Node(a, reverse[b]); // 역방향
 		}
 		M = (N+1) / 2; // 중간값
-		for(int i=1; i<=N; i++) {
-			cnt = 0;
-			visit = new boolean[N+1];
-			visit[i] = true;
+		for(int i=1; i<=N; i++) 
+		{
+			cnt 		= 0;
+			visit 		= new boolean[N+1];
+			visit[i] 	= true;
 			DFS(i, forward);
-			if(cnt < M) {
-				cnt = 0;
-				visit = new boolean[N+1];
+			if(cnt < M) 
+			{
+				cnt 	 = 0;
+				visit 	 = new boolean[N+1];
 				visit[i] = true;
 				DFS(i, reverse);
 			}
