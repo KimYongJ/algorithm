23bbb5664a3efb60ff89ebd5e53772906723098b
YongJu Kim <kyjdummy@gmail.com>
2024-05-10 17:30:33 +0900

    [Gold IV] Title: 회사 문화 1, Time: 448 ms, Memory: 30604 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,8 +1,4 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node;
 	Node next;
@@ -13,34 +9,41 @@ class Main{
 	
 	static int N, M, x, y, dp[];
 	static Node adNode[];
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void DFS(int node) {
-		for(Node now=adNode[node]; now!=null; now=now.next) {
+		for(Node now=adNode[node]; now!=null; now=now.next) 
+		{
 			dp[now.node] += dp[node]; 
 			DFS(now.node);
 		}
 		
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		dp = new int[N+1];
-		adNode = new Node[N+1];
-		
-		st = new StringTokenizer(br.readLine());
-		st.nextToken();
-		for(int i=2; i<=N; i++) {
-			x = Integer.parseInt(st.nextToken());
-			adNode[x] = new Node(i, adNode[x]);
+		N 		= read();
+		M 		= read();
+		dp 		= new int[N+1];
+		adNode 	= new Node[N+1];
+		read();
+		for(int i=2; i<=N; i++) 
+		{
+			x 			= read();
+			adNode[x] 	= new Node(i, adNode[x]);
 		}
 		
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			dp[x] += y;
+		for(int i=0; i<M; i++) 
+		{
+			x 		= read();
+			y 		= read();
+			dp[x] 	+= y;
 		}
+		
 		DFS(1);
 		
 		StringBuilder sb = new StringBuilder();
