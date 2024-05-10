7fc90f2644696679984665e657ce4839e71fa207
YongJu Kim <kyjdummy@gmail.com>
2024-05-10 21:21:09 +0900

    [Gold IV] Title: Two Dots, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.java
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -4,22 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 class Main{
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int Y, X, sY, sX;
-	static char map[][];
+	static int Y, X;
+	static char base, map[][];
 	static boolean flag, visit[][];
-	public static void DFS(char base, int beforeY, int beforeX, int y, int x) {
+	public static void DFS(int beforeY, int beforeX, int y, int x) { 
 		for(int xy[] : dxy) {
 			int nextY = y + xy[0];
 			int nextX = x + xy[1];
-			if(nextY>=0 && nextX>=0 && nextY<Y && nextX<X && map[nextY][nextX] == base && !flag) {
-				if(!visit[nextY][nextX]) {
+			if(nextY>=0 && nextX>=0 && nextY<Y && nextX<X 
+					&& map[nextY][nextX] == base && !flag) 
+			{
+				if(!visit[nextY][nextX]) 
+				{
 					visit[nextY][nextX] = true;
-					DFS(base, y,x, nextY,nextX);
-					visit[nextY][nextX] = false;
-				}else if(beforeY != nextY && beforeX != nextX 
-						&& sY==nextY && sX==nextX) {
+					DFS(y,x, nextY,nextX);
+				}else if(beforeY != nextY || beforeX != nextX)
 					flag = true;
-				}
 			}
 		}
 	}
@@ -35,16 +35,16 @@ class Main{
 		for(int y=0; y<Y; y++)
 			map[y] = br.readLine().toCharArray();
 		Loop:
-		for(int y=0; y<Y; y++) {
-			for(int x=0; x<X; x++) {
-				visit[y][x] = true;
-				DFS(map[y][x], y,x,sY = y,sX = x);
-				visit[y][x] = false;
-				if(flag) 
-					break Loop;
-					
-			}
-		}
+		for(int y=0; y<Y; y++)
+			for(int x=0; x<X; x++)
+				if(!visit[y][x]) 
+				{
+					visit[y][x] = true;
+					base = map[y][x];
+					DFS(y,x,y,x);
+					if(flag) 
+						break Loop;
+				}
 		
 		System.out.print(flag ? "Yes" : "No");
 	}
