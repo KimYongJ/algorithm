8999582cc62ac7d3e1afc6e82b3b3b4f088a4b28
YongJu Kim <kyjdummy@gmail.com>
2024-05-11 19:49:28 +0900

    [Gold V] Title: MooTube (Silver), Time: 344 ms, Memory: 39604 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
@@ -1,60 +1,60 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node, dist;Node next;
-	Node(int node, int dist, Node next){
-		this.node=node;this.dist=dist;this.next=next;
-	}
+	Node(int node, int dist, Node next){this.node=node;this.dist=dist;this.next=next;}
 }
 class Main{
-	static final int MAX = 1_000_000_000;
-	static int K, cnt;
-	static Node[] adNode;
-	static boolean visit[];
+	
+	static final int	MAX = 1_000_000_000;
+	static int 			K, cnt;
+	static Node[] 		adNode;
+	static boolean 		visit[];
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void DFS(int node, int beforeDist) {
-		for(Node now=adNode[node]; now!=null; now=now.next) {
-			if(!visit[now.node]) {
+		for(Node now=adNode[node]; now!=null; now=now.next)
+			if(!visit[now.node]) 
+			{
 				visit[now.node] = true;
-				if(K <= Math.min(beforeDist, now.dist)) {
+				if(K <= Math.min(beforeDist, now.dist)) 
+				{
 					cnt ++;
 					DFS(now.node, now.dist);
 				}
 			}
-		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int Q = Integer.parseInt(st.nextToken());
-		adNode = new Node[N+1];
+		StringBuilder sb = new StringBuilder();
 		int a,b,c;
+		int N 			= read();
+		int Q 			= read();
+		adNode 			= new Node[N+1];
+		
 		for(int i=1; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
-			adNode[a] = new Node(b,c, adNode[a]);
-			adNode[b] = new Node(a,c, adNode[b]);
+			a 			= read();
+			b 			= read();
+			c 			= read();
+			adNode[a] 	= new Node(b,c, adNode[a]);
+			adNode[b] 	= new Node(a,c, adNode[b]);
 		}
-		
-		StringBuilder sb = new StringBuilder();
+
 		while(Q-->0) 
 		{
-			st = new StringTokenizer(br.readLine());
-			K = Integer.parseInt(st.nextToken());
-			a = Integer.parseInt(st.nextToken());
-			visit = new boolean[N+1];
-			visit[a] = true;
-			cnt = 0;
+			K 			= read();
+			a 			= read();
+			visit 		= new boolean[N+1];
+			visit[a] 	= true;
+			cnt 		= 0;
+			
 			DFS(a, MAX);
-			sb.append(cnt)
-				.append('\n');
+			
+			sb.append(cnt).append('\n');
 		}
 		System.out.print(sb);
 	}
