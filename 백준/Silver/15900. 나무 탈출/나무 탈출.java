14cd71126faad91348fec8d54c9495e91eb1543e
YongJu Kim <kyjdummy@gmail.com>
2024-05-12 14:52:54 +0900

    [Silver I] Title: 나무 탈출, Time: 624 ms, Memory: 94976 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -1,45 +1,46 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 class Main{
 	
-	static int N;
-	static List<List<Integer>> list = new ArrayList<>();
+	static int N, sum;
 	static boolean visit[];
-	static int result;
+	static ArrayList<Integer>[] list;
+    private static int read() throws Exception {
+        int c, N = System.in.read() - 48;
+        while ((c = System.in.read()) > 32) N = 10 * N + c - 48;
+        return N;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N 		= Integer.parseInt(br.readLine());
+		N 		= read();
+		list	= new ArrayList[N+1];
 		visit 	= new boolean[N+1];
 		for(int i=0; i<=N; i++)
-			list.add(new ArrayList<>());
+			list[i] = new ArrayList<>();
 		
 		int a,b;
 		for(int i=1; i<N; i++) 
 		{
-			st 			= new StringTokenizer(br.readLine());
-			a 			= Integer.parseInt(st.nextToken());
-			b 			= Integer.parseInt(st.nextToken());
-			list.get(a).add(b);
-			list.get(b).add(a);
+			a 			= read();
+			b 			= read();
+			list[a].add(b);
+			list[b].add(a);
 		}
-		dfs(0,1);
-		if(result%2 != 0) System.out.println("Yes");
-		else System.out.println("No");
+		
+		dfs(1, 0);
+		
+		System.out.println(sum%2==0 ? "No" : "Yes");
 	}
-    static void dfs(int depth, int v) {
-        for (int i = 0; i < list.get(v).size(); i++) {
-            int next = list.get(v).get(i);
-            if (!visit[next]) {
-                visit[next] = true;
-                dfs(depth + 1, next);
-            }
-        }
-
-        if (list.get(v).size() == 1) result += depth;
-    }
+	public static void dfs(int n, int d) {
+		for(int i=0; i<list[n].size(); i++) {
+			int next = list[n].get(i);
+			if(!visit[next]) 
+			{
+				visit[next] = true;
+				dfs(next,d+1);
+			}
+		}
+			
+		if(list[n].size() == 1)
+			sum += d;
+	}
 }
