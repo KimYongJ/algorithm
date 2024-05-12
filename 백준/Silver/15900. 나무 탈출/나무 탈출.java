2ccd5ff42424968a65193dabec039888caa24236
YongJu Kim <kyjdummy@gmail.com>
2024-05-12 14:25:17 +0900

    [Silver I] Title: 나무 탈출, Time: 924 ms, Memory: 197244 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -1,48 +1,46 @@
-//https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-class Node{
-	int node; Node next;
-	Node(int node, Node next){this.node=node;this.next=next;}
-}
-class Main{
-	
-	static int N, sum;
-	static Node adNode[];
-	static boolean visit[];
-    private static int read() throws Exception {
-        int c, N = System.in.read() - 48;
-        while ((c = System.in.read()) > 32) N = 10 * N + c - 48;
-        return N;
+
+public class Main {
+    static int n;
+    static List<List<Integer>> list = new ArrayList<>();
+    static boolean[] visited;
+    static int result = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
+
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        dfs(0, 1);
+        if (result % 2 != 0) System.out.println("Yes");
+        else System.out.println("No");
+
     }
-	public static void DFS(int node, int depth) {
-		boolean flag = false;
-		for(Node now=adNode[node]; now!=null; now=now.next)
-			if(!visit[now.node]) 
-			{
-				visit[now.node] = true;
-				flag = true;
-				DFS(now.node,depth+1);
-			}
-		if(!flag)
-			sum += depth;
-	}
-	public static void main(String[] args)throws Exception{
-		N 		= read();
-		adNode 	= new Node[N+1];
-		visit 	= new boolean[N+1];
-		int a,b;
-		
-		for(int i=1; i<N; i++) 
-		{
-			a 			= read();
-			b 			= read();
-			adNode[a] 	= new Node(b, adNode[a]);
-			adNode[b] 	= new Node(a, adNode[b]);
-		}
-		visit[1] = true;
-		DFS(1, 0);
-		System.out.print(sum%2 == 0 ? "No" : "Yes");
-	}
+
+    static void dfs(int depth, int v) {
+        for (int i = 0; i < list.get(v).size(); i++) {
+            int next = list.get(v).get(i);
+            if (!visited[next]) {
+                visited[next] = true;
+                dfs(depth + 1, next);
+            }
+        }
+
+        if (list.get(v).size() == 1) result += depth;
+    }
+
 }
