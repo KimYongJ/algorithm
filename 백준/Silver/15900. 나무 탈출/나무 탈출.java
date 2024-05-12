d4c76eacc8de3ae0c1445193a0fcd6fd699400d7
YongJu Kim <kyjdummy@gmail.com>
2024-05-12 14:42:39 +0900

    [Silver I] Title: 나무 탈출, Time: 892 ms, Memory: 197708 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -1,46 +1,45 @@
+//https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-
-public class Main {
-    static int n;
-    static List<List<Integer>> list = new ArrayList<>();
-    static boolean[] visited;
-    static int result = 0;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1];
-        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
-
-        for (int i = 0; i < n - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
-            list.get(b).add(a);
-        }
-
-        dfs(0, 1);
-        if (result % 2 != 0) System.out.println("Yes");
-        else System.out.println("No");
-
-    }
-
+class Main{
+	
+	static int N;
+	static List<List<Integer>> list = new ArrayList<>();
+	static boolean visit[];
+	static int result;
+	public static void main(String[] args)throws Exception{
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N 		= Integer.parseInt(br.readLine());
+		visit 	= new boolean[N+1];
+		for(int i=0; i<=N; i++)
+			list.add(new ArrayList<>());
+		
+		int a,b;
+		for(int i=1; i<N; i++) 
+		{
+			st 			= new StringTokenizer(br.readLine());
+			a 			= Integer.parseInt(st.nextToken());
+			b 			= Integer.parseInt(st.nextToken());
+			list.get(a).add(b);
+			list.get(b).add(a);
+		}
+		dfs(0,1);
+		if(result%2 != 0) System.out.println("Yes");
+		else System.out.println("No");
+	}
     static void dfs(int depth, int v) {
         for (int i = 0; i < list.get(v).size(); i++) {
             int next = list.get(v).get(i);
-            if (!visited[next]) {
-                visited[next] = true;
+            if (!visit[next]) {
+                visit[next] = true;
                 dfs(depth + 1, next);
             }
         }
 
         if (list.get(v).size() == 1) result += depth;
     }
-
 }
