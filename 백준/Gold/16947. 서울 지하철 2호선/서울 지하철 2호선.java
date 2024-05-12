3b3a671c4825b29fa0f4f66f5771fe255180df74
YongJu Kim <kyjdummy@gmail.com>
2024-05-12 13:02:51 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 216 ms, Memory: 17436 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -1,9 +1,6 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Node{
 	int node; Node next;
@@ -19,8 +16,14 @@ class Main{
 	static Node[] adNode;
 	static ArrayDeque<Point> q;
 	static boolean cycleBase[], cycleCheck[];
-	public static boolean check(int base, int nowNode, int beforeNode) {
-		for(Node now=adNode[nowNode]; now!=null; now=now.next) {
+    private static int read() throws Exception {
+        int c, N = System.in.read() - 48;
+        while ((c = System.in.read()) > 32) N = 10 * N + c - 48;
+        return N;
+    }
+	public static boolean check(int base, int nowNode, int beforeNode) 
+	{
+		for(Node now=adNode[nowNode]; now!=null; now=now.next)
 			if(cycleCheck[now.node] && now.node == base && beforeNode !=now.node)
 				return true;
 			else if(!cycleCheck[now.node]) 
@@ -30,13 +33,11 @@ class Main{
 					return true;
 				cycleCheck[now.node] = false;
 			}
-		}
+		
 		return false;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N			= Integer.parseInt(br.readLine());
+		N			= read();
 		dist		= new int[N+1];
 		adNode		= new Node[N+1];
 		cycleBase	= new boolean[N+1];
@@ -46,9 +47,8 @@ class Main{
 		int a,b;
 		for(int i=0; i<N; i++)
 		{
-			st 			= new StringTokenizer(br.readLine());
-			a 			= Integer.parseInt(st.nextToken());
-			b 			= Integer.parseInt(st.nextToken());
+			a 			= read();
+			b 			= read();
 			adNode[a] 	= new Node(b, adNode[a]);
 			adNode[b] 	= new Node(a, adNode[b]);
 		}
@@ -57,8 +57,8 @@ class Main{
 		for(int i=1; i<=N; i++)
 			if(!cycleBase[i]) 
 			{
-				cycleCheck = Arrays.copyOf(cycleBase, N + 1);
-				cycleCheck[i] = true;
+				cycleCheck 		= Arrays.copyOf(cycleBase, N + 1);
+				cycleCheck[i] 	= true;
 				if(check(i,i,i)) 
 				{
 					for(int j=1; j<=N; j++) 
@@ -72,14 +72,15 @@ class Main{
 			}
 
 		// 큐에는 사이클이 담겨있고, 사이클에서 사이클이 아닌 노드들까지의 거리를 계산해 dist에 담는다.
-		while(!q.isEmpty()) {
+		while(!q.isEmpty()) 
+		{
 			Point now = q.poll();
-			for(Node next=adNode[now.node]; next!=null; next=next.next) {
-				if(!cycleBase[next.node]) {
+			for(Node next=adNode[now.node]; next!=null; next=next.next)
+				if(!cycleBase[next.node]) 
+				{
 					cycleBase[next.node] = true;
 					q.add(new Point(next.node, dist[next.node] = now.dist + 1));
 				}
-			}
 		}
 
 		StringBuilder sb = new StringBuilder();
