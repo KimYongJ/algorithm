7a2fc08882b0ab27fd2fe69a61f23dc3b94accb0
YongJu Kim <kyjdummy@gmail.com>
2024-05-13 16:28:27 +0900

    [Gold V] Title: 농장 관리, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -1,13 +1,16 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
-	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}, {-1,-1},{-1,1},{1,-1},{1,1}};
-	static int Y, X, result, nextY, nextX, map[][];
-	static boolean flag, visit[][];
+	static int 		dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}, {-1,-1},{-1,1},{1,-1},{1,1}};
+	static int 		Y, X, result, nextY, nextX, map[][];
+	static boolean 	flag, visit[][];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int base,int y, int x) {
 		for(int xy[] : dxy) 
 		{
@@ -23,22 +26,15 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Y 		= Integer.parseInt(st.nextToken());
-		X		= Integer.parseInt(st.nextToken());
+		Y 		= read();
+		X		= read();
 		map		= new int[Y+2][X+2];
 		visit 	= new boolean[Y+2][X+2];
 		for(int y=1; y<=Y; y++) 
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++) 
-			{
-				map[y][x] = Integer.parseInt(st.nextToken());
-				if(map[y][x]==0)
+				if((map[y][x] = read()) == 0)
 					visit[y][x] = true;
-			}
-		}
+			
 		
 		for(int y=1; y<=Y; y++)
 			for(int x=1; x<=X; x++)
