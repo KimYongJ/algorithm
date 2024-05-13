7c3826fe243700f543a099efea89ce98bd72b749
YongJu Kim <kyjdummy@gmail.com>
2024-05-13 09:59:59 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 164 ms, Memory: 17500 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -1,40 +1,35 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
-	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int Y, X, nextY, nextX;
-	static boolean map[][];
+	static int 		dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+	static int 		Y, X, nextY, nextX;
+	static boolean 	map[][];
 	public static void DFS(int y, int x) {
-		for(int xy[]:dxy) {
+		for(int xy[]:dxy) 
+		{
 			nextY = y + xy[0];
 			nextX = x + xy[1];
-			if(map[nextY][nextX]) {
+			if(map[nextY][nextX]) 
+			{
 				map[nextY][nextX] = false;
 				DFS(nextY,nextX);
 			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br	= new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder 	sb	= new StringBuilder();
-		StringTokenizer st;
-		int cnt, T = Integer.parseInt(br.readLine());
+		StringBuilder sb 	= new StringBuilder();
+		Reader in 			= new Reader();
+		int cnt, T 			= in.nextInt();
+		
 		while(T-->0)
 		{
 			cnt	= 0;
-			st 	= new StringTokenizer(br.readLine());
-			Y 	= Integer.parseInt(st.nextToken());
-			X	= Integer.parseInt(st.nextToken());
+			Y 	= in.nextInt();
+			X	= in.nextInt();
 			map = new boolean[Y+2][X+2];
 			
 			for(int y=1; y<=Y; y++) 
-			{
-				String str = br.readLine();
 				for(int x=1; x<=X; x++) 
-					map[y][x] = str.charAt(x-1) == '#';
-			}
+					map[y][x] = in.nextChar() == '#';
 			
 			for(int y=1; y<=Y; y++) 
 				for(int x=1; x<=X; x++) 
@@ -48,4 +43,33 @@ class Main{
 		}
 		System.out.println(sb);
 	}
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32);
+        return (char)c;
+    }
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+    boolean isNumber(byte c) {return 47 < c && c < 58;}
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
