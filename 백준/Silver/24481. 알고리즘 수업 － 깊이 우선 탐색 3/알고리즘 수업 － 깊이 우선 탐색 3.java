30456854da855fc83d481f374fa52c6255833d2f
YongJu Kim <kyjdummy@gmail.com>
2024-05-14 22:37:41 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 572 ms, Memory: 42376 KB -BaekjoonHub

--- 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
@@ -1,13 +1,16 @@
 // https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 class Main{
 	static int order[];
 	static boolean visit[];
 	static PriorityQueue<Integer>[] adList;
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int node, int depth) {
 		if(visit[node]) return;
 		
@@ -18,11 +21,9 @@ class Main{
 			DFS(adList[node].poll(), depth+1);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
+		int N = read();
+		int M = read();
+		int R = read();
 		int a,b;
 		
 		order	= new int[N+1];
@@ -37,9 +38,8 @@ class Main{
 		
 		for(int i=0; i<M; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			a = read();
+			b = read();
 			adList[a].add(b);
 			adList[b].add(a);
 		}
