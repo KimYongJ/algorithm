1a1ace3722ba4f29d80c52421dca22cebeb011a8
YongJu Kim <kyjdummy@gmail.com>
2024-05-14 22:48:16 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 556 ms, Memory: 42216 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.java
@@ -0,0 +1,56 @@
+// https://github.com/KimYongJ/algorithm
+import java.util.PriorityQueue;
+
+class Main{
+	static int order[];
+	static boolean visit[];
+	static PriorityQueue<Integer>[] adList;
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
+	public static void DFS(int node, int depth) {
+		if(visit[node]) return;
+		
+		visit[node] = true;
+		order[node] = depth;
+		
+        while (!adList[node].isEmpty())
+            DFS(adList[node].poll(), depth + 1);
+	}
+	public static void main(String[] args)throws Exception{
+		int N = read();
+		int M = read();
+		int R = read();
+		int a,b;
+		
+		order	= new int[N+1];
+		visit	= new boolean[N+1];
+		adList 	= new PriorityQueue[N+1];
+		
+		for(int i=0; i<=N; i++) 
+		{
+			order[i] 	= -1;
+			adList[i] 	= new PriorityQueue<Integer>((l,I)->I-l);
+		}
+		
+		for(int i=0; i<M; i++) 
+		{
+			a = read();
+			b = read();
+			adList[a].add(b);
+			adList[b].add(a);
+		}
+		
+		DFS(R, 0);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++)
+			sb.append(order[i]).append('\n');
+
+		System.out.print(sb);
+	}
+}
