87c519708915deefe174f36682bf1204bd8b9a1e
YongJu Kim <kyjdummy@gmail.com>
2024-05-14 22:15:02 +0900

    [Silver II] Title: 점프 점프, Time: 280 ms, Memory: 24692 KB -BaekjoonHub

--- 백준/Silver/14248. 점프 점프/점프 점프.java
+++ 백준/Silver/14248. 점프 점프/점프 점프.java
@@ -11,7 +11,9 @@ class Main{
 	public static void DFS(int now) {
 		if(now<1 || now>N || visit[now]) 
 			return;
+        
 		visit[now] = true;
+        
 		cnt++;
         
 		DFS(now + map[now]);
@@ -26,6 +28,7 @@ class Main{
 		map 	= new int[N+1];
 		visit 	= new boolean[N+1];
 		st 		= new StringTokenizer(br.readLine());
+        
 		for(int i=1; i<=N; i++)
 			map[i] = Integer.parseInt(st.nextToken());
 
