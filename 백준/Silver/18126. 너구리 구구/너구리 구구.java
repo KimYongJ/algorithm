0b02915798f0c0a83d47ead030852df3a475fae1
YongJu Kim <kyjdummy@gmail.com>
2024-05-14 23:35:38 +0900

    [Silver II] Title: 너구리 구구, Time: 140 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Silver/18126. 너구리 구구/너구리 구구.java
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.java
@@ -1,54 +1,51 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Node{
-	int node;
-	long dist;
-	Node next;
-	Node(int node, long dist, Node next){
-		this.node=node; this.dist=dist;
-		this.next=next;
-	}
-}
 class Main{
 	
-	static Node adNode[];
-	static boolean visit[];
+	static Node 	adNode[];
+	static boolean 	visit[];
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	// DFS는 자식 노드들의 거리가 가장 먼 것을 반환한다.
 	public static long DFS(int node, long dist) {
-		if(visit[node]) return 0;
+		if(visit[node])return 0;
+		
+		long sum 	= 0;
+		
 		visit[node] = true;
-		long sum = 0;
 		
-		for(Node now=adNode[node]; now!=null; now=now.next) {
+		for(Node now=adNode[node]; now!=null; now=now.next)
 			sum = Math.max(sum, DFS(now.node,now.dist));
-		}
-		
+
 		return dist + sum;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N 	= Integer.parseInt(st.nextToken());
+		int N 	= read();
 		adNode 	= new Node[N+1];
 		visit	= new boolean[N+1];
 		
 		int a,b,d;
 		for(int i=1; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			d = Integer.parseInt(st.nextToken());
-			adNode[a] = new Node(b,d, adNode[a]);
-			adNode[b] = new Node(a,d, adNode[b]);
+			a 			= read();
+			b 			= read();
+			d 			= read();
+			adNode[a] 	= new Node(b,d, adNode[a]);
+			adNode[b] 	= new Node(a,d, adNode[b]);
 		}
-		
 		System.out.print( DFS(1,0) );
-		
+	}
+}
+class Node{
+	int node;	long dist;	Node next;
+	Node(int node, long dist, Node next){
+		this.node=node; 
+		this.dist=dist;
+		this.next=next;
 	}
 }
