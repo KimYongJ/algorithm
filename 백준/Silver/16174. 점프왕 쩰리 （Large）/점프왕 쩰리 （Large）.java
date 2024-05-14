c7243de180d605d2f6bb47927dbd3a94502b0514
YongJu Kim <kyjdummy@gmail.com>
2024-05-14 15:16:30 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.java
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.java
@@ -1,13 +1,17 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
-	static final int dxy[][] = {{1,0},{0,1}};
-	static int N, nextY, nextX, map[][];
-	static boolean visit[][];
+	static final int 	dxy[][] = {{1,0},{0,1}};
+	static int 			N, nextY, nextX, map[][];
+	static boolean 		visit[][];
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static boolean DFS(int y, int x, int move) {
 		if(map[y][x] == -1)	return true;
 		if(visit[y][x])		return false;
@@ -25,17 +29,14 @@ class Main{
 		return false;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		visit = new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		System.out.print(DFS(0,0,map[0][0]) ? "HaruHaru" : "Hing");
+		N 		= read();
+		map 	= new int[N][N];
+		visit 	= new boolean[N][N];
+		
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++)
+				map[i][j] = read();
+
+		System.out.print( DFS( 0, 0, map[0][0] ) ? "HaruHaru" : "Hing");
 	}
 }
