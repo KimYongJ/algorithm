851c3dce0886e1b5c32bbe910b69421830056780
YongJu Kim <kyjdummy@gmail.com>
2024-05-15 21:51:53 +0900

    [Gold III] Title: Baaaaaaaaaduk2 (Easy), Time: 340 ms, Memory: 34332 KB -BaekjoonHub

--- 백준/Gold/16988. Baaaaaaaaaduk2 （Easy）/Baaaaaaaaaduk2 （Easy）.java
+++ 백준/Gold/16988. Baaaaaaaaaduk2 （Easy）/Baaaaaaaaaduk2 （Easy）.java
@@ -2,16 +2,21 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class Main{
 	static int 		dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
 	static int 		Y, X, sum, max, nextX, nextY, map[][];
 	static boolean 	flag, visit[][];
+	static ArrayList<int[]> zeroList;
 	public static int DFS(int y, int x) {
-		if(visit[y][x]) return 0;
-		visit[y][x] = true;
+		if(visit[y][x]) 
+			return 0;
+		
 		int cnt = 1;
+		
+		visit[y][x] = true;
 		for(int xy[] : dxy) 
 		{
 			nextY = y + xy[0];
@@ -26,12 +31,32 @@ class Main{
 		}
 		return cnt;
 	}
+	public static void findZeroPositions(int y, int x) {
+		if(visit[y][x]) return;
+		visit[y][x] = true;
+		for(int xy[] : dxy) 
+		{
+			nextY = y + xy[0];
+			nextX = x + xy[1];
+			if(nextY>=0 && nextX>=0 && nextY<Y && nextX<X && !visit[nextY][nextX]) 
+			{
+				if(map[nextY][nextX] == 0) {
+					zeroList.add(new int[] {nextY, nextX});
+					visit[nextY][nextX] = true;
+				}
+				else
+					findZeroPositions(nextY, nextX);
+			}
+		}
+	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		Y 		= Integer.parseInt(st.nextToken());
 		X 		= Integer.parseInt(st.nextToken());
 		map 	= new int[Y][X];
+		visit	= new boolean[Y][X];
+		zeroList= new ArrayList<>();
 		
 		for(int y=0; y<Y; y++) 
 		{
@@ -41,34 +66,42 @@ class Main{
 		}
 		
 		for(int y=0; y<Y; y++)
-			for(int x=0; x<X; x++) 
-				if(map[y][x] == 0) 
-				{
-					map[y][x] = 1;
-					for(int y1=y; y1<Y; y1++)
-						for(int x1=0; x1<X; x1++) 
-							if(map[y1][x1] == 0) 
-							{
-								map[y1][x1] = 1;
-								visit		= new boolean[Y][X];
-								sum			= 0;
-								for(int i=0; i<Y; i++) {
-									for(int j=0; j<X; j++) {
-										if(map[i][j] == 2 && !visit[i][j]) 
-										{
-											flag	= true;
-											int num = DFS(i,j);
-											if(flag) 
-												sum += num;
-										}
-									}
-								}
-								max = Math.max(max, sum);
-								map[y1][x1] = 0;
-							}
-					map[y][x] = 0;
-				}
-	
+			for(int x=0; x<X; x++)
+				if(!visit[y][x] && map[y][x] != 0)  // 맵을 돌면서 2의 좌표를 담는다.
+					findZeroPositions(y,x);
+
+		int size = zeroList.size();
+		int [] p1, p2;
+		
+		for(int i=0; i<size-1; i++) 
+		{
+			p1 = zeroList.get(i);
+			map[p1[0]][p1[1]] = 1;
+			
+			for(int j=i+1; j<size; j++) 
+			{
+				
+				p2 = zeroList.get(j);
+				map[p2[0]][p2[1]] = 1;
+				
+				visit		= new boolean[Y][X];
+				sum			= 0;
+				for(int y=0; y<Y; y++)
+					for(int x=0; x<X; x++) 
+						if(map[y][x] == 2 && !visit[y][x]) 
+						{
+							flag	= true;
+							int num = DFS(y,x);
+							if(flag) 
+								sum += num;
+						}
+				max = Math.max(max, sum);
+				
+				map[p2[0]][p2[1]] = 0;
+			}
+			map[p1[0]][p1[1]] = 0;
+		}
+
 		System.out.print(max);
 	}
 }
