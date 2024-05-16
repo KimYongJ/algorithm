6d17bad1085d8bd0bb10709cd7fe304828da585a
YongJu Kim <kyjdummy@gmail.com>
2024-05-16 15:03:59 +0900

    [Gold V] Title: 나의 인생에는 수학과 함께, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.java
@@ -0,0 +1,61 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	static final int 	dxy[][] = {{1,0},{0,1}};
+	static int 			N, MAX, MIN, nextY, nextX;
+	static char 		map[][];
+	public static int cal(int y, int x, int sum, char operator) {
+		int num = map[y][x]-'0';
+		switch(operator) 
+		{
+			case '*': return sum * num;
+			case '+': return sum + num;
+			case '-': return sum - num;
+		}
+		return num;
+	}
+	public static void DFS(int y, int x, int sum, char operator,boolean flag) {
+		if(flag) {
+			sum = cal(y, x, sum, operator);
+			if(y==N-1 && x==N-1) 
+			{
+				MAX = Math.max(MAX, sum);
+				MIN = Math.min(MIN, sum);
+				return;
+			}
+		}
+		for(int xy[] : dxy) {
+			nextY = y + xy[0];
+			nextX = x + xy[1];
+			if(nextY<N && nextX<N) {
+				DFS(nextY, nextX, sum, map[y][x] ,!flag);
+			}
+		}
+		
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N 	= Integer.parseInt(br.readLine());
+		map = new char[N][N];
+		MAX = Integer.MIN_VALUE;
+		MIN = Integer.MAX_VALUE;
+		
+		for(int y=0; y<N; y++) 
+		{
+			st = new StringTokenizer(br.readLine());
+			for(int x=0; x<N; x++)
+				map[y][x] = st.nextToken().charAt(0);
+		}
+		
+		DFS(0,0,0,'!',true);
+		
+		System.out.print(
+					new StringBuilder().append(MAX)
+					.append(' ').append(MIN).toString()
+				);
+	}
+}
