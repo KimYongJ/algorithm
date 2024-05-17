ec507ec12022d1340a04dc16040cb88ec2372e8b
YongJu Kim <kyjdummy@gmail.com>
2024-05-17 20:40:49 +0900

    [Gold IV] Title: 트리의 기둥과 가지, Time: 1072 ms, Memory: 33364 KB -BaekjoonHub

--- 백준/Gold/20924. 트리의 기둥과 가지/트리의 기둥과 가지.java
+++ 백준/Gold/20924. 트리의 기둥과 가지/트리의 기둥과 가지.java
@@ -1,9 +1,6 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 
 class Node{
 	int node, dist;	Node next;
@@ -12,10 +9,18 @@ class Node{
 }
 class Main{
 	
-	static int a, b, c, N, cnt, root, dist1, dist2;
-	static Node adNode[];
-	static boolean visit[];
-	public static int BFS() {
+	static int 		a, b, c, N, cnt, root, dist1, dist2;
+	static Node 	adNode[];
+	static boolean 	visit[];
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
+	public static int BFS() // 기가 노드 찾기
+	{
 		ArrayDeque<Node> q = new ArrayDeque<>();
 		
 		q.add(new Node(root,0));
@@ -42,7 +47,8 @@ class Main{
 		}
 		return root;
 	}
-	public static int DFS(int node, int sum) {
+	public static int DFS(int node, int sum)  // 가장 긴 가지 구하기
+	{
 
 		int max = 0;
 		
@@ -56,21 +62,18 @@ class Main{
 		return sum + max;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());
-		root 	= Integer.parseInt(st.nextToken());
+		N		= read();
+		root 	= read();
 		visit 	= new boolean[N+1];
 		adNode	= new Node[N+1];
 		
 		for(int i=1; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			c = Integer.parseInt(st.nextToken());
-			adNode[a] = new Node(b, c, adNode[a]);
-			adNode[b] = new Node(a, c, adNode[b]);
+			a 			= read();
+			b 			= read();
+			c 			= read();
+			adNode[a] 	= new Node(b, c, adNode[a]);
+			adNode[b] 	= new Node(a, c, adNode[b]);
 		}
 
 		dist2 = DFS( BFS(), 0);
