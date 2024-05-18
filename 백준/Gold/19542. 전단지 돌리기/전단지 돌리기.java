84bb360744653d5918bc3a36231c4b2477b62aa4
YongJu Kim <kyjdummy@gmail.com>
2024-05-18 23:38:21 +0900

    [Gold III] Title: 전단지 돌리기, Time: 216 ms, Memory: 23360 KB -BaekjoonHub

--- 백준/Gold/19542. 전단지 돌리기/전단지 돌리기.java
+++ 백준/Gold/19542. 전단지 돌리기/전단지 돌리기.java
@@ -1,9 +1,5 @@
 // https://github.com/KimYongJ/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node;
 	Node next;
@@ -11,56 +7,53 @@ class Node{
 }
 class Main{
 	
-	static int a,b, N, S, D, dist, dp[];
-	static Node adNode[];
-	static boolean visit[];
-	public static int filldp(int now) {
+	static int 		a,b, N, S, D, dist;
+	static Node 	adNode[];
+	static boolean 	visit[];
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
+	public static int DFS(int now) {
 		if(visit[now]) 
 			return 0;
+        
 		visit[now] = true;
 		
 		int max = 0;
+        
 		for(Node n=adNode[now]; n!=null; n=n.next)
 			if(!visit[n.node])
-				max = Math.max(filldp(n.node),max);
-
-		return dp[now] += max + 1;
-	}
-	public static void DFS(int now) {
-		if(visit[now]) return;
-		visit[now] = true;
+				max = Math.max(DFS(n.node),max);
 		
-		for(Node n=adNode[now]; n!=null; n=n.next)
-			if(!visit[n.node] && dp[n.node]-1 >= D) // 노드 탐색시 들어가야만 할 때 들어간다.
-				DFS(n.node);
+		if(now != S && max >= D)
+			dist += 2;
 
-		dist += 2;
+		return max + 1;
 	}
+
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer	st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		S 		= Integer.parseInt(st.nextToken());
-		D 		= Integer.parseInt(st.nextToken());
-		dp		= new int[N+1];
+		N 		= read();
+		S 		= read();
+		D 		= read();
 		adNode 	= new Node[N+1];
 		
 		
 		for(int i=1; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken( ));
-			b = Integer.parseInt(st.nextToken());
+			a = read();
+			b = read();
 			adNode[a] = new Node(b, adNode[a]);
 			adNode[b] = new Node(a, adNode[b]);
 		}
 		
 		visit = new boolean[N+1];
-		filldp(S); // 각 노드당 자신이 갖고 있는 가장 깊은 노드까지 거리를 담는다.
-		
-		visit = new boolean[N+1];
 		DFS(S);
+
 		
-		System.out.println(dist - 2);
+		System.out.println(dist);
 	}
 }
