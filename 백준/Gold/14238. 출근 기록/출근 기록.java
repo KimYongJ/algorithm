cc4a94bb94d97815a47b991de498cf9ca44dd6db
YongJu Kim <kyjdummy@gmail.com>
2024-05-19 23:22:47 +0900

    [Gold II] Title: 출근 기록, Time: 156 ms, Memory: 29028 KB -BaekjoonHub

--- 백준/Gold/14238. 출근 기록/출근 기록.java
+++ 백준/Gold/14238. 출근 기록/출근 기록.java
@@ -8,13 +8,13 @@ class Main{
 	static final int 	A = 0;
 	static final int 	B = 1;
 	static final int 	C = 2;
-	static int 			len, num[];
+	static int 			len, A_MAX, B_MAX, C_MAX;
 	static char 		make[];
 	static String 		base, result;
 	static boolean 		visit[][][][][];
 	
-	public static boolean DFS(int depth, int a, int b, int c, int before1, int before2) {
-		if(depth == len) 
+	public static boolean DFS(int a, int b, int c, int before1, int before2) {
+		if(a+b+c == len) 
 			return true;
 		
 		if(visit[a][b][c][before1][before2])
@@ -22,19 +22,22 @@ class Main{
 		
 		visit[a][b][c][before1][before2] = true;
 		
-		if(a > 0) {
-			make[len -a-b-c ] = 'A';
-			if(DFS(depth+1, a-1,b,c, A, before1))
+		if(a < A_MAX) 
+		{
+			make[a+b+c] = 'A';
+			if(DFS(a+1,b,c, A, before1))
 				return true;
 		}
-		if(b > 0 && before1 != B) {
-			make[len -a-b-c ] = 'B';
-			if(DFS(depth+1, a,b-1,c, B, before1))
+		if(b < B_MAX && before1 != B) 
+		{
+			make[a+b+c] = 'B';
+			if(DFS(a,b+1,c, B, before1))
 				return true;
 		}
-		if(c > 0 && before1 != C && before2 != C) {
-			make[len -a-b-c ] = 'C';
-			if(DFS(depth+1, a,b,c-1, C, before1))
+		if(c < C_MAX && before1 != C && before2 != C) 
+		{
+			make[a+b+c] = 'C';
+			if(DFS(a,b,c+1, C, before1))
 				return true;
 		}
 		
@@ -44,15 +47,18 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		base	= new BufferedReader(new InputStreamReader(System.in)).readLine();
 		len		= base.length();
-		num 	= new int[3];
 		make	= new char[len];
 		visit	= new boolean[len+1][len+1][len+1][3][3];
 		result	= "-1";
 		
-		for(int i=0; i<len; i++)
-			num[base.charAt(i)-'A']++;
+		for(char c : base.toCharArray()) 
+		{
+			if(c=='A') 		A_MAX++;
+			else if(c=='B') B_MAX++;
+			else			C_MAX++;
+		}
 		
-		if(DFS(0,num[0], num[1], num[2],0,0))
+		if(DFS(0,0,0,0,0))
 			result = String.valueOf(make);
 		
 		System.out.print(result);
