67e930467153d719d3598d00e013949395d7e559
YongJu Kim <kyjdummy@gmail.com>
2024-05-19 23:12:43 +0900

    [Gold II] Title: 출근 기록, Time: 156 ms, Memory: 29236 KB -BaekjoonHub

---
+++ 백준/Gold/14238. 출근 기록/출근 기록.java
@@ -0,0 +1,60 @@
+// https://github.com/KimYongJ/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+	
+	static final int 	A = 0;
+	static final int 	B = 1;
+	static final int 	C = 2;
+	static int 			len, num[];
+	static char 		make[];
+	static String 		base, result;
+	static boolean 		visit[][][][][];
+	
+	public static boolean DFS(int depth, int a, int b, int c, int before1, int before2) {
+		if(depth == len) 
+			return true;
+		
+		if(visit[a][b][c][before1][before2])
+			return false;
+		
+		visit[a][b][c][before1][before2] = true;
+		
+		if(a > 0) {
+			make[len -a-b-c ] = 'A';
+			if(DFS(depth+1, a-1,b,c, A, before1))
+				return true;
+		}
+		if(b > 0 && before1 != B) {
+			make[len -a-b-c ] = 'B';
+			if(DFS(depth+1, a,b-1,c, B, before1))
+				return true;
+		}
+		if(c > 0 && before1 != C && before2 != C) {
+			make[len -a-b-c ] = 'C';
+			if(DFS(depth+1, a,b,c-1, C, before1))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	public static void main(String[] args)throws Exception{
+		base	= new BufferedReader(new InputStreamReader(System.in)).readLine();
+		len		= base.length();
+		num 	= new int[3];
+		make	= new char[len];
+		visit	= new boolean[len+1][len+1][len+1][3][3];
+		result	= "-1";
+		
+		for(int i=0; i<len; i++)
+			num[base.charAt(i)-'A']++;
+		
+		if(DFS(0,num[0], num[1], num[2],0,0))
+			result = String.valueOf(make);
+		
+		System.out.print(result);
+	}
+}
