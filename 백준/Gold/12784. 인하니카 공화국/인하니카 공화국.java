d735f25872a3ddb0d63eb48d90e7a3ddc8ec5d2a
YongJu Kim <kyjdummy@gmail.com>
2024-05-19 21:45:08 +0900

    [Gold III] Title: 인하니카 공화국, Time: 328 ms, Memory: 32172 KB -BaekjoonHub

--- 백준/Gold/12784. 인하니카 공화국/인하니카 공화국.java
+++ 백준/Gold/12784. 인하니카 공화국/인하니카 공화국.java
@@ -14,28 +14,30 @@ class Main{
 	static int a, b, d, T, N, M, sum;
 	static boolean visit[];
 	static Node adNode[];
-	static StringBuilder sb = new StringBuilder();
+	// DFS는 자기와 부모와의 거리 대(:) 자기와 자기 자식 노드들의 거리의 합(그냥 합이아니라 최솟값의 합) 을 반환한다.
+	// 원래 DFS 코드는 dp로 이해 하기 쉽게 작성했으나 빠른 실행을 위해 dp를 없애고 바로 반환토록 간소화 시킨 코드임
 	public static int DFS(int now, int parentDist) {
 		visit[now] = true;
-		
 		int sum = 0, cnt = 0;
-		for(Node n=adNode[now]; n!=null; n=n.next) {
-			if(!visit[n.node]) {
+		
+		for(Node n=adNode[now]; n!=null; n=n.next)
+			if(!visit[n.node]) 
+			{
 				sum += DFS(n.node, n.dist);
 				cnt++;
 			}
-		}
 
-		if(now == 1) {
-			sb.append(sum).append('\n');
-		}
-		if(cnt == 0) {
+		if(now == 1)						// 루트 노드일 경우는 sum이 하위 노드들 중 짧은 거리의 합이기 때문에 바로 sum 리턴
+			return sum;
+		
+		if(cnt == 0)						// 리프 노드일 경우는 부모와의 거리 그대로 리턴 
 			return parentDist;
-		}
-		return Math.min(sum, parentDist);
+		
+		return Math.min(sum, parentDist); 	// 중간 노드일 경우는, 자기 자신과 부모의 거리와 자기 자식들의 거리의 합 중 작은것 리턴 
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder 	sb = new StringBuilder();
 		StringTokenizer st;
 		T = Integer.parseInt(br.readLine());
 		while(T-->0) {
@@ -55,7 +57,8 @@ class Main{
 				adNode[b] = new Node(a,d,adNode[b]);
 			}
 			
-			DFS(1,0);
+			sb.append(	DFS(1,0)	)
+				.append('\n');
 
 		}
 		System.out.print(sb);
