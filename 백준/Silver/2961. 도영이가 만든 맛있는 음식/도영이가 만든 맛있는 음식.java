15d6c1457f9c4653bbef085f1df294b449fe7df3
YongJu Kim <kyjdummy@gmail.com>
2024-05-21 16:18:31 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,46 +1,43 @@
-// https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main{
-	
-	static int 		N, S[], B[];
-	static int		DEPTH, result;
-	static boolean 	visit[];
-	
-	public static void Back(int depth,int mul, int sum) {
-		if(depth == DEPTH) {
-			result = Math.min(result, Math.abs(mul - sum));
-			return;
-		}
-		for(int i=0; i<N; i++) 
-			if(!visit[i]) {
-				visit[i] = true;
-				Back(depth + 1, mul * S[i] , sum + B[i]);
-				visit[i] = false;
-			}
-		
-	}
-	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		S = new int[N];		// 곱
-		B = new int[N];		// 합
-		visit = new boolean[N];
-		result = Integer.MAX_VALUE;
-		for(int i=0; i<N; i++) 
-		{
-			st	= new StringTokenizer(br.readLine());
-			S[i] = Integer.parseInt(st.nextToken());
-			B[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(DEPTH=1; DEPTH<=N; DEPTH++) // 총 몇개의 재로로 할건지 체크, 1개 재료부터 N재료까지
-		{
-			Back(0,1,0);
-		}
-		System.out.print(result);
-	}
+class Main {
+    static int N, S[], B[];
+    static int result;
+    static boolean visit[];
+
+    public static void Back(int depth, int mul, int sum, int start) {
+        if (depth > 0) {
+            result = Math.min(result, Math.abs(mul - sum));
+        }
+        if (depth == N) {
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                Back(depth + 1, mul * S[i], sum + B[i], i + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        S = new int[N];     // 곱
+        B = new int[N];     // 합
+        visit = new boolean[N];
+        result = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            S[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Back(0, 1, 0, 0);
+        System.out.print(result);
+    }
 }
