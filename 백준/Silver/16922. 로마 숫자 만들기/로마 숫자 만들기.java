3d7910cc216865b1c3d601ea06b35dc5c7f39ea2
YongJu Kim <kyjdummy@gmail.com>
2024-05-22 19:50:27 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -0,0 +1,31 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+	static final int 	arr[] = {1,5,10,50};
+	static int 			N, CNT;
+	static boolean 		visit[];
+	public static void DFS(int depth, int idx, int sum) {
+		if(depth ==  N) 
+		{ 
+			if(!visit[sum]) 
+			{
+				visit[sum] = true;
+				CNT++;
+			}
+			return;
+		}
+		for(int i=idx; i<4; i++)
+			DFS(depth + 1, i, sum + arr[i]);
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N 		= Integer.parseInt(br.readLine());
+		visit 	= new boolean[1001];
+		
+		DFS(0,0,0);
+		
+		System.out.print(CNT);
+	}
+}
