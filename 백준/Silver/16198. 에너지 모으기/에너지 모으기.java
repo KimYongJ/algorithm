c5373aaa883f3193538978c63782ee998f132bd4
YongJu Kim <kyjdummy@gmail.com>
2024-05-22 17:39:04 +0900

    [Silver I] Title: 에너지 모으기, Time: 140 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
@@ -1,12 +1,15 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
 	static int 		N, MAX, nums[];
 	static boolean 	visit[];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
 	public static int getMul(int l, int r) {
 		while(visit[l] || visit[r]) 
@@ -32,16 +35,14 @@ class Main{
 				visit[i] = false;
 			}
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N 		= Integer.parseInt(br.readLine());
+		N 		= read();
 		nums	= new int[N];
 		visit	= new boolean[N];
 		
-		st 		= new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
-			nums[i] = Integer.parseInt(st.nextToken());
+			nums[i] = read();
 		
 		N		-= 2; // 빠른 연산을 위한 전처리 
 		
