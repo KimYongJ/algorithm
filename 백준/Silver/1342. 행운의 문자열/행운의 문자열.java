0b2d25f5169c4baf2f7770eaf1da1c235ea8d6c3
YongJu Kim <kyjdummy@gmail.com>
2024-05-23 23:31:45 +0900

    [Silver I] Title: 행운의 문자열, Time: 328 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1342. 행운의 문자열/행운의 문자열.java
+++ 백준/Silver/1342. 행운의 문자열/행운의 문자열.java
@@ -5,36 +5,38 @@ import java.io.InputStreamReader;
 
 class Main{
 	
-	static int arr[];
 	static int len;
 	static int cnt;
 
-	public static void backtracking(int depth,int before) {
+	public static void backtracking(int arr[], int depth,int before) {
 		if(len == depth) 
 		{
 			cnt++;
 			return;
 		}
 		
-		for(int i=1; i<28; i++) 
-			if(arr[i] > 0 && before != i) 
+		for(int i=1; i<28; i++) {
+			if(arr[i] == 0) 
+				continue;
+			if(before != i) 
 			{
 				arr[i]--;
-				backtracking(depth + 1, i);
+				backtracking(arr, depth + 1, i);
 				arr[i]++;
 			}
+		}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int arr[] 	= new int[28];
 		String str 	= br.readLine();
 		len 		= str.length()+1;
-		arr 		= new int[28];
-		
+
 		for(char c : str.toCharArray())
 			arr[c-96]++;
 
 		
-		backtracking(1,0);
+		backtracking(arr, 1,0);
 		
 		System.out.print(cnt);
 	}
