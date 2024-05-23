a328514eb2b8999f031fcf84412c18296010de1b
YongJu Kim <kyjdummy@gmail.com>
2024-05-23 15:09:47 +0900

    [Silver III] Title: 근손실, Time: 128 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.java
+++ 백준/Silver/18429. 근손실/근손실.java
@@ -1,7 +1,4 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -9,15 +6,24 @@ class Main{
 	static int 		result;
 	static boolean 	visit[];
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void Backtracking(int depth, int sum) {
-		if(depth == N) {
-			if(sum >= 500) result++;
+		if(depth == N) 
+		{
+			if(sum >= 500) 
+				result++;
 			return;
 		}
 		for(int i=0; i<N; i++)
-			if(!visit[i]) {
+			if(!visit[i]) 
+			{
 				visit[i] = true;
-				num = sum + kit[i] - K;
+				num = sum + kit[i];
 				if(num >= 500) {
 					Backtracking(depth + 1, num);
 				}
@@ -25,16 +31,13 @@ class Main{
 			}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		kit = new int[N];
-		visit = new boolean[N];
+		N 		= read();
+		K 		= read();
+		kit 	= new int[N];
+		visit 	= new boolean[N];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			kit[i] = Integer.parseInt(st.nextToken());
+			kit[i] = read() - K;
 		
 		Backtracking(0,500);
 		
