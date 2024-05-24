92ee6ed62bf5b76e9f9d3106a55b46e12d106cb9
YongJu Kim <kyjdummy@gmail.com>
2024-05-24 20:39:50 +0900

    [Silver III] Title: 크면서 작은 수, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.java
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.java
@@ -4,49 +4,44 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+
+
 class Main{
 	
-	static int len;
-	static char base[], dummy[], des[];
-	static boolean visit[];
-	public static boolean check() {
-		for(int i=0; i<len; i++) {
-			if(base[i] > des[i]) return false;
-			else if(base[i] < des[i]) return true;
-		}
-		return false;
-	}
-	public static boolean backtracking(int depth) {
+	static int 		len, num, result, arr[];
+	static String 	str;
+	static boolean 	flag, visit[];
+	public static boolean backtracking(int depth, int sum) {
 		if(depth == len) 
 		{
-			if(check())
-				return true;
-			return false;
+			if(flag = (num < sum))
+				result = sum;
+			return flag;
 		}
 		for(int i=0; i<len; i++)
 			if(!visit[i]) 
 			{
-				visit[i] 	= true;
-				des[depth] 	= dummy[i];
-				if(backtracking(depth + 1)) return true;
-				visit[i] 	= false;
+				visit[i] = true;
+				if(backtracking(depth + 1, sum*10 + arr[i])) return true;
+				visit[i] = false;
 			}
-
 		return false;
 	}
 	public static void main(String args[])throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		base	= br.readLine().toCharArray();
-		len 	= base.length;
-		dummy 	= base.clone();
-		des		= new char[len];
-		visit 	= new boolean[len];
+		str 	= br.readLine();
+		len 	= str.length();
+		arr		= new int[len];
+		visit	= new boolean[len];
+		num 	= Integer.parseInt(str);
 		
-		Arrays.sort(dummy);
+		for(int i=0; i<str.length(); i++)
+			arr[i] = str.charAt(i)-'0';
+
+		Arrays.sort(arr);
 		
-		if(backtracking(0))
-			System.out.print(String.valueOf(des));
-		else
-			System.out.print("0");
+		backtracking(0,0);
+		
+		System.out.print(result);
 	}
 }
