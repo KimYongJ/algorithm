21908ea18830fdf4c97d24a7747bd60788470dd3
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 22:28:35 +0900

    [Silver I] Title: 기타콘서트, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.java
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.java
@@ -7,40 +7,32 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int 	result = -1;
-	static int	N, song, maxCnt; 
+	static int	N, maxCnt; 
 	static long list[]; // 각 기타에 따른 연주 가능 곡의 정보, bit로 표현
 	
-	public static int getCnt(long sum) {
-		long comp = 1;
-		int cnt = 0;
-		for(int i=0; i<64; i++)
-			if((sum & comp<<i) != 0)
-				cnt++;
-		
-		return cnt;
-	}
 	public static void comb(int base, int depth, int idx, long sum) {
-		if(depth == 0) {
-			int cnt = getCnt(sum);
-			if(maxCnt < cnt) {
+		if(depth == 0) 
+		{
+			int cnt = Long.bitCount(sum);
+			if(maxCnt < cnt) 
+			{
 				maxCnt = cnt;
 				result = base;
 			}
 			return;
 		}
 		
-		for(int i=idx; i<N; i++) {
+		for(int i=idx; i<N; i++)
 			if(list[i] != 0)
 				comb(base, depth-1, i+1, sum | list[i]);
-		}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N 		= Integer.parseInt(st.nextToken());
-		song 	= Integer.parseInt(st.nextToken());
 		list 	= new long[N];
-		
+		st.nextToken();
+        
 		for(int i=0; i<N; i++) 
 		{
 			st = new StringTokenizer(br.readLine());
