88ee8b158f306d9dba6c1f472ee6e59555e31a37
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 21:25:28 +0900

    [Silver II] Title: 매직 스퀘어로 변경하기, Time: 204 ms, Memory: 15612 KB -BaekjoonHub

--- 백준/Silver/16945. 매직 스퀘어로 변경하기/매직 스퀘어로 변경하기.java
+++ 백준/Silver/16945. 매직 스퀘어로 변경하기/매직 스퀘어로 변경하기.java
@@ -6,67 +6,59 @@ import java.util.StringTokenizer;
 
 class Main{
 	
-	static int result = Integer.MAX_VALUE;
-	static int map[][];
+	static int 		result = Integer.MAX_VALUE;
+	static int 		map[];
 	static boolean 	visit[];
 	
 	public static boolean check() {
-		int base = map[0][0] + map[1][1] + map[2][2];
-		if(base == map[2][0] + map[1][1] + map[0][2]) 
-		{
-			int sum1, sum2;
-			for(int i=0; i<3; i++) 
-			{
-				sum1 = sum2 = 0;
-				for(int j=0; j<3; j++) 
-				{
-					sum1 += map[i][j];
-					sum2 += map[j][i];
-				}
-				if(base != sum1 || base != sum2)
-					return false;
-			}
-			return true;
-		}
-		return false;
+		int base = map[0]+map[1]+map[2];
+		if(	
+			base != map[3]+map[4]+map[5] ||
+			base != map[6]+map[7]+map[8] ||
+			base != map[0]+map[3]+map[6] ||
+			base != map[1]+map[4]+map[7] ||
+			base != map[2]+map[5]+map[8] ||
+			base != map[0]+map[4]+map[8] ||
+			base != map[2]+map[4]+map[6]
+				)
+			return false;
+		return true;
 	}
-	public static void DFS(int y, int x, int sum) {
-		if(x == 3) {
-			DFS(y+1,0, sum);
-			return;
-		}
-		if(y==3) {
+	public static void DFS(int idx, int sum) {
+		if(idx == 9) {
 			if(check()) 
 				result = Math.min(result, sum);
 			return;
 		}
-		int tmp = map[y][x];
-		for(int i=1; i<10; i++) {
-			if(!visit[i]) {
+
+		int tmp = map[idx];
+		for(int i=1; i<=9; i++)			// 1부터 9까지 모두 써야하기 때문에
+			if(!visit[i]) 
+			{
 				visit[i] = true;
-				map[y][x] = i;
-				DFS(y, x+1, sum + Math.abs(i-tmp));
-				map[y][x] = tmp;
+				map[idx] = i;
+				DFS(idx+1, sum + Math.abs(i-tmp));
+				map[idx] = tmp;
 				visit[i] = false;
 			}
-		}
+		
 	}
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		map 	= new int[3][3];
-		visit	= new boolean[10];
+		map   = new int[10];
+		visit = new boolean[10];
 		
+		int idx = 0;
 		for(int i=0; i<3; i++) 
 		{
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<3; j++)
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[idx++] = Integer.parseInt(st.nextToken());
 		}
 		
-		
-		DFS(0,0,0);
+		DFS(0,0);
 		
 		System.out.print(result);
 	}
