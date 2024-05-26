d0ee596d93fd0839527bec8750937cd623175cde
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 19:38:41 +0900

    [Silver II] Title: 소-난다!, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/19699. 소－난다！/소－난다！.java
+++ 백준/Silver/19699. 소－난다！/소－난다！.java
@@ -1,15 +1,16 @@
 // https://github.com/kimyongj/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int		N, M, sum;
 	static int 		arr[];
 	static boolean 	visit[], result[];
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
 	public static void DFS(int depth, int sum, int idx) {
 		if(depth == 0) 
@@ -19,24 +20,18 @@ class Main{
 			return;
 		}
 		
-		for(int i=idx; i<N; i++) {
+		for(int i=idx; i<N; i++)
 			DFS(depth-1, sum + arr[i], i+1);
-		}
+		
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder 	sb = new StringBuilder();
-		N 	= Integer.parseInt(st.nextToken());
-		M 	= Integer.parseInt(st.nextToken());
+		N 	= read(); 
+		M 	= read();
 		arr = new int[N];
 		
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			sum += arr[i];
-		}
+		for(int i=0; i<N; i++)
+			sum += arr[i] = read();
 		
 		result 	 = new boolean[sum+1];
 		visit 	 = new boolean[sum+1];
