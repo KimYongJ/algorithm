a0da04900d23b50f7a582203822ba827de155421
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 20:01:25 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.java
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.java
@@ -1,13 +1,15 @@
 // https://github.com/kimyongj/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int LEN, TIME, MAX;
 	static int map[];
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
 	public static void DFS(int time, int idx, int sum) {
 		if(idx >= LEN || time == TIME) 
@@ -22,15 +24,12 @@ class Main{
 			DFS(time+1, idx+2, sum/2 + map[idx+1]);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		LEN		= Integer.parseInt(st.nextToken());
-		TIME 	= Integer.parseInt(st.nextToken());
+		LEN		= read();
+		TIME 	= read();
 		map 	= new int[LEN];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<LEN; i++)
-			map[i] = Integer.parseInt(st.nextToken());
+			map[i] = read();
 		
 		DFS(0, 0, 1);
 		
