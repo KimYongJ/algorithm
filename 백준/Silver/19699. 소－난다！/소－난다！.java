d77080e9c9ec259ba36fcdb563a32536b6f66e0c
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 19:27:13 +0900

    [Silver II] Title: 소-난다!, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/19699. 소－난다！/소－난다！.java
@@ -0,0 +1,71 @@
+// https://github.com/kimyongj/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int		N, M, sum;
+	static int 		arr[];
+	static boolean 	visit[];
+	static ArrayList<Integer> list = new ArrayList<>();
+	
+	public static void DFS(int depth, int sum, int idx) {
+		if(depth == 0) {
+			if(!visit[sum]) {
+				list.add(sum);
+				visit[sum] = true;
+			}
+			return;
+		}
+		
+		for(int i=idx; i<N; i++) {
+			DFS(depth-1, sum + arr[i], i+1);
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder	sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i];
+		}
+		visit = new boolean[sum+1];
+		visit[0] = visit[1] = true;
+		
+		Loop:
+		for(int i=2; i<=sum; i++) 
+		{
+			for(int j=2; j*j<=i; j++)
+				if(i%j == 0)
+					continue Loop;
+			
+			int num = i+i;
+			while(num <= sum) {
+				visit[num] = true;
+				num += i;
+			}
+		}
+		
+		DFS(M, 0, 0);
+		
+		if(list.size() == 0) {
+			sb.append(-1);
+		}else {
+			Collections.sort(list);
+			
+			for(int l : list)
+				sb.append(l).append(' ');
+		}
+		System.out.print(sb);
+	}
+}
