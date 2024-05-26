a529471f92c9277680a868dba0fb79614294bac0
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 19:37:16 +0900

    [Silver II] Title: 소-난다!, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/19699. 소－난다！/소－난다！.java
+++ 백준/Silver/19699. 소－난다！/소－난다！.java
@@ -2,23 +2,20 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main{
 	
 	static int		N, M, sum;
 	static int 		arr[];
-	static boolean 	visit[];
-	static ArrayList<Integer> list = new ArrayList<>();
+	static boolean 	visit[], result[];
 	
 	public static void DFS(int depth, int sum, int idx) {
-		if(depth == 0) {
-			if(!visit[sum]) {
-				list.add(sum);
-				visit[sum] = true;
-			}
+		if(depth == 0) 
+		{
+			if(!visit[sum]) 
+				result[sum] = visit[sum] = true;
 			return;
 		}
 		
@@ -28,26 +25,30 @@ class Main{
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder	sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		StringBuilder 	sb = new StringBuilder();
+		N 	= Integer.parseInt(st.nextToken());
+		M 	= Integer.parseInt(st.nextToken());
 		arr = new int[N];
+		
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 			sum += arr[i];
 		}
-		visit = new boolean[sum+1];
+		
+		result 	 = new boolean[sum+1];
+		visit 	 = new boolean[sum+1];
 		visit[0] = visit[1] = true;
 		
 		//에라토스테네스의 체
 		for(int i=2; i<=sum; i++) 
 		{
-			if(visit[i]) continue;
+			if(visit[i]) 
+				continue;
 			
-			int num = i+i;
+			int num = i<<1;
 			while(num <= sum) {
 				visit[num] = true;
 				num += i;
@@ -56,14 +57,13 @@ class Main{
 		
 		DFS(M, 0, 0);
 		
-		if(list.size() == 0) {
+		for(int i=0; i<=sum; i++)
+			if(result[i])
+				sb.append(i).append(' ');
+		
+		if(sb.length() == 0)
 			sb.append(-1);
-		}else {
-			Collections.sort(list);
-			
-			for(int l : list)
-				sb.append(l).append(' ');
-		}
+		
 		System.out.print(sb);
 	}
 }
