1ffc401d9664917651b4f048a2f0758a0550243a
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 16:41:02 +0900

    [Silver I] Title: 2-SAT - 1, Time: 236 ms, Memory: 19440 KB -BaekjoonHub

--- 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.java
+++ 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.java
@@ -1,65 +1,56 @@
 // https://github.com/kimyongj/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
-	static int N, M, data[][];
-	static boolean  bool[];
+	static int 		N, M, data[][];
+	static boolean  flag, bool[];
+	
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	
 	public static boolean get(int num) {
-		int abs = Math.abs(num);
-		return num > 0 ? bool[abs] : !bool[abs];
+		return num > 0 ? bool[Math.abs(num)] : !bool[Math.abs(num)];
 	}
-	public static boolean check() {
-		boolean flag = get(data[0][0]) || get(data[0][1]);
-		
-		for(int i=1; i<M && flag; i++) 
-			flag = flag && (get(data[i][0]) || get(data[i][1]));
-		
-		return flag;
-	}
-	public static boolean DFS(int cnt, int idx) {
-		if(cnt == 0)
-			return check();
-		
-		for(int i=idx; i<=N; i++) 
-		{
-			bool[i] = true;
-			if(DFS(cnt - 1, i+1)) return true;
-			bool[i] = false;
+
+	public static boolean DFS(int depth, boolean f) {
+		if(depth > N) {
+			flag = get(data[0][0]) || get(data[0][1]);
+			
+			for(int i=1; i<M && flag; i++) 
+				flag = flag && (get(data[i][0]) || get(data[i][1]));
+			
+			return flag;
 		}
+
+		bool[depth] = f;
 		
-		return false;
+		return DFS(depth + 1, true) || DFS(depth + 1, false);
 	}
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		M 		= Integer.parseInt(st.nextToken());
+		N 		= read();
+		M 		= read();
 		bool 	= new boolean[N+1];
-		data 	= new int[M][2];
+		data	= new int[M][2];
 		
 		for(int i=0; i<M; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			data[i][0] = Integer.parseInt(st.nextToken());
-			data[i][1] = Integer.parseInt(st.nextToken());
+			data[i][0] = read();
+			data[i][1] = read();
 		}
 		
 		
 		// bool 배열에 true와 false에 대해 모든 것을 담아야 함으로 조합으로 구현
 		int num = 0;
 		
-		if(check())		// 모두 false인 경우 
+		if(DFS(1, true) || DFS(1, false))
 			num = 1;
-		
-		for(int i=1; i<=N && num == 0; i++)
-			if(DFS(i,1))
-				num = 1;
 
 		System.out.print(num);
 	}
