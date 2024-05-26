4a00a389d1eae00861d95a4eed8ad22066e8abe9
YongJu Kim <kyjdummy@gmail.com>
2024-05-26 14:06:53 +0900

    [Silver II] Title: 걷다보니 신천역 삼 (Small), Time: 120 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/14650. 걷다보니 신천역 삼 （Small）/걷다보니 신천역 삼 （Small）.java
+++ 백준/Silver/14650. 걷다보니 신천역 삼 （Small）/걷다보니 신천역 삼 （Small）.java
@@ -5,22 +5,25 @@ import java.io.InputStreamReader;
 
 class Main{
 	
-	public static int DFS(int depth, int sum) {
-		if(depth == 1)
-			return sum % 3 == 0 ? 1:0;
-		
-		int cnt = 0;
+	static int result;
+    
+	public static void DFS(int depth, int sum) {
+		if(depth == 1) 
+		{
+			if(sum % 3 == 0) result++;
+			return;
+		}
 		
 		for(int i=0; i<3; i++)
-			cnt += DFS(depth-1, sum*10 + i);
-	
-		return cnt;
+			DFS(depth-1, sum*10 + i);
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-
-		System.out.print(DFS(N, 1) + DFS(N, 2));
+		DFS(N, 1);
+		DFS(N, 2);
+		
+		System.out.print(result);
 	}
 }
