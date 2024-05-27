592cb1bf9a1c6c0bc7c55e9bf53ee76f1ee946af
YongJu Kim <kyjdummy@gmail.com>
2024-05-27 16:16:00 +0900

    [Silver III] Title: 세 번 이내에 사과를 먹자, Time: 120 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.java
+++ 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.java
@@ -1,12 +1,17 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	static final int 	MAX = 5;
 	static final int 	dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
 	static int 			sy, sx, map[][];
+	
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	
 	public static boolean backtracking(int depth, int y, int x, int cnt) {
 		if(cnt >= 2)	return true;
@@ -28,33 +33,20 @@ class Main{
 		return false;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		map = new int[MAX+2][MAX+2];
 		
-		// 외부를 벽으로 막음
 		for(int i=0; i<7; i++)
-			map[i][0] = map[0][i] = map[i][6] = map[6][i] = -1;
+			map[i][0] = map[0][i] = map[i][6] = map[6][i] = -1;// 외부를 벽으로 막음
 		
 		for(int y=1; y<=MAX; y++) 
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=MAX; x++)
-				map[y][x] = Integer.parseInt(st.nextToken());	
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		sy = Integer.parseInt(st.nextToken())+1;
-		sx = Integer.parseInt(st.nextToken())+1;
+				map[y][x] = read();	
+
+		sy = read()+1;
+		sx = read()+1;
 		
 		int cnt = map[sy][sx] == 1 ? 1 : 0;
-		int result = 0;
-		
 		map[sy][sx] = -1;
-		
-		if(backtracking(0,sy,sx,cnt))
-			result = 1;
-		
-		System.out.print(result);
+		System.out.print(backtracking(0,sy,sx,cnt) ? 1 : 0);
 	}
 }
