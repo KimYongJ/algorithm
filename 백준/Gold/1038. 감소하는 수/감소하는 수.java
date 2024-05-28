cf6614ef21a0612332f69936f3f485fda98ed41f
YongJu Kim <kyjdummy@gmail.com>
2024-05-28 17:58:24 +0900

    [Gold V] Title: 감소하는 수, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -4,37 +4,35 @@ import java.io.InputStreamReader;
 
 class Main {
 
-    static int N;
-    static int cnt;
+    static int 	N;
     static long result = -1;
     public static boolean DFS(int depth, long sum) {
         if (depth == 0) {
-            if (N == ++cnt)
+            if (--N == 0)
             {
                 result = sum;
                 return true;
             }
             return false;
         }
-        for(long i=0; i<=9; i++)
-            if(sum%10 > i)
+        for(long i=0; i<=9; i++)			// 둘째 자리부터 0~9를 대입함
+            if(sum%10 > i)					// 이전 숫자보다 작아야 만 DFS진행
                 DFS(depth-1, sum*10L + i);
-        
         return false;
     }
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    N = Integer.parseInt(br.readLine());
-	    cnt = 9;
-	    if (N < 10) 
-	    {
+	    
+	    if (N < 10) {
 	        System.out.print(N);
-	    } 
+	    }
 	    else 
 	    {
+	    	N -= 9; // N에 9를 빼서 9까지 경우의 수를 제거 한다.
 	        Loop:
-	        for (int i = 1; i <= 10; i++)
-	            for (int s = 1; s <= 9; s++)
+	        for (int i = 1; i < 10; i++)	// 자리수를 지정하는 것 2자리수는 1을 전달하기 때문에 i는 1부터 시작
+	            for (int s = 1; s <= 9; s++)// 첫시작 숫자 1~9까지 바인딩
 	                if(DFS(i, s))
 	                    break Loop;
 	        System.out.print(result);
