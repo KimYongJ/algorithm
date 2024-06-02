763bb68787591a88d7117d2a8cff91cebb210957
YongJu Kim <kyjdummy@gmail.com>
2024-06-02 10:34:42 +0900

    [Gold V] Title: 캠프 준비, Time: 140 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -8,28 +8,25 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int cnt;
-	static int start, first, last;
 	static int N, L, R, limit;
 	static int arr[];
-	public static void comb(int depth, int idx,int sum) {
+
+	public static void comb(int depth, int idx, int sum, int bit) {
 		if(depth == 0) 
 		{
-			if(L<=sum && sum<=R && limit<=last-first) 
+			if(L<=sum && sum<=R && 
+					arr[31-Integer.numberOfLeadingZeros(bit)] - arr[Integer.numberOfTrailingZeros(bit)] >= limit)
 			{
 				cnt++;
 			}
 			return;
 		}
-		for(int i=idx; i<N; i++) 
-		{
-			if(depth == start)
-				first = arr[i];
-			if(depth == 1)
-				last = arr[i];
+		
+		for(int i=idx; i<N; i++)
+			comb(depth - 1, i+1, sum + arr[i], bit | 1<<i);
 			
-			comb(depth-1, i+1, sum + arr[i]);
-		}
 	}
+
 	public static void main(String[] args)throws Exception{
 		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -39,6 +36,7 @@ class Main{
 		R 		= Integer.parseInt(st.nextToken());
 		limit 	= Integer.parseInt(st.nextToken());
 		arr		= new int[N];
+		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
@@ -46,8 +44,8 @@ class Main{
 		Arrays.sort(arr);
 		
 		// 조합을 구한다. 2개부터 N개 까지 고르는 것
-		for(start=2; start<=N; start++)
-			comb(start,0,0);
+		for(int i=2; i<=N; i++)
+			comb(i,0,0,0);
 		
 		System.out.print(cnt);
 	}
