0d30b6ffd969af613904cdb5c4f3d4384522b65a
YongJu Kim <kyjdummy@gmail.com>
2024-06-02 20:06:28 +0900

    [Gold IV] Title: Yes or yes, Time: 296 ms, Memory: 22884 KB -BaekjoonHub

--- 백준/Gold/25195. Yes or yes/Yes or yes.java
+++ 백준/Gold/25195. Yes or yes/Yes or yes.java
@@ -1,11 +1,7 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Node{
-	int node;
-	Node next;
+	int node;	Node next;
 	Node(int node, Node next){this.node = node;this.next = next;}
 }
 class Main{
@@ -14,46 +10,41 @@ class Main{
 	static boolean 	visit[];
 	static Node 	adNode[];
 	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static boolean DFS(int node) {
 		boolean flag = true;
 		
-		for(Node n=adNode[node]; n!=null; n=n.next) {
-			if(!visit[n.node] && DFS(n.node)) 
-				return true;
-			else
-				flag = false;
+		for(Node n=adNode[node]; n!=null; n=n.next) 
+		{
+			if(!visit[n.node] && DFS(n.node)) return true;
+			else flag = false;
 		}
 		
 		return flag;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N 		= Integer.parseInt(st.nextToken());
-		M 		= Integer.parseInt(st.nextToken());
+		N 		= read();
+		M 		= read();
 		adNode 	= new Node[N+1];
 		visit	= new boolean[N+1];
 		
 		int a, b, n;
 		for(int i=0; i<M; i++) 
 		{
-			st 			= new StringTokenizer(br.readLine());
-			a 			= Integer.parseInt(st.nextToken());
-			b 			= Integer.parseInt(st.nextToken());
+			a 			= read();
+			b 			= read();
 			adNode[a] 	= new Node(b, adNode[a]);
 		}
 		
-		n	= Integer.parseInt(br.readLine());
-		st 	= new StringTokenizer(br.readLine());
-		
+		n	= read();
 		for(int i=0; i<n; i++)
-			visit[Integer.parseInt(st.nextToken())] = true;
-		
-		String str = "Yes";
-		
-		if(!visit[1]) 
-			str = DFS(1) ? "yes" : "Yes";
-		
-		System.out.print(str);
+			visit[read()] = true;
+
+		System.out.print(!visit[1] && DFS(1) ? "yes" : "Yes");
 	}
 }
