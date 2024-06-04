bd9d57dac9c23bce5ce40e53b203b3f6f05de170
YongJu Kim <kyjdummy@gmail.com>
2024-06-04 17:01:55 +0900

    [Gold V] Title: 진우의 민트초코우유, Time: 136 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/20208. 진우의 민트초코우유/진우의 민트초코우유.java
+++ 백준/Gold/20208. 진우의 민트초코우유/진우의 민트초코우유.java
@@ -1,8 +1,5 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -14,49 +11,51 @@ class Main{
 	static boolean 	visit[];
 	static ArrayList<int[]> position = new ArrayList<>();
 
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void backtracking(int beforeIdx, int cnt, int energy) {
+		// 현재 위치에서 처음으로 갈 수 있으면서 cnt가 큰 경우 MAX값 갱신
 		if(MAX < cnt && dist[0][beforeIdx] <= energy)	
 			MAX = cnt;
-		
+		// 모두 다 밤운했으면 끝냄
 		if(MAX == len-1) return;
 
 		for(int i=1; i<len; i++)
 			if(!visit[i]) 
 			{
 				visit[i] = true;
-				if(dist[beforeIdx][i] <= energy)
+				if(dist[beforeIdx][i] <= energy)	// 갈수있는 거리면 간다.
 					backtracking(i, cnt + 1, energy - dist[beforeIdx][i] + H);
 				visit[i] = false;
 			}
 	}
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken()); // 마을크기
-		M 		= Integer.parseInt(st.nextToken()); // 초기체력
-		H		= Integer.parseInt(st.nextToken()); // 회복양
+		N		= read(); 							// 마을크기
+		M 		= read(); 							// 초기체력
+		H		= read(); 							// 회복양
 		
-		for(int y=1; y<=N; y++) 
-		{
-			st = new StringTokenizer(br.readLine());
-			for(int x=1; x<=N; x++) 
+		for(int y=0; y<N; y++) 
+			for(int x=0; x<N; x++) 
 			{
-				int n = Integer.parseInt(st.nextToken());
+				int n = read();
 				if(n == 1) 
 				{
-					sy = y;
-					sx = x;
+					sy = y;							// 시작위치 별도 기록
+					sx = x; 						// 시작위치 별도 기록
 				}else if(n == 2) 
-					position.add(new int[] {y,x});
+					position.add(new int[] {y,x}); 	// 우유 위치 리스트에 저장
 			}
-		}
 		
-		position.add(0,new int[] {sy,sx});
+		position.add(0,new int[] {sy,sx});			// 시작 위치를 0번째 인덱스에 삽입
 		len		= position.size();
-		visit 	= new boolean[len];
-		dist	= new int[len][len];
-		// 거리 세팅
+		visit 	= new boolean[len];					// 방문배열 초기화
+		dist	= new int[len][len];				// 각 노드끼리의 거리를 미리 저장
+		// 각 노드끼리의 거리를 미리 저장
 		for(int i=0; i<len-1; i++)
 			for(int j=i+1; j<len; j++) 
 			{
