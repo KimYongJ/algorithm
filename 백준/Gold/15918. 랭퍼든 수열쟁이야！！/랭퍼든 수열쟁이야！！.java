b2cb118e9639cab3c00d09bfff79b223f9a541aa
YongJu Kim <kyjdummy@gmail.com>
2024-06-04 22:34:16 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 1708 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
@@ -0,0 +1,43 @@
+// https://github.com/kimyongj/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int N, len, idx1, idx2;
+	static int map[];
+	static int result;
+	public static void backtracking(int num) {
+		if(N < num) 
+		{
+			result++;
+			return;
+		}
+		int plus = num+1;
+		for(int i=0; i<len; i++) {
+			plus += i;
+			if(plus < len && map[i] == 0 && map[plus] == 0 && map[idx1] == map[idx2]) {
+				map[i] = map[plus] = num;
+				backtracking(num+1);
+				map[i] = map[plus] = 0;
+			}
+			plus -= i;
+		}
+		
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N 		= Integer.parseInt(st.nextToken());
+		len		= N*2;
+		idx1 	= Integer.parseInt(st.nextToken())-1;
+		idx2 	= Integer.parseInt(st.nextToken())-1;
+		map		= new int[len];
+		
+		backtracking(1);
+		
+		System.out.print(result);
+	}
+}
