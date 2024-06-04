799ef0ae3d647ba3bd5fe4a52bddb8c066921838
YongJu Kim <kyjdummy@gmail.com>
2024-06-04 22:49:23 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 1048 ms, Memory: 14748 KB -BaekjoonHub

--- 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
+++ 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
@@ -7,10 +7,10 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int N, len, idx1, idx2;
-	static int map[];
 	static int result;
+	static boolean map[];
 	public static void backtracking(int num) {
-		if(num == map[idx1])
+		if(num == idx2)
 			backtracking(num+1);
 		if(N < num) 
 		{
@@ -18,12 +18,12 @@ class Main{
 			return;
 		}
 		int plus = num+1;
-		for(int i=0; i<len; i++) {
+		for(int i=0; plus+i<len; i++) {
 			plus += i;
-			if(plus < len && map[i] == 0 && map[plus] == 0) {
-				map[i] = map[plus] = num;
+			if(!map[i] && !map[plus]) {
+				map[i] = map[plus] = true;
 				backtracking(num+1);
-				map[i] = map[plus] = 0;
+				map[i] = map[plus] = false;
 			}
 			plus -= i;
 		}
@@ -36,8 +36,9 @@ class Main{
 		len		= N*2;
 		idx1 	= Integer.parseInt(st.nextToken())-1;
 		idx2 	= Integer.parseInt(st.nextToken())-1;
-		map		= new int[len];
-		map[idx1] = map[idx2] = idx2 - idx1 - 1;
+		map		= new boolean[len];
+		map[idx1] = map[idx2] = true;
+		idx2	-= idx1 + 1;
 		
 		backtracking(1);
 		
