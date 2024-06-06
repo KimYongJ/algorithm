0bdd90c9da536b8ae633764afcce9e2e967d8371
YongJu Kim <kyjdummy@gmail.com>
2024-06-06 23:33:09 +0900

    [Gold IV] Title: 가르침, Time: 248 ms, Memory: 17452 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -0,0 +1,80 @@
+// https://github.com/kimyongj/algorithm
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int 		result;
+	static int 		N, K;
+	static int 		len, arr[];
+	
+	static ArrayList<Integer>	strList;	// 최종 비교할 문자열( 비트마스킹으로 숫자로 표현 )
+	
+	public static void check(int bit) {
+		int cnt = 0;
+		for(int num : strList) 
+		{
+			if(num == 0) 
+				cnt++;
+			else if((bit & num) == num)
+				cnt++;
+		}
+		if(result < cnt)
+			result = cnt;
+	}
+	public static void combination(int depth, int idx, int bit) {
+		if(depth == 0) 
+		{
+			check(bit);
+			return;
+		}
+		for(int i=idx; i<len; i++)
+			combination(depth -1, i+1, bit | (1<<arr[i]));
+		
+	}
+	public static void main(String[] args)throws Exception{
+		HashSet<Integer> chList 	= new HashSet<>();		// 조합의 기준이 되는 문자열들 ( 비트마스킹 표시할 거라 int로 변환 )
+		boolean visit[] 			= new boolean[26];		// 이미 사용된 값을 체크하기 위한것
+		BufferedReader 	br 			= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer	st 			= new StringTokenizer(br.readLine());
+		
+		N 		= Integer.parseInt(st.nextToken());
+		K 		= Integer.parseInt(st.nextToken());
+		strList	= new ArrayList<>(); // 중복이 제거된, 입력되는 문자열을 bit로 바꾼거
+		
+		visit[0] = visit[2] = visit[8] = visit[13] = visit[19] = true; // a,c,i,n,t 방문처리
+		
+		if(K>=5) 
+		{
+			for(int i=0; i<N; i++) 
+			{
+				String str = br.readLine();
+				int num = 0;
+				for(int j=4; j<str.length()-4; j++) 
+				{
+					int c = str.charAt(j)-'a';
+					if(!visit[c])
+					{
+						num |= 1 << (c);
+						chList.add(c);
+					}
+				}
+				strList.add(num); // 최종 대조할 문자열 리스트( 비트마스킹으로 숫자로 표현 )
+			}
+			
+			len = chList.size();
+			arr = new int[len];
+			int idx = 0;
+			for(int n : chList)
+				arr[idx++] = n;
+
+		
+			combination(Math.min(K-5,len), 0, 0); // 몇개를 고를건지, 인덱스, 현재 구해진 숫자
+		}
+		System.out.print(result);
+	}
+}
