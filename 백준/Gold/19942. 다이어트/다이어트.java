7f7480e8cef253d7a26b3ea60edee28eace807ff
YongJu Kim <kyjdummy@gmail.com>
2024-06-09 22:12:25 +0900

    [Gold IV] Title: 다이어트, Time: 128 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.java
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -1,7 +1,4 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Node{
 	int a,b,c,d,e;
@@ -18,36 +15,40 @@ class Main{
 	static int 	resultArr[];
 	static int 	arr[];
 	static int	N, A, B, C, D;
-	static int	MAX;
 	static Node	node[];
 	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static boolean validate() {
-		if(result == Integer.MAX_VALUE)
-			return true;
-		
 		for(int i=0; i<N; i++) 
-		{
-			if(resultArr[i] == 0 && arr[i] != 0 || resultArr[i] < arr[i]) {
+			if(resultArr[i] == 0 && arr[i] != 0 || resultArr[i] < arr[i])
 				return false;
-			}else if(resultArr[i] != 0 && arr[i] == 0 || resultArr[i] > arr[i]) {
+			else if(resultArr[i] != 0 && arr[i] == 0 || resultArr[i] > arr[i]) 
 				return true;
-			}
-		}
 		return true;
 	}
 	public static void dfs(int depth, int idx, int a, int b, int c, int d, int e) {
 		if(A<=a && B<=b && C<=c && D<=d) 
 		{
-			if(result == e) {
-				if(validate()) {
-					result = e;
-					resultArr = arr.clone();
+			if(result == e) 
+			{
+				if(validate()) 
+				{
+					result		= e;
+					resultArr	= arr.clone();
 				}
-			}else if(result >  e){
-				result = e;
-				resultArr = arr.clone();
+			}
+			else if(result >  e)
+			{
+				result		= e;
+				resultArr 	= arr.clone();
 			}
 		}
+		
 		if(depth == N) 
 			return;
 		
@@ -60,28 +61,18 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N			= Integer.parseInt(br.readLine());
+		N			= read();
+		A			= read();
+		B			= read();
+		C			= read();
+		D			= read();
 		arr 		= new int[N];
 		resultArr	= new int[N];
 		node		= new Node[N+1];
 		
-		st	= new StringTokenizer(br.readLine());
-		A	= Integer.parseInt(st.nextToken());
-		B	= Integer.parseInt(st.nextToken());
-		C	= Integer.parseInt(st.nextToken());
-		D	= Integer.parseInt(st.nextToken());
-		
 		for(int i=1; i<=N; i++) 
-		{
-			st		= new StringTokenizer(br.readLine());
-			node[i] = new Node(
-								Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),
-								Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),
-								Integer.parseInt(st.nextToken())
-								);
-		}
+			node[i] = new Node(read(),read(),read(),read(),read());
+		
 		
 		dfs(0,1,0,0,0,0,0);
 		
