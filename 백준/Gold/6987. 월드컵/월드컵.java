a8aab1b6003b72363281e0c2fd38b576eb72bcc2
YongJu Kim <kyjdummy@gmail.com>
2024-06-10 20:48:47 +0900

    [Gold IV] Title: 월드컵, Time: 128 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -5,18 +5,13 @@ import java.util.StringTokenizer;
 
 class Main{
 	
-	static int[]	team1,	team2;
-	static int[]	tieArr, tieDummy;
-	static int[]	winArr, winDummy;
-	static int[]	loseArr,loseDummy;
+	static int[] team1,team2,tieArr,winArr,loseArr;
 
 	public static boolean backtracking(int round) {
 		if(round == 15) 
 		{
 			for(int i=0; i<6; i++)
-				if(tieArr[i] != tieDummy[i] ||
-					winArr[i] != winDummy[i] || 
-					loseArr[i]!= loseDummy[i]) 
+				if(tieArr[i] != 0 || winArr[i] != 0 || loseArr[i]!= 0) 
 					return false;
 			return true;
 		}
@@ -25,33 +20,32 @@ class Main{
 		int b = team2[round];
 		
 		// a가 이겼을 때
-		if(winArr[a] > winDummy[a] && loseArr[b] > loseDummy[b]) {
-			winDummy[a]++;
-			loseDummy[b]++;
+		if(winArr[a] > 0 && loseArr[b] > 0) {
+			winArr[a]--;
+			loseArr[b]--;
 			if(backtracking(round + 1))
 				return true;
-			winDummy[a]--;
-			loseDummy[b]--;
+			winArr[a]++;
+			loseArr[b]++;
 		}
 		// b가 이겼을 때 
-		if(winArr[b] > winDummy[b] && loseArr[a] > loseDummy[a]) {
-			winDummy[b]++;
-			loseDummy[a]++;
+		if(winArr[b] > 0 && loseArr[a] > 0) {
+			winArr[b]--;
+			loseArr[a]--;
 			if(backtracking(round + 1))
 				return true;
-			winDummy[b]--;
-			loseDummy[a]--;
+			winArr[b]++;
+			loseArr[a]++;
 		}
 		// 무승부일 때
-		if(tieArr[a] > tieDummy[a] && tieArr[b] > tieDummy[b]) {
-			tieDummy[a]++;
-			tieDummy[b]++;
+		if(tieArr[a] > 0 && tieArr[b] > 0) {
+			tieArr[a]--;
+			tieArr[b]--;
 			if(backtracking(round + 1))
 				return true;
-			tieDummy[a]--;
-			tieDummy[b]--;
+			tieArr[a]++;
+			tieArr[b]++;
 		}
-		
 		return false;
 	}
 	public static void main(String[] args)throws Exception{
@@ -59,10 +53,13 @@ class Main{
 		StringBuilder 	sb = new StringBuilder();
 		StringTokenizer st;
 		
-		team1 = new int[15];
-		team2 = new int[15];
+		team1	= new int[15];
+		team2	= new int[15];
+		tieArr	= new int[6];
+		winArr	= new int[6];
+		loseArr = new int[6];
 		
-		int teamIdx = 0;
+		int result, teamIdx=0, total;
 		for(int i=0; i<5; i++)
 			for(int j=i+1; j<6; j++) 
 			{
@@ -70,52 +67,23 @@ class Main{
 				team2[teamIdx++]= j;
 			}
 		
-		int result;
-		int tie, win, lose, ttie, twin, tlose, tcnt, maxtie;
-		for(int r=0; r<4; r++) {
+		
+		for(int r=0; r<4; r++) 
+		{
 			st		= new StringTokenizer(br.readLine());
 			result	= 1;
-			ttie	= 0;
-			twin	= 0;
-			tlose	= 0;
-			tcnt	= 0;
-			maxtie	= 0;
-			
-			tieArr	= new int[6];
-			winArr	= new int[6];
-			loseArr = new int[6];
-			tieDummy= new int[6];
-			winDummy= new int[6];
-			loseDummy=new int[6];
-			
-			for(int t=0; t<6; t++) {
-				win		= Integer.parseInt(st.nextToken());
-				tie		= Integer.parseInt(st.nextToken());
-				lose	= Integer.parseInt(st.nextToken());
-				ttie 	+= tie;
-				twin	+= win;
-				tlose	+= lose;
-				tieArr[t] = tie;
-				winArr[t] = win;
-				loseArr[t] = lose;
-				if(tie != 0)
-					tcnt++;
-				if(win + tie + lose > 5)  	// 각 한라인의 승+패+무는 5이하여야 함
-					result = 0;
-				if(maxtie < tie)			// 무승부의 최대개 수 구하기
-					maxtie = tie;
-			}
-			if(
-				(result != 0) &&
-				(ttie % 2 == 1) ||						// 무승부의 총합이 홀수인 경우 불가
-				(twin != tlose) ||  					// 승과 패는 같아야 한다.
-				((30 - ttie) != (twin + tlose)) || 		// 총합 유효성 검증
-				!backtracking(0)						// 전부 탐색
-			  ) 		 
+			total	= 0;
+			for(int t=0; t<6; t++) 
 			{
-				result = 0;
+				winArr[t]	=	Integer.parseInt(st.nextToken());
+				tieArr[t]	=	Integer.parseInt(st.nextToken());
+				loseArr[t]	=	Integer.parseInt(st.nextToken());
+				if(tieArr[t] + winArr[t] + loseArr[t] > 5)  	// 각 한라인의 승+패+무는 5이하여야 함
+					result = 0;
+				total += tieArr[t] + winArr[t] + loseArr[t];
 			}
-
+			if( total != 30 || (result != 0 && !backtracking(0))) 		 
+				result = 0;
 			sb.append(result).append(' ');
 		}
 		
