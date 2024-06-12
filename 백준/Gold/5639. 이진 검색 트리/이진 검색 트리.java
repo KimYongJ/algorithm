955e82eadd9b6b8ef5efe76c9c204144ce52cea0
YongJu Kim <kyjdummy@gmail.com>
2024-06-13 00:05:02 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 500 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+class Node{
+	int root;
+	Node left, right;
+	Node(int root){
+		this.root = root;
+	}
+	void insert(int node) 
+	{
+		if(this.root > node) 
+		{
+			if(left == null)	
+				left = new Node(node);
+			else				
+				left.insert(node);
+		}else 
+		{
+			if(right == null)	
+				right = new Node(node);
+			else				
+				right.insert(node);
+		}
+	}
+}
+class Main{
+	
+	static StringBuilder sb = new StringBuilder();
+	static Node root;
+	public static void postOrder(Node node) {
+		if(node == null) return;
+		postOrder(node.left);
+		postOrder(node.right);
+		sb.append(node.root).append('\n');
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		root = new Node(Integer.parseInt(br.readLine()));
+		
+		while(true) 
+		{
+			try {
+				root.insert(Integer.parseInt(br.readLine()));
+			
+			}catch(Exception e) {
+				break;
+			}
+		}
+		postOrder(root);
+		System.out.print(sb);
+	}
+}
