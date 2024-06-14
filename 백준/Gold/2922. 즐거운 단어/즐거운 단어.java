407288188c6c7c517344fecdfc483c1de9d1b8fc
YongJu Kim <kyjdummy@gmail.com>
2024-06-14 22:10:40 +0900

    [Gold IV] Title: 즐거운 단어, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/2922. 즐거운 단어/즐거운 단어.java
+++ 백준/Gold/2922. 즐거운 단어/즐거운 단어.java
@@ -4,32 +4,35 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static final String vowel = "AEIOU";
-    static char arr[];
-    
-    public static void main(String[] args)throws Exception {
-        arr = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
-        System.out.print( DFS(0, 0, 0, 0) );
+	static int len;
+	
+    public static void main(String[] args)throws Exception 
+    {
+    	String vowel	= "AEIOU";
+    	char arr[]		= new char[100];
+    	
+    	for(char c : new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray())
+    		arr[len++] = vowel.indexOf(c) >= 0 ? 'a' : c;
+    	
+        System.out.print( DFS(arr, 0, 0, 0, 0) );
     }
 
-    private static long DFS(int idx, int v, int c, int hasL) {
-    	if(v>=3 || c>=3)
-    		return 0;
-    	
-        if(idx == arr.length) 
-        	return hasL;
+    private static long DFS(char arr[], int idx, int v, int c, int hasL) {
+    	if(v>=3 || c>=3)	return 0;
+        if(idx == len) 		return hasL;
         
         long cnt = 0;
+        
         if(arr[idx] == '_') 
         {
-        	cnt += DFS(idx + 1, v+1, 0, hasL) * 5; // 모음
-        	cnt += DFS(idx + 1, 0, c+1, hasL) * 20; // 자음
-        	cnt += DFS(idx + 1, 0, c+1, 1); // L을 강제로 추가
+        	cnt += DFS(arr, idx + 1, v+1, 0, hasL) * 5; // 모음
+        	cnt += DFS(arr, idx + 1, 0, c+1, hasL) * 20; // 자음
+        	cnt += DFS(arr, idx + 1, 0, c+1, 1); // L을 강제로 추가
         }
-        else if(vowel.indexOf(arr[idx]) >= 0)
-        	cnt += DFS(idx + 1, v+1, 0, hasL);
+        else if(arr[idx] == 'a')
+        	cnt += DFS(arr, idx + 1, v+1, 0, hasL);
         else
-        	cnt += DFS(idx + 1, 0, c+1, arr[idx] == 'L' ? 1 : hasL);
+        	cnt += DFS(arr, idx + 1, 0, c+1, arr[idx] == 'L' ? 1 : hasL);
         
         return cnt;
     }
