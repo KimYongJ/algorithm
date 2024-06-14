f749e914f639aa76087e3087c9d8eb28e9e21dd1
YongJu Kim <kyjdummy@gmail.com>
2024-06-14 21:57:42 +0900

    [Gold IV] Title: 즐거운 단어, Time: 128 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Gold/2922. 즐거운 단어/즐거운 단어.java
+++ 백준/Gold/2922. 즐거운 단어/즐거운 단어.java
@@ -5,76 +5,39 @@ import java.io.InputStreamReader;
 public class Main {
 
     static final String vowel = "AEIOU";
-    static long ans = 0;
-    static int LEN;
-    static int index[];
-    static char[] arr;
+    static char arr[];
     
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str		= br.readLine();
-        boolean hasL	= false;
         arr 			= new char[str.length()];
-        index 			= new int[10];
         
         for(int i=0; i<arr.length; i++) 
-        {
-        	char c = str.charAt(i);
-        	arr[i] = c;
-        	if(c == '_')
-        		index[LEN++] = i;
-        	else if(c == 'L')
-        		hasL = true;
-        }
-        	
-        findBlank(0, 1L, hasL);
+        	arr[i] = str.charAt(i);
         
-        System.out.println(ans);
+        System.out.print( DFS(0, 0, 0, false) );
     }
 
-    private static void findBlank(int depth, long sum, boolean hasL) {
-        if(depth == LEN) 
-        {
-        	if(hasL)
-        		check(sum);
-            return;
-        }
-
-        int idx = index[depth];
+    private static long DFS(int idx, int v, int c, boolean hasL) {
+    	if(v>=3 || c>=3)
+    		return 0;
+    	
+        if(idx == arr.length) 
+        	return hasL ? 1 : 0;
+        
+        long cnt = 0;
         if(arr[idx] == '_') 
         {
-        	arr[idx] = '1';// 자음
-            findBlank(depth + 1, sum * 20L, hasL);
-            arr[idx] = '2';// 모음
-            findBlank(depth + 1, sum * 5L, hasL);
-            arr[idx] = 'L';
-            findBlank(depth + 1, sum, true);
-            arr[idx] = '_';
+        	cnt += DFS(idx + 1, v+1, 0, hasL) * 5; // 모음
+        	cnt += DFS(idx + 1, 0, c+1, hasL) * 20; // 자음
+        	cnt += DFS(idx + 1, 0, c+1, true); // L을 강제로 추가
         }
+        else if(vowel.indexOf(arr[idx]) >= 0)
+        	cnt += DFS(idx + 1, v+1, 0, hasL);
+        else
+        	cnt += DFS(idx + 1, 0, c+1, arr[idx] == 'L' ? true : hasL);
         
+        return cnt;
     }
 
-    private static void check (long sum) {
-        int st = 0;
-        int ed = 2;
-        while(ed<arr.length) 
-        {
-            int v = 0;// 모음 갯수
-            int c = 0;// 자음 갯수
-            for(int i=st; i<=ed; i++) 
-            {
-                 if(vowel.indexOf(arr[i])>=0 || arr[i] == '2')
-                	 v++;
-                 else 
-                	 c++;
-            }
-
-            if(v >=3 || c>=3)
-            	return;
-            st++;
-            ed++;
-        }
-        // L값이 존재하며 자음 모음에 연속성 없음
-        ans+=sum;
-    }
 }
