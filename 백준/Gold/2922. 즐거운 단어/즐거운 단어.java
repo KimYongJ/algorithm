036b65c905d79687c22d93bbc5e13f1d2b6bd097
YongJu Kim <kyjdummy@gmail.com>
2024-06-14 22:00:19 +0900

    [Gold IV] Title: 즐거운 단어, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/2922. 즐거운 단어/즐거운 단어.java
+++ 백준/Gold/2922. 즐거운 단어/즐거운 단어.java
@@ -8,34 +8,28 @@ public class Main {
     static char arr[];
     
     public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str		= br.readLine();
-        arr 			= new char[str.length()];
-        
-        for(int i=0; i<arr.length; i++) 
-        	arr[i] = str.charAt(i);
-        
-        System.out.print( DFS(0, 0, 0, false) );
+        arr = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
+        System.out.print( DFS(0, 0, 0, 0) );
     }
 
-    private static long DFS(int idx, int v, int c, boolean hasL) {
+    private static long DFS(int idx, int v, int c, int hasL) {
     	if(v>=3 || c>=3)
     		return 0;
     	
         if(idx == arr.length) 
-        	return hasL ? 1 : 0;
+        	return hasL;
         
         long cnt = 0;
         if(arr[idx] == '_') 
         {
         	cnt += DFS(idx + 1, v+1, 0, hasL) * 5; // 모음
         	cnt += DFS(idx + 1, 0, c+1, hasL) * 20; // 자음
-        	cnt += DFS(idx + 1, 0, c+1, true); // L을 강제로 추가
+        	cnt += DFS(idx + 1, 0, c+1, 1); // L을 강제로 추가
         }
         else if(vowel.indexOf(arr[idx]) >= 0)
         	cnt += DFS(idx + 1, v+1, 0, hasL);
         else
-        	cnt += DFS(idx + 1, 0, c+1, arr[idx] == 'L' ? true : hasL);
+        	cnt += DFS(idx + 1, 0, c+1, arr[idx] == 'L' ? 1 : hasL);
         
         return cnt;
     }
