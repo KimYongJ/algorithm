c8d7efcdaf266361a92f9b841e7524eba4103e9f
YongJu Kim <kyjdummy@gmail.com>
2024-06-18 17:10:31 +0900

    [Gold III] Title: 인싸들의 가위바위보, Time: 260 ms, Memory: 39408 KB -BaekjoonHub

---
+++ 백준/Gold/16986. 인싸들의 가위바위보/인싸들의 가위바위보.java
@@ -0,0 +1,95 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int N;
+	static int MAX;			// 최대 승수
+	static int rtable[][];	// 상성 표 
+	static int action[][];	// 낼 순서
+	static boolean visit[];	// 방문 체크 배열
+	public static boolean cal() {
+		
+		int win[] = new int[3];
+		int idx[] = new int[3];
+		int p1 = 0, p2 = 1, p3 = 2, dummy = 0;
+		int max = 0; // 최대 승수
+		while(max != MAX && idx[0] != N) {
+			int a1 = action[p1][idx[p1]++];
+			int a2 = action[p2][idx[p2]++];
+			
+			if(rtable[a1][a2] == 2) {
+				max = ++win[p1];
+				dummy = p2;
+				p2 = p3;
+				p3 = dummy;
+			}
+			else if(rtable[a1][a2] == 0) {
+				max = ++win[p2];
+				dummy = p1;
+				p1 = p3;
+				p3 = dummy;
+			}
+			else {
+				if(p1 < p2) {
+					max = ++win[p2];
+					dummy = p1;
+					p1 = p3;
+					p3 = dummy;
+				}else {
+					max = ++win[p1];
+					dummy = p2;
+					p2 = p3;
+					p3 = dummy;
+				}
+			}
+		}
+		
+		return win[0] == MAX;
+	}
+	public static boolean backtracking(int idx) {
+		if(idx < 0)
+			return cal();
+		
+		for(int i=0; i<N; i++) 
+		{
+			if(!visit[i]) 
+			{
+				visit[i] = true;
+				action[0][idx] = i;
+				if(backtracking(idx - 1))
+					return true;
+				visit[i] = false;
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer	st = new StringTokenizer(br.readLine());
+		N		= Integer.parseInt(st.nextToken());
+		MAX		= Integer.parseInt(st.nextToken());
+		visit	= new boolean[N];
+		rtable	= new int[N][N];
+		action	= new int[3][20];
+		
+		for(int y=0; y<N; y++) 
+		{
+			st = new StringTokenizer(br.readLine());
+			for(int x=0; x<N; x++) 
+				rtable[y][x] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=1; i<=2; i++) 
+		{
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++)
+				action[i][j] = Integer.parseInt(st.nextToken())-1;
+		}
+		
+		System.out.print(backtracking(N-1) ? 1 : 0);
+		
+	}
+}
