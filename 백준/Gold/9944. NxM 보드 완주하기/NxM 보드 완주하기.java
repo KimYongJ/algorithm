8270bd8ed5dd997a9e461653a4cdcebb14b5da1a
YongJu Kim <kyjdummy@gmail.com>
2024-06-19 16:27:32 +0900

    [Gold III] Title: NxM 보드 완주하기, Time: 308 ms, Memory: 15808 KB -BaekjoonHub

--- 백준/Gold/9944. NxM 보드 완주하기/NxM 보드 완주하기.java
+++ 백준/Gold/9944. NxM 보드 완주하기/NxM 보드 완주하기.java
@@ -5,11 +5,11 @@ import java.util.StringTokenizer;
 
 class Main{
 	
-	static int xy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int totalBlank;
-	static int result;
-	static int Y, X;
-	static boolean visit[][];
+	static int		dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+	static int		totalBlank;
+	static int		result;
+	static int		Y, X;
+	static boolean	visit[][];
 	
 	public static void backtracking(int y, int x, int mCnt, int vCnt) {
 		if(vCnt == totalBlank) 					// 모든칸을 방문했을 경우
@@ -18,33 +18,40 @@ class Main{
 			return;
 		}
 		if(result <= mCnt) 						// 이미 구한 값보다 꺾은 횟수가 같거나 클 경우 미리 종료
+		{
 			return;
+		}
 		
 		int ny, nx, cnt;
-		for(int i=0; i<4; i++)
+		for(int xy[] : dxy)
 		{
 			cnt = 0;
 			ny = y; 
 			nx = x;
-			while(true) {
-				ny += xy[i][0];
-				nx += xy[i][1];
-				if(ny>=0 && nx>=0 && ny<Y && nx<X && !visit[ny][nx]) {
+			while(true) 
+			{
+				ny += xy[0];
+				nx += xy[1];
+				if(ny>=0 && nx>=0 && ny<Y && nx<X && !visit[ny][nx]) 
+				{
 					visit[ny][nx] = true;
 					cnt ++;
-				}else {
-					ny -= xy[i][0];
-					nx -= xy[i][1];
+				}
+				else 
+				{
+					ny -= xy[0];
+					nx -= xy[1];
 					break;
 				}
 			}
 			if(cnt > 0) 
 			{
 				backtracking(ny, nx, mCnt + 1, vCnt + cnt);
-				while(cnt--> 0) {
+				while(cnt--> 0) 
+				{
 					visit[ny][nx] = false;
-					ny -= xy[i][0];
-					nx -= xy[i][1];
+					ny -= xy[0];
+					nx -= xy[1];
 				}
 			}
 		}
@@ -59,17 +66,18 @@ class Main{
 		String str = br.readLine(); // 맵크기 입력 받음 
 		while(str != null && str.length() > 0) 
 		{
-			st		= new StringTokenizer(str);
-			Y		= Integer.parseInt(st.nextToken());
-			X		= Integer.parseInt(st.nextToken());
-			visit	= new boolean[Y][X];
-			result	= Integer.MAX_VALUE;
-			totalBlank = 0;
+			st			= new StringTokenizer(str);
+			Y			= Integer.parseInt(st.nextToken());
+			X			= Integer.parseInt(st.nextToken());
+			result		= Integer.MAX_VALUE;
+			totalBlank	= 0;
+			visit		= new boolean[Y][X];
+			
 			for(int y=0; y<Y; y++) 
 			{
 				str = br.readLine();
 				for(int x=0; x<X; x++) 
-				{
+				{ 
 					if(str.charAt(x) == '.')
 						totalBlank++;
 					else 
@@ -77,9 +85,9 @@ class Main{
 				}
 			}
 			
-			for(int y=0; y<Y; y++)
+			for(int y=0; y<Y; y++) 
 			{
-				for(int x=0; x<X; x++)
+				for(int x=0; x<X; x++) 
 				{
 					if(!visit[y][x]) 
 					{
@@ -91,8 +99,9 @@ class Main{
 			}
 			
 			if(result == Integer.MAX_VALUE)
+			{
 				result = -1;
-			
+			}
 			sb.append("Case ").append(T++).append(": ")
 				.append(result).append('\n');
 			str = br.readLine(); // 맵크기 입력 받음
