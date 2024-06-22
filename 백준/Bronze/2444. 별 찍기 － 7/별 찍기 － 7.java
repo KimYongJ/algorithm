5c1dc4d68a06716d3a968ff026a1733122aa0b18
YongJu Kim <kyjdummy@gmail.com>
2024-06-22 21:52:54 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,38 +1,38 @@
 // https://github.com/kimyongj/algorithm
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		
-		for(int i=1; i<N; i++) {
+		for(int i=1; i<=N; i++) {
 			for(int j=N-i; j>0; j--) {
-				sb.append(' ');
+				bw.write(' ');
 			}
 			for(int j=0; j<i*2-1; j++) {
-				sb.append('*');
+				bw.write('*');
 			}
-			sb.append('\n');
+			bw.write('\n');
 		}
-		
-		for(int i=0; i<N*2-1; i++)
-			sb.append('*');
-		sb.append('\n');
 		
 		for(int i=1; i<N; i++) {
 			for(int j=0; j<i; j++) {
-				sb.append(' ');
+				bw.write(' ');
 			}
 			for(int j=0; j<(N-i)*2-1; j++) {
-				sb.append('*');
+				bw.write('*');
 			}
-			sb.append('\n');
+			bw.write('\n');
 		}
-		
-		System.out.print(sb);
+
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
