ddcb151b21b89158dfb942864a989fe1d1987122
YongJu Kim <kyjdummy@gmail.com>
2024-06-24 20:58:21 +0900

    [Gold II] Title: 2048 (Easy), Time: 280 ms, Memory: 28948 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -11,106 +11,82 @@ class Main{
 	static int original[][];
 	static int order[];
 	
-	public static void move(int m) {
-		// 블록을 내린다.
-		if(m == 0) {
-			for(int x=0; x<N; x++) {
-				for(int y=N-2; y>= 0; y--) {
-					int nextY = y;
-					while(nextY < N-1 &&map[nextY][x] > 0 && map[nextY+1][x] == 0) {
-						map[nextY+1][x] = map[nextY][x];
-						map[nextY][x] = 0;
-						nextY++;
-					}
-				}
-			}
-		}
-		// 블록을 올린다.
-		else if(m == 1) {
-			for(int x=0; x<N; x++) {
-				for(int y=1; y<N; y++) {
-					int nextY = y;
-					while(nextY > 0 && map[nextY][x] > 0 && map[nextY-1][x] == 0) {
-						map[nextY-1][x] = map[nextY][x];
-						map[nextY][x] = 0;
-						nextY--;
-					}
-				}
-			}
-		}
-		// 블록을 왼쪽으로 보낸다.
-		else if(m == 2) {
-			for(int y=0; y<N; y++) {
-				for(int x=1; x<N; x++) {
-					int nextX = x;
-					while(nextX > 0 && map[y][nextX] > 0 && map[y][nextX-1] == 0) {
-						map[y][nextX-1] = map[y][nextX];
-						map[y][nextX] = 0;
-						nextX--;
-					}
-				}
-			}
-		}
-		// 블록을 오른쪽으로 보낸다.
-		else {
-			for(int y=0; y<N; y++) {
-				for(int x=N-2; x>=0; x--) {
-					int nextX = x;
-					while(nextX <N-1 && map[y][nextX] > 0 && map[y][nextX+1] == 0) {
-						map[y][nextX+1] = map[y][nextX];
-						map[y][nextX] = 0;
-						nextX++;
-					}
-				}
-			}
-		}
-	}
-	public static void sum(int m) {
+	public static int[][] sum(int m) {
+		int newMap[][] = new int[N][N];
 		// 블록을 내렸을 때 
 		if(m == 0) { 
-			for(int x=0; x<N; x++) {
-				for(int y=N-1; y>0; y--) {
-					if(map[y][x] > 0 && map[y][x] == map[y-1][x]) {
-						map[y][x] *=2;
-						map[y-1][x]=0;
-					}
-					
-				}
-			}
+            for(int c=0; c<N; c++) {
+                int nextIdx = N-1, baseBlock = -1;
+                for(int r=N-1; r>=0; r--) {
+                    if(map[r][c]==0) {
+                        continue;
+                    }
+                    if(map[r][c]==baseBlock) {
+                        newMap[nextIdx+1][c]  <<=1;
+                        baseBlock=-1;
+                        result = Math.max(result, newMap[nextIdx+1][c]);
+                    }else {
+                        newMap[nextIdx--][c] =baseBlock = map[r][c];
+                    }
+                }
+            }
 		}
 		// 블록을 올렸을 때 
 		else if(m == 1) {
-			for(int x=0; x<N; x++) {
-				for(int y=0; y<N-1; y++) {
-					if(map[y][x] > 0 && map[y][x] == map[y+1][x]) {
-						map[y][x] *=2;
-						map[y+1][x]=0;
-					}
-				}
+			for(int c=0; c<N; c++) {
+                int nextIdx = 0, baseBlock = -1;
+                for(int r=0; r<N; r++) {
+                    if(map[r][c]==0) {
+                        continue;
+                    }
+                    if(map[r][c]==baseBlock) {
+                        newMap[nextIdx-1][c] <<=1; 
+                        baseBlock=-1;
+                        result = Math.max(result, newMap[nextIdx-1][c]); 
+                    }else {
+                        newMap[nextIdx++][c] =baseBlock = map[r][c];
+                       
+                    }
+                }
 			}
 		}
 		// 블록을 왼쪽으로 보냈을 때 
 		else if(m==2) {
-			for(int y=0; y<N; y++) {
-				for(int x=0; x<N-1; x++) {
-					if(map[y][x] > 0 && map[y][x] == map[y][x+1]) {
-						map[y][x] *= 2;
-						map[y][x+1]=0;
-					}
-				}
-			}
+            for(int r=0; r<N; r++) {
+                int nextIdx = 0, baseBlock = -1;
+                for(int c=0; c<N; c++) {
+                    if(map[r][c]==0) {
+                        continue;
+                    }
+                    if(map[r][c]==baseBlock) {
+                        newMap[r][nextIdx-1]  <<=1;
+                        baseBlock=-1;
+                        result = Math.max(result, newMap[r][nextIdx-1]);
+                    }else {
+                        newMap[r][nextIdx++] =baseBlock = map[r][c];
+                    }
+                }
+            }
 		}
 		// 블록을 오른쪽으로 보냈을 때 
 		else {
-			for(int y=0; y<N; y++) {
-				for(int x=N-1; x>0; x--) {
-					if(map[y][x] > 0 && map[y][x] == map[y][x-1]) {
-						map[y][x] *=2;
-						map[y][x-1]=0;
-					}
-				}
-			}
+            for(int r=0; r<N; r++) {
+                int nextIdx =N-1, baseBlock = -1;
+                for(int c=N-1; c>=0; c--) {
+                    if(map[r][c]==0) {
+                        continue;
+                    }
+                    if(map[r][c]==baseBlock) {
+                        newMap[r][nextIdx+1]  <<=1;
+                        baseBlock=-1;
+                        result = Math.max(result, newMap[r][nextIdx+1]);
+                    }else {
+                        newMap[r][nextIdx--] =baseBlock = map[r][c];
+                    }
+                }
+            }
 		}
+		return newMap;
 	}
 	public static void cal() {
 		map = new int[N][N];
@@ -119,13 +95,9 @@ class Main{
 				map[y][x] = original[y][x];
 
 		for(int m : order) 
-		{
-			move(m);	// 해당 방향으로 이동
-			sum(m);		// 붙어있는 것들에 대해 하나로 합친다.
-			move(m);	// 해당 방향으로 이동 
-		}
+			map = sum(m);		// 붙어있는 것들에 대해 하나로 합치며 연산한다. 
 		
-		// 이하 결과 계산
+			// 이하 결과 계산
 		for(int y=0; y<N; y++) 
 			for(int x=0; x<N; x++)
 				result = Math.max(map[y][x], result);
