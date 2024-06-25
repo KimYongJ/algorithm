5b1c34b58fb2ca1e6cd149f9e187faa9e344877b
YongJu Kim <kyjdummy@gmail.com>
2024-06-25 11:34:26 +0900

    [Gold II] Title: 색종이 붙이기, Time: 216 ms, Memory: 19416 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -6,14 +6,15 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int result = 26;
-	static int oneCnt;
 	static int map[][];
 	static int paper[] = {0,5,5,5,5,5};
 	
 	public static void change(int y, int x, int len, int before, int after) {
-		for(int y1=y; y1<y+len; y1++) 
+		int ylen = y + len;
+		int xlen = x + len;
+		for(int y1=y; y1<ylen; y1++) 
 		{
-			for(int x1=x; x1<x+len; x1++) 
+			for(int x1=x; x1<xlen; x1++) 
 			{
 				if(map[y1][x1] == before) 
 				{
@@ -23,12 +24,14 @@ class Main{
 		}
 	}
 	public static boolean validate(int len, int y, int x) {
-		if(y+len > 10 || x+len > 10) {
+		int ylen = y + len;
+		int xlen = x + len;
+		if(ylen > 10 || xlen > 10) {
 			return false;
 		}
-		for(int y1=y; y1<y+len; y1++) 
+		for(int y1=y; y1<ylen; y1++) 
 		{
-			for(int x1=x; x1<x+len; x1++) 
+			for(int x1=x; x1<xlen; x1++) 
 			{
 				if(map[y1][x1] != 1) 
 				{
@@ -41,29 +44,42 @@ class Main{
 	public static void backtracking(int usepaper, int y, int x) {
 		if(result < usepaper) 
 			return;
-		if(x == 10) {
+		if(x == 10) 
+		{
 			x = 0;
 			y +=1;
 		}
-		if(y == 10) {
+		if(y < 10 && map[y][x] == 0)  // 0인 경우 0이 아닌 위치 까지 y, x를 변화 시킴
+		{
+			int nextY = y;
+			int nextX = x;
+			while(nextY < 10 && map[nextY][nextX] == 0) {
+				nextX ++ ;
+				if(nextX == 10) {
+					nextX = 0;
+					nextY ++;
+				}
+			}
+			y = nextY;
+			x = nextX;
+		}
+		if(y == 10) 
+		{
 			result = Math.min(result,  usepaper);
 			return;
 		}
-		if(map[y][x] == 0) 
+		for(int i=5; i>=1; i--) 
 		{
-			backtracking(usepaper, y, x+1);
-		}else {
-			for(int i=5; i>=1; i--) 
+			if(validate(i,y,x) && paper[i] > 0) 
 			{
-				if(validate(i,y,x) && paper[i] > 0) {
-					paper[i]--;
-					change(y,x,i,1,0);
-					backtracking(usepaper+1, y, x+1);
-					change(y,x,i,0,1);
-					paper[i]++;
-				}
+				paper[i]--;
+				change(y,x,i,1,0);
+				backtracking(usepaper+1, y, x+1);
+				change(y,x,i,0,1);
+				paper[i]++;
 			}
 		}
+		
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
@@ -75,10 +91,6 @@ class Main{
 			for(int x=0; x<10; x++)
 			{
 				map[y][x] = Integer.parseInt(st.nextToken());
-				if(map[y][x] == 1)
-				{
-					oneCnt++;
-				}
 			}
 		}
 		
