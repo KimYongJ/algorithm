ba8bb1b536462916787267fb674d59c5d5d59bcd
YongJu Kim <kyjdummy@gmail.com>
2024-06-30 13:38:17 +0900

    [Gold V] Title: 숌 사이 수열, Time: 104 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
+++ 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
@@ -1,9 +1,6 @@
 // https://github.com/kimyongj/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -12,16 +9,18 @@ class Main{
 	static int		base[];
 	static int		result[];
 	static boolean	visit[];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 
 	public static boolean backtracking(int idx) {
 		if(idx == len) 
-		{
 			return true;
-		}
 		if(result[idx] != -1) 
-		{
 			return backtracking(idx + 1);
-		}
 		
 		for(int i=0; i<N; i++)
 		{
@@ -41,21 +40,16 @@ class Main{
 	}
 
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N		= Integer.parseInt(br.readLine());
+		N		= read();
 		len		= N<<1;
 		base	= new int[N];
 		result	= new int[len];
 		visit	= new boolean[N];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
 		{
-			base[i] = Integer.parseInt(st.nextToken());
-		}
-		for(int i=0; i<len; i++) {
-			result[i] = -1;
+			base[i] = read();
+			result[i] = result[N+i] = -1;
 		}
 		
 		Arrays.sort(base);
@@ -67,8 +61,7 @@ class Main{
 				sb.append(r).append(' ');
 			System.out.print(sb);
 		}
-		else {
+		else
 			System.out.print(-1);
-		}
 	}
 }
