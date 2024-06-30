90521e2f1f6b0b83945c6dd4cbed112031a5e063
YongJu Kim <kyjdummy@gmail.com>
2024-06-30 12:47:47 +0900

    [Gold V] Title: 숌 사이 수열, Time: 116 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
+++ 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
@@ -1,10 +1,7 @@
 // https://github.com/kimyongj/algorithm
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -53,9 +50,8 @@ class Main{
 		backtracking(0);
 		
 		if(list.size() == 0) 
-		{
 			System.out.print(-1);
-		}
+		
 		else 
 		{
 			Collections.sort(list,(a,b)->{
