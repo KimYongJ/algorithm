c442205444ff274a6d0ffdbef015ba359bcecb89
YongJu Kim <kyjdummy@gmail.com>
2024-06-30 12:46:52 +0900

    [Gold V] Title: 숌 사이 수열, Time: 120 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
+++ 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
@@ -15,6 +15,12 @@ class Main{
 	static boolean	visit[];
 	static ArrayList<int[]> list = new ArrayList<>();
 
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void backtracking(int depth) {
 		if(depth == N) 
 		{
@@ -35,19 +41,14 @@ class Main{
 	}
 
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N		= Integer.parseInt(br.readLine());
+		N		= read();
 		len		= N<<1;
 		base	= new int[N];
 		result	= new int[len];
 		visit	= new boolean[len];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++) 
-		{
-			base[i] = Integer.parseInt(st.nextToken());
-		}
+			base[i] = read();
 		
 		backtracking(0);
 		
