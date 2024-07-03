dd6c4b2cd97e5b894dbc6195c745508d8a17b8d4
YongJu Kim <kyjdummy@gmail.com>
2024-07-03 15:28:18 +0900

    [Gold IV] Title: 마약수사대, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Gold/17220. 마약수사대/마약수사대.java
@@ -0,0 +1,63 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Node{
+	int node;
+	Node next;
+	Node(int node, Node next){this.node=node; this.next=next;}
+}
+class Main{
+	
+	static int N;
+	static Node adNode[];
+	static boolean visit[];
+	static boolean isRoot[]; // 루트인 것은 false 값을 갖는 배열
+	public static int DFS(int node) {
+		int cnt = 1;
+		for(Node now=adNode[node]; now!=null; now=now.next) 
+		{
+			if(!visit[now.node]) 
+			{
+				visit[now.node] = true;
+				cnt += DFS(now.node);
+			}
+		}
+		return cnt;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N		= Integer.parseInt(st.nextToken());
+		adNode	= new Node[N];
+		visit	= new boolean[N];
+		isRoot	= new boolean[N];
+		int T	= Integer.parseInt(st.nextToken());
+		int a,b;
+		while(T-->0) 
+		{
+			st = new StringTokenizer(br.readLine());
+			a = st.nextToken().charAt(0) - 'A';
+			b = st.nextToken().charAt(0) - 'A';
+			adNode[a] = new Node(b, adNode[a]);
+			isRoot[b] = true;	// 루트가 아닌 것은 true, 루트인 것은 false
+		}
+		st	= new StringTokenizer(br.readLine());
+		T	= Integer.parseInt(st.nextToken());
+		while(T-->0) 
+		{
+			visit[ st.nextToken().charAt(0) - 'A'] = true;
+		}
+		
+		int cnt = 0;
+		for(int i=0; i<N; i++) 
+		{
+			if(!isRoot[i] && !visit[i]) 
+			{
+				visit[i] = true;
+				cnt += DFS(i)-1;
+			}
+		}
+		System.out.print(cnt);
+	}
+}
