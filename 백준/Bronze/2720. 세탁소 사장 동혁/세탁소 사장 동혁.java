16af2c86bb3c54cff16777971c0f8df75c82c0df
YongJu Kim <kyjdummy@gmail.com>
2024-07-04 11:01:19 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 100 ms, Memory: 13888 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,22 +1,25 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder	sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0) 
 		{
-			int num = Integer.parseInt(br.readLine()) * 100;
+			int num = read() * 100;
 			sb.append(num / 2500).append(' ');
 			num %= 2500;
 			sb.append(num / 1000).append(' ');
 			num %= 1000;
 			sb.append(num / 500).append(' ');
-			sb.append((num %= 500)/100).append('\n');
+			sb.append((num % 500)/100).append('\n');
 		}
 		System.out.print(sb.toString());
 	}
