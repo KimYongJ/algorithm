68aa4c5f9ebcbed062a4c4a43b843afbc2b3a5d3
YongJu Kim <kyjdummy@gmail.com>
2024-07-05 17:05:58 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 408 ms, Memory: 20020 KB -BaekjoonHub

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.java
@@ -0,0 +1,32 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int result = 0;
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i=0; i<N; i++) {
+			int cnt = 0;
+			for(int j=i+1; j<N; j++) {
+				if(arr[i] > arr[j]) {
+					cnt++;
+				}else {
+					break;
+				}
+			}
+			if(result < cnt) {
+				result = cnt;
+			}
+		}
+		System.out.print(result);
+	}
+}
