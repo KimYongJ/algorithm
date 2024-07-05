40aa7916b175adf53a16d7924c519c4ea17252bb
YongJu Kim <kyjdummy@gmail.com>
2024-07-05 16:15:53 +0900

    [Bronze II] Title: 거스름돈, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,13 +1,16 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 class Main{
 
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
 		final int arr[]		= {500, 100, 50, 10, 5, 1};
-		int num				= 1000 - Integer.parseInt(br.readLine());
+		int num				= 1000 - read();
 		int result 			= 0;
 		
 		for(int i=0; i<arr.length && num != 0; i++) 
