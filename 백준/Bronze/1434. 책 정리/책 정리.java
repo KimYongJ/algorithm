de51526f4300bd915f2bd17071424bd66460a54a
YongJu Kim <kyjdummy@gmail.com>
2024-07-05 14:10:12 +0900

    [Bronze II] Title: 책 정리, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/1434. 책 정리/책 정리.java
+++ 백준/Bronze/1434. 책 정리/책 정리.java
@@ -12,25 +12,27 @@ class Main{
 		int N		= read();
 		int M		= read();
 		int box[]	= new int[N];
-		int book[]	= new int[M];
 		
 		for(int i=0; i<N; i++) {box[i]	= read();}
-		for(int i=0; i<M; i++) {book[i] = read();}
 		
-		int boxIdx = 0;			// 박스 인덱스
-		for(int i=0; i<M; i++)	// 책인덱스
+		int boxIdx = 0;						// 박스 인덱스
+		int book = read();
+		for(int i=0; i<M; i++)				// 책 인덱스
 		{
-			if(box[boxIdx] - book[i] < 0) // 박스에 책을 못 넣는다면 책인덱스 +1 을 하고 낭비된 공간 더하기.
+			if(box[boxIdx] - book < 0)	// 박스에 책을 못 넣는다면 책인덱스 +1 을 하고 낭비된 공간 더하기.
 			{
 				result += box[boxIdx++];
 			}
-			else {
-				box[boxIdx] -= book[i];
-				book[i] = 0;
+			else
+			{
+				box[boxIdx] -= book;
+				book = 0;
 			}
-			if(book[i] != 0)	// 책을 넣지 못하였다면 책 인덱스를 -1을 하여 원래 인덱스로 돌림(추후 +1을하니까)
+			if(book != 0)				// 책을 넣지 못하였다면 책 인덱스를 -1을 하여 원래 인덱스로 돌림(추후 +1을하니까)
 			{
 				i--;
+			}else {
+				book = read();
 			}
 		}
 		for(int i=boxIdx; i<N; i++) 
