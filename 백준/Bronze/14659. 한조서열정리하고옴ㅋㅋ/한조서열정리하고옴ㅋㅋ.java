c382264a7ba5ccad3133516f238e32c02cc185ec
YongJu Kim <kyjdummy@gmail.com>
2024-07-05 17:13:32 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 228 ms, Memory: 19064 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.java
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.java
@@ -2,32 +2,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 class Main{
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int result = 0;
-		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		for(int i=0; i<N; i++) {
-			int cnt = 0;
-			for(int j=i+1; j<N; j++) {
-				if(arr[i] > arr[j]) {
-					cnt++;
-				}else {
-					i = j-1;
-					break;
-				}
+		int result	= 0;
+		int N		= Integer.parseInt(br.readLine());
+		st			= new StringTokenizer(br.readLine());
+		int cnt		= 0;
+		int num1	= 0;
+		for(int i=0; i<N; i++) 
+		{
+			int num2 = Integer.parseInt(st.nextToken());
+			if(num1 > num2) 
+			{
+				cnt++;
 			}
-			if(result < cnt) {
-				result = cnt;
+			else 
+			{
+				result	= Math.max(result, cnt);
+				num1	= num2;
+				cnt		= 0;
 			}
 		}
-		System.out.print(result);
+		System.out.print(Math.max(result, cnt));
 	}
 }
