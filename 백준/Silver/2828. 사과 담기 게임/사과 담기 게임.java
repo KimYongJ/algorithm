72ee8436560c7367492a53b4fddc663244752084
YongJu Kim <kyjdummy@gmail.com>
2024-07-07 21:25:57 +0900

    [Silver V] Title: 사과 담기 게임, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -5,27 +5,30 @@ import java.util.StringTokenizer;
 
 class Main{
 	
-	public static int getNext(int b, int s, int e) {
-		if(s <= b && b<=e) {return 0;}
-		else if(b<s) {return -(s-b);}
-		else {return b-e;}
-	}
-	
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N		= Integer.parseInt(st.nextToken());
 		int end		= Integer.parseInt(st.nextToken());
-		int start	= 1;
 		int T		= Integer.parseInt(br.readLine());
+		int start	= 1;
 		int result	= 0;
+		int move;
 		while(T-- > 0) 
 		{
-			int num		= Integer.parseInt(br.readLine());
-			int move	= getNext(num, start, end);
-			result		+= Math.abs(move);
-			start		+= move;
-			end			+= move;
+			int num	= Integer.parseInt(br.readLine());
+			if(num < start)
+			{
+				result	+= move = (start-num);
+				start	-= move;
+				end		-= move;
+			}
+			else if(end < num) 
+			{
+				result	+= move = num - end;
+				start	+= move;
+				end		+= move;
+			}
 		}
 		System.out.print(result);
 	}
