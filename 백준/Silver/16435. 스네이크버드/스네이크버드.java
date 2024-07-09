1ddc5f637662f19400d7cefe6af5ac849aa66351
YongJu Kim <kyjdummy@gmail.com>
2024-07-09 11:38:59 +0900

    [Silver V] Title: 스네이크버드, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.java
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -1,28 +1,28 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken()); // 과일 개수 N
-		int L		= Integer.parseInt(st.nextToken()); // 초기 길이
+		int N		= read(); // 과일 개수 N
+		int L		= read(); // 초기 길이
 		int arr[]	= new int[N];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
 		{
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		}
 		
 		Arrays.sort(arr);
 		
-		for(int a : arr) 
+		for(int i=0; i<N; i++) 
 		{
-			if(a > L) 
+			if(arr[i] > L) 
 			{
 				break;
 			}
