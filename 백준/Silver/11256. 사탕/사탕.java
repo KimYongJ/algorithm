60ba1aa7f16a4d439f8f0ee8daba4d490647aa62
YongJu Kim <kyjdummy@gmail.com>
2024-07-09 09:58:41 +0900

    [Silver V] Title: 사탕, Time: 152 ms, Memory: 15380 KB -BaekjoonHub

--- 백준/Silver/11256. 사탕/사탕.java
+++ 백준/Silver/11256. 사탕/사탕.java
@@ -1,36 +1,42 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 class Main{
+	
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder	sb = new StringBuilder();
-		PriorityQueue<Integer> pq;
 		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
-		int J, N, cnt;
+		int J, N, cnt, arr[];
 		
 		while(T-->0)
 		{
-			pq	= new PriorityQueue<Integer>((a,b)->b-a);
 			st	= new StringTokenizer(br.readLine());
 			J	= Integer.parseInt(st.nextToken()); // 사탕 개수
 			N	= Integer.parseInt(st.nextToken()); // 상자의 개수
-			while(N-->0) 
+			arr = new int[N];
+			cnt = 0;
+			
+			for(int i=0; i<N; i++) 
 			{
 				st = new StringTokenizer(br.readLine());
-				pq.add(Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+				arr[i] = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
 			}
-			cnt = 0;
-			while(J>0) 
+			
+			Arrays.sort(arr);
+			
+			while(N-->0 && J > 0) 
 			{
-				J -= pq.poll();
 				cnt++;
+				J -= arr[N];
 			}
+			
 			sb.append(cnt).append('\n');
 		}
 		System.out.print(sb);
 	}
+	
 }
