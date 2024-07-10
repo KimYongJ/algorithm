dbf0b5b7c96dc856848525b917e251cc12f5f035
YongJu Kim <kyjdummy@gmail.com>
2024-07-10 16:26:15 +0900

    [Silver IV] Title: 당근 키우기, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.java
@@ -0,0 +1,14 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int Y = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		System.out.print(Y + X + Math.min(Y, X)/10);
+	}
+}
