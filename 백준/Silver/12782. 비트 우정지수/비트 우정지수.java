127fcfbd4d29a01ce5b7736456758b1b58e9f4d2
YongJu Kim <kyjdummy@gmail.com>
2024-07-10 14:16:02 +0900

    [Silver IV] Title: 비트 우정지수, Time: 224 ms, Memory: 23016 KB -BaekjoonHub

--- 백준/Silver/12782. 비트 우정지수/비트 우정지수.java
+++ 백준/Silver/12782. 비트 우정지수/비트 우정지수.java
@@ -1,24 +1,17 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder	sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		int _0, _1, len;
-		String a,b;
+		StringBuilder sb = new StringBuilder();
+		IN in = new IN();
+		int T = in.nextInt();
+		
 		while(T-->0) 
 		{
-			st	= new StringTokenizer(br.readLine());
-			_0	= 0;
-			_1	= 0;
-			a	= st.nextToken();
-			b	= st.nextToken();
-			len = a.length();
+			String a	= in.nextString();
+			String b	= in.nextString();
+			int len		= a.length();
+			int _0		= 0;
+			int _1		= 0;
 			for(int i=0; i<len; i++) 
 			{
 				char c = a.charAt(i);
@@ -39,4 +32,42 @@ class Main{
 		}
 		System.out.print(sb.toString());
 	}
+}
+class IN {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
