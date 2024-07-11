9c3131c05f0300eb26e9b3442691741dca37042e
YongJu Kim <kyjdummy@gmail.com>
2024-07-11 16:04:56 +0900

    [Silver IV] Title: 팬덤이 넘쳐흘러, Time: 300 ms, Memory: 37404 KB -BaekjoonHub

--- 백준/Silver/17262. 팬덤이 넘쳐흘러/팬덤이 넘쳐흘러.java
+++ 백준/Silver/17262. 팬덤이 넘쳐흘러/팬덤이 넘쳐흘러.java
@@ -1,48 +1,27 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
-class Node{
-	int s, e;
-	Node(int s, int e) {this.s=s; this.e=e;}
-}
+
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList<Node> list = new ArrayList<>();
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		int s,e;
+		int s,e, min = 9999999, max = 0;
 		for(int i=0; i<N; i++) 
 		{
 			st = new StringTokenizer(br.readLine());
 			s = Integer.parseInt(st.nextToken());
 			e = Integer.parseInt(st.nextToken());
-			list.add(new Node(s,e));
-		}
-		
-		Collections.sort(list, (a,b)->{
-			return a.e - b.e; // 종료가 가장 빠른 수를 앞으로
-		});
-		s = list.get(0).e;
-		int cnt = 0;
-		for(int i=0; i<N; i++) {
-			Node now = list.get(i);
-			if(now.s <= s && s<= now.e) {
-				cnt++;
+			if(min > e) {
+				min = e;
+			}
+			if(max < s) {
+				max = s;
 			}
 		}
-		if(cnt != N) {
-			Collections.sort(list,(a,b)->{
-				return b.s-a.s; // 시작이 제일 큰수를 앞으로 
-			});
-			e = list.get(0).s;
-			System.out.print(e - s);
-		}
-		else {
-			System.out.print(0);
-		}
+		int result = max - min;
+		System.out.print(result > 0 ? result : 0);
 	}
 }
