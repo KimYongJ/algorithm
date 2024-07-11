f6ef244b6fd3c2b4ce0867bd0fe35f9b448969a2
YongJu Kim <kyjdummy@gmail.com>
2024-07-11 16:05:52 +0900

    [Silver IV] Title: 팬덤이 넘쳐흘러, Time: 132 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/17262. 팬덤이 넘쳐흘러/팬덤이 넘쳐흘러.java
+++ 백준/Silver/17262. 팬덤이 넘쳐흘러/팬덤이 넘쳐흘러.java
@@ -1,23 +1,25 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
+		int N = read();
 		int s,e, min = 9999999, max = 0;
 		for(int i=0; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			s = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
-			if(min > e) {
+			s = read();
+			e = read();
+			if(min > e) 
+			{
 				min = e;
 			}
-			if(max < s) {
+			if(max < s) 
+			{
 				max = s;
 			}
 		}
