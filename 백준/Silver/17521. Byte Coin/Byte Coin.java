e911ee017b774f9da9d54f31d6a03debd90692e4
YongJu Kim <kyjdummy@gmail.com>
2024-07-11 16:51:35 +0900

    [Silver IV] Title: Byte Coin, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/17521. Byte Coin/Byte Coin.java
+++ 백준/Silver/17521. Byte Coin/Byte Coin.java
@@ -1,21 +1,23 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());
-		long W		= Long.parseLong(st.nextToken());
+		int N		= read();
+		long W		= read();
 		long coin	= 0;
 		long arr[]	= new long[N+1];
 		
-		arr[0] = Integer.parseInt(br.readLine());
+		arr[0] = read();
 		
 		for(int i=1; i<N; i++) 
 		{
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 			if(arr[i-1] < arr[i]) // 내일이 오를 경우 
 			{
 				if(W / arr[i-1] > 0) 
@@ -30,6 +32,6 @@ class Main{
 				coin = 0;
 			}
 		}
-		System.out.print(W + coin * arr[N-1]);
+		System.out.print(W + coin * arr[N-1]); // 모든 코인을 판다.
 	}
 }
