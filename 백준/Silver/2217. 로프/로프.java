11d57a76a5cfa18b6564e3e1f078b4ceb850c02d
YongJu Kim <kyjdummy@gmail.com>
2024-07-12 15:07:30 +0900

    [Silver IV] Title: 로프, Time: 192 ms, Memory: 21840 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,28 +1,21 @@
 // https://github.com/kimyongj/algorithm
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 class Main{
-	
-	static int read() throws Exception {// 빠른 입력을 위한 함수
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
-		return n;
-	}
-	
 	public static void main(String[] args)throws Exception{
-		int N		= read();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N		= Integer.parseInt(br.readLine());
 		int result	= 0;
-		int arr[]	= new int[N];
+		int arr[]	= new int[10001];
 		
 		for(int i=0; i<N; i++) 
 		{
-			arr[i] = read();
+			arr[Integer.parseInt(br.readLine())]++;
 		}
 		
-		Arrays.sort(arr);
-		
-		for(int i=N-1, cnt = 1; i>=0; i--,cnt++) 
-		{
-			result = Math.max(result, arr[i] * cnt);
+		for(int i=10000, j = 0; i>=0; i--) {
+			j += arr[i];
+			result = Math.max(result, i * j);
 		}
 		System.out.print(result);
 	}
