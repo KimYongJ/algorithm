d8453f3a3de34dbf076aaf1dc35ea47ee6fe0936
YongJu Kim <kyjdummy@gmail.com>
2024-07-13 17:09:57 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 444 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
@@ -1,16 +1,14 @@
 //https://github.com/KimYongJ/algorithm
 class Main{
-	
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
-	
 	public static void main(String[] args)throws Exception{
 		int cnt = 0, before = 0, now = 0;
 		int N = read();
-		for(int i=0; i<N; i++)
+		while(N-->0) 
 		{
 			now = read();
 			if(now >= before) 
