48445c859924b83404d6447bdf18edff79423052
YongJu Kim <kyjdummy@gmail.com>
2024-07-13 17:08:23 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 452 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
@@ -1,19 +1,18 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int cnt = 0, before = 0;
-		
-		st = new StringTokenizer(br.readLine());
+		int cnt = 0, before = 0, now = 0;
+		int N = read();
 		for(int i=0; i<N; i++)
 		{
-			int now = Integer.parseInt(st.nextToken());
+			now = read();
 			if(now >= before) 
 			{
 				cnt++;
