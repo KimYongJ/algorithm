4d1588f970663b88c441931492fe16db3f7e9a66
YongJu Kim <kyjdummy@gmail.com>
2024-07-14 14:53:37 +0900

    [Silver IV] Title: 알바생 강호, Time: 128 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.java
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -11,21 +11,15 @@ class Main{
 		long sum	= 0;
 		int arr[]	= new int[N];
 		
-		for(int i=0; i<N; i++) 
-		{
-			arr[i] = read() + 1;
-		}
+		for(int i=0; i<N; i++) arr[i] = read() + 1;
 		
 		Arrays.sort(arr);
 		
 		for(int i=N-1, j = 1; i>=0; i--, j++) 
-		{
 			if(arr[i] - j > 0)
-			{
 				sum += arr[i] - j;
-			}
-			else break;
-		}
+			else 
+				break;
 		
 		System.out.print(sum);
 	}
