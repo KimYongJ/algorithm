099e0d551d787a309ec26952b3c696d2391f18ba
YongJu Kim <kyjdummy@gmail.com>
2024-07-14 15:06:55 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -1,26 +1,25 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int ans		= 0;
 		int arr[]	= new int[N];
 		
-		for(int i=0; i<N; i++){
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
+		for(int i=0; i<N; i++)
+			arr[i] = read();
 		
 		for(int i=N-2; i>=0; i--) 
-		{
 			if(arr[i] >= arr[i+1]) 
 			{
 				ans += arr[i] - (arr[i+1] - 1);
 				arr[i] = (arr[i+1] - 1);
 			}
-		}
+		
 		System.out.print(ans);
 	}
 }
