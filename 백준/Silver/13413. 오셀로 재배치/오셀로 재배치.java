b0ac0785123005a58b2984d26b62bfe315c3fea5
YongJu Kim <kyjdummy@gmail.com>
2024-07-14 21:44:14 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 268 ms, Memory: 26640 KB -BaekjoonHub

--- 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.java
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.java
@@ -7,15 +7,19 @@ class Main{
 		while(T-->0) 
 		{
 			int len = in.nextInt();
-			String s1 = in.nextString();	// 말의 초기상태
-			String s2 = in.nextString(); 	// 목표 상태
+			char s1[] = new char[len];
 			int w = 0, b = 0;				// w와 b의 개수
 			
+			for(int i=0; i<len; i++) 
+			{
+				s1[i] = in.nextChar();
+			}
 			for(int i=0; i<len; i++)
 			{
-				if(s1.charAt(i) != s2.charAt(i)) 
+				char c = in.nextChar();
+				if(s1[i] != c) 
 				{
-					if(s1.charAt(i) == 'W') 
+					if(c == 'W') 
 					{
 						w++;
 					}else 
@@ -31,40 +35,41 @@ class Main{
 }
 
 class IN {
-    final int SIZE = 1 << 13;
-    byte[] buffer = new byte[SIZE];
-    int index, size;
+	final int SIZE = 1 << 15;
+	byte[] buffer = new byte[SIZE];
+	int index, size;
 
-    String nextString() throws Exception {
-        StringBuilder sb = new StringBuilder();
-        byte c;
-        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
-        do sb.appendCodePoint(c);
-        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
-        return sb.toString();
-    }
+	int nextInt() throws Exception{
+		int n = 0;
+		byte c;
+		while ((c = read()) <= 32)
+			;
+		boolean neg = c == '-';
+		if (neg)
+			c = read();
+		do
+			n = (n << 3) + (n << 1) + (c & 15); while (isNumber(c = read()));
+		return neg ? -n : n;
+	}
 
-    int nextInt() throws Exception {
-        int n = 0;
-        byte c;
-        boolean isMinus = false;
-        while ((c = read()) <= 32) { if (size < 0) return -1; }
-        if (c == 45) { c = read(); isMinus = true; }
-        do n = (n << 3) + (n << 1) + (c & 15);
-        while (isNumber(c = read()));
-        return isMinus ? ~n + 1 : n;
-    }
+	boolean isNumber(byte c) {
+		return 47 < c && c < 58;
+	}
 
+	char nextChar() throws Exception{
+		byte c;
+		while ((c = read()) <= 32)
+			;
+		return (char)c;
+	}
 
-    boolean isNumber(byte c) {
-        return 47 < c && c < 58;
-    }
-
-    byte read() throws Exception {
-        if (index == size) {
-            size = System.in.read(buffer, index = 0, SIZE);
-            if (size < 0) buffer[0] = -1;
-        }
-        return buffer[index++];
-    }
+	byte read() throws Exception{
+		if (index == size) {
+			size = System.in.read(buffer, index = 0, SIZE);
+			if (size < 0)
+				buffer[0] = -1;
+		}
+		return buffer[index++];
+	}
 }
+
