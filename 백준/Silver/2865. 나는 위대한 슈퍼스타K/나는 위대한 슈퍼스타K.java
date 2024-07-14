a009bc99c5c5606c56efb9367bd595f4fe9c8c5e
YongJu Kim <kyjdummy@gmail.com>
2024-07-14 15:44:02 +0900

    [Silver IV] Title: 나는 위대한 슈퍼스타K, Time: 128 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Silver/2865. 나는 위대한 슈퍼스타K/나는 위대한 슈퍼스타K.java
+++ 백준/Silver/2865. 나는 위대한 슈퍼스타K/나는 위대한 슈퍼스타K.java
@@ -1,9 +1,5 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 class Node{
 	int n; double a;
 	Node(int n, double a){this.n=n; this.a=a;}
@@ -11,22 +7,20 @@ class Node{
 class Main{
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq	= new PriorityQueue<Node>((a,b)->Double.compare(b.a,a.a));
-		BufferedReader	br		= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st		= new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 참가인원
-		int M = Integer.parseInt(st.nextToken()); // 장르 개수
-		int K = Integer.parseInt(st.nextToken()); // 본선에 나갈 인원
+		I in  = new I();
+		int N = in.nextInt(); // 참가인원
+		int M = in.nextInt(); // 장르 개수
+		int K = in.nextInt(); // 본선에 나갈 인원
 		int n;
 		double a, ans = 0.0;
 		boolean visit[] = new boolean[N+1];
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++) 
 			{
-				n = Integer.parseInt(st.nextToken()); // 참가번호
-				a = Double.parseDouble(st.nextToken());// 능력치
+				n = in.nextInt();		// 참가번호
+				a = in.nextDouble();	// 능력치
 				pq.add(new Node(n,a));
 			}
 		}
@@ -42,9 +36,46 @@ class Main{
 					break;
 				}
 			}
-			
 		}
-		
 		System.out.print(String.format("%.1f", ans));
 	}
+}
+
+class I {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
