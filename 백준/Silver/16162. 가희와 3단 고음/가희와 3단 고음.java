9b5d007fdabe37e2d85ac7f58075304fffaa244a
YongJu Kim <kyjdummy@gmail.com>
2024-07-15 16:53:06 +0900

    [Silver IV] Title: 가희와 3단 고음, Time: 192 ms, Memory: 19228 KB -BaekjoonHub

--- 백준/Silver/16162. 가희와 3단 고음/가희와 3단 고음.java
+++ 백준/Silver/16162. 가희와 3단 고음/가희와 3단 고음.java
@@ -9,27 +9,14 @@ class Main{
 		int N = Integer.parseInt(st.nextToken()); // 참가자들의 음의 개수 
 		int A = Integer.parseInt(st.nextToken()); // 고음의 첫항
 		int D = Integer.parseInt(st.nextToken()); // 공차
-		int arr[] = new int[N];
 		int X = 0;
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
 		for(int i=0; i<N; i++) 
 		{
-			if(arr[i] == A)
+			int num = Integer.parseInt(st.nextToken());
+			if(num == A + (X*D))
 			{
-				int x = 1;
-				for(int j=i;j<N; j++) 
-				{
-					if(arr[i]+(x*D) == arr[j]) {
-						x++;
-					}
-				}
-				if(X < x) {
-					X = x;
-					break;
-				}
+				X++;
 			}
 		}
 		System.out.print(X);
