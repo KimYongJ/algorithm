6fd80c463b3b6d96374f7c03f015fbd9db18c2c3
YongJu Kim <kyjdummy@gmail.com>
2024-07-15 09:22:21 +0900

    [Silver IV] Title: 2+1 세일, Time: 228 ms, Memory: 21476 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -1,17 +1,21 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[]	= new int[N];
 		int res		= 0;
 		
 		for(int i=0; i<N; i++) 
 		{
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 		}
 		
 		Arrays.sort(arr);
