b89a1b9dfec25cf4d7d2c0369a1365fe11a4b31b
YongJu Kim <kyjdummy@gmail.com>
2024-07-15 13:38:07 +0900

    [Silver IV] Title: 떡국, Time: 200 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/20937. 떡국/떡국.java
+++ 백준/Silver/20937. 떡국/떡국.java
@@ -1,20 +1,19 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[]	= new int[50001];
 		int max		= 0;
 		int num;
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++)
 		{
-			num = Integer.parseInt(st.nextToken());
+			num = read();
 			if(++arr[num] > max) 
 			{
 				max = arr[num];
