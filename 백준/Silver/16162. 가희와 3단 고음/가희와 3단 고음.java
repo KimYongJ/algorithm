d8860f9066a0bef7ba406808adb22a2b0c09b303
YongJu Kim <kyjdummy@gmail.com>
2024-07-15 16:55:35 +0900

    [Silver IV] Title: 가희와 3단 고음, Time: 140 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/16162. 가희와 3단 고음/가희와 3단 고음.java
+++ 백준/Silver/16162. 가희와 3단 고음/가희와 3단 고음.java
@@ -1,21 +1,22 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 참가자들의 음의 개수 
-		int A = Integer.parseInt(st.nextToken()); // 고음의 첫항
-		int D = Integer.parseInt(st.nextToken()); // 공차
+		int N = read(); // 참가자들의 음의 개수 
+		int A = read(); // 고음의 첫항
+		int D = read(); // 공차
 		int X = 0;
-		st = new StringTokenizer(br.readLine());
+		int num;
 		for(int i=0; i<N; i++) 
 		{
-			int num = Integer.parseInt(st.nextToken());
-			if(num == A + (X*D))
+			num = read();
+			if(num == A)
 			{
+				A+=D;
 				X++;
 			}
 		}
