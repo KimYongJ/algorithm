69b7d7b3a537724d086e40324068a19ae1e966f6
YongJu Kim <kyjdummy@gmail.com>
2024-07-16 11:11:57 +0900

    [Silver III] Title: 수리공 항승, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -1,34 +1,36 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken()) * 10;
-		int R = 0;
-		boolean visit[] = new boolean[10011];
+		int L = Integer.parseInt(st.nextToken());
+		int R = 1;
+		int arr[] = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
 		{
-			int num = Integer.parseInt(st.nextToken()) * 10;
-			for(int j=num-5; j<num+5; j++) 
-			{
-				visit[j] = true;
-			}
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		for(int i=0; i<10011; i++) {
-			if(visit[i]) {
-				R++;
-				int j = i;
-				for(;j<i+L && j<10011; j++) {
-					visit[j] = false;
-				}
-				i = j - 1;
+		
+		Arrays.sort(arr);
+		
+		double start = arr[0] - 0.5;
+		double end = start + L;
+		for(int i=1; i<N; i++)
+		{
+			if(arr[i] + 0.5 <= end) 
+			{
+				continue;
 			}
+			start = arr[i] - 0.5;
+			end = start + L;
+			R++;
 		}
 		System.out.print(R);
 	}
