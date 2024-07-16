56f0f524247f993b5e1efe2f07586950322525d4
YongJu Kim <kyjdummy@gmail.com>
2024-07-16 11:13:58 +0900

    [Silver III] Title: 수리공 항승, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -1,37 +1,33 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
+		int N = read();
+		int L = read();
 		int R = 1;
 		int arr[] = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++) 
-		{
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
-		double start = arr[0] - 0.5;
-		double end = start + L;
+		double start	= arr[0] - 0.5;
+		double end		= start + L;
+		
 		for(int i=1; i<N; i++)
-		{
-			if(arr[i] + 0.5 <= end) 
+			if(arr[i] + 0.5 > end) 
 			{
-				continue;
+				start	= arr[i] - 0.5;
+				end		= start + L;
+				R++;
 			}
-			start = arr[i] - 0.5;
-			end = start + L;
-			R++;
-		}
+
 		System.out.print(R);
 	}
 }
