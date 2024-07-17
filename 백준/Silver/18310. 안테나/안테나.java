03bd2a1f9274d9178cbcca7b9776ae922268a4e5
YongJu Kim <kyjdummy@gmail.com>
2024-07-17 11:37:04 +0900

    [Silver III] Title: 안테나, Time: 320 ms, Memory: 32124 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,22 +1,28 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
 		int N				= Integer.parseInt(br.readLine());
-		int arr[]			= new int[N];
+		int idx				= N / 2 + (N % 2 != 0 ? 1 : 0) ;
+		int arr[]			= new int[100001];
 		StringTokenizer st	= new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
-			arr[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(arr);
-		
-		int idx = N / 2;
-		if(N % 2 == 0) 
-			idx--;
-		System.out.print(arr[idx]);
+			arr[Integer.parseInt(st.nextToken())]++;
+				
+		for(int i=0; i<=100000; i++) 
+		{
+			if(arr[i] != 0) 
+			{
+				idx -= arr[i];
+				if(idx <= 0) 
+				{
+					System.out.print(i);
+					return;
+				}
+			}
+		}
 	}
 }
