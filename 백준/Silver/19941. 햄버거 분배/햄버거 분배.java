b108fefb836c16823dfc292586aaba41c931b212
YongJu Kim <kyjdummy@gmail.com>
2024-07-17 16:39:32 +0900

    [Silver III] Title: 햄버거 분배, Time: 112 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -0,0 +1,32 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N			= Integer.parseInt(st.nextToken());
+		int K			= Integer.parseInt(st.nextToken());
+		char arr[]		= br.readLine().toCharArray();
+		int left, right, cnt = 0;
+		for(int i=0; i<N; i++) 
+		{
+			if(arr[i] == 'P') 
+			{
+				left	= Math.max(i - K, 0) - 1;
+				right	= Math.min(i + K, N-1);
+				while(++left <= right) 
+				{
+					if(arr[left] == 'H') 
+					{
+						arr[left] = 'X';
+						cnt++;
+						break;
+					}
+				}
+			}
+		}
+		System.out.print(cnt);
+	}
+}
