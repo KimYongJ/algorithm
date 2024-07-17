222c29ae6968725c625e6f84c9ac9834e2a81474
YongJu Kim <kyjdummy@gmail.com>
2024-07-17 09:18:28 +0900

    [Silver III] Title: 모두의 마블, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/12845. 모두의 마블/모두의 마블.java
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.java
@@ -1,17 +1,17 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int N	= read();
 		int sum = 0;
 		int max = 0;
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
 		{
-			int n = Integer.parseInt(st.nextToken());
+			int n = read();
 			sum += n;
 			if(max < n) 
 			{
