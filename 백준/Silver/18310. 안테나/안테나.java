c253ebc0184ce48c6416a18f0390a1867f56478e
YongJu Kim <kyjdummy@gmail.com>
2024-07-17 11:39:42 +0900

    [Silver III] Title: 안테나, Time: 132 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,28 +1,20 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
-		int N				= Integer.parseInt(br.readLine());
+		int N				= read();
 		int idx				= N / 2 + (N % 2 != 0 ? 1 : 0) ;
 		int arr[]			= new int[100001];
-		StringTokenizer st	= new StringTokenizer(br.readLine());
+		
 		for(int i=0; i<N; i++) 
-			arr[Integer.parseInt(st.nextToken())]++;
-				
-		for(int i=0; i<=100000; i++) 
-		{
-			if(arr[i] != 0) 
-			{
-				idx -= arr[i];
-				if(idx <= 0) 
-				{
-					System.out.print(i);
-					return;
-				}
-			}
-		}
+			arr[read()]++;
+		
+		for(int i=0; i<=100000 && idx > 0; i++) 
+			if((idx-= arr[i]) <= 0) 
+				System.out.print(i);
 	}
 }
