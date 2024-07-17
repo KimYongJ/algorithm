03beb9bed81d2c858c0c12e15773b86814e73102
YongJu Kim <kyjdummy@gmail.com>
2024-07-17 10:47:13 +0900

    [Silver III] Title: 카드 문자열, Time: 192 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Silver/13417. 카드 문자열/카드 문자열.java
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.java
@@ -1,24 +1,18 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder	sb = new StringBuilder();
-		StringBuilder	str;
-		StringTokenizer st;
-		char	c, first;
-		int		N, T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringBuilder str;
+		IN in = new IN();
+		int T = in.nextInt();
 		while(T-->0) 
 		{
-			first	= 'a';
-			str		= new StringBuilder();
-			N		= Integer.parseInt(br.readLine());
-			st		= new StringTokenizer(br.readLine());
+			char first	= 'a';
+			int N		= in.nextInt();
+			str			= new StringBuilder();
 			for(int i=0; i<N; i++) 
 			{
-				c = st.nextToken().charAt(0);
+				char c = in.nextChar();
 				if(first >= c) 
 				{
 					first = c;
@@ -33,4 +27,38 @@ class Main{
 		}
 		System.out.print(sb.toString());
 	}
+}
+class IN {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
