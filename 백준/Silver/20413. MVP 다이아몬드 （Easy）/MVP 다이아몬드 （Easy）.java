e3350f214227924d444bf34017b9fdf887b72a62
YongJu Kim <kyjdummy@gmail.com>
2024-07-18 16:57:23 +0900

    [Silver II] Title: MVP 다이아몬드 (Easy), Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/20413. MVP 다이아몬드 （Easy）/MVP 다이아몬드 （Easy）.java
+++ 백준/Silver/20413. MVP 다이아몬드 （Easy）/MVP 다이아몬드 （Easy）.java
@@ -1,7 +1,4 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static int getIndex(char c) {
 		switch(c) 
@@ -14,32 +11,61 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		READ read	= new READ();
+		int N		= read.nextInt();
 		int money[] = new int[5];
 		int before	= 0;
 		int result	= 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<4; i++)
 		{
-			money[i] = Integer.parseInt(st.nextToken()) - 1;
+			money[i] = read.nextInt() - 1;
 		}
 		money[4] = money[3]+1;
 		
-		String str = br.readLine();
 		for(int i=0; i<N; i++) 
 		{
-			int idx = getIndex(str.charAt(i));
+			int idx = getIndex(read.nextChar());
 			if(idx == 4) 
 			{
 				result += money[idx];
 			}
 			else 
 			{
-				result += before = money[idx] - before;;
+				result += before = money[idx] - before;
 			}
 		}
 		System.out.print(result);
 	}
+}
+
+class READ {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
