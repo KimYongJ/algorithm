eadc52e4a9b0bce812d85d5ff0de2532e757b144
YongJu Kim <kyjdummy@gmail.com>
2024-07-19 16:29:31 +0900

    [Silver II] Title: 5차 전직, Time: 656 ms, Memory: 53296 KB -BaekjoonHub

--- 백준/Silver/16112. 5차 전직/5차 전직.java
+++ 백준/Silver/16112. 5차 전직/5차 전직.java
@@ -1,28 +1,28 @@
 //https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int arr[] = new int[N];
-		long k = 0;
-		long sum = 0;
+		int N		= Integer.parseInt(st.nextToken());
+		int K		= Integer.parseInt(st.nextToken());
+		long k		= 0;
+		long sum	= 0;
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) 
 		{
-			arr[i] = Integer.parseInt(st.nextToken());
+			pq.add(Integer.parseInt(st.nextToken()));
 		}
 		
-		Arrays.sort(arr);
-		
-		for(int i=0; i<N; i++) {
-			sum += arr[i] * k;
-			if(k<K) {
+		for(int i=0; i<N; i++) 
+		{
+			sum += pq.poll() * k;
+			if(k<K) 
+			{
 				k++;
 			}
 		}
