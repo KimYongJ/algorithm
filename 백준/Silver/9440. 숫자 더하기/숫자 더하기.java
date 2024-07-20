ff03b4d0619ab5c3f1c2dcde947ce20121567b4f
YongJu Kim <kyjdummy@gmail.com>
2024-07-20 21:27:27 +0900

    [Silver II] Title: 숫자 더하기, Time: 112 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/9440. 숫자 더하기/숫자 더하기.java
+++ 백준/Silver/9440. 숫자 더하기/숫자 더하기.java
@@ -7,26 +7,21 @@ class Main{
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
-	public static long getNum(ArrayList<Integer> list) {
-		long idx = 1;
-		long sum = 0;
-		for(int i=list.size()-1; i>=0; i--) {
-			sum += list.get(i) * idx;
-			idx *= 10;
-		}
-		return sum;
-	}
 	public static void main(String[] args)throws Exception{
 		StringBuilder sb = new StringBuilder();
-		ArrayList<Integer> arr, list1, list2;
+		StringBuilder list1 = new StringBuilder();
+		StringBuilder list2 = new StringBuilder();
+		ArrayList<Integer> arr;
+		boolean flag;
 		int firstNonZero;
 		int num = read();
 		while(num != 0) 
 		{
 			arr		= new ArrayList<>();
-			list1	= new ArrayList<>();
-			list2	= new ArrayList<>();
+			flag	= true;
 			firstNonZero = -1;
+			list1.setLength(0);
+			list2.setLength(0);
 			
 			for(int i=0; i<num; i++)
 				arr.add(read());
@@ -35,25 +30,21 @@ class Main{
 
 			while(arr.get(++firstNonZero) == 0);// 0이 아닌 수를 찾음
 			
-			list1.add(arr.get(firstNonZero));// 0이 아닌 수를 list1과 list2에 각각 넣음
+			list1.append(arr.get(firstNonZero));// 0이 아닌 수를 list1과 list2에 각각 넣음
 			arr.remove(firstNonZero);
-			list2.add(arr.get(firstNonZero));// 0이 아닌 수를 list1과 list2에 각각 넣음
+			list2.append(arr.get(firstNonZero));// 0이 아닌 수를 list1과 list2에 각각 넣음
 			arr.remove(firstNonZero);
 			
 			// list1과 list2에 각각 하나씩 숫자를 넣는다.
-			for(int i=0; i<arr.size(); i++)
+			for(int i=0; i<arr.size(); i++) 
 			{
-				if(list1.size() == list2.size())
-				{
-					list1.add(arr.get(i));
-				}
-				else 
-				{
-					list2.add(arr.get(i));
-				}
+				if(flag)list1.append(arr.get(i));
+				else	list2.append(arr.get(i));
+				flag = !flag;
 			}
-			
-			sb.append(getNum(list1) + getNum(list2)).append('\n');
+
+			sb.append(Integer.parseInt(list1.toString()) + Integer.parseInt(list2.toString()))
+				.append('\n');
 			
 			num = read();
 		}
