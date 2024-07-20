a50ddb17fa517dc77758b58f886de9d28e620638
YongJu Kim <kyjdummy@gmail.com>
2024-07-20 21:19:24 +0900

    [Silver II] Title: 숫자 더하기, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

--- 백준/Silver/9440. 숫자 더하기/숫자 더하기.java
+++ 백준/Silver/9440. 숫자 더하기/숫자 더하기.java
@@ -1,11 +1,12 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static long getNum(ArrayList<Integer> list) {
 		long idx = 1;
 		long sum = 0;
@@ -16,52 +17,45 @@ class Main{
 		return sum;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		ArrayList<Integer> arr, list1, list2;
 		StringBuilder sb = new StringBuilder();
-		int num = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> arr, list1, list2;
+		int firstNonZero;
+		int num = read();
 		while(num != 0) 
 		{
 			arr		= new ArrayList<>();
 			list1	= new ArrayList<>();
 			list2	= new ArrayList<>();
+			firstNonZero = -1;
+			
 			for(int i=0; i<num; i++)
-			{
-				arr.add(Integer.parseInt(st.nextToken()));
-			}
+				arr.add(read());
 			
 			Collections.sort(arr);
-			// 0이 아닌 가장 작은 값을 list1에 넣는다.
-			Iterator<Integer> ite = arr.iterator();
-			while(ite.hasNext()) {
-				int n = ite.next();
-				if(n != 0) {
-					if(list1.isEmpty()) {
-						list1.add(n);
-						ite.remove();
-					}
-					else if(list2.isEmpty()) {
-						list2.add(n);
-						ite.remove();
-						break;
-					}
-				}
-			}
+
+			while(arr.get(++firstNonZero) == 0);// 0이 아닌 수를 찾음
+			
+			list1.add(arr.get(firstNonZero));// 0이 아닌 수를 list1과 list2에 각각 넣음
+			arr.remove(firstNonZero);
+			list2.add(arr.get(firstNonZero));// 0이 아닌 수를 list1과 list2에 각각 넣음
+			arr.remove(firstNonZero);
 			
 			// list1과 list2에 각각 하나씩 숫자를 넣는다.
-			for(int i=0; i<arr.size(); i++) {
-				if(list1.size() == list2.size()) {
+			for(int i=0; i<arr.size(); i++)
+			{
+				if(list1.size() == list2.size())
+				{
 					list1.add(arr.get(i));
-				}else {
+				}
+				else 
+				{
 					list2.add(arr.get(i));
 				}
 			}
 			
 			sb.append(getNum(list1) + getNum(list2)).append('\n');
 			
-			st = new StringTokenizer(br.readLine());
-			num = Integer.parseInt(st.nextToken());
+			num = read();
 		}
 		System.out.print(sb.toString());
 	}
