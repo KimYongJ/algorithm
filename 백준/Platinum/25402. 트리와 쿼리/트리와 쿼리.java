46bb736c75af84d58fca89023f3eccca9f0536cd
YongJu Kim <kyjdummy@gmail.com>
2024-07-21 19:54:16 +0900

    [Platinum V] Title: 트리와 쿼리, Time: 1676 ms, Memory: 166344 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.java
@@ -2,6 +2,7 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Node{
@@ -14,27 +15,37 @@ class Node{
 class Main{
 	
 	static int N;
+	static int cnt[];
 	static int arr[];
+	static int parent[];
+	static int treeParent[];
+	static boolean v[];
 	static Node adNode[];
-	static boolean visit[];
-	public static int DFS(int node) {
-		int cnt = 1;
-		
-		for(Node now=adNode[node]; now!=null; now=now.next) {
-			if(visit[now.node]) {
-				visit[now.node] = false;
-				cnt += DFS(now.node);
+	
+	public static void makeParent(int node, int prev) {
+		for(Node now=adNode[node]; now!=null; now=now.next) 
+		{
+			if(now.node != prev) 
+			{
+				treeParent[now.node]= node; 
+				makeParent(now.node, node);
 			}
 		}
-		
-		return cnt;
+	}
+	static int find(int node) {
+		return parent[node] == node ? node : (parent[node] = find(parent[node]));
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader	br	= new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st	= new StringTokenizer(br.readLine());
-		StringBuilder sb	= new StringBuilder();
-		N		= Integer.parseInt(st.nextToken());
-		adNode	= new Node[N+1];
+		StringBuilder	sb	= new StringBuilder();
+		N			= Integer.parseInt(st.nextToken());
+		arr			= new int[N+1];
+		cnt			= new int[N+1];
+		parent		= new int[N+1];
+		treeParent	= new int[N+1];
+		v			= new boolean[N+1];
+		adNode		= new Node[N+1];
 		
 		int a,b;
 		for(int i=1; i<N; i++) 
@@ -45,32 +56,47 @@ class Main{
 			adNode[a] = new Node(b, adNode[a]);
 			adNode[b] = new Node(a, adNode[b]);
 		}
+		for(int i=0; i<=N; i++) {
+			cnt[i] = 1;
+			parent[i] = i;
+		}
 		
-		int q = Integer.parseInt(br.readLine());
-		while(q-->0) 
+		makeParent(1,-1);
+		
+		int Q = Integer.parseInt(br.readLine());
+		while(Q-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			arr = new int[a];
-			visit = new boolean[N+1];
-			for(int i=0; i<a; i++) 
+			st		= new StringTokenizer(br.readLine());
+			long res = 0;
+			int K	= Integer.parseInt(st.nextToken());
+			
+			for(int i=0; i<K; i++) 
 			{
-				b = Integer.parseInt(st.nextToken());
-				arr[i] = b;
-				visit[b] = true;
+				arr[i]		= Integer.parseInt(st.nextToken());
+				v[arr[i]]	= true;
 			}
-			int res = 0;
-			for(int start : arr) 
+
+			for(int i=0; i<K; i++) 
 			{
-				int cnt = 0;
-				if(visit[start]) {
-					visit[start] = false;
-					cnt += DFS(start);
+				if(v[treeParent[arr[i]]])
+				{
+					int parent1 = find(arr[i]);
+					int parent2 = find(treeParent[arr[i]]);
+					res += (long) cnt[parent1] * cnt[parent2];
+					parent[parent1] = parent2;
+					cnt[parent2] += cnt[parent1];
 				}
-				res += (cnt * (cnt-1)) / 2;
 			}
 			
+			for(int i=0; i<K; i++) 
+			{
+				v[arr[i]]		= false;
+				cnt[arr[i]]		= 1;
+				parent[arr[i]]	= arr[i];
+			}
+
 			sb.append(res).append('\n');
+			
 		}
 		System.out.print(sb.toString());		
 	}
