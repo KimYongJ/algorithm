b98211e3498ba4399e3007ab2e87f86ab42da9e3
YongJu Kim <kyjdummy@gmail.com>
2024-07-22 17:40:17 +0900

    [Silver II] Title: 사다리, Time: 220 ms, Memory: 16908 KB -BaekjoonHub

--- 백준/Silver/3061. 사다리/사다리.java
+++ 백준/Silver/3061. 사다리/사다리.java
@@ -1,37 +1,40 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+    private static int read() throws Exception {
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
+        }
+        boolean minus = false;
+        if (c == '-') {
+            minus = true;
+            c = System.in.read();
+        }
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+        if (minus) return -val;
+        return val;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		while(T-->0) {
+		int T = read();
+		while(T-->0) 
+		{
 			int cnt = 0;
-			int idx = 1;
-			int N = Integer.parseInt(br.readLine())+1;
+			int N = read();
 			int arr[] = new int[N];
-			st = new StringTokenizer(br.readLine());
-			for(int i=1; i<N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-			for(int i=1; i<N; i++) 
+			for(int i=0; i<N; i++) 
 			{
-				for(int j=i; j<N; j++)
-				{
-					if(i == arr[j])
-					{
-						idx = j;
-						break;
-					}
-				}
-				while(arr[i] != i) {
-					int tmp = arr[idx - 1];
-					arr[idx - 1] = arr[idx];
-					arr[idx] = tmp;
-					idx--;
+				arr[i] = read();
+			}
+			for(int i=1; i<N; i++)					// 1번 인덱스부터 맞춰감
+			{
+				for(int j=i-1; j>=0 && arr[j] > arr[j + 1]; j--) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
 					cnt++;
 				}
 			}
