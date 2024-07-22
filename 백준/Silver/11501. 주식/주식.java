d0e4e713623d0eb86dd67cef4bf0dfb9f625995e
YongJu Kim <kyjdummy@gmail.com>
2024-07-22 10:15:00 +0900

    [Silver II] Title: 주식, Time: 1104 ms, Memory: 316784 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -10,35 +10,22 @@ class Main{
 		int T = Integer.parseInt(br.readLine());
 		while(T-->0) 
 		{
-			int day			= Integer.parseInt(br.readLine());
-			long start		= Long.MIN_VALUE;
-			long res		= 0;
-			long sum		= 0;
-			int cnt			= 0;
-			long price[]	= new long[day];
-			boolean flag[]	= new boolean[day];
+			int day		= Integer.parseInt(br.readLine());
+			int price[]	= new int[day];
 			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<day; i++) 
 			{
 				price[i] = Integer.parseInt(st.nextToken());
 			}
+			long res = 0;
+			int start = price[day-1];
 			for(int i=day-1; i>=0; i--) 
 			{
-				if(price[i] > start) 
+				if(price[i] > start)
 					start = price[i];
-				else flag[i] = true;
+				else
+					res += start - price[i];
 			}
-			for(int i=0; i<day; i++) {
-				if(flag[i]) {
-					cnt++;
-					sum += price[i];
-				}else {
-					res += (price[i] * cnt) - sum;
-					cnt = 0;
-					sum = 0;
-				}
-			}
-			
 			sb.append(res).append('\n');
 		}
 		System.out.print(sb.toString());
