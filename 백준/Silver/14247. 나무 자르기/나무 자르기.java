53c0f26605277e46dea01fdbccb8ae117985e880
YongJu Kim <kyjdummy@gmail.com>
2024-07-22 11:42:44 +0900

    [Silver II] Title: 나무 자르기, Time: 260 ms, Memory: 20384 KB -BaekjoonHub

--- 백준/Silver/14247. 나무 자르기/나무 자르기.java
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.java
@@ -1,34 +1,31 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 class Node{
 	int base, inc;
 	Node(int base, int inc){this.base=base; this.inc=inc;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
 		ArrayList<Node> list = new ArrayList<>();
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			list.add(new Node(Integer.parseInt(st.nextToken()), 0));
-		}
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			list.get(i).inc = Integer.parseInt(st.nextToken());
-		}
+		int N = read();
+		
+		for(int i=0; i<N; i++)list.add(new Node(read(), 0));
+		for(int i=0; i<N; i++)list.get(i).inc = read();
+	
 		Collections.sort(list,(a,b)->{
 			if(a.inc == b.inc)return b.base - a.base;
 			return a.inc - b.inc;
 		});
 
 		long sum = 0;
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<N; i++) 
+		{
 			Node node = list.get(i);
 			sum += node.base + (node.inc * (long)(i));
 		}
