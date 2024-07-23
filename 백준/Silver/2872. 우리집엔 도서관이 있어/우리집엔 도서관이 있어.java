e1c6da792ffd39ae9b1f1526daf15261deb14045
YongJu Kim <kyjdummy@gmail.com>
2024-07-23 14:31:18 +0900

    [Silver I] Title: 우리집엔 도서관이 있어, Time: 184 ms, Memory: 15488 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.java
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.java
@@ -1,35 +1,36 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[]	= new int[N];
 		int idx		= 0;
-		int cnt		= 1;
 		for(int i=0; i<N; i++) 
 		{
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 			if(arr[i] == N) 
 			{
 				idx = i;
 			}
 		}
 		if(arr[0] == N) 
-		{
 			System.out.print(N-1);
-			return;
-		}
-		int base = arr[idx];
-		for(int i=idx-1; i>=0; i--) 
+		else 
 		{
-			if(base-1 == arr[i]) 
+			int cnt	 = 1;
+			for(int i=idx-1; i>=0; i--) 
 			{
-				--base;
-				++cnt;
+				if(arr[idx]-1 == arr[i]) 
+				{
+					--arr[idx];
+					++cnt;
+				}
 			}
+			System.out.print(N - cnt);
 		}
-		System.out.print(N - cnt);
 	}
 }
