411492805fceacc284a103375f1847ee2e5dfd72
YongJu Kim <kyjdummy@gmail.com>
2024-07-23 10:52:40 +0900

    [Silver II] Title: 숫자 놀이, Time: 104 ms, Memory: 13932 KB -BaekjoonHub

--- 백준/Silver/2777. 숫자 놀이/숫자 놀이.java
+++ 백준/Silver/2777. 숫자 놀이/숫자 놀이.java
@@ -1,37 +1,37 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0) 
 		{
-			int n	= Integer.parseInt(br.readLine());
+			int n = read();
+			if(n == 1) 
+			{
+				sb.append(1).append('\n');
+				continue;
+			}
 			int cnt = 0;
-			if(n >= 10) {
-				int bef = 0;
-				while(bef != n) 
+			int bef = 0;
+			while(bef != n) 
+			{
+				bef = n;
+				for(int i=9; i>=2; i--) 
 				{
-					bef = n;
-					for(int i=9; i>=2; i--) 
+					if(n % i == 0) 
 					{
-						if(n % i == 0) 
-						{
-							cnt++;
-							n /= i;
-							break;
-						}
+						cnt++;
+						n /= i;
+						break;
 					}
 				}
-				if(n>=10) {
-					cnt = -1;
-				}
-			}else {
-				cnt = 1;
 			}
-			sb.append(cnt).append('\n');
+			sb.append(n>=10 ? -1 : cnt).append('\n');
 		}
 		System.out.print(sb.toString());
 	}
