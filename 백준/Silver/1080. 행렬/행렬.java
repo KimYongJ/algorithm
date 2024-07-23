d71edde9b50959b87d7f9c8437fa19615d3fb3ef
YongJu Kim <kyjdummy@gmail.com>
2024-07-23 16:31:26 +0900

    [Silver I] Title: 행렬, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -0,0 +1,50 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int Y = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int cnt = 0;
+		boolean arr[][][] = new boolean[2][Y][X];
+		for(int i=0; i<2; i++) 
+		{
+			for(int y=0; y<Y; y++)
+			{
+				String str = br.readLine();
+				for(int x=0; x<X; x++) 
+				{
+					arr[i][y][x] = str.charAt(x) == '1';
+				}
+			}
+		}
+		for(int y=0; y<Y-2; y++) 
+		{
+			for(int x=0; x<X-2; x++) 
+			{
+				if(arr[0][y][x] != arr[1][y][x]) {
+					cnt ++ ;
+					for(int y1=y; y1<y+3; y1++) {
+						for(int x1=x; x1<x+3; x1++) {
+							arr[0][y1][x1] = !arr[0][y1][x1];
+						}
+					}
+				}
+			}
+		}
+		
+		Loop :
+		for(int y=0; y<Y; y++) {
+			for(int x=0; x<X; x++) {
+				if(arr[0][y][x] != arr[1][y][x]) {
+					cnt = -1;
+					break Loop;
+				}
+			}
+		}
+		System.out.print(cnt);
+	}
+}
