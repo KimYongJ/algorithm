f452a193868f25e4ed4dfe419e471c2f187d86bd
YongJu Kim <kyjdummy@gmail.com>
2024-07-24 17:02:04 +0900

    [Silver I] Title: 체인, Time: 468 ms, Memory: 65988 KB -BaekjoonHub

--- 백준/Silver/2785. 체인/체인.java
+++ 백준/Silver/2785. 체인/체인.java
@@ -1,27 +1,23 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for(int i=0; i<N; i++) 
-		{
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
+			pq.add(Integer.parseInt(st.nextToken()));
+
 		int sum	= 0;
 		int cnt = 0;
 		for(int i=0; i<N; i++) 
 		{
-			sum += arr[i];		// 지금까지 만든 체인개수
+			sum += pq.poll();	// 지금까지 만든 체인개수
 			cnt = N - 1 - i;	// 특정 인덱스일 때 남은 체인 개수
 			if(sum >= cnt) {
 				break;
