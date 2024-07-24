b81232395c60fe860aaf6ff7a58d1f970499c1ab
YongJu Kim <kyjdummy@gmail.com>
2024-07-24 15:46:26 +0900

    [Silver I] Title: 신입 사원, Time: 1236 ms, Memory: 99452 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,25 +1,24 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int a,b;
 	Node(int a, int b){this.a=a; this.b=b;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder	sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int T = read();
 		while(T-->0) {
 			PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.a-b.a);
-			int N = Integer.parseInt(br.readLine());
+			int N = read();
 			for(int i=0; i<N; i++)
 			{
-				st = new StringTokenizer(br.readLine());
-				pq.add(new Node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+				pq.add(new Node(read(),read()));
 			}
 			int cnt = 1;
 			int score = pq.poll().b;
