6c2d9f2b9438b3d217fdf5d3ecf5a13884dc68ed
YongJu Kim <kyjdummy@gmail.com>
2024-07-24 11:09:45 +0900

    [Silver I] Title: 뒤집기 II, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/1455. 뒤집기 II/뒤집기 II.java
+++ 백준/Silver/1455. 뒤집기 II/뒤집기 II.java
@@ -6,30 +6,26 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y	= Integer.parseInt(st.nextToken());
-		int X	= Integer.parseInt(st.nextToken());
-		int res = 0;
-		boolean visit[][] = new boolean[Y][X];
-		for(int y=0; y<Y; y++) {
+		int Y		= Integer.parseInt(st.nextToken());
+		int X		= Integer.parseInt(st.nextToken());
+		int res		= 0;
+		int cnt[]	= new int[X];
+		int arr[][]	= new int[Y][X];
+		for(int y=0; y<Y; y++) 
+		{
 			String str = br.readLine();
-			for(int x=0; x<X; x++) {
-				visit[y][x] = str.charAt(x) == '1';
-			}
+			for(int x=0; x<X; x++)
+				arr[y][x] = str.charAt(x)-'0';
 		}
 		
-		for(int y=Y-1; y>=0; y--) {
-			for(int x=X-1; x>=0; x--) {
-				if(visit[y][x]) // 해당 칸이 뒷면(1)이라면 
+		for(int y=Y-1; y>=0; y--)
+			for(int x=X-1; x>=0; x--)
+				if((cnt[x] + arr[y][x])%2 == 1) // 해당 칸이 뒷면(1)이라면 
 				{
 					res++;
-					for(int y1=0; y1<=y; y1++) {
-						for(int x1=0; x1<=x; x1++) {
-							visit[y1][x1] = !visit[y1][x1];
-						}
-					}
+					for(int x1=0; x1<=x; x1++)cnt[x1]++;
 				}
-			}
-		}
+
 		System.out.print(res);
 	}
 }
