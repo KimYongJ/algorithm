21301dc4355e944df3a1dfcd75fa9c3c4da14db6
YongJu Kim <kyjdummy@gmail.com>
2024-07-25 09:37:59 +0900

    [Silver I] Title: 그렇고 그런 사이, Time: 156 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Silver/20921. 그렇고 그런 사이/그렇고 그런 사이.java
+++ 백준/Silver/20921. 그렇고 그런 사이/그렇고 그런 사이.java
@@ -1,41 +1,34 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N		= Integer.parseInt(st.nextToken());
-		int K		= Integer.parseInt(st.nextToken());
+		int N		= read();
+		int K		= read();
 		int arr[]	= new int[N+1];
-		if(K == 0) 
+		
+		for(int i=1; i<=N; i++)
+			arr[i] = i;
+
+		for(int i=1; i<=N && K != 0; i++) 
 		{
-			for(int i=1; i<=N; i++)
-				sb.append(i).append(' ');
-		}
-		else 
-		{
-			for(int i=1; i<=N; i++)
-				arr[i] = i;
-			Loop:
-			for(int i=1; i<=N; i++) 
+			for(int j=N; j>i; j--) 
 			{
-				for(int j=N; j>i; j--) 
-				{
-					int tmp = arr[j];
-					arr[j]	= arr[j-1];
-					arr[j-1]= tmp;
-					if(--K == 0) 
-					{
-						break Loop;
-					}
-				}
+				int tmp = arr[j];
+				arr[j]	= arr[j-1];
+				arr[j-1]= tmp;
+				if(--K == 0) 
+					break;
 			}
-			for(int a=1;a<=N;a++)
-				sb.append(arr[a]).append(' ');
 		}
+		
+		for(int a=1;a<=N;a++)
+			sb.append(arr[a]).append(' ');
+		
 		System.out.print(sb.toString());
 	}
 }
