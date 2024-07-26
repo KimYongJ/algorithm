7ad0ee8b20c417db4bc61a50701c7445ec44b39d
YongJu Kim <kyjdummy@gmail.com>
2024-07-26 11:07:52 +0900

    [Gold V] Title: 파티가 좋아 파티가 좋아, Time: 136 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/4055. 파티가 좋아 파티가 좋아/파티가 좋아 파티가 좋아.java
+++ 백준/Gold/4055. 파티가 좋아 파티가 좋아/파티가 좋아 파티가 좋아.java
@@ -1,16 +1,28 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{int s,e;Node(int s,int e){this.s=s; this.e=e;}}
 class Main{
+	private static int read() throws Exception{
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
+        }
+        boolean minus = false;
+        if (c == '-') {
+            minus = true;
+            c = System.in.read();
+        }
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+        if (minus) return -val;
+        return val;
+    }
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.e-b.e);
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb	= new StringBuilder();
-		StringTokenizer st;
-		int P = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int P = read();
 		int T = 0, cnt, arr[];
 		while(P != 0) 
 		{
@@ -19,8 +31,7 @@ class Main{
 			arr = new int[24];
 			for(int i=0; i<P; i++) 
 			{
-				st = new StringTokenizer(br.readLine());
-				pq.add(new Node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+				pq.add(new Node(read(),read()));
 			}
 			while(!pq.isEmpty()) {
 				Node now = pq.poll();
@@ -34,7 +45,7 @@ class Main{
 			}
 			
 			sb.append(String.format("On day %d Emma can attend as many as %d parties.\n", ++T, cnt));
-			P = Integer.parseInt(br.readLine());
+			P = read();
 		}
 		System.out.print(sb.toString());
 	}
