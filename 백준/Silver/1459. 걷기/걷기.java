de5b478a81d68360840712644e3c7f7f12beddec
YongJu Kim <kyjdummy@gmail.com>
2024-07-27 21:57:17 +0900

    [Silver III] Title: 걷기, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/1459. 걷기/걷기.java
+++ 백준/Silver/1459. 걷기/걷기.java
@@ -1,31 +1,23 @@
 //https://github.com/KimYongJ/algorithm
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	static long read() throws Exception {// 빠른 입력을 위한 함수
+		long c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long X = Integer.parseInt(st.nextToken());
-		long Y = Integer.parseInt(st.nextToken());
-		long W = Integer.parseInt(st.nextToken()); // 가로세로
-		long S = Integer.parseInt(st.nextToken()); // 대각선
+		long X = read();
+		long Y = read();
+		long W = read(); // 가로세로
+		long S = read(); // 대각선
 		long result1 = (X+Y)*(long)W; // 가로세로로만 
-		long result2 = 0; 
+		long result2 = Math.min(X, Y) *S + Math.abs(X-Y)*W;
+		long result3 = 0;
 		if((X+Y) % 2 == 0) // 대각선으로만 갈 수 있다
-		{
-			result2 = Math.max(X, Y) * S;
-		}
+			result3 = Math.max(X, Y) * S;
 		else 
-		{
-			result2 = (Math.max(X, Y)-1) * S + W;
-		}
-		
-		long result3 = Math.min(X, Y) *S + Math.abs(X-Y)*W;
-		
-		
+			result3 = (Math.max(X, Y)-1) * S + W;
+
 		System.out.print(Math.min(result3,Math.min(result1, result2)));
 	}
 }
