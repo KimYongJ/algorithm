fc376e815c01c0c71dc2e2241d09f8947790b364
YongJu Kim <kyjdummy@gmail.com>
2024-07-28 13:17:57 +0900

    [Silver II] Title: 초콜릿 식사, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -1,21 +1,24 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int K = Integer.parseInt(br.readLine());
+		int K = read();
 		int M = (int)Math.pow(2,(int)Math.ceil(Math.log(K) / Math.log(2)));
 		int C = 0;
 		sb.append(M).append(' ');
-		if(K != M) {
-			while(K != 0) {
-				M /= 2;
-				if(K>=M) {
-					K-=M;
-				}
+		if(K != M) 
+		{
+			while(K != 0) 
+			{
 				C++;
+				M /= 2;
+				if(K>=M)
+					K-=M;
 			}
 		}
 		sb.append(C);
