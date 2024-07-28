b778b65587ed38522d50f2c7a4d8707ee62b5f79
YongJu Kim <kyjdummy@gmail.com>
2024-07-28 14:19:39 +0900

    [Silver III] Title: 등수 매기기, Time: 296 ms, Memory: 42720 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -1,25 +1,25 @@
 //https://github.com/KimYongJ/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 class Main{
 	public static void main(String[] args)throws Exception{
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine().trim())+1;
-		boolean visit[] = new boolean[N];
+		int N = Integer.parseInt(br.readLine())+1;
+		int arr[] = new int[500001];
+		
 		for(int i=1; i<N; i++)
-		{
-			int n  = Integer.parseInt(br.readLine().trim());
-			if(n < N && !visit[n]) visit[n] = true;
-			else pq.add(n);
-		}
+			arr[Integer.parseInt(br.readLine())]++;
 		
 		long res = 0;
-		for(int i=1; i<N; i++)
-			if(!visit[i])
-				res += Math.abs(i-pq.poll());
-
+		int rnk = 1;
+		for(int i=1; i<500001; i++) 
+		{
+			while(arr[i]-- > 0) {
+				res += Math.abs(i - rnk++);
+			}
+			if(rnk == N)
+				break;
+		}
 		System.out.print(res);
 	}
 }
