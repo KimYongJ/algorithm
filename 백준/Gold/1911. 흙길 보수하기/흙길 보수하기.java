704b6de96f46ca83ef8e0f8873da1d9e98d4639b
YongJu Kim <kyjdummy@gmail.com>
2024-07-28 15:19:05 +0900

    [Gold V] Title: 흙길 보수하기, Time: 244 ms, Memory: 20484 KB -BaekjoonHub

---
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -0,0 +1,39 @@
+//https://github.com/KimYongJ/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+class Node{
+	int s,e;
+	Node(int s, int e){this.s=s; this.e=e;}
+}
+class Main{
+	public static void main(String[] args)throws Exception{
+		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.s-b.s);
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		double L = Integer.parseInt(st.nextToken());
+		int idx = 0;
+		int cnt = 0;
+		for(int i=0; i<N; i++) 
+		{
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			pq.add(new Node(s,e));
+		}
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			int s = Math.max(now.s, idx);
+			int e = now.e;
+			int l = e-s;
+			if(l > 0) {
+				int c = (int)Math.ceil(l/L);
+				cnt += c;
+				idx = s + c*(int)L;
+			}
+		}
+		System.out.print(cnt);
+	}
+}
