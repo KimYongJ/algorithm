72471ad836425e1942ac832cde08d05754287cf7
YongJu Kim <kyjdummy@gmail.com>
2024-07-28 15:29:19 +0900

    [Gold V] Title: 흙길 보수하기, Time: 164 ms, Memory: 14832 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -1,33 +1,31 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int s,e;
 	Node(int s, int e){this.s=s; this.e=e;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.s-b.s);
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
+		int N = read();
+		int L = read();
 		int idx = 0;
 		int cnt = 0;
 		for(int i=0; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
+			int s = read();
+			int e = read();
 			pq.add(new Node(s,e));
 		}
 		while(!pq.isEmpty()) {
 			Node now = pq.poll();
 			int s = Math.max(now.s, idx);
-			int e = now.e;
-			int l = e-s;
+			int l = now.e-s;
 			if(l > 0) {
 				int c = l/L;
 				if(l%L > 0) c++;
