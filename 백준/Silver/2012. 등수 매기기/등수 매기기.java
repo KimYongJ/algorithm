0543786a560ddd3572448ed56ade7fe9735a20fd
YongJu Kim <kyjdummy@gmail.com>
2024-07-28 14:21:33 +0900

    [Silver III] Title: 등수 매기기, Time: 192 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -1,25 +1,23 @@
 //https://github.com/KimYongJ/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine())+1;
-		int arr[] = new int[500001];
+		long res	= 0;
+		int rnk		= 1;
+		int N		= read()+1;
+		int arr[]	= new int[500001];
 		
 		for(int i=1; i<N; i++)
-			arr[Integer.parseInt(br.readLine())]++;
-		
-		long res = 0;
-		int rnk = 1;
-		for(int i=1; i<500001; i++) 
-		{
-			while(arr[i]-- > 0) {
+			arr[read()]++;
+
+		for(int i=1; i<500001 && rnk != N; i++) 
+			while(arr[i]-- > 0)
 				res += Math.abs(i - rnk++);
-			}
-			if(rnk == N)
-				break;
-		}
+		
 		System.out.print(res);
 	}
 }
