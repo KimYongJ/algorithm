4da661d4d60a6d647359247ddfac801b815baa37
YongJu Kim <kyjdummy@gmail.com>
2024-07-29 10:31:42 +0900

    [Gold V] Title: 강의실 배정, Time: 708 ms, Memory: 68836 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,8 +1,7 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 class Node{
@@ -14,29 +13,27 @@ class Main{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		ArrayList<Node> list = new ArrayList<>();
+		Node[] list = new Node[N];
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		for(int i=0; i<N; i++) 
 		{
 			st		= new StringTokenizer(br.readLine());
 			int s	= Integer.parseInt(st.nextToken());
 			int e	= Integer.parseInt(st.nextToken());
-			list.add(new Node(s,e));
+			list[i] = new Node(s,e);
 		}
-		Collections.sort(list, (a,b)->{
-			if(a.s==b.s)return a.e-b.e;
-			return a.s-b.s;
-		});
-		int cnt = 0;
-		for(int i=0; i<N; i++) 
+		
+		Arrays.sort(list,(a,b)->a.s-b.s);
+
+		pq.add(list[0].e);
+		
+		int cnt = 1;
+		
+		for(int i=1; i<N; i++) 
 		{
-			Node now = list.get(i);
-			if(!pq.isEmpty() && pq.peek() <= now.s) {
-				pq.poll();
-			}else {
-				cnt++;
-			}
-			pq.add(now.e);
+			if(pq.peek() <= list[i].s) pq.poll();
+			else cnt++;
+			pq.add(list[i].e);
 		}
 		System.out.print(cnt);
 	}
