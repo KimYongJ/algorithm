95887f119d2224b35cc3436c318405e2ee829ec3
YongJu Kim <kyjdummy@gmail.com>
2024-07-29 10:33:16 +0900

    [Gold V] Title: 강의실 배정, Time: 460 ms, Memory: 26272 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,38 +1,35 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int s,e;
 	Node(int s,int e){this.s=s;this.e=e;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		Node[] list = new Node[N];
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int cnt		= 1;
+		int N		= read();
+		Node[] list = new Node[N];
+
 		for(int i=0; i<N; i++) 
-		{
-			st		= new StringTokenizer(br.readLine());
-			int s	= Integer.parseInt(st.nextToken());
-			int e	= Integer.parseInt(st.nextToken());
-			list[i] = new Node(s,e);
-		}
+			list[i] = new Node(read(),read());
 		
 		Arrays.sort(list,(a,b)->a.s-b.s);
 
 		pq.add(list[0].e);
 		
-		int cnt = 1;
-		
 		for(int i=1; i<N; i++) 
 		{
-			if(pq.peek() <= list[i].s) pq.poll();
-			else cnt++;
+			if(pq.peek() <= list[i].s) 
+				pq.poll();
+			else 
+				cnt++;
 			pq.add(list[i].e);
 		}
 		System.out.print(cnt);
