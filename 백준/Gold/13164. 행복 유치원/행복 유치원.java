f13868ce14936b7b860208bb2152738e10d73a22
YongJu Kim <kyjdummy@gmail.com>
2024-07-30 16:41:02 +0900

    [Gold V] Title: 행복 유치원, Time: 456 ms, Memory: 23544 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,21 +1,20 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());
-		int K		= Integer.parseInt(st.nextToken());
+		int N		= read();
+		int K		= read();
 		int arr[]	= new int[N];
 		int diff[]	= new int[N-1];
 		int sum	= 0;
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		for(int i=0; i<N-1; i++)
 			diff[i] = arr[i+1] - arr[i];
