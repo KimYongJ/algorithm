a8989d6e7ca2fa7cc706cac322fc88e23da2553a
YongJu Kim <kyjdummy@gmail.com>
2024-07-30 16:42:55 +0900

    [Gold V] Title: 행복 유치원, Time: 368 ms, Memory: 24320 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,5 +1,5 @@
 // https://github.com/kimyongj/algorithm
-import java.util.Arrays;
+import java.util.PriorityQueue;
 class Main{
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
@@ -7,22 +7,22 @@ class Main{
 		return n;
 	}
 	public static void main(String[] args)throws Exception{
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		int N		= read();
 		int K		= read();
 		int arr[]	= new int[N];
-		int diff[]	= new int[N-1];
 		int sum	= 0;
 		
 		for(int i=0; i<N; i++)
 			arr[i] = read();
 		
 		for(int i=0; i<N-1; i++)
-			diff[i] = arr[i+1] - arr[i];
-
-		Arrays.sort(diff);
-
-		for(int i=0; i<N - K; i++)
-			sum += diff[i];
+			pq.add(arr[i+1] - arr[i]);
+		
+		N -= K;
+		
+		for(int i=0; i<N; i++)
+			sum += pq.poll();
 		
 		System.out.print(sum);
 	}
