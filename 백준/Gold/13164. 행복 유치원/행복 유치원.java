8e01f940bc96f9f6b368571f551f6dcb86a373ae
YongJu Kim <kyjdummy@gmail.com>
2024-07-30 16:36:39 +0900

    [Gold V] Title: 행복 유치원, Time: 628 ms, Memory: 52672 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -2,39 +2,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int arr[] = new int[N];
-		int index[] = new int[K-1];
+		int N		= Integer.parseInt(st.nextToken());
+		int K		= Integer.parseInt(st.nextToken());
+		int arr[]	= new int[N];
+		int diff[]	= new int[N-1];
+		long sum	= 0;
+		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
 		
-		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)->b[0]-a[0]);
-		for(int i=1; i<N; i++)
-			pq.add(new int[] {arr[i]-arr[i-1],i-1});
+		for(int i=0; i<N-1; i++)
+			sum += diff[i] = arr[i+1] - arr[i];
+
+		Arrays.sort(diff);
+
+		for(int i=N-2; K>1; i--,K--)
+			sum -= diff[i];
 		
-		for(int i=0; i<K-1; i++)
-			index[i] = pq.poll()[1];
-		
-		Arrays.sort(index);
-		
-		long sum = 0;
-		int start = 0;
-		for(int len : index) {
-			sum += arr[len] - arr[start];
-			start = len+1;
-		}
-		if(start < N) {
-			sum += arr[N-1] - arr[start];
-		}
 		System.out.print(sum);
 	}
 }
