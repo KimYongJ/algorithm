f16b19ad944b2ce87ac20ec68ee57e9887e99f4b
YongJu Kim <kyjdummy@gmail.com>
2024-07-30 17:49:34 +0900

    [Gold V] Title: 강의실, Time: 376 ms, Memory: 25284 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.java
+++ 백준/Gold/1374. 강의실/강의실.java
@@ -1,37 +1,33 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int s,e; Node(int s,int e){this.s=s;this.e=e;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<Integer> rooms = new PriorityQueue<>();
-		PriorityQueue<Node> list = new PriorityQueue<Node>((a,b)->a.s-b.s);
-		int T = Integer.parseInt(br.readLine());
-		while(T-->0) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			st.nextToken();
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			list.add(new Node(s,e));
-		}
+		PriorityQueue<Integer> rooms	= new PriorityQueue<>();
+		PriorityQueue<Node> list		= new PriorityQueue<Node>((a,b)->a.s-b.s);
 		int res = 0;
-		while(!list.isEmpty()) {
+		int T = read();
+		while(T-->0)
+		{
+			read();
+			list.add(new Node(read(),read()));
+		}
+		
+		while(!list.isEmpty()) 
+		{
 			Node now = list.poll();
 			if(!rooms.isEmpty() && rooms.peek() <= now.s) 
-			{
 				rooms.poll();
-			}
-
-			rooms.add(now.e);
+			else res++;
 			
-			if(res < rooms.size()) {
-				res = rooms.size();
-			}
+			rooms.add(now.e);
 		}
 		System.out.print(res);
 	}
