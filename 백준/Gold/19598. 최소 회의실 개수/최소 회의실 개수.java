047329329014f7c257920d358e2269fc30ac4bd4
YongJu Kim <kyjdummy@gmail.com>
2024-07-31 13:41:02 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 380 ms, Memory: 26760 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -1,18 +1,18 @@
 // https://github.com/kimyongj/algorithm
 import java.util.PriorityQueue;
 class Node{
-	long s,e; Node(long s,long e){this.s=s; this.e=e;}
+	int s,e; Node(int s,int e){this.s=s; this.e=e;}
 }
 class Main{
-	static long read() throws Exception {// 빠른 입력을 위한 함수
-		long c, n = System.in.read() & 15;
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
 	public static void main(String[] args)throws Exception{
-		PriorityQueue<Node> list = new PriorityQueue<Node>((a,b)->Long.compare(a.s,b.s));
-		PriorityQueue<Long> rooms= new PriorityQueue<>();
-		int N	= (int)read();
+		PriorityQueue<Node> list	= new PriorityQueue<Node>((a,b)->a.s-b.s);
+		PriorityQueue<Integer> rooms= new PriorityQueue<>();
+		int N	= read();
 		int cnt = 0;
 		
 		for(int i=0; i<N; i++) 
@@ -28,7 +28,6 @@ class Main{
 				cnt++;
 			
 			rooms.add(now.e);
-			
 		}
 		System.out.print(cnt);
 	}
