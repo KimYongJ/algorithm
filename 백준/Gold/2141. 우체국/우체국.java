a461ab3feb8e6b07d0fc52f4665fb4e01eedd40f
YongJu Kim <kyjdummy@gmail.com>
2024-08-01 10:27:03 +0900

    [Gold IV] Title: 우체국, Time: 272 ms, Memory: 19196 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -1,29 +1,40 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int idx,cnt;
 	Node(int idx,int cnt){this.idx=idx; this.cnt=cnt;}
 }
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.idx-b.idx);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		long sum = 1, tmpSum = 0;
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int idx = Integer.parseInt(st.nextToken());
-			int cnt = Integer.parseInt(st.nextToken());
+		int N		= read();
+		long sum	= 1;
+		long tmpSum = 0;
+		int idx, cnt;
+		for(int i=0; i<N; i++) 
+		{
+			idx = read();
+			cnt = read();
 			sum += cnt;
 			pq.add(new Node(idx, cnt));
 		}
-		for(int i=0; i<N; i++) {
+		
+		sum /= 2;
+		
+		for(int i=0; i<N; i++) 
+		{
 			Node now = pq.poll();
 			tmpSum += now.cnt;
-			if(sum/2 <= tmpSum) {
+			if(sum <= tmpSum) 
+			{
 				System.out.print(now.idx);
 				return;
 			}
