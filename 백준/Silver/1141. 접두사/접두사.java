2b0da56d96c58de0a739bbe95458e0bb562b8207
YongJu Kim <kyjdummy@gmail.com>
2024-08-02 23:36:40 +0900

    [Silver I] Title: 접두사, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1141. 접두사/접두사.java
+++ 백준/Silver/1141. 접두사/접두사.java
@@ -7,33 +7,24 @@ import java.util.HashSet;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br			= new BufferedReader(new InputStreamReader(System.in));
-		int N						= Integer.parseInt(br.readLine());
-		HashSet<String> set			= new HashSet<>();
-		ArrayList<String> result	= new ArrayList<>();
+		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
+		int N				= Integer.parseInt(br.readLine());
+		HashSet<String> set	= new HashSet<>();
 		
 		for(int i=0; i<N; i++)
 			set.add(br.readLine());
 		
-		ArrayList<String> list	= new ArrayList<>(set);
+		ArrayList<String> list = new ArrayList<>(set);
 		
-		Collections.sort(list,(a,b)->b.length() - a.length());
+		Collections.sort(list);
 		
 		N = set.size();
 		
-		for(int i=0; i<N; i++)
-		{
-			boolean flag = true;
-			for(String s : result) 
-				if(s.startsWith(list.get(i))) 
-				{
-					flag = false;
-					break;
-				}
-			
-			if(flag) 
-				result.add(list.get(i));
-		}
-		System.out.print(result.size());
+		int cnt = 1;
+		for(int i=1; i<N; i++)
+			if(!list.get(i).startsWith(list.get(i-1)))
+				cnt++;
+		
+		System.out.print(cnt);
 	}
 }
