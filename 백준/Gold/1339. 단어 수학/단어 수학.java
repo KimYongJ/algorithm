5cfdda70825f99ffc634bd63880d11a41d611fff
YongJu Kim <kyjdummy@gmail.com>
2024-08-02 11:50:21 +0900

    [Gold IV] Title: 단어 수학, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 class Node{
 	char c;
 	long score;
@@ -12,39 +11,43 @@ class Node{
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		String arr[] = new String[N];
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb[] = new StringBuilder[N];
 		ArrayList<Node> list = new ArrayList<>();
 		
 		for(int i=0; i<26; i++)
 			list.add(new Node((char)(i+'A'),0));
 		
-		for(int i=0; i<N; i++) {
-			arr[i] = br.readLine();
-			for(int j=arr[i].length()-1,z=9; j>=0; j--,z--) {
-				char c = arr[i].charAt(j);
-				Node now = list.get(c-'A');
+		for(int i=0; i<N; i++) 
+		{
+			sb[i] = new StringBuilder(br.readLine());
+			for(int j=sb[i].length()-1,z=9; j>=0; j--,z--) 
+			{
+				Node now = list.get(sb[i].charAt(j) - 'A');
 				now.score += Math.pow(10, 9-z);
 			}
 		}
 		// 점수기준 내림차순
 		Collections.sort(list,(a,b)->Long.compare(b.score,a.score));
-		int flag = 9;
+		char flag = '9';
 		for(int i=0; i<26; i++) {
 			Node now = list.get(i);
 			if(now.score == 0)
 				break;
 			for(int n=0; n<N; n++) {
-				arr[n] = arr[n].replaceAll(now.c+"",flag+"");
+				for(int l=0; l<sb[n].length(); l++) {
+					if(sb[n].charAt(l) == now.c) {
+						sb[n].setCharAt(l, flag);
+					}
+				}
 			}
 			--flag;
 		}
 		
 		long sum = 0;
-		for(int i=0; i<N; i++) {
-			sum += Integer.parseInt(arr[i]);
-		}
+		for(int i=0; i<N; i++)
+			sum += Integer.parseInt(sb[i].toString());
+		
 		System.out.print(sum);
 	}
 }
