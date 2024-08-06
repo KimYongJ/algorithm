9075958116f140c143cfbb3a210d0baac0feeafc
YongJu Kim <kyjdummy@gmail.com>
2024-08-06 15:57:29 +0900

    [Gold IV] Title: 수열의 점수, Time: 360 ms, Memory: 24880 KB -BaekjoonHub

--- 백준/Gold/2036. 수열의 점수/수열의 점수.java
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.java
@@ -5,20 +5,20 @@ import java.util.PriorityQueue;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<Long> pos = new PriorityQueue<Long>((a,b)->Long.compare(b, a));
-		PriorityQueue<Long> neg = new PriorityQueue<>();
+		PriorityQueue<Integer> pos = new PriorityQueue<Integer>((a,b)->b-a);
+		PriorityQueue<Integer> neg = new PriorityQueue<>();
 		int N = Integer.parseInt(br.readLine());
 		boolean zero = false;
 		long sum = 0;
 		while(N-->0) {
-			long n = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
 			if(n == 0) zero = true;
 			else if(n == 1) sum++;
 			else if(n > 0) pos.add(n);
 			else neg.add(n);
 		}
-		while(pos.size() > 1) sum += pos.poll() * pos.poll();
-		while(neg.size() > 1) sum += neg.poll() * neg.poll();
+		while(pos.size() > 1) sum += (long)pos.poll() * pos.poll();
+		while(neg.size() > 1) sum += (long)neg.poll() * neg.poll();
 		
 		if(pos.size() == 1) sum += pos.poll();
 		if(!zero && neg.size() == 1) sum += neg.poll();
