bf90650a1db60a985c87a6ad0b6e76dcdf96bc4f
YongJu Kim <kyjdummy@gmail.com>
2024-08-07 15:07:28 +0900

    [Gold IV] Title: 댄스 파티, Time: 620 ms, Memory: 45852 KB -BaekjoonHub

---
+++ 백준/Gold/2831. 댄스 파티/댄스 파티.java
@@ -0,0 +1,48 @@
+// https://github.com/kimyongj/algorithm
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		PriorityQueue<Integer> m_bg = new PriorityQueue<>();
+		PriorityQueue<Integer> m_sm = new PriorityQueue<>();
+		PriorityQueue<Integer> f_bg = new PriorityQueue<>();
+		PriorityQueue<Integer> f_sm = new PriorityQueue<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) // 남자 넣기 
+		{
+			int num = Integer.parseInt(st.nextToken());
+			if(num < 0)	m_sm.add(-num);
+			else m_bg.add(num);
+		}
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) // 여자 넣기 
+		{
+			int num = Integer.parseInt(st.nextToken());
+			if(num < 0)	f_sm.add(-num);
+			else f_bg.add(num);
+		}
+		int cnt = 0;
+		while(!(m_bg.isEmpty() || f_sm.isEmpty())) 
+		{
+			if(m_bg.peek() < f_sm.peek()) {
+				cnt++;
+				m_bg.poll();
+				f_sm.poll();
+			}else f_sm.poll();
+		}
+		while(!(m_sm.isEmpty() || f_bg.isEmpty())) 
+		{
+			if(f_bg.peek() < m_sm.peek()) {
+				cnt++;
+				f_bg.poll();
+				m_sm.poll();
+			}else m_sm.poll();
+		}
+		
+		System.out.print(cnt);
+	}
+}
