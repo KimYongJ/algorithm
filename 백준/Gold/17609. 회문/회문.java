f6688924fdcf41916aed9f3167f7f5d7bb665c5e
YongJu Kim <kyjdummy@gmail.com>
2024-08-08 13:42:37 +0900

    [Gold V] Title: 회문, Time: 292 ms, Memory: 28820 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -2,6 +2,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 class Main{
+	public static int getCnt(String str, int left, int right) {
+		int cnt = 0;
+		while(left < right) {
+			char cl = str.charAt(left);
+			char cr = str.charAt(right);
+			if(cl != cr) {
+				cnt++;
+				if(right-1 != left) {
+					if(cl == str.charAt(right-1)) right--;
+					else if(str.charAt(left+1) == cr) left++;
+					else cnt++;
+				}
+			}
+			left++;
+			right--;
+		}
+		return cnt;
+	}
 	public static void main(String args[])throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -9,25 +27,10 @@ class Main{
 		while(T-->0) {
 			String str = br.readLine();
 			int len = str.length();
-			int cnt2 = 0, cnt1 = 0;
+			int cnt1 = getCnt(str, 0, len-1);
+			int cnt2 = 0;
 			int left = 0;
 			int right = len-1;
-			while(left < right) {
-				char cl = str.charAt(left);
-				char cr = str.charAt(right);
-				if(cl != cr) {
-					cnt1++;
-					if(right-1 != left) {
-						if(cl == str.charAt(right-1)) right--;
-						else if(str.charAt(left+1) == cr) left++;
-						else cnt1++;
-					}
-				}
-				left++;
-				right--;
-			}
-			left = 0;
-			right = len-1;
 			while(left < right) {
 				char cl = str.charAt(left);
 				char cr = str.charAt(right);
