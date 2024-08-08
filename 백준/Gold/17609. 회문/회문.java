9824448cc57c00e62df3391f5ca5fbe16e86c6a8
YongJu Kim <kyjdummy@gmail.com>
2024-08-08 13:56:20 +0900

    [Gold V] Title: 회문, Time: 284 ms, Memory: 29392 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -2,50 +2,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 class Main{
-	public static int getCnt(String str, int left, int right) {
-		int cnt = 0;
+	public static boolean findpal(String str, int left, int right) {
+		while(left < right)
+			if(str.charAt(left++) != str.charAt(right--))
+				return false;			
+		return true;
+	}
+	public static int find(String str, int left, int right) {
 		while(left < right) {
-			char cl = str.charAt(left);
-			char cr = str.charAt(right);
-			if(cl != cr) {
-				cnt++;
-				if(right-1 != left) {
-					if(cl == str.charAt(right-1)) right--;
-					else if(str.charAt(left+1) == cr) left++;
-					else cnt++;
-				}
+			if(str.charAt(left) != str.charAt(right)) {
+				if(findpal(str,left, right -1) || findpal(str,left+1, right)) {
+					return 1;
+				}else
+					return 2;				
 			}
 			left++;
 			right--;
 		}
-		return cnt;
+		return 0;
 	}
 	public static void main(String args[])throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder 	sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		while(T-->0) {
+		while(T-->0) 
+		{
 			String str = br.readLine();
-			int len = str.length();
-			int cnt1 = getCnt(str, 0, len-1);
-			int cnt2 = 0;
-			int left = 0;
-			int right = len-1;
-			while(left < right) {
-				char cl = str.charAt(left);
-				char cr = str.charAt(right);
-				if(cl != cr) {
-					cnt2++;
-					if(right-1 != left) {
-						if(str.charAt(left+1) == cr) left++;
-						else if(cl == str.charAt(right-1)) right--;
-						else cnt2++;
-					}
-				}
-				left++;
-				right--;
-			}
-			sb.append(Math.min(Math.min(cnt1, cnt2),2)).append('\n');
+			sb.append(find(str, 0, str.length()-1)).append('\n');
 		}
 		System.out.print(sb.toString());
 	}
