a7131f7eee0033265be266cd6c31794a7f9f76a3
YongJu Kim <kyjdummy@gmail.com>
2024-08-09 09:42:43 +0900

    [Gold III] Title: 과제, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -1,26 +1,25 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int day, score;
 	Node(int day, int score){this.day=day; this.score=score;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Integer> scorePq	= new PriorityQueue<Integer>((a,b)->b-a);
 		PriorityQueue<Node> list		= new PriorityQueue<Node>((a,b)->b.day - a.day);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int maxDay	= 0;
 		int res		= 0;
 		for(int i=0; i<N; i++) 
 		{
-			st = new StringTokenizer(br.readLine());
-			int d = Integer.parseInt(st.nextToken()); // 마감일
-			int s = Integer.parseInt(st.nextToken()); // 점수
+			int d = read(); // 마감일
+			int s = read(); // 점수
 			list.add(new Node(d,s));
 			if(maxDay < d) {
 				maxDay = d;
