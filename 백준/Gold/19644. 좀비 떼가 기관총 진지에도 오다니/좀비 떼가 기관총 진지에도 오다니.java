361baccc29ac8481370fa4f235243f6c75ef47f0
YongJu Kim <kyjdummy@gmail.com>
2024-08-11 16:33:33 +0900

    [Gold III] Title: 좀비 떼가 기관총 진지에도 오다니, Time: 524 ms, Memory: 181732 KB -BaekjoonHub

--- 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.java
+++ 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.java
@@ -5,47 +5,36 @@ import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int L = Integer.parseInt(st.nextToken()); // 기관총 앞쪽 길이 1<=3백만
 		st = new StringTokenizer(br.readLine());
-		int effectiv_range = Integer.parseInt(st.nextToken()); 		// 유효사거리
-		int kill_power = Integer.parseInt(st.nextToken()); 			// 살상력
-		int grenade = Integer.parseInt(br.readLine());				// 수류탄개수
-		int useGrend[] = new int[L+2];								// 수류탄 사용시 유효범위 안에 몇개를 사용했는지 확인
-		String result = "YES";
-		int len1 = Math.min(effectiv_range,L);
-		for(int i=1; i<=len1; i++) {
+		int effectiv_range	= Integer.parseInt(st.nextToken());	// 유효사거리
+		int kill_power		= Integer.parseInt(st.nextToken());	// 살상력
+		int grenade			= Integer.parseInt(br.readLine());	// 수류탄개수
+		int useGrend[]		= new int[L+2];						// 수류탄 사용시 유효범위 안에 몇개를 사용했는지 확인
+		
+		for(int i=1; i<=L; i++) 
+		{
 			useGrend[i] += useGrend[i-1]; // 누적합
 			int energy = Integer.parseInt(br.readLine());
-			if(energy > kill_power * (i-useGrend[i])) {
-				if(grenade > 0) {
+			int idx = Math.min(i, effectiv_range);
+			
+			if(energy > kill_power * (idx-useGrend[i])) 
+			{
+				if(grenade > 0) 
+				{
 					grenade--;
 					int rangeEnd = Math.min(i+effectiv_range, L);
 					useGrend[i]++;
 					useGrend[rangeEnd+1]--;
 				}else {
-					result = "NO";
+					System.out.print("NO");
+					return;
 				}
 			}
 		}
-		if("YES".equals(result)) {
-			for(int i=len1+1; i<=L; i++) {
-				useGrend[i] += useGrend[i-1]; // 누적합
-				int energy = Integer.parseInt(br.readLine());
-				if(energy > kill_power * (effectiv_range-useGrend[i])) {
-					if(grenade > 0) {
-						grenade--;
-						int rangeEnd = Math.min(i+effectiv_range, L);
-						useGrend[i]++;
-						useGrend[rangeEnd+1]--;
-					}else {
-						result = "NO";
-					}
-				}
-			}
-		}
-		
-		System.out.print(result);
+
+		System.out.print("YES");
 	}
 }
