25b80f58f0496ef15bd9412b581d065855710925
YongJu Kim <kyjdummy@gmail.com>
2024-08-12 17:28:16 +0900

    [Silver II] Title: 한 줄로 서기, Time: 96 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,4 +1,5 @@
 // https://github.com/kimyongj/algorithm
+import java.util.ArrayList;
 class Main{
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
@@ -6,25 +7,20 @@ class Main{
 		return n;
 	}
 	public static void main(String[] args)throws Exception{
+		ArrayList<Integer> list = new ArrayList<>();
 		StringBuilder sb = new StringBuilder();
-		int N			= read();
-		int result[]	= new int[N];
-		for(int i=1; i<=N; i++) 
-		{
-			int num = read()+1;
-			int idx = 0;
-			while(true) 
-			{
-				if(result[idx] == 0 && --num == 0) {
-					result[idx] = i;
-					break;
-				}
-				idx++;
-			}
-		}
+		int N		= read();
+		int arr[]	= new int[N];
 		
-		for(int r : result)
+		for(int i=0; i<N; i++) 
+			arr[i] = read();
+
+		for(int i=N; i>0; i--)
+			list.add(arr[i-1], i);
+		
+		for(int r : list)
 			sb.append(r).append(' ');
+		
 		System.out.print(sb.toString());
 	}
 }
