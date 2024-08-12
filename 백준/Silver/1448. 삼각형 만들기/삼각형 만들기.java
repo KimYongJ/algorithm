890080c0a6f7826d9ad0f73e75ab221774341552
YongJu Kim <kyjdummy@gmail.com>
2024-08-12 16:50:56 +0900

    [Silver III] Title: 삼각형 만들기, Time: 440 ms, Memory: 93660 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
@@ -1,23 +1,31 @@
 // https://github.com/kimyongj/algorithm
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 class Main{
 	public static void main(String[] args)throws Exception{
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b)->b-a);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N];
+		int N	= Integer.parseInt(br.readLine());
 		int res = -1;
-		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
-
-		Arrays.sort(arr);
 		
-		for(int i=N-1; i>=2; i--) {
-			int sum = arr[i-1] + arr[i-2];
-			if(arr[i] < sum) {
-				res = sum + arr[i];
-				break;
+		for(int i=0; i<N; i++)
+			pq.add(Integer.parseInt(br.readLine()));
+		
+		int a = pq.poll();
+		int b = pq.poll();
+		int c = pq.poll();
+		if(a < b+c) {
+			res = a+b+c;
+		}else {
+			while(!pq.isEmpty()) {
+				a = b;
+				b = c;
+				c = pq.poll();
+				if(a < b+c) {
+					res = a+b+c;
+					break;
+				}
 			}
 		}
 		
