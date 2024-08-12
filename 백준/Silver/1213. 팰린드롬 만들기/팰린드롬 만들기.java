df15c6bbd55a9faa31af304b3c5432aac6c47bf3
YongJu Kim <kyjdummy@gmail.com>
2024-08-12 16:20:21 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 class Main{
 	public static void main(String[] args)throws Exception{
+		final char[] text = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder	sb1 = new StringBuilder();
 		StringBuilder	sb2 = new StringBuilder();
@@ -13,7 +14,7 @@ class Main{
 			cnt[str.charAt(i)-'A']++;
 		
 		int oddCnt = 0;
-		int oddIndex = -1;
+		int oddIndex = 0;
 		for(int i=0; i<26; i++) 
 		{
 			if(cnt[i] % 2 == 1) 
@@ -23,6 +24,8 @@ class Main{
 				cnt[i]--;
 			}
 			cnt[i]/=2;
+			while(cnt[i]-->0)
+				sb1.append(text[i]);
 		}
 
 		if(oddCnt > 1) 
@@ -31,16 +34,10 @@ class Main{
 		}
 		else 
 		{
-			for(int i=0; i<26; i++)
-				while(cnt[i]-->0)
-					sb1.append((char)(i+'A'));
-
 			sb2.append(sb1.toString()); // 백업
 			
 			if(oddCnt == 1)
-			{
-				sb1.append((char)(oddIndex+'A'));
-			}
+				sb1.append(text[oddIndex]);
 			
 			sb1.append(sb2.reverse().toString());
 			
