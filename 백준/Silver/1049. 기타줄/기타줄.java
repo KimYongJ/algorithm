ac87209e6d435ffeba944e1d2d36cd261d48c7bc
YongJu Kim <kyjdummy@gmail.com>
2024-08-12 14:28:13 +0900

    [Silver IV] Title: 기타줄, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -1,25 +1,29 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); // 끊어진 기타 개수
-		int M = Integer.parseInt(st.nextToken()); // 기타 브랜드 수
+		int N = read(); // 끊어진 기타 개수
+		int M = read(); // 기타 브랜드 수
 		int one_min = Integer.MAX_VALUE;
 		int six_min = Integer.MAX_VALUE;
 		
 		while(M-->0) 
 		{
-			st = new StringTokenizer(br.readLine());
-			int six = Integer.parseInt(st.nextToken());
-			int one = Integer.parseInt(st.nextToken());
+			int six = read();
+			int one = read();
+			
 			six = Math.min(six, one * 6);
-			one_min = Math.min(one_min, one);
-			six_min = Math.min(six_min, six);
+			
+			if(one_min > one) {
+				one_min = one;
+			}
+			if(six_min > six) {
+				six_min = six;
+			}
 		}
 		
 		int res = (N/6 * six_min);
