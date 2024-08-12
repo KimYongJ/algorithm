001d5287bd70ee088bc73084f48b213633e37c39
YongJu Kim <kyjdummy@gmail.com>
2024-08-12 17:13:40 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,26 +1,23 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		int result[] = new int[N];
-		st = new StringTokenizer(br.readLine());
+		int N			= read();
+		int result[]	= new int[N];
 		for(int i=1; i<=N; i++) 
 		{
-			int num = Integer.parseInt(st.nextToken())+1;
+			int num = read()+1;
 			int idx = 0;
-			while(idx < N) {
-				if(result[idx] == 0) {
-					if(--num == 0) {
-						result[idx] = i;
-						break;
-					}
+			while(true) 
+			{
+				if(result[idx] == 0 && --num == 0) {
+					result[idx] = i;
+					break;
 				}
 				idx++;
 			}
