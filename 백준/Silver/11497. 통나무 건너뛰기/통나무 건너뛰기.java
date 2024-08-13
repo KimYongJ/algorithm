f041ac95b8ef284f2492822e7bfbc869ad350a89
YongJu Kim <kyjdummy@gmail.com>
2024-08-13 16:08:21 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 312 ms, Memory: 25920 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -1,21 +1,22 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0) 
 		{
-			int N = Integer.parseInt(br.readLine());
-			int arr[] = new int[N];
-			int base[] = new int[N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N		= read();
+			int arr[]	= new int[N];
+			int base[]	= new int[N];
+			
 			for(int i=0; i<N; i++)
-				base[i] = Integer.parseInt(st.nextToken());
+				base[i] = read();
 			
 			Arrays.sort(base);
 			
@@ -32,9 +33,7 @@ class Main{
 			{
 				int diff = Math.abs(arr[i] - arr[i-1]);
 				if(MAX < diff)
-				{
 					MAX = diff;
-				}
 			}
 			sb.append(MAX).append('\n');
 		}
