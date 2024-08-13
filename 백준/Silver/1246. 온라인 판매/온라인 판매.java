a012b5392bdb65cdb3e24828d5d9619285ef74cf
YongJu Kim <kyjdummy@gmail.com>
2024-08-13 15:36:42 +0900

    [Silver IV] Title: 온라인 판매, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/1246. 온라인 판매/온라인 판매.java
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -1,30 +1,35 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b)->b-a);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 달걀 개수
-		int M = Integer.parseInt(st.nextToken()); // 고객수
+		int N = read(); // 달걀 개수
+		int M = read(); // 고객수
 		
 		for(int i=0; i<M; i++)
-			pq.add(Integer.parseInt(br.readLine()));
+			pq.add(read());
 		
-		int price = 0;
-		int cnt = 0;
-		for(int i=1; i<=M && i<= N; i++) {
+		int price	= 0;
+		int cnt		= 0;
+		int total	= 0;
+		for(int i=1; i<=M && i<= N; i++)
+		{
 			int nowPrice = pq.poll();
-			if(price*cnt < nowPrice*i) {
+			if(total < nowPrice*i) 
+			{
 				price = nowPrice;
 				cnt = i;
+				total = price * cnt;
 			}
 		}
+		
 		StringBuilder sb = new StringBuilder();
-		sb.append(price).append(' ').append(price * cnt);
+		sb.append(price).append(' ').append(total);
 		System.out.print(sb.toString());
 	}
 }
