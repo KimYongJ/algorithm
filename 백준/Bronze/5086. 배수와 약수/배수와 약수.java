5fe72803f1d117ebd428e722e68118dca1fb48a2
YongJu Kim <kyjdummy@gmail.com>
2024-08-17 19:39:38 +0900

    [Bronze III] Title: 배수와 약수, Time: 96 ms, Memory: 13892 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,14 +1,14 @@
 //https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String args[])throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		int a = read();
+		int b = read();
 		while(a!=0 && b!=0) {
 			if(b%a == 0) {
 				sb.append("factor");
@@ -18,9 +18,8 @@ class Main{
 				sb.append("neither");
 			}
 			sb.append('\n');
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			a = read();
+			b = read();
 		}
 		System.out.print(sb.toString());
 	}
