e79d3c2984b70db48ea569d8f6d0e847c3c5ad6b
YongJu Kim <kyjdummy@gmail.com>
2024-08-18 11:36:21 +0900

    [Gold III] Title: 크게 만들기, Time: 304 ms, Memory: 19492 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,10 +1,7 @@
 // https://github.com/kimyongj/algorithm
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String args[])throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,22 +11,19 @@ class Main{
 		int K = Integer.parseInt(st.nextToken()); // 지울 수
 		int limit = N-K; // 최종 만들어야하는 길이
 		String str = br.readLine();
-		ArrayDeque<Character> dq = new ArrayDeque<>();
 
 		
 		for(int i=0; i<N; i++) {
-			while(K != 0 && !dq.isEmpty() && dq.peekLast() < str.charAt(i)) 
+			char c = str.charAt(i);
+			while(K != 0 && sb.length()>0 && sb.charAt(sb.length()-1) < c) 
 			{
-				dq.pollLast();
+				sb.deleteCharAt(sb.length()-1);
 				if(--K==0)break;
 			}
-			dq.addLast(str.charAt(i));
+			sb.append(c);
 		}
 		
-		while(limit-->0)
-			sb.append(dq.poll());
 		
-		
-		System.out.print(sb.toString());
+		System.out.print(sb.toString().substring(0,limit));
 	}
 } 
