5e6e85abb264da0896625533f053efaa6101db53
YongJu Kim <kyjdummy@gmail.com>
2024-08-18 11:31:40 +0900

    [Gold III] Title: 크게 만들기, Time: 332 ms, Memory: 33688 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -2,7 +2,7 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 class Main{
@@ -13,28 +13,23 @@ class Main{
 		int N = Integer.parseInt(st.nextToken()); // 자리수
 		int K = Integer.parseInt(st.nextToken()); // 지울 수
 		int limit = N-K; // 최종 만들어야하는 길이
-		int idx = 0;
-		int origin[] = new int[N]; // 입력된 숫자를 배열로 변경
-		
-		for(char c : br.readLine().toCharArray())
-			origin[idx++] = c-'0';
+		String str = br.readLine();
+		ArrayDeque<Character> dq = new ArrayDeque<>();
 
-		Stack<Integer> stack = new Stack<>();
 		
 		for(int i=0; i<N; i++) {
-			while(K != 0 && !stack.isEmpty() && stack.peek() < origin[i]) {
-				stack.pop();
-				K--;
+			while(K != 0 && !dq.isEmpty() && dq.peekLast() < str.charAt(i)) 
+			{
+				dq.pollLast();
+				if(--K==0)break;
 			}
-			stack.push(origin[i]);
-		}
-		while(stack.size() != limit) {
-			stack.pop();
-		}
-		while(!stack.isEmpty()) {
-			sb.append(stack.pop());
+			dq.addLast(str.charAt(i));
 		}
 		
-		System.out.print(sb.reverse().toString());
+		while(limit-->0)
+			sb.append(dq.poll());
+		
+		
+		System.out.print(sb.toString());
 	}
-}
+} 
