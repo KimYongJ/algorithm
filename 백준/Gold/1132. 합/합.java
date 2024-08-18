5cf20a57684fd05da13540a3b5d4c8d6415b459b
YongJu Kim <kyjdummy@gmail.com>
2024-08-18 18:48:16 +0900

    [Gold III] Title: 합, Time: 100 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/1132. 합/합.java
+++ 백준/Gold/1132. 합/합.java
@@ -15,9 +15,8 @@ class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N					= Integer.parseInt(br.readLine());		
 		String str[]			= new String[N];
-		long valueOfChar[]		= new long[10];
-		boolean nonZeroList[]	= new boolean[10]; // 0일 수 없는 것
-		
+		boolean nonZeroList[]	= new boolean[10];	// 0일 수 없는 것
+		long valueOfChar[]		= new long[10];		// 각 알파벳 마다 가중치
 		for(int i=0; i<N; i++)
 		{
 			str[i] = br.readLine();
@@ -25,8 +24,8 @@ class Main{
 			
 			long mul	= 1;
 			int len		= str[i].length();
-			for(int j=len-1; j>=0; j--)
-				valueOfChar[str[i].charAt(j)-'A'] += mul*=10;
+			for(int j=len-1; j>=0; j--, mul*=10)
+				valueOfChar[str[i].charAt(j)-'A'] += mul;
 		}
 
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->Long.compare(b.value,a.value));
@@ -43,9 +42,9 @@ class Main{
 			if(rank[i] == 0 && nonZeroList[i]) // 0이면 안 되지만 0인것을 발견했을 때 
 			{
 				int psZeroIdx = -1; // 0이 가능하면서도, rank가 가장 작아야 한다.
-				int dummyRank = 11;
+				int dummyRank = 11; // 0이 가능하면서도, rank가 가장 작아야 한다.
 				for(int j=0; j<10; j++)
-					if(!nonZeroList[j] && dummyRank > rank[j])
+					if(!nonZeroList[j] && dummyRank > rank[j]) // 0이 가능하면서도, rank가 가장 작은 것을 저장해 나간다.
 					{
 						psZeroIdx = j;
 						dummyRank = rank[j];
@@ -53,23 +52,17 @@ class Main{
 					}
 				
 				for(int j=0; j<10; j++) 
-					if(rank[psZeroIdx] > rank[j]) 
+					if(rank[psZeroIdx] > rank[j])  	// 목표 랭크(0이 가능하면서도 rank가 가장 작았던 것)보다 작은 것들은 모두 +1을 해버린다.
 						rank[j]++;
 
-				rank[psZeroIdx] = 0;
+				rank[psZeroIdx] = 0;				// 목표 랭크는 0으로 치환
 				break;
 			}
 		
 		
 		long res = 0;
-		for(int i=0; i<N; i++) 
-		{
-			long num = 0;
-			for(char c : str[i].toCharArray())
-				num = num * 10 + rank[c-'A'];
-
-			res += num;
-		}
+		for(int i=0; i<10; i++)
+			res += valueOfChar[i] * rank[i];
 		
 		System.out.print(res);
 	}
