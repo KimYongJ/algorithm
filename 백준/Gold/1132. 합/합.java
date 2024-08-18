e8eb08813847e1a97ee3e947fc36456fb87d5243
YongJu Kim <kyjdummy@gmail.com>
2024-08-18 18:38:22 +0900

    [Gold III] Title: 합, Time: 100 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Gold/1132. 합/합.java
+++ 백준/Gold/1132. 합/합.java
@@ -5,30 +5,30 @@ import java.util.PriorityQueue;
 class Node{
 	int ch;
 	long value;
-	Node(int ch, long value){this.ch = ch;	this.value = value;	}
+	Node(int ch, long value){
+		this.ch = ch;
+		this.value = value;
+	}
 }
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		long valueOfChar[] = new long[10];
-		String str[] = new String[N];
-		boolean nonZeroList[] = new boolean[10]; // 0일 수 없는 것
+		int N					= Integer.parseInt(br.readLine());		
+		String str[]			= new String[N];
+		long valueOfChar[]		= new long[10];
+		boolean nonZeroList[]	= new boolean[10]; // 0일 수 없는 것
+		
 		for(int i=0; i<N; i++)
 		{
 			str[i] = br.readLine();
 			nonZeroList[str[i].charAt(0)-'A'] = true;
+			
+			long mul	= 1;
+			int len		= str[i].length();
+			for(int j=len-1; j>=0; j--)
+				valueOfChar[str[i].charAt(j)-'A'] += mul*=10;
 		}
-		
-		for(int i=0; i<N; i++) 
-		{
-			long mul = 1;
-			int len = str[i].length();
-			for(int j=len-1; j>=0; j--,mul*=10) {
-				valueOfChar[str[i].charAt(j)-'A'] += mul;
-			}
-		}
-		
+
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->Long.compare(b.value,a.value));
 		for(int i=0; i<10; i++)
 			if(valueOfChar[i] > 0)
@@ -36,23 +36,22 @@ class Main{
 			
 		int rank[] = new int[10];
 		int f = 9;
-		
 		while(!pq.isEmpty())
 			rank[pq.poll().ch] = f--;
 		
-		int psZeroIdx = -1; // 0이 가능하면서도, rank가 가장 작아야 한다.
-		int dummyRank = 11;
 		for(int i=0; i<10; i++)
-			if(!nonZeroList[i] && dummyRank > rank[i])
+			if(rank[i] == 0 && nonZeroList[i]) // 0이면 안 되지만 0인것을 발견했을 때 
 			{
-				psZeroIdx = i;
-				dummyRank = rank[i];
+				int psZeroIdx = -1; // 0이 가능하면서도, rank가 가장 작아야 한다.
+				int dummyRank = 11;
+				for(int j=0; j<10; j++)
+					if(!nonZeroList[j] && dummyRank > rank[j])
+					{
+						psZeroIdx = j;
+						dummyRank = rank[j];
 
-			}
-		
-		for(int i=0; i<10; i++)
-			if(rank[i] == 0 && nonZeroList[i]) 
-			{
+					}
+				
 				for(int j=0; j<10; j++) 
 					if(rank[psZeroIdx] > rank[j]) 
 						rank[j]++;
@@ -60,6 +59,7 @@ class Main{
 				rank[psZeroIdx] = 0;
 				break;
 			}
+		
 		
 		long res = 0;
 		for(int i=0; i<N; i++) 
