53d4421ebca324834397a2d661919bd28ecf0b6c
YongJu Kim <kyjdummy@gmail.com>
2024-08-19 13:42:16 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.java
@@ -1,43 +1,55 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	 private static int read() throws Exception{
+	        int val = 0;
+	        int c = System.in.read();
+	        while (c <= ' ') {
+	            c = System.in.read();
+	        }
+	        boolean minus = false;
+	        if (c == '-') {
+	            minus = true;
+	            c = System.in.read();
+	        }
+	        do {
+	            val = 10 * val + c - 48;
+	        } while ((c = System.in.read()) >= 48 && c <= 57);
+	        if (minus) return -val;
+	        return val;
+	    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int goal[]	= new int[N];
 		int result	= 0;
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)
-			goal[i] = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)
-			goal[i] -= Integer.parseInt(st.nextToken());
+		for(int i=0; i<N; i++)goal[i] = read();
+		for(int i=0; i<N; i++)goal[i] -= read();
 		
 		for(int i=0; i<N; i++) 
 		{
-			int min = Math.abs(goal[i]);
-			int j;
 			if(goal[i] == 0) 
 				continue;
-			for(j = i+1; j<N; j++) {
-				if(goal[j] == 0)
-					break;
-				if(!((goal[i]>0 && goal[j]>0) || (goal[i]<0 && goal[j]<0)))
+			
+			int min = Math.abs(goal[i]);
+			int j;
+			
+			for(j = i+1; j<N; j++) 
+			{
+				if((goal[j] == 0) || !((goal[i]>0 && goal[j]>0) || (goal[i]<0 && goal[j]<0)))
 					break;
 				min = Math.min(min, Math.abs(goal[j]));
 			}
+			
 			if(goal[i] < 0) 
 				min = -min;
+			
 			while(--j>=0) 
 				goal[j] -= min;
 			
+			if(goal[i] != 0) 
+				i--;
+			
 			result += Math.abs(min);
-			if(goal[i] != 0) i--;
 		}
 		System.out.print(result);
 	}
