090c906186c2597fc6599337831ffe6e2c0df7e4
YongJu Kim <kyjdummy@gmail.com>
2024-08-19 15:46:08 +0900

    [Gold II] Title: 보석 도둑, Time: 920 ms, Memory: 49260 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,32 +1,28 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int w, c; Node(int w,int c){this.w=w;this.c=c;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Integer> Kpq	= new PriorityQueue<>();
 		PriorityQueue<Node> list	= new PriorityQueue<Node>((a,b)->a.w-b.w);
 		PriorityQueue<Integer> store= new PriorityQueue<Integer>((a,b)->b-a);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int N = read();
+		int K = read();
 		
 		for(int i=0; i<N; i++) 
-		{
-			st = new StringTokenizer(br.readLine());
-			list.add(new Node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
-		}
+			list.add(new Node(read(),read()));
 		
 		for(int i=0; i<K; i++)
-			Kpq.add(Integer.parseInt(br.readLine()));
+			Kpq.add(read());
 		
 		long res = 0;
-		
 		while(!Kpq.isEmpty())
 		{
 			int weight = Kpq.poll();
