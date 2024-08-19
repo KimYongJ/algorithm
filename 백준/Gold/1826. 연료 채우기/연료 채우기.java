6d8836a8eada62eeb8e33ce840edacd2b8cca095
YongJu Kim <kyjdummy@gmail.com>
2024-08-19 17:08:12 +0900

    [Gold II] Title: 연료 채우기, Time: 168 ms, Memory: 15340 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -1,30 +1,24 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-class Node{
-	int d, f;Node(int d,int f){this.d=d;this.f=f;}
-}
+class Node{int d, f;Node(int d,int f){this.d=d;this.f=f;}}
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq		= new PriorityQueue<Node>((a,b)->a.d-b.d);
 		PriorityQueue<Integer>fuelQ = new PriorityQueue<Integer>((a,b)->b-a);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N	= Integer.parseInt(br.readLine());
+		int N	= read();
 		int res = 0;
 		
 		for(int i=0; i<N; i++) 
-		{
-			st = new StringTokenizer(br.readLine());
-			pq.add(new Node(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		int goal = Integer.parseInt(st.nextToken()); // 목표위치
-		int nowp = Integer.parseInt(st.nextToken()); // 초기 연료이자 초기에 갈 수 있는 최대 위치.
-		int before = -1;
+			pq.add(new Node(read(),read()));
+
+		int goal	= read();	// 목표위치
+		int nowp	= read();	// 초기 연료이자 초기에 갈 수 있는 최대 위치.
+		int before	= -1;		// 반복을 종료하기 위한 플레그
 		
 		while(nowp < goal && before != nowp) 
 		{
