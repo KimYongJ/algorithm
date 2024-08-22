f195cd57f13e6777cb34c5723bc520689404310d
YongJu Kim <kyjdummy@gmail.com>
2024-08-23 00:34:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,9 +1,11 @@
-import java.io.*;
-
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
     public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
+        int k = read();
         int three = 0 ;
         while(true){
             if(k%5==0){
