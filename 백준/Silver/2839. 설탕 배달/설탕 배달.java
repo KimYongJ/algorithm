f5384d413de0246747a133d3b4c77bc3657ef83a
YongJu Kim <kyjdummy@gmail.com>
2024-08-23 00:35:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -7,16 +7,16 @@ class Main{
     public static void main(String[] args)throws Exception{
         int k = read();
         int three = 0 ;
-        while(true){
-            if(k%5==0){
+        while(k>=0)
+        {
+            if(k%5==0)
+            {
                 System.out.println(k/5+three);
-                return;
-            }else if(k<0){
-                System.out.println(-1);
                 return;
             }
             k-=3;
             three++;          
         }
+        System.out.println(-1);
     }
 }
