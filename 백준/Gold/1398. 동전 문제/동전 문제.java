4216855a4fff19bcbe8a42d49fcbcfc2e15d2b11
YongJu Kim <kyjdummy@gmail.com>
2024-08-22 14:40:00 +0900

    [Gold II] Title: 동전 문제, Time: 96 ms, Memory: 13892 KB -BaekjoonHub

--- 백준/Gold/1398. 동전 문제/동전 문제.java
+++ 백준/Gold/1398. 동전 문제/동전 문제.java
@@ -1,9 +1,11 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static long read() throws Exception {// 빠른 입력을 위한 함수
+		long c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int dp[] = new int[100]; // 1~99까지 숫자 중 최소로 동전을 사용하는 것
 		
@@ -18,11 +20,11 @@ class Main{
 			}
 			dp[i] = Math.min(use_1, use_25);
 		}
-		int T = Integer.parseInt(br.readLine());
+		long T = read();
 		while(T-->0) 
 		{
 			int cnt = 0;
-			long origin = Long.parseLong(br.readLine());
+			long origin = read();
 			while(origin > 0) 
 			{
 				cnt += dp[(int)(origin % 100)];
