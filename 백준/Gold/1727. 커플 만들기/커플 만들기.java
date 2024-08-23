352aa6221a720e9cc7828eb6f8036fc245523d43
YongJu Kim <kyjdummy@gmail.com>
2024-08-23 13:34:47 +0900

    [Gold II] Title: 커플 만들기, Time: 124 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Gold/1727. 커플 만들기/커플 만들기.java
+++ 백준/Gold/1727. 커플 만들기/커플 만들기.java
@@ -1,35 +1,36 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int W = Integer.parseInt(st.nextToken());
-		int man[] = new int[M];
+		int M		= read();
+		int W		= read();
+		int man[]	= new int[M];
 		int woman[] = new int[W];
-		int dp[][] = new int[M+1][W+1];
-		
-		st = new StringTokenizer(br.readLine());
+		int dp[][]	= new int[M+1][W+1];
 		for(int i=0; i<M; i++) 
-			man[i] = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
+			man[i] = read();
 		for(int i=0; i<W; i++) 
-			woman[i] = Integer.parseInt(st.nextToken());
+			woman[i] = read();
 		
 		Arrays.sort(man);
 		Arrays.sort(woman);
 		
-		for(int i=1; i<=M; i++) {
-			for(int j=1; j<=W; j++) {				
+		for(int i=1; i<=M; i++) 
+		{
+			for(int j=1; j<=W; j++) 
+			{				
 				dp[i][j] = dp[i-1][j-1] + Math.abs(man[i-1] - woman[j-1]);
-				if(i > j) {
+				if(i > j) 
+				{
 					dp[i][j] = Math.min(dp[i][j], dp[i-1][j]);
 				}
-				else if(i < j) {
+				else if(i < j)
+				{
 					dp[i][j] = Math.min(dp[i][j], dp[i][j-1]);
 				}
 			}
