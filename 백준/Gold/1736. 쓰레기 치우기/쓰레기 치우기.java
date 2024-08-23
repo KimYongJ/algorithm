06b77298249851b013539043b79d14dc5017f816
YongJu Kim <kyjdummy@gmail.com>
2024-08-23 16:34:01 +0900

    [Gold II] Title: 쓰레기 치우기, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.java
+++ 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.java
@@ -1,42 +1,46 @@
 // https://github.com/kimyongj/algorithm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y		= Integer.parseInt(st.nextToken());
-		int X		= Integer.parseInt(st.nextToken());
+		int Y		= read();
+		int X		= read();
+		int cnt		= 0;
 		int map[][]	= new int[Y][X+1];
 		PriorityQueue<Integer>[] pq = new PriorityQueue[X];
 		for(int y=0; y<Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=0; x<X; x++) 
-				map[y][x] = Integer.parseInt(st.nextToken());
-		}
-		for(int x=0; x<X; x++) {
+				map[y][x] = read();
+		
+		for(int x=0; x<X; x++) 
+		{
 			pq[x] = new PriorityQueue<>();
-			for(int y=0; y<Y; y++) {
-				if(map[y][x] == 1) {
+			for(int y=0; y<Y; y++)
+				if(map[y][x] == 1)
 					pq[x].add(y);
-				}
-			}
 		}
-		int cnt = 0;
-		for(int x=X-1; x>=0; x--) {
-			if(pq[x].size() != 0) {
+		
+		for(int x=X-1; x>=0; x--) 
+		{
+			if(pq[x].size() != 0) 
+			{
 				cnt++;
 				int now = pq[x].peek();
-				for(int x1=x-1; x1>=0; x1--) {
+				for(int x1=x-1; x1>=0; x1--) 
+				{
 					int min = 100;
-					while(!pq[x1].isEmpty() && now >= pq[x1].peek()) {
+					while(!pq[x1].isEmpty() && now >= pq[x1].peek()) 
+					{
 						min = Math.min(min,pq[x1].poll());
 					}
-					if(min < now)
+					if(min < now) 
+					{
 						now = min;
+					}
 				}
 			}
 		}
