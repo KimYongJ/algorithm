0357a09cb205748ef0ba9c52fefa8d3fa822d490
YongJu Kim <kyjdummy@gmail.com>
2024-08-25 13:47:23 +0900

    [Gold II] Title: 책 나눠주기, Time: 132 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.java
@@ -6,34 +6,31 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 class Node{	int a, b;	Node(int a, int b){this.a=a; this.b=b;} }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int find(int parent[], int x) {
 		if(parent[x] == x) return x;
 		return parent[x] = find(parent, parent[x]);
 	}
-	public static void union(int parent[], int x, int y) {
-		parent[x] = find(parent, y);
-	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0) 
 		{
 			PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.b != b.b ? a.b-b.b : a.a-b.a);
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); // 총 책의 수
-			int M = Integer.parseInt(st.nextToken()); // 학생 수
+			int N = read(); // 총 책의 수
+			int M = read(); // 학생 수
 			int cnt = 0;
 			int parent[] = new int[N+2];
+			
 			for(int i=1; i<N+2; i++)
 				parent[i] = i;
 			
-			for(int i=0; i<M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				pq.add(new Node(a,b));
-			}
+			for(int i=0; i<M; i++)
+				pq.add(new Node(read(),read()));
 			
 			while(!pq.isEmpty()) 
 			{
@@ -42,7 +39,7 @@ class Main{
 				if(x <= now.b)
 				{
 					cnt++;
-					union(parent, x, x+1);
+					parent[x] = find(parent, x+1); // 유니온
 				}
 			}
 			sb.append(cnt).append('\n');
