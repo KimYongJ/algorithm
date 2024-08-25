d01dc150896ee6f9fa5b78e484ec9c35e0a446d3
YongJu Kim <kyjdummy@gmail.com>
2024-08-25 21:28:21 +0900

    [Bronze I] Title: 이항 계수 1, Time: 96 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,25 +1,26 @@
-import java.io.*;
-import java.util.*;
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/11050
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
     public static void main(String[] args)throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int n = read();
+        int k = read();
         int[][] dp = new int[n+1][k+1];
         
-        
         for(int i=0; i<=n; i++)
-            for (int j = 0; j <=k; j++) {
-                    if(i<j) continue;
-                    else if (i == j || j== 0) {
+            for (int j = 0; j <=k; j++) 
+            	if(i>=j)
+            	{
+                    if (i == j || j== 0)
                           dp[i][j] = 1;
-                    }else {
+                    else
                           dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                    }
-             }
-                
-        
+            	}
+
         System.out.println(dp[n][k]);
     }
 }
