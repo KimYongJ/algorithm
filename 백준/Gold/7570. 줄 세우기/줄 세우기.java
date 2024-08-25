de179c6f20d4e65fe01c500c36aa3d832ed10158
YongJu Kim <kyjdummy@gmail.com>
2024-08-25 14:58:32 +0900

    [Gold II] Title: 줄 세우기, Time: 228 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -1,32 +1,30 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/7570
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N+1];
-		if(N <= 1) {
-			System.out.print(0);
-			return;
-		}
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++)
-			arr[Integer.parseInt(st.nextToken())] = i;
+		int N		= read();
+		int res		= 0;
+		int cnt		= 1;
+		int arr[]	= new int[N+1];
 		
-		int res = 0;
-		int cnt = 0;
-		for(int i=2; i<=N; i++) {
-			if(arr[i-1] < arr[i]) {
-				cnt++;
-			}else {
-				res = Math.max(res, cnt + 1);
-				cnt = 0;
-			}
+		for(int i=1; i<=N; i++)
+			arr[read()] = i;
+		
+		for(int i=2; i<=N; i++) 
+		{
+			if(arr[i-1] < arr[i])
+				res = Math.max(res, ++cnt);
+			else
+				cnt = 1;
 		}
-		res = Math.max(res, cnt);
+		
+		res = Math.max(res, 1);
+		
 		System.out.print(N-res);
 	}
 }
