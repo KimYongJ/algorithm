e634ed96326b30144dbd0185a29a0761b318593d
YongJu Kim <kyjdummy@gmail.com>
2024-08-29 17:32:11 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 96 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -36,10 +36,9 @@ class Main{
 
 			result++;
 			
-			HashSet<Integer> dummy = new HashSet<Integer>(cord);// 연산을 위한 복사
+			HashSet<Integer> dummy = new HashSet<>(cord);// 연산을 위한 복사
 			for(int i= idx+1; i<K && dummy.size() > 1; i++)// 현재 코드에 있는게 하나남을 때 까지 반복
-				if(dummy.contains(arr[i]))// 가장 늦게 사용되는것을 체크하기 위한 것
-					dummy.remove(arr[i]);
+				dummy.remove(arr[i]);// 가장 늦게 사용되는것을 체크하기 위한 것
 			
 			for(int n : dummy)
 			{
@@ -51,3 +50,8 @@ class Main{
 		System.out.print(result);
 	}
 }
+/*
+3 100
+56 71 70 25 52 77 76 8 68 71 51 65 13 23 7 16 19 54 95 18 86 74 29 76 61 93 44 96 32 72 64 19 50 49 22 14 7 64 24 83 6 3 2 76 99 7 76 100 60 60 6 50 90 49 27 51 37 61 16 84 89 51 73 28 90 77 73 39 78 96 78 13 92 54 70 69 62 78 7 75 30 67 97 98 19 86 90 90 2 39 41 58 57 84 19 8 52 39 26 7
+answer - 80
+ * */
