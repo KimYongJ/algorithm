b256f6080f24494d21b0edf86d9c36085e9b3094
YongJu Kim <kyjdummy@gmail.com>
2024-08-30 17:02:42 +0900

    [Gold I] Title: 숫자 게임, Time: 480 ms, Memory: 102276 KB -BaekjoonHub

--- 백준/Gold/2923. 숫자 게임/숫자 게임.java
+++ 백준/Gold/2923. 숫자 게임/숫자 게임.java
@@ -1,42 +1,59 @@
 // https://github.com/kimyongj/algorithm
 // https://www.acmicpc.net/problem/2923
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T	= read();
 		int A[] = new int[101];
 		int B[] = new int[101];
 		while(T-->0)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			A[Integer.parseInt(st.nextToken())]++;
-			B[Integer.parseInt(st.nextToken())]++;
+			A[read()]++;
+			B[read()]++;
 			int AA[] = new int[101];
 			int BB[] = new int[101];
-			for(int i=1; i<=100; i++) {
+			for(int i=1; i<=100; i++) 
+			{
 				AA[i] = A[i];
 				BB[i] = B[i];
 			}
-			int left = 0;
-			int right = 100;
-			int max = 0;
+			int left	= 0;
+			int right	= 100;
+			int min,max	= 0;
 			while(true) 
 			{
 				while(left<=100 && AA[left]==0)left++;
 				while(right>=0 && BB[right]==0)right--;
-				if(left > 100 || right < 0) break;
-				max = Math.max(max, left + right);
+				if(left > 100 || right < 0)
+					break;
 				
-				int min = Math.min(AA[left], BB[right]);
-				AA[left] -= min;
-				BB[right] -= min;
+				max = Math.max(max, left + right);
+				min = Math.min(AA[left], BB[right]);
+				
+				AA[left]	-= min;
+				BB[right]	-= min;
 			}
 			sb.append(max).append('\n');
 		}
 		System.out.print(sb.toString());
 	}
 }
+/*
+5
+99 1
+99 1
+5 6
+6 6
+6 6
+출력
+100
+100
+100
+100
+100
+ * */
