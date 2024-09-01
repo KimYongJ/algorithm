3ee5fd96d05491cb6e5c9d31beed9af2bc9de371
YongJu Kim <kyjdummy@gmail.com>
2024-09-01 21:53:13 +0900

    택배.java

--- 백준/Gold/8980. 택배/택배.java
+++ 백준/Gold/8980. 택배/택배.java
@@ -1,49 +1,44 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/8980
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node{
 	int s, e, box;
 	Node(int s,int e, int box){this.s=s;this.e=e;this.box=box;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());//마을수(2<=이천)
-		int C = Integer.parseInt(st.nextToken());//용량(1<=만)
-		int M = Integer.parseInt(br.readLine());// 박스정보M(1<=만)
-		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.e!=b.e ? a.e-b.e : a.s-b.s);
-		while(M-->0) 
-		{
-			st = new StringTokenizer(br.readLine());
-			pq.add(new Node(
-					Integer.parseInt(st.nextToken()),
-					Integer.parseInt(st.nextToken()),
-					Integer.parseInt(st.nextToken())));
-		}
+		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.e-b.e);
+		int N		= read();//마을수(2<=이천)
+		int C		= read();//용량(1<=만)
+		int M		= read();// 박스정보M(1<=만)
+		int res		= 0;// 결과
+		int capa[]	= new int[N]; // 각 노드별 최대담을 수 있는 용량 
 		
-		int res = 0; // 결과
-		int capa[] = new int[N]; // 각 노드별 최대담을 수 있는 용량 
 		Arrays.fill(capa, C);
+
+		while(M-->0) 
+			pq.add(new Node(read(),read(),read()));
+		
 		while(!pq.isEmpty()) 
 		{
-			Node now = pq.poll();
-			int s = now.s;
-			int e = now.e;
-			int box = now.box;
+			Node now	= pq.poll();
+			int s		= now.s;
+			int e		= now.e;
+			int box		= now.box;
 			
-			int min = Integer.MAX_VALUE;
 			for(int i=s; i<e; i++)
-				min = Math.min(min, capa[i]);
-			
-			res += min = Math.min(box, min);
+				box = Math.min(box, capa[i]);
 			
 			for(int i=s; i<e; i++) 
-				capa[i] -= min;
+				capa[i] -= box;
+			
+			res += box;
 		}
 		System.out.print(res);
 	}
