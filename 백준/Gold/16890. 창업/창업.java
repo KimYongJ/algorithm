80c6946d23c8500920e7a78ee163fcee9be4540b
YongJu Kim <kyjdummy@gmail.com>
2024-09-02 20:43:55 +0900

    [Gold I] Title: 창업, Time: 284 ms, Memory: 24520 KB -BaekjoonHub

---
+++ 백준/Gold/16890. 창업/창업.java
@@ -0,0 +1,47 @@
+// https://github.com/kimyongj/algorithm
+// https://www.acmicpc.net/problem/16890
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder front = new StringBuilder();
+		StringBuilder back	= new StringBuilder();
+		char[] forward	= br.readLine().toCharArray();
+		char[] reverse	= br.readLine().toCharArray();
+		
+		Arrays.sort(forward);
+		Arrays.sort(reverse);
+		
+		int len = forward.length;
+		int fStartIdx = 0, rStartIdx = len-1;
+		int fEndIdx = len/2 + len%2 -1, rEndIdx = fEndIdx+1;
+		for(int i=0; i<len; i++)
+		{
+			if(i%2==0)// 정방향으로 놓길 원하는 사람 
+			{
+				if(forward[fStartIdx] < reverse[rStartIdx])
+					front.append(forward[fStartIdx++]);
+				else 
+					back.append(forward[fEndIdx--]);
+			}
+			else // 역방향으로 놓길 원하는 사람
+			{
+				if(forward[fStartIdx] < reverse[rStartIdx]) 
+					front.append(reverse[rStartIdx--]);
+				else 
+					back.append(reverse[rEndIdx++]);
+			}
+		}
+		
+		front.append(back.reverse().toString());
+		
+		System.out.print(front.toString());	
+	}
+}
+/*
+bb // 1<=30만 문자열(소문자)
+aa
+답ab
+ * */
