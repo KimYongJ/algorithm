4465abc3f8b9ae7d0b904e0af610d453743cee01
YongJu Kim <kyjdummy@gmail.com>
2024-09-03 17:55:23 +0900

    [Gold I] Title: 동전 뒤집기, Time: 152 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -1,27 +1,27 @@
-// https://github.com/kimyongj/algorithm
-// https://www.acmicpc.net/problem/1285
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1285
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N		= Integer.parseInt(br.readLine());// 범위 0<=20
-		int row[]	= new int[N]; // 
+		int row[]	= new int[N];
 		for(int i=0; i<N; i++)
 		{
 			String str = br.readLine();
 			for(int j=0; j<N; j++)
-				row[i] |= str.charAt(j) == 'T' ? (1<<N-j-1) : 0;
+				row[i] |= str.charAt(j) == 'T' ? (1<<j) : 0;
 		}
 		
 		int ans = Integer.MAX_VALUE;
 		
-		for(int s=0; s<(1<<N); s++)
+		for(int s=0; s<(1<<N); s++) // 특정 열에 대해 뒤집을지 말지 결정 ex)N=3 -> 000, 001, 002 ... 111까지
 		{
 			int sum = 0;
-			for(int x=0; x<N; x++) 
+			for(int x=0; x<N; x++) // 각 행마다 위에서 정한 s를 다 대입함
 			{
-				int Tcnt = Integer.bitCount(row[x] ^ s);
+				int Tcnt = Integer.bitCount(row[x] ^ s);// 가로행에다 열에 대한 값(s)를 도입해 뒤집음
 				sum += Math.min(Tcnt,  N-Tcnt);
 			}
 			ans = Math.min(ans, sum);
