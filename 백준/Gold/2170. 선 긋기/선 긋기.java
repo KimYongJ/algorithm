25827cbad99abf3b54496cc01fa5baa48cbc7cac
YongJu Kim <kyjdummy@gmail.com>
2024-09-06 23:22:10 +0900

    [Gold V] Title: 선 긋기, Time: 1032 ms, Memory: 51936 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -1,34 +1,36 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2170
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-class Node{
-	int s,e; Node(int s,int e){this.s=s;this.e=e;}
-}
+class Node{int s,e; Node(int s,int e){this.s=s;this.e=e;}}
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)->a.s-b.s);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int s,e, res = 0;
+		int N	= read();
+		int res = 0;
+		
 		for(int i=0; i<N; i++)
+			pq.add(new Node(read(),read()));
+		
+		Node now	= pq.poll();
+		int s		= now.s;
+		int e		= now.e;
+		while(!pq.isEmpty())
 		{
-			st = new StringTokenizer(br.readLine());
-			s = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
-			pq.add(new Node(s,e));
-		}
-		Node now = pq.poll();
-		s = now.s;
-		e = now.e;
-		while(!pq.isEmpty()) {
 			now = pq.poll();
-			if(now.s <= e) {
+			if(now.s <= e)
+			{
 				e = Math.max(e, now.e);
-			}else {
+			}
+			else 
+			{
 				res += e - s;
 				s = now.s;
 				e = now.e;
