e9e44a97f74ad7e09bce4561aa16f9bf156e08c5
YongJu Kim <kyjdummy@gmail.com>
2024-09-06 16:22:06 +0900

    [Platinum IV] Title: 옥토끼는 통신교육을 풀어라!!, Time: 356 ms, Memory: 28560 KB -BaekjoonHub

---
+++ 백준/Platinum/17383. 옥토끼는 통신교육을 풀어라！！/옥토끼는 통신교육을 풀어라！！.java
@@ -0,0 +1,57 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/17383
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static boolean check(long mid, long arr[]) {
+		long one = 0;		// 1의 개수
+		long two = 0;		// 2의 개수
+		for(long a : arr) {
+			long flag = (long)Math.ceil((double)a/mid);
+			if(flag == 1) one++;
+			else if(flag == 2) two++;
+			else {
+				one -= flag - 2;// 1사용 개수
+			}
+		}
+		if(one < 0) return false;
+		if(one <= 0 && two > 0) return false;
+		return true;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // N(1<=십만)
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long arr[] = new long[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Long.parseLong(st.nextToken()); // 원소 범위(1<=1억)
+		}
+		long start = 1;
+		long end = 1_100_000_000;
+		long mid;
+		long res = 0;
+		while(start <= end) 
+		{
+			mid = (start + end) / 2;
+			if(check(mid, arr)) {
+				res = mid;
+				end = mid -1;
+			}else {
+				start = mid + 1;
+			}
+		}
+		System.out.print(res);
+	}
+}
+/*
+7
+35 4 4 4 4 4 4
+출력 : 5
+7
+1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000
+출력 : 1000000000
+7
+150000000 200000000 250000000 450000000 500000000 700000000 750000000
+출력 : 250000000
+ * */
