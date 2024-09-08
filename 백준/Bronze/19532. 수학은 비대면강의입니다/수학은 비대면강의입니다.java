5f24c10935ee985113d60b5887903ca8715ad0aa
YongJu Kim <kyjdummy@gmail.com>
2024-09-08 18:48:23 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,31 +1,28 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/19532
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a,b,c,d,e,f,x,y;
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		f = Integer.parseInt(st.nextToken());
-		
-		x = -1000;
-		while(++x < 1000) {
+		int a=read(),b=read(),c=read(),d=read(),e=read(),f=read(),x=-1000,y;
+
+		while(++x < 1000) 
+		{
 			y = -1000;
-			while(++y < 1000){
-				if((a*x + b*y == c) &&( d*x + e*y == f)) {
+			while(++y < 1000)
+				if((a*x + b*y == c) &&( d*x + e*y == f))
+				{
 					sb.append(x).append(' ').append(y);
-					break;
+					System.out.print(sb.toString());
+					return;
 				}
-			}
 		}
-		System.out.print(sb.toString());
 	}
 }
