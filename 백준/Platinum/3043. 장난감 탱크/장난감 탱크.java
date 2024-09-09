d4113643d81646ed7a6a9934d7511b78947bee5c
YongJu Kim <kyjdummy@gmail.com>
2024-09-09 20:56:35 +0900

    [Platinum IV] Title: 장난감 탱크, Time: 152 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Platinum/3043. 장난감 탱크/장난감 탱크.java
+++ 백준/Platinum/3043. 장난감 탱크/장난감 탱크.java
@@ -1,9 +1,6 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3043
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Node{
 	int num, idx;
 	Node(int n,int i){this.num=n;this.idx=i;}
@@ -15,76 +12,89 @@ class Main{
 	static final char MOVE_DOWN		= 'D';
 	static final char MOVE_LEFT		= 'L';
 	static final char MOVE_RIGHT	= 'R';
+
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
-	public static void mark(int num, char c) {
+	public static void mark(int num, char c) 
+	{
 		sb.append(num).append(' ').append(c).append('\n');
 	}
-	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N		= Integer.parseInt(br.readLine()); // N(5<=500)
-		Node row[]	= new Node[N];// 가로 정보
-		Node col[]	= new Node[N];// 세로정보
-		int cnt		= 0;
+	public static int MOVE_UP_OR_LEFT(Node arr[], int N, char MOVE) {
+		int cnt = 0;
 		for(int i=0; i<N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int r	= Integer.parseInt(st.nextToken());
-			int c	= Integer.parseInt(st.nextToken());
+			Node now = arr[i];
+			int idx = now.idx;
+			int target = i + 1;
+			while(idx > target) {
+				++cnt;
+				--idx;
+				mark(now.num, MOVE);
+			}
+		}
+		return cnt;
+	}
+	public static int MOVE_DOWN_OR_RIGHT(Node arr[], int N, char MOVE) {
+		int cnt = 0;
+		for(int i=N-1; i>=0; i--) {
+			Node now = arr[i];
+			int idx = now.idx;
+			int target = i + 1;
+			while(idx < target) {
+				++cnt;
+				++idx;
+				mark(now.num, MOVE);
+			}
+		}
+		return cnt;
+	}
+	public static void main(String[] args)throws Exception{
+		int N		= read();		// N(5<=500)
+		int cnt		= 0;
+		Node row[]	= new Node[N];	// 가로 정보
+		Node col[]	= new Node[N];	// 세로정보
+		
+		for(int i=0; i<N; i++)
+		{
+			int r	= read();
+			int c	= read();
 			row[i]	= new Node(i+1, r);
 			col[i]	= new Node(i+1, c);
 		}
+		
 		Arrays.sort(row,(a,b)->a.idx - b.idx);
 		Arrays.sort(col,(a,b)->a.idx - b.idx);
 		
-		
-		for(int i=0; i<N; i++)
-		{
-			Node now = row[i];
-			int idx = now.idx;
-			int target = i + 1;
-			while(idx > target) {
-				++cnt;
-				--idx;
-				mark(now.num, MOVE_UP);
-			}
-		}
-		for(int i=N-1; i>=0; i--) {
-			Node now = row[i];
-			int idx = now.idx;
-			int target = i + 1;
-			while(idx < target) {
-				++cnt;
-				++idx;
-				mark(now.num, MOVE_DOWN);
-			}
-		}
-		for(int i=0; i<N; i++)
-		{
-			Node now = col[i];
-			int idx = now.idx;
-			int target = i + 1;
-			while(idx > target) {
-				++cnt;
-				--idx;
-				mark(now.num, MOVE_LEFT);
-			}
-		}
-		for(int i=N-1; i>=0; i--) {
-			Node now = col[i];
-			int idx = now.idx;
-			int target = i + 1;
-			while(idx < target) {
-				++cnt;
-				++idx;
-				mark(now.num, MOVE_RIGHT);
-			}
-		}
-		
-		
-		
-		
+		cnt += MOVE_UP_OR_LEFT(		row, N, MOVE_UP);
+		cnt += MOVE_DOWN_OR_RIGHT(	row, N, MOVE_DOWN);
+		cnt += MOVE_UP_OR_LEFT(		col, N, MOVE_LEFT);
+		cnt += MOVE_DOWN_OR_RIGHT(	col, N, MOVE_RIGHT);
+
 		System.out.println(cnt);
 		System.out.print(sb.toString());
 	}
 }
+/*
+입력
+6
+1 1
+1 2
+2 1
+5 6
+6 5
+6 6
+출력
+8
+2 R
+2 D
+3 D
+3 R
+4 U
+4 L
+5 L
+5 U
+ * */
