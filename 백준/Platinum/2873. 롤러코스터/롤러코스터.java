4a1b5ea79bb28c631a6f1dd9dbc998eb313f7c3a
YongJu Kim <kyjdummy@gmail.com>
2024-09-13 22:41:46 +0900

    [Platinum III] Title: 롤러코스터, Time: 304 ms, Memory: 19216 KB -BaekjoonHub

--- 백준/Platinum/2873. 롤러코스터/롤러코스터.java
+++ 백준/Platinum/2873. 롤러코스터/롤러코스터.java
@@ -1,13 +1,12 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2873
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
-	static final char ud[] = {'U','D'};
-	static final char lr[] = {'L','R'};
-	public static void input(StringBuilder sb, int X, int Y,char mm[], char dir) {
-		int flag = 1;
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	public static void input(StringBuilder sb, int X, int Y,char mm[], char dir, int flag) {
 		for(int x=0; x<X; x++)
 		{
 			for(int y=1; y<Y; y++)
@@ -17,27 +16,30 @@ class Main{
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int Y = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
+		final char ud[] = {'U','D'};
+		final char lr[] = {'L','R'};
+		final String RU = "RU";
+		final String RD = "RD";
+		int Y = read();
+		int X = read();
 		
-		if(Y % 2 == 1 || X % 2 == 1)
+		if(Y % 2 == 1 || X % 2 == 1)			// 하나라도 홀 수 일 경우 그냥 탐색하면됨
 		{
 			if(Y % 2 == 0)
-				input(sb, X, Y, ud, 'R');// 다운부터 시작 W모양
+				input(sb, X, Y, ud, 'R', 1);	// 다운부터 시작 W모양
 			else
-				input(sb, Y, X, lr, 'D');// 오른쪽 부터 시작 ㄹ모양
-		}else {
+				input(sb, Y, X, lr, 'D', 1);	// 오른쪽 부터 시작 ㄹ모양
+		}
+		else									// 둘다 짝수일 경우
+		{
 			int my = 0, mx = 0, mn = 1001;
 			for(int y=0; y<Y; y++)
 			{
-				st = new StringTokenizer(br.readLine());
 				for(int x=0; x<X; x++)
 				{
-					int n = Integer.parseInt(st.nextToken());
-					if((y+x)%2 == 1 && mn > n)
+					int n = read();
+					if((y+x)%2 == 1 && mn > n)	// 두 좌표를 더했을 때 홀 수에 있는 위치의 최솟값을 찾음
 					{
 						mn = n;
 						my = y;
@@ -46,54 +48,41 @@ class Main{
 				}
 			}
 			
-			int sy = 0;
-			int lrFlag = 1; // 왼쪽 오른쪽 플레그
-			while(sy<Y)
+			int sy = 0;							// 세로 행 인덱스
+			int lrFlag = 1; 					// 왼쪽 오른쪽 플레그
+			while(sy<Y)							// 세로는 2라인씩 탐색한다. (0,1) , (2,3) , (4,5) 씩..
 			{
-				if(sy == my || sy + 1 == my)
+				if(sy == my || sy + 1 == my)	// 해당 구간에 최소 값이 있는지 체크
 				{
 					StringBuilder part = new StringBuilder();
 					part.append('D');
-					String RU = "RU";
-					String RD = "RD";
 					boolean flag = true;
 					int repeat = X-2;
-					while(repeat-->0)
+					while(repeat-->0)			// 규칙 : 처음 D를 넣고 X-2번씩 RU, RD를 반복하면됨
 					{
 						if(flag) part.append(RU);
 						else part.append(RD);
 						flag = !flag;
 					}
-					if(mx == 0) {
+					if(mx == 0)					// 최소값이 0번째 인덱스에 있을 때
 						part.insert(0, 'R');
-					}else if(mx == X-1) {
+					else if(mx == X-1)			// 최소값이 맨마지막 인덱스에 있을 때
 						part.append('R');
-					}else {
+					else						// 최소값이 중간에 있을 때
 						part.insert(mx*2, 'R');
-					}
-					
 					
 					sb.append(part.toString()).append('D');
-					lrFlag = 0; // 다음엔 왼쪽 탐색토록함
+					lrFlag = 0; 				// 다음엔 L을 먼저 탐색토록함
 				}
-				else
-				{
-					for(int i=0; i<2; i++)
-					{
-						for(int x=1; x<X; x++)
-						{
-							sb.append(lr[lrFlag]);
-						}
-						sb.append('D');
-						lrFlag = (lrFlag + 1) % 2;
-					}
-				}
+				else							// 최소값이 없다면 ㄹ모양으로 탐색
+					input(sb, 2, X, lr, 'D', lrFlag);
 				
-				sy+=2;
+				sy+=2;							// 두라인씩 탐색
 			}
 		}
 		
 		sb.deleteCharAt(sb.length()-1);
+		
 		System.out.print(sb.toString());
 	}
 }
