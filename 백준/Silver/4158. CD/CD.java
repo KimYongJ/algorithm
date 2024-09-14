67e1e0da932e40f9d5ad7ca183654409eb36ef10
YongJu Kim <kyjdummy@gmail.com>
2024-09-14 18:24:51 +0900

    [Silver V] Title: CD, Time: 588 ms, Memory: 24240 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -1,42 +1,41 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/4158
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+			int N = read();
+			int M = read();
 			if(N==0 && M == 0)
 				break;
-            
 			int res = 0;
 			int arr[] = new int[N];
-            
 			for(int i=0; i<N; i++)
-				arr[i] = Integer.parseInt(br.readLine());
+				arr[i] = read();
 			
 			for(int i=0; i<N; i++)
 			{
-				int g = Integer.parseInt(br.readLine());
+				int g = read();
 				int s = 0;
 				int e = N-1;
 				while(s <= e)
 				{
 					int mid = (s + e) / 2;
-					if(arr[mid] == g) {
+					if(arr[mid] == g)
+					{
 						res++;
 						break;
-					}else if(arr[mid] < g) {
-						s = mid+1;
-					}else {
-						e = mid-1;
 					}
+					if(arr[mid] < g)
+						s = mid+1;
+					else
+						e = mid-1;
 				}
 			}
 			sb.append(res).append('\n');
