06c7a6090cbc5bcbd938d8cea9dd2a155b75ae30
YongJu Kim <kyjdummy@gmail.com>
2024-09-14 20:54:04 +0900

    [Platinum II] Title: 수 지우기, Time: 100 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Platinum/1467. 수 지우기/수 지우기.java
+++ 백준/Platinum/1467. 수 지우기/수 지우기.java
@@ -7,31 +7,38 @@ class Main{
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int Ncnt[] = new int[10];
-		int Dcnt[] = new int[10];
+		int Ncnt[] = new int[10]; // 기본숫자 카운팅 정렬
+		int Dcnt[] = new int[10]; // 지울숫자 카운팅 정렬
 		
-		ArrayList<Integer> nlist = new ArrayList<>();
-		ArrayList<Integer> result= new ArrayList<>();
-		
-		for(char c : br.readLine().toCharArray()) {
+		ArrayList<Integer> nlist = new ArrayList<>();//기본숫자 담을 배열
+		ArrayList<Integer> result= new ArrayList<>();//결과를 담을 배열
+		char c;
+		while((c=(char)System.in.read()) != '\n'){
 			int n = c-'0';
 			nlist.add(n);
-			Ncnt[n]++;
+			Ncnt[n]++;// 기본 숫자 카운팅 정렬
 		}
-		for(char c : br.readLine().toCharArray()) {
-			Dcnt[c-'0']++;
+		while((c=(char)System.in.read()) != '\n'){
+			Dcnt[c-'0']++;// 지울숫자 카운팅 정렬
 		}
 		
-		for(int now : nlist) {
+		for(int now : nlist) 
+		{
+			// 지울 숫자가 0개이상이고, 기본숫자와 지울숫자의 개수가 같을 때
 			if(Dcnt[now] > 0 && Ncnt[now] == Dcnt[now]) {
 				Ncnt[now]--;
 				Dcnt[now]--;
 			}else {
-				while(!result.isEmpty()) {
+				while(!result.isEmpty())// 결과 배열이 빌 때 까지 반복
+				{
+					// 마지막 인덱스를 꺼내옴
 					int last = result.get(result.size() - 1);
+					// 확인하려는 값보다 마지막 인덱스가 크거나 같다면, 혹은 
+					// 마지막인덱스가 지울것이 아니라면 break
 					if(last >= now || Dcnt[last] == 0) {
 						break;
 					}else {
+						// 리스트의 마지막에 담긴 값이 확인하려는 값보다 작은데, 지울 숫자라면 지움
 						Dcnt[last]--;
 						result.remove(result.size()-1);
 					}
