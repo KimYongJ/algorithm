5055529d334a4ef73fb1b1ef57ec9a087f576225
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 16:17:48 +0900

    [Silver I] Title: 접두사 찾기, Time: 356 ms, Memory: 32288 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
@@ -2,26 +2,59 @@
 //https://www.acmicpc.net/problem/14426
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
+class Node{
+	boolean now;
+	Node[] next = new Node[26];
+	Node(boolean now) {this.now = now;}
+}
+class Trie{
+	Node[] string;
+	Trie() {string = new Node[26];}
+	
+	public void add(String str)
+	{
+		int c = str.charAt(0) - 'a';
+		if(string[c] == null) {
+			string[c] = new Node(true);
+		}
+		Node now[] = string[c].next;
+		for(int i=1; i<str.length(); i++) {
+			c = str.charAt(i) - 'a';
+			if(now[c] == null) {
+				now[c] = new Node(true);
+			}
+			now = now[c].next;
+		}
+	}
+	public boolean contains(String str) {
+		int c = str.charAt(0) - 'a';
+		if(string[c] == null) return false;
+		Node now[] = string[c].next;
+		for(int i=1; i<str.length(); i++) {
+			c = str.charAt(i) - 'a';
+			if(now[c] == null) return false;
+			now = now[c].next;
+		}
+		return true;
+	}
+}
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());//기준 문자열 N개 (1<=만)
-		int M = Integer.parseInt(st.nextToken());//검사할 문자열 M개 (1<=만)
-		HashSet<String> set = new HashSet<>();
-		for(int i=0; i<N; i++) {
-			String str = br.readLine();
-			for(int j=1; j<=str.length(); j++) {
-				set.add(str.substring(0,j));
-			}
-		}
-		int res = 0;
-		for(int i=0; i<M; i++) {
-			if(set.contains(br.readLine()))
+		int N		= Integer.parseInt(st.nextToken());//기준 문자열 N개	(1<=만)
+		int M		= Integer.parseInt(st.nextToken());//검사할 문자열 M개	(1<=만)
+		int res		= 0;
+		Trie trie	= new Trie();
+		
+		while(N-->0)
+			trie.add(br.readLine());
+		
+		while(M-->0)
+			if(trie.contains(br.readLine()))
 				res++;
-		}
+
 		System.out.print(res);
 	}
 }
