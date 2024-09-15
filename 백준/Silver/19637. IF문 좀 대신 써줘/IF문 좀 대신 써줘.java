86f1459752c8ab3cf3d7034b9b609d94243234d0
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 13:56:03 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 308 ms, Memory: 31636 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -1,22 +1,37 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/19637
 class Main{
+	static int readInt() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	static String readString() throws Exception{
+		StringBuilder sb = new StringBuilder();
+		int c = System.in.read();
+		while(c <= 32) {c = System.in.read();}
+		while(c > 32) {
+			sb.append((char)c);
+			c = System.in.read();
+		}
+		return sb.toString();
+	}
 	public static void main(String[] args)throws Exception{
 		StringBuilder sb = new StringBuilder();
-		Reader in	= new Reader();
-		int N		= in.nextInt();
-		int M		= in.nextInt();
+		int N		= readInt();
+		int M		= readInt();
 		int level[] = new int[N];
 		String name[] = new String[N];
 		
 		for(int i=0; i<N; i++)
 		{
-			name[i]	= in.nextString();
-			level[i]= in.nextInt();
+			name[i]	= readString();
+			level[i]= readInt();
 		}
+        
 		while(M-->0)
 		{
-			int number = in.nextInt();
+			int number = readInt();
 			int s	= 0;
 			int e	= N-1;
 			int idx	= 0;
@@ -28,49 +43,11 @@ class Main{
 					idx = mid;
 					e = mid -1;
 				}
-				else
-				{
-					s = mid + 1;
-				}
+				else 
+                    s = mid + 1;
 			}
-			
 			sb.append(name[idx]).append('\n');
 		}
 		System.out.print(sb.toString());
 	}
-}
-
-class Reader {
-    final int SIZE = 1 << 13;
-    byte[] buffer = new byte[SIZE];
-    int index, size;
-
-    String nextString() throws Exception {
-        StringBuilder sb = new StringBuilder();
-        byte c;
-        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
-        do sb.appendCodePoint(c);
-        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
-        return sb.toString();
-    }
-    int nextInt() throws Exception {
-        int n = 0;
-        byte c;
-        while ((c = read()) <= 32) { if (size < 0) return -1; }
-        if (c == 45) { c = read();}
-        do n = (n << 3) + (n << 1) + (c & 15);
-        while (isNumber(c = read()));
-        return n;
-    }
-
-    boolean isNumber(byte c) {
-        return 47 < c && c < 58;
-    }
-    byte read() throws Exception {
-        if (index == size) {
-            size = System.in.read(buffer, index = 0, SIZE);
-            if (size < 0) buffer[0] = -1;
-        }
-        return buffer[index++];
-    }
 }
