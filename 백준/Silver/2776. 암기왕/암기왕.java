497cc7bfa930425b21986b7a2326ddf7153ac7b3
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 10:52:04 +0900

    [Silver IV] Title: 암기왕, Time: 1068 ms, Memory: 39504 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,42 +1,47 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/2776
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			int N = Integer.parseInt(br.readLine());
-			int arr[] = new int[N];
-			st = new StringTokenizer(br.readLine());
-			for(int i=0; i<N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
+			int N		= read();
+			int arr[]	= new int[N];
+			
+			for(int i=0; i<N; i++)
+				arr[i] = read();
+			
 			Arrays.sort(arr);
-			int M = Integer.parseInt(br.readLine());
-			st = new StringTokenizer(br.readLine());
-			while(M-->0) {
-				int num = Integer.parseInt(st.nextToken());
-				int s = 0;
-				int e = N-1;
+			
+			int M = read();
+			while(M-->0)
+			{
+				int num	= read();
+				int s	= 0;
+				int e	= N-1;
 				boolean flag = false;
-				while(s <= e) {
+				while(s <= e)
+				{
 					int mid = (s + e) / 2;
-					if(arr[mid] == num) {
+					if(arr[mid] == num)
+					{
 						flag = true;
 						break;
 					}
-					if(arr[mid] < num) {
+					if(arr[mid] < num)
 						s = mid + 1;
-					}else {
+					else 
 						e = mid - 1;
-					}
 				}
 				sb.append(flag ? 1 : 0).append('\n');
 			}
