84a6fefecb4b247d6dc1cc9ec04d0b9ef43b7eb6
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 11:22:18 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 252 ms, Memory: 20308 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,34 +1,31 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/2776
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+			int N = read();
+			int M = read();
 			int res = 0;
 			int arr1[] = new int[N];
 			int arr2[] = new int[M];
 			
-			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++)
-				arr1[i] = Integer.parseInt(st.nextToken());
+				arr1[i] = read();
 			
-			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<M; i++)
-				arr2[i] = Integer.parseInt(st.nextToken());
+				arr2[i] = read();
 			
 			Arrays.sort(arr2);
-            
+			
 			for(int n : arr1)
 			{
 				int s = 0;
