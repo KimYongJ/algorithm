2003a039a3eca8555a5d5c7b66b6fb304fafeabd
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 13:46:27 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 312 ms, Memory: 36156 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -1,27 +1,22 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/19637
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
 		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		String name[] = new String[N];
+		Reader in	= new Reader();
+		int N		= in.nextInt();
+		int M		= in.nextInt();
 		int level[] = new int[N];
+		String name[] = new String[N];
+		
 		for(int i=0; i<N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			name[i]	= st.nextToken();
-			level[i]= Integer.parseInt(st.nextToken());
+			name[i]	= in.nextString();
+			level[i]= in.nextInt();
 		}
 		while(M-->0)
 		{
-			int number = Integer.parseInt(br.readLine());
+			int number = in.nextInt();
 			int s	= 0;
 			int e	= N-1;
 			int idx	= 0;
@@ -43,4 +38,39 @@ class Main{
 		}
 		System.out.print(sb.toString());
 	}
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read();}
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
