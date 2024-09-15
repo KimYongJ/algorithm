618f48f74b4873ced539a286071e50ea825c5c29
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 14:13:19 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 140 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -1,30 +1,28 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/17266
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static boolean check(int pos[], int mid, int N) {
 		int s = 0;// 가장왼쪽
-		for(int p : pos) {
-			if(p - mid > s) {
+		for(int p : pos)
+		{
+			if(p - mid > s)
 				return false;
-			}
 			s = p + mid;
 		}
 		return pos[pos.length-1] + mid >= N;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	// 굴다리 길이 (1<=십만)
-		int M		= Integer.parseInt(br.readLine());	// 가로등의 개수 M (1<=N)
-		int pos[]	= new int[M];						// 가로등의 위치(오름차순 정렬)
+		int N		= read();		// 굴다리 길이 (1<=십만)
+		int M		= read();		// 가로등의 개수 M (1<=N)
+		int pos[]	= new int[M];	// 가로등의 위치(오름차순 정렬)
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<M; i++)
-		{
-			pos[i] = Integer.parseInt(st.nextToken());
-		}
+			pos[i] = read();
 		
 		int res	= 0;
 		int s	= 0;
@@ -32,12 +30,13 @@ class Main{
 		while(s <= e)
 		{
 			int mid = (s + e) / 2;
-			if(check(pos, mid, N)) {
+			if(check(pos, mid, N))
+			{
 				res = mid;
 				e = mid - 1;
-			}else {
-				s = mid + 1;
 			}
+			else
+				s = mid + 1;
 		}
 		System.out.print(res);
 	}
