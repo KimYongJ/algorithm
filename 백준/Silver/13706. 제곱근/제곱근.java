60466f44a654d6f841aa806809d1bfff43fcffdc
YongJu Kim <kyjdummy@gmail.com>
2024-09-15 15:10:07 +0900

    [Silver IV] Title: 제곱근, Time: 112 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Silver/13706. 제곱근/제곱근.java
@@ -0,0 +1,11 @@
+//https://github.com/KimYongJ/algorithm
+//https://www.acmicpc.net/problem/13706
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(new BigInteger(br.readLine()).sqrt());
+	}
+}
