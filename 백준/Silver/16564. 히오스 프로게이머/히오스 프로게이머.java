e53d7d5edb361593d6ad4b7736cf6ea52dbd4f71
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 11:34:20 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 140 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
@@ -1,9 +1,11 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16564
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static boolean check(int[] arr, long mid, int K) {
 		for(int a : arr)
 			if(mid >= a)
@@ -16,16 +18,14 @@ class Main{
 		return true;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 캐릭터의 개수 N(1<=백만)
-		int K		= Integer.parseInt(st.nextToken());	// 레벨 총합 K(1<=십업)
-		int arr[]	= new int[N];						// 현재 각 캐릭터의 레벨(1<=십억)
+		int N		= read();		// 캐릭터의 개수 N(1<=백만)
+		int K		= read();		// 레벨 총합 K(1<=십업)
+		int arr[]	= new int[N];	// 현재 각 캐릭터의 레벨(1<=십억)
 		long s		= 1;
 		long e		= Integer.MAX_VALUE;
 		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 
 		long T = 0;// 최대 올릴 수 있는 레벨
 		while(s <= e)
