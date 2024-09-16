3a86f7e0cc47802b700af171d9e0ca09cef6c467
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 13:07:33 +0900

    [Silver III] Title: 선분 위의 점, Time: 428 ms, Memory: 27600 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.java
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.java
@@ -1,17 +1,19 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/11663
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int getFirst(int arr[], int len, int goal) { // arr에서 goal보다크거나 같은 가장 왼쪽 값 
-		int res = 0;
-		long s = 0;
-		long e = len - 1;
+		int res	= 0;
+		int s	= 0;
+		int e	= len - 1;
 		while(s <= e)
 		{
-			int mid = (int)((s + e) >> 1);
+			int mid = (s + e) >> 1;
 			if(arr[mid] < goal)
 				s = mid + 1;
 			else
@@ -23,12 +25,12 @@ class Main{
 		return res;
 	}
 	public static int getLast(int arr[], int len, int goal) { // arr에서 goal보다 작거나 같은 가장 오른쪽 값 
-		int res = 0;
-		long s = 0;
-		long e = len - 1;
+		int res	= 0;
+		int s	= 0;
+		int e	= len - 1;
 		while(s <= e)
 		{
-			int mid = (int)((s + e) >> 1);
+			int mid = (s + e) >> 1;
 			if(arr[mid] <= goal)
 			{
 				s = mid + 1;
@@ -40,27 +42,23 @@ class Main{
 		return res + 1;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N		= Integer.parseInt(st.nextToken());	// 점의개수(1<=십만)
-		int M		= Integer.parseInt(st.nextToken());	// 선분의 개수(1<=십만)
-		int arr[]	= new int[N];						// 점의 좌표를 담을 배열(좌표는 1<=십억)
+		int N		= read();		// 점의개수(1<=십만)
+		int M		= read();		// 선분의 개수(1<=십만)
+		int arr[]	= new int[N];	// 점의 좌표를 담을 배열(좌표는 1<=십억)
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int f		= Integer.parseInt(st.nextToken());// 선분의 시작
-			int l		= Integer.parseInt(st.nextToken());// 선분의 끝
+			int f		= read();	// 선분의 시작
+			int l		= read();	// 선분의 끝
 			int first	= 0;
 			int last	= 0;
-			if(arr[N-1] >= f && l >= arr[0])
+			if(arr[N-1] >= f && l >= arr[0])	// 유효범위 안일 때만 연산
 			{
 				first	= getFirst(arr, N, f);
 				last	= getLast(arr, N, l);
