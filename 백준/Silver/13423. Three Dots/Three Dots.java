31b5dcc0769fbbd9615e68bebbe10d3cbd4e26f8
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 22:55:51 +0900

    [Silver II] Title: Three Dots, Time: 488 ms, Memory: 26296 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.java
+++ 백준/Silver/13423. Three Dots/Three Dots.java
@@ -1,22 +1,26 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/13423
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-- > 0)
 		{
-			int N		= Integer.parseInt(br.readLine());	// 점의 개수(3<=천)
-			int arr[]	= new int[N];						// 점을 담을 배열(-1억<=1억)
 			int res		= 0;
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N		= read();		// 점의 개수(3<=천)
+			int arr[]	= new int[N];	// 점을 담을 배열(-1억<=1억)
+			
 			for(int i=0; i<N; i++)
-				arr[i] = Integer.parseInt(st.nextToken());
+				arr[i] = read();
 			
 			Arrays.sort(arr);
 			
