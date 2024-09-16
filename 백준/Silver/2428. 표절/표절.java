7b7029c088d53618419a013ed2dccf96f901da41
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 16:37:16 +0900

    [Silver III] Title: 표절, Time: 276 ms, Memory: 25696 KB -BaekjoonHub

--- 백준/Silver/2428. 표절/표절.java
+++ 백준/Silver/2428. 표절/표절.java
@@ -1,44 +1,42 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2428
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader 	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N		= Integer.parseInt(br.readLine());	// 솔루션개수 (1<=십만)
-		int arr[]	= new int[N];						// 각 솔루션 파일크기(1<=억)
+		int N		= read();		// 솔루션 개수 (1<=십만)
+		int arr[]	= new int[N];	// 각 솔루션 파일 크기(1<=억)
 		long cnt	= 0;
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
 		for(int i=0; i<N; i++)
 		{
-			long target = (long)((arr[i] * 10) / 9);
+			int target	= (arr[i] * 10) / 9;
+			int s		= i + 1;
+			int e		= N - 1;
+			int r		= i;
 			
-			int s = i + 1;
-			int e = N - 1;
-			int r = 0;
 			while(s <= e)
 			{
 				int mid = (s + e) >> 1;
-				if(arr[mid] <= target) {
+				if(arr[mid] <= target)
+				{
 					r = mid;
 					s = mid + 1;
-				}else{
-					e = mid - 1;					
 				}
+				else
+					e = mid - 1;					
 			}
-			if(r != 0) {
-				cnt += r - i;
-			}
+			
+			cnt += r - i;
 		}
 		
 		System.out.print(cnt);
