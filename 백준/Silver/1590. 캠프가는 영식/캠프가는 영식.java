c4c85b13fe173a43dced1638cf83a7986419f6ed
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 19:58:38 +0900

    [Silver IV] Title: 캠프가는 영식, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.java
+++ 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.java
@@ -1,52 +1,34 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1590
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 버스의 개수(1<=50)
-		int T = Integer.parseInt(st.nextToken());	// 터미널에 도착하는시간 (1<=백만)
-		
-		HashSet<Integer> set = new HashSet<>();
+		int N	= read();	// 버스의 개수(1<=50)
+		int T	= read();	// 터미널에 도착하는시간 (1<=백만)
+		int min = Integer.MAX_VALUE;
 		for(int i=0; i<N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int S = Integer.parseInt(st.nextToken());	// 버스 시작 시각(1<=백만)
-			int I = Integer.parseInt(st.nextToken());	// 간격(1<=만)
-			int C = Integer.parseInt(st.nextToken());	// 대수(1<=백)
+			int S = read();	// 버스 시작 시각(1<=백만)
+			int I = read();	// 간격(1<=만)
+			int C = read();	// 대수(1<=백)
 			while(C-->0)
 			{
-				set.add(S);
+				if(T <= S)
+				{
+					min = Math.min(min,  S - T);
+					break;
+				}
 				S += I;
 			}
 		}
 		
-		ArrayList<Integer> list = new ArrayList<>(set);
+		if(min == Integer.MAX_VALUE)
+			min = -1;
 		
-		Collections.sort(list);
-		
-		int s = 0;
-		int e = list.size()-1;
-		int res = -1;
-		while(s <= e)
-		{
-			int mid = (s + e) >> 1;
-			if(list.get(mid) < T) {
-				s = mid + 1;
-			}else {
-				e = mid - 1;
-				res = mid;
-			}
-		}
-		if(res != -1)
-			res = list.get(res) - T;
-
-		System.out.print(res);
+		System.out.print(min);
 	}
 }
