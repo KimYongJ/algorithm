37c955ce6e07ec367561aea3a7957475ed50d144
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 19:35:20 +0900

    [Silver I] Title: 맥주 축제, Time: 736 ms, Memory: 28696 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.java
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.java
@@ -1,14 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17503
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Node{
 	int pre, lev;// 선호도(0<=만), 도수(1<=인트최대)
 	Node(int p, int l){this.pre = p; this.lev = l;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static boolean check(Node[] node, long level, int target, int day) {
 		long sum = 0;
 		for(Node n : node)
@@ -23,30 +25,23 @@ class Main{
 		return day == 0 && sum >= target;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 축제 기간(1<=이십만)
-		int M		= Integer.parseInt(st.nextToken());	// 채워야하는 선호도의 합(1<=인트형최대)
-		int K		= Integer.parseInt(st.nextToken());	// 마실 수 있는 맥주 종류수 (N<=이십만)
+		int N		= read();	// 축제 기간(1<=이십만)
+		int M		= read();	// 채워야하는 선호도의 합(1<=인트형최대)
+		int K		= read();	// 마실 수 있는 맥주 종류수 (N<=이십만)
 		Node node[] = new Node[K];
 
 		for(int i=0; i<K; i++)
-		{
-			st		= new StringTokenizer(br.readLine());
-			int p	= Integer.parseInt(st.nextToken());	// 선호도
-			int l	= Integer.parseInt(st.nextToken());	// 도수
-			node[i] = new Node(p, l);
-		}
+			node[i] = new Node(read(), read()); // 선호도, 도수
 		
 		Arrays.sort(node, (a,b)-> b.pre - a.pre);		// 선호도 기준 내림차순
 		
-		long s = 0;
-		long e = Integer.MAX_VALUE;
-		long res = -1;
+		long s		= 0;
+		long e		= Integer.MAX_VALUE;
+		long res 	= -1;
 		
 		while(s <= e)
 		{
-			long mid = (s + e) >> 1;
+			long mid = (int)((s + e) >> 1);
 			if(check(node, mid, M, N))
 			{
 				res = mid;
