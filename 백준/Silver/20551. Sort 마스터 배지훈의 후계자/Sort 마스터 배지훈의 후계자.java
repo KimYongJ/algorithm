5bedec9a8118774041bc261dce99819c334aed1c
YongJu Kim <kyjdummy@gmail.com>
2024-09-16 12:29:11 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 680 ms, Memory: 33796 KB -BaekjoonHub

--- 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
@@ -1,29 +1,33 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/20551
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());	// 배열 A의 원소 개수 (1<=이십만)
-		int M = Integer.parseInt(st.nextToken());	// 질문의 개수 (1<=이십만)
-		int arr[] = new int[N];						// 원소를 담을 배열(-십억<=+십억)
+		int N		= read();		// 배열 A의 원소 개수(1<=이십만)
+		int M		= read();		// 질문의 개수(1<=이십만)
+		int arr[]	= new int[N];	// 원소를 담을 배열(-십억<=+십억)
 		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
 		while(M-->0)
 		{
-			int targetNumber = Integer.parseInt(br.readLine());
-			int s = 0;
-			int e = N-1;
+			int targetNumber = read();
+			int s	= 0;
+			int e	= N-1;
 			int idx = -1;
+			
 			while(s <= e)
 			{
 				int mid = (s + e) >> 1;
@@ -34,7 +38,6 @@ class Main{
 						idx = mid;
 				}else
 					s = mid + 1;
-
 			}
 			sb.append(idx)
 				.append('\n');
