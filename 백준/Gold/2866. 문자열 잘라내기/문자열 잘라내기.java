39c32255a603b622e4f7383c57a0cb53b1a125b5
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 17:24:03 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 232 ms, Memory: 28912 KB -BaekjoonHub

--- 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
@@ -1,21 +1,25 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2866
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br 	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		int Y				= Integer.parseInt(st.nextToken());	// 2<=천
-		int X				= Integer.parseInt(st.nextToken());	// 2<=천
+		int Y				= read();	// 2<=천
+		int X				= read();	// 2<=천
 		char base[][]		= new char[Y][X];
 		StringBuilder arr[]	= new StringBuilder[X];
 		HashSet<String> set = new HashSet<>();
 		
 		for(int y=0; y<Y; y++)
-			base[y] = br.readLine().toCharArray();
+		{
+			for(int x=0; x<X; x++)
+				base[y][x] = (char)System.in.read();
+			System.in.read();
+		}
 		
 		for(int x=0; x<X; x++)
 		{
