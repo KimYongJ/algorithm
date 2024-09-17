c43b7010be3d551f1ce22ab0fd9cf5a2f60ab2dc
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 14:03:53 +0900

    [Gold V] Title: 용액, Time: 152 ms, Memory: 15760 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,27 +1,31 @@
 //https://github.com/kimyongj/algorithm
-//https://www.acmicpc.net/problem/2470
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+//https://www.acmicpc.net/problem/2467
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine()); 	// 전체 용액수 N(2<=십만)
-		int arr[]	= new int[N];						// 용액의 특성값(-십억<=십억)
+		int N		= read();
+		int arr[]	= new int[N];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
-		int res1	= 0;
-		int res2	= 0;
 		int s		= 0;
 		int e		= N-1;
 		int diff	= Integer.MAX_VALUE;
-		while(s < e)
+		int res1	= 0;
+		int res2	= 0;
+		
+		while(s<e)
 		{
 			int sum = arr[s] + arr[e];
 			if(Math.abs(sum) < diff)
@@ -30,11 +34,11 @@ class Main{
 				res1 = arr[s];
 				res2 = arr[e];
 			}
-			if(sum < 0) s++;
-            else e--;
+			if(sum < 0)
+				s++;
+			else
+				e--;
 		}
-
-
-		System.out.print( new StringBuilder().append(res1).append(' ').append(res2).toString() );
+		System.out.print( new StringBuilder().append(res1).append(' ').append(res2) );
 	}
 }
