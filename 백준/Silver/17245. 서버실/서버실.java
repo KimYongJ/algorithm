e704b06e01fccf0a7c5c444ace3fc7e4ec211c0c
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 12:19:27 +0900

    [Silver II] Title: 서버실, Time: 388 ms, Memory: 44328 KB -BaekjoonHub

--- 백준/Silver/17245. 서버실/서버실.java
+++ 백준/Silver/17245. 서버실/서버실.java
@@ -1,13 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17245
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int s		= 1;
 		int e		= 0;
 		int res		= 0;
@@ -15,11 +16,9 @@ class Main{
 		
 		ArrayList<Integer> list = new ArrayList<>();
 		for(int i=0; i<N; i++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0; j<N; j++)
 			{
-				int n = Integer.parseInt(st.nextToken());
+				int n = read();
 				if(n > 0)
 				{
 					e = Math.max(e, n);
@@ -27,7 +26,7 @@ class Main{
 					total += n;
 				}
 			}
-		}
+
 		
 		total = (long)Math.ceil(total / 2.0);
 		
@@ -35,6 +34,7 @@ class Main{
 		{
 			int mid		= (s + e) >> 1;
 			long sum	= 0;
+			
 			for(int l : list)
 				sum += Math.min(mid, l);
 			
