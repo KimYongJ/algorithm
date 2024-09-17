c35440a2df57eda8adf996017fdf7e4b282b72d0
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 15:04:37 +0900

    [Gold V] Title: 개똥벌레, Time: 256 ms, Memory: 29076 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,36 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/3020
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N		= Integer.parseInt(st.nextToken());	// 길이 N(2<=이십만) 항상짝수
+		int M		= Integer.parseInt(st.nextToken());	// 높이 H(2<=오십만)
+		int arr[]	= new int[M+1];						// 종유석을 담을 배열
+		
+		for(int i=0; i<N; i++)
+		{
+			if(i % 2 == 0)
+			{
+				arr[0] += 1;
+				arr[Integer.parseInt(br.readLine())] += -1;
+			}else
+				arr[M - Integer.parseInt(br.readLine())] += 1;
+		}
+		int min = arr[0];
+		for(int i=1; i<M; i++)
+		{
+			arr[i] += arr[i-1];
+			min = Math.min(min, arr[i]);
+		}
+		int cnt = 0;
+		for(int i=0; i<M; i++)
+			if(arr[i] == min)
+				cnt++;
+		
+		System.out.print(new StringBuilder().append(min).append(' ').append(cnt));
+	}
+}
