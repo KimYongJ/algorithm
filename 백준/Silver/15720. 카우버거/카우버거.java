77eebb50b3e8cdbc5a184ca27891160c89c8458c
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 21:23:30 +0900

    [Silver V] Title: 카우버거, Time: 112 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/15720. 카우버거/카우버거.java
+++ 백준/Silver/15720. 카우버거/카우버거.java
@@ -1,50 +1,45 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/15720
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 class Main{
-	static BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	public static int poll(PriorityQueue<Integer> pq) {
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	public static int poll(PriorityQueue<Integer> pq)
+	{
 		int sum = 0;
-		while(!pq.isEmpty()) sum += pq.poll();
+		while(!pq.isEmpty())
+			sum += pq.poll();
 		return sum;
 	}
 	public static int input(PriorityQueue<Integer> pq, int len)throws Exception{
-		st = new StringTokenizer(br.readLine());
-		int sum = 0;
-		for(int i=0; i<len; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			sum += n;
+		int sum = 0, n;
+		for(int i=0; i<len; i++)
+		{
+			sum += n = read();
 			pq.add(n);
 		}
 		return sum;
 	}
 	public static void main(String[] args)throws Exception{
-		br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		int B = Integer.parseInt(st.nextToken());	// 버거 개수(1<=1000)
-		int C = Integer.parseInt(st.nextToken());	// 사이드 개수(1<=1000)
-		int D = Integer.parseInt(st.nextToken());	// 음료 개수(1<=1000)
-		int sum1 = 0;
-		int sum2 = 0;
 		PriorityQueue<Integer> bq = new PriorityQueue<Integer>((a,b)->b-a);
 		PriorityQueue<Integer> cq = new PriorityQueue<Integer>((a,b)->b-a);
 		PriorityQueue<Integer> dq = new PriorityQueue<Integer>((a,b)->b-a);
+		
+		int B		= read();	// 버거 개수(1<=1000)
+		int C		= read();	// 사이드 개수(1<=1000)
+		int D		= read();	// 음료 개수(1<=1000)
+		int sum1	= 0;
+		int sum2	= 0;
 		
 		sum1 += input(bq, B);
 		sum1 += input(cq, C);
 		sum1 += input(dq, D);
 
 		while(!bq.isEmpty() && !cq.isEmpty() && !dq.isEmpty())
-		{
-			int total = bq.poll() + cq.poll() + dq.poll();
-			sum2 += total - (total / 10);
-		}
+			sum2 += ((bq.poll() + cq.poll() + dq.poll()) * 9) / 10;
 		
 		sum2 += poll(bq) + poll(cq) + poll(dq);
 		
