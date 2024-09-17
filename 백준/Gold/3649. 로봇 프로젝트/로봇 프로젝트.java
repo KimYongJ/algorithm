53d0be6a6d82a94ac685fb2eea0c8b31fed46b01
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 15:56:46 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 1168 ms, Memory: 56640 KB -BaekjoonHub

--- 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
@@ -58,12 +58,15 @@ class Reader {
     int nextInt() throws Exception {
         int n = 0;
         byte c;
-        boolean isMinus = false;
-        while ((c = read()) <= 32) { if (size < 0) return -1; }
-        if (c == 45) { c = read(); isMinus = true; }
+        while ((c = read()) <= 32) 
+        { 
+        	if (size < 0)
+        		return -1; 
+        }
+        if (c == 45) { c = read(); }
         do n = (n << 3) + (n << 1) + (c & 15);
         while (isNumber(c = read()));
-        return isMinus ? ~n + 1 : n;
+        return n;
     }
     boolean isNumber(byte c) {
         return 47 < c && c < 58;
