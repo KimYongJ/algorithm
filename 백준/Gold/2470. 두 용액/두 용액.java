9105c142b10725c16fe6ee7ce85e1998c3363fc9
YongJu Kim <kyjdummy@gmail.com>
2024-09-17 13:47:12 +0900

    [Gold V] Title: 두 용액, Time: 304 ms, Memory: 28176 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,40 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/2470
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N		= Integer.parseInt(br.readLine()); 	// 전체 용액수 N(2<=십만)
+		int arr[]	= new int[N];						// 용액의 특성값(-십억<=십억)
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		int res1	= 0;
+		int res2	= 0;
+		int s		= 0;
+		int e		= N-1;
+		int diff	= Integer.MAX_VALUE;
+		while(s < e)
+		{
+			int abs = arr[s] + arr[e];
+			if(Math.abs(abs) < diff)
+			{
+				diff = Math.abs(abs);
+				res1 = arr[s];
+				res2 = arr[e];
+			}
+			if(abs < 0)
+				s++;
+			else e--;
+			
+		}
+		System.out.print( new StringBuilder().append(res1).append(' ').append(res2).toString() );
+	}
+}
