3a03eba77c3862993866f7eaa0cef759f03d9c35
YongJu Kim <kyjdummy@gmail.com>
2024-09-18 16:50:55 +0900

    [Gold IV] Title: 세 수의 합, Time: 288 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -3,13 +3,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 class Main
 {
 	public static void main(String[] args)throws Exception
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		HashSet<Integer> set = new HashSet<>();
 		int N		= Integer.parseInt(br.readLine());	// 5<=1000
 		int arr[]	= new int[N];						// 1<=이억
 		
@@ -17,21 +15,29 @@ class Main
 			arr[i] = Integer.parseInt(br.readLine());
 		
 		Arrays.sort(arr);
-		// 두수의 합 미리 저장
-		for(int i=0; i<N; i++)
-			for(int j=0; j<N; j++)
-				set.add(arr[i] + arr[j]);
 		
-		// 가장 큰수부터 확인하기
 		for(int i=N-1; i>=0; i--)
-		{
-			for(int j=0; j<N; j++) {
-				if(set.contains(arr[i] - arr[j])) {
-					System.out.print(arr[i]);
-					return;
+			for(int j=i; j>=0; j--)
+			{
+				int target = arr[i] - arr[j]; // 두 수를 합친 숫자
+				if(target <= 0)
+					continue;
+				
+				int s = 0;
+				int e = j;
+				while(s <= e)
+				{
+					int sum = arr[s] + arr[e];
+					if(sum == target) {
+						System.out.print(arr[i]);
+						return;
+					}
+					if(sum < target)
+						s++;
+					else e--;
 				}
+				
 			}
-		}
 		
 	}
 }
