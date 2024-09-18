59ac3e4964ebcf69476e4f09493d30094e1b5684
YongJu Kim <kyjdummy@gmail.com>
2024-09-18 14:51:17 +0900

    [Gold IV] Title: 합이 0, Time: 364 ms, Memory: 18032 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -1,20 +1,21 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3151
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
+	public static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		boolean isNegative = n == 13;
+		if (isNegative) n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+		if (c == 13) System.in.read();
+		return isNegative ? ~n + 1 : n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	// 학생수 (1<=만)
-		int arr[]	= new int[N];						// 학생들의 코딩 실력(-만<=만)
+		int N		= read();		// 학생수 (1<=만)
+		int arr[]	= new int[N];	// 학생들의 코딩 실력(-만<=만)
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 
@@ -26,38 +27,41 @@ class Main{
 			
 			int left	= i + 1;
 			int right	= N - 1;
+			
 			while(left < right)
 			{
 				int sum = arr[i] + arr[left] + arr[right];
-				if(0 < sum)
-					right--;
-				else if(sum < 0)
-					left++;
-				else	// 원하는 값을 찾았다면 중복된 값 체크
+				
+				if(sum == 0)// 원하는 값을 찾았다면 중복된 값 체크
 				{
 					if(arr[left] == arr[right])
 					{
-						int len = right - left + 1;
-						int add = len * (len - 1) / 2;
-						cnt += (long)add;
+						long len = right - left + 1;// 양쪽이 같으면 조합을 구함
+						cnt += len * (len - 1) / 2;
 						break;
 					}
+					
 					int leftCount = 1;
-					while(left + 1< right && arr[left + 1] == arr[left]) {
+					while(left + 1< right && arr[left + 1] == arr[left])
+					{
 						leftCount++;
 						left++;
 					}
 					
 					int rightCount = 1;
-					while(left < right - 1 && arr[right - 1] == arr[right]) {
+					while(left < right - 1 && arr[right - 1] == arr[right])
+					{
 						rightCount++;
 						right--;
 					}
 					
 					cnt += (long)leftCount * rightCount;
-					
-					left++;
 				}
+				
+				if(0 < sum)
+					right--;
+				else
+					left++;
 			}
 			
 		}
