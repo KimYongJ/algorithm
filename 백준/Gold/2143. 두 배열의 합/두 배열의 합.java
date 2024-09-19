e3333dd7336bfbb5d0708ba945120cf85d32ab20
YongJu Kim <kyjdummy@gmail.com>
2024-09-19 15:58:14 +0900

    [Gold III] Title: 두 배열의 합, Time: 328 ms, Memory: 67908 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -11,33 +11,29 @@ class Main{
         return m ? ~n + 1 : n;
     }
 	public static void main(String[] args)throws Exception{
+		HashMap<Long, Integer> hm = new HashMap<>();
 		long cnt		= 0;
+		long sum		= 0;
 		long T			= read();		// -십억<=십억
+		
 		int N			= read();		// 1<=천
-		long arr1[]		= new long[N];	// -백만<=백만
+		int arr1[]		= new int[N];	// -백만<=백만
 		for(int i=0; i<N; i++)
 			arr1[i] = read();
 		
 		int M			= read();
-		long arr2[]		= new long[M];	// -백만<=백만
+		int arr2[]		= new int[M];	// -백만<=백만
 		for(int i=0; i<M; i++)
 			arr2[i] = read();
 		
-		HashMap<Long, Long> hm = new HashMap<>();
-		for(int i=0; i<N; i++)
-		{
-			long sum = 0;
+		for(int i=0; i<N; i++, sum = 0)
 			for(int j=i; j<N; j++)
-				hm.put(sum += arr1[j], hm.getOrDefault(sum, 0L) + 1);
-		}
+				hm.put(sum += arr1[j], hm.getOrDefault(sum, 0) + 1);
 		
-		for(int i=0; i<M; i++)
-		{
-			long sum = 0;
+		for(int i=0; i<M; i++, sum = 0)
 			for(int j=i; j<M; j++)
-				cnt += hm.getOrDefault(T - (sum += arr2[j]), 0L);
-		}
-		
+				cnt += hm.getOrDefault(T - (sum += arr2[j]), 0);
+
 		System.out.print(cnt);
 	}
 }
