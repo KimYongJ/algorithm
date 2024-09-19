1a74e0b7a72b0bf2ee905b3c4160bde875bd6169
YongJu Kim <kyjdummy@gmail.com>
2024-09-19 10:44:06 +0900

    [Gold III] Title: Ax+Bsin(x)=C ②, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/14786. Ax＋Bsin（x）＝C ②/Ax＋Bsin（x）＝C ②.java
+++ 백준/Gold/14786. Ax＋Bsin（x）＝C ②/Ax＋Bsin（x）＝C ②.java
@@ -1,20 +1,22 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14786
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());	// 0<=십만
-		int B = Integer.parseInt(st.nextToken());	// 0<=십만
-		int C = Integer.parseInt(st.nextToken());	// 0<=십만
-		// Ax + Bsin(x) = C를 만족하는 X찾기
+		int A = read();	// 0<=십만
+		int B = read();	// 0<=십만
+		int C = read();	// 0<=십만
+		
 		double diff = 0.0000000009;
-		double s = 0;
-		double e = 100_001;
-		double x = 0;
+		double s	= 0;
+		double e	= 100_001;
+		double x	= 0;
+		
+		// Ax + Bsin(x) = C를 만족하는 X찾기
 		while(e - s > diff)
 		{
 			double mid = (s + e) / 2;
