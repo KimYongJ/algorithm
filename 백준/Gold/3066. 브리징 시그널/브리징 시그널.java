b848689aabcc9d9c4641c4e1a57ef9eb0a360fea
YongJu Kim <kyjdummy@gmail.com>
2024-09-20 22:04:50 +0900

    [Gold II] Title: 브리징 시그널, Time: 240 ms, Memory: 28592 KB -BaekjoonHub

--- 백준/Gold/3066. 브리징 시그널/브리징 시그널.java
+++ 백준/Gold/3066. 브리징 시그널/브리징 시그널.java
@@ -1,11 +1,13 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3066
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int getIdx(ArrayList<Integer> list, int target) {
 		int size = list.size();
 		if(size == 0)
@@ -19,30 +21,29 @@ class Main{
 		while(s <= e)
 		{
 			int mid = (s + e) >> 1;
-			if(target == list.get(mid)) {
+			if(target == list.get(mid))
 				return mid;
-			}
-			if(list.get(mid) < target) {
+		    else if(list.get(mid) < target)
+            {
 				s = mid + 1;
 				idx = mid + 1;
 			}
-			else if(target <= list.get(mid))
+			else
 				e = mid - 1;
 		}
 		return idx;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			int N		= Integer.parseInt(br.readLine());	// 포트의개수(1<=사만)
+			int N		= read();	// 포트의개수(1<=사만)
 			int arr[][] = new int[N][2];
 			for(int i=0; i<N; i++)
 			{
 				arr[i][0] = i + 1;
-				arr[i][1] = Integer.parseInt(br.readLine());
+				arr[i][1] = read();
 			}
 			
 			Arrays.sort(arr,(a,b)->a[0] - b[0]);
