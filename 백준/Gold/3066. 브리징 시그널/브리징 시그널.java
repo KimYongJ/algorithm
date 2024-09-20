33f7d2390972702060823ad21c5e3557b56921e8
YongJu Kim <kyjdummy@gmail.com>
2024-09-20 22:20:04 +0900

    [Gold II] Title: 브리징 시그널, Time: 164 ms, Memory: 16612 KB -BaekjoonHub

--- 백준/Gold/3066. 브리징 시그널/브리징 시그널.java
+++ 백준/Gold/3066. 브리징 시그널/브리징 시그널.java
@@ -1,29 +1,23 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3066
-import java.util.ArrayList;
-import java.util.Arrays;
 class Main{
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
-	public static int getIdx(ArrayList<Integer> list, int target) {
-		int size = list.size();
-		if(size == 0)
-			return 0;
-		if(list.get(size-1) < target)
-			return size;
+	public static int getIdx(int LIS[], int target, int len)
+	{
 		
 		int s	= 0;
-		int e	= list.size() - 1;
+		int e	= len - 1;
 		int idx	= 0;
 		while(s <= e)
 		{
 			int mid = (s + e) >> 1;
-			if(target == list.get(mid))
+			if(target == LIS[mid])
 				return mid;
-		    else if(list.get(mid) < target)
+		    else if(LIS[mid] < target)
             {
 				s = mid + 1;
 				idx = mid + 1;
@@ -38,31 +32,29 @@ class Main{
 		int T = read();
 		while(T-->0)
 		{
-			int N		= read();	// 포트의개수(1<=사만)
-			int arr[][] = new int[N][2];
+			int N		= read();		// 포트의개수(1<=사만)
+			int arr[]	= new int[N];
+			
 			for(int i=0; i<N; i++)
-			{
-				arr[i][0] = i + 1;
-				arr[i][1] = read();
-			}
+				arr[i] = read();
+
+			int idx;
+			int len		= 1;
+			int LIS[]	= new int[N];		// 최장 증가하는 부분 수열을 담을 배열
+			LIS[0]		= arr[0];			// 초기값을 arr[0]번째 값으로 넣는다.
 			
-			Arrays.sort(arr,(a,b)->a[0] - b[0]);
-			
-			// list의 y값 중, 가장 긴 증가하는 부분수열의 개수를 구하면된다.
-			ArrayList<Integer> list = new ArrayList<>();
-			for(int a[] : arr)
+			for(int i=1; i<N; i++)
 			{
-				int now = a[1];
-				
-				int idx = getIdx(list, now); // list에서 now가 들어갈 위치를 가져온다.
-				
-				if(idx < list.size())
-					list.set(idx, now);
+				if(arr[i] > LIS[len-1])		// 찾을값인 arr[i]가 LIS의 마지막 값보다 크다면 바로 LIS에 넣음 
+					LIS[len++] = arr[i];
 				else
-					list.add(now);
+				{
+					idx = getIdx(LIS, arr[i], len);	// LIS에서 arr[i]값 보다 작은 값중 가장 큰 IDX의 +1을 구해온다.
+
+					LIS[idx] = arr[i];		// 값 덮어씌우기
+				}
 			}
-			
-			sb.append(list.size()).append('\n');
+			sb.append(len).append('\n');
 		}
 		System.out.print(sb.toString());
 	}
