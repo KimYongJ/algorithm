945f372d14e27725c8cf8f6458bb20b3ef7f587d
YongJu Kim <kyjdummy@gmail.com>
2024-09-21 22:36:41 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 468 ms, Memory: 96076 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,51 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/12015
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static int getIdx(int[] LIS, int target, int e)
+	{
+		int s	= 0;
+		int res = 1;
+		while(s <= e)
+		{
+			int mid = (s + e) >> 1;
+			if(LIS[mid] == target)
+				return mid;
+			if(target <= LIS[mid])
+			{
+				res = mid;
+				e = mid - 1;
+			}
+			else
+				s = mid + 1;
+		}
+		return res;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N		= Integer.parseInt(br.readLine());	// 1<=백만
+		int arr[]	= new int[N];						// 1<=백만
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		int len		= 1;
+		int LIS[]	= new int[N];
+		LIS[0]		= arr[0];			// 첫번 째 값을 넣는다.
+		for(int i=1; i<N; i++)
+		{
+			if(LIS[len-1] < arr[i])
+				LIS[len++] = arr[i];
+			else
+			{
+				int idx = getIdx(LIS, arr[i], len - 1);
+				if(LIS[idx] > arr[i])
+					LIS[idx] = arr[i];
+			}
+		}
+		System.out.print(len);
+	}
+}
