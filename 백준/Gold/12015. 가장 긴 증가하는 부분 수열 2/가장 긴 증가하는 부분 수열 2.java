0b524c999b68d9ce1e09d2b0797b5021082b62e5
YongJu Kim <kyjdummy@gmail.com>
2024-09-21 22:40:32 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 284 ms, Memory: 22276 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,9 +1,11 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12015
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int getIdx(int[] LIS, int target, int e)
 	{
 		int s	= 0;
@@ -11,8 +13,6 @@ class Main{
 		while(s <= e)
 		{
 			int mid = (s + e) >> 1;
-			if(LIS[mid] == target)
-				return mid;
 			if(target <= LIS[mid])
 			{
 				res = mid;
@@ -24,17 +24,15 @@ class Main{
 		return res;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	// 1<=백만
-		int arr[]	= new int[N];						// 1<=백만
+		int N		= read();		// 1<=백만
+		int arr[]	= new int[N];	// 1<=백만
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		int len		= 1;
 		int LIS[]	= new int[N];
-		LIS[0]		= arr[0];			// 첫번 째 값을 넣는다.
+		LIS[0]		= arr[0];		// 첫번 째 값을 넣는다.
 		for(int i=1; i<N; i++)
 		{
 			if(LIS[len-1] < arr[i])
