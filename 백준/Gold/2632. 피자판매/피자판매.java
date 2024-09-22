fb1e0459cb503512fa34c1859b2dcd42cec58174
YongJu Kim <kyjdummy@gmail.com>
2024-09-22 21:19:17 +0900

    [Gold II] Title: 피자판매, Time: 152 ms, Memory: 30564 KB -BaekjoonHub

---
+++ 백준/Gold/2632. 피자판매/피자판매.java
@@ -0,0 +1,52 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/2632
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int target		= Integer.parseInt(br.readLine());// 목표 크기 1<=이백만
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m			= Integer.parseInt(st.nextToken());// 3<=천
+		int n			= Integer.parseInt(st.nextToken());// 3<=천
+		int arr1[]		= new int[m << 1];
+		int arr2[]		= new int[n << 1];
+		int prevSum1[]	= new int[2_000_001];// 각 배열의 누적합을 카운팅 해줄 배열 ex) 누적합 결과 21 => prevSum1[21]++;
+		int prevSum2[]	= new int[2_000_001];// 각 배열의 누적합을 카운팅 해줄 배열
+		long cnt		= 0;
+		int sum1		= 0;
+		int sum2		= 0;
+		
+		for(int i=0; i<m; i++)
+			sum1 += arr1[i] = arr1[i + m] = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<n; i++)
+			sum2 += arr2[i] = arr2[i + n] = Integer.parseInt(br.readLine());
+
+
+		prevSum1[sum1]	= 1;// 피자 A의 모든 조각을 다 사용했을 때의 합을 1로 설정(추후 곱하기를 하기 때문에 1처리)
+		prevSum1[0]		= 1;// 부분합이 0인 경우(추후 곱하기를 하기 때문에 1처리)
+		prevSum2[sum2]	= 1;// 피자 B의 모든 조각을 다 사용했을 때의 합을 1로 설정(추후 곱하기를 하기 때문에 1처리)
+		prevSum2[0]		= 1;// 부분합이 0인 경우(추후 곱하기를 하기 때문에 1처리)
+		
+		for(int i=0; i<m; i++)	// 배열 arr1의 누적합
+		{
+			sum1 = 0;
+			for(int j=i; j<i + m - 1; j++)
+				prevSum1[sum1 += arr1[j]] ++;
+		}
+		for(int i=0; i<n; i++)	// 배열 arr2의 누적합
+		{
+			sum2 = 0;
+			for(int j=i; j<i + n - 1; j++)
+				prevSum2[sum2 += arr2[j]] ++;
+		}
+		
+		for(int i=0; i<=target; i++)
+			cnt += prevSum1[i] * prevSum2[target - i];
+		
+		System.out.print(cnt);
+	}
+}
