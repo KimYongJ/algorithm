0f3bab29b8fd1b76aa1418551be7172757206b84
YongJu Kim <kyjdummy@gmail.com>
2024-09-22 17:06:18 +0900

    [Gold II] Title: 반도체 설계, Time: 132 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -1,9 +1,11 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2352
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int getIdx(int LIS[], int target, int e) {
 		int idx	= 0;
 		int s	= 0;
@@ -23,28 +25,22 @@ class Main{
 		return idx;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read(); // (1 ≤ n ≤ 40,000)
 		int arr[]	= new int[N];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		int len		= 1;
 		int LIS[]	= new int[N];
 		LIS[0] = arr[0];
 		
 		for(int i=1; i<N; i++)
-		{
 			if(LIS[len-1] < arr[i])
 				LIS[len++] = arr[i];
 			else
-			{
-				int idx = getIdx(LIS, arr[i], len - 1);
-				LIS[idx] = arr[i];
-			}
-		}
+				LIS[getIdx(LIS, arr[i], len - 1)] = arr[i];
+
 		System.out.print(len);
 	}
 }
