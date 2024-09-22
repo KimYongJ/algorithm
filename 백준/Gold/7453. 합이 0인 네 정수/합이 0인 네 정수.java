6bf82f78891d47214c260b3dec403e62c3504153
YongJu Kim <kyjdummy@gmail.com>
2024-09-22 16:38:53 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 2356 ms, Memory: 273328 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -1,67 +1,74 @@
-//https://github.com/kimyongj/algorithm
-//https://www.acmicpc.net/problem/7453
-import java.util.Arrays;
-class Main{
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        boolean m = n == 13;
-        if (m)n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48) {
-        n = (n << 3) + (n << 1) + (c & 15);}
-        return m ? ~n + 1 : n;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static long cnt;
+    static int[] A, B, C, D, AB, CD;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        A = new int[N];
+        B = new int[N];
+        C = new int[N];
+        D = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st.nextToken());
+            C[i] = Integer.parseInt(st.nextToken());
+            D[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        Arrays.sort(C);
+        Arrays.sort(D);
+
+        AB = new int[N * N];
+        CD = new int[N * N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                AB[i * N + j] = A[i] + B[j];
+                CD[i * N + j] = C[i] + D[j];
+            }
+        }
+        
+        Arrays.sort(AB);
+        Arrays.sort(CD);
+
+        int start = 0;
+        int end = N * N - 1;
+
+        while (start < N * N && end >= 0) {
+            int sum = AB[start] + CD[end];
+
+            if (sum == 0) {
+                long cnt1 = 1;
+                long cnt2 = 1;
+
+                while (++start < N*N && AB[start] == AB[start-1]) {
+                    cnt1++;
+                }
+
+                while(--end >= 0 && CD[end] == CD[end+1]) {
+                    cnt2++;
+                }
+
+                cnt += cnt1 * cnt2;
+            } else if (sum < 0) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        System.out.println(cnt);
     }
-	public static void main(String[] args)throws Exception{
-		int N		= read();	// 1 ≤ n ≤ 4000
-		int LEN		= N * N;
-		int A[]		= new int[N];
-		int B[]		= new int[N];
-		int C[]		= new int[N];
-		int D[]		= new int[N];
-		int AB[]	= new int[LEN];
-		int CD[]	= new int[LEN];
-		
-		for(int i=0; i<N; i++)
-		{
-			A[i] = read();
-			B[i] = read();
-			C[i] = read();
-			D[i] = read();
-		}
-		
-		for(int i=0, len = 0; i<N; i++) 
-			for(int j=0; j<N; j++, len++)
-			{
-				AB[len] = A[i] + B[j];
-				CD[len] = C[i] + D[j];
-			}
-		
-		Arrays.sort(AB);
-		Arrays.sort(CD);
-		
-		long cnt	= 0;
-		int left	= 0;
-		int right	= LEN-1;
-		
-		while(0<=right && left < LEN)
-		{
-			int sum = AB[left] + CD[right];
-			if(0 < sum)
-				right--;
-			else if(sum < 0)
-				left++;
-			else
-			{
-				long leftCnt	= 1;
-				long rightCnt	= 1;
-				while(++left< LEN && AB[left] == AB[left - 1])
-					leftCnt++;
-				while(0 <= --right && CD[right] == CD[right + 1])
-					rightCnt++;
-				
-				cnt += leftCnt * rightCnt;
-			}
-		}
-		
-		System.out.print(cnt);
-	}
 }
