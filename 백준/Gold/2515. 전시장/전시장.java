284895df6df6ca5b58e11a8deb302ecd8f789853
YongJu Kim <kyjdummy@gmail.com>
2024-09-23 17:56:22 +0900

    [Gold II] Title: 전시장, Time: 1256 ms, Memory: 100340 KB -BaekjoonHub

---
+++ 백준/Gold/2515. 전시장/전시장.java
@@ -0,0 +1,59 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/2515
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+class Node{
+	int H, C;
+	Node(int H, int C){this.H=H; this.C=C;}
+}
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());	// 그림 개수(1<=삼십만)
+		int S = Integer.parseInt(st.nextToken());	// 판매 가능 그림길이(1<=이천만 & S<=H)
+		Node list[] = new Node[N];
+		for(int i=0; i<N; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			int h = Integer.parseInt(st.nextToken());	// 높이(1<=이천만)
+			int c = Integer.parseInt(st.nextToken());	// 가격(1<=천)
+			list[i] = new Node(h,c);
+		}
+		
+		Arrays.sort(list,(a,b)-> a.H != b.H ? a.H-b.H : a.C - b.C);// 높이 기준 오름차순 정렬,가격 기준 오름차순 정렬 
+		
+		int dp[][] = new int[N][2];	// 각 그림을 설치했을 때와, 설치하지 않았을 때 최댓값을 담는다.
+		dp[0][0] = 0;				// 0번째 그림을 설치하지 않았을 때 최댓값
+		dp[0][1] = list[0].C;		// 0번째 그림을 설치했을 때 최댓값 
+		
+		for(int i=1; i<N; i++)
+		{
+			// 그림i를 설치하지 않았을 때 최대값
+			dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);	
+			// 그림i를 설치했을 때 최대값
+			dp[i][1] = list[i].C; 
+			
+			int targetHeight = list[i].H - S; // 해당 높이를 갖는 값이 가장큰 것을 찾음
+			int s	= 0;
+			int e	= i-1;
+			int idx = -1;
+			while(s <= e)
+			{
+				int mid = (s + e) >> 1;
+				if(list[mid].H <= targetHeight) {
+					s = mid + 1;
+					idx = mid;
+				}
+				else
+					e = mid - 1;
+			}
+			if(idx != -1)	// targetHeight와 가장 가까운 인덱스를 찾으면 더해줌
+				dp[i][1] += Math.max(dp[idx][0], dp[idx][1]);
+		}
+		
+		System.out.print(Math.max(dp[N-1][0],dp[N-1][1]));
+	}
+}
