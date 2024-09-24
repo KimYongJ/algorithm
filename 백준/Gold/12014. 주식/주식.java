595359c694e555ae1bc08438fd51b4662ca561ed
YongJu Kim <kyjdummy@gmail.com>
2024-09-24 16:31:07 +0900

    [Gold II] Title: 주식, Time: 164 ms, Memory: 17112 KB -BaekjoonHub

--- 백준/Gold/12014. 주식/주식.java
+++ 백준/Gold/12014. 주식/주식.java
@@ -13,10 +13,11 @@ class Main{
 			int mid = (s + e) >> 1;
 			if(LIS[mid] == target)
 				return mid;
-			if(LIS[mid] < target) {
+			if(LIS[mid] < target)
 				s = mid + 1;
-			}else {
-				e = mid - 1;
+			else
+			{
+				e	= mid - 1;
 				idx = mid;
 			}
 		}
@@ -37,13 +38,13 @@ class Main{
 				arr[i] = read();
 			
 			LIS[0] = arr[0];
+			
 			for(int i=1; i<N; i++)
-			{
 				if(LIS[len - 1] < arr[i])
 					LIS[len++] = arr[i];
 				else
 					LIS[getIdx(LIS, arr[i], len - 1, 0)] = arr[i];
-			}
+			
 			
 			sb.append("Case #").append(t).append('\n')
 				.append(len >= K ? 1 : 0).append('\n');
