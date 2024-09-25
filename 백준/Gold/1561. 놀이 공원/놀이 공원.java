eca05b0ea4c5a2c6755d817716eb13237ab10bc9
YongJu Kim <kyjdummy@gmail.com>
2024-09-25 18:02:10 +0900

    [Gold I] Title: 놀이 공원, Time: 128 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.java
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.java
@@ -1,19 +1,18 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1561
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 아이들수(1<=20억)
-		int M		= Integer.parseInt(st.nextToken());	// 놀이기구수(1<=만)
-		int arr[]	= new int[M];						// 운행시간(1<=30)
+		int N		= read();				// 아이들수(1<=20억)
+		int M		= read();				// 놀이기구수(1<=만)
+		int arr[]	= new int[M];			// 운행시간(1<=30)
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<M; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 
 		if(N <= M)
 		{
@@ -21,22 +20,22 @@ class Main{
 			return;
 		}
 		
-		long totalTime	= 0;// 모든 아이가 놀이기구를 타는데 걸린 시간
+		long totalTime	= 0;				// 모든 아이가 놀이기구를 타는데 걸린 시간
 		long s			= 0;
-		long e			= 60_000_000_001L;
+		long e			= 60_000_000_000L;
 		
 		while(s <= e) {
-			long mid = (s + e) >> 1;// 모든 아이가 놀이기구를 타는데 걸린 시간
+			long mid = (s + e) >> 1;		// 모든 아이가 놀이기구를 타는데 걸린 시간
 			long sum = M;
 			
 			for(int i=0; i<M; i++)
-				sum += mid / arr[i];// 해당시간까지 놀이기구를 탄 아이들의 수
+				sum += mid / arr[i];		// 해당시간까지 놀이기구를 탄 아이들의 수
 			
-			if(sum < N)				// mid시간으로N명을 만들 수 없을 때 s를 추가해줌
+			if(sum < N)						// mid시간으로N명을 만들 수 없을 때 s를 추가해줌
 			{
 				s = mid + 1;
 			}
-			else					// mid시간으로 N명이 가능할 때 결과를 대입하여 조건을 만족하는 최소 mid를 구함
+			else							// mid시간으로 N명이 가능할 때 결과를 대입하여 조건을 만족하는 최소 mid를 구함
 			{
 				e = mid - 1;
 				totalTime = mid;
