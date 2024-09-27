c771ee3f0151d5f5808559d3c841e3cd7a72f9e0
YongJu Kim <kyjdummy@gmail.com>
2024-09-27 15:44:00 +0900

    [Gold I] Title: 냅색문제, Time: 264 ms, Memory: 21076 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.java
+++ 백준/Gold/1450. 냅색문제/냅색문제.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 class Main{
+	
 	static int N, C, arr[];
-	static ArrayList<Integer> left, right;
+
 	public static void getSum(int s, int e, long sum, ArrayList<Integer> list) {
 		if(s == e)
 		{
@@ -19,73 +20,6 @@ class Main{
 		getSum(s + 1, e, sum, list);
 		if(sum+arr[s] <= C)
 			getSum(s + 1, e, sum+arr[s], list);
-	}
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N	= Integer.parseInt(st.nextToken());	// 물건개수(1<=30)
-		C	= Integer.parseInt(st.nextToken());	// 총무게(0<=10억)
-		arr = new int[N];						// 주어지는 무게들(1<=10억)
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		
-		if(C == 0)
-		{
-			System.out.print(1);
-			return;
-		}
-		
-		left	= new ArrayList<>();
-		right	= new ArrayList<>();
-		
-		getSum(0, N/2, 0, left);	// arr의 0번째 인덱스부터 N/2인덱스까지 부분합을 모두 구해 left 리스트에 담는다.
-		getSum(N/2, N, 0, right);	// arr의 N/2번째 인덱스부터 마지막까지 부분합을 모두 구해 right 리스트에 담는다. 
-		
-		Collections.sort(left);
-		Collections.sort(right);
-		
-		
-		if(left.size() != 0 && right.size() != 0)
-		{
-			long cnt	= 0;
-			int len		= left.size();
-			int s		= 0;
-			while(s < len)
-			{
-				int lvalue = left.get(s);
-				long lcnt = 0;
-				while(s < len && left.get(s) == lvalue)
-				{
-					lcnt++;
-					s++;
-				}
-				int upper = getUpperBound(right, C - lvalue); // C-lvalue 값보다 작거나 같은 가장 큰수
-				if(upper == -1)
-				{
-					cnt++;
-					continue;
-				}
-				int lower = getLowerBound(right, right.get(upper));
-				long rcnt = upper - lower + 1;
-				
-				cnt += lcnt * rcnt;
-			}
-			
-			System.out.print(cnt);
-		}
-		else if(left.size() == 0 && right.size() == 0) {
-			System.out.print(1);
-		}else {
-			ArrayList<Integer> list = left.size() != 0 ? left : right;
-			long rcnt = getUpperBound(list, C);
-			if(rcnt == -1) {
-				System.out.print(1);
-			}else {
-				System.out.print(rcnt + 1);
-			}
-		}
-		
 	}
 	public static int getUpperBound(ArrayList<Integer> list, int target) {
 		int s = 0;
@@ -117,5 +51,58 @@ class Main{
 				s = mid + 1;
 		}
 		return idx;
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N	= Integer.parseInt(st.nextToken());	// 물건개수(1<=30)
+		C	= Integer.parseInt(st.nextToken());	// 총무게(0<=10억)
+		arr = new int[N];						// 주어지는 무게들(1<=10억)
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		if(C == 0)
+		{
+			System.out.print(1);
+			return;
+		}
+		
+		ArrayList<Integer> left	= new ArrayList<>();
+		ArrayList<Integer> right= new ArrayList<>();
+		
+		getSum(0, N/2, 0, left);	// arr의 0번째 인덱스부터 N/2인덱스까지 부분합을 모두 구해 left 리스트에 담는다.
+		getSum(N/2, N, 0, right);	// arr의 N/2번째 인덱스부터 마지막까지 부분합을 모두 구해 right 리스트에 담는다. 
+		
+		Collections.sort(left);
+		Collections.sort(right);
+		
+		long cnt	= 0;
+		int len		= left.size();
+		int s		= 0;
+		while(s < len)
+		{
+			int lvalue	= left.get(s);
+			int lidx	= getUpperBound(left, lvalue);
+			long lcnt	= lidx - s + 1;
+
+			s = lidx + 1;
+			
+			int upper = getUpperBound(right, C - lvalue); // C-lvalue 값보다 작거나 같은 가장 큰수
+			if(upper == -1)
+			{
+				cnt++;
+				continue;
+			}
+			
+			int lower = getLowerBound(right, right.get(upper));
+			
+			long rcnt = upper - lower + 1;
+			
+			cnt += lcnt * rcnt;
+		}
+		
+		System.out.print(cnt);
+	
 	}
 }
