cd91a9c35a6242b6143ff675b1717e046d2ef18b
YongJu Kim <kyjdummy@gmail.com>
2024-09-29 16:45:53 +0900

    [Gold I] Title: 구간 합 구하기, Time: 504 ms, Memory: 102148 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -3,25 +3,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-class SegmentTree{
-	long tree[];	// 각 원소가 담길 일차원 배열(논리상 트리로 이용)
-	int treeSize;	// 트리의 크기
-	public SegmentTree(int arrSize) {
-		// 트리의 높이
-		int h		= (int) Math.ceil(Math.log(arrSize) / Math.log(2));
-		treeSize	= (int)Math.pow(2, h + 1);	// 높이를 이용해 배열의 사이즈를 구한다.
-		tree		= new long[treeSize];		// 배열 초기화(논리상 트리로 이용)
-	}
+class Main{
 	// 파라미터 : 원소배열, 현재노드, 현재구간 배열시작, 현재구간 배열 끝
-	public long init(long[] arr, int node, int start, int end) {
+	public static long init(long tree[], long[] arr, int node, int start, int end) {
 		if(start == end)	// 배열의 시작과 끝이 같다면 leaf노드이므로 원소 배열 값 그대로 담는다.
 			return tree[node] = arr[start];
 		else				// leaf 노드가 아닐 경우 왼쪽, 오른쪽 자식노드의 합을 담는다.
-			return tree[node] = init(arr, node<<1, start, (start + end) >> 1)
-							+	init(arr, (node<<1) + 1, ((start+end) >> 1) + 1, end);
+			return tree[node] = init(tree, arr, node<<1, start, (start + end) >> 1)
+							+	init(tree, arr, (node<<1) + 1, ((start+end) >> 1) + 1, end);
 	}
 	// 파라미터 : 현재노드 , 배열의 시작, 배열의 끝, 변경된 데이터의 인덱스, 원래 데이터 값과 변경 데이터 값의 차이
-	public void update(int node, int start, int end, int idx, long diff) {
+	public static void update(long tree[], int node, int start, int end, int idx, long diff) {
 		if(idx < start || end < idx) // 변경할 idx 값이 범위 밖이면 종료
 			return;
 		// 차를 저장
@@ -29,23 +21,22 @@ class SegmentTree{
 		
 		// 리프노드가 아닐 경우 아래 자식노드들(왼쪽, 오른쪽)도 확인
 		if(start != end) {
-			update(node<<1, start, (start+end) >> 1, idx, diff);
-			update((node<<1)+1, ((start+end)>>1)+1, end, idx, diff);
+			update(tree, node<<1, start, (start+end) >> 1, idx, diff);
+			update(tree, (node<<1)+1, ((start+end)>>1)+1, end, idx, diff);
 		}
 	}
-	// 파라미터 : 현재 노드, 배열의 시작,배열의 끝, 원하는 누적합의 시작, 원하는 누적합의 끝
-	public long sum(int node, int start, int end, int left, int right) {
+	// 파라미터 : 현재 노드, 배열의 시작, 배열의 끝, 원하는 누적합의 시작, 원하는 누적합의 끝
+	public static long sum(long tree[], int node, int start, int end, int left, int right)
+	{
 		if(left> end || right < start) 
 			return 0;
 		// 범위내 완전 포함시 바로 리턴
 		if(left <= start && end <= right)
 			return tree[node];
 		
-		return sum(node<<1, start, (start+end)>>1, left, right)
-				+ sum((node<<1)+1, ((start+end)>>1)+1, end, left, right);
+		return sum(tree, node<<1, start, (start+end)>>1, left, right)
+				+ sum(tree, (node<<1)+1, ((start+end)>>1)+1, end, left, right);
 	}
-}
-class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -58,9 +49,11 @@ class Main{
 		for(int i=1; i<=N; i++)
 			arr[i] = Long.parseLong(br.readLine());
 		
-		SegmentTree sgt = new SegmentTree(N);
+		int h			= (int) Math.ceil(Math.log(N) / Math.log(2));
+		int treeSize	= (int)Math.pow(2, h + 1);	// 높이를 이용해 배열의 사이즈를 구한다.
+		long tree[]		= new long[treeSize];		// 각 원소가 담길 일차원 배열(논리상 트리로 이용)
 		
-		sgt.init(arr, 1, 1, N);
+		init(tree, arr, 1, 1, N);// 파라미터 : 원소배열, 현재노드, 현재구간 배열시작, 현재구간 배열 끝
 		
 		for(int i=0; i<M+K; i++)
 		{
@@ -70,11 +63,11 @@ class Main{
 			long b	= Long.parseLong(st.nextToken());
 			if(cmd == 1)// b번째 수를 c로 바꾸기
 			{
-				sgt.update(1, 1, N, a, b-arr[a]);// 파라미터 : 현재노드 , 배열의 시작, 배열의 끝, 변경된 데이터의 인덱스, 원래 데이터 값과 변경 데이터 값의 차이
+				update(tree, 1, 1, N, a, b-arr[a]);// 파라미터 : 현재노드 , 배열의 시작, 배열의 끝, 변경된 데이터의 인덱스, 원래 데이터 값과 변경 데이터 값의 차이
 				arr[a] = b;
 			}
 			else// b번째 수부터 c번째 수까지 합 구하기
-				sb.append(sgt.sum(1,  1,  N,  a, (int)b)).append('\n');// 파라미터 : 현재 노드, 배열의 시작, 배열의 끝, 원하는 누적합의 시작, 원하는 누적합의 끝
+				sb.append(sum(tree, 1,  1,  N,  a, (int)b)).append('\n');// 파라미터 : 현재 노드, 배열의 시작, 배열의 끝, 원하는 누적합의 시작, 원하는 누적합의 끝
 		}
 		System.out.print(sb);
 	}
