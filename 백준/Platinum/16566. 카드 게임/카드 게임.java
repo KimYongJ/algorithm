3fda112f16ae54513767f2a02e78e5cd69666704
YongJu Kim <kyjdummy@gmail.com>
2024-09-30 22:12:43 +0900

    [Platinum V] Title: 카드 게임, Time: 520 ms, Memory: 70132 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.java
+++ 백준/Platinum/16566. 카드 게임/카드 게임.java
@@ -1,21 +1,23 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16566
-import java.util.Arrays;
 class Main{
 
 	static int N, K, h, size;
 	static int[] arr, tree;
+	
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
 		return n;
 	}
+	
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
 			return tree[treeNode] = arr[s];
 		int mid = (s + e) >> 1;
 		return tree[treeNode] = Math.max(init(treeNode*2, s, mid) ,init(treeNode*2 + 1, mid + 1, e));
 	}
+	
 	public static int update(int treeNode, int s, int e, int originIdx) {
 		if(originIdx < s || e < originIdx) // 목표 인덱스가 범위 밖이면 현재 트리노드 값을 리턴하여 max연산을 하도록 한다.
 			return tree[treeNode];
@@ -32,6 +34,7 @@ class Main{
 
 		return tree[treeNode] = Math.max(update(treeNode*2, s, mid, originIdx), update(treeNode*2 + 1, mid + 1, e, originIdx));
 	}
+	
 	public static int getTarget(int treeNode, int s, int e, int target) {
 		if(s == e) {
 			update(1, 1, size, s);
@@ -52,7 +55,9 @@ class Main{
 		else							// 오른쪽 노드가 target보다 크다면
 			return getTarget(nextTreeNode + 1, mid + 1, e, target);
 	}
+	
 	public static void main(String[] args)throws Exception{
+		
 		StringBuilder sb = new StringBuilder();
 		N		= read();	// N, 숫자 범위 1<=사백만 안씀
 		size	= read();	// 내카드번호 1<=N
@@ -63,12 +68,11 @@ class Main{
 
 		boolean isExist[] = new boolean[N+1];
 		for(int i=0; i<size; i++)
-			isExist[read()] = true;
+			isExist[read()] = true;	// 입력을 받아서 Arrays.sort보다 이 코드가 더 빠름
 		
 		for(int i = 1, idx = 1; i<=N; i++) 
 			if(isExist[i])
 				arr[idx++] = i;
-		
 		
 		init(1, 1, size);	// 세그먼트 트리 초기화
 		
