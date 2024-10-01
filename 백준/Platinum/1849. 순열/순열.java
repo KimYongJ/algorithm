1292031efe74567ff0bf854ad7a3413d10ff117b
YongJu Kim <kyjdummy@gmail.com>
2024-10-01 15:48:55 +0900

    [Platinum V] Title: 순열, Time: 240 ms, Memory: 21276 KB -BaekjoonHub

--- 백준/Platinum/1849. 순열/순열.java
+++ 백준/Platinum/1849. 순열/순열.java
@@ -1,13 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1849
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
 	
 	static int N, H, idx;
 	static int[] arr, tree;
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
 			return tree[treeNode] = 1;	// 리프노드는 1 대입
@@ -32,7 +35,7 @@ class Main{
 	public static void query(int treeNode, int s, int e, int cnt) {
 		if(s == e)				// 리프노드에 들어 왔다는 것은 cnt만큼 앞이 비어있다는 것을 의미
 		{
-			update(1, 1, N, s);	// 해당 위치는 이제 비어있지 않으므로 update해서 s(리프노드)부터 루트노드까지 -1을 빼줌
+			update(1, 1, N, s);	// 해당 위치는 사용했기 때문에 update해서 s(리프노드)부터 루트노드까지 -1을 빼줌
 			arr[s] = ++idx;		// 결과 대입
 			return;
 		}
@@ -43,9 +46,8 @@ class Main{
 			query(treeNode * 2 + 1, mid + 1, e, cnt - tree[treeNode * 2]);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		N	= Integer.parseInt(br.readLine());				// 원소개수 (1<=십만)
+		N	= read();										// 원소개수 (1<=십만)
 		H	= (int)Math.ceil(Math.log(N) / Math.log(2));	// 세그먼트 트리의 높이
 		arr = new int[N + 1];
 		tree= new int[1<<(H + 2)];
@@ -54,7 +56,7 @@ class Main{
 		init(1, 1, N);
 		// 	각 입력에 대해 1번부터 N번까지 숫자 중 자기 앞에 비어있는 숫자와 같은 곳에 들어가 결과를 대입한다
 		for(int i=0; i<N; i++)
-			query(1, 1, N, Integer.parseInt(br.readLine())+1);
+			query(1, 1, N, read() + 1);
 		
 		for(int i=1; i<=N; i++)
 			sb.append(arr[i]).append('\n');
