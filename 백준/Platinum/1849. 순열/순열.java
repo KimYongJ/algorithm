3b1f61c9adbb1dd05f36bdecf4d8f868dac1c8c4
YongJu Kim <kyjdummy@gmail.com>
2024-10-01 15:47:19 +0900

    [Platinum V] Title: 순열, Time: 324 ms, Memory: 27668 KB -BaekjoonHub

--- 백준/Platinum/1849. 순열/순열.java
+++ 백준/Platinum/1849. 순열/순열.java
@@ -10,26 +10,30 @@ class Main{
 	static int[] arr, tree;
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
-			return tree[treeNode] = 1;
+			return tree[treeNode] = 1;	// 리프노드는 1 대입
+		
 		int mid = (s + e) >> 1;
+		// 내부 노드들은 리프노드부터 누적합으로 계산
 		return tree[treeNode] = init(treeNode*2, s, mid) + init(treeNode*2 + 1, mid + 1, e);
 	}
-	public static void update(int treeNode, int s, int e,int originIdx, int diff) {
-		if(originIdx < s || e < originIdx)
+	public static void update(int treeNode, int s, int e,int originIdx) {
+		if(originIdx < s || e < originIdx)	// s~e범위가 originIdx에 해당하지 않으면 바로 종료
 			return;
 		
-		tree[treeNode] += diff;
+		tree[treeNode]--;	// 값 업데이트
 		
-		if(s != e) {
+		if(s != e)			// 리프노드가 아닌 경우 계속 연산함
+		{
 			int mid = (s + e) >> 1;
-			update(treeNode * 2, s, mid , originIdx, diff);
-			update(treeNode * 2 + 1, mid +1, e, originIdx, diff);
+			update(treeNode * 2, s, mid , originIdx);
+			update(treeNode * 2 + 1, mid +1, e, originIdx);
 		}
 	}
 	public static void query(int treeNode, int s, int e, int cnt) {
-		if(s == e){
-			update(1, 1, N, s, -1);
-			arr[s] = ++idx;
+		if(s == e)				// 리프노드에 들어 왔다는 것은 cnt만큼 앞이 비어있다는 것을 의미
+		{
+			update(1, 1, N, s);	// 해당 위치는 이제 비어있지 않으므로 update해서 s(리프노드)부터 루트노드까지 -1을 빼줌
+			arr[s] = ++idx;		// 결과 대입
 			return;
 		}
 		int mid = (s + e) >> 1;
@@ -41,18 +45,16 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		N	= Integer.parseInt(br.readLine());	// 원소개수 (1<=십만)
+		N	= Integer.parseInt(br.readLine());				// 원소개수 (1<=십만)
 		H	= (int)Math.ceil(Math.log(N) / Math.log(2));	// 세그먼트 트리의 높이
 		arr = new int[N + 1];
 		tree= new int[1<<(H + 2)];
 		
-		init(1, 1, N);	// 0~N까지 자기 앞에 비어있는 숫자
-		
+		// 세그먼트 트리의 노드들은 자기 앞에 비어있는 숫자를 갖고있는 것으로 생각하면됨, 각 리프노드들은 1이며, 그위 노드들은 리프노드들의 누적합
+		init(1, 1, N);
+		// 	각 입력에 대해 1번부터 N번까지 숫자 중 자기 앞에 비어있는 숫자와 같은 곳에 들어가 결과를 대입한다
 		for(int i=0; i<N; i++)
-		{
-			int cnt = Integer.parseInt(br.readLine());
-			query(1, 1, N, cnt+1);
-		}
+			query(1, 1, N, Integer.parseInt(br.readLine())+1);
 		
 		for(int i=1; i<=N; i++)
 			sb.append(arr[i]).append('\n');
