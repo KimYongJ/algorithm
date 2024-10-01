207196db6beba995e301404df8a5418ccecc6d66
YongJu Kim <kyjdummy@gmail.com>
2024-10-01 20:56:54 +0900

    [Platinum V] Title: 순열복원, Time: 256 ms, Memory: 22052 KB -BaekjoonHub

--- 백준/Platinum/1777. 순열복원/순열복원.java
+++ 백준/Platinum/1777. 순열복원/순열복원.java
@@ -1,29 +1,32 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1777
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N, H, idx;
 	static int[] arr, tree, res;
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
 			return tree[treeNode] = 1;
 		
 		int mid = (s + e) >> 1;
+		
 		return tree[treeNode] = init(treeNode*2, s, mid) + init(treeNode*2 + 1, mid + 1, e);
 	}
 	public static void update(int treeNode, int s, int e, int originIdx, int diff) {
 		if(originIdx < s || e < originIdx)
 			return;
-		if(s == e)
-		{
-			tree[treeNode] += diff;
-			return;
-		}
 		
 		tree[treeNode] += diff;
+		
+		if(s == e)
+			return;
 		
 		int mid = (s + e) >> 1;
 
@@ -45,23 +48,21 @@ class Main{
 			query(treeNode * 2 + 1, mid + 1, e, cnt - tree[treeNode * 2]);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		N	= Integer.parseInt(br.readLine());	// 순열 크기 (1<=십만)
-		H	= (int)Math.ceil(Math.log(N+1) / Math.log(2));
-		idx = N;
-		arr = new int[N + 1];
-		res = new int[N + 1];
-		tree= new int[1<<(H+1)];
+		N		= read();	// 순열 크기 (1<=십만)
+		H		= (int)Math.ceil(Math.log(N+1) / Math.log(2));
+		idx 	= N;
+		arr 	= new int[N + 1];
+		res 	= new int[N + 1];
+		tree	= new int[1<<(H+1)];
 		
 		init(1, 1, N);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
-		for(int i=N; i>0; i--)
-			query(1, 1, N, arr[i]);// arr[i]의 숫자만큼 오른쪽에서 1의 개수 를 헤아려 넣는다.
+		for(int i=N; i>0; i--)		// 뒤에서부터 탐색하며 왼쪽에서 오른쪽으로 채워나간다.
+			query(1, 1, N, arr[i]);
 		
 		for(int i=1; i<=N; i++)
 			sb.append(res[i]).append(' ');
