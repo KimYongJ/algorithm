8a36df9d2f2c805ab9521a5c782f413d57d1b2e7
YongJu Kim <kyjdummy@gmail.com>
2024-10-02 22:15:07 +0900

    [Gold I] Title: 최솟값, Time: 316 ms, Memory: 23580 KB -BaekjoonHub

--- 백준/Gold/10868. 최솟값/최솟값.java
+++ 백준/Gold/10868. 최솟값/최솟값.java
@@ -1,12 +1,14 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/10868
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int[] arr, segTree;
-	
+	// 빠른 입력을 위한 함수
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
 			return segTree[treeNode] = arr[s];
@@ -22,29 +24,28 @@ class Main{
 		int mid = (s + e) >> 1;
 		return Math.min( getMin(treeNode*2, s, mid, left, right), getMin(treeNode*2 + 1, mid + 1, e, left, right) );
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N	= Integer.parseInt(st.nextToken());			// 정수들의 수(1<=십만)
-		int M	= Integer.parseInt(st.nextToken());			// 범위수(1<=십만)
-		int H	= (int)Math.ceil(Math.log(N) / Math.log(2));	// 세그먼트 트리의 높이
+		int N	= read();									// 정수들의 수(1<=십만)
+		int M	= read();									// 범위수(1<=십만)
+		int H	= (int)Math.ceil(Math.log(N) / Math.log(2));// 세그먼트 트리의 높이
 		arr		= new int[N+1];
 		segTree = new int[1<<(H + 1)];
 		
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 		
 		init(1, 1, N);
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			sb.append(getMin(1, 1, N, a, b))
 				.append('\n');
 		}
+		
 		System.out.print(sb.toString());
 	}
 }
