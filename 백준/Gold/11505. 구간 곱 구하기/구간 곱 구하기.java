b80759b5a5358c9d143e034f5ece5e6b530df657
YongJu Kim <kyjdummy@gmail.com>
2024-10-03 11:48:39 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 276 ms, Memory: 41460 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -1,10 +1,9 @@
-//https://github.com/KimYongJ/algorithm
+//https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/11505
 class Main{
 	
-	static long 	MOD = 1_000_000_007;
-	static long[]	tree;
-	static int[]	arr;
+	static final int MOD = 1_000_000_007;
+	static long[] arr, tree;
 	
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
@@ -14,60 +13,60 @@ class Main{
 	public static long init(int treeNode, int s, int e) {
 		if(s == e)
 			return tree[treeNode] = arr[s];
+		int mid = (s + e) >> 1;
+		return tree[treeNode] = (init(treeNode*2, s, mid) * init(treeNode*2+1, mid+1, e)) % MOD;
+	}
+	public static void update(int treeNode, int s, int e, int originIdx, int diff) {
+		if(originIdx < s || e < originIdx)
+			return;
+		if(s == e)
+		{
+			tree[treeNode] = diff;
+			return;
+		}
 		
 		int mid = (s + e) >> 1;
+		update(treeNode*2, s, mid, originIdx, diff);
+		update(treeNode*2+1, mid+1, e, originIdx, diff);
 		
-		return tree[treeNode] = (init(treeNode * 2, s, mid) * init(treeNode*2 +1, mid + 1, e)) % MOD;
+		tree[treeNode] = (tree[treeNode * 2] * tree[treeNode * 2 + 1]) % MOD;
 	}
-	public static long getRes(int treeNode, int s, int e, int left, int right) {
+	public static long query(int treeNode, int s, int e, int left, int right) {
 		if(right < s || e < left)
 			return 1;
 		
-		if(left<=s && e<=right)
+		if(left <= s && e <= right)
 			return tree[treeNode];
 		
 		int mid = (s + e) >> 1;
-		return (getRes(treeNode*2, s, mid, left, right) * getRes(treeNode*2+1, mid+1, e, left, right)) % MOD;
-	}
-	public static long update(int treeNode, int s, int e, int originIdx, int after) {
-		if(originIdx < s || e < originIdx)
-			return tree[treeNode];
 		
-		if(s == e) 
-			return tree[treeNode] = after;
-		
-		int mid = (s + e) >> 1;
-		
-		return tree[treeNode] = (update(treeNode<<1, s, mid, originIdx, after) *	update((treeNode<<1) + 1, mid + 1, e, originIdx, after)) % MOD;
+		return (query(treeNode*2, s, mid, left, right) * query(treeNode*2+1, mid+1, e, left, right)) % MOD;
 	}
 	public static void main(String[] args)throws Exception{
 		StringBuilder sb = new StringBuilder();
-		int N = read();	// 수의 개수(1<=백만)
-		int M = read();	// 수의 변경이 일어나는 횟수(1<=만)
-		int K = read();	// 구간 곱을 구하는 횟수(1<=만)
-		int H = (int)Math.ceil(Math.log(N) / Math.log(2));
-		
-		arr = new int[N + 1];
-		tree= new long[1<<(H + 1)];
+		int N	= read();
+		int M	= read() + read();
+		int H	= (int)Math.ceil(Math.log(N) / Math.log(2));
+		arr		= new long[N + 1];
+		tree	= new long[1<<(H+1)];
 		
 		for(int i=1; i<=N; i++)
 			arr[i] = read();
-
+		
 		init(1, 1, N);
 		
-		for(int i=M+K; i>=1; i--)
+		while(M-->0)
 		{
-			int cmd = read();
+			int cmd	= read();
 			int a	= read();
 			int b	= read();
-
-			if(cmd == 1)	// a를 b로바꾼다.
+			
+			if(cmd == 1)	// arr[a]를 b로 치환
 				update(1, 1, N, a, b);
-			else			// a~b구간 곱을 구한다.
-				sb.append( getRes(1, 1, N, a, b) )
+			else			// a ~ b 범위 곱셈 출력
+				sb.append( query(1, 1, N, a, b) )
 					.append('\n');
 		}
-		
-		System.out.print(sb);
+		System.out.print(sb.toString());
 	}
 }
