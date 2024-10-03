524661c82e093df9d518dec82b6aef3ebb8c547a
YongJu Kim <kyjdummy@gmail.com>
2024-10-03 19:59:28 +0900

    [Platinum IV] Title: 먹이사슬, Time: 1364 ms, Memory: 40812 KB -BaekjoonHub

--- 백준/Platinum/2532. 먹이사슬/먹이사슬.java
+++ 백준/Platinum/2532. 먹이사슬/먹이사슬.java
@@ -1,9 +1,6 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2532
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Node{
 	int l, r;Node(int l, int r){this.l=l; this.r=r;}
 }
@@ -11,23 +8,10 @@ class Main{
 	
 	static int N, len, LIS[];
 	static Node node[];
-	public static int upperBound(int target) {
-		int s	= 0;
-		int e	= len - 1;
-		int res = 0;
-		while(s <= e)
-		{
-			int mid = (s + e) >> 1;
-			if(target <= LIS[mid])
-			{
-				s = mid + 1;
-				res = mid;
-			}
-			else
-				e = mid - 1;
-		}
-		
-		return res+1;
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 	public static int binarySearch(int target) {
 		int s	= 0;
@@ -36,51 +20,43 @@ class Main{
 		while(s <= e)
 		{
 			int mid = (s + e) >> 1;
-			if(target == LIS[mid])
+			if(target < LIS[mid])
 			{
-				return upperBound(target); // target을 갖는 가장큰 idx
-			}
-			if(LIS[mid] < target)
-			{
-				e = mid - 1;
 				res = mid;
+				e = mid - 1;
 			}
-			else {
-				s= mid + 1;
-			}
+			else
+				s = mid + 1;
 		}
 		
 		return res;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());	// 동물의 수 (1<=50만)
-		len		= 1;								// 가장긴 부분수열의 길이를 나타낼 변수
-		LIS		= new int[N];						// 가장긴 부분수열을 담을 배열
+		N		= read();		// 동물의 수 (1<=50만)
+		len		= 1;			// 가장긴 부분수열의 길이를 나타낼 변수
+		LIS		= new int[N];	// 가장긴 부분수열을 담을 배열
 		node	= new Node[N];
 		
 		for(int i=0; i<N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			st.nextToken();
-			int l = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
-			node[i] = new Node(l, r);
+			read();
+			node[i] = new Node(read(), read());
 		}
 		
 		Arrays.sort(node, (a,b) -> a.l != b.l ? a.l - b.l : b.r - a.r);
 		
-		LIS[0] = node[0].r;
+		LIS[0] = -node[0].r;				// 음수로 바꿔줌으로, 가장 긴 감소하는 부분수열을 가장 긴 증가하는 부분수열로 연산토록한다.
 		
 		for(int i=1; i<N; i++)
 		{
 			if(node[i].l == node[i-1].l && node[i].r == node[i-1].r)
 				continue;
-			if(node[i].r <= LIS[len-1])
-				LIS[len++] = node[i].r;
+			int val = -node[i].r;			// 음수로 바꿔줌으로, 가장 긴 감소하는 부분수열을 가장 긴 증가하는 부분수열로 연산토록한다.
+			if(LIS[len-1] <= val)
+				LIS[len++] = val;
 			else {
-				int idx = binarySearch(node[i].r);// 가장 긴 감소하는 부분수열
-				LIS[idx] = node[i].r;
+				int idx = binarySearch(val);// 가장 긴 증가하는 부분 수열
+				LIS[idx] = val;
 			}
 		}
 		System.out.print(len);
