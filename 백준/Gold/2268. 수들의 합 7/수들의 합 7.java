5e0b132c8776c0988376b13a8b486e24a041320e
YongJu Kim <kyjdummy@gmail.com>
2024-10-03 22:52:56 +0900

    [Gold I] Title: 수들의 합 7, Time: 1032 ms, Memory: 64900 KB -BaekjoonHub

--- 백준/Gold/2268. 수들의 합 7/수들의 합 7.java
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.java
@@ -1,12 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2268
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static long[] arr, tree;
-
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	static long sum(int treeNode, int s, int e, int left, int right) {
 		if(right < s || e < left)
 			return 0;
@@ -17,6 +20,7 @@ class Main{
 		
 		return sum(treeNode*2, s, mid, left, right) + sum(treeNode*2+1, mid+1, e, left, right);
 	}
+	
 	static void modify(int treeNode, int s, int e, int originIdx, long diff) {
 		if(originIdx < s || e < originIdx)
 			return;
@@ -30,12 +34,11 @@ class Main{
 			modify(treeNode*2+1, mid+1, e, originIdx, diff);
 		}
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder	sb = new StringBuilder();
-		int N	= Integer.parseInt(st.nextToken());
-		int M	= Integer.parseInt(st.nextToken());
+		int N	= read();
+		int M	= read();
 		int H	= (int)Math.ceil(Math.log(N) / Math.log(2));
 		arr		= new long[N+1];
 		tree	= new long[1<<(H+1)];
@@ -43,10 +46,10 @@ class Main{
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int cmd = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int cmd	= read();
+			int a	= read();
+			int b	= read();
+			
 			if(cmd == 0)// a~b구간 합
 			{	
 				long res = 0;
@@ -57,7 +60,8 @@ class Main{
 				sb.append( res )
 					.append('\n');
 			}
-			else {
+			else
+			{
 				modify(1, 1, N, a, b - arr[a]);
 				arr[a] = b;
 			}
