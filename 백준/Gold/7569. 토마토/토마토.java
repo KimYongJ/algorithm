d63e45857a82883af112541d0dd248a1cb4fed84
YongJu Kim <kyjdummy@gmail.com>
2024-10-03 21:01:29 +0900

    [Gold V] Title: 토마토, Time: 252 ms, Memory: 43788 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -8,15 +8,13 @@ class Node{
 
 class Main{
 
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        boolean m = n == 13;
-        if (m)n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48) {
-        n = (n << 3) + (n << 1) + (c & 15);}
-        return m ? ~n + 1 : n;
+	static int read() throws Exception {
+        int c, N = System.in.read() - 48;
+        while ((c = System.in.read()) > 32)
+            N = 10 * N + c - 48;
+        return N;
     }
-    
+   
     public static void main(String[] args)throws Exception{
     	final int[][] dxy	= {{0,0,1},{0,0,-1},{0,1,0},{0,-1,0},{1,0,0},{-1,0,0}};
     	ArrayDeque<Node> q	= new ArrayDeque<>();
