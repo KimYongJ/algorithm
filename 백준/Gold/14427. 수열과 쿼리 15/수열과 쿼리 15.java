b7ecc2b08a85bfb9216e431f1ddffe8ca6798ea7
YongJu Kim <kyjdummy@gmail.com>
2024-10-04 11:15:43 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 256 ms, Memory: 22028 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -1,13 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14427
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int[] arr, tree;
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
 			return tree[treeNode] = arr[s];
@@ -16,61 +18,59 @@ class Main{
 		
 		return tree[treeNode] = Math.min(init(treeNode*2, s, mid), init(treeNode*2+1, mid + 1, e));
 	}
+	
 	public static void update(int treeNode, int s, int e, int originIdx, int diff) {
 		if(originIdx < s || e < originIdx)
 			return;
 
-		if(s == e) {
+		if(s == e){
 			tree[treeNode] = diff;
 			return;
 		}
 		
-		int mid = (s + e) >> 1;
-		update(treeNode*2, s, mid, originIdx, diff);
-		update(treeNode*2+1, mid + 1, e, originIdx, diff);
+		int mid				= (s + e) >> 1;
+		int nextTreeNode	= treeNode << 1;
 		
-		tree[treeNode] = Math.min(tree[treeNode * 2], tree[treeNode * 2 + 1]);
+		update(nextTreeNode, s, mid, originIdx, diff);
+		update(nextTreeNode | 1, mid + 1, e, originIdx, diff);
+		
+		tree[treeNode] = Math.min(tree[nextTreeNode], tree[nextTreeNode | 1]);
 	}
+	
 	public static int getMinIdx(int treeNode, int s, int e) {
 		if(s == e)
 			return s;
-		int mid = (s + e) >> 1;
-		int nextTreeNode = treeNode << 1;
-		if(tree[nextTreeNode] <= tree[nextTreeNode | 1])
-			return getMinIdx(nextTreeNode, s, mid);
-		else
-			return getMinIdx(nextTreeNode | 1, mid + 1, e);
+		
+		int mid				= (s + e) >> 1;
+		int nextTreeNode	= treeNode << 1;
+		
+		return tree[nextTreeNode] <= tree[nextTreeNode | 1] 	? 
+						getMinIdx(nextTreeNode, s, mid)			:
+						getMinIdx(nextTreeNode | 1, mid + 1, e);
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());	// 수열의크기 (1<=십만)
-		int H = (int)Math.ceil(Math.log(N) / Math.log(2));
 		
-		arr = new int[N+1];
-		tree= new int[1<<(H+1)];
+		int N	= read();									// 수열의크기 (1<=십만)
+		int H	= (int)Math.ceil(Math.log(N) / Math.log(2));// 세그먼트 트리의 높이
+		arr 	= new int[N+1];
+		tree	= new int[1<<(H+1)];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
-		init(1, 1, N);
+		init(1, 1, N);			// 세그먼트 트리(배열) 초기화
 		
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
+		
 		while(T-- > 0)
-		{
-			st = new StringTokenizer(br.readLine());
-			int cmd = Integer.parseInt(st.nextToken());
-			if(cmd == 2)	// 해당 배열에서 가장작은 값의 인덱스 출력			
+			if(read() == 2)		// 해당 배열에서 가장작은 값의 인덱스 출력			
 				sb.append(getMinIdx(1, 1, N)).append('\n');
-			else
-			{
-				int idx = Integer.parseInt(st.nextToken());
-				int value = Integer.parseInt(st.nextToken());
-				update(1, 1, N, idx, value);
-			}
-		}
+			else				// 해당 위치 값을 없데이트, 그 후 min 최신화
+				update(1, 1, N, read(), read());
+			
+
 		System.out.print(sb.toString());
 	}
 }
