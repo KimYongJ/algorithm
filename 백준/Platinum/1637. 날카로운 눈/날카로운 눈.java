14ebad769c3d5fc0b4a9a22c72b0615f950378d2
YongJu Kim <kyjdummy@gmail.com>
2024-10-04 16:00:12 +0900

    [Platinum IV] Title: 날카로운 눈, Time: 144 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
+++ 백준/Platinum/1637. 날카로운 눈/날카로운 눈.java
@@ -1,10 +1,11 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1637
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
-	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static long getSum(int N, int[] A, int[] B, int[] C, long target) {
 		long sum = 0;
 		for(int i=0; i<N; i++)
@@ -13,27 +14,27 @@ class Main{
 		return sum;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N	= Integer.parseInt(br.readLine());	// 1<=이만
+		int N	= read();	// 1<=이만
 		int A[] = new int[N];
 		int B[] = new int[N];
 		int C[] = new int[N];
 		long s	= 1;
 		long e	= 0;
+		
 		for(int i=0; i<N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			A[i] = Integer.parseInt(st.nextToken());
-			B[i] = Integer.parseInt(st.nextToken());
-			C[i] = Integer.parseInt(st.nextToken());
+			A[i] = read();
+			B[i] = read();
+			C[i] = read();
 			e = Math.max(B[i], e);
 		}
+		
 		long res = -1;
+		
 		while(s <= e)
 		{
 			long mid = (s + e) >> 1;
-			
+			// mid값 이하의 수들의 총 개수를 구해온다. 
 			long sum = getSum(N, A, B, C, mid);
 			if(sum % 2 == 0)
 				s = mid + 1;
@@ -43,14 +44,18 @@ class Main{
 				res = mid;
 			}
 		}
+		
 		if(res == -1)
 		{
 			System.out.print("NOTHING");
 			return;
 		}
+		
 		StringBuilder sb = new StringBuilder();
+		
 		sb.append(res).append(' ');
 		sb.append(getSum(N, A, B, C, res) - getSum(N, A, B, C, res - 1));
+		
 		System.out.print(sb.toString());
 	}
 }
