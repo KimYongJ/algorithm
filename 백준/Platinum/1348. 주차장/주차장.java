ccb8c07688365aa89d26c2b63eb4dbc091a46efe
YongJu Kim <kyjdummy@gmail.com>
2024-10-04 22:09:21 +0900

    [Platinum II] Title: 주차장, Time: 276 ms, Memory: 29260 KB -BaekjoonHub

--- 백준/Platinum/1348. 주차장/주차장.java
+++ 백준/Platinum/1348. 주차장/주차장.java
@@ -1,130 +1,145 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1348
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-    static final int[] dx = {-1, 1, 0, 0};
-    static final int[] dy = {0, 0, -1, 1};
-    static int n, m, ccnt = 1, pcnt = 1, answer = Integer.MAX_VALUE;
-    static int[][] graph = new int[51][51];
-    static int[] c = new int[300], f = new int[300];
-    static int[][] bvisit = new int[51][51];
-    static boolean[] visit = new boolean[300];
-    static ArrayList<Pair>[] adj = new ArrayList[110];
-    static Queue<Triple> q = new LinkedList<>();
+class Node{
+	int y, x, time; 
+	Node(int y, int x){this.y=y; this.x=x;}
+	Node(int y, int x, int time){
+		this.y=y; this.x=x; this.time=time;
+	}
+}
+class Time{int t, idx; Time(int t,int idx){this.t=t; this.idx=idx;}}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        for (int i = 0; i < 110; i++) {
-            adj[i] = new ArrayList<>();
-        }
-        
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        for (int i = 1; i <= n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < s.length(); j++) {
-                char x = s.charAt(j);
-                if (x == 'C') {
-                    graph[i][j + 1] = ccnt++;
-                } else if (x == '.') {
-                    graph[i][j + 1] = 0;
-                } else if (x == 'X') {
-                    graph[i][j + 1] = -1;
-                } else if (x == 'P') {
-                    graph[i][j + 1] = 110 + pcnt++;
-                }
-            }
-        }
+class Main{
+	
+	static final int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+	
+	public static void BFS(int map[][], int Y, int X, Node start, int idx, int dist[][], ArrayList<Integer> link){
+		ArrayDeque<Node> q		= new ArrayDeque<>();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (graph[i][j] > 0 && graph[i][j] < 110) {
-                    for (int[] row : bvisit) {
-                        Arrays.fill(row, -1);
-                    }
-                    q.add(new Triple(i, j, 0));
-                    bvisit[i][j] = 0;
-                    bfs(graph[i][j]);
-                }
-            }
-        }
+		boolean[][] visit		= new boolean[Y][X];
+		visit[start.y][start.x]		= true;
+		
+		q.add(new Node(start.y, start.x, 0));
+		
+		while(!q.isEmpty())
+		{
+			Node now = q.poll();
+			
+			if(map[now.y][now.x]== 2) {
+				dist[idx][now.y * X + now.x] = now.time;
+				link.add(now.y * X + now.x);
+			}
 
-        int l = 1, r = 1000000;
-        while (l <= r) {
-            int mid = (l + r) / 2, match = 0;
-            Arrays.fill(c, -1);
-            Arrays.fill(f, -1);
-            for (int i = 1; i < ccnt; i++) {
-                if (c[i] == -1) {
-                    Arrays.fill(visit, false);
-                    if (dfs(i, mid)) match++;
-                }
-            }
-            if (match == ccnt - 1) {
-                answer = Math.min(answer, mid);
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        
-        if (ccnt == 1) {
-            System.out.println(0);
-        } else if (answer == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);
-        }
-    }
+			for(int xy[] : dxy)
+			{
+				int nextY = now.y + xy[0];
+				int nextX = now.x + xy[1];
+				int nextTime = now.time+1;
+				if(nextY>=0 && nextX>=0 && nextY<Y && nextX<X && !visit[nextY][nextX] && map[nextY][nextX] != 3)
+				{
+					visit[nextY][nextX] = true;
+					q.add(new Node(nextY, nextX, nextTime));
+				}
+			}
+		}
+	}
 
-    static boolean dfs(int s, int up) {
-        visit[s] = true;
-        for (Pair e : adj[s]) {
-            if (e.weight > up) continue;
-            if (f[e.node] == -1 || (!visit[f[e.node]] && dfs(f[e.node], up))) {
-                c[s] = e.node;
-                f[e.node] = s;
-                return true;
-            }
-        }
-        return false;
-    }
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int Y		= Integer.parseInt(st.nextToken());
+		int X		= Integer.parseInt(st.nextToken());
+		int map[][] = new int[Y][X];				// 1은 차, 2는 주차장, 3은 벽
+		int carCnt	= 0;
+		int pkCnt	= 0;
+		ArrayList<Node> car = new ArrayList<>();
+		for(int y=0; y<Y; y++)
+		{
+			String str = br.readLine();
+			for(int x=0; x<X; x++)
+			{
+				char c = str.charAt(x);
+				if(c == 'C')
+				{
+					map[y][x] = 1;
+					carCnt++;
+					car.add(new Node(y, x));
+				}
+				else if(c == 'P') {
+					map[y][x] = 2;
+					pkCnt++;
+				}
+				else if(c == 'X')
+					map[y][x] = 3;
+			}
+		}
+		
+		if(carCnt == 0) {
+			System.out.print(0);
+			return;
+		}
+		
+		if(pkCnt < carCnt) {
+			System.out.print(-1);
+			return;
+		}
+		
+		int len = Y * X;
+		ArrayList<Integer> link[] = new ArrayList[carCnt];// 이분매칭을 위한 유형 그래프로 변경
+		int dist[][] = new int[carCnt][len];	// 자동차의 인덱스별 주차장의 위치에 시간이 담겨있다.
+		for(int i=0; i<carCnt; i++) {
+			link[i] = new ArrayList<>();
+			BFS(map, Y, X, car.get(i), i, dist, link[i]);
+		}
+		
+		int s = 1;
+		int e = len;
+		int res = -1;
+		while(s <= e)
+		{
+			int mid = (s + e) >> 1;				// 해당 초에 모두 방문이 가능 한지 체크
 
-    static void bfs(int s) {
-        while (!q.isEmpty()) {
-            Triple current = q.poll();
-            int x = current.x, y = current.y, cnt = current.count;
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if (nx < 1 || ny < 1 || nx > n || ny > m || bvisit[nx][ny] != -1 || graph[nx][ny] == -1) continue;
-                bvisit[nx][ny] = cnt + 1;
-                if (graph[nx][ny] > 110) adj[s].add(new Pair(graph[nx][ny], cnt + 1));
-                q.add(new Triple(nx, ny, cnt + 1));
-            }
-        }
-    }
-
-    static class Pair {
-        int node, weight;
-
-        Pair(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
-        }
-    }
-
-    static class Triple {
-        int x, y, count;
-
-        Triple(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
+			if(check(mid, len, carCnt, link, dist))
+			{
+				res = mid;
+				e = mid - 1;
+			}
+			else
+				s = mid + 1;
+		}
+		System.out.print(res);
+	}
+	public static boolean check(int maxTime, int len, int carSize, ArrayList<Integer> link[], int dist[][]) {
+		int[] parkOwner = new int[len];
+		Arrays.fill(parkOwner, -1);
+		int matched = 0;
+		for(int i=0; i<carSize; i++)
+		{
+			boolean[] visit = new boolean[len];
+			if(DFS(link, i, maxTime, parkOwner, visit, dist))
+				matched++;
+		}
+		
+		return matched == carSize;
+	}
+	public static boolean DFS(ArrayList<Integer> link[], int idx, int maxTime,int[] parkOwner, boolean visit[], int dist[][])
+	{
+		for(int parkIndex : link[idx]) {
+			if(!visit[parkIndex] && dist[idx][parkIndex] <= maxTime) {
+				visit[parkIndex] = true;
+				if(parkOwner[parkIndex] == -1 || DFS(link, parkOwner[parkIndex], maxTime, parkOwner, visit, dist)) {
+					parkOwner[parkIndex] = idx;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
