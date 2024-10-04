8854051dcef17125bfe743112fe5e5910074b67a
YongJu Kim <kyjdummy@gmail.com>
2024-10-04 17:02:10 +0900

    [Gold III] Title: 이상한 배열, Time: 280 ms, Memory: 36032 KB -BaekjoonHub

--- 백준/Gold/28099. 이상한 배열/이상한 배열.java
+++ 백준/Gold/28099. 이상한 배열/이상한 배열.java
@@ -1,14 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1637
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N, H, arr[], tree[];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static int init(int treeNode, int s, int e) {
 		if(s == e)
 			return tree[treeNode] = arr[s];
@@ -23,26 +25,25 @@ class Main{
 		int mid = (s + e) >> 1;
 		return Math.max(query(treeNode*2, s, mid, left, right), query(treeNode*2+1, mid+1, e, left, right));
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-- >0)
 		{
-			N	= Integer.parseInt(br.readLine());
+			N	= read();
 			H	= (int)Math.ceil(Math.log(N) / Math.log(2));
 			arr = new int[N+1];
 			tree= new int[1 <<(H+1)];
 			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			for(int i=1; i<=N; i++)
-				arr[i] = Integer.parseInt(st.nextToken());
+				arr[i] = read();
 			
 			init(1, 1, N);
 			
 			boolean flag = true;
 			int idxArr[] = new int[N + 1];
+			
 			for(int i=1; i<=N; i++)
 			{
 				if(idxArr[arr[i]] != 0)
