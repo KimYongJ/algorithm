ae5eb836ab3a2f15a47423130cf0ba0d4dba513d
YongJu Kim <kyjdummy@gmail.com>
2024-10-05 13:56:56 +0900

    [Gold I] Title: 교차개수세기, Time: 988 ms, Memory: 59652 KB -BaekjoonHub

--- 백준/Gold/1615. 교차개수세기/교차개수세기.java
+++ 백준/Gold/1615. 교차개수세기/교차개수세기.java
@@ -1,70 +1,74 @@
-import java.util.ArrayList;
-import java.util.List;
-
+//https://github.com/KimYongJ/algorithm
+//https://www.acmicpc.net/problem/1615
+class Node{
+	int node;
+	Node next;
+	Node(int node, Node next){
+		this.node = node;
+		this.next = next;
+	}
+}
 public class Main {
-    static final int[] dx = {1, 0, -1, 0, 1, -1, -1, 1};
-    static final int[] dy = {0, 1, 0, -1, 1, 1, -1, -1};
+
     static final int N = 1 << 11;
-    static List<Integer>[] arr = new ArrayList[2001];
+    
     static long[] tree = new long[N << 1];
-    static int n, m;
     
     static int read() throws Exception {// 빠른 입력을 위한 함수
     	int c, n = System.in.read() & 15;
     	while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
     	return n;
     }
-    
-    public static void main(String[] args) throws Exception {
-        n = read();
-        m = read();
+    static long query(int treeNode, int s, int e, int l, int r) {
+        if (l > e || r < s)
+        	return 0;
+        
+        if (l <= s && e <= r)
+        	return tree[treeNode];
+        
+        int m = (s + e) >> 1;
+        
+        return query(treeNode << 1, s, m, l, r) + query(treeNode << 1 | 1, m + 1, e, l, r);
+    }
 
-        for (int i = 0; i < 2001; i++) {
-            arr[i] = new ArrayList<>();
+    static void update(int treeNode, int s, int e, int l, int r) {
+        if (l > e || r < s)
+        	return;
+        if (l <= s && e <= r)
+        {
+            tree[treeNode]++;
+            return;
         }
+        
+        int m = (s + e) >> 1;
+        
+        update(treeNode << 1, s, m, l, r);
+        update(treeNode << 1 | 1, m + 1, e, l, r);
+        
+        tree[treeNode] = tree[treeNode << 1] + tree[treeNode << 1 | 1];
+    }
+    public static void main(String[] args) throws Exception {
+        int n			= read();
+        int m			= read();
+        Node adNode[]	= new Node[n + 1];
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++)
+        {
             int a = read();
             int b = read();
-            arr[a].add(b);
+            adNode[a] = new Node(b, adNode[a]);
         }
 
         long ans = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int v : arr[i]) {
-                ans += query(v + 1, n);
-            }
-            for (int v : arr[i]) {
-                update(v, v);
-            }
+        for (int i = 1; i <= n; i++)
+        {
+            for(Node next=adNode[i]; next != null; next=next.next)
+                ans += query(1, 0, N - 1, next.node + 1, n);
+            
+            for(Node next=adNode[i]; next != null; next=next.next)
+                update(1, 0, N - 1, next.node, next.node);
+            
         }
         System.out.println(ans);
-    }
-
-    static long query(int l, int r) {
-        return query(l, r, 1, 0, N - 1);
-    }
-
-    static long query(int l, int r, int i, int s, int e) {
-        if (l > e || r < s) return 0;
-        if (l <= s && e <= r) return tree[i];
-        int m = (s + e) >> 1;
-        return query(l, r, i << 1, s, m) + query(l, r, i << 1 | 1, m + 1, e);
-    }
-
-    static void update(int l, int r) {
-        update(l, r, 1, 0, N - 1);
-    }
-
-    static void update(int l, int r, int i, int s, int e) {
-        if (l > e || r < s) return;
-        if (l <= s && e <= r) {
-            tree[i]++;
-            return;
-        }
-        int m = (s + e) >> 1;
-        update(l, r, i << 1, s, m);
-        update(l, r, i << 1 | 1, m + 1, e);
-        tree[i] = tree[i << 1] + tree[i << 1 | 1];
     }
 }
