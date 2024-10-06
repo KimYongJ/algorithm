fba3a806081ffc584e7fa9262a0472ab8e8d385b
YongJu Kim <kyjdummy@gmail.com>
2024-10-06 15:36:40 +0900

    [Gold I] Title: 등차수열, Time: 552 ms, Memory: 33964 KB -BaekjoonHub

--- 백준/Gold/1994. 등차수열/등차수열.java
+++ 백준/Gold/1994. 등차수열/등차수열.java
@@ -2,66 +2,45 @@
 //https://www.acmicpc.net/problem/1994
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 class Main{
-	public static int find(int dp[][], int arr[], int i, int j) {
-		if(i > j)
-			return 0;
-		if(i == j)
-			return 1;
-		
-		int ret = dp[i][j];
-		if(ret != -1)
-			return ret;
-		
-		ret = 2;
-		
-		int d = arr[j] - arr[i];
-		int next = arr[j] + d;
-		
-		int idx = Arrays.binarySearch(arr,  next);
-		if(0 <= idx && arr[idx] == next)
-			return ret = find(dp, arr, j, idx) + 1;
-		
-		return ret;
-	}
+
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N	= Integer.parseInt(br.readLine());
-		int max	= 1;
-
-		HashMap<Integer,Integer> hm = new HashMap<>();
-		ArrayList<Integer> list		= new ArrayList<>();
-		
-		for(int i=0; i<N; i++)
+		int N;
+		if((N=Integer.parseInt(br.readLine())) == 1)
 		{
-			int num = Integer.parseInt(br.readLine());
-			int cnt = hm.getOrDefault(num, 0);
-			if(cnt == 0)
-				list.add(num);
-
-			hm.put(num, ++cnt);
-			
-			if(max < cnt)
-				max = cnt;
+			System.out.print(1);
+			return;
 		}
 		
-		int arr[] = list.stream().mapToInt(Integer::intValue).toArray();
-
-		N = arr.length;
+		int max		= 1;
+		int arr[]	= new int[N+1];
 		
+		for(int i=1; i<=N; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+
 		Arrays.sort(arr);
 		
-		int dp[][] = new int[2001][2001];
+		int dp[][] = new int[N+1][N+1];
 		
-		for(int d[] : dp)
-			Arrays.fill(d, -1);
-		
-		for(int i=0; i<N-1; i++)
-			for(int j=i+1; j<N; j++)
-				max = Math.max(max, find(dp, arr, i, j));
+		for(int i=1; i<N; i++)
+			for(int j=i+1; j<=N; j++)
+			{
+				dp[i][j]	= 2;
+				int next	= (arr[i]<<1) - arr[j];
+				int k		= i;
+				
+				while(--k >=1)
+					if(arr[k] == next)
+						break;
+				
+				dp[i][j] = Math.max(dp[i][j],  dp[k][i] + 1);
+				
+				if(max < dp[i][j])
+					max = dp[i][j];
+				
+			}
 
 		System.out.print(max);
 	}
