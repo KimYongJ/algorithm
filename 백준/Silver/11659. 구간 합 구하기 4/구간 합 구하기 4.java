7c0490c6f3c6110bb71e81a33b1c21298f98f5ce
YongJu Kim <kyjdummy@gmail.com>
2024-10-06 14:50:54 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 256 ms, Memory: 21396 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,49 +1,30 @@
-// https://github.com/KimYongJ/algorithm
+//https://github.com/KimYongJ/algorithm
+//https://www.acmicpc.net/problem/11659
 
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
     public static void main(String[] args)throws Exception{
     	StringBuilder sb = new StringBuilder();
-    	Reader r = new Reader();
         int n, m, i, j;
         
-        n = r.read();
-        m = r.read();
+        n = read();
+        m = read();
         
         int[] dp = new int[n+1];
         
         for(i=1; i<=n; i++)
-            dp[i] = r.read() + dp[i-1];
+            dp[i] = read() + dp[i-1];
         
-        for(int t=0; t<m; t++){
-            i = r.read();
-            j = r.read();
+        for(int t=0; t<m; t++)
+        {
+            i = read();
+            j = read();
             sb.append( dp[j] - dp[i-1] ).append("\n");
         }
         System.out.println(sb.toString());
-    }
-}
-
-// 빠른 입력을 위해 만듦
-class Reader {
-    final int SIZE = 1 << 13;
-    byte[] buffer = new byte[SIZE];
-    int index, size;
-    int read() throws Exception {
-        int n = 0;
-        byte c;
-        while ((c = get()) <= 32);
-        do n = (n << 3) + (n << 1) + (c & 15);
-        while (isNumber(c = get()));
-        return n;
-    }
-    boolean isNumber(byte c) {
-        return 47 < c && c < 58;
-    }
-    byte get() throws Exception {
-        if (index == size) {
-            size = System.in.read(buffer, index = 0, SIZE);
-            if (size < 0) buffer[0] = -1;
-        }
-        return buffer[index++];
     }
 }
