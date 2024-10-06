5b300c197e38582349c9db9ee6bb2407c694cc23
YongJu Kim <kyjdummy@gmail.com>
2024-10-06 16:00:08 +0900

    [Gold I] Title: 등차수열, Time: 192 ms, Memory: 30300 KB -BaekjoonHub

--- 백준/Gold/1994. 등차수열/등차수열.java
+++ 백준/Gold/1994. 등차수열/등차수열.java
@@ -1,13 +1,14 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/1994
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 class Main{
-
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int N = read();
 		if(N == 1)
 		{
 			System.out.print(1);
@@ -18,7 +19,7 @@ class Main{
 		int arr[]	= new int[N+1];
 		
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 
 		Arrays.sort(arr);
 		
@@ -29,16 +30,20 @@ class Main{
 		for(int i=1; i<N; i++)
 			for(int j=i+1; j<=N; j++)
 			{
-				dp[i][j]	= 2;					// 두개를 확인하는 것이기 때문에 어떤 등차수열도 2는 최소이기 때문에 기본값
-				int before	= (arr[i]<<1) - arr[j];	// arr[i]의 이전 값을 구함 ex) 두 숫자 4, 6이 있을 때 4와 6이전 값은 2이다. 이렇게 2를 찾는것
+				// 두개를 확인하는 것이기 때문에 어떤 등차수열도 2는 최소이기 때문에 기본값
+				dp[i][j]	= 2;
+				// arr[i]의 이전 값을 구함 ex) 두 숫자 4, 6이 있을 때 4와 6이전 값은 2이다. 이렇게 2를 찾는것
+				int before	= (arr[i]<<1) - arr[j];
 
 				int s		= 0;
 				int e		= i - 1;
+				// 기본 값은 0으로하여 추후 before와 같은 숫자를 못찾은 경우 dp[0][i]가 되어 무조건0이 되도록한다.i는 1부터시작함으로 무조건 dp[i][?]는 0임
 				int k		= 0;
 
 				while(s <= e)
 				{
 					int mid = (s + e) >> 1;
+				// before와 같을 때만 mid를 k에 대입, before와 같은 가장 큰 인덱스를 k에 대입하도록 s를 높여줌
 					if(arr[mid] == before)
 					{
 						k = mid;
