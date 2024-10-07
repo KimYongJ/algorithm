e3bf2ea2f16121cded0ea99f4c4c8433a819881a
YongJu Kim <kyjdummy@gmail.com>
2024-10-07 19:59:03 +0900

    [Gold V] Title: 경쟁적 전염, Time: 140 ms, Memory: 16456 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -1,22 +1,22 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/18405
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+
 class Node{
 	int y, x;
 	Node(int y, int x){this.y=y; this.x=x;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		final int dxy[][]	= {{1,0},{0,1},{-1,0},{0,-1}};
-		BufferedReader	br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 맵크기(1<=200)
-		int K		= Integer.parseInt(st.nextToken());	// 1~K까지 바이러스 종류(1<=1,000)
+		final int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
+		int N		= read();	// 맵크기(1<=200)
+		int K		= read();	// 1~K까지 바이러스 종류(1<=1,000)
 		int map[][]	= new int[N+2][N+2];
 		
 		ArrayList<Node> list[] = new ArrayList[K+1];
@@ -28,36 +28,34 @@ class Main{
 			map[0][i] = map[N+1][i] = map[i][0] = map[i][N+1] = 1001;
 		
 		for(int y=1; y<=N; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=N; x++)
 			{
-				map[y][x] = Integer.parseInt(st.nextToken());
+				map[y][x] = read();
 				if(map[y][x] != 0)
 					list[map[y][x]].add(new Node(y,x));
 			}
-		}
 		
 		ArrayDeque<Node> q = new ArrayDeque<>();
 		for(int k=1; k<=K; k++)
 			for(Node now : list[k])
 				q.add(now);
 		
-		st = new StringTokenizer(br.readLine());
-		
-		int T = Integer.parseInt(st.nextToken());
-		int Y = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-		while(T-->0) {
+		int T = read();
+		int Y = read();
+		int X = read();
+		while(T-->0)
+		{
 			int size = q.size();
 			while(size-- >0)
 			{
 				Node now = q.poll();
 				
-				for(int xy[] : dxy) {
+				for(int xy[] : dxy)
+				{
 					int nextY = now.y + xy[0];
 					int nextX = now.x + xy[1];
-					if(map[nextY][nextX] == 0) {
+					if(map[nextY][nextX] == 0)
+					{
 						map[nextY][nextX] = map[now.y][now.x];
 						q.add(new Node(nextY, nextX));
 					}
