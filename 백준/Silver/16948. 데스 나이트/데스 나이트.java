3c95dcde0e01e92d38d4afb160523ff1aa76fb90
YongJu Kim <kyjdummy@gmail.com>
2024-10-08 10:02:12 +0900

    [Silver I] Title: 데스 나이트, Time: 108 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.java
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.java
@@ -1,29 +1,31 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16948
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Node{int y, x, cnt;Node(int y, int x,int c){this.y=y; this.x=x;this.cnt=c;}}
 
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		final int dxy[][] = {{-2, -1}, {-2, 1}, {0,-2}, {0, 2}, {2, -1}, {2, 1}};
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());	// 맵크기(5<=200)
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int sy = Integer.parseInt(st.nextToken());
-		int sx = Integer.parseInt(st.nextToken());
-		int ey = Integer.parseInt(st.nextToken());
-		int ex = Integer.parseInt(st.nextToken());
-		boolean visit[][] = new boolean[N][N];
+		final int dxy[][]	= {{-2, -1}, {-2, 1}, {0,-2}, {0, 2}, {2, -1}, {2, 1}};
+		int N				= read();	// 맵크기(5<=200)
+		int sy				= read();
+		int sx				= read();
+		int ey				= read();
+		int ex				= read();
+		boolean visit[][]	= new boolean[N][N];
+		ArrayDeque<Node> q	= new ArrayDeque<>();
 		
-		ArrayDeque<Node> q = new ArrayDeque<>();
 		visit[sy][sx] = true;
+		
 		q.add(new Node(sy, sx, 0));
-		while(!q.isEmpty()) {
+		
+		while(!q.isEmpty())
+		{
 			Node now = q.poll();
 			
 			if(now.y==ey && now.x==ex)
