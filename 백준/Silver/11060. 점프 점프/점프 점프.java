c8f2b2fcf37acae7cc44fc19e34a5cef61c11e8a
YongJu Kim <kyjdummy@gmail.com>
2024-10-08 10:20:15 +0900

    [Silver II] Title: 점프 점프, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,24 +1,25 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16948
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 
 class Node{int idx, cnt; Node(int i, int c){this.idx=i; this.cnt=c;}}
 
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] arggs)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N				= Integer.parseInt(br.readLine());	// 배열의 크기(1<=천)
+		int N				= read();	// 배열의 크기(1<=천)
 		int map[]			= new int[N+1];
 		boolean visit[]		= new boolean[N+1];
 		ArrayDeque<Node> q	= new ArrayDeque<>();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			map[i] = Integer.parseInt(st.nextToken()); // 0<=100
+			map[i] = read(); // 0<=100
 		
 		visit[1] = true;
 		q.add(new Node(1, 0));
