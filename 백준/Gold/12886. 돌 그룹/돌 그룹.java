90845a0ad72959a07554b6de101f96293306488d
YongJu Kim <kyjdummy@gmail.com>
2024-10-08 14:17:17 +0900

    [Gold IV] Title: 돌 그룹, Time: 172 ms, Memory: 28312 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -1,21 +1,23 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12886
-//채점 데이터가 너무 약해서.. 아래 코드 포함 다른 코드들이 통과하는듯..
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+//채점 데이터가 너무 약한가.. 아래 코드 포함 다른 코드들이 통과하는데.. 이유가..
+
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		int A				= Integer.parseInt(st.nextToken());
-		int B				= Integer.parseInt(st.nextToken());
-		int C				= Integer.parseInt(st.nextToken());
+		int A				= read();
+		int B				= read();
+		int C				= read();
 		int tmp[]			= new int[3];
 		boolean visit[][]	= new boolean[1500][1500];
 		ArrayDeque<int[]> q = new ArrayDeque<>();
-
+		visit[B][A] = visit[C][B] = visit[C][A] =
 		visit[A][B] = visit[B][C] = visit[A][C] = true;;
 
 		q.add(new int[] {A,B,C});
@@ -30,23 +32,35 @@ class Main{
 			}
 			
 			for(int i=0; i<3; i++)
-				for(int j=0; j<3; j++)
-					if(i!=j && now[i] < now[j])
+			{
+				int j = (i + 1) % 3;
+				if(now[i] != now[j])
+				{
+					int anoIdx = 3 - (i+j);
+					int valueI, valueJ;
+					if(now[i] < now[j])
 					{
-						int anoIdx = 3 - (i+j);
-						int valueI = now[i] << 1;
-						int valueJ = now[j] - now[i];
-						
-						if(!visit[valueI][valueJ])
-						{
-							visit[valueI][valueJ] = true;
-							tmp[i]		= valueI;
-							tmp[j]		= valueJ;
-							tmp[anoIdx] = now[anoIdx];
-							
-							q.add(new int[] {tmp[0], tmp[1], tmp[2]});
-						}
+						valueI = now[i] << 1;
+						valueJ = now[j] - now[i];						
 					}
+					else
+					{
+						valueI = now[i] - now[j];
+						valueJ = now[j] << 1;
+					}
+					
+					
+					if(!visit[valueI][valueJ] && !visit[valueJ][valueI])
+					{
+						visit[valueI][valueJ] = visit[valueJ][valueI] = true;
+						tmp[i]		= valueI;
+						tmp[j]		= valueJ;
+						tmp[anoIdx] = now[anoIdx];
+						
+						q.add(new int[] {tmp[0], tmp[1], tmp[2]});
+					}
+				}
+			}
 		}
 		System.out.print(0);
 	}
