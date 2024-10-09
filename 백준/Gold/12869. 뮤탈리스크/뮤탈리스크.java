49bbbda0ec40f1cf579816131ae95915d517196e
YongJu Kim <kyjdummy@gmail.com>
2024-10-09 16:30:01 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 104 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -1,24 +1,24 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12869
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 class Node{
 	int a,b,c, cnt;Node(int a, int b, int c, int cnt){this.a=a;this.b=b;this.c=c;this.cnt=cnt;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		final int attack[][] = {{9,3,1},{9,1,3},{1,3,9},{1,9,3},{3,1,9},{3,9,1}};
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		ArrayDeque<Node> q = new ArrayDeque<>();
-		int e[] = new int[3];
-		boolean visit[][][] = new boolean[61][61][61];
+		final int attack[][]	= {{9,3,1},{9,1,3},{1,3,9},{1,9,3},{3,1,9},{3,9,1}};
+		ArrayDeque<Node> q		= new ArrayDeque<>();
+		int N					= read();
+		int e[]					= new int[3];
+		boolean visit[][][]		= new boolean[61][61][61];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			e[i] = Integer.parseInt(st.nextToken());
+			e[i] = read();
 		
 		q.add(new Node(e[0], e[1], e[2], 0));
 		
@@ -27,12 +27,14 @@ class Main{
 		while(!q.isEmpty())
 		{
 			Node now = q.poll();
-			if(now.a <= 0 && now.b<=0 && now.c<=0) {
+			if(now.a <= 0 && now.b<=0 && now.c<=0)
+			{
 				System.out.print(now.cnt);
 				return;
 			}
 			int nextCnt = now.cnt + 1;
-			for(int a[] : attack) {
+			for(int a[] : attack)
+			{
 				int nextA = Math.max(0, now.a - a[0]);
 				int nextB = Math.max(0, now.b - a[1]);
 				int nextC = Math.max(0, now.c - a[2]);
