b551f4ce9f86a8b71254bae2415ce27d00a142b3
YongJu Kim <kyjdummy@gmail.com>
2024-10-10 10:04:52 +0900

    [Gold IV] Title: 연구소 2, Time: 204 ms, Memory: 29568 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.java
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -1,10 +1,7 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17141
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 class Node{int y, x; Node(int y, int x){this.y=y; this.x=x;}}
 
@@ -15,18 +12,25 @@ class Main{
 	static Node pos[];
 	static ArrayList<Node> posList;
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void BFS()
 	{
 		ArrayDeque<Node> q	= new ArrayDeque<>();
 		boolean visit[][]	= new boolean[N+2][N+2];
+		
 		for(Node n : pos)
 		{
 			visit[n.y][n.x]= true; 
 			q.add(n);
 		}
 		
-		int bnkCnt	= M;
-		int time	= 0;
+		int bnkCnt	= M;	// 빈칸을 채운 개수 시작은 M개로
+		int time	= 0;	// 걸리는 시간
+		
 		while(!q.isEmpty())
 		{
 			int size = q.size();
@@ -49,48 +53,41 @@ class Main{
 			++time;
 		}
 		
-		
 		if(bnkCnt == blankCnt)
 			res = Math.min(res, time);
 	}
 	public static void DFS(int depth, int idx) {
 		if(depth == M)
-		{
 			BFS();
-			return;
-		}
-		for(int i=idx; i<posList.size(); i++)
-		{
-			pos[depth] = posList.get(i);
-			DFS(depth + 1, i + 1);
-		}
+		else
+			for(int i=idx; i<posList.size(); i++)
+			{
+				pos[depth] = posList.get(i);
+				DFS(depth + 1, i + 1);
+			}
 	}
 	public static void main(String args[]) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N		= Integer.parseInt(st.nextToken()); // 5 ≤ N ≤ 50
-		M		= Integer.parseInt(st.nextToken());	// 1 ≤ M ≤ 10
+		N		= read();	// 5 ≤ N ≤ 50
+		M		= read();	// 1 ≤ M ≤ 10
 		map		= new int[N+2][N+2];
 		pos		= new Node[M];
 		posList = new ArrayList<>();
 		res		= Integer.MAX_VALUE;
 		
+		// 패딩 삽입
 		for(int i=0; i<=N+1; i++)
 			map[i][0] = map[i][N+1] = map[0][i] = map[N+1][i] = 1;
+		
 		// 0은 빈 칸, 1은 벽, 2는 바이러스를 놓을 수 있는 칸
 		for(int y=1; y<=N; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=N; x++)
 			{
-				map[y][x] = Integer.parseInt(st.nextToken());
+				map[y][x] = read();
 				if(map[y][x] == 2)
 					posList.add(new Node(y,x));
 				if(map[y][x] != 1)
 					blankCnt++;
 			}
-		}
 		
 		DFS(0, 0);
 
