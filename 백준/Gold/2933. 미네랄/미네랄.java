17f4799e2eb626ec75a308f9aec2580756518e54
YongJu Kim <kyjdummy@gmail.com>
2024-10-17 13:15:28 +0900

    [Gold I] Title: 미네랄, Time: 200 ms, Memory: 38284 KB -BaekjoonHub

--- 백준/Gold/2933. 미네랄/미네랄.java
+++ 백준/Gold/2933. 미네랄/미네랄.java
@@ -1,10 +1,7 @@
 //https://github.com/KimYongJ
 //https://www.acmicpc.net/problem/2933
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 class Node{int y, x; Node(int y, int x){this.y=y; this.x=x;}}
 
@@ -13,7 +10,14 @@ class Main{
 	static int	dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
 	static int	Y, X;
 	static char	map[][];
-	static boolean isContinue;
+	static boolean isContinue, visit[][];
+	static ArrayList<Node> list;
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
 	public static int findX(int y, boolean flag) {
 		int x = -1;
@@ -34,7 +38,7 @@ class Main{
 		}
 		return x;
 	}
-	public static void DFS(int y, int x, ArrayList<Node>list, boolean[][] visit) {
+	public static void DFS(int y, int x) {
 		for(int xy1[] : dxy)
 		{
 			int nextY = y + xy1[0];
@@ -46,37 +50,35 @@ class Main{
 				visit[nextY][nextX] = true;
 				if(nextY == Y - 1)
 					isContinue = true;
-				DFS(nextY, nextX, list, visit);
+				DFS(nextY, nextX);
 			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Y = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
-		map = new char[Y][X];
+		Y	= read();
+		X	= read();
+		map	= new char[Y][X];
 		
 		for(int y=0; y<Y; y++)
 		{
-			String str = br.readLine();
 			for(int x=0; x<X; x++)
-				map[y][x] = str.charAt(x);
+				map[y][x] = (char)System.in.read();
+			System.in.read();
 		}
 		boolean flag = true;// true : 왼쪽에서 오른쪽, false : 오른쪽에서 왼쪽
-		int T = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			int y = Y - Integer.parseInt(st.nextToken());	// 어느 위치에 창을 던지는지 체크
-			int x = findX(y, flag);// 어느 x좌표인지 체크
-			flag = !flag;
-			if(x == -1)// 미네랄이 없다면 스킵
+			int y	= Y - read();		// 어느 위치에 창을 던지는지 체크
+			int x	= findX(y, flag);	// 어느 x좌표인지 체크
+			flag	= !flag;
+			
+			if(x == -1)					// 미네랄이 없다면 스킵
 				continue;
 			
-			map[y][x] = '.';	// 해당 위치의 미네랄을 파괴
-			// 파괴된 위치를 기준으로 상하좌우 미네랄이 있는지 체크, 미네랄이 있다면 그 클러스터를 bfs로 탐색해 땅에 닿지 않은 것을 파악함
-			boolean visit[][] = new boolean[Y][X];
+			map[y][x] = '.';			// 해당 위치의 미네랄을 파괴
+			// 파괴된 위치를 기준으로 상하좌우 미네랄이 있는지 체크, 미네랄이 있다면 그 클러스터를 DFS로 탐색해 땅에 닿지 않은 것을 파악함
+			visit = new boolean[Y][X];
 			for(int xy[] : dxy)
 			{
 				int nextY = y + xy[0];
@@ -86,26 +88,28 @@ class Main{
 				{
 					if(nextY == Y - 1)	// 좌표가 땅바닥에 닿았으면 스킵
 						continue;
-					// 해당 좌표(nextY,nextX)를 시작으로 bfs를 돌면서 가장 낮은 Y 값이 땅에 닿아있는지 체크한다.
-					ArrayList<Node> list	= new ArrayList<>();	// 떨어뜨릴 클러스터 좌표들을 담음
-					isContinue				= false;
+					// 해당 좌표(nextY,nextX)를 시작으로 DFS를 돌면서 가장 낮은 Y 값이 땅에 닿아있는지 체크한다.
+					list		= new ArrayList<>();// 떨어뜨릴 클러스터 좌표들을 담음리스트
+					isContinue	= false;
 					
 					list.add(new Node(nextY, nextX));
 					visit[nextY][nextX] = true;
-					// 같은 묶음인 것들은 list에 담음
-					DFS(nextY, nextX, list, visit);
 					
-					// 땅에 닿지 않은 좌표를 찾을 때 해당 좌표들 중
+					DFS(nextY, nextX);// 같은 클러스터 들은 list에 담음
+					
+					// 땅에 닿지 않은 좌표를 찾을 때 해당 좌표들 중 x를만나기 까지 가장 짧은 길이를 찾음
 					if(!isContinue)
 					{
-						boolean check[] = new boolean[X];
 						Collections.sort(list, (a,b)->b.y - a.y);
-						// list의 좌표 중, 자기y좌표 + 1이 x가 아닌 좌표들 중 x를 만나기 까지 가장 짧은 이동거리
-						int minDist = 101;
+						
+						boolean check[] = new boolean[X];// 중복 탐색을 방지해야 하기 때문에 x좌표는 딱 한번만 탐색함
+						int minDist		= 101;// list의 좌표 중, 자기y좌표 + 1이 x가 아닌 좌표들 중 x를 만나기 까지 가장 짧은 이동거리
+						
 						for(Node now : list)
 						{
-							int nextY1 = now.y + 1;
-							int dist = 1;
+							int nextY1	= now.y + 1;
+							int dist	= 1;
+							
 							if(nextY1 < Y && map[nextY1][now.x] == '.')
 							{
 								if(check[now.x])
