de27e3f307fdb03519de7e07453e85737fe2ce28
YongJu Kim <kyjdummy@gmail.com>
2024-10-19 23:12:04 +0900

    [Silver I] Title: 튜터-튜티 관계의 수, Time: 260 ms, Memory: 31944 KB -BaekjoonHub

--- 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
+++ 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
@@ -1,21 +1,19 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/24542
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
 public class Main {
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int find(int node, int[] parent) {
 		if(parent[node] == node) return node;
 		return parent[node] = find(parent[node], parent);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N			= Integer.parseInt(st.nextToken());	// 교육생 수 N(2<=이십만)
-		int M			= Integer.parseInt(st.nextToken());	// 친분 관계의 수(1<=N-1)
+		int N			= read();	// 교육생 수 N(2<=이십만)
+		int M			= read();	// 친분 관계의 수(1<=N-1)
 		int parent[]	= new int[N+1];
 		
 		for(int i=1; i<=N; i++)
@@ -23,9 +21,8 @@ public class Main {
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			if(b < a)
 			{
 				int dummy = b;
