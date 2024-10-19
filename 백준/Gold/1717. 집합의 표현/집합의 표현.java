727571e8a180b4b85a6e654b96c61e15f074794e
YongJu Kim <kyjdummy@gmail.com>
2024-10-19 22:14:54 +0900

    [Gold V] Title: 집합의 표현, Time: 220 ms, Memory: 23536 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,32 +1,32 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/1717
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 public class Main {
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int find(int a, int[] parent) {
 		if(parent[a] == a) return a;
 		return parent[a] = find(parent[a], parent);
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		final String yes = "YES";
-		final String no = "NO";
-		int N = Integer.parseInt(st.nextToken());
-		int T = Integer.parseInt(st.nextToken());
-		int parent[] = new int[N+1];
+		StringBuilder sb	= new StringBuilder();
+		final String yes	= "YES";
+		final String no		= "NO";
+		
+		int N			= read();
+		int T			= read();
+		int parent[]	= new int[N+1];
 		
 		for(int i=1; i<=N; i++)
 			parent[i] = i;
 		
 		while(T-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int cmd = Integer.parseInt(st.nextToken());
-			int a	= Integer.parseInt(st.nextToken());
-			int b	= Integer.parseInt(st.nextToken());
+			int cmd = read();
+			int a	= read();
+			int b	= read();
 			if(b < a)
 			{
 				int dummy = b;
