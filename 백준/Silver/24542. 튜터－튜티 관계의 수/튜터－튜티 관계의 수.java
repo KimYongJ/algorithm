8ae9c11848f893f7d08543b34572f32164a95dad
YongJu Kim <kyjdummy@gmail.com>
2024-10-19 23:15:12 +0900

    [Silver I] Title: 튜터-튜티 관계의 수, Time: 228 ms, Memory: 19752 KB -BaekjoonHub

--- 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
+++ 백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.java
@@ -1,6 +1,5 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/24542
-import java.util.HashMap;
 public class Main {
 	static int read() throws Exception {// 빠른 입력을 위한 함수
 		int c, n = System.in.read() & 15;
@@ -12,12 +11,16 @@ public class Main {
 		return parent[node] = find(parent[node], parent);
 	}
 	public static void main(String[] args)throws Exception{
-		int N			= read();	// 교육생 수 N(2<=이십만)
-		int M			= read();	// 친분 관계의 수(1<=N-1)
+		int N			= read();		// 교육생 수 N(2<=이십만)
+		int M			= read();		// 친분 관계의 수(1<=N-1)
 		int parent[]	= new int[N+1];
+		int cnt[]		= new int[N+1];
 		
 		for(int i=1; i<=N; i++)
+		{
 			parent[i] = i;
+			cnt[i] = 1;
+		}
 		
 		while(M-->0)
 		{
@@ -34,17 +37,14 @@ public class Main {
 			int bParent = find(b, parent);
 			
 			parent[bParent] = aParent;
+			cnt[aParent] += cnt[bParent];
 		}
-		HashMap<Integer, Integer> hm = new HashMap<>();
-		for(int i=1; i<=N; i++)
-		{
-			int p = find(i, parent);
-			hm.put(p, hm.getOrDefault(p,0) + 1);
-		}
+
 		long res = 1;
-		for(int value : hm.values())
-			res = (res * value) % 1_000_000_007;
-		
+		for(int i=1; i<=N; i++)
+			if(parent[i] == i)
+				res = (res * cnt[i]) % 1_000_000_007;
+
 		System.out.print(res);
 	}
 }
