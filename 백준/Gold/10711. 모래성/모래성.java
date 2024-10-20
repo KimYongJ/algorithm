35ab55d6b7c838095162e9da097c9889f2ac421b
YongJu Kim <kyjdummy@gmail.com>
2024-10-20 21:14:57 +0900

    [Gold II] Title: 모래성, Time: 256 ms, Memory: 42412 KB -BaekjoonHub

--- 백준/Gold/10711. 모래성/모래성.java
+++ 백준/Gold/10711. 모래성/모래성.java
@@ -1,37 +1,37 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/10711
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 
-class Node{
-	int y, x; Node(int y, int x){this.y=y; this.x=x;}
-}
+class Node{int y, x; Node(int y, int x){this.y=y; this.x=x;}}
+
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		final int dxy[][]	= {{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
-		BufferedReader	br	= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st	= new StringTokenizer(br.readLine());
-		int Y				= Integer.parseInt(st.nextToken());
-		int X				= Integer.parseInt(st.nextToken());
+		int Y				= read();
+		int X				= read();
 		int map[][]			= new int[Y][X];
 		ArrayDeque<Node> q	= new ArrayDeque<>();
 		
 		for(int y=0; y<Y; y++)
 		{
-			String str = br.readLine();
 			for(int x=0; x<X; x++)
 			{
-				char c = str.charAt(x);
+				char c = (char)System.in.read();
 				if(c != '.')
 					map[y][x] = c - '0';
 				else 
 					q.add(new Node(y,x));
 			}
+			System.in.read();
 		}
+		
 		int T = -1;
+		
 		while(!q.isEmpty())
 		{
 			int size = q.size();
@@ -42,11 +42,9 @@ class Main{
 				{
 					int nextY = now.y + xy[0];
 					int nextX = now.x + xy[1];
-					if(0<=nextY && 0<=nextX && nextY<Y && nextX<X && map[nextY][nextX] != 0)
-					{
-						if(--map[nextY][nextX] == 0)
+					if(0<=nextY && 0<=nextX && nextY<Y && nextX<X 
+							&& map[nextY][nextX] != 0 && --map[nextY][nextX] == 0)
 							q.add(new Node(nextY, nextX));
-					}
 				}
 			}
 			T++;
