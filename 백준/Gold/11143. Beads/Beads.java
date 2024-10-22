48efac62694266929f9de36d2915c2665c20e205
YongJu Kim <kyjdummy@gmail.com>
2024-10-22 22:04:29 +0900

    [Gold I] Title: Beads, Time: 300 ms, Memory: 28076 KB -BaekjoonHub

--- 백준/Gold/11143. Beads/Beads.java
+++ 백준/Gold/11143. Beads/Beads.java
@@ -1,13 +1,14 @@
 //https://github.com/KimYongJ
 //https://www.acmicpc.net/problem/11143
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static long[] arr;
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
 	public static void update(int treeNode, int s, int e, int originIdx, int diff) {
 		if(originIdx < s || e < originIdx)
@@ -15,7 +16,8 @@ class Main{
 		
 		arr[treeNode] += diff;
 		
-		if(s!=e){
+		if(s != e)
+		{
 			int nextNode = treeNode << 1;
 			int mid = (s + e) >> 1;
 			update(nextNode, s, mid, originIdx, diff);
@@ -33,24 +35,21 @@ class Main{
 		return query(nextNode, s, mid, left, right) + query(nextNode | 1, mid + 1, e, left, right);
 	}
 	public static void main(String args[])throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+			int N = read();
+			int t = read() + read();
 			int H = (int)Math.ceil(Math.log(N) / Math.log(2)) + 1;
 			
 			arr = new long[(1 << H)];
-
+			
 			while(t-->0)
 			{
-				st = new StringTokenizer(br.readLine());
-				char c = st.nextToken().charAt(0);
-				int i = Integer.parseInt(st.nextToken());
-				int j = Integer.parseInt(st.nextToken());
+				char c = (char)System.in.read();
+				int i = read();
+				int j = read();
 				if(c=='P')
 					update(1, 1, N, i, j);
 				else
