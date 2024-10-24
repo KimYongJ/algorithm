ca73091f70f73269dff86d39ab856f135609d6bf
YongJu Kim <kyjdummy@gmail.com>
2024-10-24 21:09:34 +0900

    [Gold III] Title: Coding of Permutations, Time: 188 ms, Memory: 16836 KB -BaekjoonHub

--- 백준/Gold/8120. Coding of Permutations/Coding of Permutations.java
+++ 백준/Gold/8120. Coding of Permutations/Coding of Permutations.java
@@ -1,35 +1,44 @@
 //https://github.com/KimYongJ
 //https://www.acmicpc.net/problem/8120
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 class Main{
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		ArrayList<Integer> list = new ArrayList<>();
-		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N+1];
-		int res[] = new int[N+1];
+		
+		int N		= read();
+		int arr[]	= new int[N+1];
+		int res[]	= new int[N+1];
+		
 		for(int i=1; i<=N; i++)
 		{
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 			list.add(i);
 		}
 		
 		for(int i=N; i>=1; i--)
 		{
 			int idx = list.size() - arr[i] - 1;
-			if(0<=idx && idx < list.size()) {
+			if(0<=idx)
+			{
 				res[i] = list.get(idx);
 				list.remove(idx);
-			}else {
+			}
+			else
+			{
 				System.out.print("NIE");
 				return;
 			}
 		}
+		
 		for(int i=1; i<=N; i++)
 			sb.append(res[i]).append('\n');
 		
