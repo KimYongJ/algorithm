67e2b6129dc7668ab29bce8c51e392eb886a504e
YongJu Kim <kyjdummy@gmail.com>
2024-10-24 17:14:17 +0900

    [Gold II] Title: 전화 돌리기, Time: 252 ms, Memory: 27024 KB -BaekjoonHub

--- 백준/Gold/24526. 전화 돌리기/전화 돌리기.java
+++ 백준/Gold/24526. 전화 돌리기/전화 돌리기.java
@@ -1,26 +1,25 @@
 //https://github.com/KimYongJ
 //https://www.acmicpc.net/problem/24526
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 
 class Node{ int node; Node next; Node(int nd, Node nt){node=nd; next=nt;} }
 
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N			= Integer.parseInt(st.nextToken());
-		int T			= Integer.parseInt(st.nextToken());
+		int N			= read();
+		int T			= read();
 		int indg[]		= new int[N + 1];
 		Node adNode[]	= new Node[N + 1];
 		
 		while(T-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
+			int s = read();
+			int e = read();
 			adNode[e] = new Node(s, adNode[e]);
 			++indg[s];
 		}
