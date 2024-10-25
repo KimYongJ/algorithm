8aba38f6f611e0433f6b15e854906e03122e882f
YongJu Kim <kyjdummy@gmail.com>
2024-10-25 16:55:55 +0900

    [Gold II] Title: 수식 트리, Time: 356 ms, Memory: 39684 KB -BaekjoonHub

--- 백준/Gold/17501. 수식 트리/수식 트리.java
+++ 백준/Gold/17501. 수식 트리/수식 트리.java
@@ -1,16 +1,22 @@
 //https://github.com/KimYongJ
 //https://www.acmicpc.net/problem/17501
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main
 {
     static int N, Lidx, Ridx;
     static int[][] tree;
     static int[] elements;
-
+    
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
+    
     public static int DFS(int node, int flag) {
     	// 리프노드에 도착한 경우
     	if(node <= N)
@@ -28,26 +34,23 @@ public class Main
     	return leftValue + rightValue;
     }
     public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N			= Integer.parseInt(br.readLine());	// 노드의 개수
-        tree 		= new int[200_010][3];				// 트리표현, [0]:왼쪽자식노드 [1]:오른쪽자식노드 [2]:부호
-        elements	= new int[N];						// 각 노드당 들어있는 값
+        N			= read();				// 노드의 개수
+        tree 		= new int[200_010][3];	// 트리표현, [0]:왼쪽자식노드 [1]:오른쪽자식노드 [2]:부호
+        elements	= new int[N];			// 각 노드당 들어있는 값
         
         // 피연산자 입력
         for (int i = 1; i <= N; i++)
         {
-            elements[i-1]	= Integer.parseInt(br.readLine());
+            elements[i-1]	= read();
             tree[i][2]		= 1;
         }
         
         // 연산자와 연결 정보 입력
         for (int i = N + 1; i < N<<1; i++)
         {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	char c = st.nextToken().charAt(0);
-            tree[i][2] = (c == '-') ? -1 : 1;
-            tree[i][0] = Integer.parseInt(st.nextToken());
-            tree[i][1] = Integer.parseInt(st.nextToken());
+            tree[i][2] = (System.in.read() == '-') ? -1 : 1;
+            tree[i][0] = read();
+            tree[i][1] = read();
         }
         
         Arrays.sort(elements);
