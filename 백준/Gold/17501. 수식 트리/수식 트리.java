3483c209364e3e7f9d16b76828e1787553aa8189
YongJu Kim <kyjdummy@gmail.com>
2024-10-25 16:59:08 +0900

    [Gold II] Title: 수식 트리, Time: 332 ms, Memory: 38480 KB -BaekjoonHub

--- 백준/Gold/17501. 수식 트리/수식 트리.java
+++ 백준/Gold/17501. 수식 트리/수식 트리.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main
 {
-    static int N, Lidx, Ridx;
+    static int N, root, Lidx, Ridx;
     static int[][] tree;
     static int[] elements;
     
@@ -18,35 +18,30 @@ public class Main
     }
     
     public static int DFS(int node, int flag) {
-    	// 리프노드에 도착한 경우
-    	if(node <= N)
-    	{
-    		if(flag < 0)// 음수일 경우 작은 수 반환
-    			return elements[Lidx++];
-    		return elements[Ridx--];
-    	}
     	
+    	if(node <= N)// 리프노드에 도착한 경우
+    		return flag < 0 ? elements[Lidx++] : elements[Ridx--];// 음수일 경우 작은 수 반환
+
     	int leftValue = DFS(tree[node][0], flag);
     	int rightValue= DFS(tree[node][1], flag * tree[node][2]);
     	
-    	if(tree[node][2] < 0)// 음수일 경우
-    		return leftValue - rightValue;
-    	return leftValue + rightValue;
+    	// 후위 탐색(음수일 경우 마이너스 반환)
+    	return tree[node][2] < 0 ? leftValue - rightValue : leftValue + rightValue;
     }
     public static void main(String[] args) throws Exception {
         N			= read();				// 노드의 개수
         tree 		= new int[200_010][3];	// 트리표현, [0]:왼쪽자식노드 [1]:오른쪽자식노드 [2]:부호
         elements	= new int[N];			// 각 노드당 들어있는 값
-        
+        root		= (N<<1) - 1;			// 루트노드 번호
+        Ridx		= N - 1;
         // 피연산자 입력
         for (int i = 1; i <= N; i++)
         {
             elements[i-1]	= read();
             tree[i][2]		= 1;
         }
-        
         // 연산자와 연결 정보 입력
-        for (int i = N + 1; i < N<<1; i++)
+        for (int i = N + 1; i <= root; i++)
         {
             tree[i][2] = (System.in.read() == '-') ? -1 : 1;
             tree[i][0] = read();
@@ -55,9 +50,6 @@ public class Main
         
         Arrays.sort(elements);
         
-        Ridx = N - 1;
-        
-        int root = (N<<1) - 1;
         System.out.println( DFS(root, 1) );
     }
 }
