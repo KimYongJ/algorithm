af02ae0222fa5ba1934205785f10d2560ce357b6
YongJu Kim <kyjdummy@gmail.com>
2024-10-27 20:16:43 +0900

    [Gold III] Title: 육각 보드, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/12946. 육각 보드/육각 보드.java
+++ 백준/Gold/12946. 육각 보드/육각 보드.java
@@ -1,31 +1,35 @@
 //https://github.com/KimYongJ
 //https://www.acmicpc.net/problem/12946
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
 	
 	static final int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1},{1,-1},{-1,1}};
 	static int N, max;
 	static int[][] color, map;
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());
-		map		= new int[N][N];
-		color	= new int[N][N];
+		N		= read();
+		map		= new int[N+2][N+2];
+		color	= new int[N+2][N+2];
 
-		for(int y=0; y<N; y++)
+		for(int y=1; y<=N; y++)
 		{
-			String str = br.readLine();
-			for(int x=0; x<N; x++)
-				map[y][x] = str.charAt(x) == 'X' ? 1 : 0;
+			for(int x=1; x<=N; x++)
+				if(System.in.read() == 'X')
+					map[y][x] = 1;
+			System.in.read();
 		}
 		
-		for(int y=0; y<N && max != 3; y++)
-			for(int x=0; x<N && max != 3; x++)
-				if(map[y][x] == 1 && color[y][x] == 0)
+		for(int y=1; y<=N; y++)
+			for(int x=1; x<=N; x++)
+				if(max == 3)
+					break;
+				else if(map[y][x] == 1 && color[y][x] == 0)
 					DFS(y, x, 1);
 
 		System.out.print(max);
@@ -33,13 +37,14 @@ class Main{
 	public static void DFS(int y, int x, int col) {
 		if(max == 3)
 			return;
+		
 		max = Math.max(color[y][x] = col, max);
 		
 		for(int xy[] : dxy)
 		{
 			int nextY = xy[0] + y;
 			int nextX = xy[1] + x;
-			if(0<=nextY && 0<=nextX && nextY < N && nextX < N && map[nextY][nextX] == 1)
+			if(map[nextY][nextX] == 1)
 			{
 				if(color[nextY][nextX] == 0)
 					DFS(nextY, nextX, col == 1 ? 2 : 1);
