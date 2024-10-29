3427a841075bbd87534207dab0ba6dc085fe3b45
YongJu Kim <kyjdummy@gmail.com>
2024-10-29 21:48:32 +0900

    [Gold II] Title: Labyrinth, Time: 404 ms, Memory: 58592 KB -BaekjoonHub

--- 백준/Gold/3482. Labyrinth/Labyrinth.java
+++ 백준/Gold/3482. Labyrinth/Labyrinth.java
@@ -1,9 +1,6 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3482
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+// 가장 먼노드 응용 문제
 class Node{
 	int node; Node next;
 	Node(int node, Node next){this.node=node; this.next=next;}
@@ -15,51 +12,12 @@ class Main{
 	static int maxY, maxX, maxDist;
 	static boolean visit[][];
 	
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		while(T-->0)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			X		= Integer.parseInt(st.nextToken());
-			Y		= Integer.parseInt(st.nextToken());
-			maxY	= -1;
-			maxX	= -1;
-			maxDist = 0;
-			map		= new int[Y][X];
-			visit	= new boolean[Y][X];
-			
-			for(int y=0; y<Y; y++)
-			{
-				String str = br.readLine();
-				for(int x=0; x<X; x++)
-				{
-					if(str.charAt(x) == '#')
-						map[y][x] = 1;	// 1은 바위, 0은 갈 수 있는 곳
-				}
-			}
-			
-			for(int y=0; y<Y; y++) {
-				for(int x=0; x<X; x++) {
-					if(map[y][x] == 0 && !visit[y][x]) {
-						if(maxY == -1) {
-							maxY = y;
-							maxX = x;
-						}
-						visit[y][x] = true;
-						DFS(y, x, 0);
-					}
-				}
-			}
-			maxDist = 0;
-			visit	= new boolean[Y][X];
-			DFS(maxY, maxX, 0);
-			sb.append("Maximum rope length is ").append(maxDist).append(".\n");
-		}
-		System.out.print(sb.toString());
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
+	
 	public static void DFS(int y, int x, int dist) {
 		if(maxDist < dist)
 		{
@@ -75,5 +33,48 @@ class Main{
 				DFS(nextY, nextX, dist + 1);
 			}
 		}
+	}
+	public static void main(String[] args)throws Exception{
+		StringBuilder sb = new StringBuilder();
+		int T = read();
+		while(T-->0)
+		{
+			X		= read();
+			Y		= read();
+			maxY	= -1;
+			maxX	= -1;
+			maxDist = 0;
+			map		= new int[Y][X];
+			visit	= new boolean[Y][X];
+			
+			for(int y=0; y<Y; y++)
+			{
+				for(int x=0; x<X; x++)
+				{
+					if(System.in.read() == '#')
+						map[y][x] = 1;	// 1은 바위, 0은 갈 수 있는 곳
+				}
+				System.in.read();
+			}
+			
+			for(int y=0; y<Y; y++) 
+				for(int x=0; x<X; x++) 
+					if(map[y][x] == 0 && !visit[y][x])
+					{
+						if(maxY == -1)
+						{
+							maxY = y;
+							maxX = x;
+						}
+						visit[y][x] = true;
+						DFS(y, x, 0);
+					}
+			
+			maxDist = 0;
+			visit	= new boolean[Y][X];
+			DFS(maxY, maxX, 0);
+			sb.append("Maximum rope length is ").append(maxDist).append(".\n");
+		}
+		System.out.print(sb.toString());
 	}
 }
