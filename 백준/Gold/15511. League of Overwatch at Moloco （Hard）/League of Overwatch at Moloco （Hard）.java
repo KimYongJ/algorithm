597156bdc094444a81b9c57e0b4adb885c52aa5b
YongJu Kim <kyjdummy@gmail.com>
2024-10-29 13:04:09 +0900

    [Gold III] Title: League of Overwatch at Moloco (Hard), Time: 592 ms, Memory: 78488 KB -BaekjoonHub

--- 백준/Gold/15511. League of Overwatch at Moloco （Hard）/League of Overwatch at Moloco （Hard）.java
+++ 백준/Gold/15511. League of Overwatch at Moloco （Hard）/League of Overwatch at Moloco （Hard）.java
@@ -1,9 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/15511
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node; Node next;
 	Node(int node, Node next){this.node=node; this.next=next;}
@@ -13,42 +9,44 @@ class Main{
 	static Node adNode[];
 	static int color[];
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static boolean DFS(int node, int nowColor) {
 		color[node] = nowColor;
+		
 		int nextColor = nowColor == 1 ? 2 : 1;
-		for(Node next=adNode[node]; next!=null; next=next.next) {
-			if(color[next.node] == 0)
-			{
-				if(!DFS(next.node, nextColor))
-					return false;
-			}else if(color[next.node] == nowColor)
+		
+		for(Node next=adNode[node]; next!=null; next=next.next)
+			if(color[next.node] == 0 && !DFS(next.node, nextColor))
 				return false;
-		}
+			else if(color[next.node] == nowColor)
+				return false;
+
 		return true;
 	}
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N	= Integer.parseInt(st.nextToken());	// 직원 수(1<=백만)
-		int M	= Integer.parseInt(st.nextToken());	// 기존팀정보(1<=백만)
+		int N	= read();	// 직원 수(1<=백만)
+		int M	= read();	// 기존팀정보(1<=백만)
 		adNode	= new Node[N+1];
 		color	= new int[N+1];
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			adNode[a] = new Node(b, adNode[a]);
 			adNode[b] = new Node(a, adNode[b]);
 		}
 		for(int i=1; i<=N; i++)
-			if(color[i] == 0)
-				if( !DFS(i,1) )
-				{
-					System.out.print("IMPOSSIBLE");
-					return;
-				}
+			if(color[i] == 0 && !DFS(i,1))
+			{
+				System.out.print("IMPOSSIBLE");
+				return;
+			}
 		System.out.print("POSSIBLE");
 	}
 }
