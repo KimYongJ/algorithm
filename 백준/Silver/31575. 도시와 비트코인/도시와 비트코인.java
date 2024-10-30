cb38ecbf2b5a6522b6b7a8455ebb72d39f1c54f2
YongJu Kim <kyjdummy@gmail.com>
2024-10-30 14:47:40 +0900

    [Silver III] Title: 도시와 비트코인, Time: 148 ms, Memory: 16720 KB -BaekjoonHub

--- 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.java
+++ 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.java
@@ -1,44 +1,42 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/31575
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 class Node{
-	int y, x; Node(int y, int x){
-		this.y=y; this.x=x;
-	}
+	int y, x; Node(int y, int x){this.y=y; this.x=x;}
 }
 class Main{
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		final int dxy[][] = {{0,1},{1,0}};
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int X = Integer.parseInt(st.nextToken());
-		int Y = Integer.parseInt(st.nextToken());
+		int X		= read();
+		int Y		= read();
 		int map[][] = new int[Y + 2][X + 2];
 		
-		for(int y=1; y<=Y; y++) {
-			st = new StringTokenizer(br.readLine());
-			for(int x=1; x<=X; x++) {
-				map[y][x] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
+		for(int y=1; y<=Y; y++)
+			for(int x=1; x<=X; x++)
+				map[y][x] = read();
+
 		ArrayDeque<Node> q = new ArrayDeque<>();
 		q.add(new Node(1,1));
 		map[1][1] = 0;
 		while(!q.isEmpty())
 		{
 			Node now = q.poll();
-			if(now.y==Y && now.x==X) {
+			if(now.y==Y && now.x==X)
+			{
 				System.out.print("Yes");
 				return;
 			}
-			for(int xy[] : dxy) {
+			for(int xy[] : dxy)
+			{
 				int nextY = now.y + xy[0];
 				int nextX = now.x + xy[1];
-				if(map[nextY][nextX] == 1) {
+				if(map[nextY][nextX] == 1)
+				{
 					map[nextY][nextX] = 0;
 					q.add(new Node(nextY, nextX));
 				}
