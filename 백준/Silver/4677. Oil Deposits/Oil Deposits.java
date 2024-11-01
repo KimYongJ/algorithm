137c9010fc4bc522c65f2707451bc746448a0cec
YongJu Kim <kyjdummy@gmail.com>
2024-11-01 22:43:15 +0900

    [Silver II] Title: Oil Deposits, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Silver/4677. Oil Deposits/Oil Deposits.java
+++ 백준/Silver/4677. Oil Deposits/Oil Deposits.java
@@ -1,45 +1,49 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/4677
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
 	static int Y, X, cnt, map[][];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int y, int x) {
+		
 		map[y][x] = 0;
-		for(int xy[] : dxy) {
+		
+		for(int xy[] : dxy)
+		{
 			int nextY = y + xy[0];
 			int nextX = x + xy[1];
-			if(0<=nextY && 0<=nextX && nextY < Y && nextX < X && map[nextY][nextX] == 1) {
+			if(map[nextY][nextX] == 1)
 				DFS(nextY, nextX);
-			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			Y		= Integer.parseInt(st.nextToken());
-			X		= Integer.parseInt(st.nextToken());
+			Y		= read();
+			X		= read();
 			if(Y==0 || X == 0)
 				break;
-			map		= new int[Y][X];
+			map		= new int[Y+2][X+2];
 			cnt		= 0;
-			for(int y=0; y<Y; y++)
+			for(int y=1; y<=Y; y++)
 			{
-				String str = br.readLine();
-				for(int x=0; x<X; x++)
-					if(str.charAt(x) == '@')
+				for(int x=1; x<=X; x++)
+					if(System.in.read() == '@')
 						map[y][x] = 1;
+				System.in.read();
 			}
 			
-			for(int y=0; y<Y; y++)
-				for(int x=0; x<X; x++)
+			for(int y=1; y<=Y; y++)
+				for(int x=1; x<=X; x++)
 					if(map[y][x] == 1)
 					{
 						++cnt;
