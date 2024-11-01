376ea114e56a8d2852942efa34e598889aae8f2c
YongJu Kim <kyjdummy@gmail.com>
2024-11-01 11:11:51 +0900

    [Gold I] Title: 두 번째 트리의 지름, Time: 332 ms, Memory: 27640 KB -BaekjoonHub

--- 백준/Gold/19581. 두 번째 트리의 지름/두 번째 트리의 지름.java
+++ 백준/Gold/19581. 두 번째 트리의 지름/두 번째 트리의 지름.java
@@ -1,8 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/19581
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Node{
 	int node, dist; Node next;
 	Node(int n, int d, Node next){node=n; dist=d; this.next=next;}
@@ -12,7 +9,13 @@ class Main{
 	static Node adNode[];
 	static int maxNode, maxDist;
 	static boolean visit[];
-
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int node, int dist) {
 		if(maxDist < dist)
 		{
@@ -27,18 +30,16 @@ class Main{
 			}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N	= Integer.parseInt(br.readLine()); // 노드수(3<=십만)
+		int N	= read(); // 노드수(3<=십만)
 		adNode	= new Node[N + 1];
 		
 		for(int i=1; i<N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());// 가중치(1<=이만)
-			adNode[a] = new Node(b, d, adNode[a]);
-			adNode[b] = new Node(a, d, adNode[b]);
+			int a		= read();
+			int b		= read();
+			int d		= read();// 가중치(1<=이만)
+			adNode[a]	= new Node(b, d, adNode[a]);
+			adNode[b]	= new Node(a, d, adNode[b]);
 		}
 		int node[] = new int[2];	// 가장 먼 노드 2개를 담을 배열
 		int dist[] = new int[2];	// 두번째 먼 노드의 거리를 담을 배열
