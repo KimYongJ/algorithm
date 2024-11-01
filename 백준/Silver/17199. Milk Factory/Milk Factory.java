16a1fee0fe9d395a007d96de48bdeca0538fe733
YongJu Kim <kyjdummy@gmail.com>
2024-11-01 22:31:18 +0900

    [Silver I] Title: Milk Factory, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/17199. Milk Factory/Milk Factory.java
+++ 백준/Silver/17199. Milk Factory/Milk Factory.java
@@ -1,10 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17199
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{
 	int node; Node next;
 	Node(int node, Node next){this.node=node; this.next=next;}
@@ -15,23 +10,30 @@ class Main{
 	static Node adNode[];
 	static boolean visit[];
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N	= Integer.parseInt(br.readLine());
+		int N	= read();
 		adNode	= new Node[N + 1];
+		
 		for(int i=1; i<N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			adNode[b] = new Node(a, adNode[b]); // 간선을 반대로 받음 
 		}
 		
 		for(int i=1; i<=N; i++)
 		{
-			visit = new boolean[N+1];
-			cnt	= 0;
+			visit	= new boolean[N+1];
+			cnt		= 0;
+			
 			DFS(i);
+			
 			if(cnt == N-1)
 			{
 				System.out.print(i);
@@ -41,7 +43,7 @@ class Main{
 
 		System.out.print(-1);
 	}
-	public static void DFS(int now) {
+	public static void DFS(int now){
 		for(Node next=adNode[now]; next!=null; next=next.next)
 			if(!visit[next.node])
 			{
