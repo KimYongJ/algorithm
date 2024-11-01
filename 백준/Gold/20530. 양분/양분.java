848089ea0541fc29beacdbb01e025504315da616
YongJu Kim <kyjdummy@gmail.com>
2024-11-01 16:44:29 +0900

    [Gold I] Title: 양분, Time: 1300 ms, Memory: 49484 KB -BaekjoonHub

--- 백준/Gold/20530. 양분/양분.java
+++ 백준/Gold/20530. 양분/양분.java
@@ -1,12 +1,11 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/20530
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
 class Node{
 	int node; Node next;
 	Node(int node, Node next){this.node=node; this.next=next;}
 }
+
 class Main{
 	
 	static Node adNode[];
@@ -14,16 +13,23 @@ class Main{
 	static int[] parent, groupKey;
 	static boolean[] visit, isCycle;
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void check_cycle_DFS(int nowNode, int rootNode) {
 		isCycle[nowNode] = true;
 		if(nowNode != rootNode)
 			check_cycle_DFS(parent[nowNode],rootNode);
 	}
+	
 	public static void find_cycle_DFS(int nowNode, int beforeNode) {
+		visit[nowNode] = true;
 		for(Node next=adNode[nowNode]; next!=null && !findCycle; next=next.next)
 			if(!visit[next.node])
 			{
-				visit[next.node] = true;
 				parent[next.node] = nowNode; 
 				find_cycle_DFS(next.node, nowNode); 
 			}
@@ -32,21 +38,19 @@ class Main{
 				check_cycle_DFS(nowNode, next.node);
 			}
 	}
+	
 	public static void grouping_DFS(int nowNode, int key) {
+		visit[nowNode] = true;
+		groupKey[nowNode] = key;
 		for(Node next=adNode[nowNode]; next!=null; next=next.next)
 			if(!visit[next.node] && !isCycle[next.node])
-			{
-				visit[next.node]	= true;
-				groupKey[next.node] = key;
 				grouping_DFS(next.node, key);
-			}
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int N		= Integer.parseInt(st.nextToken());	// 노드수(1<=이십만)
-		int Q		= Integer.parseInt(st.nextToken());	// 쿼리수(1<=이십만)
+		int N		= read();	// 노드수(1<=이십만)
+		int Q		= read();	// 쿼리수(1<=이십만)
 		adNode		= new Node[N+1];
 		visit		= new boolean[N+1];
 		isCycle		= new boolean[N+1];
@@ -56,32 +60,22 @@ class Main{
 		
 		for(int i=1; i<=N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			adNode[a] = new Node(b, adNode[a]);
 			adNode[b] = new Node(a, adNode[b]);
 		}
 		
-		visit[1] = true;
 		find_cycle_DFS(1,0);
 		
 		visit = new boolean[N+1];
+		
 		for(int i=1; i<=N; i++)
 			if(isCycle[i])
-			{
-				groupKey[i] = i;
-				visit[i] = true;
 				grouping_DFS(i, i);
-			}
 		
 		while(Q-->0)
-		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			sb.append(groupKey[a] == groupKey[b] ? 1 : 2).append('\n');
-		}
+			sb.append(groupKey[read()] == groupKey[read()] ? 1 : 2).append('\n');
 		
 		System.out.print(sb.toString());
 	}
