9258dbc294fb96f1458db80c96a0ee1580357cb9
YongJu Kim <kyjdummy@gmail.com>
2024-11-03 20:05:18 +0900

    [Platinum V] Title: 즉흥 여행 (Easy), Time: 880 ms, Memory: 41324 KB -BaekjoonHub

--- 백준/Platinum/26146. 즉흥 여행 （Easy）/즉흥 여행 （Easy）.java
+++ 백준/Platinum/26146. 즉흥 여행 （Easy）/즉흥 여행 （Easy）.java
@@ -1,9 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/26146
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Node{
 	int node; Node next;
 	Node(int node, Node next){this.node=node; this.next=next;}
@@ -13,7 +9,11 @@ class Main{
 	static int N, M;
 	static Node[] forward, reverse;
 	static boolean visit[];
-	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static int DFS(int node, Node[] adNode) {
 		visit[node] = true;
 		int cnt = 1;
@@ -23,18 +23,15 @@ class Main{
 		return cnt;
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());	// 노드 개수(1<=이십만)
-		M		= Integer.parseInt(st.nextToken());	// 항공편 개수(0<=오십만)
+		N		= read();	// 노드 개수(1<=이십만)
+		M		= read();	// 항공편 개수(0<=오십만)
 		forward	= new Node[N+1];
 		reverse	= new Node[N+1];
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			forward[a] = new Node(b, forward[a]);
 			reverse[b] = new Node(a, reverse[b]);
 		}
