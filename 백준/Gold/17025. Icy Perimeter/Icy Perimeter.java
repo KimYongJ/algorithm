fdd988e5ae2794e3a0ac60ce6d0e0e4bb1e41643
YongJu Kim <kyjdummy@gmail.com>
2024-11-03 17:25:07 +0900

    [Gold V] Title: Icy Perimeter, Time: 388 ms, Memory: 79536 KB -BaekjoonHub

--- 백준/Gold/17025. Icy Perimeter/Icy Perimeter.java
+++ 백준/Gold/17025. Icy Perimeter/Icy Perimeter.java
@@ -3,54 +3,61 @@
 // 가장큰 덩어리의 #수, #의 둘레
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+class Node{
+	int y, x;
+	Node(int y, int x){this.y=y; this.x=x;}
+}
 class Main{
 	
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int N, map[][];
+	static int N;
 	static int maxArea, minPerimeter;
 	static int area, perimeter;
-	static boolean visit[][];
-	
-	public static void DFS(int y, int x) {
-		visit[y][x] = true;
-		++area;
-		// 둘레 계산
-		for(int xy[] : dxy)
-		{
-			int nextY = y + xy[0];
-			int nextX = x + xy[1];
-			if(map[nextY][nextX] != 1)
-				++perimeter;
-		}
-		for(int xy[] : dxy)
-		{
-			int nextY = y + xy[0];
-			int nextX = x + xy[1];
-			if(map[nextY][nextX] == 1 && !visit[nextY][nextX])
-				DFS(nextY, nextX);
+	static boolean[][] map, visit;
+
+	public static void BFS(int y, int x) {
+		ArrayDeque<Node> q = new ArrayDeque<>();
+		q.add(new Node(y, x));
+		while(!q.isEmpty()) {
+			Node now = q.poll();
+			if(visit[now.y][now.x])
+				continue;
+			
+			visit[now.y][now.x] = true;
+			++area;
+			
+			for(int xy[] : dxy)
+			{
+				int nextY = now.y + xy[0];
+				int nextX = now.x + xy[1];
+				if(!map[nextY][nextX])
+					++perimeter;
+				else if(map[nextY][nextX] && !visit[nextY][nextX])
+					q.add(new Node(nextY, nextX));
+			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N		= Integer.parseInt(br.readLine());
-		map		= new int[N+2][N+2];
+		map		= new boolean[N+2][N+2];
 		minPerimeter = Integer.MAX_VALUE;
 		visit = new boolean[N+2][N+2];
 		for(int y=1; y<=N; y++)
 		{
 			String str = br.readLine();
 			for(int x=1; x<=N; x++)
-				if(str.charAt(x-1)=='#')
-					map[y][x] = 1;
+				map[y][x] = str.charAt(x-1)=='#';
 		}
 		
 		for(int y=1; y<=N; y++)
 			for(int x=1; x<=N; x++)
-				if(map[y][x] == 1 && !visit[y][x])
+				if(map[y][x] && !visit[y][x])
 				{
 					area = 0;
 					perimeter = 0;
-					DFS(y, x);
+					BFS(y, x);
 					if(maxArea < area)
 					{
 						maxArea	= area;
