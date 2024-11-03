74739fca3a8ce3f3fa98680f9775f70f8f691f8f
YongJu Kim <kyjdummy@gmail.com>
2024-11-03 22:21:55 +0900

    [Silver III] Title: Daisy Chains in the Field, Time: 116 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/5938. Daisy Chains in the Field/Daisy Chains in the Field.java
+++ 백준/Silver/5938. Daisy Chains in the Field/Daisy Chains in the Field.java
@@ -1,12 +1,7 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/5938
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Node{
-	int node; Node next;
-	Node(int node, Node next){this.node=node; this.next=next;}
+	int node; Node next;Node(int node, Node next){this.node=node; this.next=next;}
 }
 class Main{
 	
@@ -14,32 +9,38 @@ class Main{
 	static Node adNode[];
 	static boolean visit[];
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int node) {
 		visit[node] = true;
 		for(Node next=adNode[node]; next!=null; next=next.next)
 			if(!visit[next.node])
 				DFS(next.node);
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());
-		M		= Integer.parseInt(st.nextToken());
+		N		= read();
+		M		= read();
 		adNode	= new Node[N+1];
 		visit	= new boolean[N+1];
+		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adNode[a] = new Node(b, adNode[a]);
-			adNode[b] = new Node(a, adNode[b]);
+			int a		= read();
+			int b		= read();
+			adNode[a]	= new Node(b, adNode[a]);
+			adNode[b]	= new Node(a, adNode[b]);
 		}
 		
 		DFS(1);
 		
 		StringBuilder sb = new StringBuilder();
 		boolean flag = false;
+		
 		for(int i=1; i<=N; i++)
 			if(!visit[i])
 			{
