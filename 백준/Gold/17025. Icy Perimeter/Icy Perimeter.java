a5a18266e2bccbc6f9980a45cc08d014e4971c5f
YongJu Kim <kyjdummy@gmail.com>
2024-11-03 17:27:21 +0900

    [Gold V] Title: Icy Perimeter, Time: 252 ms, Memory: 72532 KB -BaekjoonHub

--- 백준/Gold/17025. Icy Perimeter/Icy Perimeter.java
+++ 백준/Gold/17025. Icy Perimeter/Icy Perimeter.java
@@ -1,21 +1,21 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17025
-// 가장큰 덩어리의 #수, #의 둘레
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 class Node{
-	int y, x;
-	Node(int y, int x){this.y=y; this.x=x;}
+	int y, x;Node(int y, int x){this.y=y; this.x=x;}
 }
 class Main{
 	
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int N;
-	static int maxArea, minPerimeter;
-	static int area, perimeter;
+	static int N, area, maxArea, perimeter, minPerimeter;
 	static boolean[][] map, visit;
 
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void BFS(int y, int x) {
 		ArrayDeque<Node> q = new ArrayDeque<>();
 		q.add(new Node(y, x));
@@ -38,30 +38,31 @@ class Main{
 			}
 		}
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());
-		map		= new boolean[N+2][N+2];
-		minPerimeter = Integer.MAX_VALUE;
-		visit = new boolean[N+2][N+2];
+		N				= read();
+		map				= new boolean[N+2][N+2];
+		visit			= new boolean[N+2][N+2];
+		minPerimeter	= Integer.MAX_VALUE;
+		
 		for(int y=1; y<=N; y++)
 		{
-			String str = br.readLine();
 			for(int x=1; x<=N; x++)
-				map[y][x] = str.charAt(x-1)=='#';
+				map[y][x] = System.in.read()=='#';
+			System.in.read();
 		}
 		
 		for(int y=1; y<=N; y++)
 			for(int x=1; x<=N; x++)
 				if(map[y][x] && !visit[y][x])
 				{
-					area = 0;
-					perimeter = 0;
+					area		= 0;
+					perimeter	= 0;
 					BFS(y, x);
 					if(maxArea < area)
 					{
-						maxArea	= area;
-						minPerimeter = perimeter;
+						maxArea		= area;
+						minPerimeter= perimeter;
 					}
 					else if(maxArea == area && perimeter < minPerimeter)
 						minPerimeter = perimeter;
