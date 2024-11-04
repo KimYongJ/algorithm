e3763545e740f32f838c228d83d92e6f20ae4cc4
YongJu Kim <kyjdummy@gmail.com>
2024-11-04 14:23:51 +0900

    [Gold I] Title: 사자와 토끼, Time: 384 ms, Memory: 44628 KB -BaekjoonHub

--- 백준/Gold/17834. 사자와 토끼/사자와 토끼.java
+++ 백준/Gold/17834. 사자와 토끼/사자와 토끼.java
@@ -1,8 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17834
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Node{
 	int node; Node next;
 	Node(int node, Node next){this.node=node; this.next=next;}
@@ -11,32 +8,38 @@ class Main{
 	
 	static int[] colorCnt, color;
 	static Node adNode[];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int node, int col) {
 		++colorCnt[col];
 		color[node] = col;
-		for(Node next=adNode[node]; next!=null; next=next.next) {
-			if(color[next.node] == 0) {
+		for(Node next=adNode[node]; next!=null; next=next.next)
+		{
+			if(color[next.node] == 0)
 				DFS(next.node, col == 1 ? 2 : 1);
-			}else if(color[next.node] == col) {
+			else if(color[next.node] == col)
+			{
 				System.out.print(0);
 				System.exit(0);
 			}
 		}
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N	= Integer.parseInt(st.nextToken());	// 노드 수 (2 ≤ N ≤ 50,000)
-		int M	= Integer.parseInt(st.nextToken());	// 간선 수 (1 ≤ M ≤ 500,000)
+		int N	= read();	// 노드 수 (2 ≤ N ≤ 50,000)
+		int M	= read();	// 간선 수 (1 ≤ M ≤ 500,000)
 		adNode	= new Node[N+1];
 		color	= new int[N+1];
 		colorCnt= new int[3];
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = read();
+			int b = read();
 			adNode[a] = new Node(b, adNode[a]);
 			adNode[b] = new Node(a, adNode[b]);			
 		}
