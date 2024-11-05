aa9717a84c8809ab0b984092c40f441f27da77d4
YongJu Kim <kyjdummy@gmail.com>
2024-11-05 13:12:26 +0900

    [Gold IV] Title: 북쪽나라의 도로, Time: 152 ms, Memory: 16652 KB -BaekjoonHub

---
+++ 백준/Gold/1595. 북쪽나라의 도로/북쪽나라의 도로.java
@@ -0,0 +1,51 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1595
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Node{
+	int node, dist; Node next;
+	Node(int n, int d, Node nt){node=n; dist=d; next=nt;}
+}
+class Main{
+	
+	static final int MAX = 10_001;
+	static Node adNode[];
+	static int maxNode;
+	static long maxDist;
+	static boolean visit[];
+	public static void DFS(int node, long dist) {
+		if(maxDist < dist) {
+			maxDist = dist;
+			maxNode = node;
+		}
+		visit[node] = true;
+		for(Node next=adNode[node]; next!=null; next=next.next)
+			if(!visit[next.node])
+				DFS(next.node, dist + next.dist);
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		adNode = new Node[MAX];
+		while(true)
+		{
+			String str = br.readLine();
+			if(str == null || "".equals(str))
+				break;
+			StringTokenizer st = new StringTokenizer(str);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			adNode[a] = new Node(b,d,adNode[a]);
+			adNode[b] = new Node(a,d,adNode[b]);
+		}
+		visit = new boolean[MAX];
+		DFS(1, 0);
+		
+		maxDist = 0;
+		visit = new boolean[MAX];
+		DFS(maxNode, 0);
+		
+		System.out.print(maxDist);
+	}
+}
