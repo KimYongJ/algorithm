b2b535a688814a645c7fd0116c50aa27d8da9f2d
YongJu Kim <kyjdummy@gmail.com>
2024-11-05 21:59:52 +0900

    [Silver II] Title: Sheba’s Amoebas, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/16390. Sheba’s Amoebas/Sheba’s Amoebas.java
+++ 백준/Silver/16390. Sheba’s Amoebas/Sheba’s Amoebas.java
@@ -1,14 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16390
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1},{-1,1},{-1,-1},{1,-1},{1,1}};
 	static int Y, X, map[][];
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int y, int x) {
 		map[y][x] = 0;
 		for(int xy[] : dxy) {
@@ -18,19 +20,18 @@ class Main{
 				DFS(nextY, nextX);
 		}
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Y = Integer.parseInt(st.nextToken());
-		X = Integer.parseInt(st.nextToken());
+		Y	= read();
+		X	= read();
 		map = new int[Y][X];
 		
 		for(int y=0; y<Y; y++)
 		{
-			String str = br.readLine();
 			for(int x=0; x<X; x++)
-				if(str.charAt(x) == '#')
+				if(System.in.read() == '#')
 					map[y][x] = 1;
+			System.in.read();
 		}
 		
 		int cnt = 0;
