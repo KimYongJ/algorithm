4daa799e4a6474da8407e9e624d1dd39b743f766
YongJu Kim <kyjdummy@gmail.com>
2024-11-06 20:48:22 +0900

    [Gold I] Title: 망가진 나무, Time: 1036 ms, Memory: 51084 KB -BaekjoonHub

--- 백준/Gold/24232. 망가진 나무/망가진 나무.java
+++ 백준/Gold/24232. 망가진 나무/망가진 나무.java
@@ -1,74 +1,81 @@
-import java.util.*;
-
-public class Main {
-    static final int INF = Integer.MAX_VALUE; // 정수 최대값을 사용하여 무한대를 표현합니다.
-
-    static class Node {
-        int vertex; // 연결된 정점
-        boolean flag; // 엣지의 방향성 플래그
-        int idx; // 엣지의 인덱스
-
-        public Node(int vertex, boolean flag, int idx) {
-            this.vertex = vertex;
-            this.flag = flag;
-            this.idx = idx;
-        }
-    }
-
-    static int N; // 정점의 개수
-    static List<List<Node>> edge = new ArrayList<>(); // 각 정점에서 연결된 엣지 리스트
-    static boolean[] flip; // 각 엣지를 뒤집어야 하는지 여부
-    static boolean[] ans; // 결과를 저장하는 배열
-    static int minFlip = INF; // 최소 뒤집기 횟수
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        flip = new boolean[N - 1];
-        ans = new boolean[N - 1];
-        for (int i = 0; i <= N; i++) {
-            edge.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < N - 1; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            edge.get(u).add(new Node(v, false, i)); // u에서 v로 가는 엣지, 초기 방향은 false
-            edge.get(v).add(new Node(u, true, i)); // v에서 u로 가는 엣지, 반대 방향은 true
-        }
-
-        int flipCount = init(1, 0); // 초기화 함수를 호출하여 뒤집힌 엣지 수를 계산
-        dfs(1, 0, flipCount); // 깊이 우선 탐색을 시작
-
-        for (int i = 0; i < N - 1; i++) {
-            System.out.print(ans[i] ? 1 : 0); // 결과 출력
-        }
-        sc.close();
-    }
-
-    static int init(int node, int prev) {
-        int sum = 0;
-        for (Node n : edge.get(node)) {
-            if (n.vertex == prev) continue; // 이전 정점을 다시 방문하지 않도록 예외 처리
-            flip[n.idx] = n.flag; // 엣지의 초기 방향 설정
-            sum += init(n.vertex, node) + (n.flag ? 1 : 0); // 재귀적으로 탐색하면서 뒤집힌 엣지 수를 누적
-        }
-        return sum;
-    }
-
-    static void dfs(int node, int prev, int flipCount) {
-        if (minFlip > flipCount) { // 현재 뒤집기 횟수가 이전 최소값보다 작은 경우 갱신
-            minFlip = flipCount;
-            for (int i = 0; i < N - 1; i++) {
-                ans[i] = flip[i]; // 결과 배열을 현재 상태로 업데이트
-            }
-        }
-        for (Node n : edge.get(node)) {
-            if (n.vertex == prev) continue; // 이전 정점 방문 방지
-            boolean currentFlag = flip[n.idx];
-            flip[n.idx] = !currentFlag; // 현재 엣지의 방향을 뒤집음
-            dfs(n.vertex, node, flipCount + (flip[n.idx] ? 1 : -1)); // 재귀적으로 깊이 우선 탐색
-            flip[n.idx] = currentFlag; // 방향을 원래대로 복구
-        }
-    }
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/24232
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Node{
+	int node, idx, isReverse; // 정방향 : 0 / 역방향 : 1
+	Node next;
+	Node(int node, int idx, int isReverse, Node next){
+		this.node=node; this.idx=idx; this.isReverse=isReverse;
+		this.next=next;
+	}
+}
+class Main{
+	
+	static int N;
+	static int reverseCnt[];		// 각 노드당 역방향의 개수를 카운팅할 배열
+	static int isReverse[];			// 간선을 최종적으로 뒤집을지 말지 결정하는 배열
+	static Node adNode[];
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N			= Integer.parseInt(br.readLine());
+		adNode		= new Node[N+1];
+		isReverse	= new int[N-1];
+		reverseCnt	= new int[N+1];
+		
+		for(int i=0; i<N-1; i++)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adNode[a] = new Node(b, i, 0, adNode[a]);
+			adNode[b] = new Node(a, i, 1, adNode[b]);
+		}
+		// 1번 노드의 역방향 개수를 담는다.
+		reverseCnt[1] = reverse_edge_DFS(1, 0);
+		// 1번노드의 역방향 개수를 활용해 모든 노드의 역방향 개수를 구한다(내가갈때 정방향이면 상대 노드는 역방향간선이 + 1, 내가 갈 때 역방향이면 상대 노드는 역방향 간선이 -1이된다)
+		all_reverse_edge_DFS(1, 0, reverseCnt[1]);
+		
+		// 역방향 개수가 가장 작은 노드를 찾는다.
+		int minNode = 0;
+		int minReverseEdge = 1<<30;
+		for(int i=1; i<=N; i++)
+			if(reverseCnt[i] < minReverseEdge)
+			{
+				minReverseEdge = reverseCnt[i];
+				minNode = i;
+			}
+		
+		// 역방향 개수가 가장 작은 노드를 DFS로 탐색하며 결과를 담는다.
+		find_result_DFS(minNode, 0);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i : isReverse)
+			sb.append(i);
+		System.out.print(sb.toString());
+	}
+	public static void all_reverse_edge_DFS(int node, int prev, int cnt) {
+		for(Node next=adNode[node]; next!=null; next=next.next)
+			if(next.node != prev)
+			{
+				reverseCnt[next.node] = reverseCnt[node] + (next.isReverse == 0 ? 1 : -1);
+				all_reverse_edge_DFS(next.node, node, reverseCnt[next.node]);
+			}
+	}
+	public static int reverse_edge_DFS(int node, int prev) {
+		int cnt = 0;
+		for(Node next=adNode[node]; next!=null; next=next.next)
+			if(next.node != prev)
+				cnt += reverse_edge_DFS(next.node, node) + next.isReverse;
+		return cnt;
+	}
+	public static void find_result_DFS(int node, int prev) {
+		for(Node next=adNode[node]; next!=null; next=next.next)
+			if(next.node != prev)
+			{
+				isReverse[next.idx] = next.isReverse;	// 역방향이면 next.isReverse가 1이된다. 해당 간선이 역방향이라면 자연스럽게 1이담기게됨
+				find_result_DFS(next.node, node);
+			}
+	}
 }
