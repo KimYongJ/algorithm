ccc66b2db0123cc37f30e705e11727dd029fcfd1
YongJu Kim <kyjdummy@gmail.com>
2024-11-06 20:55:54 +0900

    [Gold I] Title: 망가진 나무, Time: 412 ms, Memory: 30880 KB -BaekjoonHub

--- 백준/Gold/24232. 망가진 나무/망가진 나무.java
+++ 백준/Gold/24232. 망가진 나무/망가진 나무.java
@@ -1,8 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/24232
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Node{
 	int node, idx, isReverse; // 정방향 : 0 / 역방향 : 1
 	Node next;
@@ -16,31 +13,29 @@ class Main{
 	static int N;
 	static int reverseCnt[];		// 각 노드당 역방향의 개수를 카운팅할 배열
 	static int isReverse[];			// 간선을 최종적으로 뒤집을지 말지 결정하는 배열
-	static Node adNode[];
-	static boolean visit[];
+	static Node adNode[];			// 인접노드를 담을 배열
+	static boolean visit[];			// DFS마다 방문 체크
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N			= Integer.parseInt(br.readLine());
+		N			= read();
 		adNode		= new Node[N+1];
 		isReverse	= new int[N-1];
 		reverseCnt	= new int[N+1];
+		visit		= new boolean[N+1];
 		
 		for(int i=0; i<N-1; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			adNode[a] = new Node(b, i, 0, adNode[a]);
-			adNode[b] = new Node(a, i, 1, adNode[b]);
+			int a		= read();
+			int b		= read();
+			adNode[a]	= new Node(b, i, 0, adNode[a]);
+			adNode[b]	= new Node(a, i, 1, adNode[b]);
 		}
 		// 1번 노드의 역방향 개수를 담는다.
-		visit = new boolean[N+1];
 		visit[1] = true;
 		reverseCnt[1] = reverse_edge_DFS(1);
 		
 		// 1번노드의 역방향 개수를 활용해 모든 노드의 역방향 개수를 구한다(내가갈때 정방향이면 상대 노드는 역방향간선이 + 1, 내가 갈 때 역방향이면 상대 노드는 역방향 간선이 -1이된다)
-		visit = new boolean[N+1];
-		visit[1] = true;
+		visit[1] = false;	// 이미 다 true기 때문에 false로함
 		all_reverse_edge_DFS(1, reverseCnt[1]);
 		
 		// 역방향 개수가 가장 작은 노드를 찾는다.
@@ -54,7 +49,6 @@ class Main{
 			}
 		
 		// 역방향 개수가 가장 작은 노드를 DFS로 탐색하며 결과를 담는다.
-		visit = new boolean[N+1];
 		visit[minNode] = true;
 		find_result_DFS(minNode);
 		
@@ -75,9 +69,9 @@ class Main{
 	}
 	public static void all_reverse_edge_DFS(int node, int cnt) {
 		for(Node next=adNode[node]; next!=null; next=next.next)
-			if(!visit[next.node])
+			if(visit[next.node])
 			{
-				visit[next.node] = true;
+				visit[next.node] = false;// 첫번째 DFS에서 이미 다 true기 때문에 false로함
 				reverseCnt[next.node] = reverseCnt[node] + (next.isReverse == 0 ? 1 : -1);
 				all_reverse_edge_DFS(next.node, reverseCnt[next.node]);
 			}
@@ -90,5 +84,10 @@ class Main{
 				isReverse[next.idx] = next.isReverse;	// 역방향이면 next.isReverse가 1이된다. 해당 간선이 역방향이라면 자연스럽게 1이담기게됨
 				find_result_DFS(next.node);
 			}
+	}
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
