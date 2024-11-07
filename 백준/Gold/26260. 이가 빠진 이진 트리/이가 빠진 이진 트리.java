a677e25d0daf9fdc8d387473e3d77110b8d3210a
YongJu Kim <kyjdummy@gmail.com>
2024-11-07 13:16:39 +0900

    [Gold V] Title: 이가 빠진 이진 트리, Time: 360 ms, Memory: 28008 KB -BaekjoonHub

--- 백준/Gold/26260. 이가 빠진 이진 트리/이가 빠진 이진 트리.java
+++ 백준/Gold/26260. 이가 빠진 이진 트리/이가 빠진 이진 트리.java
@@ -1,41 +1,44 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/26260
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Main{
 	
 	static int N, idx, map[];
 	static StringBuilder sb = new StringBuilder();
 	
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
+	
 	public static void postOrderDFS(int start, int end) {
-		if(start==end)
+		if(start == end)	// 리프노드에 닿으면 결과를 출력한다.
 		{
 			sb.append(map[start]).append(' ');
 			return;
 		}
+		int mid = (start + end) >> 1;	// 왼쪽 오른쪽을 나눌 기준이 되는 노드
 		
-		int mid = (start + end) >> 1;
+		postOrderDFS(start, mid -1);	// 왼쪽 서브트리를 먼저 탐색
+		postOrderDFS(mid + 1, end);		// 오른쪽 서브트리를 그 다음 탐색
 		
-		postOrderDFS(start, mid - 1);
-		postOrderDFS(mid + 1, end);
-		
-		sb.append(map[mid]).append(' ');
+		sb.append(map[mid]).append(' ');// 자기자신을 넣음(리프노드가 넣어진 후 자기자신을 넣기 때문에 후위순회임.
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N	= Integer.parseInt(br.readLine());
+		N	= read();
 		map = new int[N];
 		idx = 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			if((map[i] = Integer.parseInt(st.nextToken()))==-1)
+			if((map[i] = read())==-1)
 				idx = i;
 
-		map[idx] = Integer.parseInt(br.readLine());
+		map[idx] = read();
 		
 		Arrays.sort(map);
 		
