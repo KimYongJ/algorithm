11a2e929329761cb5d8a4a9ffad1ed5fdbbc4a59
YongJu Kim <kyjdummy@gmail.com>
2024-11-08 22:46:48 +0900

    [Silver IV] Title: Bookshelf 2, Time: 108 ms, Memory: 13964 KB -BaekjoonHub

--- 백준/Silver/6148. Bookshelf 2/Bookshelf 2.java
+++ 백준/Silver/6148. Bookshelf 2/Bookshelf 2.java
@@ -1,14 +1,18 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/6148
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N, H, min, arr[];
 	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void DFS(int depth, int sum) {
-		if(H <= sum) {
+		if(H <= sum)
+		{
 			min = Math.min(min, sum-H);
 			return;
 		}
@@ -17,16 +21,15 @@ class Main{
 		DFS(depth+1, sum + arr[depth]);
 		DFS(depth+1, sum);
 	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());
-		H		= Integer.parseInt(st.nextToken());
+		N		= read();
+		H		= read();
 		arr		= new int[N];
 		min		= 1<<30;
 		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 		
 		DFS(0, 0);
 		
