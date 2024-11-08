17e86a4416bdeffe416549b61e073a84c8034422
YongJu Kim <kyjdummy@gmail.com>
2024-11-08 22:45:31 +0900

    [Silver IV] Title: Bookshelf 2, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/6148. Bookshelf 2/Bookshelf 2.java
+++ 백준/Silver/6148. Bookshelf 2/Bookshelf 2.java
@@ -6,20 +6,16 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int N, H, min, arr[];
-	static boolean visit[];
-	public static void DFS(int idx, int depth, int sum) {
-		if(H <= sum)
+	
+	public static void DFS(int depth, int sum) {
+		if(H <= sum) {
 			min = Math.min(min, sum-H);
+			return;
+		}
 		if(depth == N)
 			return;
-		
-		for(int i=idx; i<N; i++)
-			if(!visit[i])
-			{
-				visit[i] = true;
-				DFS(i+1,depth + 1, sum + arr[i]);
-				visit[i] = false;
-			}
+		DFS(depth+1, sum + arr[depth]);
+		DFS(depth+1, sum);
 	}
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,13 +23,12 @@ class Main{
 		N		= Integer.parseInt(st.nextToken());
 		H		= Integer.parseInt(st.nextToken());
 		arr		= new int[N];
-		visit	= new boolean[N];
 		min		= 1<<30;
 		
 		for(int i=0; i<N; i++)
 			arr[i] = Integer.parseInt(br.readLine());
 		
-		DFS(0, 0, 0);
+		DFS(0, 0);
 		
 		System.out.print(min);
 	}
