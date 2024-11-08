ccbcae48af10fafe8ce356f031e6920899b0ef97
YongJu Kim <kyjdummy@gmail.com>
2024-11-08 14:49:22 +0900

    [Platinum V] Title: 전개도, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Platinum/2642. 전개도/전개도.java
+++ 백준/Platinum/2642. 전개도/전개도.java
@@ -1,8 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2642
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Pos{
 	int y, x, num;
 	Pos(int y, int x, int n){this.y=y; this.x=x; num=n;}
@@ -16,23 +13,18 @@ class Main{
 	static boolean visit[][];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		map 	= new int[8][8];
 		face	= new int[7];	// 자기와 마주보는 값
 		pos		= new Pos[6];	// 전개도의 1~6까지 숫자의 위치를 담을 배열
-		
-		for(int y=1; y<=6; y++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int x=1; x<=6; x++)
-			{
-				map[y][x] = Integer.parseInt(st.nextToken());
-				if(map[y][x] != 0)
-					pos[posIdx++] = new Pos(y,x, map[y][x]);
-			}
-		}
 
-		if(posIdx < 6)
+		// 입력된 숫자가 0 이상인 경우 pos배열에 담음
+		for(int y=1; y<=6; y++)
+			for(int x=1; x<=6; x++)
+				if((map[y][x] = read()) != 0)
+					pos[posIdx++] = new Pos(y,x, map[y][x]);
+
+		// 0이상의 숫자가 6개가 아닌 경우 0 출력
+		if(posIdx != 6)
 		{
 			System.out.print(0);
 			return;
@@ -40,7 +32,7 @@ class Main{
 
 		for(Pos p : pos)
 		{
-			int useCnt = 0;
+			int faceCnt = 0;// p.y,p.x 좌표와 마주보는 면의 개수
 			for(int i=0; i<4; i++)
 			{
 				int nextY = p.y + dxy[i][0];
@@ -54,12 +46,12 @@ class Main{
 				int faceNumber = DFS(p.y, p.x, i, 0);
 				if(0<faceNumber)
 				{
-					face[p.num] = faceNumber;
-					++useCnt;
+					face[p.num] = faceNumber;	// 자기와 마주보는 면을 입력
+					++faceCnt;					// 자기와 마주보는 면개수 + 1
 				}
 			}
 			// 자기와 마주보는 면이 1개 미만이나 초과일 경우 0출력
-			if(useCnt != 1)
+			if(faceCnt != 1)
 			{
 				System.out.print(0);
 				return;
@@ -69,7 +61,8 @@ class Main{
 	}
 	public static int DFS(int y, int x, int dir, int depth) {
 		if(depth == 2)
-			return map[y][x];
+			return map[y][x];	// 같은 방향으로 2번간 곳의 숫자 반환(마주보는면)
+		
 		for(int i=0; i<4; i++)
 		{
 			int nextY = y + dxy[i][0];
@@ -82,6 +75,12 @@ class Main{
 					return num;
 			}
 		}
+		
 		return 0;
+	}
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
