810b66828ab28e71232787dbe3541429210addcc
YongJu Kim <kyjdummy@gmail.com>
2024-11-09 16:38:26 +0900

    [Silver I] Title: Cow Hopscotch, Time: 360 ms, Memory: 16156 KB -BaekjoonHub

--- 백준/Silver/10748. Cow Hopscotch/Cow Hopscotch.java
+++ 백준/Silver/10748. Cow Hopscotch/Cow Hopscotch.java
@@ -1,13 +1,16 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/10748
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static final int MOD = 1_000_000_007;
 	static int Y, X;
 	static int[][] map, dp;
+	
+	static int read() throws Exception {// 빠른 입력을 위한 함수
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	
 	public static int solved(int y, int x) {
 		if(y==Y-1 && x==X-1)
@@ -29,22 +32,18 @@ class Main{
 		return dp[y][x];
 	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Y	= Integer.parseInt(st.nextToken());
-		X	= Integer.parseInt(st.nextToken());
+		Y	= read();
+		X	= read();
+			  read(); // 값 하나 버림
 		map = new int[Y][X];
 		dp	= new int[Y][X];
 		
 		for(int y=0; y<Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=0; x<X; x++)
 			{
-				map[y][x] = Integer.parseInt(st.nextToken());
-				dp[y][x] = -1;
+				map[y][x]	= read();
+				dp[y][x]	= -1;
 			}
-		}
 		
 		System.out.print( solved(0,0) );
 	}
