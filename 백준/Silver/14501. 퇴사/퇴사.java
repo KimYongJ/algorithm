982df183a4adc23647f4f7be8c81fb58f3e21439
YongJu Kim <kyjdummy@gmail.com>
2024-11-11 22:07:48 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,42 +1,38 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14501
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int max;
 	static int N, T[], P[];
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		N = read();
 		T = new int[N + 1];
 		P = new int[N + 1];
 		
 		for(int i=1; i<=N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			T[i] = Integer.parseInt(st.nextToken());	// 걸리는 기간
-			P[i] = Integer.parseInt(st.nextToken());	// 보상
+			T[i] = read();	// 걸리는 기간
+			P[i] = read();	// 보상
 		}
 		
-		bruteforce(1, 0);// 현재 일자, 금액의 합
+		bruteforce(1, 0);
 		
 		System.out.print(max);
 	}
-	// day까지 일했을 때 sum의 값 + 일안하고 day로 넘어왔을 때 sum의 값
 	public static void bruteforce(int day, int sum) {
 		if(N < day)
 		{
 			max = Math.max(max, sum);
 			return;
 		}
-		
-		bruteforce(day + 1, sum);
-		
-		if(day + T[day]-1 <= N)		// 일을 마친 후의 날짜가 N이하일 때만
+		bruteforce(day+1, sum);			// 해당 날짜에 일을 안하는 경우
+		if(day + T[day] - 1 <= N)		// 일을 하는경우는 무조건 마치는 날이 N일 이하여야 한다.
 			bruteforce(day + T[day], sum + P[day]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
