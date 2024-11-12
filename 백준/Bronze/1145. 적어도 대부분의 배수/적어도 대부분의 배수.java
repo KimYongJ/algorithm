4c1bb9315ac36e3468ecd922389a696d21ea5734
YongJu Kim <kyjdummy@gmail.com>
2024-11-12 17:58:10 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 112 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -4,24 +4,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 class Main{
+	
+	static int min	= 1<<30;
+	static int num[]= new int[5];
+	
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int num[] = new int[5];
-		
+
 		for(int i=0; i<5; i++)
 			num[i] = Integer.parseInt(st.nextToken());
 		
-		int res = 0;
-		int cnt = 0;
-		while(cnt < 3)
-		{
-			++res;
-			cnt = 0;
-			for(int n : num)
-				if(res % n == 0)
-					++cnt;
-		}
-		System.out.print(res);
+		bruteforce(0, 0, 1);
+		
+		System.out.print(min);
 	}
+	public static void bruteforce(int idx, int depth, int lcm) {
+		if(depth == 3) {
+			min = Math.min(min, lcm);
+			return;
+		}
+		for(int i=idx; i<5; i++)
+		{
+			int nextLcm = LCM(lcm, num[i]);
+			bruteforce(i + 1, depth + 1, nextLcm);
+		}
+	}
+	public static int LCM(int a, int b)
+	{return a*b / GCD(a,b);}
+	public static int GCD(int a, int b)
+	{return b == 0 ? a : GCD(b, a % b);}
 }
