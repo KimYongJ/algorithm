2d7df36eae0a01b12e8980a9bdde23a3abb34c02
YongJu Kim <kyjdummy@gmail.com>
2024-11-12 16:18:03 +0900

    [Silver IV] Title: 토너먼트, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,46 +1,23 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1057
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 class Main{
-	
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N	= Integer.parseInt(st.nextToken());
-		int A	= Integer.parseInt(st.nextToken())-1;
-		int B	= Integer.parseInt(st.nextToken())-1;
+		Integer.parseInt(st.nextToken());
+		int A	= Integer.parseInt(st.nextToken());
+		int B	= Integer.parseInt(st.nextToken());
 		int res	= 0;
-		ArrayList<Boolean> list = new ArrayList<>();
-		
-		for(int i=0; i<N; i++)
-			list.add(i==A || i==B);
-		
-		while(true)
+
+		while(A != B)
 		{
+			A = A/2 + A%2;
+			B = B/2 + B%2;
 			++res;
-			ArrayList<Boolean> next = new ArrayList<>();
-			for(int i=0; i<list.size(); i+=2) {
-				if(i+1 == list.size()) {
-					next.add(list.get(i));
-					break;
-				}
-				else {
-					boolean a = list.get(i);
-					boolean b = list.get(i+1);
-					next.add(a || b);
-					if(a&&b)
-					{
-						System.out.print(res);
-						return;
-					}
-				}
-			}
-			list = next;
 		}
+		System.out.print(res);
 	}
 }
