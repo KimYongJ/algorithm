27a4cce505df6358c2cef9481b9fe7c2f5eadae4
YongJu Kim <kyjdummy@gmail.com>
2024-11-12 16:59:03 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 96 ms, Memory: 13996 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,15 +1,17 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2851
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int sum		= 0;
 		int before	= 0;
 		for(int i=0; i<10; i++)
 		{
-			sum += Integer.parseInt(br.readLine());
+			sum += read();
 			if(100 <= sum)
 			{
 				if(100 - before < sum - 100)
