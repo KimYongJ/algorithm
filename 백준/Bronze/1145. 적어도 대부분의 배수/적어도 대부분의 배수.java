002d2039d7d1777fbd4a88e719f9ebe27443c786
YongJu Kim <kyjdummy@gmail.com>
2024-11-12 18:03:17 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -1,19 +1,13 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1145
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int min	= 1<<30;
 	static int num[]= new int[5];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		for(int i=0; i<5; i++)
-			num[i] = Integer.parseInt(st.nextToken());
+			num[i] = read();
 		
 		bruteforce(0, 0, 1);
 		
@@ -25,16 +19,20 @@ class Main{
 			min = Math.min(min, lcm);
 			return;
 		}
-		if(min < lcm)
+		if(min < lcm || 5 == idx)
 			return;
-		for(int i=idx; i<5; i++)
-		{
-			int nextLcm = LCM(lcm, num[i]);
-			bruteforce(i + 1, depth + 1, nextLcm);
-		}
+		
+		bruteforce(idx + 1, depth + 1, LCM(lcm, num[idx]));
+		bruteforce(idx + 1, depth, lcm);
+
 	}
 	public static int LCM(int a, int b)
 	{return a*b / GCD(a,b);}
 	public static int GCD(int a, int b)
 	{return b == 0 ? a : GCD(b, a % b);}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 }
