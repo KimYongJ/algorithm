f368a2aa24f53dd471420d6c4bbe8ced0611644c
YongJu Kim <kyjdummy@gmail.com>
2024-11-12 22:26:34 +0900

    [Silver I] Title: 회전 초밥, Time: 156 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -4,54 +4,58 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 class Main{
-	
-	static int cnt;
-	static int N, D, K, C, res, B[];
-	static boolean visit[];
-	
+
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());	// 초밥 벨트에 놓인 접시의 수(2<=삼만)
-		D		= Integer.parseInt(st.nextToken());	// 초밥의 적힌 최대 숫자(2<=삼천)
-		K		= Integer.parseInt(st.nextToken());	// 연속해서 먹는 접시의 수(1<=C<=D)
-		C		= Integer.parseInt(st.nextToken());	// 쿠폰번호(1<=C<=D)
-		res 	= 0;
-		B		= new int[N];
-		visit	= new boolean[D+1];
+		int N		= Integer.parseInt(st.nextToken());	// 초밥 벨트에 놓인 접시의 수(2<=삼만)
+		int D		= Integer.parseInt(st.nextToken());	// 초밥의 적힌 최대 숫자(2<=삼천)
+		int K		= Integer.parseInt(st.nextToken());	// 연속해서 먹는 접시의 수(1<=C<=D)
+		int C		= Integer.parseInt(st.nextToken());	// 쿠폰번호(1<=C<=D)
+		int len		= N + K;
+		int B[]		= new int[len];
+		int cnt		= 0;
+		int res		= 0;
+		int visit[]	= new int[D+1];
 		
 		for(int i=0; i<N; i++)
 			B[i] = Integer.parseInt(br.readLine());
+		for(int i=N; i<len; i++)
+			B[i] = B[i-N];
 		
-		int i = -1;
-		while(++i != N)
+		for(int i=0; i<K; i++)
 		{
-			cnt = 0;
-			DFS(i, 0);
+			if(visit[B[i]] == 0)
+				++cnt;
+			++visit[B[i]];
+		}
+		
+		if(visit[C] == 0)
+			res = 1;
+		
+		res += cnt;
+		
+		int L = 0;
+		int R = K-1;
+		while(R <len-1)
+		{
+			if(--visit[B[L]]==0)
+				--cnt;
+
+			++L;
+			++R;
+			
+			if(++visit[B[R]] == 1)
+				++cnt;
+			
+			
+			if(visit[C] == 0)
+				res = Math.max(res, cnt + 1);
+			else
+				res = Math.max(res, cnt);
 		}
 		
 		System.out.print(res);
 	}
-	public static void DFS(int idx, int depth) {
-		if(depth == K)
-		{
-			if(!visit[C])
-				++cnt;
-			res = Math.max(cnt, res);
-			return;
-		}
-		
-		if(idx == N)
-			idx = 0;
-		
-		if(!visit[B[idx]])
-		{
-			++cnt;
-			visit[B[idx]] = true;
-			DFS(idx + 1, depth + 1);
-			visit[B[idx]] = false;
-		}
-		else
-			DFS(idx + 1, depth + 1);
-	}
+
 }
