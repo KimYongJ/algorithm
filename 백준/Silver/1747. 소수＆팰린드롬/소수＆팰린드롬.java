e14c6e40686441e3883eaff02bd41e3a9a73a60f
YongJu Kim <kyjdummy@gmail.com>
2024-11-12 21:25:35 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 152 ms, Memory: 19308 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,34 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1747
+class Main{
+	public static void main(String[] args)throws Exception{
+		final int MAX = 1_003_002;
+		boolean prime[] = new boolean[MAX]; // 소수면 false
+		prime[0] = prime[1] = true;
+		for(int i=2; i<MAX; i++)
+			if(!prime[i])
+			{
+				for(int j=i<<1; j<MAX; j+=i)
+					prime[j] = true;
+			}
+
+		for(int i=read(); i<MAX; i++)
+			if(!prime[i] && isPal(i))
+			{
+				System.out.print(i);
+				return;
+			}
+	}
+	public static boolean isPal(int num) {
+		String str = String.valueOf(num);
+		for(int i=0, j=str.length()-1; i<str.length()/2; i++,j--)
+			if(str.charAt(i) != str.charAt(j))
+				return false;
+		return true;
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+}
