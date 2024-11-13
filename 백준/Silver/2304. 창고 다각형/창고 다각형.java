e1614bca4de2de586de00a9500d69f4110499e17
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 15:54:21 +0900

    [Silver II] Title: 창고 다각형, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,39 +1,37 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2304
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N	= Integer.parseInt(br.readLine());
+		int N	= read();
 		int H[] = new int[1001];
 		int len = 0;
-		
+		int res = 0;
+		int IDX	= 0;
+		int sIdx= 0;
+
 		while(N-->0)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int i	= Integer.parseInt(st.nextToken());	// 가로 인덱스
-			H[i]	= Integer.parseInt(st.nextToken());	// 높이
+			int i	= read();			// 가로 인덱스
+			H[i]	= read();			// 높이
 			len		= Math.max(len, i);
 		}
-		
-		int res = 0;
-		int h	= 0;
-		int IDX	= 0;
-		
-		for(int i=0; i<=len; i++)
+
+		for(int i=0, h = 0; i<=len; i++)
 			if(h < H[i])				// 기존 높이보다 큰것이 나타나면
 			{
 				res += (i - IDX) * h;
 				h	= H[i];				// 높이 다시 세팅
 				IDX	= i;				// 시작 인덱스 다시 세팅
 			}
-		
-		int sIdx= 0;
-		h = 0;
-		for(int i=len; i>=IDX; i--)
+
+		for(int i=len, h = 0; i>=IDX; i--)
 			if(h < H[i])
 			{
 				res		+= (sIdx - i) * h;
@@ -44,7 +42,6 @@ class Main{
 			}
 			else if(IDX == i)
 				res += (sIdx - i + 1) * h;
-			
 
 		System.out.print(res);
 	}
