7ae363b073195c3d3f270e50fead1b7fed4268d0
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 21:31:02 +0900

    방 배정하기.java

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -1,33 +1,26 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14697
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a[] = new int[3];
-		a[0] = Integer.parseInt(st.nextToken());
-		a[1] = Integer.parseInt(st.nextToken());
-		a[2] = Integer.parseInt(st.nextToken());
-		int target = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(a);
-		
-		int lenA = (target / a[0]) + 1;
-		int lenB = (target / a[1]) + 1;
-		int lenC = (target / a[2]) + 1;
+		int a[]		= new int[] {read(),read(),read()};
+		int target	= read();
+		int lenA	= (target / a[0]) + 1;
+		int lenB	= (target / a[1]) + 1;
+		int lenC	= (target / a[2]) + 1;
 		
 		for(int i=0; i<lenA; i++)
 			for(int j=0; j<lenB; j++)
 				for(int z=0; z<lenC; z++)
 				{
 					int sum = i*a[0] + j*a[1] + z*a[2];
-					if(target == sum) {
+					if(target == sum)
+					{
 						System.out.print(1);
 						return;
 					}
