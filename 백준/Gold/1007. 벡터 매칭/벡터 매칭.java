ed78492c8cf447df54b00c170cf7346420d72dbc
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 17:46:46 +0900

    [Gold II] Title: 벡터 매칭, Time: 168 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/1007. 벡터 매칭/벡터 매칭.java
+++ 백준/Gold/1007. 벡터 매칭/벡터 매칭.java
@@ -1,21 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1007
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N, pair[][];
 	static double min;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			N			= Integer.parseInt(br.readLine());
+			N			= read();
 			min			= Double.MAX_VALUE;
 			pair		= new int[N][2];
 			int sumX	= 0;
@@ -23,10 +18,9 @@ class Main{
 			int x,y;
 			for(int i=0; i<N; i++)
 			{
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				sumX += x = Integer.parseInt(st.nextToken());
-				sumY += y = Integer.parseInt(st.nextToken());
-				pair[i] = new int[] {x,y};
+				sumX += x = read();
+				sumY += y = read();
+				pair[i] = new int[] {x<<1,y<<1};
 			}
 			
 			selectMinus(N>>1, sumX, sumY, 0);
@@ -35,8 +29,8 @@ class Main{
 		}
 		System.out.print(sb.toString());
 	}
-	static void selectMinus(int toPick, int sumX, int sumY, int idx) {
-		if(toPick == 0)
+	static void selectMinus(int depth, int sumX, int sumY, int idx) {
+		if(depth == 0)
 		{
 			double result = Math.sqrt((long)sumX*sumX + (long)sumY*sumY);
 			if(result < min)
@@ -45,7 +39,16 @@ class Main{
 		}
 		
 		for(int i=idx; i< N; i++)
-			selectMinus(toPick - 1, sumX - (pair[i][0]<<1), sumY - (pair[i][1]<<1), i + 1);
-
+			selectMinus(depth - 1, sumX - pair[i][0], sumY - pair[i][1], i + 1);
+		
 	}
+	
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
