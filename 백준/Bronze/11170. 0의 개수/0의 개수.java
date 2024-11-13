7f385aef9701ba6278d11190daced3596d104512
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 21:50:57 +0900

    [Bronze I] Title: 0의 개수, Time: 128 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.java
+++ 백준/Bronze/11170. 0의 개수/0의 개수.java
@@ -7,28 +7,37 @@ import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
+		final int MAX	= 1_000_001;
+		int dp[]		= new int[MAX];
+		dp[0]			= 1;
+		for(int i=1, c = 0; i<MAX; i++, c = 0)
+		{
+			int num = i;
+			while(num != 0)
+			{
+				if(num % 10 == 0)
+					++c;
+				num /= 10;
+			}
+			dp[i] = c + dp[i-1];
+		}
+		
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder	sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		while(T-->0)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken())-1;
+			int n = Integer.parseInt(st.nextToken()) - 1;
 			int m = Integer.parseInt(st.nextToken());
-			int c = 0;
-			while(++n<=m)
+			int res = 0;
+			if(n<0)
 			{
-				int num = n;
-				if(num == 0)
-					++c;
-				while(num != 0)
-				{
-					if(num % 10 == 0)
-						++c;
-					num /= 10;
-				}
+				n = 0;
+				res = 1;
 			}
-			sb.append(c).append('\n');
+			res += dp[m] - dp[n];
+			sb.append(res).append('\n');
 		}
 		System.out.print(sb);
 	}
