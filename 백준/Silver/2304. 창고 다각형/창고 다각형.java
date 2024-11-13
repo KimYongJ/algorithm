3abf8032287640a71c841e6520ed7bb5e664c773
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 16:04:55 +0900

    [Silver II] Title: 창고 다각형, Time: 96 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -10,38 +10,21 @@ class Main{
 	
 	public static void main(String[] args)throws Exception{
 		int N	= read();
-		int H[] = new int[1001];
-		int len = 0;
-		int res = 0;
-		int IDX	= 0;
-		int sIdx= 0;
-
+		int H[] = new int[1002];
+		int L[] = new int[1002];
+		int R[] = new int[1002];
+		int res	= 0;
 		while(N-->0)
+			H[read()] = read();			// 높이
+		
+		for(int i=1; i<=1000; i++)
+			L[i] = Math.max(H[i], L[i-1]);
+		
+		for(int i=1000; i>0; i--)
 		{
-			int i	= read();			// 가로 인덱스
-			H[i]	= read();			// 높이
-			len		= Math.max(len, i);
+			R[i]= Math.max(H[i], R[i+1]);
+			res	+= Math.min(R[i], L[i]);
 		}
-
-		for(int i=0, h = 0; i<=len; i++)
-			if(h < H[i])				// 기존 높이보다 큰것이 나타나면
-			{
-				res += (i - IDX) * h;
-				h	= H[i];				// 높이 다시 세팅
-				IDX	= i;				// 시작 인덱스 다시 세팅
-			}
-
-		for(int i=len, h = 0; i>=IDX; i--)
-			if(h < H[i])
-			{
-				res		+= (sIdx - i) * h;
-				h		= H[i];
-				sIdx	= i;
-				if(IDX == i)
-					res += h;
-			}
-			else if(IDX == i)
-				res += (sIdx - i + 1) * h;
 
 		System.out.print(res);
 	}
