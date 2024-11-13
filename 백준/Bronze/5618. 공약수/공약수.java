6b64b1306a6f2e5109dfe975ab1ce61f719048ff
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 22:36:06 +0900

    [Bronze II] Title: 공약수, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -1,31 +1,33 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/5618
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-class Main{
+import java.util.TreeSet;
+class Main{	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder	sb = new StringBuilder();
-		int n		= Integer.parseInt(br.readLine());
-		int num[]	= new int[n];
-		int max		= 0;
-		boolean flag= false;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++)
-			max = Math.max(max,num[i] = Integer.parseInt(st.nextToken()));
+		int n	= read();
+		int GCD = GCD(read(), read());
+		if(n == 3)
+			GCD = GCD(GCD, read());
 		
-		for(int i=1; i<=max; i++, flag = false)
-		{
-			for(int m : num)
-				flag |= m % i != 0;
-
-			if(!flag)
-				sb.append(i).append('\n');
-		}
+		TreeSet<Integer> set = new TreeSet<>();
+		for(int i=1; i*i<=GCD; i++)
+			if(GCD % i == 0)
+			{
+				set.add(i);
+				set.add(GCD / i);
+			}
+		
+		for(int s : set)
+			sb.append(s).append('\n');
 		
 		System.out.print(sb);
+	}
+	public static int GCD(int a, int b) {
+		return b == 0 ? a : GCD(b, a%b);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
