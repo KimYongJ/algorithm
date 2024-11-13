a194332a9efe885aad753f53543bf7776afcbba7
YongJu Kim <kyjdummy@gmail.com>
2024-11-13 22:17:10 +0900

    [Bronze II] Title: 주사위, Time: 100 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.java
+++ 백준/Bronze/1233. 주사위/주사위.java
@@ -1,24 +1,25 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1233
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a		= Integer.parseInt(st.nextToken());
-		int b		= Integer.parseInt(st.nextToken());
-		int c		= Integer.parseInt(st.nextToken());
+		int a		= read();
+		int b		= read();
+		int c		= read();
 		int max		= 0;
 		int cnt[]	= new int[81];
 		
 		for(int i=1; i<=a; i++)
 			for(int j=1; j<=b; j++)
 				for(int z=1; z<=c; z++)
-					max = Math.max(max,++cnt[i+j+z]);
+					if(max < ++cnt[i+j+z])
+						max = cnt[i+j+z];
 		
 		for(int i=0; i<81; i++)
 			if(cnt[i] == max)
