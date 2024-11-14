5a0aa6acf1473198e8678fb42881111fa78811fd
YongJu Kim <kyjdummy@gmail.com>
2024-11-14 22:29:45 +0900

    [Silver II] Title: 꽃길, Time: 112 ms, Memory: 15068 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -1,46 +1,55 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14620
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
-	static boolean visit[][];
-	static int N, map[][];
-	static int result = 1<<30;
+	static boolean	visit[][];
+	static int		N, LEN, map[][];
+	static int		result = 1<<30;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());
+		N		= read();
+		LEN		= N*N;
 		map		= new int[N][N];
 		visit	= new boolean[N][N];
-		for(int y=0; y<N; y++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int x=0; x<N; x++)
-				map[y][x] = Integer.parseInt(st.nextToken());
-		}
 		
-		bruteforce(0, 1, 0);
+		for(int y=0; y<N; y++)
+			for(int x=0; x<N; x++)
+				map[y][x] = read();
+		
+		bruteforce(0, 0, 0);
 		
 		System.out.print(result);
 	}
-	public static void bruteforce(int depth, int y, int sum) {
-		if(depth == 3) {
+	public static void bruteforce(int depth, int idx, int sum) {
+		if(depth == 3)
+		{
 			result = Math.min(result, sum);
 			return;
 		}
-		for(; y<N-1; y++)
+		
+		if(result < sum)
+			return;
+		
+		for(int i=idx; i<LEN; i++)
 		{
-			for(int x = 1; x<N-1; x++)
+			int y = i / N;
+			int x = i % N;
+			
+			if(y==0 || N-1==y || x==0 || N-1==x)
+				continue;
+			
+			if(!visit[y][x+1] && !visit[y][x] && !visit[y][x-1] && !visit[y+1][x] && !visit[y-1][x])
 			{
-				if(!visit[y][x+1] && !visit[y][x] && !visit[y][x-1] && !visit[y+1][x] && !visit[y-1][x])
-				{
-					visit[y][x+1] = visit[y][x] = visit[y][x-1] = visit[y+1][x] = visit[y-1][x] = true;
-					bruteforce(depth + 1, y, sum + map[y][x+1] + map[y][x] + map[y][x-1] + map[y+1][x] + map[y-1][x]);
-					visit[y][x+1] = visit[y][x] = visit[y][x-1] = visit[y+1][x] = visit[y-1][x] = false;
-				}
+				visit[y][x+1] = visit[y][x] = visit[y][x-1] = visit[y+1][x] = visit[y-1][x] = true;
+				bruteforce(depth + 1, i + 1, sum + map[y][x+1] + map[y][x] + map[y][x-1] + map[y+1][x] + map[y-1][x]);
+				visit[y][x+1] = visit[y][x] = visit[y][x-1] = visit[y+1][x] = visit[y-1][x] = false;
 			}
 		}
+
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
