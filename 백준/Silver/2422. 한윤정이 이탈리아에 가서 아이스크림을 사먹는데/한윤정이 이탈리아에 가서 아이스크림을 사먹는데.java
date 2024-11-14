65bbc8040b378afcf1303c44b6b63cb4422979bf
YongJu Kim <kyjdummy@gmail.com>
2024-11-14 14:45:58 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 128 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -1,14 +1,28 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2422
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static boolean pair[][];
 	static int ice[];
 	static int N, M, cnt;
 	
+	public static void main(String[] args)throws Exception{
+		N		= read();
+		M		= read();
+		ice		= new int[4];
+		pair	= new boolean[N+1][N+1];
+		
+		while(M-->0)
+		{
+			int a = read();
+			int b = read();
+			pair[a][b] = pair[b][a] = true;
+		}
+		
+		bruteforce(1, 1);
+		
+		System.out.print(cnt);
+	}
 	public static void bruteforce(int depth, int idx) {
 
 		if(depth == 4)
@@ -24,24 +38,9 @@ class Main{
 				bruteforce(depth + 1, i + 1);
 		}
 	}
-	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());
-		M		= Integer.parseInt(st.nextToken());
-		ice		= new int[4];
-		pair	= new boolean[N+1][N+1];
-		
-		while(M-->0)
-		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			pair[a][b] = pair[b][a] = true;
-		}
-		
-		bruteforce(1, 1);
-		
-		System.out.print(cnt);
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
