4515072e3b88fd88003bb5fd1e613f5cbafdf154
YongJu Kim <kyjdummy@gmail.com>
2024-11-14 13:41:52 +0900

    [Silver IV] Title: Maximum Subarray, Time: 144 ms, Memory: 15372 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
@@ -4,34 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 class Main{
-	
-	static int max;
-	static int N, arr[];
+
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		while(T-->0)
 		{
-			N	= Integer.parseInt(br.readLine());
-			arr = new int[N+1];
-			max = ~Integer.MAX_VALUE;
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N		= Integer.parseInt(br.readLine());
+			int max		= -1000;
+			int arr[]	= new int[N+1];
 			
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int i=1; i<=N; i++)
-				arr[i] = Integer.parseInt(st.nextToken()) + arr[i-1];
-
-			for(int i=1; i<=N; i++)
-				comb(0, i, N + 1 - i);
-
+			{
+				arr[i] = Integer.parseInt(st.nextToken());
+				if(0 < arr[i - 1])
+					arr[i] += arr[i-1];
+				
+				max = Math.max(max, arr[i]);
+			}
+			
 			sb.append(max).append('\n');
 		}
 		System.out.print(sb);
-	}
-	public static void comb(int s, int e, int depth) {
-		if(depth == 0)
-			return;
-		max = Math.max(max, arr[e] - arr[s]);
-		comb(s + 1, e + 1, depth - 1);
 	}
 }
