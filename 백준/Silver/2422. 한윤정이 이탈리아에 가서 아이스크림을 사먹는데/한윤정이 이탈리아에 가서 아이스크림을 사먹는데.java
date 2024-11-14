89cc3a04e62ab0f7110326ff5d9c68cabcebe448
YongJu Kim <kyjdummy@gmail.com>
2024-11-14 14:44:12 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 180 ms, Memory: 19108 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -10,16 +10,18 @@ class Main{
 	static int N, M, cnt;
 	
 	public static void bruteforce(int depth, int idx) {
-		if(depth == 3)
+
+		if(depth == 4)
 		{
-			if(!pair[ice[0]][ice[1]] && !pair[ice[1]][ice[2]] && !pair[ice[2]][ice[0]])
+			if(!pair[ice[1]][ice[3]])
 				++cnt;
 			return;
 		}
 		for(int i=idx; i<=N; i++)
 		{
 			ice[depth] = i;
-			bruteforce(depth + 1, i + 1);
+			if(!pair[ice[depth]][ice[depth-1]])
+				bruteforce(depth + 1, i + 1);
 		}
 	}
 	public static void main(String[] args)throws Exception{
@@ -27,7 +29,7 @@ class Main{
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N		= Integer.parseInt(st.nextToken());
 		M		= Integer.parseInt(st.nextToken());
-		ice		= new int[3];
+		ice		= new int[4];
 		pair	= new boolean[N+1][N+1];
 		
 		while(M-->0)
@@ -38,7 +40,7 @@ class Main{
 			pair[a][b] = pair[b][a] = true;
 		}
 		
-		bruteforce(0, 1);
+		bruteforce(1, 1);
 		
 		System.out.print(cnt);
 	}
