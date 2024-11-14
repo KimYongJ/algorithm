02c291335a35fbf2776f2bbac555ac099ce50f0f
YongJu Kim <kyjdummy@gmail.com>
2024-11-14 13:43:57 +0900

    [Silver IV] Title: Maximum Subarray, Time: 112 ms, Memory: 13996 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
@@ -1,30 +1,30 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/10211
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
-
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
 		{
-			int N		= Integer.parseInt(br.readLine());
+			int N		= read();
 			int max		= -1000;
 			int arr[]	= new int[N+1];
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int i=1; i<=N; i++)
 			{
-				arr[i] = Integer.parseInt(st.nextToken());
+				arr[i] = read();
 				if(0 < arr[i - 1])
 					arr[i] += arr[i-1];
 				
 				max = Math.max(max, arr[i]);
 			}
-			
 			sb.append(max).append('\n');
 		}
 		System.out.print(sb);
