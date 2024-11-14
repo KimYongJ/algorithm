db7506c771143329110b5bba1f4d1a3a246f225c
YongJu Kim <kyjdummy@gmail.com>
2024-11-14 13:10:09 +0900

    [Silver V] Title: 수 이어가기, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.java
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.java
@@ -1,31 +1,13 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2635
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
 	
 	static int N, maxLen, arr[];
 	static StringBuilder sb;
-	public static void DFS(int depth) {
-		int num = arr[depth-2] - arr[depth-1];
-		if(num < 0)
-		{
-			if(maxLen < depth)
-			{
-				maxLen	= depth;
-				sb		= new StringBuilder();
-				for(int i=0; i<depth; i++)
-					sb.append(arr[i]).append(' ');
-			}
-			return;
-		}
-		arr[depth] = num;
-		DFS(depth + 1);
-	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());	// 0<=30,000;
-		arr		= new int[30_000];
+		N		= read();	// 0<=30,000;
+		arr		= new int[50];
 		arr[0]	= N;
 		
 		for(int i=0; i<=N; i++)
@@ -36,5 +18,27 @@ class Main{
 		
 		System.out.println(maxLen);
 		System.out.print(sb);
+	}
+	
+	static void DFS(int depth) {
+		arr[depth] = arr[depth-2] - arr[depth-1];
+		if(arr[depth] < 0)
+		{
+			if(maxLen < depth)
+			{
+				maxLen	= depth;
+				sb		= new StringBuilder();
+				for(int i=0; i<depth; i++)
+					sb.append(arr[i]).append(' ');
+			}
+			return;
+		}
+		DFS(depth + 1);
+	}
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
