d9b87f9a95aa10834fc602f3642541aca8c5652f
YongJu Kim <kyjdummy@gmail.com>
2024-11-15 23:07:22 +0900

    [Silver I] Title: 부분수열의 합, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -2,42 +2,30 @@
 //https://www.acmicpc.net/problem/14225
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
 class Main{
-	
-	static int N, arr[];
-	static boolean visit[];
-	
+
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		final int MAX = 2_000_001;
-		N		= Integer.parseInt(br.readLine());
-		arr		= new int[N];
-		visit	= new boolean[MAX];
+		int N		= Integer.parseInt(br.readLine());
+		int arr[]	= new int[N];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
 		
-		for(int i=1; i<=N; i++)
-			comb(i, 0, 0);
+		Arrays.sort(arr);
 		
-		for(int i=1; i<=MAX; i++)
-			if(!visit[i])
-			{
-				System.out.print(i);
-				return;
-			}
-				
-	}
-	public static void comb(int depth, int idx, int sum) {
-		if(depth == 0)
+		int res = 0;
+		
+		for(int i=0; i<N; i++)
 		{
-			visit[sum] = true;
-			return;
+			if(res+1 < arr[i])
+				break;
+			res += arr[i];
 		}
-		for(int i=idx; i<N; i++)
-			comb(depth - 1, i + 1, sum + arr[i]);
+		
+		System.out.print(res+1);
 	}
 }
