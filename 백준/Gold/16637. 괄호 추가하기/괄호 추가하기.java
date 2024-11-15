4d6ced463f369d85a15ee1bbb0bf734315b18f1c
YongJu Kim <kyjdummy@gmail.com>
2024-11-15 17:00:33 +0900

    [Gold III] Title: 괄호 추가하기, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,27 +1,22 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16637
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
 	
 	static int max = ~(1<<30);
-	static int N, len, num[];
-	static char op[];
+	static int N, len, num[], op[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N	= Integer.parseInt(br.readLine());	// 수식의 길이 1<=19
+		N	= read();	// 수식의 길이 1<=19
 		len	= N/2;
 		num	= new int[len+1];
-		op	= new char[len];
-		int idx		= 0;
-		String str	= br.readLine();
+		op	= new int[len];
+		
 		for(int i=0; i<len; i++)
 		{
-			num[i]	= str.charAt(idx++) - '0';
-			op[i]	= str.charAt(idx++);
+			num[i]	= System.in.read() - '0';
+			op[i]	= (char)System.in.read();
 		}
-		num[len] = str.charAt(idx) - '0';
+		num[len] = System.in.read() - '0';
 		
 		DFS(0,num[0]);
 		
@@ -41,12 +36,17 @@ class Main{
 			DFS(idx + 2, cal(op[idx], sum ,nextSum));
 		}
 	}
-	public static int cal(char op, int l, int r) {
+	public static int cal(int op, int l, int r) {
 		switch(op)
 		{
 		case '+': return l+r;
 		case '-': return l-r;
 		default : return l*r;
 		}
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
