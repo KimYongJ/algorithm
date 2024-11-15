6e4c9be20c6098e68b04a3cccaee6935e3a45d03
YongJu Kim <kyjdummy@gmail.com>
2024-11-15 13:34:26 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 112 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
@@ -1,21 +1,57 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2670
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine().trim());
-		double arr[] = new double[T+1];
-		double dp[] = new double[T+1];
-		double res = 0;
-		
-		for(int i=1; i<=T; i++)
+		Reader in		= new Reader();
+		int T			= in.nextInt();
+		double arr[]	= new double[T+1];
+		double res		= 0;
+		arr[1]			= in.nextDouble();
+		for(int i=2; i<=T; i++)
 		{
-			arr[i] = Double.parseDouble(br.readLine());
-			dp[i] = Math.max(arr[i], arr[i] * dp[i-1]);
-			res = Math.max(res, dp[i]);
+			arr[i]	= in.nextDouble();
+			arr[i]	= Math.max(arr[i], arr[i] * arr[i-1]);
+			res		= Math.max(res, arr[i]);
 		}
 		System.out.print(String.format("%.3f", res));
 	}
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
