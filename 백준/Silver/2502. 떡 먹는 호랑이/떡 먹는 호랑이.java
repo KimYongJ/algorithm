8699f60c98776b01d86e14fc2debc481f79d2905
YongJu Kim <kyjdummy@gmail.com>
2024-11-16 22:47:29 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 104 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
@@ -1,31 +1,33 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/2502
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int D = Integer.parseInt(st.nextToken());	// 할머니가 넘어온 날(3<=30)
-		int K = Integer.parseInt(st.nextToken());	// 이 날 준 떡개수(10<=십만)
-		int dp[] = new int[D];
-		dp[0] = K;
-		for(int i=1; i<K; i++) {
+		int D		= read();	// 할머니가 넘어온 날(3<=30)
+		int K		= read();	// 이 날 준 떡개수(10<=십만)
+		int dp[]	= new int[D];
+		dp[0]		= K;
+		
+		for(int i=1; i<K; i++)
+		{
 			dp[1] = i;
 			
-			for(int j=2; j<D; j++) {
+			for(int j=2; j<D; j++)
+			{
 				int diff = dp[j-2] - dp[j-1];
 				if(0 < diff)
 					dp[j] = diff;
 				else 
 					break;
 			}
-			if(dp[D-1] != 0 && dp[D-1] < dp[D-2]) {
-				System.out.println(dp[D-1]);
-				System.out.println(dp[D-2]);
+			if(dp[D-1] != 0 && dp[D-1] < dp[D-2])
+			{
+				System.out.println(new StringBuilder()
+						.append(dp[D-1]).append('\n').append(dp[D-2]));
 				break;
 			}
 		}
