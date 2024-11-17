e0fc0ee3d4cc53eac45b9711460c4874e4b0c560
YongJu Kim <kyjdummy@gmail.com>
2024-11-17 16:47:59 +0900

    [Silver II] Title: 친구, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -1,23 +1,16 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/1058
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
 
-	static int N;
-	static boolean map[][];
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N	= Integer.parseInt(br.readLine());
-		map = new boolean[N][N];
+		int N			= read();
+		boolean[][]map	= new boolean[N][N];
 		
 		for(int y=0; y<N; y++)
 		{
-			String str = br.readLine();
 			for(int x=0; x<N; x++)
-				map[y][x] = map[x][y] = str.charAt(x) == 'Y';
+				map[y][x] = map[x][y] = System.in.read() == 'Y';
+			System.in.read();
 		}
 		
 		int res = 0;
@@ -29,18 +22,22 @@ class Main{
 					continue;
 				else if(map[y][x])
 					++cnt;
-				else if(isFriend(y, x))
-					++cnt;
+				else
+					for(int i=0; i<N; i++)
+						if(map[i][x] && map[y][i])
+						{
+							++cnt;
+							break;
+						}
 
 			res = Math.max(res, cnt);
 		}
 		System.out.print(res);
 	}
-	public static boolean isFriend(int y, int x) {
-		for(int i=0; i<N; i++) {
-			if(map[i][x] && map[y][i])
-				return true;
-		}
-		return false;
+
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
