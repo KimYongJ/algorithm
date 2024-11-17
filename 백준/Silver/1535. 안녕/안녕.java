f5ce98672e1aa01161cf4120d765b5994d365ff0
YongJu Kim <kyjdummy@gmail.com>
2024-11-17 15:50:16 +0900

    [Silver II] Title: 안녕, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -1,34 +1,26 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/1535
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int[]E		= new int[N+1];		// 잃는 체력
 		int[]P		= new int[N+1];		// 얻는 기쁨
 		int[][]dp	= new int[101][N+1];// 체력 당 얻는 기쁨을 담을 배열
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			E[i] = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
+			E[i] = read();
+
 		for(int i=1; i<=N; i++)
-			P[i] = Integer.parseInt(st.nextToken());
+			P[i] = read();
 		
-		for(int e=1; e<=100; e++) {
-			for(int n=1; n<=N; n++) {
+		for(int e=1; e<=100; e++)
+			for(int n=1; n<=N; n++)
 				if(E[n] < e)
 					dp[e][n] = Math.max(dp[e][n-1], dp[e-E[n]][n-1] + P[n]);
 				else
 					dp[e][n] = dp[e][n-1];
 
-			}
-		}
 		System.out.print(dp[100][N]);
 	}
 
