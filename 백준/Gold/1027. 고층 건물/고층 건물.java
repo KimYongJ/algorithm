84d8fce4f6f17841875b405474b79742f7ebe785
YongJu Kim <kyjdummy@gmail.com>
2024-11-17 21:33:28 +0900

    [Gold IV] Title: 고층 건물, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.java
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -1,47 +1,46 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/1027
 class Main{
-	
-	static int N;
-	static double arr[];
-	
+
 	public static void main(String[] args)throws Exception{
-		N	= read();
-		arr = new double[N + 1];
+		int N		= read();
+		double[]arr	= new double[N + 1];
 
 		for(int i=1; i<=N; i++)
 			arr[i] = read();
 		
 		int ans = 0;
 		
-		for(int i=1; i<=N; i++)
-			ans = Math.max(ans, get(i));
+		for(int n=1, cnt = 0; n<=N; n++, cnt = 0)
+		{
+			double incl = 0.0;
+			// 왼쪽
+			for(int i=n - 1; 1<=i; i--)
+			{
+				double nextIncl = (arr[n] - arr[i]) / (n - i);
+				if(i == n - 1 || incl > nextIncl)
+				{
+					incl = nextIncl;
+					++cnt;
+				}
+			}
+			// 오른쪽
+			for(int i=n + 1; i<=N; i++)
+			{
+				double nextIncl = (arr[n] - arr[i]) / (n - i);
+				if(i == n + 1 || incl < nextIncl)
+				{
+					incl = nextIncl;
+					++cnt;
+				}
+			}
+
+			ans = Math.max(ans, cnt);
+		}
 		
 		System.out.print(ans);
 	}
-	public static int get(int idx) {
-		int cnt = 0;
-		double incl = 0.0;
-		for(int i=idx - 1; 1<=i; i--)
-		{
-			double nextIncl = (arr[idx] - arr[i]) / (idx - i);
-			if(i == idx -1 || incl > nextIncl)
-			{
-				incl = nextIncl;
-				++cnt;
-			}
-		}
-		for(int i=idx + 1; i<=N; i++) {
-			double nextIncl = (arr[idx] - arr[i]) / (idx - i);
-			if(i == idx + 1 || incl < nextIncl)
-			{
-				incl = nextIncl;
-				++cnt;
-			}
-		}
-		
-		return cnt;
-	}
+
 	static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
