e728e36e15de98994dc94c97fd570c0c67236cec
YongJu Kim <kyjdummy@gmail.com>
2024-11-17 19:13:29 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -7,37 +7,35 @@ import java.io.InputStreamReader;
 class Main{
 	
 	static char[] a, b;
+	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		a = br.readLine().toCharArray();
 		b = br.readLine().toCharArray();
 		
-		DFS(b.length - a.length, 0, b.length-1, true);
+		boolean res = DFS(b.length - a.length, 0, b.length-1, true);
 		
-		System.out.print(0);
+		System.out.print(res ? 1 : 0);
 	}
-	public static void DFS(int depth, int s, int e, boolean flag) {
+	public static boolean DFS(int depth, int s, int e, boolean flag) {
 		if(depth == 0)
-		{
-			if(isSame(s, e, flag))
-			{
-				System.out.print(1);
-				System.exit(0);
-			}
-			return;
-		}
+			return isSame(s, e, flag);
 
-		if(flag) {
-			if(b[e] == 'A')
-				DFS(depth - 1, s, e-1, flag);
-			if(b[s] == 'B')
-				DFS(depth - 1, s + 1, e, !flag);
-		}else {
-			if(b[s] == 'A')
-				DFS(depth - 1, s + 1, e, flag);
-			if(b[e] == 'B')
-				DFS(depth - 1, s, e - 1, !flag);
+		if(flag)
+		{
+			if(b[e] == 'A' && DFS(depth - 1, s, e-1, flag))
+				return true;
+			if(b[s] == 'B' && DFS(depth - 1, s + 1, e, !flag))
+				return true;
 		}
+		else
+		{
+			if(b[s] == 'A' && DFS(depth - 1, s + 1, e, flag))
+				return true;
+			if(b[e] == 'B' && DFS(depth - 1, s, e - 1, !flag))
+				return true;
+		}
+		return false;
 	}
 	public static boolean isSame(int s, int e, boolean flag) {
 		if(flag)
