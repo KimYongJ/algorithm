1b1b5106a7f7fb4d52dc4a63fe02078a71dbea5c
YongJu Kim <kyjdummy@gmail.com>
2024-11-18 22:11:16 +0900

    [Silver III] Title: N번째 큰 수, Time: 228 ms, Memory: 24368 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,38 +1,24 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/2075
+import java.io.DataInputStream;
 import java.util.PriorityQueue;
 class Node{
 	int cur, y, x;
 	Node(int c, int y, int x){this.y=y; this.x=x; cur=c;}
 }
 class Main{
-    static int read() throws Exception {
-        int r = 0, c = System.in.read();
-        while (c <= ' ') c = System.in.read();
-        boolean negative = false;
-        if (c == '-') {
-            negative = true;
-            c = System.in.read();
-        }
-        while (c >= '0' && c <= '9') {
-            r *= 10;
-            r += c - '0';
-            c = System.in.read();
-        }
-        return negative ? -r : r;
-    }
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Node> pq	= new PriorityQueue<>((a,b)->b.cur-a.cur);
-		int N		= read();
+		int N		= readInt();
 		int map[][] = new int[N][N];
 		
 		
 		for(int y=0; y<N-1; y++) 
 			for(int x=0; x<N; x++)
-				map[y][x] = read();
+				map[y][x] = readInt();
 
 		for(int x=0; x<N; x++)
-			pq.add(new Node(map[N-1][x] = read(),N-1, x));
+			pq.add(new Node(map[N-1][x] = readInt(),N-1, x));
 		
 		int ans = 0;
 		
@@ -47,4 +33,30 @@ class Main{
 		
 		System.out.print(ans);
 	}
+	
+    static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    static DataInputStream inputStream = new DataInputStream(System.in);;
+    static byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+    static int curIdx, maxIdx;
+
+    static int readInt() throws Exception {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    static byte read() throws Exception {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
 }
