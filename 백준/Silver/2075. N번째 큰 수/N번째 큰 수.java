7f0b07c054a49e96db9cc2fdf3b75686489d737d
YongJu Kim <kyjdummy@gmail.com>
2024-11-18 21:31:30 +0900

    [Silver III] Title: N번째 큰 수, Time: 648 ms, Memory: 79804 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,21 +1,22 @@
 //https://github.com/KimYongJ/algorithm
 //https://www.acmicpc.net/problem/2075
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
 class Main{
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
 		PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+		int N = read();
+		
 		for(int y=0; y<N; y++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int x=0; x<N; x++)
-				pq.add(Integer.parseInt(st.nextToken()));
-		}
+				pq.add(read());
 		
 		while(--N>0)
 			pq.poll();
