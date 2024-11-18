a1f59e27b7082798fe99aaf459f35c2705565356
YongJu Kim <kyjdummy@gmail.com>
2024-11-18 15:06:22 +0900

    [Gold IV] Title: 괄호 제거, Time: 136 ms, Memory: 16936 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.java
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.java
@@ -3,10 +3,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 class Point{
 	int a, b;
 	Point(int a, int b){this.a=a; this.b=b;}
@@ -14,23 +13,25 @@ class Point{
 class Main{
 	
 	static char arr[];
-	static Set<String> result	= new HashSet<>();
+	static Set<String> result	= new TreeSet<>();
 	static List<Point> list		= new ArrayList<>();
+	
 	public static void bruteforce(int idx) {
 		if(idx == list.size())
 		{
 			StringBuilder sb = new StringBuilder();
 			for(char c : arr)
-				if(c != '!')
+				if(c != 0)
 					sb.append(c);
 			result.add(sb.toString());
 			return;
 		}
 		
-		bruteforce(idx + 1);	// 해당 괄호를 안지운다. 
+		bruteforce(idx + 1);	// 해당 괄호를 안지운다.
+		
 		Point p = list.get(idx);
-		arr[p.a]= '!';
-		arr[p.b]= '!';
+		arr[p.a]= 0;
+		arr[p.b]= 0;
 		bruteforce(idx + 1);	// 해당 괄호를 지운다.
 		arr[p.a]= '(';
 		arr[p.b]= ')';
@@ -41,9 +42,8 @@ class Main{
 		String str	= br.readLine();
 		arr			= new char[str.length()];
 		int[] par	= new int[11];				// 시작괄호('(') 를 담을 배열
-		int parIdx	= 0;						// 시작괄호 배열의 인덱스
 		
-		for(int i=0; i<str.length(); i++)
+		for(int i=0, parIdx = 0; i<str.length(); i++)
 		{
 			arr[i] = str.charAt(i);
 			if(arr[i] == '(')		// 시작괄호를 만나면 배열에 담음
@@ -56,12 +56,8 @@ class Main{
 
 		result.remove(str);
 
-		List<String> res = new ArrayList<>(result);
-		
-		Collections.sort(res);
-		
 		StringBuilder sb = new StringBuilder();
-		for(String r : res)
+		for(String r : result)
 			sb.append(r).append('\n');
 		System.out.print(sb);
 	}
