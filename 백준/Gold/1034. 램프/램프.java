551851ed6d51cbc2a88bff235cbfb8b40d104e61
YongJu Kim <kyjdummy@gmail.com>
2024-11-18 11:00:11 +0900

    [Gold IV] Title: 램프, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.java
+++ 백준/Gold/1034. 램프/램프.java
@@ -2,6 +2,7 @@
 //https://www.acmicpc.net/problem/1034
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 class Main{
 	
@@ -9,45 +10,32 @@ class Main{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int Y			= Integer.parseInt(st.nextToken());	// Y(1<=50)
-		int X			= Integer.parseInt(st.nextToken());	// X(1<=50)
-		int zeroCnt[]	= new int[Y];
-		boolean map[][]	= new boolean[Y][X];
-		
+						  Integer.parseInt(st.nextToken());	// X(1<=50)
+		String str[]	= new String[Y];
+		HashMap<String, Integer> map = new HashMap<>();
+		HashMap<String, Integer> zero= new HashMap<>();
 		for(int y=0; y<Y; y++)
 		{
-			String str = br.readLine();
-			for(int x=0; x<X; x++) {
-				map[y][x] = str.charAt(x) == '1';
-				if(!map[y][x])
-					++zeroCnt[y];
+			str[y] = br.readLine();
+			map.put(str[y], map.getOrDefault(str[y], 0) + 1);
+			if(!zero.containsKey(str[y]))
+			{
+				int cnt = 0;
+				for(char c : str[y].toCharArray())
+					if(c == '0')
+						++cnt;
+				zero.put(str[y], cnt);
 			}
 		}
-		
 		int K	= Integer.parseInt(br.readLine());	// 누르는횟수(0<=천)
 		int res = 0;
 		
 		for(int y=0; y<Y; y++)
-			if(zeroCnt[y] <= K && (K-zeroCnt[y]) % 2 == 0)
-			{
-				int cnt = 1;
-				for(int y1=0; y1<Y; y1++)
-				{
-					if(y == y1)
-						continue;
-					boolean flag = true;
-					for(int x=0; x<X; x++)
-					{
-						if(map[y][x] != map[y1][x])
-						{
-							flag = false;
-							break;
-						}
-					}
-					if(flag)
-						++cnt;
-				}
-				res = Math.max(res, cnt);
-			}
+		{
+			int zeroCnt = zero.get(str[y]);
+			if(zeroCnt <= K && (K-zeroCnt) % 2 == 0)
+				res = Math.max(res, map.get(str[y]));
+		}
 		
 		System.out.print(res);
 	}
