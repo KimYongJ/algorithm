bc09a3b6f813698315edfa198d6bbf2dc1448b22
YongJu Kim <kyjdummy@gmail.com>
2024-11-19 17:46:34 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
@@ -1,39 +1,33 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14658
-import java.util.ArrayList;
-import java.util.Collections;
-class Node{
-	int x, y;
-	Node(int x, int y){this.x=x; this.y=y;}
-}
 class Main{
 	
-	static ArrayList<Node> star = new ArrayList<>();
+	static int[][] star;
 	static int L, K;
 	
 	public static void main(String[] args)throws Exception{
-		read();read();	// 안쓰는것
-		L = read();		// 트램펄린 한변길이(1<=십만)
-		K = read();		// 별똥별수(1<=백만)
-		
+		read();read();			// 안쓰는것
+		L		= read();		// 트램펄린 한변길이(1<=십만)
+		K		= read();		// 별똥별수(1<=백만)
+		star	= new int[K][2];
 		for(int i=0; i<K; i++)
-			star.add(new Node(read(),read()));
-	
-		
-		Collections.sort(star,(a,b)->a.x!=b.x ? a.x-b.x : a.y-b.y);
-		
+		{
+			star[i][0] = read();
+			star[i][1] = read();
+		}
+
 		int res = 0;
-		for(int i=0; i<star.size(); i++)
-			for(int j=0; j<star.size(); j++)
-				res = Math.max(res, get(star.get(i).x, star.get(j).y));
+		for(int i=0; i<K; i++)
+			for(int j=0; j<K; j++)
+				res = Math.max(res, get(star[i][0], star[j][1]));
 		
 		System.out.print(K - res);
 	}
 	public static int get(int x, int y) {
 		int res = 0;
 		
-		for(Node now : star)
-			if(x<=now.x && now.x<=x+L && now.y<=y && y-L<=now.y)
+		for(int[] now : star)
+			if(x<=now[0] && now[0]<=x+L && now[1]<=y && y-L<=now[1])
 				++res;
 		
 		return res;
