4ae48b181c831e983398ef7c7311dc9ec1be114d
YongJu Kim <kyjdummy@gmail.com>
2024-11-21 13:21:01 +0900

    [Gold III] Title: 고기잡이, Time: 156 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Gold/7573. 고기잡이/고기잡이.java
+++ 백준/Gold/7573. 고기잡이/고기잡이.java
@@ -1,39 +1,34 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/7573
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
-	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 맵크기 2<=만
-		int L		= Integer.parseInt(st.nextToken());	// 그물 길이 (4<=100, L≤4N-4만족하는짝수)
-		int M		= Integer.parseInt(st.nextToken());	// 물고기 수 (1<=100)
+		int N		= read();	// 맵크기 2<=만
+		int L		= read();	// 그물 길이 (4<=100, L≤4N-4만족하는짝수)
+		int M		= read();	// 물고기 수 (1<=100)
 		int[][]pos	= new int[M][2];
 		
 		for(int i=0; i<M; i++)
-		{
-			st = new StringTokenizer(br.readLine());
-			int y = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-			pos[i] = new int[] {y,x};
-		}
+			pos[i] = new int[] {read(),read()};
 		
 		int max = 0;
 		int len = L / 2;
 		for(int width=1; width<len; width++)
 		{
-			for(int height = len - width; height>=0; height--)
+			int originHeight = len - width;
+			// 특정 점을 기준으로 그 점이 그물의 가장 왼쪽위일때 혹은, 그물의 가장 왼쪽아래일때 까지 모두 다 탐색해야 한다.
+			for(int height = originHeight; height>=0; height--)
 			{
 				for(int ps[] : pos)
 				{
-					int sy = ps[0] - height;
-					int sx = ps[1];
-					int ey = sy + len - width;
-					int ex = sx + width;
+					int sy	= ps[0] - height;
+					int sx	= ps[1];
+					int ey	= sy + originHeight;
+					int ex	= ps[1] + width;
 					int cnt = 0;
 					for(int p[] : pos)
 					{
