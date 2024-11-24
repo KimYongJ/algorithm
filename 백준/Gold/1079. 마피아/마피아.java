c1a6b545926e243c724392eb51b87ce7811bc1fe
YongJu Kim <kyjdummy@gmail.com>
2024-11-24 21:41:00 +0900

    [Gold II] Title: 마피아, Time: 448 ms, Memory: 22468 KB -BaekjoonHub

--- 백준/Gold/1079. 마피아/마피아.java
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -1,42 +1,34 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1079
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int			res, M, N;
-	static int[]		origin;
+	static int[]		guilt;
 	static int[][]		R;
 	static boolean[]	kill;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());
-		origin	= new int[N];
+		N		= read();
+		guilt	= new int[N];
 		R		= new int[N][N];
 		kill	= new boolean[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++)
-			origin[i] = Integer.parseInt(st.nextToken());
+			guilt[i] = read();
 
 		for(int y=0; y<N; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=0; x<N; x++)
-				R[y][x] = Integer.parseInt(st.nextToken());
-		}
+				R[y][x] = read();
 
-		M = Integer.parseInt(br.readLine());
+		M = read();
 
-		bruteforce(0, N, origin);
+		bruteforce(0, N);
 		
 		System.out.print(res);
 	}
 	
-	public static void bruteforce(int day, int people, int[] guilt) {
-		if(people < 2)
+	public static void bruteforce(int day, int people) {
+		if(people == 1)
 		{
 			res = Math.max(res, day);
 			return;
@@ -45,19 +37,14 @@ class Main{
 		if(people % 2 == 0)	// 참가자 짝수 = 밤(마피아가 타인 킬)
 		{
 			for(int i=0; i<N; i++)
-			{
 				if(!kill[i] && i != M)
 				{
 					kill[i] = true;
-					for(int j=0; j<N; j++)
-						guilt[j] += R[i][j];
-					bruteforce(day + 1, people - 1, guilt);
-					for(int j=0; j<N; j++)
-						guilt[j] -= R[i][j];
+					for(int j=0; j<N; j++) guilt[j] += R[i][j];
+					bruteforce(day + 1, people - 1);
+					for(int j=0; j<N; j++) guilt[j] -= R[i][j];
 					kill[i] = false;
 				}
-			}
-			
 		}
 		else				// 홀수 = 낮(시민이 한명 킬)
 		{
@@ -72,15 +59,21 @@ class Main{
 				}
 			
 			if(idx == M)
-			{
 				res = Math.max(res, day);
-			}
 			else
 			{
 				kill[idx] = true;
-				bruteforce(day , people - 1, guilt);
+				bruteforce(day , people - 1);
 				kill[idx] = false;
 			}
 		}
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
