1c60536befa2263d1db9d6499929ef18aa21a433
YongJu Kim <kyjdummy@gmail.com>
2024-11-26 13:45:08 +0900

    [Platinum V] Title: Ignition, Time: 344 ms, Memory: 23836 KB -BaekjoonHub

--- 백준/Platinum/13141. Ignition/Ignition.java
+++ 백준/Platinum/13141. Ignition/Ignition.java
@@ -3,18 +3,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-class Node{
-	int node, dist; Node next;
-	Node(int n, int d, Node nt){node=n; dist=d; next=nt;}
-}
-
 class Main{
 	
 	static final int MAX = 1<<30;
 	static int		N, M;
 	static double[][]dist;
-	static Node[]	adNode;
+	static int[][]	adNode;
 	static double	MIN = MAX;
 	
 	public static void main(String[] args)throws Exception{
@@ -22,7 +16,7 @@ class Main{
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N		= Integer.parseInt(st.nextToken());	// 2<=200
 		M		= Integer.parseInt(st.nextToken());	// N-1<=20000
-		adNode	= new Node[N+1];
+		adNode	= new int[N+1][N+1];
 		dist	= new double[N+1][N+1];
 
 		// 플로이드워셜을 위한 기본 세팅
@@ -37,8 +31,8 @@ class Main{
 			int a		= Integer.parseInt(st.nextToken());
 			int b		= Integer.parseInt(st.nextToken());
 			int l		= Integer.parseInt(st.nextToken());
-			adNode[a]	= new Node(b, l, adNode[a]);
-			adNode[b]	= new Node(a, l, adNode[b]);
+			adNode[a][b]= Math.max(adNode[a][b], l);
+			adNode[b][a]= Math.max(adNode[b][a], l);
 			dist[a][b]	= Math.min(dist[a][b], l);
 			dist[b][a]	= Math.min(dist[b][a], l);
 		}
@@ -52,69 +46,19 @@ class Main{
 		for(int start=1; start<=N; start++)
 		{
 			double maxBurnTime = 0;
-			for(int nowNode=1; nowNode<=N; nowNode++) // 변경
+			for(int nowNode=1; nowNode<=N; nowNode++)
 			{
-				for(Node next=adNode[nowNode]; next!=null; next=next.next)
+				for(int nextNode=1; nextNode<=N; nextNode++)
 				{
-					int nextNode	= next.node;
 					double max		= Math.max(dist[start][nowNode], dist[start][nextNode]);
 					double min		= Math.min(dist[start][nowNode], dist[start][nextNode]);
-					double burnTime = max + ((min+ next.dist-max) / 2.0);
-					maxBurnTime = Math.max(maxBurnTime, burnTime);
+					double burnTime = max + ((min + adNode[nowNode][nextNode] - max) / 2.0);
+					maxBurnTime		= Math.max(maxBurnTime, burnTime);
 				}
 			}
 			MIN = Math.min(MIN, maxBurnTime);
 		}
 		
-		
-		
-//		for(int i=1; i<=N; i++)
-//		{
-//			dijkstra(i);
-//			
-//			double maxDist = 0;
-//			
-//			for(int n=1; n<=N; n++)
-//				maxDist = Math.max(dist[n], maxDist);
-//			
-//			for(int n=1; n<=N; n++)
-//			{
-//				double res = 0;
-//				if(dist[n] == maxDist)
-//				{
-//					for(Node next=adNode[n]; next!=null; next=next.next)
-//					{
-//						double min = Math.min(dist[n], dist[next.node]);
-//						double max = Math.max(dist[n], dist[next.node]);
-//						res = Math.max(res, max + ((min + next.dist-max) / 2.0));
-//					}
-//					MIN = Math.min(MIN, res);
-//				}
-//			}
-//		}
-		
 		System.out.printf("%.1f",MIN);
 	}
-//	public static void dijkstra(int start) {
-//		pq		= new PriorityQueue<>((a,b)->a.dist-b.dist);
-//		dist	= new int[N+1];
-//		Arrays.fill(dist, MAX);
-//		dist[start] = 0;
-//		pq.add(new Point(start, 0));
-//		
-//		while(!pq.isEmpty())
-//		{
-//			Point now = pq.poll();
-//			
-//			for(Node next=adNode[now.node]; next!=null; next=next.next)
-//			{
-//				int nextDist = now.dist + next.dist;
-//				if(nextDist < dist[next.node])
-//				{
-//					dist[next.node] = nextDist;
-//					pq.add(new Point(next.node, nextDist));
-//				}
-//			}
-//		}
-//	}
 }
