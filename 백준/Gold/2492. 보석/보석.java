fee079c145e44652ad062381d5331f1d3e4c546d
YongJu Kim <kyjdummy@gmail.com>
2024-11-27 22:09:24 +0900

    [Gold III] Title: 보석, Time: 128 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/2492. 보석/보석.java
+++ 백준/Gold/2492. 보석/보석.java
@@ -1,54 +1,49 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2492
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Node{int x,y;Node(int x,int y){this.x=x;this.y=y;}}
 
 class Main{
-	
-	static int Y, X, CNT;
-	static int N, M, T, K;
-	static Node node[];
-	
+
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N	= Integer.parseInt(st.nextToken());	// 지도 넓이(1<=백만)
-		M	= Integer.parseInt(st.nextToken());	// 지도 높이(1<=백만)
-		T	= Integer.parseInt(st.nextToken());	// 금강석의 개수(1<=100)
-		K	= Integer.parseInt(st.nextToken());	// 정사각형의 크기(1<=백만)
-		node= new Node[T];
+		int N		= read();	// 지도 넓이(1<=백만)
+		int M		= read();	// 지도 높이(1<=백만)
+		int T		= read();	// 금강석의 개수(1<=100)
+		int K		= read();	// 정사각형의 크기(1<=백만)
+		Node[]node	= new Node[T];
 		
 		for(int i=0; i<T; i++)
-		{
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			node[i] = new Node(x,y);
-		}
+			node[i] = new Node(read(),read());
+		
+		int CNT = 0;
+		int Y	= 0;
+		int X	= 0;
 		
 		for(Node n1 : node)
 			for(Node n2 : node)
 			{
-				int x	= n1.x + K > N ? N - K : n1.x;
-				int y	= n2.y + K > M ? M - K : n2.y;
+				int x	= n1.x + K > N ? N - K : n1.x;	// 시작x , x+K가 범위초과시 x를 N-K로 세팅
+				int y	= n2.y + K > M ? M - K : n2.y;	// 시작y , x와 마찬가지로 범위 초과시 값세팅
 				int cnt	= 0;
+				
 				for(Node n : node) 
 					if(x<=n.x && n.x<=x+K && y<=n.y && n.y<=y+K)
 						++cnt;
 				
 				if(CNT < cnt)
 				{
-					CNT = cnt;
-					Y = y + K;
-					X = x;
+					CNT	= cnt;
+					Y	= y + K;
+					X	= x;
 				}
 			}
 		
 		StringBuilder sb = new StringBuilder();
 		sb.append(X).append(' ').append(Y).append('\n').append(CNT);
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
