881df3a9e6f9885ab1d94b4ef6a24681e726613d
YongJu Kim <kyjdummy@gmail.com>
2024-11-28 16:50:45 +0900

    [Gold I] Title: 피아의 아틀리에 ~신비한 대회의 연금술사~, Time: 1716 ms, Memory: 304100 KB -BaekjoonHub

--- 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
+++ 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
@@ -6,11 +6,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Node{	int value;char ch;Node(int v, char c){value=v; ch=c;} Node(){value=0;ch='W';} }
-class Final{ int y, x, idx;Final(int y, int x, int i){this.y=y; this.x=x; idx=i;} Final(){y=0;x=0;idx=0;}}
+class Final{ int y, x, idx, dir;Final(int y, int x, int i, int d){this.y=y; this.x=x; idx=i;dir=d;} Final(){y=0;x=0;idx=0;dir=0;}}
 
 class Main{
 	
-	static Node[][][] origin;
+	static Node[][][][] origin;
 	static Node[][] result, dummy;
 	static Final finalData[];
 	static int N, max;
@@ -18,7 +18,7 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N			= Integer.parseInt(br.readLine());	// 재료 개수(3 ≤ n ≤ 10)
-		origin		= new Node[N][4][4];
+		origin		= new Node[4][N][4][4];
 		dummy		= new Node[5][5];
 		result		= new Node[5][5];
 		finalData	= new Final[3];
@@ -33,27 +33,48 @@ class Main{
 			{
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for(int x=0; x<4; x++)
-					origin[i][y][x] = new Node(Integer.parseInt(st.nextToken()),'W');
+					origin[0][i][y][x] = new Node(Integer.parseInt(st.nextToken()),'W');
 			}
 			// 초기 원소 값(알파벳)
 			for(int y=0; y<4; y++)
 			{
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for(int x=0; x<4; x++)
-					origin[i][y][x].ch = st.nextToken().charAt(0);
+					origin[0][i][y][x].ch = st.nextToken().charAt(0);
 			}
 		}
-		
 		for(int y=0; y<5; y++)
 			for(int x=0; x<5; x++)
 			{
-				dummy[y][x] = new Node();
-				result[y][x] = new Node();
+				dummy[y][x]	= new Node();
+				result[y][x]= new Node();
 			}
 		
+		for(int i=1; i<4; i++)
+			origin[i] = rotate(origin[i-1]);
+
 		bruteforce(0, 0);		
 		
 		System.out.print(max);
+	}
+	public static Node[][][] rotate(Node o[][][]) {
+		Node arr[][][] = copy(o);
+		for(int i=0; i<N; i++)
+		{
+			for(int y=0; y<4; y++)
+				for(int x=0; x<4; x++)
+				{
+					dummy[y][x].value = arr[i][y][x].value;
+					dummy[y][x].ch = arr[i][y][x].ch;
+				}
+			for(int y=0; y<4; y++)
+				for(int x=0; x<4; x++)
+				{
+					arr[i][y][x].value = dummy[3-x][y].value;
+					arr[i][y][x].ch = dummy[3-x][y].ch;
+				}
+		}
+		return arr;
 	}
 	public static void bruteforce(int depth, int bitmask) {
 		if(depth == 3)
@@ -69,14 +90,14 @@ class Main{
 			{
 				for(int dir=0; dir<4; dir++)//현재(0),90도(1),180도(2),270(3)
 				{
-					rotate(origin[i]);
 					for(int y=0; y<=1; y++)
 					{
 						for(int x=0; x<=1; x++)
 						{
 							finalData[depth].y=y;
 							finalData[depth].x=x;
-							finalData[depth].idx=i;							
+							finalData[depth].idx=i;
+							finalData[depth].dir=dir;
 							bruteforce(depth + 1, bitmask | flag);
 						}
 					}
@@ -84,19 +105,15 @@ class Main{
 			}
 		}
 	}
-	public static void rotate(Node arr[][]) {
-		for(int y=0; y<4; y++)
-			for(int x=0; x<4; x++)
-			{
-				dummy[y][x].value = arr[y][x].value;
-				dummy[y][x].ch = arr[y][x].ch;
-			}
-		for(int y=0; y<4; y++)
-			for(int x=0; x<4; x++)
-			{
-				arr[y][x].value = dummy[3-x][y].value;
-				arr[y][x].ch = dummy[3-x][y].ch;
-			}
+	public static Node[][][] copy(Node[][][] o) {
+		Node[][][] result= new Node[N][4][4];
+		
+		for(int n=0; n<N; n++)
+			for(int y=0; y<4; y++)
+				for(int x=0; x<4; x++)
+					result[n][y][x] = new Node(o[n][y][x].value, o[n][y][x].ch);
+		
+		return result;
 	}
 	public static void cal() {
 		for(int y=0; y<5; y++)
@@ -107,7 +124,7 @@ class Main{
 			}
 		
 		for(int i=0; i<3; i++)
-			result = INPUT( result , finalData[i].idx, finalData[i].y, finalData[i].x);
+			result = INPUT( result , finalData[i].idx, finalData[i].y, finalData[i].x, finalData[i].dir);
 		
 		// 폭탄의 품질 = 7R + 5B + 3G + 2Y
 		int sum[] = new int['Z'];
@@ -118,13 +135,13 @@ class Main{
 		
 		max = Math.max(max, 7*sum['R'] + 5*sum['B'] + 3*sum['G'] + 2*sum['Y']);
 	}	
-	public static Node[][] INPUT(Node[][] nextResult, int idx, int Y, int X){
+	public static Node[][] INPUT(Node[][] nextResult, int idx, int Y, int X, int dir){
 		for(int y=0; y<4; y++)
 			for(int x=0; x<4; x++)
 			{
-				nextResult[y+Y][x+X].value += origin[idx][y][x].value;
-				if(origin[idx][y][x].ch != 'W')
-					nextResult[y+Y][x+X].ch = origin[idx][y][x].ch;
+				nextResult[y+Y][x+X].value += origin[dir][idx][y][x].value;
+				if(origin[dir][idx][y][x].ch != 'W')
+					nextResult[y+Y][x+X].ch = origin[dir][idx][y][x].ch;
 				
 				if(nextResult[y+Y][x+X].value < 0)
 					nextResult[y+Y][x+X].value = 0;
