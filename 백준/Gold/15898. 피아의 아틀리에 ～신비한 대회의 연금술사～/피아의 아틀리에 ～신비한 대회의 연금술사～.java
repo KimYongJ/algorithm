2da0ef74d01864db258c2608ed51b84f5a3bb4dc
YongJu Kim <kyjdummy@gmail.com>
2024-11-28 17:15:10 +0900

    [Gold I] Title: 피아의 아틀리에 ~신비한 대회의 연금술사~, Time: 1220 ms, Memory: 304812 KB -BaekjoonHub

--- 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
+++ 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
@@ -1,6 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/15898
-// 맵크기 5*5 / 재료는 최대 3개넣음 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -49,7 +48,7 @@ class Main{
 				dummy[y][x]	= new Node();
 				result[y][x]= new Node();
 			}
-		
+		// 각 재료에 대해 미리 모든 회전을 저장해 놓음
 		for(int i=1; i<4; i++)
 			origin[i] = rotate(origin[i-1]);
 
@@ -58,7 +57,13 @@ class Main{
 		System.out.print(max);
 	}
 	public static Node[][][] rotate(Node o[][][]) {
-		Node arr[][][] = copy(o);
+		Node[][][] arr = new Node[N][4][4];
+		
+		for(int n=0; n<N; n++)
+			for(int y=0; y<4; y++)
+				for(int x=0; x<4; x++)
+					arr[n][y][x] = new Node(o[n][y][x].value, o[n][y][x].ch);
+		
 		for(int i=0; i<N; i++)
 		{
 			for(int y=0; y<4; y++)
@@ -82,47 +87,28 @@ class Main{
 			cal();
 			return;
 		}
-		// 필요한거, 깊이에 따른넣을 y,x좌표  + 재료 idx
 		for(int i=0; i<N; i++)
-		{
-			int flag = 1<<i;
-			if((bitmask & flag) == 0)
-			{
+			if((bitmask & (1<<i)) == 0)
 				for(int dir=0; dir<4; dir++)//현재(0),90도(1),180도(2),270(3)
-				{
 					for(int y=0; y<=1; y++)
-					{
 						for(int x=0; x<=1; x++)
 						{
-							finalData[depth].y=y;
-							finalData[depth].x=x;
-							finalData[depth].idx=i;
-							finalData[depth].dir=dir;
-							bruteforce(depth + 1, bitmask | flag);
+							finalData[depth].y		= y;
+							finalData[depth].x		= x;
+							finalData[depth].idx	= i;
+							finalData[depth].dir	= dir;
+							bruteforce(depth + 1, bitmask | (1<<i));
 						}
-					}
-				}
-			}
-		}
-	}
-	public static Node[][][] copy(Node[][][] o) {
-		Node[][][] result= new Node[N][4][4];
-		
-		for(int n=0; n<N; n++)
-			for(int y=0; y<4; y++)
-				for(int x=0; x<4; x++)
-					result[n][y][x] = new Node(o[n][y][x].value, o[n][y][x].ch);
-		
-		return result;
 	}
 	public static void cal() {
+		// 결과 연산을 위한 result 초기화
 		for(int y=0; y<5; y++)
 			for(int x=0; x<5; x++)
 			{
 				result[y][x].value = 0;
 				result[y][x].ch = 'W';
 			}
-		
+		// result에 값을 하나씩 넣는다.
 		for(int i=0; i<3; i++)
 			result = INPUT( result , finalData[i].idx, finalData[i].y, finalData[i].x, finalData[i].dir);
 		
@@ -139,14 +125,9 @@ class Main{
 		for(int y=0; y<4; y++)
 			for(int x=0; x<4; x++)
 			{
-				nextResult[y+Y][x+X].value += origin[dir][idx][y][x].value;
+				nextResult[y+Y][x+X].value = Math.min(9,Math.max(0,nextResult[y+Y][x+X].value + origin[dir][idx][y][x].value));
 				if(origin[dir][idx][y][x].ch != 'W')
 					nextResult[y+Y][x+X].ch = origin[dir][idx][y][x].ch;
-				
-				if(nextResult[y+Y][x+X].value < 0)
-					nextResult[y+Y][x+X].value = 0;
-				else if(9 < nextResult[y+Y][x+X].value)
-					nextResult[y+Y][x+X].value = 9;
 			}
 		
 		return nextResult;
