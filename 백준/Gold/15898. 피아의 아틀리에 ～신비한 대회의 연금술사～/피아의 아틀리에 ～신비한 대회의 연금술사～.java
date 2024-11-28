5d0cdecd0f6b441a736d79aa145bfe1b3dfe95ac
YongJu Kim <kyjdummy@gmail.com>
2024-11-28 16:34:23 +0900

    [Gold I] Title: 피아의 아틀리에 ~신비한 대회의 연금술사~, Time: 1784 ms, Memory: 312044 KB -BaekjoonHub

--- 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
+++ 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
@@ -6,11 +6,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Node{	int value;char ch;Node(int v, char c){value=v; ch=c;} Node(){value=0;ch='W';} }
+class Final{ int y, x, idx;Final(int y, int x, int i){this.y=y; this.x=x; idx=i;} Final(){y=0;x=0;idx=0;}}
 
 class Main{
 	
-	static Node origin[][][];
-	static Node dummy[][];
+	static Node[][][] origin;
+	static Node[][] result, dummy;
+	static Final finalData[];
 	static int N, max;
 	
 	public static void main(String[] args)throws Exception{
@@ -18,6 +20,12 @@ class Main{
 		N			= Integer.parseInt(br.readLine());	// 재료 개수(3 ≤ n ≤ 10)
 		origin		= new Node[N][4][4];
 		dummy		= new Node[5][5];
+		result		= new Node[5][5];
+		finalData	= new Final[3];
+		
+		for(int i=0; i<3; i++)
+			finalData[i] = new Final();
+		
 		for(int i=0; i<N; i++)
 		{
 			// 초기 효능 값(숫자)
@@ -36,24 +44,24 @@ class Main{
 			}
 		}
 		
-		Node[][]param= new Node[5][5];
 		for(int y=0; y<5; y++)
 			for(int x=0; x<5; x++)
 			{
-				param[y][x] = new Node();
 				dummy[y][x] = new Node();
+				result[y][x] = new Node();
 			}
 		
-		bruteforce(0, 0, param);		
+		bruteforce(0, 0);		
 		
 		System.out.print(max);
 	}
-	public static void bruteforce(int depth, int bitmask, Node[][] result) {
+	public static void bruteforce(int depth, int bitmask) {
 		if(depth == 3)
 		{
-			cal(result);
+			cal();
 			return;
 		}
+		// 필요한거, 깊이에 따른넣을 y,x좌표  + 재료 idx
 		for(int i=0; i<N; i++)
 		{
 			int flag = 1<<i;
@@ -66,9 +74,10 @@ class Main{
 					{
 						for(int x=0; x<=1; x++)
 						{
-							Node[][] nextResult = copy(result);
-							nextResult = INPUT( nextResult , i, y, x); // 순서 : 최종결과배열, 넣을 재료 위치, 최종결과에넣을 시작y좌표, 최종결과에넣을 시작x좌표, 방향
-							bruteforce(depth + 1, bitmask | flag, nextResult);
+							finalData[depth].y=y;
+							finalData[depth].x=x;
+							finalData[depth].idx=i;							
+							bruteforce(depth + 1, bitmask | flag);
 						}
 					}
 				}
@@ -89,7 +98,26 @@ class Main{
 				arr[y][x].ch = dummy[3-x][y].ch;
 			}
 	}
-	
+	public static void cal() {
+		for(int y=0; y<5; y++)
+			for(int x=0; x<5; x++)
+			{
+				result[y][x].value = 0;
+				result[y][x].ch = 'W';
+			}
+		
+		for(int i=0; i<3; i++)
+			result = INPUT( result , finalData[i].idx, finalData[i].y, finalData[i].x);
+		
+		// 폭탄의 품질 = 7R + 5B + 3G + 2Y
+		int sum[] = new int['Z'];
+
+		for(int y=0; y<5; y++)
+			for(int x=0; x<5; x++)
+				sum[result[y][x].ch] += result[y][x].value;
+		
+		max = Math.max(max, 7*sum['R'] + 5*sum['B'] + 3*sum['G'] + 2*sum['Y']);
+	}	
 	public static Node[][] INPUT(Node[][] nextResult, int idx, int Y, int X){
 		for(int y=0; y<4; y++)
 			for(int x=0; x<4; x++)
@@ -105,24 +133,5 @@ class Main{
 			}
 		
 		return nextResult;
-	}
-	public static Node[][] copy(Node[][] origin){
-		Node[][] result = new Node[5][5];
-		
-		for(int y=0; y<5; y++)
-			for(int x=0; x<5; x++)
-				result[y][x] = new Node(origin[y][x].value, origin[y][x].ch);
-		
-		return result;
-	}
-	public static void cal(Node[][] result) {
-		// 폭탄의 품질 = 7R + 5B + 3G + 2Y
-		int sum[] = new int['Z'];
-
-		for(int y=0; y<5; y++)
-			for(int x=0; x<5; x++)
-				sum[result[y][x].ch] += result[y][x].value;
-		
-		max = Math.max(max, 7*sum['R'] + 5*sum['B'] + 3*sum['G'] + 2*sum['Y']);
 	}
 }
