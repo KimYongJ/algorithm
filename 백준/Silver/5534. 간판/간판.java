64814ac1fd8764be4cb8eb27b1b2c89462e77826
YongJu Kim <kyjdummy@gmail.com>
2024-12-04 22:45:51 +0900

    [Silver IV] Title: 간판, Time: 340 ms, Memory: 37640 KB -BaekjoonHub

---
+++ 백준/Silver/5534. 간판/간판.java
@@ -0,0 +1,45 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/5534
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Main{
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N		= Integer.parseInt(br.readLine());
+		String base	= br.readLine();
+		int cnt		= 0;
+		
+		LOOP:
+		for(int i=0; i<N; i++)
+		{
+			String compare = br.readLine();
+			int maxInterval = compare.length();
+			int interval = 0;
+			
+			while(++interval <= maxInterval)
+			{
+				for(int start=0; start<compare.length(); start++)
+				{
+					sb.setLength(0);
+					
+					int s = start;
+					while(s<compare.length() && sb.length() != base.length())
+					{
+						sb.append(compare.charAt(s));
+						s+=interval;
+					}
+					if(base.equals(sb.toString()))
+					{
+						++cnt;
+						continue LOOP;
+					}
+				}
+			}
+			
+		}
+		
+		System.out.print(cnt);
+	}
+}
