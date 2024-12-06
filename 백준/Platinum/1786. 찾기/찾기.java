86b10c0801cba3c17130a3dd81fc792a57755ae5
YongJu Kim <kyjdummy@gmail.com>
2024-12-06 17:03:39 +0900

    [Platinum V] Title: 찾기, Time: 396 ms, Memory: 40192 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,29 +1,24 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1786
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[] text = br.readLine().toCharArray();
-		char[] pattern = br.readLine().toCharArray();
-		int tlen = text.length;
-		int plen = pattern.length;
-		int fail[] = new int[plen];
+		StringBuilder sb = new StringBuilder();
+		char[] text		= readString().toCharArray();
+		char[] pattern	= readString().toCharArray();
+		int tlen		= text.length;
+		int plen		= pattern.length;
+		int fail[]		= new int[plen];
+		int cnt			= 0;
 		
 		for(int i=1, j=0; i<plen; i++)
 		{
 			while(0<j && pattern[i] != pattern[j])
 				j = fail[j - 1];
+			
 			if(pattern[i] == pattern[j])
 				fail[i] = ++j;
 		}
-		
-		ArrayList<Integer> ans = new ArrayList<>();
-		
+
 		for(int i=0, j=0; i<tlen; i++)
 		{
 			while(0<j && text[i] != pattern[j])
@@ -33,19 +28,24 @@ class Main{
 			{
 				if(j == plen - 1)
 				{
-					ans.add(i - j + 1);
+					sb.append(i - j + 1).append(' ');
 					j = fail[j];
+					++cnt;
 				}
 				else ++j;
 			}
 		}
-		
-		Collections.sort(ans);
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append(ans.size()).append('\n');
-		for(int a : ans)
-			sb.append(a).append(' ');
+		sb.insert(0, '\n');
+		sb.insert(0, cnt);
 		System.out.print(sb);
 	}
+    public static String readString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = System.in.read()) > 13) {
+            sb.append((char) c);
+        }
+        return sb.toString();
+    }
 }
+
