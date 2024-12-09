2d17550b07a4deb26893bf28802289a0000c9295
YongJu Kim <kyjdummy@gmail.com>
2024-12-09 14:20:51 +0900

    [Platinum IV] Title: 광고, Time: 228 ms, Memory: 21812 KB -BaekjoonHub

--- 백준/Platinum/1305. 광고/광고.java
+++ 백준/Platinum/1305. 광고/광고.java
@@ -1,24 +1,31 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1305
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		char[] str = br.readLine().toCharArray();
-		int len = str.length;
-		int []fail = new int[len];
+		int N				= read();
+		StringBuilder sb	= readString();
+		int len				= sb.length();
+		int []fail			= new int[len];
 		
 		for(int i=1,j=0; i<len; i++)
 		{
-			while(0<j && str[i] != str[j])
+			while(0<j && sb.charAt(i) != sb.charAt(j))
 				j = fail[j - 1];
-			if(str[i] == str[j])
+			if(sb.charAt(i) == sb.charAt(j))
 				fail[i] = ++j;
 		}
 		
 		System.out.print(N - fail[len-1]);
 	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+    public static StringBuilder readString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = System.in.read()) > 13) {sb.append((char) c);}
+        return sb;
+    }
 }
