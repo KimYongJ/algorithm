732e11632ac351f9c8a6a961a7afd613700394a3
YongJu Kim <kyjdummy@gmail.com>
2024-12-10 13:39:47 +0900

    [Platinum II] Title: 최대 문자열 붙여넣기, Time: 704 ms, Memory: 82636 KB -BaekjoonHub

---
+++ 백준/Platinum/2401. 최대 문자열 붙여넣기/최대 문자열 붙여넣기.java
@@ -0,0 +1,81 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/2401
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Main{
+	
+	static String	text, pattern[];
+	static int		N, tlen;
+	static int[]	DP, len;
+	static int[][]	fail;
+	static boolean	match[][];
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		text	= br.readLine();					// 긴 문자열(1<=십만)
+		tlen	= text.length();
+		N		= Integer.parseInt(br.readLine());	// 짧은 문자열의 개수(1<=500)
+		pattern = new String[N];					// 짧은 문자열을 담을 배열(1<=만)
+		len		= new int[N];
+		match	= new boolean[N][text.length()];
+		fail	= new int[N][];
+		DP		= new int[tlen+1];
+		
+		
+		for(int i=0; i<N; i++)
+		{
+			pattern[i]	= br.readLine();				// 짧은 문자열을 배열에 저장
+			len[i]		= pattern[i].length();			// 짧은 문자열의 길이를 저장
+			fail[i]		= getFail(pattern[i], len[i]);	// 짧은 문자열의 fail값 저장
+			KMP(pattern[i],i);							// 구한 fail을 통해 바로 KMP알고리즘 실행
+		}
+		
+        // 바텀업 방식으로 DP 계산
+        for (int i = 0; i < tlen; i++) {
+            DP[i + 1] = Math.max(DP[i + 1], DP[i]); // 현재 위치까지 최대값 유지
+
+            for (int n = 0; n < N; n++) {
+                if (match[n][i]) {
+                    int endIdx = i + len[n];
+                    if (endIdx <= tlen) { // 범위 초과 방지
+                        DP[endIdx] = Math.max(DP[endIdx], DP[i] + len[n]);
+                    }
+                }
+            }
+        }
+		System.out.print(DP[tlen]);
+	}
+	
+	public static void KMP(String pattern, int idx) {
+		for(int i=0, j=0; i<tlen; i++)
+		{
+			while(0<j && text.charAt(i) != pattern.charAt(j))
+				j = fail[idx][j - 1];
+			
+			if(text.charAt(i) == pattern.charAt(j))
+			{
+				if(j == len[idx] - 1)
+				{
+					match[idx][i - j] = true;			// 해당 문자의 시작점에 true로 마킹
+					j = fail[idx][j];
+				}
+				else ++j;
+			}
+		}
+	}
+	public static int[] getFail(String pattern, int LEN) {
+		int fail[] = new int[LEN];
+		
+		for(int i=1, j=0; i<LEN; i++)
+		{
+			while(0<j && pattern.charAt(i) != pattern.charAt(j))
+				j = fail[j - 1];
+			
+			if(pattern.charAt(i) == pattern.charAt(j))
+				fail[i] = ++j;
+		}
+		
+		return fail;
+	}
+}
+
