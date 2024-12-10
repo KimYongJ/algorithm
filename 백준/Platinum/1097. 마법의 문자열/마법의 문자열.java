bbadd8d82ae5dbff70dbf4da889afb96727f09cf
YongJu Kim <kyjdummy@gmail.com>
2024-12-10 17:29:04 +0900

    [Platinum V] Title: 마법의 문자열, Time: 664 ms, Memory: 59104 KB -BaekjoonHub

--- 백준/Platinum/1097. 마법의 문자열/마법의 문자열.java
+++ 백준/Platinum/1097. 마법의 문자열/마법의 문자열.java
@@ -7,35 +7,42 @@ class Main{
 	static int ans;
 	static int N, K;
 	static String str[];
+	static StringBuilder sb;
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N	= Integer.parseInt(br.readLine());	// 단어의 개수 N(1<8)
-		str	= new String[N];
+		N		= Integer.parseInt(br.readLine());	// 단어의 개수 N(1<8)
+		str		= new String[N];
+		sb		= new StringBuilder();
 		
 		for(int i=0; i<N; i++)
-			str[i] = br.readLine();// 최대길이 20 문자열
+			str[i] = br.readLine();					// 최대길이 20 문자열
 		
-		K = Integer.parseInt(br.readLine());	// 1<=200
+		K = Integer.parseInt(br.readLine());		// 1<=200
 
-		bruteforce(0, 0, "");
+		bruteforce(0, 0);
 		
 		System.out.print(ans);
 	}
-	public static void bruteforce(int depth, int bitmask, String text) {
+	public static void bruteforce(int depth, int bitmask) {
 		if(depth == N)
 		{
-			KMP(text + text, text);
+			KMP(sb.toString());
 			return;
 		}
 		for(int i=0; i<N; i++)
 		{
 			int flag = 1<<i;
 			if((bitmask & flag) == 0)
-				bruteforce(depth + 1, bitmask | flag, text + str[i]);
+			{
+				int oldLen = sb.length();
+				sb.append(str[i]);
+				bruteforce(depth + 1, bitmask | flag);
+				sb.setLength(oldLen);
+			}
 		}
 	}
-	public static void KMP(String text, String pattern) {
+	public static void KMP(String pattern) {
 		int plen	= pattern.length();
 		int fail[]	= new int[plen];
 		// fail 배열 구하기
@@ -48,14 +55,15 @@ class Main{
 				fail[i] = ++j;
 		}
 		// KMP알고리즘
-		int tlen = text.length() - 1;
+		int tlen = (pattern.length() * 2) - 1;
 		int cnt = 0;
 		for(int i=0, j=0; i<tlen; i++)
 		{
-			while(0<j && text.charAt(i) != pattern.charAt(j))
+			int idx = i % plen;
+			while(0<j && pattern.charAt(idx) != pattern.charAt(j))
 				j = fail[j - 1];
 			
-			if(text.charAt(i) == pattern.charAt(j))
+			if(pattern.charAt(idx) == pattern.charAt(j))
 			{
 				if(j+1 == plen)
 				{
