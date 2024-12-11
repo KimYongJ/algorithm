f401907236729eaa907be89d52a90ec24bcf096d
YongJu Kim <kyjdummy@gmail.com>
2024-12-11 22:38:40 +0900

    [Gold V] Title: Secret, Time: 156 ms, Memory: 16188 KB -BaekjoonHub

--- 백준/Gold/13220. Secret/Secret.java
+++ 백준/Gold/13220. Secret/Secret.java
@@ -1,25 +1,23 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/13220
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String args[]) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N			= Integer.parseInt(br.readLine());	// 1<=100000
+		int N			= read();	// 1<=100000
 		int len			= N<<1;
 		int text[]		= new int[len];
 		int pattern[]	= new int[N];
 		int fail[]	 	= new int[N];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0,j=N; i<N; i++,j++)
-			text[j] = text[i] = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
+			text[j] = text[i] = read();
+		
 		for(int i=0; i<N; i++)
-			pattern[i] = Integer.parseInt(st.nextToken());
+			pattern[i] = read();
 		
 		for(int i=1, j=0; i<N; i++)
 		{
