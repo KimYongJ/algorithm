fd6e4ffd92aa1f693155ffdc1cdc4af2acb8f436
YongJu Kim <kyjdummy@gmail.com>
2024-12-11 13:47:33 +0900

    [Platinum IV] Title: 시계 사진들, Time: 200 ms, Memory: 18120 KB -BaekjoonHub

--- 백준/Platinum/10266. 시계 사진들/시계 사진들.java
+++ 백준/Platinum/10266. 시계 사진들/시계 사진들.java
@@ -1,22 +1,24 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/10266
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		final int MAX	= 360_000;
 		final int LEN	= MAX<<1;
 		int fail[]		= new int[MAX];
 		boolean patn[]	= new boolean[MAX];
 		boolean text[]	= new boolean[LEN];
-		int N			= Integer.parseInt(br.readLine());// 바늘수(2 ≤ 200 000)
+		int N			= read();// 바늘수(2 ≤ 200 000)
+		
 		// (0 ≤ n < 360,000)
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)text[Integer.parseInt(st.nextToken())] = true;
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)patn[Integer.parseInt(st.nextToken())] = true;
+		for(int i=0; i<N; i++)text[read()] = true;
+		for(int i=0; i<N; i++)patn[read()] = true;
 		
 		for(int i=0, j=MAX; i<MAX;)
 			text[j++] = text[i++];
