62fc753c0f3f3ca3be1ad8be425e07502acd3102
YongJu Kim <kyjdummy@gmail.com>
2024-12-12 10:53:06 +0900

    [Platinum IV] Title: 주기문, Time: 456 ms, Memory: 80944 KB -BaekjoonHub

--- 백준/Platinum/1498. 주기문/주기문.java
+++ 백준/Platinum/1498. 주기문/주기문.java
@@ -1,23 +1,26 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1498
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+    public static String readString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = System.in.read()) > 13) {sb.append((char) c);}
+        return sb.toString();
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char text[] = br.readLine().toCharArray();
+		StringBuilder sb = new StringBuilder();
+		char text[] = readString().toCharArray();
 		int len		= text.length;
 		int fail[]	= new int[len];
-		
+		// fail을 구함과 동시에 답도 구한다. 
 		for(int i=1,j=0; i<len; i++)
 		{
 			while(0<j && text[i] != text[j])
 				j = fail[j - 1];
+			
 			if(text[i] == text[j])
 				fail[i] = ++j;
-		}
-		StringBuilder sb = new StringBuilder();
-		for(int i=1; i<len; i++)
+			// 답구하는 부분
 			if(fail[i] != 0)
 			{
 				int idx		= i + 1;
@@ -25,7 +28,7 @@ class Main{
 				if(idx % period == 0)
 					sb.append(idx).append(' ').append(idx / period).append('\n');
 			}
-
+		}
 		System.out.print(sb.toString());
 	}
 }
