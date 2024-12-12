bc698f1fbbfe3fe222e79195c00a5b6d2bc79ad0
YongJu Kim <kyjdummy@gmail.com>
2024-12-12 10:07:57 +0900

    [Platinum IV] Title: 라디오 전송, Time: 188 ms, Memory: 23648 KB -BaekjoonHub

--- 백준/Platinum/3356. 라디오 전송/라디오 전송.java
+++ 백준/Platinum/3356. 라디오 전송/라디오 전송.java
@@ -1,13 +1,20 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3356
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+    public static String readString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = System.in.read()) > 13) {sb.append((char) c);}
+        return sb.toString();
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int len		= Integer.parseInt(br.readLine());
-		char ptn[]	= br.readLine().toCharArray();
+		int len		= read();
+		char ptn[]	= readString().toCharArray();
 		int fail[]	= new int[len];
 		
 		for(int i=1, j=0; i<len; i++)
