a303d96860563a1f3ab2caa5aa984f2729dbc39b
YongJu Kim <kyjdummy@gmail.com>
2024-12-12 14:49:33 +0900

    [Platinum IV] Title: 체커, Time: 152 ms, Memory: 20176 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.java
+++ 백준/Platinum/1090. 체커/체커.java
@@ -1,33 +1,29 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1090
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Location{int x,y;Location(int x, int y){this.x=x; this.y=y;}}
 
 class Main{
-	
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N				= Integer.parseInt(st.nextToken());
+		int N				= read();
 		int result[]		= new int[N];
 		Location[] location	= new Location[N];
 		
 		Arrays.fill(result, 1<<30);
 		
 		for(int i=0; i<N; i++)
-		{
-			st = new StringTokenizer(br.readLine());
-			location[i] = new Location(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		}
+			location[i] = new Location(read(), read());
+		
 		for(int i=0; i<N; i++)
-		{
 			for(int j=0; j<N; j++)
 			{
-				int cost[] = new int[N];	// 각 체커마다 정해진 x,y좌표로 이동하는데 드는 비용
+				int cost[] = new int[N];// 각 체커마다 정해진 x,y좌표로 이동하는데 드는 비용
 				for(int n=0; n<N; n++)
 					cost[n] = Math.abs(location[i].x - location[n].x) + Math.abs(location[j].y - location[n].y);
 				// 비용 오름차순 정렬
@@ -37,7 +33,6 @@ class Main{
 				for(int n=0; n<N; n++)
 					result[n] = Math.min(result[n],res += cost[n]);
 			}
-		}
 		
 		StringBuilder sb = new StringBuilder();
 		for(int r : result)
