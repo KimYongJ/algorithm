f0ab4567cb6abfdd592f79a3d7f42d8c0caee4cf
YongJu Kim <kyjdummy@gmail.com>
2024-12-12 10:06:31 +0900

    [Platinum IV] Title: 라디오 전송, Time: 236 ms, Memory: 25868 KB -BaekjoonHub

--- 백준/Platinum/3356. 라디오 전송/라디오 전송.java
+++ 백준/Platinum/3356. 라디오 전송/라디오 전송.java
@@ -6,20 +6,18 @@ import java.io.InputStreamReader;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
-		int fail[]	= getFail(N, br.readLine());
-		System.out.print(N - fail[N-1]);
-	}
-	public static int[] getFail(int len, String pattern) {
+		int len		= Integer.parseInt(br.readLine());
+		char ptn[]	= br.readLine().toCharArray();
 		int fail[]	= new int[len];
 		
 		for(int i=1, j=0; i<len; i++)
 		{
-			while(0<j && pattern.charAt(i) != pattern.charAt(j))
+			while(0<j && ptn[i] != ptn[j])
 				j = fail[j - 1];
-			if(pattern.charAt(i) == pattern.charAt(j))
+			
+			if(ptn[i] == ptn[j])
 				fail[i] = ++j;
 		}
-		return fail;
+		System.out.print(len - fail[len-1]);
 	}
 }
