8ccf37374d9cb51fa3b5a67d391a04ea6987977e
YongJu Kim <kyjdummy@gmail.com>
2024-12-12 22:01:43 +0900

    [Platinum III] Title: Slot Machines, Time: 280 ms, Memory: 22932 KB -BaekjoonHub

--- 백준/Platinum/14959. Slot Machines/Slot Machines.java
+++ 백준/Platinum/14959. Slot Machines/Slot Machines.java
@@ -1,23 +1,21 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14959
-// K(고유한 난수길이)와 P(패턴의 길이)출력, 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int K		= 0;
 		int P		= 0;
-		int N		= Integer.parseInt(br.readLine());// 1<=백만
+		int N		= read();// 1<=백만
 		int arr1[]	= new int[N];
 		int fail[]	= new int[N];
+		int total	= 1<<30;
 		
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=N-1; i>=0; i--)
-			arr1[i] = Integer.parseInt(st.nextToken());
+			arr1[i] = read();
 
 		for(int i=1,j=0; i<N; i++)
 		{
@@ -27,12 +25,12 @@ class Main{
 			if(arr1[i] == arr1[j])
 				fail[i] = ++j;
 		}
-		int total = 1<<30;
 		for(int i=0; i<N; i++)
 		{
 			int kk = N - (i+1);
 			int pp = (i+1) - fail[i];
-			if(kk+pp < total) {
+			if(kk+pp < total)
+			{
 				total = kk+pp;
 				K = kk;
 				P = pp;
