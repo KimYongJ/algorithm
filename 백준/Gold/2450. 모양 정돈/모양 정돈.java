bafc712f000c6f72108d2e452c796cc63be7c602
YongJu Kim <kyjdummy@gmail.com>
2024-12-13 13:46:16 +0900

    [Gold II] Title: 모양 정돈, Time: 132 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/2450. 모양 정돈/모양 정돈.java
+++ 백준/Gold/2450. 모양 정돈/모양 정돈.java
@@ -1,46 +1,50 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2450
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
 		final int comb[][] = {{1,2,3},{1,3,2},{2,1,3},{2,3,1},{3,1,2},{3,2,1}};
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[]	= new int[N];
 		int CNT[]	= new int[4];
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		for(int i=0; i<N; i++)
-		{
-			arr[i] = Integer.parseInt(st.nextToken());
-			CNT[arr[i]]++;
-		}
+			CNT[arr[i] = read()]++;
 		
 		int min = 1<<30;
 		for(int c[] : comb)
 		{
 			int changeCnt[] = new int[4];
 			int cnt = 0;
-			// 첫번째 구간 나와야할 숫자가 안나오면, 그 안나온 숫자의 개수를 세준다.
-			for(int i=0; i<CNT[c[0]]; i++)
+			int len = CNT[c[0]];
+			int len2= len + CNT[c[1]];
+			
+			// 첫번째 구간 나와야할 숫자가 안나오면, 첫번 째 구간 숫자가 아닌 숫자에 +1을 더해 마킹한다.
+			for(int i=0; i<len; i++)
 				if(arr[i] != c[0])
 				{
-					++changeCnt[arr[i]];	// 바꾼 그 숫자를 더 한다.
-					++cnt; // 결과 추가
+					++changeCnt[arr[i]];// 바꾼 숫자를 마킹한다.
+					++cnt;				// 바꾼 횟수 추가
 				}
-			for(int i=CNT[c[0]]; i<CNT[c[1]] + CNT[c[0]]; i++)
-				if(arr[i] == c[0])
+			// 두번 째 구간 탐색
+			for(int i=len; i<len2; i++)
+			{
+				if(arr[i] == c[0])			// 두번째 구간에서 첫번 째 숫자를 만난 경우
 				{
-					if(changeCnt[c[1]] != 0)
-						--changeCnt[c[1]];
-					else ++cnt;
+					if(changeCnt[c[1]] != 0)// 첫번째 구간에서 두번째 숫자를 만난 적이있다면,
+						--changeCnt[c[1]];	// 교체한 카운트 차감
+					else ++cnt;				// 만난 적이 없다면 무조건 바꿔야 하므로 cnt+1
 				}
+				// 두번 째 구간에서 세번째 숫자를 만난 경우 무조건 바꿔야 하므로 cnt+1
 				else if(arr[i] == c[2])
-				{
 					++cnt;
-				}
+			}
 			
+			// 최소값 갱신
 			if(cnt < min)
 				min = cnt;
 		}
