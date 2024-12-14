4389398ac1ff455d64dd94e6f5e3db570daf09aa
YongJu Kim <kyjdummy@gmail.com>
2024-12-14 22:42:00 +0900

    [Bronze I] Title: 오렌지먹은지오랜지, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/27962. 오렌지먹은지오랜지/오렌지먹은지오랜지.java
@@ -0,0 +1,36 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/27962
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N		= Integer.parseInt(br.readLine());
+		String text = br.readLine();
+		int len		= text.length();
+		
+		while(len>=0)
+		{
+			int diff = 0;
+			for(int i=0; i<len; i++){
+				if(text.charAt(i) != text.charAt(text.length() - len + i))
+				{
+					++diff;
+					if(1 < diff)
+						break;
+				}
+			}
+			
+			if(diff == 1)
+			{
+				System.out.print("YES");
+				return;
+			}
+			--len;
+		}
+		
+		System.out.print("NO");
+	}
+}
