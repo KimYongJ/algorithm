84ce0eebde1e3ea796cae942ecd1fa2f4dd4ac93
YongJu Kim <kyjdummy@gmail.com>
2024-12-15 12:57:52 +0900

    [Gold I] Title: 귀농, Time: 940 ms, Memory: 298104 KB -BaekjoonHub

---
+++ 백준/Gold/1184. 귀농/귀농.java
@@ -0,0 +1,69 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1184
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int cnt;
+	static int N, psum[][];
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N		= Integer.parseInt(br.readLine());
+		psum	= new int[N+1][N+1];
+		
+		for(int y=1; y<=N;  y++)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int x=1; x<=N; x++)
+				psum[y][x] = Integer.parseInt(st.nextToken())+ psum[y][x-1] + psum[y-1][x] - psum[y-1][x-1];
+		}
+		
+		// 가능한 꼭지점의 좌표
+		for(int y=1; y<N; y++)
+		{
+			for(int x=1; x<N; x++)
+			{
+				HashMap<Integer,Integer> map1 = new HashMap<>();	// 2사분면 4사분면 비교
+				HashMap<Integer,Integer> map2 = new HashMap<>();	// 1사분면 3사분면 비교
+
+				for(int y1=1; y1<=y; y1++)
+				{
+					for(int x1=1; x1<=x; x1++)						// 2사분면
+					{
+						int sum = getSum(y1,x1, y, x);
+						map1.put(sum, map1.getOrDefault(sum, 0) + 1);
+					}
+					for(int x1=N; x1>x; x1--)						// 1사분면
+					{
+						int sum = getSum(y1, x+1, y, x1);
+						map2.put(sum, map2.getOrDefault(sum, 0) + 1);
+					}
+				}
+				
+				for(int y1=N; y1>y; y1--)
+				{
+					for(int x1=N; x1>x; x1--)						// 4사분면
+					{
+						cnt += map1.getOrDefault(getSum(y+1, x+1, y1, x1), 0);
+					}
+					for(int x1=1; x1<=x; x1++)						// 3사분면
+					{
+						cnt += map2.getOrDefault(getSum(y+1, x1, y1, x), 0);
+					}
+				}
+				
+			}
+		}
+		
+		System.out.print(cnt);
+	}
+	public static int getSum(int y1, int x1, int y2, int x2) {
+		return psum[y2][x2] - psum[y2][x1-1] - psum[y1-1][x2] + psum[y1-1][x1-1];
+		
+	}
+}
