ead8d068ecd064ca9155e8a9225a97b460cb971f
YongJu Kim <kyjdummy@gmail.com>
2024-12-16 16:15:56 +0900

    [Platinum IV] Title: 상대음감의 노래찾기, Time: 252 ms, Memory: 19408 KB -BaekjoonHub

--- 백준/Platinum/27504. 상대음감의 노래찾기/상대음감의 노래찾기.java
+++ 백준/Platinum/27504. 상대음감의 노래찾기/상대음감의 노래찾기.java
@@ -1,39 +1,36 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/27504
 //1초, 1024MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[][]	= new int[N][];
-		
-		for(int i=0; i<N; i++)
+		// 각 순서마다 각 음의 차이 값을 담음
+		for(int i=0, len = 0; i<N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int len = Integer.parseInt(st.nextToken());
-			arr[i] = new int[len - 1];
-			int s = Integer.parseInt(st.nextToken());
-			for(int j=0; j<len-1; j++)
+			len		= read() - 1;
+			arr[i]	= new int[len];
+			for(int j=0,s = read(), e=0; j<len; j++, s = e)
 			{
-				int e = Integer.parseInt(st.nextToken());
+				e = read();
 				arr[i][j] = e - s;
-				s = e;
 			}
 		}
-		int len = Integer.parseInt(br.readLine()) - 1;
-		int pattern[] = new int[len];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int s = Integer.parseInt(st.nextToken());
-		for(int i=0; i<len; i++)
-		{
-			int e = Integer.parseInt(st.nextToken());
-			pattern[i] = e - s;
-			s = e;
-		}
 		
+		int len			= read() - 1;
+		int pattern[]	= new int[len];
+		// 패턴에 대해 차이 값을 기준으로 배열에 담음
+		for(int i=0, s = read(), e = 0; i<len; i++, s = e)
+		{
+			e = read();
+			pattern[i] = e - s;
+		}
+		// 입력된 패턴에 대해 fail을 구함
 		int fail[] = new int[len];
 		for(int i=1, j=0; i<len; i++)
 		{
@@ -43,6 +40,7 @@ class Main{
 			if(pattern[i] == pattern[j])
 				fail[i] = ++j;
 		}
+		// 패턴에 대해 매칭되는 것이 있는지 확인
 		StringBuilder sb = new StringBuilder();
 		for(int idx=0; idx<N; idx++)
 		{
