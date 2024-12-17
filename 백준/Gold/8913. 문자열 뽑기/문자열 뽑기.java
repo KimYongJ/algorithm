ce585b15470634e3573f163f2d30ba1cbdb061ab
YongJu Kim <kyjdummy@gmail.com>
2024-12-17 11:18:39 +0900

    [Gold III] Title: 문자열 뽑기, Time: 280 ms, Memory: 40216 KB -BaekjoonHub

--- 백준/Gold/8913. 문자열 뽑기/문자열 뽑기.java
+++ 백준/Gold/8913. 문자열 뽑기/문자열 뽑기.java
@@ -3,14 +3,14 @@
 //2초, 128MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		while(T-->0)
-				sb.append(bruteforce(br.readLine()) ? 1 : 0).append('\n');
+				sb.append(bruteforce(br.readLine()) ? 1 : 0)
+					.append('\n');
 
 		System.out.print(sb.toString());
 	}
@@ -20,37 +20,19 @@ class Main{
 		if(str.length() == 1)
 			return false;
 		
-		ArrayList<Integer> list = new ArrayList<>();
-		char flag	= str.charAt(0);
-		int len		= str.length();
+		int cnt = 1;
+		int len = str.length();
+		
 		for(int i=1; i<len; i++)
-			if(str.charAt(i) != flag)
+			if(str.charAt(i) == str.charAt(i-1))
+				++cnt;
+			else
 			{
-				list.add(i);
-				flag = str.charAt(i);
-			}
-		
-		if(list.size() == 0)
-			return true;
-		
-		for(int i=0, s=0, e=0; i<list.size(); i++)
-		{
-			e = list.get(i);
-			if(1 < e-s)
-			{
-				String front = str.substring(0, s);
-				String back = str.substring(e, len);
-				if(bruteforce(front + back))
+				if(2 <= cnt && bruteforce(str.substring(0,i-cnt) + str.substring(i, len)))
 					return true;
+				cnt = 1;
 			}
-			s = e;
-		}
-		if(1 < len - list.get(list.size()-1))
-		{
-			if(bruteforce(str.substring(0,list.get(list.size()-1))))
-				return true;
-		}
-		
-		return false;
+
+		return cnt == str.length();
 	}
 }
