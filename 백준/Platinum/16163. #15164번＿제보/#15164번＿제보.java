b7a975e9c3ff9367de35c85c83db0277da4894ec
YongJu Kim <kyjdummy@gmail.com>
2024-12-17 23:54:27 +0900

    [Platinum V] Title: #15164번_제보, Time: 292 ms, Memory: 52128 KB -BaekjoonHub

--- 백준/Platinum/16163. #15164번＿제보/#15164번＿제보.java
+++ 백준/Platinum/16163. #15164번＿제보/#15164번＿제보.java
@@ -1,16 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16163
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
+    public static String readString() throws Exception {
+        StringBuilder sb = new StringBuilder(" ");
+        int c;
+        while ((c = System.in.read()) > 13) {sb.append((char) c).append(' ');}
+        return sb.toString();
+    }
 	public static void main(String args[])throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		for(char c : br.readLine().toCharArray())
-			sb.append(' ').append(c);
-		sb.append(' ');
-		
-		int len = sb.length();
+		char str[] = readString().toCharArray();
+		int len = str.length;
 		int A[] = new int[len];
 		long cnt = 0;
 		for(int i=0,r=0,p=0; i<len; i++)
@@ -19,7 +18,7 @@ class Main{
 			if(i<=r)
 				A[i] = Math.min(r-i, A[2*p-i]);
 			
-			while(0<=i-A[i]-1 && i+A[i]+1<len && sb.charAt(i-A[i]-1) == sb.charAt(i+A[i]+1))
+			while(0<=i-A[i]-1 && i+A[i]+1<len && str[i-A[i]-1] == str[i+A[i]+1])
 				++A[i];
 			
 			if(r < i+A[i])
