0cea3d8a65fd4f032726bb4bcf9201f09c7deb48
YongJu Kim <kyjdummy@gmail.com>
2024-12-17 23:24:29 +0900

    [Platinum IV] Title: ABB, Time: 284 ms, Memory: 24848 KB -BaekjoonHub

--- 백준/Platinum/18171. ABB/ABB.java
+++ 백준/Platinum/18171. ABB/ABB.java
@@ -10,35 +10,30 @@ class Main{
 		StringBuilder sb = new StringBuilder();
 		int len = Integer.parseInt(br.readLine());
 		for(char c : br.readLine().toCharArray())
-			sb.append(' ').append(c);
-		sb.append(' ');
+			sb.append('*').append(c);
+		sb.append('*');
 		
-		len = sb.length();
-		char[] str	= sb.toString().toCharArray();
-		int A[]		= new int[len];
+		int strlen	= sb.length();
+		int A[]		= new int[strlen];
 		
-		for(int i=0,r=0,p=0; i<len; i++)
+		for(int i=0,r=0,p=0; i<strlen; i++)
 		{
 			if(i<=r)
 				A[i] = Math.min(r-i, A[2*p-i]);
 			
-			while(0<=i-A[i]-1 && i+A[i]+1<len && str[i-A[i]-1] == str[i+A[i]+1])
+			while(0<=i-A[i]-1 && i+A[i]+1<strlen && sb.charAt(i-A[i]-1) == sb.charAt(i+A[i]+1))
 				++A[i];
 			
 			if(r < i + A[i])
 			{
 				r = i + A[i];
 				p = i;
+				if(r == strlen - 1)
+				{
+					System.out.print(len - A[i]);
+					return;
+				}
 			}
 		}
-		int maxNum = 0;
-		int maxIdx = len-1;
-		for(int idx=len-1, i=0; idx>=0; idx--,i++) {
-			if(A[idx] == i && maxNum < A[idx]) {
-				maxNum = A[idx];
-				maxIdx = idx;
-			}
-		}
-		System.out.print((maxIdx - maxNum) / 2);
 	}
 }
