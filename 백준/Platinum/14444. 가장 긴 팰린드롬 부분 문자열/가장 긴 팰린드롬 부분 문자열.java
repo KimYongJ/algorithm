8f59af5ea348447c8315b76100ee48439ab689fd
YongJu Kim <kyjdummy@gmail.com>
2024-12-17 15:36:17 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 204 ms, Memory: 18860 KB -BaekjoonHub

---
+++ 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.java
@@ -0,0 +1,50 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/14444
+//2초, 512MB
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Main{
+	
+	static int len, A[];
+	
+	public static void manachers(char[] str, int len) {
+		int r = 0;
+		int p = 0;
+		for(int i=0; i<len; i++)
+		{
+			if(i <= r)
+				A[i] = Math.min(A[2*p - i], r - i);
+			else
+				A[i] = 0;
+			
+			while(0 <= i - A[i] - 1 && i + A[i] + 1 < len && str[i - A[i] - 1] == str[i + A[i] + 1])
+				++A[i];
+			
+			if(r < i + A[i])
+			{
+				r = i + A[i];
+				p = i;
+			}
+		}
+	}
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : br.readLine().toCharArray())
+			sb.append('#').append(c);
+		sb.append('#');
+		
+		len = sb.length();
+		A	= new int[len*2 + 2];
+		
+		manachers(sb.toString().toCharArray(), len);
+		
+		int ans = -1;
+		
+		for(int i=0; i<len; i++)
+			ans = Math.max(ans, A[i]);
+		
+		System.out.print(ans);
+	}
+}
