61f8a3b0df2b99668aaead27745ddf4864a5f197
YongJu Kim <kyjdummy@gmail.com>
2024-12-19 15:50:12 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 164 ms, Memory: 16604 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,33 +1,31 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2167
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y		= Integer.parseInt(st.nextToken());
-		int X		= Integer.parseInt(st.nextToken());
+		int Y		= read();
+		int X		= read();
 		int psum[][]= new int[Y+2][X+2];
 		
 		for(int y=1; y<=Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++)
-				psum[y][x] += psum[y][x-1] + psum[y-1][x] - psum[y-1][x-1] + Integer.parseInt(st.nextToken());
-		}
+				psum[y][x] += psum[y][x-1] + psum[y-1][x] - psum[y-1][x-1] + read();
 		
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
+		int N = read();
 		while(N-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
+			int y1 = read();
+			int x1 = read();
+			int y2 = read();
+			int x2 = read();
 			sb.append(psum[y2][x2] - psum[y2][x1-1] - psum[y1-1][x2] + psum[y1-1][x1-1]).append('\n');
 		}
 		System.out.print(sb);
