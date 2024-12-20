4d203def2643bdafbda2b8b34bedde5a53241ca7
YongJu Kim <kyjdummy@gmail.com>
2024-12-20 11:02:16 +0900

    [Gold IV] Title: 도도의 음식 준비, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/22953. 도도의 음식 준비/도도의 음식 준비.java
+++ 백준/Gold/22953. 도도의 음식 준비/도도의 음식 준비.java
@@ -1,8 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/22953
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static long MAX = 1_000_000_000_001L;
@@ -11,16 +8,13 @@ class Main{
 	static int time[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N	= Integer.parseInt(st.nextToken());	// 요리사수(1<=10)
-		F	= Integer.parseInt(st.nextToken());	// 만들 음식개수(1<=백만)
-		C	= Integer.parseInt(st.nextToken());	// 격려 횟 수(0<=5)
-		time= new int[N];						//각 요리사의 조리 시간(1<=백만)
-		
-		st = new StringTokenizer(br.readLine());
+		N	= read();		// 요리사수(1<=10)
+		F	= read();		// 만들 음식개수(1<=백만)
+		C	= read();		// 격려 횟 수(0<=5)
+		time= new int[N];	//각 요리사의 조리 시간(1<=백만)
+
 		for(int i=0; i<N; i++)
-			time[i] = Integer.parseInt(st.nextToken());
+			time[i] = read();
 		
 		back(0, 0);
 		
@@ -66,4 +60,10 @@ class Main{
 		}
 		return false;
 	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 }
+
