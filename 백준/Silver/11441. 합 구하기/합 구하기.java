890205ea6af78e76a515692d939180cbac87c681
YongJu Kim <kyjdummy@gmail.com>
2024-12-20 23:29:45 +0900

    [Silver III] Title: 합 구하기, Time: 236 ms, Memory: 21180 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.java
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -1,27 +1,27 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/11441
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N+1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken()) + arr[i-1];
+		int N		= read();
+		int arr[]	= new int[N+1];
 		
-		int T = Integer.parseInt(br.readLine());
+		for(int i=1; i<=N; i++)
+			arr[i] = read() + arr[i-1];
+		
+		int T = read();
 		while(T-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			
+			int s = read();
+			int e = read();
 			sb.append(arr[e] - arr[s-1]).append('\n');
 		}
 		System.out.print(sb);
