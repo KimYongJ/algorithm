9f3e15c7971013d92f882ba33cb61058f3df3a5b
YongJu Kim <kyjdummy@gmail.com>
2024-12-20 11:00:44 +0900

    [Gold IV] Title: 도도의 음식 준비, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Gold/22953. 도도의 음식 준비/도도의 음식 준비.java
+++ 백준/Gold/22953. 도도의 음식 준비/도도의 음식 준비.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 class Main{
 	
-	static long MAX = 2_000_000_000_000L;
+	static long MAX = 1_000_000_000_001L;
 	static long min = MAX;
 	static int N, F, C;
 	static int time[];
@@ -19,47 +19,42 @@ class Main{
 		time= new int[N];						//각 요리사의 조리 시간(1<=백만)
 		
 		st = new StringTokenizer(br.readLine());
-		long sum = 0;
 		for(int i=0; i<N; i++)
-			sum += time[i] = Integer.parseInt(st.nextToken());
+			time[i] = Integer.parseInt(st.nextToken());
 		
-		sum -= N;
-		
-		back(0, sum < C);
+		back(0, 0);
 		
 		System.out.print(min);
 	}
-	public static void back(int depth, boolean check) {
-		if(check)
-			binarySearch();
+	public static void back(int depth, int idx) {
+		binarySearch();
+		
 		if(depth == C)
-		{
-			binarySearch();
 			return;
-		}
-		for(int i=0; i<N; i++)
+
+		for(int i=idx; i<N; i++)
 			if(1 < time[i])
 			{
 				time[i] -= 1;
-				back(depth + 1, check);
+				back(depth + 1, i);
 				time[i] += 1;
 			}
 	}
 	public static void binarySearch() {
-		long res	= 0;
 		long s		= 0;
 		long e		= MAX;
 		long mid	= 0;
-		while(s <= e) {
+		while(s <= e)
+		{
 			mid = (s + e) >> 1;
 			if(validate(mid))
 			{
-				res = mid;
 				e = mid - 1;
+				if(mid < min)
+					min = mid;
 			}
 			else s = mid + 1;
 		}
-		min = Math.min(min, res);
 	}
 	public static boolean validate(long maxTime) {
 		long cnt = 0;
