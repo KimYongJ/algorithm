089c7e5ea7546494659dff795b00ca06d1bacb17
YongJu Kim <kyjdummy@gmail.com>
2024-12-21 09:21:40 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 120 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -7,22 +7,25 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 횡단보도(1<=십만)
-		int K = Integer.parseInt(st.nextToken());	// 연속한 신호등개수
-		int B = Integer.parseInt(st.nextToken());	// 고장난 신호등 개수
-		int arr[] = new int[N+1];
+		int N		= Integer.parseInt(st.nextToken());	// 횡단보도(1<=십만)
+		int K		= Integer.parseInt(st.nextToken());	// 연속한 신호등개수
+		int B		= Integer.parseInt(st.nextToken());	// 고장난 신호등 개수
+		int arr[]	= new int[N+1];
+		int ksum	= 0;
+		
 		
 		for(int i=1; i<=B; i++)
 			arr[Integer.parseInt(br.readLine())] = 1;
 		
-		int ksum = 0;
 		for(int i=1; i<=K; i++)
 			ksum += arr[i];
 		
 		int min = ksum;
-		for(int i=K+1,j=1; i<=N; i++,j++) {
-			ksum += arr[i] - arr[j];
-			min = Math.min(min, ksum);
+		for(int i=K+1,j=1; i<=N; i++)
+		{
+			ksum += arr[i] - arr[j++];
+			if(ksum < min)
+				min = ksum;
 		}
 		
 		System.out.print(min);
