5f5eb33ae8d983bf5693071315f7102e160c34b5
YongJu Kim <kyjdummy@gmail.com>
2024-12-21 09:05:38 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 124 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.java
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.java
@@ -1,21 +1,19 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/13900
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		long arr[]	= new long[N+1];
 		long prev[]	= new long[N+1];
 		long res	= 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			prev[i] = (arr[i] = Integer.parseInt(st.nextToken())) + prev[i-1];
+			prev[i] = (arr[i] = read()) + prev[i-1];
 		
 		for(int i=1; i<N; i++)
 			res += arr[i] * (prev[N] - prev[i]);
