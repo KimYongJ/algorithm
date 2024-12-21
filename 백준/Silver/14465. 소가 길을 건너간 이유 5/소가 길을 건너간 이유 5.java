f2e7168ac22fa9cadcbfd96084ac389d941f1917
YongJu Kim <kyjdummy@gmail.com>
2024-12-21 09:18:59 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 120 ms, Memory: 14868 KB -BaekjoonHub

---
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -0,0 +1,30 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/14465
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());	// 횡단보도(1<=십만)
+		int K = Integer.parseInt(st.nextToken());	// 연속한 신호등개수
+		int B = Integer.parseInt(st.nextToken());	// 고장난 신호등 개수
+		int arr[] = new int[N+1];
+		
+		for(int i=1; i<=B; i++)
+			arr[Integer.parseInt(br.readLine())] = 1;
+		
+		int ksum = 0;
+		for(int i=1; i<=K; i++)
+			ksum += arr[i];
+		
+		int min = ksum;
+		for(int i=K+1,j=1; i<=N; i++,j++) {
+			ksum += arr[i] - arr[j];
+			min = Math.min(min, ksum);
+		}
+		
+		System.out.print(min);
+	}
+}
