9dc67e68f639287fe7fe73d517351f562d722914
YongJu Kim <kyjdummy@gmail.com>
2024-12-22 20:11:32 +0900

    [Gold IV] Title: 중복 없는 수, Time: 848 ms, Memory: 58532 KB -BaekjoonHub

--- 백준/Gold/11037. 중복 없는 수/중복 없는 수.java
+++ 백준/Gold/11037. 중복 없는 수/중복 없는 수.java
@@ -8,29 +8,25 @@ import java.util.Collections;
 
 class Main{
 	
-	static int arr[];
-	static boolean visit[];
-	static ArrayList<Integer> list;
+	static boolean visit[]			= new boolean[10];
+	static ArrayList<Integer> list	= new ArrayList<>();
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String str;
-		arr		= new int[9];
-		visit	= new boolean[10];
-		list	= new ArrayList<>();
 		
-		back(8);
+		back(0, 0);
 		
 		Collections.sort(list);
-
+		
+		String str;
 		while((str = br.readLine()) != null && str.length() != 0)
 			sb.append( binarySearch(Integer.parseInt(str)) ).append('\n');
 
 		System.out.print(sb);
 	}
 	public static int binarySearch(int target) {
-		int s	= 0;
+		int s	= 1;
 		int e	= list.size() - 1;
 		int res	= 0;
 		
@@ -49,32 +45,17 @@ class Main{
 		
 		return res;
 	}
-	public static void back(int depth) {
-		make();
-		if(depth < 0)
+	public static void back(int depth, int num) {
+		list.add(num);
+		if(9 <= depth)
 			return;
 		
 		for(int i=1; i<=9; i++)
 			if(!visit[i])
 			{
 				visit[i] = true;
-				arr[depth] = i;
-				back(depth - 1);
-				arr[depth] = 0;
+				back(depth + 1, num + i * (int)Math.pow(10, depth));
 				visit[i] = false;
 			}
-	}
-	public static void make() {
-		int num = 0;
-		int i = 8;
-		int mul = 1;
-		while(0<=i) {
-			if(arr[i] == 0)
-				break;
-			num += mul*arr[i];
-			i--;
-			mul *= 10;
-		}
-		list.add(num);
 	}
 }
