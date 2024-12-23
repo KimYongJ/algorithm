cb00482416b6e7b928f59754643a4eeae07f28c8
YongJu Kim <kyjdummy@gmail.com>
2024-12-23 22:16:57 +0900

    [Gold IV] Title: GREAT + SWERC = PORTO, Time: 300 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Gold/10529. GREAT ＋ SWERC ＝ PORTO/GREAT ＋ SWERC ＝ PORTO.java
+++ 백준/Gold/10529. GREAT ＋ SWERC ＝ PORTO/GREAT ＋ SWERC ＝ PORTO.java
@@ -1,87 +1,75 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/10529
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-class Main{
-	
-	static ArrayList<Integer> list = new ArrayList<>();
-	static int[][] arr;
-	static int[] order;
-	static int N, cnt, len;
-	static boolean[] visit;
-	
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Set<Integer> set = new HashSet<>();
-		N		= Integer.parseInt(br.readLine());
-		arr		= new int[N][];
-		
-		for(int i=0; i<N; i++)
-		{
-			String str = br.readLine();
-			int len = str.length();
-			arr[i] = new int[len];
-			for(int j=0; j<len; j++)
-			{
-				arr[i][j] = str.charAt(j) - 'A';
-				set.add(arr[i][j]);
-			}
-		}
-		
-		list.addAll(set);
-		
-		len		= list.size();
-		order	= new int[30];
-		visit	= new boolean[10];
-		
-		comb(0);
-		
-		System.out.print(cnt);
-	}
-	public static void comb(int depth) {
-		if(depth == len) {
-			cal();
-			return;
-		}
-		for(int i=0; i<10; i++)
-		{
-			if(!visit[i])
-			{
-				visit[i] = true;
-				order[list.get(depth)] = i;
-				comb(depth + 1);
-				visit[i] = false;
-			}
-		}
-	}
-	public static void cal() {
-		int number[] = new int[N];
-		for(int i=0; i<N; i++)
-		{
-			int num = changeNum(arr[i]);
-			if(num == -1)
-				return;
-			number[i] = num;
-		}
-		int sum = 0;
-		for(int i=0; i<N-1; i++)
-			sum += number[i];
-		if(sum == number[N-1])
-			++cnt;
-	}
-	
-	public static int changeNum(int arr[]) {
-		if(order[arr[0]] == 0)
-			return -1;
-		int num = 0;
-		for(int i=0; i<arr.length; i++) {
-			num = num * 10 + order[arr[i]];
-		}
-		return num;
-	}
+public class Main {
+    static boolean[] check = new boolean[10], visit = new boolean[10];
+    static int n = 0, answer = 0;
+    static String[] str;
+    static Map<Character, Integer> ch = new HashMap<>();
+    static long[] sum = new long[10];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        str = new String[num];
+
+        // 입력 처리 및 문자 매핑
+        for (int i = 0; i < num; i++) {
+            str[i] = br.readLine();
+            for (char x : str[i].toCharArray()) {
+                if (!ch.containsKey(x)) {
+                    ch.put(x, n++);
+                }
+            }
+        }
+
+        // 각 문자에 대해 첫 글자인 경우 체크
+        for (int i = 0; i < num; i++) {
+            check[ch.get(str[i].charAt(0))] = true;
+        }
+
+        // 각 문자의 가중치 계산
+        for (int i = 0; i < num; i++) {
+            int len = str[i].length();
+            long base = 1;
+            for (int j = len - 1; j >= 0; j--, base *= 10) {
+                if (i == num - 1) {
+                    sum[ch.get(str[i].charAt(j))] -= base;
+                } else {
+                    sum[ch.get(str[i].charAt(j))] += base;
+                }
+            }
+        }
+
+        // 백트래킹 탐색
+        f(0, 0);
+
+        // 결과 출력
+        System.out.println(answer);
+    }
+
+    public static void f(int idx, long cost) {
+        if (idx == n) {
+            if (cost == 0) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int k = 0; k <= 9; k++) {
+            if (k == 0 && !check[idx] && !visit[k]) {
+                visit[k] = true;
+                f(idx + 1, cost);
+                visit[k] = false;
+            } else if (k > 0 && !visit[k]) {
+                visit[k] = true;
+                f(idx + 1, cost + sum[idx] * k);
+                visit[k] = false;
+            }
+        }
+    }
 }
