e4d6137698a7768a0276ff62f4256d9b90caeddd
YongJu Kim <kyjdummy@gmail.com>
2024-12-24 14:56:26 +0900

    [Gold IV] Title: Crush Fever, Time: 316 ms, Memory: 88564 KB -BaekjoonHub

--- 백준/Gold/14673. Crush Fever/Crush Fever.java
+++ 백준/Gold/14673. Crush Fever/Crush Fever.java
@@ -1,8 +1,5 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14673
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static final int dxy[][] = {{1,0},{0,1},{-1,0},{0,-1}};
@@ -11,18 +8,13 @@ class Main{
 	static int map[][];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		X	= Integer.parseInt(st.nextToken());
-		Y	= Integer.parseInt(st.nextToken());
+		X	= read();
+		Y	= read();
 		map	= new int[Y+2][X+2];
 		
 		for(int y=1; y<=Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++)
-				map[y][x] = Integer.parseInt(st.nextToken());
-		}
+				map[y][x] = read();
 		
 		back(0, 0, map);
 		
@@ -81,5 +73,9 @@ class Main{
 			}
 		return map;
 	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
 }
-
