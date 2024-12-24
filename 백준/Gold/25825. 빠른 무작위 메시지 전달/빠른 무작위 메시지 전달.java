ac85ed3f52e1c6f785a6f6ff0cbc208a23d2f852
YongJu Kim <kyjdummy@gmail.com>
2024-12-24 13:20:46 +0900

    [Gold IV] Title: 빠른 무작위 메시지 전달, Time: 120 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Gold/25825. 빠른 무작위 메시지 전달/빠른 무작위 메시지 전달.java
+++ 백준/Gold/25825. 빠른 무작위 메시지 전달/빠른 무작위 메시지 전달.java
@@ -1,72 +1,60 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/25825
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int group[][] = {{0,1},{2,3},{4,5},{6,7},{8,9},{10,11}};
 	static int map[][];
 	static boolean visit[];
-	static int MAX = 1<<30;
+	static int MIN = 1<<30;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		map		= new int[12][12];
 		visit	= new boolean[12];
 		
 		for(int y=0; y<12; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=0; x<12; x++)
-				map[y][x] = Integer.parseInt(st.nextToken());
-		}
+				map[y][x] = read();
 		
 		for(int i=0; i<6; i++)
-		{
-			visit[group[i][0]] = true;
-			back(0, 0, i, group[i][0]);
-			visit[group[i][0]] = false;
-			visit[group[i][1]] = true;
-			back(0, 0, i, group[i][1]);
-			visit[group[i][1]] = false;
-		}
+			for(int j=0; j<=1; j++)
+			{
+				visit[group[i][j]] = true;
+				back(0, 0, i, group[i][j]);
+				visit[group[i][j]] = false;
+			}
 		
-		System.out.print(MAX);
+		System.out.print(MIN);
 	}
 	public static void back(int depth, int time, int groupIdx, int prev) {
 		if(depth == 11)
 		{
-			MAX = Math.min(MAX, time);
+			MIN = Math.min(MIN, time);
 			return;
 		}
-		if(!visit[group[groupIdx][0]])
+		// 같은 그룹이 방문하지 않았을 때
+		if(!visit[group[groupIdx][0]] || !visit[group[groupIdx][1]])
 		{
-			visit[group[groupIdx][0]] = true;
-			back(depth + 1, time + map[prev][group[groupIdx][0]], groupIdx, group[groupIdx][0]);
-			visit[group[groupIdx][0]] = false;
+			int idx = visit[group[groupIdx][0]] ? 1 : 0;
+			visit[group[groupIdx][idx]] = true;
+			back(depth + 1, time + map[prev][group[groupIdx][idx]], groupIdx, group[groupIdx][idx]);
+			visit[group[groupIdx][idx]] = false;
 		}
-		else if(!visit[group[groupIdx][1]]) {
-			visit[group[groupIdx][1]] = true;
-			back(depth + 1, time + map[prev][group[groupIdx][1]], groupIdx, group[groupIdx][1]);
-			visit[group[groupIdx][1]] = false;
-		}
-		else {
+		// 같은 그룹이 다 방문했다면 다른 그룹을 찾는다.
+		else
+		{
 			for(int i=0; i<6; i++)
-			{
-				if(!visit[group[i][0]])
-				{
-					visit[group[i][0]] = true;
-					back(depth + 1, time + map[prev][group[i][0]], i, group[i][0]);
-					visit[group[i][0]] = false;
-				}
-				if(!visit[group[i][1]]) {
-					visit[group[i][1]] = true;
-					back(depth + 1, time + map[prev][group[i][1]], i, group[i][1]);
-					visit[group[i][1]] = false;
-				}
-			}
+				for(int j=0; j<=1; j++)
+					if(!visit[group[i][j]])
+					{
+						visit[group[i][j]] = true;
+						back(depth + 1, time + map[prev][group[i][j]], i, group[i][j]);
+						visit[group[i][j]] = false;
+					}
 		}
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
