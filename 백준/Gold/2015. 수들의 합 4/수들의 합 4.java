bc6a71f560aaba2e77d5817797cd7a719b6ba5f5
YongJu Kim <kyjdummy@gmail.com>
2024-12-26 16:46:12 +0900

    [Gold IV] Title: 수들의 합 4, Time: 336 ms, Memory: 36064 KB -BaekjoonHub

---
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -0,0 +1,34 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/2015
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long N		= Integer.parseInt(st.nextToken());	// 값 개수 1<이십만
+		long K		= Integer.parseInt(st.nextToken());	// 목적 값|이십억|
+		long psum[]	= new long[(int)N + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++)
+			psum[i] = psum[i-1] + Integer.parseInt(st.nextToken());
+		
+		long ans = 0;
+		HashMap<Long, Long> hm = new HashMap<>();
+		
+		for(int i=1; i<=N; i++) {
+			if(psum[i] == K)
+				++ans;
+			
+			long need = psum[i] - K;
+			
+			ans += hm.getOrDefault(need, 0L);
+			
+			hm.put(psum[i], hm.getOrDefault(psum[i],0L) + 1);
+		}
+		System.out.print(ans);
+	}
+}
