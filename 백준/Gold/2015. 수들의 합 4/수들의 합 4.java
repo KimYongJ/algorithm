50b7b05b533bca9cd6d83847c5b90f6dc80eb936
YongJu Kim <kyjdummy@gmail.com>
2024-12-26 17:23:33 +0900

    [Gold IV] Title: 수들의 합 4, Time: 200 ms, Memory: 26836 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -1,30 +1,33 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2015
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 class Main{
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long N		= Integer.parseInt(st.nextToken());	// 값 개수 1<이십만
-		long K		= Integer.parseInt(st.nextToken());	// 목적 값|이십억|
-		long psum[]	= new long[(int)N + 1];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++)
-			psum[i] = psum[i-1] + Integer.parseInt(st.nextToken());
-		
-		long ans = 0;
 		HashMap<Long, Long> hm = new HashMap<>();
-		
+		long N		= read();;	// 값 개수 1<이십만
+		long K		= read();;	// 목적 값|이십억|
+		long ans	= 0L;
+		long psum[]	= new long[(int)N + 1];
+
 		for(int i=1; i<=N; i++)
 		{
-			if(psum[i] == K) ++ ans;
+			psum[i] = psum[i-1] + read();
+			
+			if(psum[i] == K)
+				++ans;
+			
 			ans += hm.getOrDefault(psum[i] - K, 0L);
-			hm.put(psum[i], hm.getOrDefault(psum[i],0L) + 1);
+			hm.put(psum[i], hm.getOrDefault(psum[i], 0L) + 1);
 		}
+		
 		System.out.print(ans);
 	}
 }
