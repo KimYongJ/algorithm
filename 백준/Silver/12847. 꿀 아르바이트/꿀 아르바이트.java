4b92f2ed6aaab90339d7a2da6e3d0ebba19b4424
YongJu Kim <kyjdummy@gmail.com>
2024-12-27 14:38:54 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 132 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -1,20 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12847
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 근무 가능한 일(1<=십만)
-		int M		= Integer.parseInt(st.nextToken());	// 일할날(0<=n)
+		int N		= read();	// 근무 가능한 일(1<=십만)
+		int M		= read();	// 일할날(0<=n)
 		int arr[]	= new int[N];
 		long sum	= 0;
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		for(int i=0; i<M; i++)
 			sum += arr[i];
@@ -28,5 +22,10 @@ class Main{
 				max = sum;
 		}
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
