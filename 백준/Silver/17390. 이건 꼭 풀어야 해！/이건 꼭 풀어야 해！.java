267ce4e39d130538b3994e2dc11b8c4e11155dc6
YongJu Kim <kyjdummy@gmail.com>
2024-12-27 14:22:18 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 424 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
@@ -1,20 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17390
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 수열 길이1<=삼십만
-		int Q = Integer.parseInt(st.nextToken());	// 질문개수 N<=삼십만
-		int psum[] = new int[N+1];
+		int N		= read();// 수열 길이1<=삼십만
+		int Q		= read();// 질문개수 N<=삼십만
+		int psum[]	= new int[N+1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			psum[i] = Integer.parseInt(st.nextToken());	// 1<=천
+			psum[i] = read();// 1<=천
 		
 		Arrays.sort(psum);
 		
@@ -24,11 +18,15 @@ class Main{
 		StringBuilder sb = new StringBuilder();
 		while(Q-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int l = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
+			int l = read();
+			int r = read();
 			sb.append(psum[r] - psum[l-1]).append('\n');
 		}
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
