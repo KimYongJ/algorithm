32632e122d3cb197b77f5ebf5c933dd5b4076472
YongJu Kim <kyjdummy@gmail.com>
2024-12-28 09:24:22 +0900

    [Silver IV] Title: 수열 (Easy), Time: 184 ms, Memory: 22244 KB -BaekjoonHub

--- 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.java
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.java
@@ -1,25 +1,25 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/23827
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int N		= read();
 		long arr[]	= new long[N+1];
 		long psum[] = new long[N+1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++)
-			psum[i] += psum[i-1] + (arr[i] = Integer.parseInt(st.nextToken()));
+		long res	= 0;
 		
-		long res = 0;
-		for(int i=1; i<N; i++) {
+		for(int i=1; i<=N; i++)
+			psum[i] += psum[i-1] + (arr[i] = read());
+		
+		for(int i=1; i<N; i++)
+		{
 			res += (psum[N] - psum[i]) * arr[i];
 			res = res % 1_000_000_007;
 		}
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
