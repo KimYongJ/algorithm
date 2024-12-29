4922754bcdca438cd8442cda722d38f9503951ac
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 18:32:36 +0900

    [Silver IV] Title: 점수를 최대로, Time: 396 ms, Memory: 28768 KB -BaekjoonHub

--- 백준/Silver/29767. 점수를 최대로/점수를 최대로.java
+++ 백준/Silver/29767. 점수를 최대로/점수를 최대로.java
@@ -1,23 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/29767
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
 		PriorityQueue<Long> pq = new PriorityQueue<>((a,b)->Long.compare(b, a));
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 교실개수(1<=삼십만)
-		int K = Integer.parseInt(st.nextToken());	// 학생수
+		int N = read();	// 교실개수(1<=삼십만)
+		int K = read();	// 학생수
 		long sum[] = new long[N + 1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			pq.add(sum[i] += sum[i-1] + Integer.parseInt(st.nextToken()));
+			pq.add(sum[i] += sum[i-1] + read());
 		
 		long res = 0;
 		while(K-->0)
@@ -25,4 +18,12 @@ class Main{
 
 		System.out.print(res);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
