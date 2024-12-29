992e86ebf26b2392a2b6e0d09fad8bf1f0094cd5
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 15:36:37 +0900

    [Silver III] Title: 줄줄이 박수, Time: 324 ms, Memory: 30156 KB -BaekjoonHub

--- 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.java
+++ 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.java
@@ -1,32 +1,25 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/29718
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y		= Integer.parseInt(st.nextToken());
-		int X		= Integer.parseInt(st.nextToken());
+		int Y		= read();
+		int X		= read();
 		int map[][] = new int[Y+2][X+2];
 		
 		for(int y=1; y<=Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++)
-				map[y][x] = Integer.parseInt(st.nextToken()) + map[y][x-1] + map[y-1][x] - map[y-1][x-1];
-		}
-		
-		int s	= Integer.parseInt(br.readLine());
+				map[y][x] = read() + map[y][x-1] + map[y-1][x] - map[y-1][x-1];
+
 		int max = 0;
 		
-		for(int x1=s,x2=0; x1<=X; x1++,x2++)
-		{
-			int sum = map[Y][x1] - map[Y][x2];
-			if(max < sum)
-				max = sum;
-		}
+		for(int x1=read(),x2=0; x1<=X; x1++,x2++)
+			max = Math.max(max, map[Y][x1] - map[Y][x2]);
+
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
