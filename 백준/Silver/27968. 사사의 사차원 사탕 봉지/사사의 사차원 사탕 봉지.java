3b4f5909f972a394dc72d3d4215e50d88f2a046c
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 15:11:29 +0900

    [Silver II] Title: 사사의 사차원 사탕 봉지, Time: 476 ms, Memory: 42192 KB -BaekjoonHub

--- 백준/Silver/27968. 사사의 사차원 사탕 봉지/사사의 사차원 사탕 봉지.java
+++ 백준/Silver/27968. 사사의 사차원 사탕 봉지/사사의 사차원 사탕 봉지.java
@@ -1,29 +1,22 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/27968
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N, M;
 	static long psum[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N		= Integer.parseInt(st.nextToken());	// 아이 수(1<=삼십만)
-		M		= Integer.parseInt(st.nextToken());	// 최대 횟수(1<=삼십만)
+		N		= (int)read();	// 아이 수(1<=삼십만)
+		M		= (int)read();	// 최대 횟수(1<=삼십만)
 		psum	= new long[M + 1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=M; i++)
-			psum[i] = psum[i-1] + Long.parseLong(st.nextToken());
+			psum[i] = psum[i-1] + read();
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<N; i++)
 		{
-			int res = binarySearch(Long.parseLong(br.readLine()));
+			int res = binarySearch(read());
 			sb.append(res == -1 ? "Go away!" : res)
 				.append('\n');
 		}
@@ -36,12 +29,18 @@ class Main{
 		while(s <= e)
 		{
 			int idx = (s + e) >> 1;
-			if(target <= psum[idx]) {
+			if(target <= psum[idx])
+			{
 				e = idx - 1;
 				r = idx;
 			}
 			else s = idx + 1;
 		}
 		return r;
+	}
+	static long read() throws Exception {
+		long c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
