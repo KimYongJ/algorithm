96d07e21a92bb18eab35ed5336a60b14c5172793
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 18:43:11 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 352 ms, Memory: 48264 KB -BaekjoonHub

---
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -0,0 +1,39 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/29700
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int Y		= Integer.parseInt(st.nextToken());	// 세로1<=천
+		int X		= Integer.parseInt(st.nextToken());	// 가로1<=오천
+		int K		= Integer.parseInt(st.nextToken());	// 인원수 1<=10
+		int map[][]	= new int[Y][X];
+		
+		for(int y=0; y<Y; y++)
+		{
+			String str = br.readLine();
+			int cnt = 0;
+			for(int x=0; x<X; x++)
+			{
+				if(str.charAt(x) == '0')
+				{
+					if(K<= ++cnt)
+						map[y][x] = 1;
+				}
+				else cnt = 0;
+			}
+		}
+		
+		int res = 0;
+		for(int y=0; y<Y; y++)
+		{
+			for(int x=1; x<X; x++)
+				map[y][x] += map[y][x-1];
+			res += map[y][X-1];
+		}
+		System.out.print(res);
+	}
+}
