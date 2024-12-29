6843d4ba6fa8f64953ba2de322f0e10590410012
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 17:06:41 +0900

    [Silver II] Title: 정보대 등산, Time: 148 ms, Memory: 16648 KB -BaekjoonHub

--- 백준/Silver/12841. 정보대 등산/정보대 등산.java
+++ 백준/Silver/12841. 정보대 등산/정보대 등산.java
@@ -1,41 +1,29 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12841
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		long len[]	= new long[N+1];	// 횡단보도 길이(1<=십만)
 		long left[] = new long[N+1];	// 왼쪽 길 거리(1<=십만)
 		long right[]= new long[N+1];	// 왼쪽 길 거리(1<=십만)
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
-			len[i] = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
+			len[i] = read();
+
 		for(int i=2; i<=N; i++)
-			left[i] = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=2; i<=N; i++)
-			right[i] = Integer.parseInt(st.nextToken());
+			left[i] = read() + left[i-1];
 		
 		for(int i=2; i<=N; i++)
-		{
-			left[i] += left[i-1];
-			right[i]+= right[i-1];
-		}
+			right[i] = read() + right[i-1];
 		
 		long min = 1L<<60;
 		int idx = 0;
 		
-		for(int i=1; i<=N; i++) {
+		for(int i=1; i<=N; i++)
+		{
 			long sum = left[i] + len[i] + right[N] - right[i];
-			if(sum < min) {
+			if(sum < min)
+			{
 				min = sum;
 				idx = i;
 			}
@@ -44,5 +32,10 @@ class Main{
 		StringBuilder sb = new StringBuilder();
 		sb.append(idx).append(' ').append(min);
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
