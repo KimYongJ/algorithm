1e9a8f18e9d35e97bef729659165a16ec2d68d0a
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 16:36:43 +0900

    [Silver III] Title: 상자의 균형, Time: 176 ms, Memory: 23676 KB -BaekjoonHub

--- 백준/Silver/20116. 상자의 균형/상자의 균형.java
+++ 백준/Silver/20116. 상자의 균형/상자의 균형.java
@@ -1,21 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/20116
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 상자개수(1<=이십만)
-		int L		= Integer.parseInt(st.nextToken());	// 상자 사이즈(1<=십억)
+		int N		= read();	// 상자개수(1<=이십만)
+		int L		= read();	// 상자 사이즈(1<=십억)
 		double psum[] = new double[N+1];
 		double arr[][]= new double[N+1][2];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
 		{
-			psum[i]		= Integer.parseInt(st.nextToken());
+			psum[i]		= read();
 			arr[i][0]	= psum[i] - L;
 			arr[i][1]	= psum[i] + L;
 		}
@@ -26,11 +20,20 @@ class Main{
 		for(int i=N-1, j=1; i>0; i--,j++)
 		{
 			double mid = psum[i+1] / j;
-			if(mid <= arr[i][0] || arr[i][1] <= mid) {
+			if(mid <= arr[i][0] || arr[i][1] <= mid)
+			{
 				System.out.print("unstable");
 				return;
 			}
 		}
 		System.out.print("stable");
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
