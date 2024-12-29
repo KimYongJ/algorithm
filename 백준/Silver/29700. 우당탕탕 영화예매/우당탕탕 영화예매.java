adee4584a4e3acee773341d9795ece59639e3638
YongJu Kim <kyjdummy@gmail.com>
2024-12-29 18:45:17 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 184 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -1,31 +1,31 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/29700
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y		= Integer.parseInt(st.nextToken());	// 세로1<=천
-		int X		= Integer.parseInt(st.nextToken());	// 가로1<=오천
-		int K		= Integer.parseInt(st.nextToken());	// 인원수 1<=10
+		int Y		= read();	// 세로1<=천
+		int X		= read();	// 가로1<=오천
+		int K		= read();	// 인원수 1<=10
 		int res		= 0;
 		for(int y=0; y<Y; y++)
 		{
-			String str = br.readLine();
 			int cnt = 0;
 			for(int x=0; x<X; x++)
 			{
-				if(str.charAt(x) == '0')
+				if(System.in.read()== '0')
 				{
 					if(K<= ++cnt)
 						++res;
 				}
 				else cnt = 0;
 			}
+			System.in.read();// 개행 읽어없앰
 		}
 		
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
