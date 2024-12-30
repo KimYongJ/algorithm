6f58bb3edc35b5c21ef3ad9474f9270190a87b19
YongJu Kim <kyjdummy@gmail.com>
2024-12-30 14:37:44 +0900

    [Silver I] Title: 주지수, Time: 304 ms, Memory: 23680 KB -BaekjoonHub

--- 백준/Silver/15724. 주지수/주지수.java
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -1,34 +1,32 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/15724
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder	sb = new StringBuilder();
 		
-		int Y		= Integer.parseInt(st.nextToken());	// 1<=1024
-		int X		= Integer.parseInt(st.nextToken());	// 1<=1024
+		int Y		= read();	// 1<=1024
+		int X		= read();	// 1<=1024
 		int map[][] = new int[Y+2][X+2];
 		
 		for(int y=1; y<=Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++)
-				map[y][x] = Integer.parseInt(st.nextToken()) + map[y-1][x] + map[y][x-1] - map[y-1][x-1];
-		}
-		int K = Integer.parseInt(br.readLine());
+				map[y][x] = read() + map[y-1][x] + map[y][x-1] - map[y-1][x-1];
+		
+		int K = read();
+		
 		while(K-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
+			int y1 = read();
+			int x1 = read();
+			int y2 = read();
+			int x2 = read();
 			sb.append(map[y2][x2]-map[y2][x1-1]-map[y1-1][x2]+map[y1-1][x1-1]).append('\n');
 		}
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
