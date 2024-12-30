84e89bc12fc603d599bd73e008faeaa1c554489a
YongJu Kim <kyjdummy@gmail.com>
2024-12-30 22:01:14 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 132 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -1,23 +1,26 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14929
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[]	= new int[N+1];
 		int psum[]	= new int[N+1];
+		long res	= 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			psum[i] = (arr[i] = Integer.parseInt(st.nextToken())) + psum[i-1];
+			psum[i] = (arr[i] = read()) + psum[i-1];
 		
-		long res = 0;
 		for(int i=1; i<=N; i++)
 			res += arr[i] *(psum[N]-psum[i]);
 		
 		System.out.print(res);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
