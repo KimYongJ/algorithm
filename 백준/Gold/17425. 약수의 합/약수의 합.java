662df3721be553429b59e0beb1671451f45c44a0
YongJu Kim <kyjdummy@gmail.com>
2024-12-30 17:48:28 +0900

    [Gold IV] Title: 약수의 합, Time: 328 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.java
+++ 백준/Gold/17425. 약수의 합/약수의 합.java
@@ -1,29 +1,28 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17425
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		final int MAX = 1_000_000;
 		long a[] = new long[MAX+1];
 		long g[] = new long[MAX+1];
-		a[1] = 1;
-		for(int n=2; n<=MAX; n++)
-		{
-			++a[n];
+		
+		for(int n=1; n<=MAX; n++)
 			for(int i=1; i*n<=MAX; i++)
 				a[n*i] += n;
-		}
 		
 		for(int i=1; i<=MAX; i++)
 			g[i] = g[i-1] + a[i];
 		
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		while(T-->0)
-			sb.append(g[Integer.parseInt(br.readLine())]).append('\n');
+			sb.append(g[read()]).append('\n');
 
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
