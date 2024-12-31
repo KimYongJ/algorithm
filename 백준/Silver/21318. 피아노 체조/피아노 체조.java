58f923caf25d899a4c10069c1859e406af48da4f
YongJu Kim <kyjdummy@gmail.com>
2024-12-31 13:57:28 +0900

    [Silver I] Title: 피아노 체조, Time: 252 ms, Memory: 19924 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.java
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -1,34 +1,33 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/21318
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int N		= Integer.parseInt(br.readLine());	// 악보 개수(1<=십만)
+		int N		= read();	// 악보 개수(1<=십만)
 		int psum[]	= new int[N+1];
 		int arr[]	= new int[N+1];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
 		{
-			arr[i] = Integer.parseInt(st.nextToken());// 난이도(1<=십억)
+			arr[i] = read();// 난이도(1<=십억)
 			if(arr[i] < arr[i-1])
 				++psum[i-1];
 			
 			psum[i] += psum[i-1];
 		}
 		
-		int Q = Integer.parseInt(br.readLine());// 질문개수(1<=십만)
+		int Q = read();// 질문개수(1<=십만)
 		while(Q-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			sb.append(psum[y-1] - psum[x-1]).append('\n');
+			int x = read() - 1;
+			int y = read() - 1;
+			sb.append(psum[y] - psum[x]).append('\n');
 		}
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
