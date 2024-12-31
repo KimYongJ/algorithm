ea6bbc3c1c9d601d218cfdfc9d0fddb3a22c0918
YongJu Kim <kyjdummy@gmail.com>
2024-12-31 14:37:17 +0900

    [Gold IV] Title: 부분합, Time: 128 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,22 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1806
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 수열길이 (10 ≤ N < 100,000)
-		int S		= Integer.parseInt(st.nextToken());	// 타겟(0 < S ≤ 100,000,000)
+		int N		= read();	// 수열길이 (10 ≤ N < 100,000)
+		int S		= read();	// 타겟(0 < S ≤ 100,000,000)
 		int left	= 0;
 		int right	= 1;
 		int min		= 1<<30;
 		int psum[]	= new int[N+1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			psum[i] = psum[i-1] + Integer.parseInt(st.nextToken());
+			psum[i] = psum[i-1] + read();
 		
 		while(right <= N)
 		{
@@ -30,5 +24,10 @@ class Main{
 			}
 		}
 		System.out.print(min == 1<<30 ? 0 : min);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
