698113f3a9eb40df5c32153b19a53ec7fb423a60
YongJu Kim <kyjdummy@gmail.com>
2025-01-01 18:06:59 +0900

    [Silver II] Title: Generic Queries, Time: 620 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/16713. Generic Queries/Generic Queries.java
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -1,28 +1,26 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16713
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 개수(1<=백만)
-		int Q = Integer.parseInt(st.nextToken());	// 쿼리개수(1<=삼백만)
-		int arr[] = new int[N+1];
+		int N		= read();	// 개수(1<=백만)
+		int Q		= read();	// 쿼리개수(1<=삼백만)
+		int arr[]	= new int[N+1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken()) ^ arr[i-1];
+			arr[i] = read() ^ arr[i-1];
 		
 		int res = 0;
 		while(Q-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int l = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
+			int l = read();
+			int r = read();
 			res ^= (arr[r] ^ arr[l-1]);
 		}
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
