70ee31f1ce9c9324b678a22ee766acf20cd67f88
YongJu Kim <kyjdummy@gmail.com>
2025-01-01 17:53:14 +0900

    [Silver II] Title: 출석체크, Time: 148 ms, Memory: 15200 KB -BaekjoonHub

--- 백준/Silver/20438. 출석체크/출석체크.java
+++ 백준/Silver/20438. 출석체크/출석체크.java
@@ -1,37 +1,28 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/20438
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N			= Integer.parseInt(st.nextToken());	// 학생수
-		int K			= Integer.parseInt(st.nextToken());	// 졸고있는 학생수
-		int Q			= Integer.parseInt(st.nextToken());	// 출석 코드를 보낼 학생 수
-		int M			= Integer.parseInt(st.nextToken());	// 질의 수
+		int N			= read();	// 학생수
+		int K			= read();	// 졸고있는 학생수
+		int Q			= read();	// 출석 코드를 보낼 학생 수
+		int M			= read();	// 질의 수
 		int psum[]		= new int[N+3];
 		boolean sleep[] = new boolean[N+3];
 		
-		Arrays.fill(psum, 1);
+		Arrays.fill(psum, 1);			// 모든학생을 일단 출석 처리를 한다.
 		psum[0] = psum[1] = psum[2] = 0;
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<K; i++)
-			sleep[Integer.parseInt(st.nextToken())] = true;
+			sleep[read()] = true;
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<Q; i++)
 		{
-			int num = Integer.parseInt(st.nextToken());
-            if(sleep[num])
-                continue;
-            
-			for(int j=num; j<=N+2; j+=num)
-				if(!sleep[j])
-					psum[j] = 0;
+			int num = read();
+            if(!sleep[num])
+				for(int j=num; j<=N+2; j+=num)
+					if(!sleep[j])
+						psum[j] = 0;
 		}
 		
 		for(int i=3; i<=N+2; i++)
@@ -40,11 +31,15 @@ class Main{
 		StringBuilder sb = new StringBuilder();
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int l = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
+			int l = read();
+			int r = read();
 			sb.append(psum[r] - psum[l-1]).append('\n');
 		}
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
