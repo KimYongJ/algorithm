d45f28979f3d3dffcfe11daf26bf8eed4e3a72c6
YongJu Kim <kyjdummy@gmail.com>
2025-01-01 17:09:56 +0900

    [Silver IV] Title: ∑|ΔEasyMAX|, Time: 108 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/17203. ∑｜ΔEasyMAX｜/∑｜ΔEasyMAX｜.java
+++ 백준/Silver/17203. ∑｜ΔEasyMAX｜/∑｜ΔEasyMAX｜.java
@@ -1,20 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17203
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 노래 개수(1<=천)
-		int Q		= Integer.parseInt(st.nextToken());	// 질의개수(1<=천)
+		int N		= read();	// 노래 개수(1<=천)
+		int Q		= read();	// 질의개수(1<=천)
 		int arr[]	= new int[N+1];
 		int diff[]	= new int[N+1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		for(int i=2; i<=N; i++)
 			diff[i] = Math.abs((arr[i] - arr[i-1])) + diff[i-1];
@@ -22,11 +16,18 @@ class Main{
 		StringBuilder sb = new StringBuilder();
 		while(Q-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int l = Integer.parseInt(st.nextToken());
-			int r = Integer.parseInt(st.nextToken());
+			int l = read();
+			int r = read();
 			sb.append(diff[r] - diff[l]).append('\n');
 		}
 		System.out.print(sb);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
