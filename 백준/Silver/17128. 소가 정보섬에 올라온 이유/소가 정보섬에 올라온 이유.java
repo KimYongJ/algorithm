0c795854e80cd3cca4898f4688afc9c24cc49ad4
YongJu Kim <kyjdummy@gmail.com>
2025-01-01 20:26:20 +0900

    [Silver II] Title: 소가 정보섬에 올라온 이유, Time: 348 ms, Memory: 25136 KB -BaekjoonHub

--- 백준/Silver/17128. 소가 정보섬에 올라온 이유/소가 정보섬에 올라온 이유.java
+++ 백준/Silver/17128. 소가 정보섬에 올라온 이유/소가 정보섬에 올라온 이유.java
@@ -1,21 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17128
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 소의개수	4<=이십만
-		int Q		= Integer.parseInt(st.nextToken());	// 값 변경회수	1<=이십만
-		long arr[]	= new long[N+3];
-		long sum[]	= new long[N];
-		long total	= 0;
-		
-		st = new StringTokenizer(br.readLine());
+		int N		= read();	// 소의개수	4<=이십만
+		int Q		= read();	// 값 변경회수	1<=이십만
+		int arr[]	= new int[N+3];
+		int sum[]	= new int[N];
+		int total	= 0;
+
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		arr[N]	= arr[0];
 		arr[N+1]= arr[1];
@@ -31,27 +25,28 @@ class Main{
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		st = new StringTokenizer(br.readLine());
 		while(Q-->0)
 		{
-			int idx[] = getIdx(Integer.parseInt(st.nextToken()), N);
-			
-			for(int i : idx)
+			int start = read();
+			for(int i=0; i<4; i++)
 			{
-				sum[i] = -sum[i];
-				total += sum[i] + sum[i];
+				int idx = --start;
+				if(idx < 0)
+					idx = N + idx;
+				
+				sum[idx] = -sum[idx];
+				total += sum[idx] + sum[idx];
 			}
 			sb.append(total).append('\n');
 		}
 		System.out.print(sb);
 	}
-	public static int[] getIdx(int start,int N) {
-		int idx[] = new int[4];
-		for(int i=0; i<4; i++) {
-			idx[i] = --start;
-			if(idx[i] < 0)
-				idx[i] = N + idx[i];
-		}
-		return idx;
-	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
