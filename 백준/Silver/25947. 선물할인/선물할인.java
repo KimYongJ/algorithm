442671a4527fc6b8704795b155179dae00fcbb58
YongJu Kim <kyjdummy@gmail.com>
2025-01-02 14:16:17 +0900

    [Silver I] Title: 선물할인, Time: 280 ms, Memory: 26012 KB -BaekjoonHub

--- 백준/Silver/25947. 선물할인/선물할인.java
+++ 백준/Silver/25947. 선물할인/선물할인.java
@@ -1,27 +1,21 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/25947
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N, M, G, arr[];
 	static long[] psum, half;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());	// 선물개수(1<=십만)
-		M		= Integer.parseInt(st.nextToken());	// 예산(1<=십억)
-		G		= Integer.parseInt(st.nextToken());	// 반값 가능한 최대선물수(0<=N)
+		N		= read();	// 선물개수(1<=십만)
+		M		= read();	// 예산(1<=십억)
+		G		= read();	// 반값 가능한 최대선물수(0<=N)
 		arr		= new int[N+1];
 		psum	= new long[N+1];
 		half	= new long[N+1];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
@@ -49,9 +43,11 @@ class Main{
 		System.out.print(res);
 	}
 	public static long validate(int cnt) {
-		if(cnt <= G)
-			return half[cnt];
-
-		return half[cnt] - half[cnt-G] + psum[cnt-G];
+		return cnt <= G ? half[cnt] : half[cnt] - half[cnt-G] + psum[cnt-G];
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
