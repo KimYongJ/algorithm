6e5eea26cae31e1f7fb52f3c9ae6e8ac5cab6c6b
YongJu Kim <kyjdummy@gmail.com>
2025-01-03 13:59:33 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 248 ms, Memory: 21952 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -1,34 +1,37 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/19951
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	//구덩이개수(1≤100,000)
-		int M		= Integer.parseInt(st.nextToken());	//조교수(1≤100,000)
+		int N		= read();	//구덩이개수(1≤100,000)
+		int M		= read();	//조교수(1≤100,000)
 		int arr[]	= new int[N+2];
 		int psum[]	= new int[N+2];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());// k가 양수인경우 a,b범위내 흙 추가, 음수는 흙 감소
+			int a = read();
+			int b = read();
+			int k = read();// k가 양수인경우 a,b범위내 흙 추가, 음수는 흙 감소
 			psum[a] += k;
 			psum[b+1] += -k;
 		}
+		
 		StringBuilder sb = new StringBuilder();
+		
 		for(int i=1; i<=N; i++)
 			sb.append(arr[i] + (psum[i] += psum[i-1])).append(' ');
 
 		System.out.print(sb);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
