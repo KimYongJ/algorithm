b9e5404b0fc49a40d03f61fb75d6560c00b36b10
YongJu Kim <kyjdummy@gmail.com>
2025-01-04 20:16:31 +0900

    [Gold V] Title: 사과나무, Time: 172 ms, Memory: 15308 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.java
+++ 백준/Gold/20002. 사과나무/사과나무.java
@@ -1,36 +1,33 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/20002
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int map[][] = new int[N+2][N+2];
 		
 		for(int y=1; y<=N; y++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=N; x++)
-				map[y][x] = Integer.parseInt(st.nextToken()) + map[y][x-1] + map[y-1][x] - map[y-1][x-1];
-		}
+				map[y][x] = read() + map[y][x-1] + map[y-1][x] - map[y-1][x-1];
 
 		int max = ~(1<<30);
 
 		for(int len = 0; len<N; len++)
-		{
 			for(int y=1; y+len<=N; y++)
-			{
 				for(int x=1; x+len<=N; x++)
 				{
 					int y1 = y+len;
 					int x1 = x+len;
 					max = Math.max(max, map[y1][x1] - map[y1][x-1] - map[y-1][x1] + map[y-1][x-1]);
 				}
-			}
-		}
 
 		System.out.print(max);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
