ea6fd9a1025ea381a86fc0c0d54a735f04f250bd
YongJu Kim <kyjdummy@gmail.com>
2025-01-05 11:46:37 +0900

    [Gold V] Title: 시간이 겹칠까?, Time: 240 ms, Memory: 22920 KB -BaekjoonHub

--- 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.java
+++ 백준/Gold/28018. 시간이 겹칠까？/시간이 겹칠까？.java
@@ -1,33 +1,30 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/28018
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder	sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int N		= Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N		= read();
 		int arr[]	= new int[1_000_002];
 		
 		while(N-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			arr[s]++;
-			arr[e+1]--;
+			arr[read()]++;
+			arr[read()+1]--;
 		}
 		
-		for(int i=2; i<=1000000; i++)
+		for(int i=2; i<=1_000_000; i++)
 			arr[i] += arr[i-1];
 		
-		int Q = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
+		int Q = read();
+		
 		while(Q-->0)
-			sb.append(arr[Integer.parseInt(st.nextToken())]).append('\n');
+			sb.append(arr[read()]).append('\n');
+		
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
