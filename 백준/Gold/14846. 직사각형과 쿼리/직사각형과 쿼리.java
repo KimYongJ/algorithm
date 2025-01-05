f3a40dddc7a7a28ea67d1da6a568a8c9e4533238
YongJu Kim <kyjdummy@gmail.com>
2025-01-05 16:52:43 +0900

    [Gold IV] Title: 직사각형과 쿼리, Time: 320 ms, Memory: 29076 KB -BaekjoonHub

--- 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.java
+++ 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.java
@@ -1,38 +1,27 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14846
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N			= Integer.parseInt(br.readLine());
+		int N			= read();
 		int map[][][]	= new int[11][N+2][N+2];
 		
 		for(int y=1; y<=N; y++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=N; x++)
 			{
-				int idx = Integer.parseInt(st.nextToken());
-				map[idx][y][x] = 1;
+				map[read()][y][x] = 1;
 				
 				for(int i=1; i<=10; i++)
 					map[i][y][x] += map[i][y-1][x] + map[i][y][x-1] - map[i][y-1][x-1];
 			}
-		}
 		
 		StringBuilder sb = new StringBuilder();
-		int Q = Integer.parseInt(br.readLine());
+		int Q = read();
 		while(Q-->0)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
+			int y1 = read();
+			int x1 = read();
+			int y2 = read();
+			int x2 = read();
 			int cnt = 0;
 			
 			for(int i=1; i<=10; i++)
@@ -42,5 +31,10 @@ class Main{
 			sb.append(cnt).append('\n');
 		}
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
