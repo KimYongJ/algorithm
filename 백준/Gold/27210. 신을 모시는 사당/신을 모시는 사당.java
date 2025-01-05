5381c4853e9b44df347018f719efc3231307ee9d
YongJu Kim <kyjdummy@gmail.com>
2025-01-05 11:37:00 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.java
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.java
@@ -1,24 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/27210
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int max		= 0;
 		int sum1	= 0;
 		int sum2	= 0;
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
 		{
-			int value = Integer.parseInt(st.nextToken());
-			
-			if(value == 1)
+			if(read() == 1)
 			{
 				sum1++;
 				sum2--;
@@ -39,4 +30,12 @@ class Main{
 		}
 		System.out.print(max);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
