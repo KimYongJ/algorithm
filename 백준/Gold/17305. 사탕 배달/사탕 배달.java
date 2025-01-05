060fa9dfdc6866077909915b36531fe60b8267bb
YongJu Kim <kyjdummy@gmail.com>
2025-01-05 16:11:37 +0900

    [Gold IV] Title: 사탕 배달, Time: 456 ms, Memory: 29248 KB -BaekjoonHub

--- 백준/Gold/17305. 사탕 배달/사탕 배달.java
+++ 백준/Gold/17305. 사탕 배달/사탕 배달.java
@@ -1,25 +1,18 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17305
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int W = Integer.parseInt(st.nextToken());
-		
 		PriorityQueue<Integer> g3 = new PriorityQueue<>((a,b)-> b-a);
 		PriorityQueue<Integer> g5 = new PriorityQueue<>((a,b)-> b-a);
+		int N = read();
+		int W = read();
 
 		for(int i=0; i<N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int g = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
+			int g = read();
+			int s = read();
 			if(g == 3)
 				g3.add(s);
 			else
@@ -28,16 +21,16 @@ class Main{
 		
 		long sum3[] = new long[g3.size() + 1];
 		long sum5[] = new long[g5.size() + 1];
-		int len3 = g3.size();
-		int len5 = g5.size();
+		int len3	= g3.size();
+		int len5	= g5.size();
 		
 		for(int i=1; i<=len3; i++)
 			sum3[i] = sum3[i-1] + g3.poll();
 		for(int i=1; i<=len5; i++)
 			sum5[i] = sum5[i-1] + g5.poll();
 		
-		
 		long max = 0;
+		
 		for(int i=0; i<=len5; i++)
 		{
 			int weight5 = i * 5;
@@ -49,5 +42,10 @@ class Main{
 			max = Math.max(max, sum5[i] + sum3[Math.min(remainWeight / 3, len3)]);
 		}
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
