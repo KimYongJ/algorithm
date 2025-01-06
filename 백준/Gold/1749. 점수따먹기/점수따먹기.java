5f40a373fe3dfc4aed891ee1d1beea436a76b9df
YongJu Kim <kyjdummy@gmail.com>
2025-01-06 15:27:02 +0900

    [Gold IV] Title: 점수따먹기, Time: 140 ms, Memory: 15160 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -6,16 +6,17 @@ class Main{
 		int X		= read();
 		int map[][] = new int[Y+2][X+2];
 		
+		// 카데인 알고리즘 사용을 위해 행단위로 누적합을 구함
 		for(int y=1; y<=Y; y++)
 			for(int x=1; x<=X; x++)
 				map[y][x] = read() + map[y][x-1];
 
 		int ans = ~(1<<30);
-		for(int x=1; x<=X; x++)
-			for(int x1=x; x1<=X; x1++)
+		for(int x=1; x<=X; x++)				// 왼쪽 오른쪽 열을 고정해놓고 y를 움직이면서 직사각형 중 가장 큰 것을 구한다.
+			for(int x1=x; x1<=X; x1++)		// 왼쪽 오른쪽 열을 고정해놓고 y를 움직이면서 직사각형 중 가장 큰 것을 구한다.
 			{
 				int sum = 0;
-				for(int y=1; y<=Y; y++)
+				for(int y=1; y<=Y; y++)		// y를 움직이면서 가장 큰 직사각형을 구함
 				{
 					int row = map[y][x1] - map[y][x-1];
 					
