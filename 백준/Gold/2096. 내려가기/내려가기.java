a571b626c2dd7d8fc346f418580cbb973c8716ea
YongJu Kim <kyjdummy@gmail.com>
2025-01-06 22:11:38 +0900

    [Gold V] Title: 내려가기, Time: 308 ms, Memory: 41784 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -14,7 +14,6 @@ class Main{
 		int a2,b2,c2;
 		int a3,b3,c3;
 		int x,y,z;
-		int x1,y1,z1;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		a = a1 = Integer.parseInt(st.nextToken());
 		b = b1 = Integer.parseInt(st.nextToken());
@@ -23,17 +22,17 @@ class Main{
 		for(int j=1; j<N; j++)
 		{
 			st = new StringTokenizer(br.readLine());
-			x = x1 = Integer.parseInt(st.nextToken());
-			y = y1 = Integer.parseInt(st.nextToken());
-			z = z1 = Integer.parseInt(st.nextToken());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			z = Integer.parseInt(st.nextToken());
 			
 			a2 = Math.max(a, b) + x;
 			b2 = Math.max(a, Math.max(b, c)) + y;
 			c2 = Math.max(b, c) + z;
 			
-			a3 = Math.min(a1, b1) + x1;
-			b3 = Math.min(a1, Math.min(b1, c1)) + y1;
-			c3 = Math.min(b1, c1) + z1;
+			a3 = Math.min(a1, b1) + x;
+			b3 = Math.min(a1, Math.min(b1, c1)) + y;
+			c3 = Math.min(b1, c1) + z;
 			
 			a = a2;
 			b = b2;
