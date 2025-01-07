d716b2aad6f1d13d1e5cc7ce320322d9e7ef7c05
YongJu Kim <kyjdummy@gmail.com>
2025-01-07 23:03:52 +0900

    [Silver III] Title: 두 수의 합, Time: 248 ms, Memory: 24448 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -2,35 +2,26 @@
 //https://www.acmicpc.net/problem/3273
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int N			= Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int arr[] = new int[N];
+		int arr[]		= new int[N];
+		boolean visit[] = new boolean[1_000_001];
 		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(arr);
+			visit[arr[i] = Integer.parseInt(st.nextToken())] = true;
 		
 		int x = Integer.parseInt(br.readLine());
-		int s = 0;
-		int e = N-1;
-		int c = 0;
-		while(s<e) {
-			int sum = arr[e] + arr[s];
-			if(sum == x)
-			{
-				++c;
-				++s;
-			}
-			else if(sum < x)++s;
-			else --e;
+		int cnt = 0;
+		for(int num : arr)
+		{
+			int target = x - num;
+			if(0 <= target && target <= 1_000_000 && visit[target])
+				++cnt;
 		}
-		
-		System.out.print(c);
+		System.out.print(cnt / 2);
 	}
 }
