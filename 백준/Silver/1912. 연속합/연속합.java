621d8efdf44ed5fb2ba35bfb15bb8fb0df491db8
YongJu Kim <kyjdummy@gmail.com>
2025-01-07 22:09:55 +0900

    [Silver II] Title: 연속합, Time: 124 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,23 +2,37 @@
 //https://www.acmicpc.net/problem/1912
 class Main{
 	public static void main(String[] args)throws Exception{
-		int N		= read();
+		int N		= readInt();
 		int curSum	= 0;
 		int maxSum	= ~(1<<30);
 		while(N-->0)
 		{
-			int n	= read();
+			int n	= readInt();
 			curSum	= Math.max(curSum + n, n);
 			maxSum	= Math.max(curSum, maxSum);
 		}
 		System.out.print(maxSum);
 	}
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        boolean m = n == 13;
-        if (m)n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48) {
-        n = (n << 3) + (n << 1) + (c & 15);}
-        return m ? ~n + 1 : n;
+    private static int readInt() throws Exception {
+        int t = 0;
+        byte v;
+        while ((v = read()) <= 32);
+        boolean negative = (v == '-');
+        if (negative) v = read();
+        do {
+            t = t * 10 + (v - '0');
+        } while ((v = read()) >= '0' && v <= '9');
+        return (negative) ? -t : t;
+    }
+
+    static final int SIZE = 1 << 13;
+    static byte[] buffer = new byte[SIZE];
+    static int index, size;
+    private static byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
