fa424f796735ca1d8648c916c8376bced1e22396
YongJu Kim <kyjdummy@gmail.com>
2025-01-07 22:14:36 +0900

    [Gold IV] Title: 목장 건설하기, Time: 156 ms, Memory: 18668 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
@@ -4,18 +4,13 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		int Y		= read();	//1<=천
 		int X		= read();	//1<=천
-		int map[][] = new int[Y+1][X+1];
 		int dp[][]	= new int[Y+1][X+1];
-		
-		for(int y=0; y<Y; y++)
-			for(int x=0; x<X; x++)
-				map[y][x] = read();
-		
+
 		int max = 0;
 		
 		for(int y=1; y<=Y; y++)
 			for(int x=1; x<=X; x++)
-				if(map[y-1][x-1] == 0)
+				if(read() == 0)
 				{
 					dp[y][x] = Math.min(dp[y-1][x], Math.min(dp[y][x-1], dp[y-1][x-1])) + 1;
 					max = Math.max(dp[y][x], max);
