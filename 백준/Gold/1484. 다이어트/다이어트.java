6830090eca71e4d549e3748751ab90956b3179e4
YongJu Kim <kyjdummy@gmail.com>
2025-01-07 22:42:57 +0900

    [Gold V] Title: 다이어트, Time: 116 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -0,0 +1,31 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1484
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int G	= Integer.parseInt(br.readLine());
+		long s	= 1;
+		long e	= 2;
+		
+		while(e <= 100_000)
+		{
+			long ss		= s*s;
+			long ee		= e*e;
+			long diff	= ee-ss;
+			
+			if(diff == G)
+				sb.append(e).append('\n');
+
+			if(diff<=G)
+				++e;
+			else
+				++s;
+		}
+		if(sb.length() == 0)
+			sb.append(-1);
+		System.out.print(sb);
+	}
+}
