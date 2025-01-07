7025df8ba37930947b48d45b4787351d5ae24aaa
YongJu Kim <kyjdummy@gmail.com>
2025-01-07 23:06:52 +0900

    [Silver III] Title: 두 수의 합, Time: 144 ms, Memory: 15704 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,27 +1,28 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3273
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N			= Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt			= 0;
+		int N			= read();
 		int arr[]		= new int[N];
 		boolean visit[] = new boolean[1_000_001];
 		
 		for(int i=0; i<N; i++)
-			visit[arr[i] = Integer.parseInt(st.nextToken())] = true;
+			visit[arr[i] = read()] = true;
 		
-		int x = Integer.parseInt(br.readLine());
-		int cnt = 0;
+		int x = read();
+		
 		for(int num : arr)
 		{
 			int target = x - num;
 			if(0 <= target && target <= 1_000_000 && visit[target])
 				++cnt;
 		}
-		System.out.print(cnt / 2);
+		System.out.print(cnt >> 1);
 	}
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
 }
