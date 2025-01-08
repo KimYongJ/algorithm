295beb3c7362446e206ddfc66d30c004df826750
YongJu Kim <kyjdummy@gmail.com>
2025-01-08 21:36:43 +0900

    [Gold III] Title: 소형기관차, Time: 120 ms, Memory: 15416 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.java
+++ 백준/Gold/2616. 소형기관차/소형기관차.java
@@ -1,19 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2616
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	// 객차수 3?<=오만
+		int N		= read();	// 객차수 3?<=오만
 		int psum[]	= new int[N+1];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			psum[i] += Integer.parseInt(st.nextToken()) + psum[i-1];// 손님의수 0<=백
+			psum[i] += read() + psum[i-1];// 손님의수 0<=백
 		
-		int max = Integer.parseInt(br.readLine());	// 최대로 끌 수 있는 객차 수
+		int max = read();	// 최대로 끌 수 있는 객차 수
 		
 		int dp[][] = new int[4][N+1];
 		
@@ -22,5 +17,10 @@ class Main{
 				dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j-max] + psum[j] - psum[j-max]);
 		
 		System.out.print(dp[3][N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
