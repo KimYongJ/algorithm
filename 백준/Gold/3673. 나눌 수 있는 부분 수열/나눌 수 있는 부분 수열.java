d2b421391574d963794c94fd3faa25a94ba57eaa
YongJu Kim <kyjdummy@gmail.com>
2025-01-08 21:32:58 +0900

    [Gold III] Title: 나눌 수 있는 부분 수열, Time: 288 ms, Memory: 74344 KB -BaekjoonHub

--- 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.java
+++ 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.java
@@ -1,25 +1,19 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/3673
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());// 테스트 케이스 수 (1<=200)
+		int T = read();// 테스트 케이스 수 (1<=200)
 		while(T-->0)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int M		= Integer.parseInt(st.nextToken()); // 나눌 수(1<=백만)
-			int N		= Integer.parseInt(st.nextToken()); // 수열의크기(1<=오만)
+			int M		= read(); // 나눌 수(1<=백만)
+			int N		= read(); // 수열의크기(1<=오만)
 			long arr[]	= new long[N+1];
-			st = new StringTokenizer(br.readLine());
 			long mod[]	= new long[M];
 			
 			for(int i=1; i<=N; i++)
 			{
-				arr[i] = Integer.parseInt(st.nextToken()) + arr[i-1];
+				arr[i] = read() + arr[i-1];
 				mod[(int)(arr[i] % M)]++;
 			}
 			
@@ -32,5 +26,10 @@ class Main{
 			
 		}
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
