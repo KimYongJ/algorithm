507c9d52a36cf062eb1d54cb22b9432c7e0bc432
YongJu Kim <kyjdummy@gmail.com>
2025-01-11 21:45:11 +0900

    [Silver II] Title: DNA 비밀번호, Time: 192 ms, Memory: 19720 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,37 +1,37 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12891
 class Main{
+	
+	static char[] str;
+	static int len, P, result;
+	static int[] ACGT;
+	
 	public static void main(String[] args)throws Exception{
-		char C[] = {'A','C','G','T'};
-		int len		= read();
-		int P		= read();
-		char str[]	= readString().toCharArray();
-		int ACGT[]	= new int[90];
-		int CNT[]	= new int[90];
+		len		= read();
+		P		= read();
+		str		= readString().toCharArray();
+		ACGT	= new int[90];
+		ACGT['A'] = read();
+		ACGT['C'] = read();
+		ACGT['G'] = read();
+		ACGT['T'] = read();
 
-		for(int i=0; i<4; i++)
-			ACGT[C[i]] = read();
-		
 		for(int i=0; i<P; i++)
-			CNT[str[i]]++;
+			ACGT[str[i]]--;
 		
-		int cnt = cal(ACGT, CNT, C);
+		if(0 >= ACGT['A'] && 0 >= ACGT['C'] && 0 >= ACGT['G'] && 0 >= ACGT['T'])
+			++result;
 		
 		for(int i=P; i<len; i++)
 		{
-			CNT[str[i - P]]--;
-			CNT[str[i]]++;
-			cnt += cal(ACGT, CNT, C);
+			ACGT[str[i - P]]++;
+			ACGT[str[i]]--;
+			if(0 >= ACGT['A'] && 0 >= ACGT['C'] && 0 >= ACGT['G'] && 0 >= ACGT['T'])
+				++result;
 		}
 		
-		System.out.print(cnt);
+		System.out.print(result);
 	}
-	private static int cal(int[] ACGT, int[] CNT, char[] ch) {
-		for(char c : ch)
-			if(ACGT[c] > CNT[c])
-				return 0;
-		return 1;
-	}	
 	static int read() throws Exception {
 		int c, n = System.in.read() & 15;
 		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
