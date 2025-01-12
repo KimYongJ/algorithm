c3891f76021a742f4c98a7dd9529ef449c22ca33
YongJu Kim <kyjdummy@gmail.com>
2025-01-12 17:21:12 +0900

    [Gold III] Title: 연속하는 소수의 합, Time: 240 ms, Memory: 26876 KB -BaekjoonHub

--- 백준/Gold/7512. 연속하는 소수의 합/연속하는 소수의 합.java
+++ 백준/Gold/7512. 연속하는 소수의 합/연속하는 소수의 합.java
@@ -1,9 +1,6 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/7512
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
 	
 	static final int MAX = 10_000_000;
@@ -11,6 +8,35 @@ class Main{
 	static int len;
 	static int[] prime, eIdx, sIdx, sum;
 	
+	public static void main(String args[])throws Exception{
+		StringBuilder sb = new StringBuilder();
+		
+		initPrime();
+		
+		int T = read();
+		for(int i=1; i<=T; i++)
+		{
+			int max = 0;
+			
+			len		= read();
+			eIdx	= new int[len];	// 각 배열의 종료인덱스
+			sIdx	= new int[len];	// 각 배열의 시작인덱스
+			sum		= new int[len];	// 각 배열마다의 총 합
+
+			for(int j=0; j<len; j++)
+			{
+				eIdx[j] = read();	// 종료인덱스를 입력받음, ex)3입력시 0~2까지 범위가되는것
+				for(int z=0; z<eIdx[j]; z++)
+					sum[j] += prime[z];	// sum[j] 에 종료인덱스 만큼 더한다. ex)3입력시 0~2번째 소수를 더함
+				
+				max = Math.max(max, sum[j]);
+			}
+			
+			sb.append("Scenario ").append(i).append(":\n")
+				.append(cal(max)).append("\n\n");
+		}
+		System.out.print(sb);
+	}
 	public static void initPrime(){
 		int size = 0;
 		isPrime = new boolean[MAX];
@@ -31,7 +57,6 @@ class Main{
 				prime[j++] = i;
 	}
 	public static int cal(int max) {
-
 		while(isContinue())	// sum배열에 같은 값이 모두 들어가있는지 체크 및 무한반복
 		{
 			for(int i=0; i<len; i++)		// sum배열을 돌면서 max보다 작거나, 소수가 아니면 계속 증가시킨다
@@ -48,7 +73,6 @@ class Main{
 				max = Math.max(sum[i], max);
 			}
 		}
-		
 		return max;
 	}
 	public static boolean isContinue(){
@@ -58,37 +82,9 @@ class Main{
 				return true;
 		return false;
 	}
-	
-	public static void main(String args[])throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder	sb = new StringBuilder();
-		
-		initPrime();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for(int i=1; i<=T; i++)
-		{
-			int max = 0;
-			
-			len		= Integer.parseInt(br.readLine());
-			eIdx	= new int[len];	// 각 배열의 종료인덱스
-			sIdx	= new int[len];	// 각 배열의 시작인덱스
-			sum		= new int[len];	// 각 배열마다의 총 합
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<len; j++)
-			{
-				eIdx[j] = Integer.parseInt(st.nextToken());	// 종료인덱스를 입력받음, ex)3입력시 0~2까지 범위가되는것
-				for(int z=0; z<eIdx[j]; z++)
-					sum[j] += prime[z];	// sum[j] 에 종료인덱스 만큼 더한다. ex)3입력시 0~2번째 소수를 더함
-				
-				max = Math.max(max, sum[j]);
-			}
-			
-			sb.append("Scenario ").append(i).append(":\n")
-				.append(cal(max)).append("\n\n");
-		}
-		System.out.print(sb);
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
