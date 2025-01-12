4177fe6e1da913a96a72d469a2a1d1b6b77e03d7
YongJu Kim <kyjdummy@gmail.com>
2025-01-12 20:21:23 +0900

    [Gold III] Title: Load Balancing (Silver), Time: 196 ms, Memory: 20728 KB -BaekjoonHub

--- 백준/Gold/11997. Load Balancing （Silver）/Load Balancing （Silver）.java
+++ 백준/Gold/11997. Load Balancing （Silver）/Load Balancing （Silver）.java
@@ -7,19 +7,18 @@ import java.util.StringTokenizer;
 import java.util.TreeSet;
 class Main{
 	
+	static HashMap<Integer, Integer> mapY = new HashMap<>();;
+	static HashMap<Integer, Integer> mapX = new HashMap<>();;
 	static TreeSet<Integer> setY = new TreeSet<>();
 	static TreeSet<Integer> setX = new TreeSet<>();
-	static HashMap<Integer, Integer> mapY;
-	static HashMap<Integer, Integer> mapX;
 	static int map[][];
 	
 	public static void main(String[] args)throws Exception{
-		TreeSet<Integer> setY = new TreeSet<>();
-		TreeSet<Integer> setX = new TreeSet<>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N		= Integer.parseInt(br.readLine());
 		int pos[][] = new int[N][2];
-		
+		int yIdx	= 0;
+		int xIdx	= 0;
 		for(int i=0; i<N; i++)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -28,44 +27,37 @@ class Main{
 			setY.add(pos[i][0]);
 			setX.add(pos[i][1]);
 		}
-		mapY = new HashMap<>();
-		mapX = new HashMap<>();
-		int idx = 0;
-		for(int y : setY)
-			mapY.put(y, ++idx);
-		idx = 0;
-		for(int x : setX)
-			mapX.put(x, ++idx);
 		
-		int Y	= setY.size();
-		int X	= setX.size();
-		map		= new int[Y+2][X+2];
+		for(int y : setY) mapY.put(y, ++yIdx);
+		for(int x : setX) mapX.put(x, ++xIdx);
+		
+		map	= new int[yIdx+2][xIdx+2];
 		
 		for(int i=0; i<N; i++)
-		{
-			int y = mapY.get(pos[i][0]);
-			int x = mapX.get(pos[i][1]);
-			map[y][x] = 1;
-		}
-		for(int y=1; y<=Y; y++)
-			for(int x=1; x<=X; x++)
+			map[mapY.get(pos[i][0])][mapX.get(pos[i][1])] = 1;
+		
+		for(int y=1; y<=yIdx; y++)
+			for(int x=1; x<=xIdx; x++)
 				map[y][x] += map[y-1][x] + map[y][x-1] - map[y-1][x-1];
 		
 		int min = 1<<30;
-		for(int y=1; y<=Y; y++)
-			for(int x=1; x<=X; x++)
+		for(int y=1; y<=yIdx; y++)
+			for(int x=1; x<=xIdx; x++)
 			{
-				int max = 0;
-				max = Math.max(max, getMin(1,1,y,x));		// 2사분면
-				max = Math.max(max, getMin(1,x+1,y,X));		// 1사분면
-				max = Math.max(max, getMin(y+1,1,Y,x));		// 3사분면
-				max = Math.max(max, getMin(y+1,x+1,Y,X));	// 4사분면
+				// 2,1,3,4 분면 순서
+				int max = getMax(
+								getSum(1,1,y,x), getSum(1,x+1,y,xIdx)
+								,getSum(y+1,1,yIdx,x), getSum(y+1,x+1,yIdx,xIdx)
+						);
 				min = Math.min(max, min);
 			}
 		
 		System.out.print(min);
 	}
-	public static int getMin(int y1, int x1, int y2, int x2) {
+	public static int getMax(int a, int b, int c, int d) {
+		return Math.max(a, Math.max(b, Math.max(c,d)));
+	}
+	public static int getSum(int y1, int x1, int y2, int x2) {
 		return map[y2][x2] - map[y2][x1-1] - map[y1-1][x2] + map[y1-1][x1-1];
 	}
 }
