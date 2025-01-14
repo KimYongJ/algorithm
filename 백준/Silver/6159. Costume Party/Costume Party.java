2882f629ed60e98878d435f341b2c5c80afabaf2
YongJu Kim <kyjdummy@gmail.com>
2025-01-14 23:24:37 +0900

    [Silver V] Title: Costume Party, Time: 132 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/6159. Costume Party/Costume Party.java
+++ 백준/Silver/6159. Costume Party/Costume Party.java
@@ -1,37 +1,35 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/6159
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 소의 수(2<=이만)
-		int S		= Integer.parseInt(st.nextToken());	// 매칭값(1<=백만)
+		int N		= read();	// 소의 수(2<=이만)
+		int S		= read();	// 매칭값(1<=백만)
 		int arr[]	= new int[N];
 		int res		= 0;
 		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(br.readLine());// 매칭값(1<=백만)
+			arr[i] = read();// 매칭값(1<=백만)
 		
 		Arrays.sort(arr);
 		
 		int s = 0;
 		int e = N - 1;
-		while(s<e) {
+		while(s<e)
+		{
 			int sum = arr[s] + arr[e];
-			if(sum <= S) {
-				res += e - s;
-			}
 			if(sum <= S)
+			{
+				res += e - s;
 				++s;
-			if(S < sum)
-				--e;
+			}
+			else --e;
 		}
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
