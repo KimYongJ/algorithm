fc84744286695b3626d0fee4f2ff4b3ba7e883af
YongJu Kim <kyjdummy@gmail.com>
2025-01-14 22:04:58 +0900

    [Silver II] Title: 과일 탕후루, Time: 136 ms, Memory: 15704 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -1,21 +1,16 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/30804
 //2초 / 1024MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	//개수(1<=이십만)
+		int N		= read();	//개수(1<=이십만)
 		int arr[]	= new int[N];
 		int dp[]	= new int[10];
 		int max		= 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int s=0, e=0, cnt = 0; e<N; e++)
 		{
-			arr[e] = Integer.parseInt(st.nextToken());	// 1<=9사이 정수
+			arr[e] = read();	// 1<=9사이 정수
 			if(dp[arr[e]]++ == 0)
 				++cnt;
 			
@@ -27,5 +22,10 @@ class Main{
 		}
 		
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
