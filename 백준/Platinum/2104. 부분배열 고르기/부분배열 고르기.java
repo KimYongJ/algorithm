a78fdf7b32df5c0a5d306c4dfd4461a14d0cdac8
YongJu Kim <kyjdummy@gmail.com>
2025-01-15 21:28:04 +0900

    [Platinum V] Title: 부분배열 고르기, Time: 160 ms, Memory: 18440 KB -BaekjoonHub

--- 백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.java
+++ 백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.java
@@ -1,33 +1,28 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/2104
 //2초 / 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N	= Integer.parseInt(br.readLine());// 1<=십만
+		int N		= read();// 1<=십만
 		long[] arr	= new long[N+2];
 		long[] psum = new long[N+1];
 		long res	= 0;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++) {
-			psum[i] = arr[i] = Long.parseLong(st.nextToken());//0<=백만
+		
+		for(int i=1; i<=N; i++)
+		{
+			psum[i] = arr[i] = read();//0<=백만
 			psum[i] += psum[i-1];
 		}
 		
 		Stack stack = new Stack();
+		stack.push(0);
 		for(int i=1; i<=N+1; i++)
 		{
 			while(!stack.isEmpty() && arr[stack.peek()] > arr[i])
 			{
 				long H = arr[stack.pop()];
-				int idx = 0;
-				if(!stack.isEmpty())
-					idx = stack.peek();
+				int idx = stack.peek();
 				
 				long W = psum[i-1] - psum[idx];
 				
@@ -36,6 +31,11 @@ class Main{
 			stack.push(i);
 		}
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
 class Stack{
