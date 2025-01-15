58d9dcba420d1747cbf1b5cf4cfac13639f8d3b3
YongJu Kim <kyjdummy@gmail.com>
2025-01-15 21:34:52 +0900

    [Silver III] Title: 고양이 카페, Time: 120 ms, Memory: 15196 KB -BaekjoonHub

--- 백준/Silver/28353. 고양이 카페/고양이 카페.java
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.java
@@ -1,24 +1,18 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/28353
 //2초 / 1024MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 고양이수(1≤N≤오천)
-		int K = Integer.parseInt(st.nextToken());	// 최대무게K(1≤K≤10의9승)
-		int C = 0;
-		int s = 0;
-		int e = N-1;
-		int arr[] = new int[N];
-		st = new StringTokenizer(br.readLine());
+		int N		= read();	// 고양이수(1≤N≤오천)
+		int K		= read();	// 최대무게K(1≤K≤10의9승)
+		int C		= 0;
+		int s		= 0;
+		int e		= N-1;
+		int arr[]	= new int[N];
+		
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
@@ -34,5 +28,10 @@ class Main{
 				--e;
 		}
 		System.out.print(C);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
