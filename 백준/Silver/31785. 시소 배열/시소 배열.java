1608e604e3e7fe61cc6f8860fb3645522b432ba7
YongJu Kim <kyjdummy@gmail.com>
2025-01-15 23:04:45 +0900

    [Silver I] Title: 시소 배열, Time: 684 ms, Memory: 118592 KB -BaekjoonHub

--- 백준/Silver/31785. 시소 배열/시소 배열.java
+++ 백준/Silver/31785. 시소 배열/시소 배열.java
@@ -1,81 +1,50 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    private void solution(int n, int[][] arr) {
-        StringBuilder answer = new StringBuilder();
-        int[] nums = new int[500001];
-        int startIndex = 0;
-        int nextIndex = 0;
-        double frontSum = 0;
-        double backSum = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if (arr[i][0] == 1) {
-                nums[nextIndex] = arr[i][1];
-                backSum += arr[i][1];
-                nextIndex += 1;
-
-                int count = nextIndex - startIndex;
-                if (count % 2 == 0) {
-                    int midIndex = startIndex + count / 2 - 1;
-                    frontSum += nums[midIndex];
-                    backSum -= nums[midIndex];
-                }
-            } else {
-                int count = nextIndex - startIndex;
-
-                if (frontSum > backSum) {
-                    answer.append((long)backSum).append('\n');
-                    backSum = 0;
-                    nextIndex = startIndex + count / 2;
-                } else {
-                    answer.append((long)frontSum).append('\n');
-                    frontSum = 0;
-                    startIndex += count / 2;
-                }
-
-                count = nextIndex - startIndex;
-                if (count == 1) {
-                    frontSum = 0;
-                    backSum = nums[startIndex];
-                } else if (backSum == 0) {
-                    int last = count / 2 - 1;
-                    for (int j = count - 1; j != last; j--) {
-                        frontSum -= nums[startIndex + j];
-                        backSum += nums[startIndex + j];
-                    }
-                } else {
-                    int last = count / 2;
-                    for (int j = 0; j != last; j++) {
-                        backSum -= nums[startIndex + j];
-                        frontSum += nums[startIndex + j];
-                    }
-                }
-            }
-        }
-        
-        for (int i = startIndex; i < nextIndex; i++) {
-            answer.append(nums[i]).append(' ');
-        }
-        if (answer.length() > 0) {
-            answer.setLength(answer.length() - 1);
-        }
-
-        System.out.println(answer);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            String[] temp = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(temp[0]);
-            arr[i][1] = arr[i][0] == 2 ? 0 : Integer.parseInt(temp[1]);
-        }
-
-        new Main().solution(n, arr);
-    }
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/31785
+//1초 / 1024MB
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T		= Integer.parseInt(br.readLine());
+		int arr[]	= new int[500_001];
+		int s		= 0;
+		int e		= 0;
+		
+		while(T-->0)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int type = Integer.parseInt(st.nextToken());
+			if(type == 1)
+				arr[e++] = Integer.parseInt(st.nextToken());
+			else
+			{
+				int mid = s + (e - s)/2;
+				
+				int sum1 = getSum(arr, s, mid);
+				int sum2 = getSum(arr, mid, e);
+				if(sum1 <= sum2)
+				{
+					sb.append(sum1).append('\n');
+					s = mid;
+				}
+				else
+				{
+					sb.append(sum2).append('\n');
+					e = mid;
+				}
+			}
+		}
+		while(s<e)
+			sb.append(arr[s++]).append(' ');
+		System.out.print(sb);
+	}
+	public static int getSum(int arr[], int s, int e) {
+		int sum = 0;
+		while(s<e)
+			sum += arr[s++];
+		return sum;
+	}
 }
