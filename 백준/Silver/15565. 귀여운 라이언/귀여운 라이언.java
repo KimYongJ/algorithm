5b1208ba7003b8d00db9ce1a3d00d80d55e54fa8
YongJu Kim <kyjdummy@gmail.com>
2025-01-15 21:30:27 +0900

    [Silver I] Title: 귀여운 라이언, Time: 144 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,28 +1,27 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/15565
 //1초 / 256MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	//숫자의 개수(1<=10의6승)
-		int K		= Integer.parseInt(st.nextToken());	//1의최소개수(1<=N)
+		int N		= read();		//숫자의 개수(1<=10의6승)
+		int K		= read();		//1의최소개수(1<=N)
 		int arr[]	= new int[N];	// 슬라이딩 윈도우 활용, arr에는 1의 인덱스만 담는다.
 		int len		= 0;
 		int res		= 1<<30;
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			if(Integer.parseInt(st.nextToken()) == 1)
+			if(read() == 1)
 				arr[len++] = i;
 		
-		// k만큼 옮겨가며 길이를 계산
-		for(int i=K-1; i<len; i++)
-			res = Math.min(res, arr[i] - arr[i-K+1] + 1);
+		// 슬라이딩 윈도우, 0부터 K만큼 옮겨가며 길이 계산
+		for(int i=K-1,j=0; i<len; i++,j++)
+			res = Math.min(res, arr[i] - arr[j] + 1);
 		
 		System.out.print(res == (1<<30) ? -1 : res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
