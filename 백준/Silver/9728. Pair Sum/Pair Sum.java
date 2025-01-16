f49b5d1aa3e258768cb49429091d3e6350dcb76e
YongJu Kim <kyjdummy@gmail.com>
2025-01-16 22:45:33 +0900

    [Silver IV] Title: Pair Sum, Time: 424 ms, Memory: 43664 KB -BaekjoonHub

---
+++ 백준/Silver/9728. Pair Sum/Pair Sum.java
@@ -0,0 +1,44 @@
+//https://github.com/KimYongJ/algorithm
+//https://www.acmicpc.net/problem/9728
+// 1초 / 128MB
+// 요약 : 두 원소의 합이 정확히 M과 같은 것의 숫자를 센다, 배열은 오름차순정렬되있음
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());// 10만이하
+		for(int i=1; i<=T; i++)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N		= Integer.parseInt(st.nextToken());	// 2<=이만
+			int M		= Integer.parseInt(st.nextToken());
+			int arr[]	= new int[N];						// 1<=십억
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++)
+				arr[j] = Integer.parseInt(st.nextToken());
+			
+			int s = 0;
+			int e = N-1;
+			int c = 0;
+			while(s<e)
+			{
+				int sum = arr[s] + arr[e];
+				if(sum <= M)
+				{
+					++s;
+					if(sum==M)
+						++c;
+				}
+				else
+					--e;
+			}
+			sb.append("Case #").append(i).append(": ").append(c).append('\n');
+		}
+		System.out.print(sb);
+	}
+}
