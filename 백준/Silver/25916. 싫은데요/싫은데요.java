fd0e3f1932ee035c228592891a1053269e7cee86
YongJu Kim <kyjdummy@gmail.com>
2025-01-16 20:14:01 +0900

    [Silver I] Title: 싫은데요, Time: 204 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.java
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -1,20 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/25916
 //1초 / 1024MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 배열개수(1≤오십만)
-		int M		= Integer.parseInt(st.nextToken());	// 사용가능한 최대 값(1≤10의9승)
+		int N		= read();	// 배열개수(1≤오십만)
+		int M		= read();	// 사용가능한 최대 값(1≤10의9승)
 		int arr[]	= new int[N];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		int s	= 0;
 		int e	= 0;
@@ -32,5 +26,10 @@ class Main{
 		}
 		
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
