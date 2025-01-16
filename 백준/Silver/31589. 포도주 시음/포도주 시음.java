8b77fba7fb27cf0652ffa0e733eb73ff4f6da083
YongJu Kim <kyjdummy@gmail.com>
2025-01-16 20:15:27 +0900

    [Silver III] Title: 포도주 시음, Time: 536 ms, Memory: 28740 KB -BaekjoonHub

--- 백준/Silver/31589. 포도주 시음/포도주 시음.java
+++ 백준/Silver/31589. 포도주 시음/포도주 시음.java
@@ -1,33 +1,34 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/31589
 //1초 / 512MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());	
-		int N		= Integer.parseInt(st.nextToken());	// 포도주 수N (1≤300,000)
-		int K		= Integer.parseInt(st.nextToken());	// 최대 선택 가능 수(1≤N)
+		int N		= read();	// 포도주 수N (1≤300,000)
+		int K		= read();	// 최대 선택 가능 수(1≤N)
 		long arr[]	= new long[N];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		Arrays.sort(arr);
 		
-		long res = arr[N-1];
-		int s = 0;
-		int e = N - 2;
+		long res	= arr[N-1];
+		int s		= 0;
+		int e		= N - 2;
+		
 		--K;
+		
 		while(K>1)
 		{
 			res += arr[e--] - arr[s++];
 			K-=2;
 		}
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
