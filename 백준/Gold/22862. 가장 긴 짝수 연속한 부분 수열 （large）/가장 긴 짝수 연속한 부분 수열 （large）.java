22d2fde973de663d4757fc47c160bab69fa588f6
YongJu Kim <kyjdummy@gmail.com>
2025-01-17 21:32:58 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 188 ms, Memory: 19008 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -42,12 +42,11 @@ class Reader {
     int nextInt() throws Exception {
         int n = 0;
         byte c;
-        boolean isMinus = false;
         while ((c = read()) <= 32) { if (size < 0) return -1; }
-        if (c == 45) { c = read(); isMinus = true; }
+        if (c == 45) { c = read();}
         do n = (n << 3) + (n << 1) + (c & 15);
         while (isNumber(c = read()));
-        return isMinus ? ~n + 1 : n;
+        return  n;
     }
 
 
