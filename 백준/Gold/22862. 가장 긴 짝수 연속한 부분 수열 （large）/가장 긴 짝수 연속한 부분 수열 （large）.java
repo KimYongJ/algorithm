86f00c2567d9edc1f1ebd43cd85839782067e402
YongJu Kim <kyjdummy@gmail.com>
2025-01-17 21:32:25 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 188 ms, Memory: 19124 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -5,12 +5,13 @@
 // 비슷한 백준 문제 : 22857
 class Main{
 	public static void main(String[] args)throws Exception{
-		int N		= read();// 수열길이(1<=백만)
-		int K		= read();// 최대 삭제 횟수(1<=십만)
+		Reader in	= new Reader();
+		int N		= in.nextInt();// 수열길이(1<=백만)
+		int K		= in.nextInt();// 최대 삭제 횟수(1<=십만)
 		int arr[]	= new int[N];
 
 		for(int i=0; i<N; i++)
-			arr[i] = read() % 2;
+			arr[i] = in.nextInt() % 2;
 		
 		int s	= 0;
 		int e	= 0;
@@ -30,9 +31,33 @@ class Main{
 		}
 		System.out.print(max);
 	}
-	static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
-		return n;
-	}
+}
+
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+
+    boolean isNumber(byte c) {return 47 < c && c < 58;}
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
