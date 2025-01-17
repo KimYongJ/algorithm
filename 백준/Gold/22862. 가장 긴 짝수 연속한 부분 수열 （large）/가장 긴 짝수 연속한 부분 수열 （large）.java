820c50a197307ed92219dee14d51e1cc511f468d
YongJu Kim <kyjdummy@gmail.com>
2025-01-17 21:29:04 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 268 ms, Memory: 18380 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -3,20 +3,14 @@
 // 1초 / 1024MB
 // 수열에서 읨의로 K번 수를 삭제한 후 짝수로 이뤄진 연속한 부분 수열 중 가장 긴 길이 출력
 // 비슷한 백준 문제 : 22857
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());// 수열길이(1<=백만)
-		int K		= Integer.parseInt(st.nextToken());// 최대 삭제 횟수(1<=십만)
+		int N		= read();// 수열길이(1<=백만)
+		int K		= read();// 최대 삭제 횟수(1<=십만)
 		int arr[]	= new int[N];
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken()) % 2;
+			arr[i] = read() % 2;
 		
 		int s	= 0;
 		int e	= 0;
@@ -35,5 +29,10 @@ class Main{
 					--k;
 		}
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
