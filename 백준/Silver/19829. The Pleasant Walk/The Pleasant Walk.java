5c8de7dd60c036a2944423918026e615f7df98d5
YongJu Kim <kyjdummy@gmail.com>
2025-01-18 09:46:54 +0900

    [Silver V] Title: The Pleasant Walk, Time: 128 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Silver/19829. The Pleasant Walk/The Pleasant Walk.java
+++ 백준/Silver/19829. The Pleasant Walk/The Pleasant Walk.java
@@ -1,39 +1,34 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/19829
 //1초 / 512MB
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+//요약 : 배열의 인접한 숫자가 서로 다른 가장긴 연속된 부분수열 찾기
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 집의 수(1<=십만)
-		int K		= Integer.parseInt(st.nextToken());	// 색상 수(1<=십만)
+		int N		= read();	// 집의 수(1<=십만)
+		int K		= read();	// 색상 수(1<=십만)
 		int max		= 1;
 		int arr[]	= new int[N];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		int s = 0;
 		int e = 1;
 		while(e<N)
 		{
 			if(arr[e] != arr[e-1])
-			{
 				max = Math.max(max, e-s+1);
-				++e;
-			}
 			else
-			{
-				s = e ++;
-			}
+				s = e;
+			
+			++e;
 		}
 		
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
