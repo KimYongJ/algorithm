4d3141518a3086400363bb63099afe5210052e59
YongJu Kim <kyjdummy@gmail.com>
2025-01-20 10:23:59 +0900

    [Gold IV] Title: 팀 빌딩, Time: 128 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.java
@@ -2,26 +2,21 @@
 //https://www.acmicpc.net/problem/22945
 //1초 / 1024MB
 //요약 : 배열에서 2명을 뽑는데, 뽑을 때 능력치가 최대가 되는 능력치를 출력, 능력치 공식 : [A,B사이 존재하는 다른 원소 수 *MIN(A,B능력치)]
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	// 사람수(2<=십만)
-		int arr[]	= new int[N];						// 능력치(1<=만)
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N		= read();		// 사람수(2<=십만)
+		int arr[]	= new int[N];	// 능력치(1<=만)
+
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
-		int res = 0;
-		int s = 0;
-		int e = N - 1;
+		int res	= 0;
+		int s	= 0;
+		int e	= N - 1;
+		
 		while(s<e)
 		{
-			int min2 = Math.min(arr[e], arr[s]);
-			int value = (e - s - 1) * min2;
+			int value = (e - s - 1) * Math.min(arr[e], arr[s]);
 			if(res < value)
 				res = value;
 			
@@ -29,8 +24,13 @@ class Main{
 				--e;
 			else
 				++s;
-			
 		}
+		
 		System.out.print(res);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
