bbec89427e7511a8331028cac78fe686764b7288
YongJu Kim <kyjdummy@gmail.com>
2025-01-20 23:13:33 +0900

    [Gold V] Title: 근성아 일하자, Time: 672 ms, Memory: 43016 KB -BaekjoonHub

--- 백준/Gold/32358. 근성아 일하자/근성아 일하자.java
+++ 백준/Gold/32358. 근성아 일하자/근성아 일하자.java
@@ -1,27 +1,22 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/32358
 //1초 / 1024MB
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+//요약 : type이 1일 때 배열에 값을 넣고, 2일 때 현재 위치에서 가장가까운 곳이동하면서 그 거리를 더한다. 
 import java.util.TreeSet;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		TreeSet<Integer> set = new TreeSet<>();
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int now		= 0;
 		long res	= 0;
 
 		while(N-->0)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int type = Integer.parseInt(st.nextToken());
+			int type = read();
 			if(type == 1)
 			{
-				int value = Integer.parseInt(st.nextToken());
+				int value = read();
 				if(value != now)
 					set.add(value);
 			}
@@ -51,5 +46,13 @@ class Main{
 		}
 		System.out.print(res);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 
 }
