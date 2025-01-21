541fe7916facaf41dbb7852e1b6d0da60122dae2
YongJu Kim <kyjdummy@gmail.com>
2025-01-21 11:15:46 +0900

    [Gold IV] Title: 불꽃놀이, Time: 556 ms, Memory: 43616 KB -BaekjoonHub

---
+++ 백준/Gold/32375. 불꽃놀이/불꽃놀이.java
@@ -0,0 +1,46 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/32375
+//1초 / 1024MB
+//요약 : K점수를 시작으로 점점 큰수를 만든다, 수는 1~2개 더해서 K이상의 수를 계속 만들어나가며 만든 횟수를 최대로 출력, 없으면 -1출력
+//팁 : K가 증가해야하는 것은 고려할 필요 없음, 배열에서 1~2개를 사용해 K보다 큰것만 만들어내면 됨 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N		= Integer.parseInt(st.nextToken());	// 폭죽개수(1<=이십만)
+		int K		= Integer.parseInt(st.nextToken());	// 시작 화려한 점수(1<=십억)
+		int arr[]	= new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());	// 화려한 정도(1<=십억)
+		
+		Arrays.sort(arr);
+		
+		int s = 0;
+		int e = N-1;
+		int ans = 0;
+		while(s <= e)
+		{
+			if(K <= arr[e])
+			{
+				++ans;
+				--e;
+			}
+			else if(K <= arr[s] + arr[e] && s != e)
+			{
+				++ans;
+				++s;
+				--e;
+			}
+			else
+				++s;
+		}
+		
+		System.out.print(ans == 0 ? -1 : ans);
+	}
+}
