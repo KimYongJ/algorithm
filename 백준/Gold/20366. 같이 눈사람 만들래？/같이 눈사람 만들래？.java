82d89d7bfea697f3f59989030644bd0745f0e793
YongJu Kim <kyjdummy@gmail.com>
2025-01-22 16:22:50 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 352 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -4,11 +4,8 @@
 //요약 : 배열에서 2개씩 2번골라서 각각 합친 값의 차이가 최소인 값 출력 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
-class pair{int sum, i,j;pair(int sum, int i, int j){this.sum=sum; this.i=i; this.j=j;}}
 
 class Main{
 	public static void main(String[] args)throws Exception{
@@ -19,22 +16,28 @@ class Main{
 		for(int i=0; i<N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());// 지름(1<=십억)
 		
-		ArrayList<pair> list = new ArrayList<>();
-		for(int i=0; i<N-1; i++)
-			for(int j=i+1; j<N; j++)
-				list.add(new pair(arr[i] + arr[j], i, j));
-		
-		Collections.sort(list,(a,b)->a.sum-b.sum);
+		Arrays.sort(arr);
 		
 		int min = Integer.MAX_VALUE;
-		for(int i=1; i<list.size(); i++)
-		{
-			pair p1 = list.get(i-1);
-			pair p2 = list.get(i);
-			if(p1.i == p2.i || p1.i == p2.j || p1.j == p2.i || p1.j == p2.j)
-				continue;
-			min = Math.min(min, Math.abs(p1.sum-p2.sum));
-		}
+		for(int l=0; l<N; l++)
+			for(int r=l+3; r<N; r++)
+			{
+				int sum1 = arr[l] + arr[r];
+				int s = l+1;
+				int e = r-1;
+				while(s<e)
+				{
+					int sum2 = arr[s] + arr[e];
+					
+					min = Math.min(min, Math.abs(sum1-sum2));
+					
+					if(sum1 < sum2)
+						--e;
+					else
+						++s;
+				}
+			}
+		
 		System.out.print(min);
 	}
 }
