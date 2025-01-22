72a00e0448903a16b1c0cd9e5e8637f9e8fe474b
YongJu Kim <kyjdummy@gmail.com>
2025-01-22 21:50:38 +0900

    [Platinum V] Title: Counting Inversions, Time: 500 ms, Memory: 34080 KB -BaekjoonHub

--- 백준/Platinum/10090. Counting Inversions/Counting Inversions.java
+++ 백준/Platinum/10090. Counting Inversions/Counting Inversions.java
@@ -2,14 +2,27 @@
 //https://www.acmicpc.net/problem/10090
 //1초 / 256MB
 //요약 : 배열의 왼쪽부터 시작하여 오른쪽으로 갈때 자기보다 작은 쌍이 몇개나있는지 쌍의 총개수 출력
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N;
 	static int[] arr, tree;
 
+	public static void main(String[] args)throws Exception{
+		N	= read();
+		tree= new int[N<<2];
+		arr	= new int[N];
+
+		for(int i=0; i<N; i++)
+			arr[i] = read();
+		
+		long sum = 0;
+		for(int i=N-1; i>=0; i--)
+		{
+			sum += query(1, 1, N, 1, arr[i] - 1);
+			update(1, 1, N, arr[i]);
+		}
+		System.out.print(sum);
+	}
 	public static int query(int treeNode, int s, int e, int left, int right) {
 		if(e < left || right < s)
 			return 0;
@@ -37,22 +50,9 @@ class Main{
 			update(nextNode | 1, mid + 1, e, idx);
 		}
 	}
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N	= Integer.parseInt(br.readLine());
-		tree= new int[N<<2];
-		arr	= new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		
-		long sum = 0;
-		for(int i=N-1; i>=0; i--)
-		{
-			sum += query(1, 1, N, 1, arr[i] - 1);
-			update(1, 1, N, arr[i]);
-		}
-		System.out.print(sum);
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
