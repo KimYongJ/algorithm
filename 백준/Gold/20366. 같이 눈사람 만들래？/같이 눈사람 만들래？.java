4d48b0b71c6b68607cc4b79fc0e47ef22475f787
YongJu Kim <kyjdummy@gmail.com>
2025-01-22 14:40:18 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 488 ms, Memory: 29416 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -4,8 +4,12 @@
 //요약 : 배열에서 2개씩 2번골라서 각각 합친 값의 차이가 최소인 값 출력 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
+
+class pair{int sum, i,j;pair(int sum, int i, int j){this.sum=sum; this.i=i; this.j=j;}}
+
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,47 +19,22 @@ class Main{
 		for(int i=0; i<N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());// 지름(1<=십억)
 		
-		Arrays.sort(arr);
+		ArrayList<pair> list = new ArrayList<>();
+		for(int i=0; i<N-1; i++)
+			for(int j=i+1; j<N; j++)
+				list.add(new pair(arr[i] + arr[j], i, j));
+		
+		Collections.sort(list,(a,b)->a.sum-b.sum);
 		
 		int min = Integer.MAX_VALUE;
-		for(int i=0; i<N-1; i++)
+		for(int i=1; i<list.size(); i++)
 		{
-			for(int j=i+1; j<N; j++)
-			{
-				int s = plus1(0,i,j);
-				int e = plus2(1, i, j, s);
-				while(s<N && e<N)
-				{
-					int sumsi = arr[s] + arr[i];
-					int sumej = arr[e] + arr[j];
-					int abs1 = Math.abs(sumsi - sumej);
-					if(sumsi < sumej)
-						s = plus2(s+1,i,j, e);
-					else e = plus2(e+1, i,j,s);
-					min = Math.min(min, abs1);
-				}
-				s = plus1(0,i,j);
-				e = plus2(1, i, j, s);
-				while(s<N && e<N)
-				{
-					int sumsj = arr[s] + arr[j];
-					int sumei = arr[e] + arr[i];
-					int abs1 = Math.abs(sumsj - sumei);
-					if(sumsj < sumei)
-						s = plus2(s+1,i,j, e);
-					else e = plus2(e+1, i,j,s);
-					min = Math.min(min, abs1);
-				}
-			}
+			pair p1 = list.get(i-1);
+			pair p2 = list.get(i);
+			if(p1.i == p2.i || p1.i == p2.j || p1.j == p2.i || p1.j == p2.j)
+				continue;
+			min = Math.min(min, Math.abs(p1.sum-p2.sum));
 		}
 		System.out.print(min);
-	}
-	public static int plus2(int s, int n1, int n2, int n3) {
-		while(s == n1 || s == n2 || s == n3)++s;
-		return s;
-	}
-	public static int plus1(int s, int n1, int n2) {
-		while(s == n1 || s == n2)++s;
-		return s;
 	}
 }
