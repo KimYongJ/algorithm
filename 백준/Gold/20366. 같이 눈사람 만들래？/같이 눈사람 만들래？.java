027f13ba390e7a17915d410322e5a99602985bde
YongJu Kim <kyjdummy@gmail.com>
2025-01-22 16:26:27 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 172 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -2,25 +2,20 @@
 //https://www.acmicpc.net/problem/20366
 //2초 / 1024MB
 //요약 : 배열에서 2개씩 2번골라서 각각 합친 값의 차이가 최소인 값 출력 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());// 4<=600
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int arr[] = new int[N];
+		int N		= read();		// 4<=600
+		int arr[]	= new int[N];
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());// 지름(1<=십억)
+			arr[i] = read();		// 지름(1<=십억)
 		
 		Arrays.sort(arr);
 		
 		int min = Integer.MAX_VALUE;
+		
 		for(int l=0; l<N; l++)
-			for(int r=l+3; r<N && min != 0; r++)
+			for(int r=l+3; r<N; r++)
 			{
 				int sum1 = arr[l] + arr[r];
 				int s = l+1;
@@ -35,9 +30,20 @@ class Main{
 						--e;
 					else
 						++s;
+					
+					if(min == 0)
+					{
+						System.out.print(0);
+						return;
+					}
 				}
 			}
 		
 		System.out.print(min);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
