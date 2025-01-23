ec6f02efcad24ee20729af658b6bb9ad2be171f4
YongJu Kim <kyjdummy@gmail.com>
2025-01-23 10:17:20 +0900

    [Gold III] Title: 커넥티드 카 실험, Time: 1044 ms, Memory: 238560 KB -BaekjoonHub

---
+++ 백준/Gold/25395. 커넥티드 카 실험/커넥티드 카 실험.java
@@ -0,0 +1,66 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/25395
+//2초 / 1024MB
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N		= Integer.parseInt(st.nextToken());	// 자동차수(1<=백만)
+		int S		= Integer.parseInt(st.nextToken());	// 시작노드(1<=N)
+		int pos[]	= new int[N+1];		// 현재 위치
+		int fuel[]	= new int[N+1];		// 갈 수 있는 거리
+		boolean v[]	= new boolean[N+1];	// 해당 위치 방문 유무
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++)pos[i]  = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++)fuel[i] = Integer.parseInt(st.nextToken());
+		
+		v[S] = true;
+		q.add(S);
+		while(!q.isEmpty())
+		{
+			int now = q.poll();
+			// 왼쪽으로 가며 탐색
+			for(int i=now-1, f = fuel[now]; i>=1; i--)
+			{
+				int needFuel = pos[i+1] - pos[i];	// 다음 위치까지 드는 연료
+				if(needFuel <= f && !v[i])
+				{
+					q.add(i);
+					v[i] = true;
+				}
+
+				f -= needFuel;
+				
+				if(fuel[i] >= f)
+					break;
+			}
+			for(int i=now+1, f = fuel[now]; i<=N; i++)
+			{
+				int needFuel = pos[i] - pos[i-1];
+				if(needFuel <= f && !v[i])
+				{
+					q.add(i);
+					v[i] = true;
+				}
+				
+				f -= needFuel;
+				
+				if(fuel[i] >= f)
+					break;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=N; i++)
+			if(v[i])
+				sb.append(i).append(' ');
+		System.out.print(sb);
+	}
+}
