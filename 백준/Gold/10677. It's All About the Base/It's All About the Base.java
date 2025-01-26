7c4296bfbca400a5d9887430ad89f37ae8984a6a
YongJu Kim <kyjdummy@gmail.com>
2025-01-26 17:21:03 +0900

    [Gold V] Title: It's All About the Base, Time: 244 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Gold/10677. It's All About the Base/It's All About the Base.java
+++ 백준/Gold/10677. It's All About the Base/It's All About the Base.java
@@ -3,7 +3,6 @@
 //1초 / 256MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,13 +11,16 @@ class Main{
 		while(T-->0)
 		{
 			// 가능한 진수 : 10~15,000진수
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int num1 = Integer.parseInt(st.nextToken());	// X진수로 쓰인 숫자 N
-			int num2 = Integer.parseInt(st.nextToken());	// Y진수로 쓰인 숫자 N
+			String str = br.readLine();
+			int A[] = new int[3];// X진수로 쓰인 숫자 N
+			int B[] = new int[3];// Y진수로 쓰인 숫자 N
+			
+			for(int i=0; i<3; i++)A[i] = str.charAt(i)-'0';
+			for(int i=0; i<3; i++)B[i] = str.charAt(i+4)-'0';
 			
 			for(int i=10; i<=15_000; i++)
 			{
-				int dec = decimal(num1, i);	// num1을 모든 진법을 사용해 10진수로 변경
+				int dec = decimal(A, i);	// num1을 모든 진법을 사용해 10진수로 변경
 				// dec을, 이진탐색으로 num2로 만들 수 있는지 탐색
 				
 				int s	= 10;
@@ -27,8 +29,9 @@ class Main{
 				while(s <= e)
 				{
 					int mid = (s + e) >> 1;
-					int dec2= decimal(num2, mid);
-					if(dec == dec2) {
+					int dec2= decimal(B, mid);
+					if(dec == dec2)
+					{
 						res = mid;
 						break;
 					}
@@ -47,16 +50,7 @@ class Main{
 		}
 		System.out.print(sb);
 	}
-	public static int decimal(int num, int d) {
-		int res = 0;
-		int mul = 1;
-		while(num != 0)
-		{
-			res += (num%10) * mul;
-			num/=10;
-			mul *= d;
-		}
-		
-		return res;
+	public static int decimal(int[] num, int d) {
+		return d*d*num[0] + d*num[1] + num[2];
 	}
 }
