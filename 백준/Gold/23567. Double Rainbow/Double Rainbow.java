354dd1ffe76dfbab0f35b52c135dbce64f0e5fb3
YongJu Kim <kyjdummy@gmail.com>
2025-01-27 11:58:10 +0900

    [Gold V] Title: Double Rainbow, Time: 136 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Gold/23567. Double Rainbow/Double Rainbow.java
+++ 백준/Gold/23567. Double Rainbow/Double Rainbow.java
@@ -2,48 +2,36 @@
 //https://www.acmicpc.net/problem/23567
 //1초 / 1024MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	//점의 수 1<=만
-		int K		= Integer.parseInt(st.nextToken());	//점에 입력될 숫자의 수 1<=N
+		int N		= read();	//점의 수 1<=만
+		int K		= read();	//점에 입력될 숫자의 수 1<=N
 		int arr[]	= new int[N];
 		int cnt[]	= new int[K+1];
 		int origin[]= new int[K+1];
 		
 		for(int i=0; i<N; i++)
-			++origin[arr[i] = Integer.parseInt(br.readLine())];
+			++origin[arr[i] = read()];
 		
-		int s = 0;
-		int e = 0;
-		int cntK = 0;
-		int minLen = Integer.MAX_VALUE;
+		int s		= 0;
+		int e		= -1;
+		int cntK	= 0;
+		int minLen	= Integer.MAX_VALUE;
 		
-		while(e < N)
+		while(++e < N)
 		{
 			if(cnt[arr[e]]++ == 0)
 				++cntK;
 			
-			if(K == cntK)
+			while(K == cntK)
 			{
-				while(K == cntK)
-				{
-					if(validate(origin, cnt))
-						minLen = Math.min(minLen, e - s + 1);
-					
-					if(--cnt[arr[s++]] == 0)
-						--cntK;
-				}
+				if(validate(origin, cnt))
+					minLen = Math.min(minLen, e - s + 1);
+				
+				if(--cnt[arr[s++]] == 0)
+					--cntK;
 			}
-			
-			++e;
 		}
-		
 		
 		if(minLen == Integer.MAX_VALUE)
 			minLen = 0;
@@ -51,10 +39,14 @@ class Main{
 		System.out.print(minLen);
 	}
 	public static boolean validate(int o[], int c[]) {
-		for(int i=1; i<o.length; i++) {
-			if(o[i] - c[i] == 0)
+		for(int i=1; i<o.length; i++)
+			if(o[i] == c[i])
 				return false;
-		}
 		return true;
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
