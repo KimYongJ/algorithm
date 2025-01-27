64f1222a7cf54419a4b31ddb25be9f93978c61c8
YongJu Kim <kyjdummy@gmail.com>
2025-01-27 12:19:21 +0900

    [Gold V] Title: Paired Up, Time: 300 ms, Memory: 22064 KB -BaekjoonHub

--- 백준/Gold/14527. Paired Up/Paired Up.java
+++ 백준/Gold/14527. Paired Up/Paired Up.java
@@ -2,31 +2,26 @@
 //https://www.acmicpc.net/problem/14527
 //2초 / 512MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+
 class Cow{
 	int cnt, time;
 	Cow(int c, int t){
 		cnt=c; time=t;
 	}
 }
+
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		Cow cow[]	= new Cow[N];
 		
 		for(int i=0; i<N; i++)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int c	= Integer.parseInt(st.nextToken());
-			int t	= Integer.parseInt(st.nextToken());
-			cow[i]	= new Cow(c,t);
-		}
+			cow[i]	= new Cow(read(),read());
+		
 		// 시간기준 오름 차순
 		Arrays.sort(cow, (a,b)->a.time-b.time);
+		
 		int s = 0;
 		int e = N-1;
 		int m = 0;
@@ -43,5 +38,10 @@ class Main{
 			if(cow[e].cnt <= 0)--e;
 		}
 		System.out.print(m);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
