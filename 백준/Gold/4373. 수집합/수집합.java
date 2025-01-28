19c20b1f849d9c05df09b3ecb187cd99a3a13d7b
YongJu Kim <kyjdummy@gmail.com>
2025-01-28 16:05:00 +0900

    [Gold I] Title: 수집합, Time: 824 ms, Memory: 68856 KB -BaekjoonHub

--- 백준/Gold/4373. 수집합/수집합.java
+++ 백준/Gold/4373. 수집합/수집합.java
@@ -2,15 +2,16 @@
 //https://www.acmicpc.net/problem/4373
 //1초 / 128MB
 //두 배열에서 차이가 최소인 것을 찾는 문제 응용(유사문제 : 14746, 17095)
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 class Node implements Comparable<Node>{
 	int sum, max, min;
-	Node(int s, int a, int b){
-		sum=s;
-		max=a;
-		min=b;
+	Node(int s, int mx, int mn){
+		sum=s; max=mx; min=mn;
 	}
 	@Override
 	public int compareTo(Node o) {
@@ -19,70 +20,52 @@ class Node implements Comparable<Node>{
 }
 class Main{
 	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true)
 		{
-			int N = read();
+			int N = Integer.parseInt(br.readLine());
 			if(N == 0)
 				break;
-			
 			int arr[] = new int[N];
-			
 			for(int i=0; i<N; i++)
-				arr[i] = read();
+				arr[i] = Integer.parseInt(br.readLine());
 			
 			Arrays.sort(arr);
 			
 			ArrayList<Node> sum = new ArrayList<>();
-			ArrayList<Node> dif = new ArrayList<>();
-			
+			ArrayList<Node> diff= new ArrayList<>();
 			for(int i=0; i<N-1; i++)
 				for(int j=i+1; j<N; j++)
 				{
 					sum.add(new Node(arr[i] + arr[j], arr[j], arr[i]));
-					dif.add(new Node(arr[j] - arr[i], arr[j], arr[i]));
+					diff.add(new Node(arr[j] - arr[i], arr[j],arr[i]));
 				}
 			
 			Collections.sort(sum);
-			Collections.sort(dif);
+			Collections.sort(diff);
 			
-			int max = Integer.MIN_VALUE;
-
 			int idx1 = 0;
 			int idx2 = 0;
-			
-			while(idx1<sum.size() && idx2<dif.size())
-			{
-				Node node1 = sum.get(idx1);
-				Node node2 = dif.get(idx2);
-				if(node1.sum < node2.sum)
+			int max = Integer.MIN_VALUE;
+			while(idx1<sum.size() && idx2<diff.size()) {
+				Node s = sum.get(idx1);
+				Node d = diff.get(idx2);
+				if(s.sum <= d.sum)
 					++idx1;
-				else if(node1.sum > node2.sum)
+				else if(s.sum > d.sum)
 					++idx2;
-				else
-				{
-					if(node1.max != node2.max && node1.max != node2.min && 
-							node1.min != node2.max && node1.min != node2.min)
-						max = Math.max(max, node2.max);
-                    
-					++idx1;
-				}
 				
+				if(s.sum == d.sum && s.max != d.max && s.max != d.min && s.min != d.max && s.min != d.min)
+					max = Math.max(max, d.max);
 			}
 			
 			if(max == Integer.MIN_VALUE)
 				sb.append("no solution").append('\n');
 			else
 				sb.append(max).append('\n');
+			
 		}
 		System.out.print(sb);
 	}
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        boolean m = n == 13;
-        if (m)n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48) {
-        n = (n << 3) + (n << 1) + (c & 15);}
-        return m ? ~n + 1 : n;
-    }
 }
