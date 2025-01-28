5fc13c73c0ed3eb3e91e1f71d86a5ddc93e33ed3
YongJu Kim <kyjdummy@gmail.com>
2025-01-28 15:25:55 +0900

    [Gold I] Title: 수집합, Time: 848 ms, Memory: 69288 KB -BaekjoonHub

--- 백준/Gold/4373. 수집합/수집합.java
+++ 백준/Gold/4373. 수집합/수집합.java
@@ -1,34 +1,35 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/4373
 //1초 / 128MB
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+//두 배열에서 차이가 최소인 것을 찾는 문제 응용(유사문제 : 14746, 17095)
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-class Node{
+class Node implements Comparable<Node>{
 	int sum, max, min;
 	Node(int s, int a, int b){
 		sum=s;
 		max=a;
 		min=b;
 	}
+	@Override
+	public int compareTo(Node o) {
+		return sum - o.sum;
+	}
 }
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true)
 		{
-			int N = Integer.parseInt(br.readLine().trim());
+			int N = read();
 			if(N == 0)
 				break;
 			
 			int arr[] = new int[N];
 			
 			for(int i=0; i<N; i++)
-				arr[i] = Integer.parseInt(br.readLine().trim());
+				arr[i] = read();
 			
 			Arrays.sort(arr);
 			
@@ -42,8 +43,8 @@ class Main{
 					dif.add(new Node(arr[j] - arr[i], arr[j], arr[i]));
 				}
 			
-			Collections.sort(sum, (a,b)->a.sum - b.sum);
-			Collections.sort(dif, (a,b)->a.sum - b.sum);
+			Collections.sort(sum);
+			Collections.sort(dif);
 			
 			int max = Integer.MIN_VALUE;
 
@@ -62,21 +63,26 @@ class Main{
 				{
 					if(node1.max != node2.max && node1.max != node2.min && 
 							node1.min != node2.max && node1.min != node2.min)
-					{
 						max = Math.max(max, node2.max);
-					}
-					++idx2;
+                    
+					++idx1;
 				}
 				
 			}
 			
 			if(max == Integer.MIN_VALUE)
-				sb.append("no solution");
+				sb.append("no solution").append('\n');
 			else
-				sb.append(max);
-			
-			sb.append('\n');
+				sb.append(max).append('\n');
 		}
 		System.out.print(sb);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
