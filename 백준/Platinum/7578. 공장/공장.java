d5d0d57985acb1574e25eb4010539d4b63ba60a9
YongJu Kim <kyjdummy@gmail.com>
2025-01-29 12:16:33 +0900

    [Platinum V] Title: 공장, Time: 388 ms, Memory: 28292 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.java
+++ 백준/Platinum/7578. 공장/공장.java
@@ -2,30 +2,22 @@
 //https://www.acmicpc.net/problem/7578
 //1초 / 256MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N;
 	static int[] arr, tree, counting;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N			= Integer.parseInt(br.readLine());	// 1<=오십만
+		N			= read();				// 1<=오십만
 		tree		= new int[N<<2];
-		counting	= new int[1_000_001];				// 입력수 0<=백만
+		counting	= new int[1_000_001];	// 입력수 0<=백만
 		arr			= new int[N+1];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			counting[Integer.parseInt(st.nextToken())] = i;
-		
-		st = new StringTokenizer(br.readLine());
+			counting[read()] = i;
 		
 		for(int i=1; i<=N; i++)
-			arr[i] = counting[Integer.parseInt(st.nextToken())];
+			arr[i] = counting[read()];
 		
 		long res = 0;
 		for(int i=N; i>0; i--)
@@ -33,6 +25,7 @@ class Main{
 			res += query(1, 1, N, 1, arr[i] - 1);
 			update(1, 1, N, arr[i]);
 		}
+		
 		System.out.print(res);
 	}
 	public static void update(int treeNode, int s, int e, int idx) {
@@ -59,5 +52,10 @@ class Main{
 		int mid = (s + e) >> 1;
 		return query(nextNode, s, mid, left, right) +
 				query(nextNode | 1, mid + 1, e, left, right);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
