0267832f945e434750f74d3c3c6b559bfc2f1280
YongJu Kim <kyjdummy@gmail.com>
2025-01-30 19:13:43 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 132 ms, Memory: 15468 KB -BaekjoonHub

--- 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.java
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.java
@@ -1,23 +1,19 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12846
 //1초 / 512MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		long arr[]	= new long[N+1];
 		long max	= 0;
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		int stack[]	 = new int[N+1];
 		int stackIdx = -1;
+		
 		for(int i=0; i<=N; i++)
 		{
 			while(0<=stackIdx && arr[i] < arr[stack[stackIdx]])
@@ -32,5 +28,10 @@ class Main{
 		}
 		
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
