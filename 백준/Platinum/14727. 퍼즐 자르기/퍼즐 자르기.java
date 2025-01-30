0fca05889dcdb96b8a1d61eec79c49ebb4018fe2
YongJu Kim <kyjdummy@gmail.com>
2025-01-30 19:26:14 +0900

    [Platinum V] Title: 퍼즐 자르기, Time: 136 ms, Memory: 15548 KB -BaekjoonHub

--- 백준/Platinum/14727. 퍼즐 자르기/퍼즐 자르기.java
+++ 백준/Platinum/14727. 퍼즐 자르기/퍼즐 자르기.java
@@ -1,17 +1,14 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14727
 //2초 / 256MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	// 1<=십만
+		int N		= read();	// 1<=십만
 		long arr[]	= new long[N+1];
 		long max	= 0;
 		for(int i=0; i<N; i++)
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = read();
 		
 		int stack[] = new int[N];
 		int stIdx	= -1;
@@ -28,5 +25,10 @@ class Main{
 			stack[++stIdx] = i;
 		}
 		System.out.print(max);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
