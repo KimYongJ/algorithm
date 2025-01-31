323f94d8d2cba8aaa2085a79d80c3dac73d0ff8c
YongJu Kim <kyjdummy@gmail.com>
2025-01-31 16:02:20 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 432 ms, Memory: 88116 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.java
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.java
@@ -2,96 +2,89 @@
 //https://www.acmicpc.net/problem/10999
 //2초 / 256MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
+	static int N, M, K;
 	static long[] arr, tree, lazy;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 수 개수 1<=백만
-		int M = Integer.parseInt(st.nextToken());	// 변경 일어나는 개수 1<=만
-		int K = Integer.parseInt(st.nextToken());	// 구간합 개수 1<=만
-		int S = M + K;
+		StringBuilder sb = new StringBuilder();
+		N	= readInt();	// 1<=백만
+		M	= readInt();	// 1<=만
+		K	= readInt();	// 1<=만
+		
 		arr = new long[N+1];
 		tree= new long[N<<2];
-		lazy= new long[N<<2];
+		lazy= new long[N<<2];		// 지연 연산을 위한 배열
 		
 		for(int i=1; i<=N; i++)
-			arr[i] = Long.parseLong(br.readLine());	// long MAX, MIN
+			arr[i] = readLong();	// long 최소 최대범위
 		
 		init(1, 1, N);
 		
-		StringBuilder sb = new StringBuilder();
-		while(S-->0)
+		int T = M + K;
+		while(T-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int t = Integer.parseInt(st.nextToken());
-			if(t == 1)
-			{
-				int l = Integer.parseInt(st.nextToken());
-				int r = Integer.parseInt(st.nextToken());
-				long v = Long.parseLong(st.nextToken());
-				
-				update(1, 1, N, l, r, v);
-			}
-			else	// 해당 구간의 구간 합
-			{
-				int l = Integer.parseInt(st.nextToken());
-				int r = Integer.parseInt(st.nextToken());
-				sb.append(query(1, 1, N, l, r)).append('\n');
-			}
+			int f = readInt();
+			int b = readInt();
+			int c = readInt();
+			
+			if(f == 1)	// b,c에 d를 더한다.
+				update(1, 1, N, b, c, readLong());
+			
+			else		// b, c 수의 합
+				sb.append(query(1, 1, N, b, c)).append('\n');
 		}
 		
 		System.out.print(sb);
 	}
-	public static void propagate(int treeNode, int s, int e) {
-		if(lazy[treeNode] != 0) {
-			tree[treeNode] += (e - s + 1) * lazy[treeNode];
-			if(s != e)
-			{
-				lazy[treeNode << 1] += lazy[treeNode];
-				lazy[treeNode << 1 | 1] += lazy[treeNode];
-			}
-			lazy[treeNode] = 0;
-		}
-	}
-	public static void update(int treeNode, int s, int e, int left, int right, long value) {
-		
+	public static long query(int treeNode, int s, int e, int left, int right) {
 		propagate(treeNode, s, e);
 		
-		if(e<left || right < s)
-			return;
-		if(left<=s && e<=right)
-		{
-			lazy[treeNode] += value;
-			propagate(treeNode, s, e);
-			return;
-		}
-		
-		int nextNode = treeNode << 1;
-		int mid = (s + e) >> 1;
-		update(nextNode, s, mid, left, right, value);
-		update(nextNode | 1, mid + 1, e, left, right, value);
-		
-		tree[treeNode] = tree[nextNode] + tree[nextNode | 1];
-	}
-	public static long query(int treeNode, int s, int e, int left, int right)
-	{
-		propagate(treeNode, s, e);
-		
-		if(e <left || right < s)
+		if(e<left || right <s)
 			return 0;
 		if(left<=s && e<=right)
 			return tree[treeNode];
 		
-		int nextNode = treeNode << 1;
-		int mid = (s + e) >> 1;
-		return query(nextNode, s, mid, left, right) + query(nextNode | 1, mid + 1, e, left, right);
+		int nextNode= treeNode << 1;
+		int mid		= (s + e) >> 1;
+		
+		return query(nextNode, s, mid, left, right) 
+				+ query(nextNode | 1, mid + 1, e, left, right);
+	}
+	public static void update(int treeNode, int s, int e, int left, int right,long diff) {
+		propagate(treeNode, s, e);
+		
+		if(e < left || right < s)
+			return;
+		if(left<= s && e<=right)
+		{
+			lazy[treeNode] = diff;
+			propagate(treeNode, s, e);
+			return;
+		}
+		
+		int nextNode= treeNode << 1;
+		int mid		= (s + e) >> 1;
+		
+		update(nextNode, s, mid, left, right, diff);
+		update(nextNode | 1, mid + 1, e, left, right, diff);
+		
+		tree[treeNode] = tree[nextNode] + tree[nextNode | 1];
+	}
+	public static void propagate(int treeNode, int s, int e) {
+		if(lazy[treeNode] != 0)
+		{
+			// 현재 treeNode에 값 갱신
+			tree[treeNode] += (e - s + 1) * lazy[treeNode];
+			// leaf노드가 아니면, 다음 lazy에 전파 갱싱
+			if(s != e)
+			{
+				lazy[treeNode << 1]		+= lazy[treeNode];
+				lazy[treeNode << 1 | 1] += lazy[treeNode];
+			}
+			lazy[treeNode] = 0;
+		}
 	}
 	public static long init(int treeNode, int s, int e) {
 		if(s == e)
@@ -99,8 +92,20 @@ class Main{
 		
 		int nextNode= treeNode << 1;
 		int mid		= (s + e) >> 1;
-		
 		return tree[treeNode] = init(nextNode, s, mid) 
-								+ init(nextNode | 1, mid + 1, e); 
+								+ init(nextNode | 1, mid + 1, e);
 	}
+	static int readInt() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+    static long readLong() throws Exception {
+        long c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
