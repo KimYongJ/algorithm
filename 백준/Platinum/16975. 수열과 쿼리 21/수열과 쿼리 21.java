fa3b9590c852fd21cf43db934b86f919ccd45c3d
YongJu Kim <kyjdummy@gmail.com>
2025-01-31 16:43:05 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 380 ms, Memory: 28392 KB -BaekjoonHub

--- 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.java
@@ -2,46 +2,38 @@
 //https://www.acmicpc.net/problem/16975
 //2초 / 512MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N;
 	static long[] arr, tree, lazy;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringBuilder sb = new StringBuilder();
-		N	= Integer.parseInt(br.readLine());	// 1<=십만
+		N	= read();	// 1<=십만
 		arr = new long[N+1];
 		tree= new long[N<<2];
 		lazy= new long[N<<2];
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());	// 1<=백만
+			arr[i] = read();// 1<=백만
 		
 		init(1, 1, N);
 		
-		int T = Integer.parseInt(br.readLine());	// 1<=십만
+		int T = read();	// 1<=십만
+		
 		while(T-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int f = Integer.parseInt(st.nextToken());
-			if(f == 1)
+			if(read() == 1)
 			{
-				int l = Integer.parseInt(st.nextToken());	//1<=N
-				int r = Integer.parseInt(st.nextToken());	//1<=N
-				int k = Integer.parseInt(st.nextToken());	//|백만|
-				
-				update(1, 1, N, l, r, k);
+				//1<=N
+				//1<=N
+				//|백만|
+				update(1, 1, N, read(), read(), read());
 			}
 			else
 			{
-				int t = Integer.parseInt(st.nextToken());
-				sb.append(query(1, 1, N, t, t)).append('\n');
+				sb.append(query(1, 1, N, read())).append('\n');
 			}
 		}
 		System.out.print(sb);
@@ -52,10 +44,11 @@ class Main{
 			tree[treeNode] = arr[s];
 			return;
 		}
-		int nextNode= treeNode << 1;
+		
 		int mid		= (s + e) >> 1;
-		init(nextNode, s, mid);
-		init(nextNode | 1, mid + 1, e);
+		
+		init(treeNode << 1, s, mid);
+		init(treeNode << 1 | 1, mid + 1, e);
 	}
 	public static void propagate(int treeNode, int s, int e) {
 		if(lazy[treeNode] != 0)
@@ -74,31 +67,38 @@ class Main{
 		
 		if(e < left || right < s)
 			return;
+		
 		if(left <= s && e <= right)
 		{
-			lazy[treeNode] = diff;
+			lazy[treeNode] += diff;
 			propagate(treeNode, s, e);
 			return;
 		}
-		
-		int nextNode= treeNode << 1;
+
 		int mid		= (s + e) >> 1;
 		
-		update(nextNode, s, mid, left, right, diff);
-		update(nextNode | 1, mid + 1, e, left, right, diff);
+		update(treeNode << 1, s, mid, left, right, diff);
+		update(treeNode << 1 | 1, mid + 1, e, left, right, diff);
 	}
-	public static long query(int treeNode, int s, int e, int left, int right) {
+	public static long query(int treeNode, int s, int e, int idx) {
 		propagate(treeNode, s, e);
 		
-		if(e < left || right < s)
+		if(e < idx || idx < s)
 			return 0;
-		if(left <= s && e <= right)
+		if(s==e)
 			return tree[treeNode];
-		
-		int nextNode= treeNode << 1;
+
 		int mid 	= (s + e) >> 1;
 		
-		return query(nextNode, s, mid, left, right) 
-				+ query(nextNode | 1, mid + 1, e, left, right);
+		return query(treeNode << 1, s, mid, idx) 
+				+ query(treeNode << 1 | 1, mid + 1, e, idx);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
