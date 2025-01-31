7ee8828abe25d210494bc53e86767808cdee7887
YongJu Kim <kyjdummy@gmail.com>
2025-01-31 22:47:56 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
@@ -2,16 +2,10 @@
 //https://www.acmicpc.net/problem/19947
 //1초 / 512mb
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int H		= Integer.parseInt(st.nextToken());	// 초기비용 만<=십만
-		int Y		= Integer.parseInt(st.nextToken());	// 투자 기간 0<=10
+		int H		= read();	// 초기비용 만<=십만
+		int Y		= read();	// 투자 기간 0<=10
 		int arr[]	= new int[11];
 		arr[0]		= H;
 		
@@ -26,7 +20,11 @@ class Main{
 				arr[i] = Math.max(arr[i], (int)(arr[i-5] * 1.35));
 		}
 		
-		
 		System.out.print(arr[Y]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
