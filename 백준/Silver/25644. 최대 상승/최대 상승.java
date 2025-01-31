ca581782682ca8451c1ee5dbbde59601989f1552
YongJu Kim <kyjdummy@gmail.com>
2025-01-31 18:48:35 +0900

    [Silver V] Title: 최대 상승, Time: 180 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.java
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -2,25 +2,24 @@
 //https://www.acmicpc.net/problem/25644
 //1초 / 512mb
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int min		= 1<<30;
 		int result	= 0;
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<N; i++)
 		{
-			int n = Integer.parseInt(st.nextToken());
-			result = Math.max(result, n - min);
-			min = Math.min(min, n);
+			int n	= read();
+			result	= Math.max(result, n - min);
+			min		= Math.min(min, n);
 		}
 
 		System.out.print(result);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
