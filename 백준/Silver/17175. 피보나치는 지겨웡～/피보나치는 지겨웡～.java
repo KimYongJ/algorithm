e2f7b7fca31d0b3934c67c484b27842dd5d45691
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 19:50:52 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.java
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.java
@@ -1,16 +1,12 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17175
 //1초 / 512mb
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());	// 1<=50
-		int arr[] = new int[N+2];
-		arr[0]= arr[1] = 1;
+		int N		= read();	// 1<=50
+		int arr[]	= new int[N+2];
+		
+		arr[0] = arr[1] = 1;
 		
 		for(int i=2; i<=N; i++)
 		{
@@ -20,5 +16,10 @@ class Main{
 		}
 		
 		System.out.print(arr[N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
