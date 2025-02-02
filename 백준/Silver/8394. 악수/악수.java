70a9fe124f6f7a682d0638f3b1cbe76d01cf367c
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 16:50:11 +0900

    [Silver III] Title: 악수, Time: 168 ms, Memory: 53104 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.java
@@ -0,0 +1,21 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/8394
+//1초 / 256mb
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N	= Integer.parseInt(br.readLine());	// 1<=천만
+		int dp[]= new int[N+4];
+		
+		dp[2] = 2;
+		dp[3] = 3;
+		for(int i=4; i<=N; i++)
+			dp[i] = (dp[i-1] + dp[i-2]) % 10;
+		
+		System.out.print(dp[N]);
+	}
+}
