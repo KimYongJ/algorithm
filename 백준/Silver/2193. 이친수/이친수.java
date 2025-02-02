d00c3f05063a34bbdfd4290bb6e611ca4c7ea8d2
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 22:47:10 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,19 +2,21 @@
 //https://www.acmicpc.net/problem/2193
 //2초 / 128mb
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());//1<=90
+		int N		= read();//1<=90
 		long dp[]	= new long[N+3];
 		
-		dp[1] = dp[2] = 1;
-		for(int i=3; i<=N; i++)
+		dp[1] = 1;
+		
+		for(int i=2; i<=N; i++)
 			dp[i] = dp[i-1] + dp[i-2];
 		
 		System.out.print(dp[N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
