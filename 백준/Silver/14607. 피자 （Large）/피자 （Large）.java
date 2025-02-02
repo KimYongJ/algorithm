06101ed96869e9f4c931150853a6099760a8abbb
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 21:47:06 +0900

    [Silver III] Title: 피자 (Large), Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/14607. 피자 （Large）/피자 （Large）.java
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.java
@@ -1,13 +1,31 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14607
 //1초 / 512mb
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.HashMap;
 class Main{
+	
+	static HashMap<Long , Long> map = new HashMap<>();
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long N = Integer.parseInt(br.readLine());	// 1<=십억
+		System.out.print(DFS(read()));	// 1<=십억
+	}
+	public static long DFS(long n) {
+		if(n == 1)
+			return 0;
 		
-		System.out.print((N*N - N) >> 1);
+		if(map.containsKey(n))
+			return map.get(n);
+		
+		long left	= n/2;
+		long right	= n - left;
+		long res	= left * right + DFS(left) + DFS(right); 
+		
+		map.put(n, res);
+		return res;
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
