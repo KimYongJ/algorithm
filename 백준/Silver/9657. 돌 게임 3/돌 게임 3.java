51ce32b13421419d23a3cc217640b78ba109ae86
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 16:04:51 +0900

    [Silver III] Title: 돌 게임 3, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -11,7 +11,8 @@ class Main{
 		int N = Integer.parseInt(br.readLine());
 		int arr[] = new int[N+5];
 		arr[1] = arr[3] = arr[4] = 1;
-		
+		// 게임이론 : 상대가 이기는 수를 고르지 않는다. 상대가 이길 수 있다면 내가 진것임
+		// 현재에 올 수 있는 모든 위치의 값이 상근이면 창영 이가 이긴다.
 		for(int i=5; i<=N; i++)
 			if(arr[i-1] + arr[i-3] + arr[i-4] != 3)
 				arr[i] = 1;
