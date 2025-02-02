49ccafff3ec24077f1cfecbf3515b5691787129f
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 20:02:29 +0900

    [Silver III] Title: 피보나치, Time: 336 ms, Memory: 35376 KB -BaekjoonHub

--- 백준/Silver/9711. 피보나치/피보나치.java
+++ 백준/Silver/9711. 피보나치/피보나치.java
@@ -1,15 +1,10 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/9711
 //1초 / 128mb
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BigInteger dp[] = new BigInteger[10_001];
 		dp[1] = BigInteger.ONE;
 		dp[2] = BigInteger.ONE;
@@ -17,18 +12,27 @@ class Main{
 		for(int i=3; i<10_001; i++)
 			dp[i] = dp[i-1].add(dp[i-2]);
 		
-		
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		for(int i=1; i<=T; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int P = Integer.parseInt(st.nextToken());
-			BigInteger Q = new BigInteger(st.nextToken());
+			int P = read();
+			BigInteger Q = new BigInteger(readString());
 			
 			sb.append("Case #").append(i).append(": ")
 				.append(dp[P].mod(Q)).append('\n');
 		}
 		System.out.print(sb);
 	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+    public static String readString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = System.in.read()) > 13) {sb.append((char) c);}
+        return sb.toString();
+    }
 }
