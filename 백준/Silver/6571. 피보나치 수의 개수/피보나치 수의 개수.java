bf4d0dbd65979d4bc86dcac61b7a3f21b218b87b
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 15:29:05 +0900

    [Silver III] Title: 피보나치 수의 개수, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.java
+++ 백준/Silver/6571. 피보나치 수의 개수/피보나치 수의 개수.java
@@ -1,73 +1,84 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/6571
+//1초 / 256mb
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        BigInteger upperLimit = BigInteger.TEN.pow(100);
-        
-        // 문제 의도에 맞는 피보나치 수열: 1, 2, 3, 5, ...
-        ArrayList<BigInteger> list = new ArrayList<>();
-        BigInteger f1 = BigInteger.ONE;        // 첫 항: 1
-        BigInteger f2 = BigInteger.valueOf(2);   // 두 번째 항: 2
-        list.add(f1);
-        list.add(f2);
-        
-        while (true) {
-            BigInteger next = f1.add(f2);
-            if (next.compareTo(upperLimit) > 0) break;
-            list.add(next);
-            f1 = f2;
-            f2 = next;
-        }
-        
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            BigInteger lower = new BigInteger(st.nextToken());
-            BigInteger upper = new BigInteger(st.nextToken());
-            if (lower.add(upper).equals(BigInteger.ZERO))
-                break;
-            
-            int idx1 = binarySearchLowerBound(list, lower);
-            int idx2 = binarySearchUpperBound(list, upper);
-            
-            int count = (idx1 > idx2 ? 0 : idx2 - idx1 + 1);
-            sb.append(count).append('\n');
-        }
-        System.out.print(sb);
-    }
-    
-    // 리스트에서 target 이상의 첫 번째 원소의 인덱스를 찾음
-    public static int binarySearchLowerBound(ArrayList<BigInteger> list, BigInteger target) {
-        int s = 0, e = list.size() - 1, res = list.size();
-        while (s <= e) {
-            int mid = (s + e) >> 1;
-            if (list.get(mid).compareTo(target) >= 0) {
-                res = mid;
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
-        }
-        return res;
-    }
-    
-    // 리스트에서 target 이하의 마지막 원소의 인덱스를 찾음
-    public static int binarySearchUpperBound(ArrayList<BigInteger> list, BigInteger target) {
-        int s = 0, e = list.size() - 1, res = -1;
-        while (s <= e) {
-            int mid = (s + e) >> 1;
-            if (list.get(mid).compareTo(target) <= 0) {
-                res = mid;
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return res;
-    }
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		BigInteger upperBound	= BigInteger.TEN.pow(100);
+		
+		ArrayList<BigInteger> list = new ArrayList<>();
+		BigInteger f1 = BigInteger.ONE;
+		BigInteger f2 = BigInteger.TWO;
+		list.add(f1);
+		list.add(f2);
+		
+		while(true)
+		{
+			BigInteger next = f1.add(f2);
+			if(next.compareTo(upperBound) > 0)
+				break;
+			list.add(next);
+			f1 = f2;
+			f2 = next;
+		}
+		
+		while(true)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			BigInteger lower = new BigInteger(st.nextToken());
+			BigInteger upper = new BigInteger(st.nextToken());
+			if(lower.add(upper).equals(BigInteger.ZERO))
+				break;
+			
+			int idx1 = binarySearch1(list, lower);
+			int idx2 = binarySearch2(list, upper);
+			if(idx1 < 0 || idx2 < 0)
+				sb.append(0).append('\n');
+			else
+				sb.append(idx1 > idx2 ? 0 : idx2 - idx1 + 1).append('\n');
+		}
+		System.out.print(sb);
+	}
+	public static int binarySearch1(ArrayList<BigInteger> list, BigInteger target) {
+		int s = 0;
+		int e = list.size() - 1;
+		int res = -1;
+		while(s <= e)
+		{
+			int mid = (s + e) >> 1;
+			BigInteger now = list.get(mid);
+			if(target.compareTo(now) <= 0) {
+				res = mid;
+				e = mid - 1;
+			}
+			else
+				s = mid + 1;
+		}
+		return res;
+	}
+	public static int binarySearch2(ArrayList<BigInteger> list, BigInteger target) {
+		int s = 0;
+		int e = list.size() - 1;
+		int res = -1;
+		while(s <= e)
+		{
+			int mid = (s + e) >> 1;
+			BigInteger now = list.get(mid);
+			if(now.compareTo(target) <= 0) {
+				res = mid;
+				s = mid + 1;
+			}
+			else
+				e = mid - 1;
+		}
+		return res;
+	}
 }
