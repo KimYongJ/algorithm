ddd8a3b9d467e3b980b3ab2e1b17595cb0c15007
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 16:29:57 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 120 ms, Memory: 21908 KB -BaekjoonHub

---
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
@@ -0,0 +1,33 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/1788
+//2초 / 128mb
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int len = Math.abs(N);
+		long dp[] = new long[len+2];	// 양수
+		dp[1] = 1;
+		
+		for(int i=2; i<=len; i++)
+		{
+			dp[i] = dp[i-1] + dp[i-2];
+			
+			dp[i] %= 1_000_000_000;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		if(N == 0)
+			sb.append(0);
+		else
+			sb.append(len % 2 == 0 && N < 0 ? -1 : 1);
+		
+		sb.append('\n').append(dp[len]);
+		
+		System.out.print(sb);
+	}
+}
