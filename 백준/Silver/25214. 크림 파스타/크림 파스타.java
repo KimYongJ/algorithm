c0ce73f2f417b0dbeb4322ea5132458c42c33cd8
YongJu Kim <kyjdummy@gmail.com>
2025-02-02 14:01:25 +0900

    [Silver IV] Title: 크림 파스타, Time: 344 ms, Memory: 26256 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.java
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.java
@@ -2,37 +2,34 @@
 //https://www.acmicpc.net/problem/25214
 //1초 / 1024mb
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		sb.append(0).append(' ');
 		
-		int N		= Integer.parseInt(br.readLine());
+		int N		= read();
 		int arr[]	= new int[N+2];
 		int dp[]	= new int[N+2];
-		int min[]	= new int[N+2];
 		
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		min[1] = arr[1] = Integer.parseInt(st.nextToken());
+		int min = arr[1] = read();
 		for(int i=2; i<=N; i++)
 		{
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 			if(arr[i-1] >= arr[i])
 				dp[i] = dp[i-1];
 			else
-				dp[i] = Math.max(dp[i-1], arr[i] - min[i-1]);
+				dp[i] = Math.max(dp[i-1], arr[i] - min);
 			
-			min[i] = Math.min(arr[i], min[i-1]);
+			min = Math.min(arr[i], min);
 			
 			sb.append(dp[i]).append(' ');
 		}
 		
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
