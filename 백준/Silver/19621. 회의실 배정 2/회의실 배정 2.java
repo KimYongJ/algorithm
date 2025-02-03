f94f6f58c28e4c4750ab18b40d92db9f0182307f
YongJu Kim <kyjdummy@gmail.com>
2025-02-03 13:47:20 +0900

    [Silver II] Title: 회의실 배정 2, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
@@ -3,73 +3,29 @@
 //1초 / 256MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-class Node{
-	int s, e, cnt;
-	Node(int s, int e, int cnt){
-		this.s=s; this.e=e; this.cnt=cnt;
-	}
-}
+
 class Main{
-	
-	static int N;
-	static int[] dp,endTime;
-	static Node[] node;
 	
 	public static void main(String[]args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N		= Integer.parseInt(br.readLine());	//회의 수(1<=25)
-		dp		= new int[N+1];
-		endTime	= new int[N+1];
-		node	= new Node[N+1];
-		
-		node[0] = new Node(0,0,0);
-		
+		int N		= Integer.parseInt(br.readLine());	//회의 수(1<=25)
+		int[]dp		= new int[N+1];
+		int[]arr	= new int[N+1];
+
 		for(int i=1; i<=N; i++)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());	// 0<=int최대값, 회의시간은 모두 다름
-			int e = Integer.parseInt(st.nextToken());	// 0<=int최대값, 회의시간은 모두 다름
-			int c = Integer.parseInt(st.nextToken());	// 회의 인원(1<=천)
-			node[i] = new Node(s,e,c);
+			Integer.parseInt(st.nextToken());	// 0<=int최대값, 회의시간은 모두 다름
+			Integer.parseInt(st.nextToken());	// 0<=int최대값, 회의시간은 모두 다름
+			arr[i] = Integer.parseInt(st.nextToken());	// 회의 인원(1<=천)
 		}
 		
-		Arrays.sort(node, (a,b)->a.e-b.e);
-		
-		for(int i=1; i<=N; i++)
-		{
-			if(endTime[i-1] <= node[i].s)
-			{
-				dp[i] = dp[i-1] + node[i].cnt;
-				endTime[i] = node[i].e;
-			}
-			else
-			{
-				int idx = getBeforeMaxCnt(i, node[i].s);
-				if(dp[i-1] < dp[idx] + node[i].cnt)
-				{
-					dp[i] = dp[idx] + node[i].cnt;
-					endTime[i] = node[i].e;
-				}
-				else if(dp[i-1] > dp[idx] + node[i].cnt)
-				{
-					dp[i] = dp[i-1];
-					endTime[i] = endTime[i-1];
-				}
-				else
-				{
-					dp[i] = dp[i-1];
-					endTime[i] = Math.min(endTime[i-1], node[i].e);
-				}
-			}
-		}
+		dp[1] = arr[1];
+
+		for(int i=2; i<=N; i++)
+			dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i]);
+
 		System.out.print(dp[N]);
-	}
-	public static int getBeforeMaxCnt(int idx, int targetTime) {
-		while(--idx >=0)
-			if(endTime[idx] <= targetTime)
-				return idx;
-		return 0;
 	}
 }
