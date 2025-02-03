fefcc1fa47458f90465541fddb99ecffba9ea872
YongJu Kim <kyjdummy@gmail.com>
2025-02-03 16:53:02 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -4,49 +4,48 @@
 //가장 긴 감소하는 부분수열의 길이 출력
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 class Main{
 
+	static int len = 1;
+	
 	public static void main(String[]args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N		= Integer.parseInt(br.readLine());
 		int arr[]	= new int[N+1];// 1<=천
+		int desc[]	= new int[N+1];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());// 1<=천
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		
-		list.add(arr[1]);
+
+		desc[1] = arr[1];
 		
 		for(int i=2; i<=N; i++)
 		{
-			int idx = binarySearch(list, arr[i]); // arr[i]보다 작은 숫자의 인덱스를 가져온다.
-			if(idx < 0)
-				list.add(0,arr[i]);	// 자기보다 작은 수가 없으면 맨 앞에 넣음
-			else
-			{
-				list.remove(idx);
-				list.add(idx, arr[i]);
-			}
+			int idx = binarySearch(desc, arr[i]); // arr[i]보다 작은 숫자의 인덱스를 가져온다.
+			if(idx < 0)					// 자기보다 작은 수를 찾을 수 없는 경우 뒤에 값 삽입
+				desc[++len] = arr[i];
+			else						// 자기보다 작은 수를 찾으면 그 수는 자기로 대체
+				desc[idx] = arr[i];
 		}
-		System.out.print(list.size());
+		System.out.print(len);
 	}
-	public static int binarySearch(ArrayList<Integer> list, int target) {
-		int s = 0;
-		int e = list.size() - 1;
+	public static int binarySearch(int[] desc, int target) {
+		int s	= 1;
+		int e	= len;
 		int res = -1;
+		
 		while(s <= e)
 		{
 			int mid = (s + e) >> 1;
-			if(list.get(mid) <= target) {
-				res = mid;
+			if(desc[mid] > target)
 				s = mid + 1;
-			}
 			else
+			{
+				res = mid;
 				e = mid - 1;
+			}
 		}
 		return res;
 	}
