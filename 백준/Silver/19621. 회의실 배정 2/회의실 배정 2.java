320c1b0a7f3d134d6879380d9f28a0221bd9560d
YongJu Kim <kyjdummy@gmail.com>
2025-02-03 13:48:32 +0900

    [Silver II] Title: 회의실 배정 2, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
@@ -1,24 +1,19 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/19621
 //1초 / 256MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
 	public static void main(String[]args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());	//회의 수(1<=25)
+		int N		= read();	//회의 수(1<=25)
 		int[]dp		= new int[N+1];
 		int[]arr	= new int[N+1];
 
 		for(int i=1; i<=N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			Integer.parseInt(st.nextToken());	// 0<=int최대값, 회의시간은 모두 다름
-			Integer.parseInt(st.nextToken());	// 0<=int최대값, 회의시간은 모두 다름
-			arr[i] = Integer.parseInt(st.nextToken());	// 회의 인원(1<=천)
+			read();	// 0<=int최대값, 회의시간은 모두 다름
+			read();	// 0<=int최대값, 회의시간은 모두 다름
+			arr[i] = read();	// 회의 인원(1<=천)
 		}
 		
 		dp[1] = arr[1];
@@ -27,5 +22,10 @@ class Main{
 			dp[i] = Math.max(dp[i-1], dp[i-2] + arr[i]);
 
 		System.out.print(dp[N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
