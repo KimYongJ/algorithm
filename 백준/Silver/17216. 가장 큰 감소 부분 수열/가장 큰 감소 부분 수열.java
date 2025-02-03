e4c0d9cd9aaf5eba61f3caa14bd0c45034e194ad
YongJu Kim <kyjdummy@gmail.com>
2025-02-03 16:10:47 +0900

    [Silver II] Title: 가장 큰 감소 부분 수열, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.java
+++ 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.java
@@ -24,12 +24,12 @@ class Main{
 		int max = 0;
 		
 		for(int i=1; i<=N; i++)
-		{ 
-			// arr[i]+1번째 인덱스부터 MAX까지 저장된 값 중 가장 큰 값을 들고온다. 이 때 저장된 큰 값은 dp[0]~dp[i-1]값이다.
-			int maxNum = query(1, 1, MAX, arr[i] + 1, MAX);
-			
-			dp[i] += maxNum;
-			
+		{
+			// arr[i]값 자체를 인덱스로 사용, 그 안에 데이터는 dp[i]값을 저장,
+			// arr[i]+1범위 부터 1000 범위안에 가장 큰 dp[x]값을 찾아 dp[i]에 더한다.
+			// 즉, i보다 작은 0~j까지 dp[0~j]중 가장 큰 dp[j]값을 가져와 더한다.
+			dp[i] += query(1, 1, MAX, arr[i] + 1, MAX);
+			// arr[i]값에 dp[i]를 넣는다.
 			update(1, 1, MAX, arr[i], dp[i]);
 			
 			max = Math.max(max, dp[i]);
@@ -37,30 +37,33 @@ class Main{
 		System.out.print(max);
 	}
 	public static void update(int treeNode, int s, int e, int idx, int value) {
-		if(e < idx || idx < s)
-			return;
+		if(e<idx || idx < s)
+			return ;
 		if(s == e)
 		{
-			tree[treeNode] = Math.max(tree[treeNode], value);
+			tree[treeNode] = value;
 			return;
 		}
-		int nextNode = treeNode << 1;
-		int mid = (s + e) >> 1;
+		
+		int nextNode= treeNode << 1;
+		int mid		= (s + e) >> 1;
+		
 		update(nextNode, s, mid, idx, value);
 		update(nextNode | 1, mid + 1, e, idx, value);
-		
 		tree[treeNode] = Math.max(tree[nextNode], tree[nextNode | 1]);
 	}
 	public static int query(int treeNode, int s, int e, int left, int right) {
 		if(e < left || right < s)
 			return 0;
-		if(left <= s && e<= right)
+		if(left<=s && e<=right)
 			return tree[treeNode];
 		
 		int nextNode= treeNode << 1;
 		int mid		= (s + e) >> 1;
-		return Math.max(query(nextNode, s, mid, left, right),
-						query(nextNode | 1, mid + 1, e, left, right));
 		
+		return Math.max(
+						query(nextNode, s, mid, left, right), 
+						query(nextNode | 1, mid + 1, e, left, right)
+				);
 	}
 }
