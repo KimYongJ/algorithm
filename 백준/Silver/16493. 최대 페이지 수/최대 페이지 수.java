a42f86b80fc40aa7aa0477c4834c8f95b00e71f7
YongJu Kim <kyjdummy@gmail.com>
2025-02-04 22:22:20 +0900

    [Silver II] Title: 최대 페이지 수, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
@@ -2,25 +2,18 @@
 //https://www.acmicpc.net/problem/16493
 //2초 / 512mb
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 1<=200
-		int M = Integer.parseInt(st.nextToken());	// 1<=20
-		int dp[][] = new int[M+1][N+1];
-		int day[] = new int[M+1];
-		int page[] = new int[M+1];
+		int N		= read();	// 1<=200
+		int M		= read();	// 1<=20
+		int dp[][]	= new int[M+1][N+1];
+		int day[]	= new int[M+1];
+		int page[]	= new int[M+1];
 		
 		for(int m=1; m<=M; m++)
 		{
-			st = new StringTokenizer(br.readLine());
-			day[m]	= Integer.parseInt(st.nextToken());
-			page[m]	= Integer.parseInt(st.nextToken());
+			day[m]	= read();
+			page[m]	= read();
 		}
 		
 		for(int m=1; m<=M; m++)
@@ -34,5 +27,10 @@ class Main{
 			}
 		}
 		System.out.print(dp[M][N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
