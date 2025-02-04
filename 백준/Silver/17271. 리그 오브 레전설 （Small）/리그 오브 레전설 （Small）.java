331e751378306c5a8e545e695003076bc048f2f3
YongJu Kim <kyjdummy@gmail.com>
2025-02-04 10:58:12 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.java
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.java
@@ -1,42 +1,31 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/17271
 //1초 / 256MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		final int MOD = 1_000_000_007;
-		int N		= Integer.parseInt(st.nextToken());	// 최대시간 1<=만
-		int M		= Integer.parseInt(st.nextToken());	// B스킬 시간 2<=100
+		int N		= read();	// 최대시간 1<=만
+		int M		= read();	// B스킬 시간 2<=100
 		int dp[]	= new int[10_001];
-		if(M == 1)
+		
+		dp[0] = 1;
+		
+		for(int i=1; i<=N; i++)
 		{
-			dp[1] = 2;
-			for(int i=2; i<=N; i++)
-			{
-				dp[i] = dp[i-1] << 1;
-				if(dp[i] >= MOD)
-					dp[i] -=MOD;
-			}
+			dp[i] = dp[i-1];
 			
-			System.out.print(dp[N]);
-			return;
-		}
-		for(int i=1; i<M; i++)
-			dp[i] = 1;
-		
-		dp[M] = 2;
-		
-		for(int i=M + 1; i<=N; i++)
-		{
-			dp[i] = dp[i-1] + dp[i-M];
+			if(0<= i-M)
+				dp[i] += dp[i-M];
+			
 			if(dp[i] >= MOD)
 				dp[i] -=MOD;
 		}
 		System.out.print(dp[N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
