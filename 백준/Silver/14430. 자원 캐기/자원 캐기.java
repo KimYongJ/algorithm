29ec2fd1c4c8466e55594980787714d7270fd200
YongJu Kim <kyjdummy@gmail.com>
2025-02-05 14:16:31 +0900

    [Silver II] Title: 자원 캐기, Time: 200 ms, Memory: 22272 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.java
@@ -0,0 +1,31 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/14430
+//2초 / 25MB
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int Y		= Integer.parseInt(st.nextToken());	// 세로 1<=300
+		int X		= Integer.parseInt(st.nextToken());	// 가로 1<=300
+		int dp[][]	= new int[Y+1][X+1];
+		
+		for(int y=1; y<=Y; y++)
+		{
+			st = new StringTokenizer(br.readLine());
+			for(int x=1; x<=X; x++)
+			{
+				dp[y][x] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int y=1; y<=Y; y++) {
+			for(int x=1; x<=X; x++) {
+				dp[y][x] += Math.max(dp[y-1][x], dp[y][x-1]);
+			}
+		}
+		System.out.print(dp[Y][X]);
+	}
+}
