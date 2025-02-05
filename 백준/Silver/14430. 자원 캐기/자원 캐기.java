2f5de617ff992c1c173832cf3b86eba378f37f9b
YongJu Kim <kyjdummy@gmail.com>
2025-02-05 14:18:32 +0900

    [Silver II] Title: 자원 캐기, Time: 116 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/14430. 자원 캐기/자원 캐기.java
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.java
@@ -1,27 +1,21 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/14430
 //2초 / 25MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int Y		= Integer.parseInt(st.nextToken());	// 세로 1<=300
-		int X		= Integer.parseInt(st.nextToken());	// 가로 1<=300
+		int Y		= read();	// 세로 1<=300
+		int X		= read();	// 가로 1<=300
 		int dp[][]	= new int[Y+1][X+1];
 		
 		for(int y=1; y<=Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++)
-			{
-				dp[y][x] = Integer.parseInt(st.nextToken()) + Math.max(dp[y-1][x], dp[y][x-1]);
-			}
-		}
-		
+				dp[y][x] = read() + Math.max(dp[y-1][x], dp[y][x-1]);
+
 		System.out.print(dp[Y][X]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
