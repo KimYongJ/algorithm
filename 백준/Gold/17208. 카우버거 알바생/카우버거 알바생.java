c0b07ce420a046b3b6b465b376cd1be248ed7553
YongJu Kim <kyjdummy@gmail.com>
2025-02-05 22:29:09 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 188 ms, Memory: 51244 KB -BaekjoonHub

--- 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
@@ -2,37 +2,23 @@
 //https://www.acmicpc.net/problem/17208
 //3초 / 512mb
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{ 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());	// 주문 수1<=100
-		int M		= Integer.parseInt(st.nextToken());	// 남은 치즈버거  1<=300
-		int K		= Integer.parseInt(st.nextToken());	// 남은 감자튀김  1<=300
-		int cheese[]= new int[N+1];
-		int fries[]	= new int[N+1];
+		int N		= read();	// 주문 수1<=100
+		int M		= read();	// 남은 치즈버거  1<=300
+		int K		= read();	// 남은 감자튀김  1<=300
 		int dp[][][]= new int[N+1][M+1][K+1];
 		
 		for(int i=1; i<=N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			cheese[i]	= Integer.parseInt(st.nextToken());//1<=300
-			fries[i]	= Integer.parseInt(st.nextToken());//1<=300
-		}
-		
-		for(int i=1; i<=N; i++)
-		{
-			int chs = cheese[i];
-			int frs = fries[i];
+			int chs = read();//1<=300
+			int frs = read();//1<=300
 			for(int c=1; c<=M; c++)
 			{
 				for(int f=1; f<=K; f++)
 				{
 					dp[i][c][f] = dp[i-1][c][f];
+					
 					if(0<=c-chs && 0<=f-frs)
 						dp[i][c][f] = Math.max(dp[i][c][f], dp[i-1][c-chs][f-frs] + 1);
 				}
@@ -40,5 +26,10 @@ class Main{
 		}
 		
 		System.out.print(dp[N][M][K]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
