742e0e9129fdefc974247fe4eb1de44d461d545b
YongJu Kim <kyjdummy@gmail.com>
2025-02-06 16:23:13 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 176 ms, Memory: 24992 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -1,22 +1,17 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/9184
 //1초 / 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int dp[][][] = new int[101][101][101];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());// -50<=50
-			int b = Integer.parseInt(st.nextToken());// -50<=50
-			int c = Integer.parseInt(st.nextToken());// -50<=50
+			int a = read();// -50<=50
+			int b = read();// -50<=50
+			int c = read();// -50<=50
 			if(a== -1 && b==-1 && c==-1)
 				break;
 
@@ -42,4 +37,12 @@ class Main{
 		return dp[a][b][c] = DFS(a-1, b,c) + DFS(a-1,b-1,c) 
 								+ DFS(a-1,b,c-1) - DFS(a-1,b-1,c-1); 
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
