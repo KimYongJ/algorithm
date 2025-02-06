ce36e07bd942f55102fb40842c809df38384f330
YongJu Kim <kyjdummy@gmail.com>
2025-02-06 10:59:21 +0900

    [Gold IV] Title: 여행 가자, Time: 116 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,10 +1,7 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1976
 //2초 / 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.StringTokenizer;
 class Node{
 	int node;
 	Node next;
@@ -21,29 +18,22 @@ class Main{
 		print();
 	}
 	public static void input() throws Exception{
-		N		= Integer.parseInt(br.readLine());	//도시 수1<=200
-		M		= Integer.parseInt(br.readLine());	//여행계획에 속한 도시 수 1<=천
+		N		= read();	//도시 수1<=200
+		M		= read();	//여행계획에 속한 도시 수 1<=천
 		dir		= new int[M];
 		visit	= new boolean[N+1];
 		adNode	= new Node[N + 1];
 		
 		for(int y=1; y<=N; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=N; x++)
-			{
-				int flag = Integer.parseInt(st.nextToken());
-				if(flag == 1)
+				if(read() == 1)
 				{
 					adNode[y] = new Node(x, adNode[y]);
 					adNode[x] = new Node(y, adNode[x]);
 				}
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=0; i<M; i++)
-			dir[i] = Integer.parseInt(st.nextToken());
+			dir[i] = read();
 	}
 	public static void print() {
 		for(int d: dir)
@@ -71,9 +61,12 @@ class Main{
 			}
 		}
 	}
-	
-	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
+	}
+
 	static int N, M, dir[];
 	static boolean visit[];
 	static Node adNode[];
