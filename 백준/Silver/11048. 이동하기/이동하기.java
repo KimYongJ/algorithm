7c9ecdcb93ede36e752677529009359b46e3defc
YongJu Kim <kyjdummy@gmail.com>
2025-02-06 16:52:01 +0900

    [Silver II] Title: 이동하기, Time: 180 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,28 +1,21 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/11048
 //1초 / 256MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y = Integer.parseInt(st.nextToken());// 1<=천
-		int X = Integer.parseInt(st.nextToken());// 1<=천
+		int Y = read();// 1<=천
+		int X = read();// 1<=천
 		int map[][] = new int[Y+1][X+1];
 		for(int y=1; y<=Y; y++)
-		{
-			st = new StringTokenizer(br.readLine());
 			for(int x=1; x<=X; x++)
-			{
-				int a = map[y-1][x];
-				int b = map[y][x-1];
-				int c = map[y-1][x-1];
-				map[y][x] = Integer.parseInt(st.nextToken())
-						+ Math.max(a, Math.max(b, c)); 
-			}
-		}
+				map[y][x] = read()
+						+ Math.max(map[y-1][x], Math.max(map[y][x-1], map[y-1][x-1])); 
+
 		System.out.print(map[Y][X]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
