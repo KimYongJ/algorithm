72eb119ac696e11b8a832f6696a350755aad2ad8
YongJu Kim <kyjdummy@gmail.com>
2025-02-06 16:53:51 +0900

    [Silver II] Title: 이동하기, Time: 156 ms, Memory: 19492 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -3,19 +3,43 @@
 //1초 / 256MB
 class Main{
 	public static void main(String[] args)throws Exception{
-		int Y = read();// 1<=천
-		int X = read();// 1<=천
+		Reader in = new Reader();
+		int Y = in.nextInt();// 1<=천
+		int X = in.nextInt();// 1<=천
 		int map[][] = new int[Y+1][X+1];
 		for(int y=1; y<=Y; y++)
 			for(int x=1; x<=X; x++)
-				map[y][x] = read()
+				map[y][x] = in.nextInt()
 						+ Math.max(map[y-1][x], Math.max(map[y][x-1], map[y-1][x-1])); 
 
 		System.out.print(map[Y][X]);
 	}
-	static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
-		return n;
-	}
+}
+class Reader {
+    private final int SIZE = 1 << 13;
+    private byte[] buffer = new byte[SIZE];
+    private int index, size;
+
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isnumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    private boolean isnumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    private byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
