6448af5342ff0900a2fab94f0ba1b7a6135a9b63
YongJu Kim <kyjdummy@gmail.com>
2025-02-07 16:07:46 +0900

    [Gold IV] Title: 사이클 게임, Time: 276 ms, Memory: 19008 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,15 +1,11 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/20040
 //1초 / 512MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());// 점의개수 n(3<=오십만) / 노드는 0부터 n-1까지  
-		int M		= Integer.parseInt(st.nextToken());// 진행된 차례(3<=백만)
+
+		int N		= read();// 점의개수 n(3<=오십만) / 노드는 0부터 n-1까지  
+		int M		= read();// 진행된 차례(3<=백만)
 		int parent[]= new int[N];
 		int result	= 0;
 		
@@ -18,10 +14,10 @@ class Main{
 		
 		for(int i=0; i<M; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int parent1 = getParent(parent, Integer.parseInt(st.nextToken()));
-			int parent2 = getParent(parent, Integer.parseInt(st.nextToken()));
-			if(parent1 == parent2) {
+			int parent1 = getParent(parent, read());
+			int parent2 = getParent(parent, read());
+			if(parent1 == parent2)
+			{
 				result = i + 1;
 				break;
 			}
@@ -38,5 +34,10 @@ class Main{
 	public static int getParent(int[] parent, int node) {
 		if(parent[node] == node) return node;
 		return parent[node] = getParent(parent, parent[node]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
