bc723d3e0f1e885312a215f295c89f2766b8355e
YongJu Kim <kyjdummy@gmail.com>
2025-02-07 14:26:04 +0900

    [Gold IV] Title: 친구비, Time: 116 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,9 +1,6 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/16562
 //2초 / 512MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -12,27 +9,23 @@ class Main{
 	static boolean[] visit;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());	// 학생수(1<=만)
-		M		= Integer.parseInt(st.nextToken());	// 친구 관계수(0<=만)
-		TOTAL	= Integer.parseInt(st.nextToken());	// 가지고있는돈(1<=천만)
+		N		= read();	// 학생수(1<=만)
+		M		= read();	// 친구 관계수(0<=만)
+		TOTAL	= read();	// 가지고있는돈(1<=천만)
 		parent	= new int[N+1];
 		money	= new int[N+1];
 		visit	= new boolean[N+1];
 		
-		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
 		{
 			parent[i]	= i;
-			money[i]	= Integer.parseInt(st.nextToken());
+			money[i]	= read();
 		}
 		
 		for(int i=0; i<M; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int parent1 = getParent(Integer.parseInt(st.nextToken()));
-			int parent2 = getParent(Integer.parseInt(st.nextToken()));
+			int parent1 = getParent( read() );
+			int parent2 = getParent( read() );
 			if(parent1 != parent2)
 			{
 				if(parent2 < parent1)
@@ -48,6 +41,7 @@ class Main{
 		}
 		
 		int total = 0;
+		
 		for(int i=1; i<=N; i++)
 		{
 			int parentNum = getParent(i);
@@ -63,5 +57,10 @@ class Main{
 	public static int getParent(int node) {
 		if(parent[node] == node) return node;
 		return parent[node] = getParent(parent[node]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
