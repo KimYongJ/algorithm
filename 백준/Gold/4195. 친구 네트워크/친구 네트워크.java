4596cf7a23d38eb2d4f0d9399dc89a2079d314a0
YongJu Kim <kyjdummy@gmail.com>
2025-02-07 11:16:48 +0900

    [Gold II] Title: 친구 네트워크, Time: 516 ms, Memory: 57656 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -4,70 +4,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 class Main{
 	
-	static HashSet<String> set = new HashSet<>();
 	static HashMap<String, Integer> map = new HashMap<>();
-	static String[][] rel;
+	static int T, N, idx;
 	static int[] parent, cnt;
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 		while(T-->0)
 		{
-			int N = Integer.parseInt(br.readLine());//1<=십만
-			rel = new String[N][2];
-			set.clear();
+			N		= Integer.parseInt(br.readLine());//1<=십만
+			parent	= new int[N<<1];
+			cnt		= new int[N<<1];
+			idx		= 0;
+			
 			map.clear();
+			
 			for(int i=0; i<N; i++)
 			{
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				rel[i][0] = st.nextToken();
-				rel[i][1] = st.nextToken();
-				set.add(rel[i][0]);
-				set.add(rel[i][1]);
-			}
-			
-			int idx = 0;
-			
-			for(String s : set)
-				map.put(s, idx++);
-			
-			parent	= new int[idx];
-			cnt		= new int[idx];
-			
-			for(int i=0; i<idx; i++)
-			{
-				parent[i]	= i;
-				cnt[i]		= 1;
-			}
-			
-			for(int i=0; i<N; i++)
-			{
-				int parent1 = getParent(map.get(rel[i][0]));
-				int parent2 = getParent(map.get(rel[i][1]));
+				int parent1 = getParent( getNumber(st.nextToken()) );
+				int parent2 = getParent( getNumber(st.nextToken()) );
 				
 				if(parent2 < parent1)
 				{
-					int tmp = parent1;
-					parent1 = parent2;
-					parent2 = tmp;
+					int tmp = parent2;
+					parent2 = parent1;
+					parent1 = tmp;
 				}
-
-				parent[parent2] = parent1;
 				
 				if(parent1 != parent2)
+				{
+					parent[parent2] = parent1;
 					cnt[parent1] += cnt[parent2];
-				
+				}
 				sb.append(cnt[parent1]).append('\n');
 			}
-			
 		}
 		System.out.print(sb);
+	}
+	public static int getNumber(String str) {
+		if(map.containsKey(str))
+		{
+			return map.get(str);
+		}
+		map.put(str, idx);
+		parent[idx] = idx;
+		cnt[idx]	= 1;
+		return idx++;
 	}
 	public static int getParent(int node) {
 		if(parent[node] == node) return node;
