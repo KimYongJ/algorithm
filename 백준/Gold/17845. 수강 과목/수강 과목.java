bd5dcfc24d25ed3586ef1e0adacd000f489867b6
YongJu Kim <kyjdummy@gmail.com>
2025-02-08 21:59:51 +0900

    [Gold V] Title: 수강 과목, Time: 160 ms, Memory: 51428 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -2,25 +2,18 @@
 //https://www.acmicpc.net/problem/17845
 //1초 / 512MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());// 1<=만 최대공부시간
-		int K = Integer.parseInt(st.nextToken());// 1<=천 과목 수
-		int dp[][] = new int[K+1][N+1];// 과목 : 시간
+		int N		= read();// 1<=만 최대공부시간
+		int K		= read();// 1<=천 과목 수
+		int dp[][]	= new int[K+1][N+1];// 과목 : 시간
 		int value[] = new int[K+1];
-		int time[] = new int[K+1];
+		int time[]	= new int[K+1];
 		
 		for(int i=1; i<=K; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			value[i]= Integer.parseInt(st.nextToken());
-			time[i]	= Integer.parseInt(st.nextToken());
+			value[i]= read();
+			time[i]	= read();
 		}
 		
 		//과목 순회
@@ -34,7 +27,11 @@ class Main{
 					dp[i][j] = Math.max(dp[i][j], dp[i-1][j - time[i]] + value[i]);
 			}
 		}
-		
 		System.out.print(dp[K][N]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
