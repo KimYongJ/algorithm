036d70b9b02f99b0125123a4ed08c6b221dcaca0
YongJu Kim <kyjdummy@gmail.com>
2025-02-08 22:01:48 +0900

    [Gold V] Title: 수강 과목, Time: 160 ms, Memory: 51360 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -7,26 +7,19 @@ class Main{
 		int N		= read();// 1<=만 최대공부시간
 		int K		= read();// 1<=천 과목 수
 		int dp[][]	= new int[K+1][N+1];// 과목 : 시간
-		int value[] = new int[K+1];
-		int time[]	= new int[K+1];
 		
 		for(int i=1; i<=K; i++)
 		{
-			value[i]= read();
-			time[i]	= read();
-		}
-		
-		//과목 순회
-		for(int i=1; i<=K;i++)
-		{
-			// 시간 순회
+			int value	= read();
+			int time	= read();
 			for(int j=1; j<=N; j++)
 			{
 				dp[i][j] = dp[i-1][j];
-				if(0<=j-time[i])
-					dp[i][j] = Math.max(dp[i][j], dp[i-1][j - time[i]] + value[i]);
+				if(0<=j-time)
+					dp[i][j] = Math.max(dp[i][j], dp[i-1][j - time] + value);
 			}
 		}
+
 		System.out.print(dp[K][N]);
 	}
 	static int read() throws Exception {
