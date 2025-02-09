e4953b22899a0c3952b11309b49b6578e4662e46
YongJu Kim <kyjdummy@gmail.com>
2025-02-09 22:08:20 +0900

    [Gold V] Title: 창영이와 커피, Time: 124 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
@@ -2,29 +2,29 @@
 //https://www.acmicpc.net/problem/22115
 //2초 / 512MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());// 커피개수 1<=백
-		int K = Integer.parseInt(st.nextToken());// 카페인양 0<=십만
-		int dp[] = new int[K + 1];
+		int N		= read();// 커피개수 1<=백
+		int K		= read();// 카페인양 0<=십만
+		int dp[]	= new int[K + 1];
 		
 		Arrays.fill(dp, 200);
 		dp[0] = 0;
-		st = new StringTokenizer(br.readLine());
+		
 		for(int i=1; i<=N; i++)
 		{
-			int cf = Integer.parseInt(st.nextToken());
+			int cf = read();
 			for(int j=K; j>=cf; j--)
 				dp[j] = Math.min(dp[j], dp[j - cf] + 1);
 		}
 		
 		System.out.print(dp[K] == 200 ? -1 : dp[K]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
