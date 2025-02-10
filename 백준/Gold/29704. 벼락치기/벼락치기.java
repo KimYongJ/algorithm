36aec22dbdb64229f7233f50a5747e20a943b894
YongJu Kim <kyjdummy@gmail.com>
2025-02-10 21:35:54 +0900

    [Gold V] Title: 벼락치기, Time: 116 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/29704. 벼락치기/벼락치기.java
+++ 백준/Gold/29704. 벼락치기/벼락치기.java
@@ -2,38 +2,28 @@
 //https://www.acmicpc.net/problem/29704
 //1초 / 1024MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());// 문제 개수1<=천
-		int T		= Integer.parseInt(st.nextToken());// 남은 제출 기한 1<=천
-		int day[]	= new int[N+1];
-		int cost[]	= new int[N+1];
+		int N		= read();// 문제 개수1<=천
+		int T		= read();// 남은 제출 기한 1<=천
 		int costSum = 0;
-		int dp[][]	= new int[N + 1][T + 1];
-		
+		int dp[]	= new int[T+1];
+
 		for(int i=1; i<=N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			day[i] = Integer.parseInt(st.nextToken());//1<=천
-			costSum += cost[i] = Integer.parseInt(st.nextToken());//1<=오천
+			int day = read();//1<=천
+			int cost= read();//1<=오천
+			
+			for(int j=T; j>=day; j--)
+				dp[j] = Math.max(dp[j], dp[j-day] + cost);
+			
+			costSum += cost;
 		}
-		
-		for(int i=1; i<=N; i++)
-		{
-			for(int j=0; j<=T; j++)
-			{
-				dp[i][j] = dp[i-1][j];
-				if(0<=j-day[i] )
-					dp[i][j] = Math.max(dp[i][j], dp[i-1][j-day[i]] + cost[i]);
-			}
-		}
-		
-		System.out.print(costSum - dp[N][T]);
+		System.out.print(costSum - dp[T]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
