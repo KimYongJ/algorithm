4b6079689bf8b7c2b97c0d2653af401ba3bd0eff
YongJu Kim <kyjdummy@gmail.com>
2025-02-10 13:35:21 +0900

    [Silver II] Title: 상자넣기, Time: 112 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -1,19 +1,15 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1965
 //2초 / 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N		= Integer.parseInt(br.readLine());// 1<=천
+		int N		= read();// 1<=천
 		int arr[]	= new int[N + 1];
 		int dp[]	= new int[N + 1];
 		int result	= 0;
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		
 		for(int i=1; i<=N; i++)
 		{
@@ -26,5 +22,10 @@ class Main{
 			result = Math.max(result, dp[i]);
 		}
 		System.out.print(result);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
