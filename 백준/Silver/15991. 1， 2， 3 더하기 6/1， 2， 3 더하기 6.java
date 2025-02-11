bca1ebae5786891e692f788d71e08c38bb74fadd
YongJu Kim <kyjdummy@gmail.com>
2025-02-11 13:38:08 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 116 ms, Memory: 15056 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
@@ -1,13 +1,9 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/15991
 //1초 / 512MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		final int MOD = 1_000_000_009;
-		
+		StringBuilder sb = new StringBuilder();
 		int N		= 100_001;
 		long dp[]	= new long[N];
 		
@@ -17,17 +13,19 @@ class Main{
 		dp[6] = 6;
 		
 		for(int i=7; i<N; i++)
-			dp[i] = (dp[i-2] + dp[i-4] + dp[i-6])%MOD;
+			dp[i] = (dp[i-2] + dp[i-4] + dp[i-6])%1_000_000_009;
 		
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
+		int T = read();
 		
 		while(T-->0)
-		{
-			int n = Integer.parseInt(br.readLine());//0<=십만
-			sb.append(dp[n]).append('\n');
-		}
+			//0<=십만
+			sb.append(dp[read()]).append('\n');
+		
 		System.out.print(sb);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
