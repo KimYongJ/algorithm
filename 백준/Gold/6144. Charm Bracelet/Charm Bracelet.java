7b42923209957790d3b2ade3ac916ffe4d22ee2e
YongJu Kim <kyjdummy@gmail.com>
2025-02-12 09:20:24 +0900

    [Gold V] Title: Charm Bracelet, Time: 208 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Gold/6144. Charm Bracelet/Charm Bracelet.java
+++ 백준/Gold/6144. Charm Bracelet/Charm Bracelet.java
@@ -12,7 +12,7 @@ class Main{
 		int M		= Integer.parseInt(st.nextToken());// 착용가능최대무게 1<=12880
 		int W[]		= new int[N + 1];
 		int V[]		= new int[N + 1];
-		int dp[][]	= new int[N + 1][M + 1];
+		int dp[]	= new int[M + 1];
 		
 		for(int i=1; i<=N; i++)
 		{
@@ -21,13 +21,10 @@ class Main{
 			V[i] = Integer.parseInt(st.nextToken());// 매력값
 		}
 		
-		for(int i=1; i<=N; i++) {
-			for(int m=1; m<=M; m++) {
-				dp[i][m] = dp[i-1][m];
-				if(0<=m-W[i])
-					dp[i][m] = Math.max(dp[i][m], dp[i-1][m-W[i]] + V[i]);
-			}
-		}
-		System.out.print(dp[N][M]);
+		for(int i=1; i<=N; i++)
+			for(int m=M; m>=W[i]; m--)
+				dp[m] = Math.max(dp[m], dp[m-W[i]] + V[i]);
+
+		System.out.print(dp[M]);
 	}
 }
