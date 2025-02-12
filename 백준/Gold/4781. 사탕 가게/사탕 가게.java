8edd90f30cffbbed1b3388c34654494a432d9213
YongJu Kim <kyjdummy@gmail.com>
2025-02-12 11:34:21 +0900

    [Gold IV] Title: 사탕 가게, Time: 840 ms, Memory: 31956 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -14,36 +14,32 @@ class Main{
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());//사탕의 종류 수(1<=오천)
 			int M = (int)Math.round((Double.parseDouble(st.nextToken()) * 100));//돈의 양(0.01 <= 100.00)
-			int C[] = new int[N + 1];//칼로리(1<=오천)
-			int P[] = new int[N + 1];//가격(0.01<=100.00)
-			int dp[][] = new int[N + 1][M + 1];// 최대 칼로리를 담음
+			int C = 0;//칼로리(1<=오천)
+			int P = 0;//가격(0.01<=100.00)
+			int dp[] = new int[M + 1];// 최대 칼로리를 담음
+			
 			if(N == 0 && M == 0)
 				break;
 			
 			for(int i=1; i<=N; i++)
 			{
 				st = new StringTokenizer(br.readLine());
-				C[i] = Integer.parseInt(st.nextToken());//칼로리(1<=오천)
-				P[i] = (int)Math.round((Double.parseDouble(st.nextToken()) * 100));//가격(0.01<=100.00)
-			}
-			
-			for(int i=1;i <=N; i++)
-			{
+				C = Integer.parseInt(st.nextToken());//칼로리(1<=오천)
+				P = (int)Math.round((Double.parseDouble(st.nextToken()) * 100));//가격(0.01<=100.00)
+				
 				for(int j=1; j<=M; j++)
-				{
-					dp[i][j] = dp[i-1][j];
-					if(j>=P[i])
-						dp[i][j] = Math.max(dp[i][j], dp[i][j-P[i]] + C[i]);
-					
-				}
+					if(j>=P)
+						dp[j] = Math.max(dp[j], dp[j-P] + C);
 			}
 			
 			int max = 0;
+			
 			for(int j=1; j<=M; j++)
-				max = Math.max(max, dp[N][j]);
+				max = Math.max(max, dp[j]);
 			
 			sb.append(max).append('\n');
 		}
 		System.out.print(sb);
 	}
 }
+
