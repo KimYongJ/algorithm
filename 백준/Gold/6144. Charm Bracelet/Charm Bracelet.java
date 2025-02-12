a078ec59959305e7e11e7364c4c05289f7d9d319
YongJu Kim <kyjdummy@gmail.com>
2025-02-12 09:22:23 +0900

    [Gold V] Title: Charm Bracelet, Time: 168 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/6144. Charm Bracelet/Charm Bracelet.java
+++ 백준/Gold/6144. Charm Bracelet/Charm Bracelet.java
@@ -1,30 +1,26 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/6144
 //1초 / 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());// 팔찌개수1<=3402
-		int M		= Integer.parseInt(st.nextToken());// 착용가능최대무게 1<=12880
-		int W[]		= new int[N + 1];
-		int V[]		= new int[N + 1];
+		int N		= read();// 팔찌개수1<=3402
+		int M		= read();// 착용가능최대무게 1<=12880
 		int dp[]	= new int[M + 1];
 		
 		for(int i=1; i<=N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			W[i] = Integer.parseInt(st.nextToken());// 무게
-			V[i] = Integer.parseInt(st.nextToken());// 매력값
+			int W = read();// 무게
+			int V = read();// 매력값
+			
+			for(int m=M; m>=W; m--)
+				dp[m] = Math.max(dp[m], dp[m-W] + V);
 		}
 		
-		for(int i=1; i<=N; i++)
-			for(int m=M; m>=W[i]; m--)
-				dp[m] = Math.max(dp[m], dp[m-W[i]] + V[i]);
-
 		System.out.print(dp[M]);
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
