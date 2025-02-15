668441cc2e5702c164d52f1cc96d4fda61b76456
YongJu Kim <kyjdummy@gmail.com>
2025-02-15 09:35:11 +0900

    [Gold V] Title: Dairy Queen, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Gold/6109. Dairy Queen/Dairy Queen.java
+++ 백준/Gold/6109. Dairy Queen/Dairy Queen.java
@@ -8,12 +8,14 @@ class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());// 만들어야 하는 돈 1<=300
-		int C = Integer.parseInt(st.nextToken());// 동전 개수 1<=200
-		int dp[] = new int[N + 1];
-		dp[0] = 1;
-		// 입력되는 돈은 큰 것 부터 내림차순
-		while(C-->0) {
+		int N	= Integer.parseInt(st.nextToken());// 만들어야 하는 돈 1<=300
+		int C	= Integer.parseInt(st.nextToken());// 동전 개수 1<=200
+		int dp[]= new int[N + 1];
+		
+		dp[0]	= 1;
+
+		while(C-->0)
+		{
 			int coin = Integer.parseInt(br.readLine());
 			for(int i=coin; i<=N; i++)
 				dp[i] += dp[i-coin];
