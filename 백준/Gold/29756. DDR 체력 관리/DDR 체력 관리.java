19ff27e2d247902176bf13ba1601d82f53ee8b27
YongJu Kim <kyjdummy@gmail.com>
2025-02-18 23:37:47 +0900

    [Gold IV] Title: DDR 체력 관리, Time: 116 ms, Memory: 15152 KB -BaekjoonHub

--- 백준/Gold/29756. DDR 체력 관리/DDR 체력 관리.java
+++ 백준/Gold/29756. DDR 체력 관리/DDR 체력 관리.java
@@ -1,11 +1,7 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/29756
 //1초 1024MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N, K;
@@ -13,27 +9,21 @@ class Main{
 	static int[] score, hp;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N		= Integer.parseInt(st.nextToken());// 구간개수 1<=천
-		K		= Integer.parseInt(st.nextToken());// 회복되는 체력 1<=10
+		N		= read();// 구간개수 1<=천
+		K		= read();// 회복되는 체력 1<=10
 		score	= new int[N + 1];
 		hp		= new int[N + 1];
 		dp		= new int[N + 1][101];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++)
-			score[i] = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++)
-			hp[i] = Integer.parseInt(st.nextToken());
+
+		for(int i=1; i<=N; i++)	score[i] = read();
+		for(int i=1; i<=N; i++)	hp[i] = read();
 		
 		for(int[] row : dp)
 			Arrays.fill(row, -1);
 		
-		System.out.print(top_down(N, 100));
+		System.out.print(top_down(N, 100, dp));
 	}
-	public static int top_down(int depth, int curHP) {
+	public static int top_down(int depth, int curHP, int[][] dp) {
 		if(depth == 0)
 			return 0;
 		if(dp[depth][curHP] >= 0)
@@ -41,11 +31,16 @@ class Main{
 		// 체력은 100을 넘을 수 없다.
 		int nextHP = Math.min(100, curHP + K);
 		// 해당 게임을 안하는 경우
-		dp[depth][curHP] = top_down(depth - 1, nextHP);
+		dp[depth][curHP] = top_down(depth - 1, nextHP, dp);
 		// 해당 게임을 하는 경우
-		if(nextHP - hp[depth] >= 0) {
-			dp[depth][curHP] = Math.max(dp[depth][curHP], top_down(depth - 1, nextHP - hp[depth]) + score[depth]);
-		}
+		if(nextHP - hp[depth] >= 0)
+			dp[depth][curHP] = Math.max(dp[depth][curHP], top_down(depth - 1, nextHP - hp[depth], dp) + score[depth]);
+		
 		return dp[depth][curHP];
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
