82425dd23fc8217f04f9e56fd3b0e4d0e39714aa
YongJu Kim <kyjdummy@gmail.com>
2025-02-21 22:54:30 +0900

    [Platinum III] Title: 성대나라의 물탱크, Time: 1704 ms, Memory: 136588 KB -BaekjoonHub

---
+++ 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.java
@@ -0,0 +1,100 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/18227
+//1초 256MB
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+class Node{
+	int node;
+	Node next;
+	Node(int node, Node next){
+		this.node = node;
+		this.next = next;
+	}
+}
+class Main{
+	
+	static int N, S, cnt;
+	static int[][] range;
+	static int[] depth;
+	static Node[] adNode;
+	static long[] tree;
+	static boolean[] visit;
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N		= Integer.parseInt(st.nextToken());// 도시 수(1<=이십만)
+		S		= Integer.parseInt(st.nextToken());// 수도의 번호(1<=N)
+		adNode	= new Node[N + 1];
+		range	= new int[N + 1][2];
+		depth	= new int[N + 1];
+		tree	= new long[N * 4];
+		visit	= new boolean[N + 1];
+		
+		for(int i=1; i<N; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adNode[a] = new Node(b, adNode[a]);
+			adNode[b] = new Node(a, adNode[b]);
+		}
+		visit[S] = true;
+		DFS(S, 1);
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T-->0)
+		{
+			st = new StringTokenizer(br.readLine());
+			int f = Integer.parseInt(st.nextToken());
+			int i = Integer.parseInt(st.nextToken());
+			if(f == 1) {
+				update(1, 1, N, range[i][0]);
+			}
+			else {
+				long cnt = query(1, 1, N, range[i][0], range[i][1]);
+				sb.append(depth[i] * cnt).append('\n');
+			}
+		}
+		System.out.print(sb);
+	}
+	public static long query(int treeNode, int s, int e, int left, int right) {
+		if(e < left || right < s)
+			return 0;
+		if(left <= s && e <= right)
+			return tree[treeNode];
+		int mid = (s + e) >> 1;
+		
+		return query(treeNode << 1, s, mid, left, right) + query(treeNode << 1 | 1, mid + 1, e, left, right);
+	}
+	public static void update(int treeNode, int s, int e, int idx) {
+		if(e < idx || idx < s)
+			return;
+		if(s == e)
+		{
+			tree[treeNode]++;
+			return;
+		}
+		int mid = (s + e) >> 1;
+		update(treeNode << 1, s, mid, idx);
+		update(treeNode << 1 | 1, mid + 1, e, idx);
+		tree[treeNode] = tree[treeNode << 1] + tree[treeNode << 1 | 1];
+	}
+	public static void DFS(int node, int dep) {
+		depth[node] = dep;
+		range[node][0] = ++cnt;
+		for(Node next=adNode[node]; next!=null; next=next.next)
+		{
+			if(!visit[next.node])
+			{
+				visit[next.node] = true;
+				DFS(next.node, dep + 1);
+			}
+		}
+		range[node][1] = cnt;
+	}
+}
+
