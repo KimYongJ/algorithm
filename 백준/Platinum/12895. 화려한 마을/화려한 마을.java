3aef937cc8f657766f4c10b01e87c2fb301f94b2
YongJu Kim <kyjdummy@gmail.com>
2025-02-23 20:11:32 +0900

    [Platinum III] Title: 화려한 마을, Time: 284 ms, Memory: 19844 KB -BaekjoonHub

--- 백준/Platinum/12895. 화려한 마을/화려한 마을.java
+++ 백준/Platinum/12895. 화려한 마을/화려한 마을.java
@@ -1,66 +1,65 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/12895
 //2초 512MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 class Main{
 	
 	static int N, T, Q;
-	static long[] tree, lazy;
+	static int[] tree, lazy;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		N		= Integer.parseInt(st.nextToken());//집개수 1<=십만
-		T		= Integer.parseInt(st.nextToken());//사용할 색개수 1<=30
-		Q		= Integer.parseInt(st.nextToken());//작업의 개수 1<=십만
-		tree	= new long[N*4];
-		lazy	= new long[N*4];
+		StringBuilder sb= new StringBuilder();
+		Reader in		= new Reader();
+
+		N		= in.nextInt();//집개수 1<=십만
+		T		= in.nextInt();//사용할 색개수 1<=30
+		Q		= in.nextInt();//작업의 개수 1<=십만
+		tree	= new int[N*4];
+		lazy	= new int[N*4];
 		
 		init(1, 1, N);
 		
 		while(Q-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			char c = st.nextToken().charAt(0);
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
+			char c = in.nextChar();
+			int x = in.nextInt();
+			int y = in.nextInt();
 			if(y < x)
 			{
 				int t = y;
 				y = x;
 				x = t;
 			}
+			
 			if(c == 'C')// x~y 사이 z번으로 칠함
-			{
-				int z = Integer.parseInt(st.nextToken());
-				update(1, 1, N, x, y, 1L << (z - 1));
-			}
+				update(1, 1, N, x, y, 1 << (in.nextInt() - 1));
+			
 			else// x~y 출력
-			{
-				long result = query(1, 1, N, x, y);
-				sb.append(Long.bitCount(result))
+				sb.append(Integer.bitCount(query(1, 1, N, x, y)))
 					.append('\n');
-			}
 		}
 		System.out.print(sb);
 	}
 	public static void init(int treeNode, int s, int e) {
-		if(s == e) {
+		if(s == e)
+		{
 			tree[treeNode] = 1;
 			return;
 		}
+		
 		int mid = (s + e) >> 1;
+				
 		init(treeNode << 1, s, mid);
 		init(treeNode << 1 | 1, mid + 1, e);
+		
 		tree[treeNode] = tree[treeNode << 1] | tree[treeNode << 1 | 1];
 	}
-	public static long query(int treeNode, int s, int e, int left, int right) {
-        propagate(treeNode, s, e);
+	public static int query(int treeNode, int s, int e, int left, int right) {
+		
+		propagate(treeNode, s, e);
+		
 		if(e < left || right < s)
 			return 0;
+		
 		if(left <= s && e <= right)
 			return tree[treeNode];
 		
@@ -69,11 +68,12 @@ class Main{
 		return query(treeNode << 1, s, mid, left, right)
 				| query(treeNode << 1 | 1, mid + 1, e, left , right);
 	}
-	public static void update(int treeNode, int s, int e, int left, int right, long value) {
+	public static void update(int treeNode, int s, int e, int left, int right, int value) {
 		propagate(treeNode, s, e);
 		
 		if(e < left || right < s)
 			return;
+		
 		if(left<=s && e<=right)
 		{
 			lazy[treeNode] = value;
@@ -101,3 +101,40 @@ class Main{
 		}
 	}
 }
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
+
