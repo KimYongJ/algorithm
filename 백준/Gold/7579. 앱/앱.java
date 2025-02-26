2d0d9cc02918be7b82cf3cbd47923d168c2a8b97
YongJu Kim <kyjdummy@gmail.com>
2025-02-26 21:02:26 +0900

    [Gold III] Title: 앱, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,26 +1,18 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/7579
 //1초 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N		= Integer.parseInt(st.nextToken());//실행앱개수(1<=100)
-		int M		= Integer.parseInt(st.nextToken());// 확보할 메모리 양(1<=천만)
+		int N		= read();//실행앱개수(1<=100)
+		int M		= read();// 확보할 메모리 양(1<=천만)
 		int byt[]	= new int[N + 1];
 		int cost[]	= new int[N + 1];
 		int total	= 0;
-		
-		st = new StringTokenizer(br.readLine());
+
 		for(int i=1; i<=N; i++)
-			byt[i] = Integer.parseInt(st.nextToken());// 사용중인 메모리의 바이트 수(1<=천만)
-		st = new StringTokenizer(br.readLine());
+			byt[i] = read();// 사용중인 메모리의 바이트 수(1<=천만)
 		for(int i=1; i<=N; i++)
-			total += cost[i] = Integer.parseInt(st.nextToken());// 비활성화시 드는 비용(0<=100)
+			total += cost[i] = read();// 비활성화시 드는 비용(0<=100)
 		
 		int dp[] = new int[total + 1];
 		
@@ -34,6 +26,10 @@ class Main{
 				System.out.print(i);
 				return;
 			}
-		
+	}
+	static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+		return n;
 	}
 }
