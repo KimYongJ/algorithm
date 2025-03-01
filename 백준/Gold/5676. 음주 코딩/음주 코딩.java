dba32d68ede311b9dfced44be81bef9e36dbc095
YongJu Kim <kyjdummy@gmail.com>
2025-03-01 09:28:57 +0900

    [Gold I] Title: 음주 코딩, Time: 632 ms, Memory: 100804 KB -BaekjoonHub

--- 백준/Gold/5676. 음주 코딩/음주 코딩.java
+++ 백준/Gold/5676. 음주 코딩/음주 코딩.java
@@ -4,7 +4,6 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main{
@@ -27,8 +26,6 @@ class Main{
 			K = Integer.parseInt(st.nextToken());//1<=십만
 			arr = new int[N + 1];
 			tree= new int[N << 2];
-			
-			Arrays.fill(tree, 1);
 			
 			st = new StringTokenizer(br.readLine());
 			for(int i=1; i<=N;i++)
