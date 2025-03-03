6d8740d378be78f5c796f21224ef1173b9928220
YongJu Kim <kyjdummy@gmail.com>
2025-03-03 11:48:58 +0900

    [Gold II] Title: 할로윈의 양아치, Time: 240 ms, Memory: 21100 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -78,3 +78,60 @@ class Main{
 		return n;
 	}
 }
+////이하 유니온 파인드 방식
+//class Main{
+//
+//	static int[] parent;
+//	public static void main(String[] args)throws Exception{
+//		int N			= read();// 거리에 있는 아이들 수(1<=삼만)
+//		int M			= read();// 아이들의 친구 관계수(0<=십만)
+//		int K			= read()-1;// 뺏을 수 있는 아이수(1<=삼천)
+//		int[] candy		= new int[N + 1];
+//		int[] childCnt	= new int[N + 1];
+//		int[] candySum	= new int[N + 1];
+//		parent			= new int[N + 1];
+//		
+//		for(int i=1; i<=N; i++)
+//		{
+//			candy[i] = read();
+//			parent[i] = i;
+//		}
+//		
+//		while(M-->0)
+//		{
+//			int parentA = getParent(read());
+//			int parentB = getParent(read());
+//			
+//			if(parentA < parentB)
+//				parent[parentB] = parentA;
+//			else
+//				parent[parentA] = parentB;
+//		}
+//		
+//		for(int i=1; i<=N; i++)
+//		{
+//			int parentNum = getParent(i);
+//			candySum[parentNum] += candy[i];
+//			childCnt[parentNum]++;
+//		}
+//		
+//		int dp[] = new int[K + 1];
+//		
+//		for(int i=1; i<=N; i++)
+//			if(candySum[i] != 0)
+//			{
+//				for(int j=K; j>=childCnt[i]; j--)
+//					dp[j] = Math.max(dp[j], dp[j-childCnt[i]] + candySum[i]);
+//			}
+//		
+//		System.out.print(dp[K]);
+//	}
+//	static int getParent(int node) {
+//		return parent[node] == node ? node :(parent[node] = getParent(parent[node]));
+//	}
+//	static int read() throws Exception {
+//		int c, n = System.in.read() & 15;
+//		while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+//		return n;
+//	}
+//}
