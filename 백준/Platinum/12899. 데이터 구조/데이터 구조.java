39abf1dd5ebe7815d7addfaf5111163109ec45e0
YongJu Kim <kyjdummy@gmail.com>
2025-03-14 22:45:08 +0900

    [Platinum IV] Title: 데이터 구조, Time: 1444 ms, Memory: 334632 KB -BaekjoonHub

---
+++ 백준/Platinum/12899. 데이터 구조/데이터 구조.java
@@ -0,0 +1,60 @@
+//https://github.com/kimyongj/algorithm
+//https://www.acmicpc.net/problem/12899
+//2초 / 512MB
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static final int LEN = 2_000_000;
+	static int N;
+	static int[] tree;
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		tree = new int[LEN * 4];
+		while(N-->0)
+		{
+			st = new StringTokenizer(br.readLine());
+			int T = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			if(T==1)
+				update(1, 1, LEN, X);
+			else
+				sb.append( query(1, 1, LEN, X) )
+					.append('\n');
+		}
+		System.out.print(sb);
+	}
+	public static int query(int treeNode, int s, int e, int cnt) {
+		tree[treeNode]--;
+		
+		if(s == e)
+			return s;
+		
+		int mid = (s + e) >> 1;
+		int next = treeNode << 1;
+		if(tree[next] >= cnt)
+			return query(next, s, mid, cnt);
+		else
+			return query(next | 1, mid + 1, e, cnt - tree[next]);
+	}
+	public static void update(int treeNode, int s, int e, int idx) {
+		if(idx < s || e < idx)
+			return;
+		if(s == e)
+		{
+			tree[treeNode]++;
+			return;
+		}
+		
+		int mid = (s + e) >> 1;
+		update(treeNode << 1, s, mid, idx);
+		update(treeNode << 1 | 1, mid + 1, e, idx);
+		tree[treeNode] = tree[treeNode << 1] + tree[treeNode << 1 | 1];
+	}
+}
