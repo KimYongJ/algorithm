09ea8188a6db18bc608b9144d1ed67f745e97fb9
YongJu Kim <kyjdummy@gmail.com>
2025-03-21 22:59:00 +0900

    [Gold II] Title: 동전 분배, Time: 268 ms, Memory: 76000 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -3,6 +3,7 @@
 //2초 128MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main{
@@ -13,10 +14,10 @@ class Main{
 		while(T-->0)
 		{
 			int N = Integer.parseInt(br.readLine());// 동전의 종류(1<=100)
-			int value[] = new int[N + 1];
-			int count[] = new int[N + 1];
+			int value[] = new int[N];
+			int count[] = new int[N];
 			int sum		= 0;
-			for(int i=1; i<=N; i++)
+			for(int i=0; i<N; i++)
 			{
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				value[i] = Integer.parseInt(st.nextToken());// 동전금액 , 금액의 총합은 십만원을 넘지 않음
@@ -31,29 +32,29 @@ class Main{
 			
 			sum /= 2;
 			
-			boolean dp[] = new boolean[sum + 1];// 동전을 순차적으로 사용해서 dp[i]원을 만들 수 있는지를 담는다.
-			dp[0] = true;						// 0원은 무조건 만들 수 있음
-			for(int i=1; i<=N; i++)				// 동전종류를 순차적으로 반복
+			int dp[][] = new int[N + 1][sum+1]; // 특정 금액을 만들 때 사용한 동전의 최소개수를 담는 dp
+			// 모든 갑을 큰수로 세팅
+			for(int i=0; i<=N; i++)
+				Arrays.fill(dp[i], 1<<30);
+			
+			dp[0][0] = 0;// 0원을 만드는데 사용된 동전의 개수 0개
+			
+			for(int i=0; i<N; i++)			// 종류별로 순회
 			{
-				for(int j=sum; j>=value[i]; j--)// 현재가 미래에 영향을 안줘야 하기 때문에 뒤에서부터 탐색
-					if(dp[j - value[i]])		// 가지 치기를 위해 동전을 1개이상 사용해서 만들 수 있을 때만 이하 반복문 실행
-					{
-						int val = j;
-						int c = 0;
-						while(++c <= count[i] && val <= sum)
-						{
-							dp[val] = true;
-							val += value[i];
-						}
-//						// 동전개수만큼 반복
-//						for(int c=1; c<=count[i]; c++)
-//							// j-value[i] 원이 만들 수 있기 때문에, 이제 코인을 사용해 그이상 만들 수 있는지 계속 탐색
-//							if(j - value[i] + c * value[i] <= sum)
-//								dp[j - value[i] + c * value[i]] = true;
-					}
+				for(int j=0; j<=sum; j++)	// j금액부터 sum금액까지 순회
+				{
+					if(dp[i][j] == 1<<30)	// 해당값을 만들지 못하는 경우 스킵, 현재를 통해 미래를 만들어나가는 것이기 때문에
+						continue;
+					// 만드려는 금액이 sum 이하고, 지금까지 사용한 동전이 쓸 수있는 count[i]개 미만이면
+					if(j + value[i] <= sum && dp[i][j] < count[i])
+						//i번째 코인을 사용해 j+value[i]금액을 만들 때 동전개수를 세팅
+						dp[i][j + value[i]] = Math.min(dp[i][j + value[i]], dp[i][j] + 1);
+					// 다음 연산을 위해 자기 바로 다음 인덱스를 0으로 세팅, 이 의미는 다음 동전이 j금액일 때 자기는 동전을 하나도 안써도 그금액을 만들 수 있음을 의미
+					dp[i+1][j] = 0;
+				}
 			}
-			sb.append(dp[sum] ? 1 : 0)
-				.append('\n');
+			
+			sb.append(dp[N][sum] != 1<<30 ? 1 : 0).append('\n');
 		}
 		System.out.print(sb);
 	}
