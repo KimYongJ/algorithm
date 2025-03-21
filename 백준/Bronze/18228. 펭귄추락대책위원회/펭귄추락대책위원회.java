c3ac8e6af7bb9ac17c632179fc0dfbf3c0c456ae
YongJu Kim <kyjdummy@gmail.com>
2025-03-22 08:34:35 +0900

    [Bronze II] Title: 펭귄추락대책위원회, Time: 356 ms, Memory: 38832 KB -BaekjoonHub

--- 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.java
+++ 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.java
@@ -3,33 +3,32 @@
 //2초 512MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int N	= Integer.parseInt(br.readLine());
+		int min1= 1<<30;
+		int min2= 1<<30;
+		boolean flag = true;
 		
-		int N = Integer.parseInt(br.readLine());
-		int res = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++) {
+		for(int i=1; i<=N; i++)
+		{
 			int now = Integer.parseInt(st.nextToken());
+			
 			if(now < 0)
 			{
-				if(!pq.isEmpty())
-					res += pq.peek();
-				pq.clear();
+				flag = !flag;
 				continue;
 			}
-			pq.add(now);
 			
-			if(i == N && !pq.isEmpty()) {
-				res += pq.peek();
-			}
+			if(flag)
+				min1 = Math.min(min1, now);
+			else
+				min2 = Math.min(min2, now);
 		}
-		System.out.print(res);
+		System.out.print(min1 + min2);
 	}
-
 }
