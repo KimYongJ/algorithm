f832da0d76854d218736e04e8da2827235a20f21
YongJu Kim <kyjdummy@gmail.com>
2025-03-23 12:20:32 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 276 ms, Memory: 20632 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,18 +1,17 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/1197
 //1초 128MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 class Node implements Comparable<Node>{
 	int a,b,c;
 	Node(int a, int b, int c){
-		this.a=a; this.b=b; this.c=c;
+		this.a=a; 
+		this.b=b; 
+		this.c=c;
 	}
 	@Override
 	public int compareTo(Node o) {
-		return c - o.c;
+		return this.c - o.c;
 	}
 }
 class Main{
@@ -20,32 +19,25 @@ class Main{
 	static int[] parent;
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());	// 1<=만
-		int E = Integer.parseInt(st.nextToken());	// 1<=십만
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		int result = 0;
+		int V		= read();	// 1<=만
+		int E		= read();	// 1<=십만
+		int result	= 0;
 		
 		parent = new int[V + 1];
 		for(int i=1; i<=V; i++)
 			parent[i] = i;
 		
 		for(int i=0; i<E; i++)
-		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			pq.add(new Node(a,b,c));
-		}
+			pq.add(new Node(read(), read(), read()));
 		
 		while(!pq.isEmpty())
 		{
 			Node now = pq.poll();
 			int p1 = getParent(parent[now.a]);
 			int p2 = getParent(parent[now.b]);
-			if(p1 != p2) {
+			if(p1 != p2)
+			{
 				result += now.c;
 				if(p1 < p2)
 					parent[p2] = p1;
@@ -56,7 +48,16 @@ class Main{
 		System.out.print(result);
 	}
 	public static int getParent(int node) {
-		return parent[node] = parent[node] == node ? 
-				node : getParent(parent[node]);
+		if(parent[node] == node)
+			return node;
+		return parent[node] = getParent(parent[node]);
 	}
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean m = n == 13;
+        if (m)n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) {
+        n = (n << 3) + (n << 1) + (c & 15);}
+        return m ? ~n + 1 : n;
+    }
 }
