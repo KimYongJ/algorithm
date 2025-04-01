1f2b046ba6d88c4796f32840d3687ce0174f533b
YongJu Kim <kyjdummy@gmail.com>
2025-04-01 23:07:56 +0900

    [Gold I] Title: 크롬, Time: 224 ms, Memory: 18988 KB -BaekjoonHub

--- 백준/Gold/20667. 크롬/크롬.java
+++ 백준/Gold/20667. 크롬/크롬.java
@@ -18,43 +18,49 @@ class Main{
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N		= Integer.parseInt(st.nextToken());// 크롭탭수(1<=100)
 		int C		= Integer.parseInt(st.nextToken());// 목표CPU사용량(1<=천
+		// M에 1을 더하는 이유는 dp에서 유효한 값을 확인하기 위해 +1로 시작하기 때문에 최종 확인할 M도 +1을 하는것
 		int M		= Integer.parseInt(st.nextToken()) + 1;// 목표메모리할당량(1<=십만)
 		int psum	= 0;
 		Node node[] = new Node[N];
 		
 		for(int i=0; i<N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int c = Integer.parseInt(st.nextToken());// CPU(1<=목표CPU할당량)
-			int m = Integer.parseInt(st.nextToken());// 메모리(1<=목표메모리할당량)
-			int p = Integer.parseInt(st.nextToken());// 우선순위(1<=5)
+			st		= new StringTokenizer(br.readLine());
+			int c	= Integer.parseInt(st.nextToken());// CPU(1<=목표CPU할당량)
+			int m	= Integer.parseInt(st.nextToken());// 메모리(1<=목표메모리할당량)
+			int p	= Integer.parseInt(st.nextToken());// 우선순위(1<=5)
 			node[i] = new Node(c,m,p);
-			psum += p;
+			psum	+= p;
 		}
-		// cpu 사용량 : 우선순위 = 메모리할당량의 최대
+		
+		// dp[cpu 사용량][우선순위] = 메모리 할당량의 최대
 		int dp[][] = new int[C + 1][psum + 1];
-		dp[0][0] = 1;
+		
+		dp[0][0] = 1;// dp[0][0]에 1을 넣어 유효한 값임을 표현
+		
 		for(Node now : node)
 		{
-			for(int c=C; c>=0; c--)
-				for(int p=psum; p>=0; p--)
+			for(int c=C; c>=0; c--)			// 역순으로 하는 이유는 현재의 수정이 미래에 영향을 주지 않기 위해
+			{
+				for(int p=psum; p>=0; p--)	// 역순으로 하는 이유는 현재의 수정이 미래에 영향을 주지 않기 위해
 				{
 					int nextC = Math.min(C,c + now.cpu);
 					int nextP = p + now.priority;
-					if(dp[c][p] != 0)
-					{
-						dp[nextC][nextP] = Math.max(dp[nextC][nextP], dp[c][p] + now.memory);
-					}		
+					if(dp[c][p] != 0) // 유효한 값일 때만 현재를 통해 다음을 갱신
+						dp[nextC][nextP] = Math.max(dp[nextC][nextP], dp[c][p] + now.memory);	
 				}
+			}
 		}
 		
 		for(int p = 1; p<=psum; p++)
+		{
 			if(dp[C][p] >= M)
 			{
 				System.out.print(p);
 				return;
 			}
-			
+		}
+
 		System.out.print(-1);
 	}
 }
