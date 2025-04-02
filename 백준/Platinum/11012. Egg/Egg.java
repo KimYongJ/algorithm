476ec8e96c2ffed8b567570ae4d32dbd18e4afda
YongJu Kim <kyjdummy@gmail.com>
2025-04-02 21:44:14 +0900

    [Platinum II] Title: Egg, Time: 2364 ms, Memory: 236304 KB -BaekjoonHub

--- 백준/Platinum/11012. Egg/Egg.java
+++ 백준/Platinum/11012. Egg/Egg.java
@@ -1,10 +1,7 @@
 //https://github.com/kimyongj/algorithm
 //https://www.acmicpc.net/problem/11012
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 class Point implements Comparable<Point>{
 	int x, y;
 	Point(int x, int y){
@@ -26,6 +23,66 @@ class Main{
 	static final int LEN = 100_001;
 	static ArrayList<Node> nodes;
 	
+	public static void main(String[] args)throws Exception{
+		StringBuilder sb = new StringBuilder();
+		Reader in = new Reader();
+		int T = in.nextInt();//테스트케이스 수(1<=20)
+		while(T-->0)
+		{
+			int N = in.nextInt();// 좌표수
+			int M = in.nextInt();// 쿼리수
+			Point point[] = new Point[N];
+			int roots[] = new int[LEN + 1];
+			nodes = new ArrayList<>();
+			
+			for(int i=0; i<N; i++)
+			{
+				int x	= in.nextInt() + 1;// 좌표의수N(0<=10,000)
+				int y	= in.nextInt() + 1;// 쿼리수M(0<=50,000)
+				point[i]= new Point(x,y);
+			}
+			// 초기 세그먼트 트리 생성, roots[0]에는 초기 세그먼트트리의 root번호, 즉 nodes의 사이즈가 들어간다.
+			roots[0] = init(1, LEN);
+			
+			Arrays.sort(point);
+			
+			int prevY = 0;
+			for(Point p : point)
+			{
+				for(int y=prevY; y<=p.y; y++)
+				{
+					// 이전 탐색한 y좌표 부터 현재 파악하려는 y좌표까지 복사를 진행한다.
+					// 이로써 쿼리에서 어떤 y좌표가 입력되어도 그 때의 세그먼트트리 루트노드를 찾아갈 수 있다.
+					roots[y] = roots[prevY]; 
+				}
+				// 복사된 루트노드(root[p.y])를 시작으로 x의 인덱스를 마킹하고 새로운 루트 노드를 재저장한다.
+				// p.y가 같은 값이 여러개 입력되어도, 결국 roots[p.y]에는 마지막으로 x좌표를 업데이트한 루트노드번호만 저장됨
+				roots[p.y] = update(roots[p.y], 1, LEN, p.x);
+				
+				prevY = p.y;
+			}
+			// 남은 y좌표를 끝까지 update한다.
+			for(int y=prevY; y<=LEN; y++)
+				roots[y] = roots[prevY];
+			
+			int result = 0;
+			
+			while(M-->0)
+			{
+				// l, r, b, t가 주어짐 (0<=10의5승), l과r은 x좌표, b,t는 y좌표
+				int l = in.nextInt() + 1;
+				int r = in.nextInt() + 1;
+				int b = in.nextInt() + 1;
+				int t = in.nextInt() + 1;
+				
+				result += sum(roots[t], 1, LEN, l, r)
+								- sum(roots[b - 1], 1, LEN, l, r);
+			}
+			sb.append(result)
+				.append('\n');
+		}
+		System.out.print(sb);
+	}
 	public static int init(int s, int e) {
 		if(s == e)	// 리프노드인 경우 노드를 추가하고 해당 노드 번호를 반환하고 종료
 		{
@@ -75,68 +132,6 @@ class Main{
 		return sum(nodes.get(nowNode).left, s, mid, left, right)
 				+ sum(nodes.get(nowNode).right, mid + 1, e, left, right);
 	}
-	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());//테스트케이스 수(1<=20)
-		while(T-->0)
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());// 좌표수
-			int M = Integer.parseInt(st.nextToken());// 쿼리수
-			Point point[] = new Point[N];
-			int roots[] = new int[LEN + 1];
-			nodes = new ArrayList<>();
-			
-			for(int i=0; i<N; i++)
-			{
-				st = new StringTokenizer(br.readLine());
-				int x	= Integer.parseInt(st.nextToken()) + 1;// 좌표의수N(0<=10,000)
-				int y	= Integer.parseInt(st.nextToken()) + 1;// 쿼리수M(0<=50,000)
-				point[i]= new Point(x,y);
-			}
-			// 초기 세그먼트 트리 생성, roots[0]에는 초기 세그먼트트리의 root번호, 즉 nodes의 사이즈가 들어간다.
-			roots[0] = init(1, LEN);
-			
-			Arrays.sort(point);
-			
-			int prevY = 0;
-			for(Point p : point)
-			{
-				for(int y=prevY; y<=p.y; y++)
-				{
-					// 이전 탐색한 y좌표 부터 현재 파악하려는 y좌표까지 복사를 진행한다.
-					// 이로써 쿼리에서 어떤 y좌표가 입력되어도 그 때의 세그먼트트리 루트노드를 찾아갈 수 있다.
-					roots[y] = roots[prevY]; 
-				}
-				// 복사된 루트노드(root[p.y])를 시작으로 x의 인덱스를 마킹하고 새로운 루트 노드를 재저장한다.
-				// p.y가 같은 값이 여러개 입력되어도, 결국 roots[p.y]에는 마지막으로 x좌표를 업데이트한 루트노드번호만 저장됨
-				roots[p.y] = update(roots[p.y], 1, LEN, p.x);
-				
-				prevY = p.y;
-			}
-			// 남은 y좌표를 끝까지 update한다.
-			for(int y=prevY; y<=LEN; y++)
-				roots[y] = roots[prevY];
-			
-			int result = 0;
-			while(M-->0)
-			{
-				st = new StringTokenizer(br.readLine());
-				// l, r, b, t가 주어짐 (0<=10의5승), l과r은 x좌표, b,t는 y좌표
-				int l = Integer.parseInt(st.nextToken()) + 1;
-				int r = Integer.parseInt(st.nextToken()) + 1;
-				int b = Integer.parseInt(st.nextToken()) + 1;
-				int t = Integer.parseInt(st.nextToken()) + 1;
-				
-				result += sum(roots[t], 1, LEN, l, r)
-								- sum(roots[b - 1], 1, LEN, l, r);
-			}
-			sb.append(result)
-				.append('\n');
-		}
-		System.out.print(sb);
-	}
 }
 //1				// 테스트케이스 수(1<=20)
 //3 1			// 좌표의수N(0<=10,000), 쿼리수M(0<=50,000)
@@ -145,3 +140,32 @@ class Main{
 //1 1
 //1 2 1 3		// M개의 줄에 l, r, b, t가 주어짐 (0<=10의5승), l과r은 x좌표, b,t는 y좌표
 ////답 : 2
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
