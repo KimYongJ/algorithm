7bb5c75f08366306c333aa191363a3fad9779577
YongJu Kim <kyjdummy@gmail.com>
2025-04-12 08:42:23 +0900

    [Gold IV] Title: 정복자, Time: 172 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Gold/14950. 정복자/정복자.java
+++ 백준/Gold/14950. 정복자/정복자.java
@@ -2,19 +2,15 @@
 //https://www.acmicpc.net/problem/14950
 //2초 256MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());// 도시 수 1<=만
-		int M = Integer.parseInt(st.nextToken());// 도로 개수 1<=만
-		int T = Integer.parseInt(st.nextToken());// 증가 비용 1<=100
-		int parent[] = new int[N + 1];
+		Reader in	= new Reader();
+		int N		= in.nextInt();// 도시 수 1<=만
+		int M		= in.nextInt();// 도로 개수 1<=만
+		int T		= in.nextInt();// 증가 비용 1<=100
+		int parent[]= new int[N + 1];
 		
 		for(int i=1; i<=N; i++)
 			parent[i] = i;
@@ -22,21 +18,23 @@ class Main{
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());// 노드 번호
-			int b = Integer.parseInt(st.nextToken());// 노드 번호
-			int cost = Integer.parseInt(st.nextToken());// 비용 1<=만
+			int a		= in.nextInt();// 노드 번호
+			int b		= in.nextInt();// 노드 번호
+			int cost	= in.nextInt();// 비용 1<=만
 			pq.add(new Node(a,b,cost));
 		}
 		
 		int cnt = N - 1;
-		int ans = 0;
-		int plus = 0;
+		int ans	= 0;
+		int plus= 0;
+		
 		while(cnt != 0)
 		{
 			Node now = pq.poll();
+			
 			int p1 = getParent(now.a, parent);
 			int p2 = getParent(now.b, parent);
+			
 			if(p1 != p2)
 			{
 				if(p1 < p2)
@@ -44,8 +42,11 @@ class Main{
 				else
 					parent[p1] = p2;
 				
+				
 				ans += now.cost + plus;
+				
 				cnt -= 1;
+				
 				plus+= T;
 			}
 		}
@@ -69,4 +70,34 @@ class Node implements Comparable<Node>{
 	public int compareTo(Node o) {
 		return cost - o.cost;
 	}
+}
+
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
