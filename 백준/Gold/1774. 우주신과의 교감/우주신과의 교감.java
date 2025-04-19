147d3dfdf7ff20e3561825691d347890d01cd7f3
YongJu Kim <kyjdummy@gmail.com>
2025-04-19 09:54:34 +0900

    [Gold III] Title: 우주신과의 교감, Time: 200 ms, Memory: 37288 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -2,30 +2,14 @@
 //https://www.acmicpc.net/problem/1774
 //2초 128MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-class Node implements Comparable<Node>{
-	int a,b;
-	double dist;
-	Node(int a, int b, double dist){
-		this.a=a;
-		this.b=b;
-		this.dist=dist;
-	}
-	@Override
-	public int compareTo(Node o) {
-		return Double.compare(dist , o.dist);
-	}
-}
+
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		Reader in = new Reader();
 		
-		int N			= Integer.parseInt(st.nextToken());	// 1<=1000
-		int M			= Integer.parseInt(st.nextToken());	// 1<=1000
+		int N			= in.nextInt();	// 1<=1000
+		int M			= in.nextInt();	// 1<=1000
 		int cnt			= N - 1;
 		int parent[]	= new int[N + 1];
 		int data[][]	= new int[N + 1][2];
@@ -33,18 +17,16 @@ class Main{
 		
 		for(int i=1; i<=N; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			data[i][0] = Integer.parseInt(st.nextToken());
-			data[i][1] = Integer.parseInt(st.nextToken());
+			data[i][0] = in.nextInt();
+			data[i][1] = in.nextInt();
 			
 			parent[i] = i;
 		}
 		
 		for(int i=0; i<M; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int parent1 = getParent(Integer.parseInt(st.nextToken()), parent);
-			int parent2 = getParent(Integer.parseInt(st.nextToken()), parent);
+			int parent1 = getParent(in.nextInt(), parent);
+			int parent2 = getParent(in.nextInt(), parent);
 			
 			if(parent1 != parent2)
 			{
@@ -97,4 +79,47 @@ class Main{
 		
 		return parent[node] = getParent(parent[node], parent);
 	}
+}
+
+class Node implements Comparable<Node>{
+	int a,b;
+	double dist;
+	Node(int a, int b, double dist){
+		this.a=a;
+		this.b=b;
+		this.dist=dist;
+	}
+	@Override
+	public int compareTo(Node o) {
+		return Double.compare(dist , o.dist);
+	}
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
