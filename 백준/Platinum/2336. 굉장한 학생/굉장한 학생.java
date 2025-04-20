c307b72707f6473298120c0b000f276bccf4e0cd
YongJu Kim <kyjdummy@gmail.com>
2025-04-20 19:08:49 +0900

    [Platinum II] Title: 굉장한 학생, Time: 992 ms, Memory: 151256 KB -BaekjoonHub

--- 백준/Platinum/2336. 굉장한 학생/굉장한 학생.java
+++ 백준/Platinum/2336. 굉장한 학생/굉장한 학생.java
@@ -10,60 +10,53 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static final int MAX = 1<<30;
+	static int[] tree;
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N		= Integer.parseInt(br.readLine());	// 1 ≤ N ≤ 500,000
+		Node[] node = new Node[N + 1];
+		tree		= new int[N * 4];
 		
-		int N		= Integer.parseInt(br.readLine());
-		Node []node = new Node[N + 1];
-		int []tree	= new int[N * 4];
+		node[0]	= new Node(MAX,MAX,MAX);
 		
-		node[0] = new Node(MAX,MAX,MAX);
+		for(int i=1; i<=N; i++)
+			node[i] = new Node(0,0,0);
 		
-		Arrays.fill(tree, MAX);
-		
-		for(int i=0; i<3; i++)		// 시험 3개 반복
+		for(int i=0; i<3; i++)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=N; j++)	// 등수
+			for(int rnk=1; rnk<=N; rnk++)
 			{
-				int rnk = Integer.parseInt(st.nextToken());
-				
-				if(node[rnk] == null)
-					node[rnk] = new Node(0,0,0);
-				
+				int index = Integer.parseInt(st.nextToken());// 해당 학생의 번호
+
 				if(i == 0)
-				{
-					node[rnk].rank1 = j;
-				}
+					node[index].rank1 = rnk;
 				else if(i == 1)
-				{
-					node[rnk].rank2 = j;
-				}
+					node[index].rank2 = rnk;
 				else
-				{
-					node[rnk].rank3 = j;
-				}
+					node[index].rank3 = rnk;
 			}
 		}
 		
+		Arrays.fill(tree, MAX);
 		Arrays.sort(node);
 		
 		int ans = 0;
 		
 		for(Node now : node)
 		{
-			int min = query( 1, 1, N, 1, now.rank2, tree );
+			int min = query( 1, 1, N, 1, now.rank2 );
 			
 			if(min > now.rank3)
 				++ans;
 			
-			update( 1, 1, N, now.rank2, now.rank3, tree );
+			update( 1, 1, N, now.rank2, now.rank3 );
 		}
 		
 		System.out.print(ans);
 	}
-	public static void update(int treeNode, int s, int e, int targetIdx, int value, int[] tree) {
+	public static void update(int treeNode, int s, int e, int targetIdx, int value) {
 		if(e < targetIdx || targetIdx < s)
 			return;
 		
@@ -75,27 +68,26 @@ class Main{
 		
 		int mid = (s + e) >> 1;
 		
-		update(treeNode << 1, s, mid, targetIdx, value, tree);
-		update(treeNode << 1 | 1, mid + 1, e, targetIdx, value, tree);
+		update(treeNode << 1, s, mid, targetIdx, value);
+		update(treeNode << 1 | 1, mid + 1, e, targetIdx, value);
 		
 		tree[treeNode] = Math.min(tree[treeNode << 1], tree[treeNode << 1 | 1]);
 	}
-	public static int query(int treeNode, int s, int e, int left, int right, int[] tree) {
-		
+	public static int query(int treeNode, int s, int e, int left, int right) {
 		if(e < left || right < s)
 			return MAX;
 		
-		if(left<=s && e<= right)
+		if(left<=s && e<=right)
 			return tree[treeNode];
 		
 		int mid = (s + e) >> 1;
-		
-		int l = query(treeNode << 1, s, mid, left, right, tree);
-		int r = query(treeNode << 1 | 1, mid + 1, e, left, right, tree);
+		int l	= query(treeNode << 1, s, mid, left, right);
+		int r	= query(treeNode << 1 | 1, mid + 1, e, left, right);
 		
 		return Math.min(l, r);
 	}
 }
+
 class Node implements Comparable<Node>{
 	int rank1, rank2, rank3;
 	Node(int r1, int r2, int r3){
