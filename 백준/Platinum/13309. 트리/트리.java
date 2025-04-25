5feb531e0d2156abf0ca2e0862e9dd0dbff0a04c
YongJu Kim <kyjdummy@gmail.com>
2025-04-25 18:57:31 +0900

    [Platinum I] Title: 트리, Time: 2764 ms, Memory:  140656 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13309. 트리/트리.java
+++ 백준/Platinum/13309. 트리/트리.java
@@ -10,101 +10,26 @@ class Main{
 
 	static int N, Q;
 	static int idx;
-	static int[] tree;
 	static int[] treeIdx;
 	static int[] chainHead;
 	static int[] chainLevel;
 	static int[] chainParent;
-	static boolean[] visit;
+	static boolean[] tree;
 	static ArrayList<Integer>[] adNode;
-	
-	static int query(int treeNode, int s, int e, int left, int right) {
-		if(e < left || right < s)
-			return 0;
-		if(left<=s && e<=right)
-			return tree[treeNode];
-		
-		int mid = (s + e) >> 1;
-		return query(treeNode << 1, s, mid, left, right)
-				+ query(treeNode << 1 | 1,mid + 1, e, left, right);
-	}
-	static void update(int treeNode, int s, int e, int targetIdx, int value) {
-		if(e < targetIdx || targetIdx < s)
-			return;
-		if(s == e) {
-			tree[treeNode] = value;
-			return;
-		}
-		int mid = (s + e) >> 1;
-		update(treeNode << 1, s, mid, targetIdx, value);
-		update(treeNode << 1 | 1, mid + 1, e, targetIdx, value);
-		
-		tree[treeNode] = tree[treeNode << 1] + tree[treeNode << 1 | 1];
-	}
-	static void setHLD(int nowNode, int level) {
-		treeIdx[nowNode] = ++idx;
-		chainLevel[nowNode] = level;
-		for(int i=0; i<adNode[nowNode].size(); i++)
-		{
-			int child = adNode[nowNode].get(i);
-			
-			if(visit[child])
-				continue;
-			
-			visit[child] = true;
-			
-			if(i == 0)	// heavy인 경우, 체인 그대로 유지
-			{
-				chainHead[child] = chainHead[nowNode];	// heavy는 부모의 값을 그대로 저장
-				chainParent[child] = chainParent[nowNode];// heavy는 부모의 값을 그대로 저장
-				setHLD(child, level);
-			}
-			else		// light인 경우, 새로운 체인 시작
-			{
-				chainHead[child] = child;		// head는 자기자신
-				chainParent[child] = nowNode;	// 다음 점프할 노드는 자기의 부모노드로 세팅
-				setHLD(child, level + 1);		// 레벨을 늘려 새로운 체인 시작
-			}
-			update(1, 1, N, treeIdx[child], 1);// 1번 노드는 부모가 없으므로 간선 값을 0으로 세팅
-		}
-	}
-	static void moveHeavyChildToFront(int nowNode, int[] size) {
-		int heavySize = 0;
-		int heavyIdx = 0;
-		size[nowNode] = 1;
-		for(int i=0; i<adNode[nowNode].size(); i++)
-		{
-			int child = adNode[nowNode].get(i);
-			
-			if(visit[child])
-				continue;
-			
-			visit[child] = true;
-			
-			moveHeavyChildToFront(child, size);
-			size[nowNode] += size[child];
-			if(heavySize < size[child])
-			{
-				heavySize = size[child];
-				heavyIdx = i;
-			}
-		}
-		if(adNode[nowNode].size() > 0)
-			Collections.swap(adNode[nowNode], 0, heavyIdx);
-	}
+
 	public static void main(String[] ags)throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder	sb = new StringBuilder();
 		
-		N = Integer.parseInt(st.nextToken());// 트리의 정점개수(1 ≤ 200,000)
-		Q = Integer.parseInt(st.nextToken());// 질의 개수(1 ≤ 200,000)
-		tree = new int[N * 4];
-		treeIdx = new int[N + 1];
-		chainHead = new int[N + 1];
-		chainLevel = new int[N + 1];
-		chainParent = new int[N + 1];
-		adNode = new ArrayList[N + 1];
+		N			= Integer.parseInt(st.nextToken());// 트리의 정점개수(1 ≤ 200,000)
+		Q			= Integer.parseInt(st.nextToken());// 질의 개수(1 ≤ 200,000)
+		tree		= new boolean[N * 4]; // 초기값 다 false로, 연결시 false
+		treeIdx		= new int[N + 1];
+		chainHead	= new int[N + 1];
+		chainLevel	= new int[N + 1];
+		chainParent	= new int[N + 1];
+		adNode		= new ArrayList[N + 1];
 		
 		for(int i=0; i<=N; i++)
 			adNode[i] = new ArrayList<>();
@@ -113,19 +38,14 @@ class Main{
 		{
 			int parent = Integer.parseInt(br.readLine());
 			adNode[parent].add(child);
-			adNode[child].add(parent);
 		}
 		
 		// 해당 함수에서 HLD를 위한 무거운 자식 노드를 맨앞으로 옮긴다.
-		visit = new boolean[N + 1];
-		visit[1] = true;
-		moveHeavyChildToFront(1, new int[N + 1]);
+		moveHeavyChildToFront(1, 0, new int[N + 1]);
 		
 		chainHead[1] = 1;	// 1번노드의 헤드는 자기 자신
 		
-		visit = new boolean[N + 1];
-		visit[1] = true;
-		setHLD(1, 1);	// HLD 세팅
+		setHLD(1, 0, 1);	// HLD 세팅
 		
 		while(Q-->0)
 		{
@@ -146,27 +66,23 @@ class Main{
 			}
 			while(isPossible && chainLevel[node1] != chainLevel[node2])
 			{
-				int sum = query(1, 1, N, treeIdx[chainHead[node2]], treeIdx[node2]);
-				int ex = treeIdx[node2] - treeIdx[chainHead[node2]] + 1;// 예상 결과 값, 자기와 자기 부모노드 사이 간선 개수 카운팅
-				if(sum == ex)	// 노드가 연결되어있는게 맞다면
-					node2 = chainParent[node2];
+				boolean notConnected = query(1, 1, N, treeIdx[chainHead[node2]], treeIdx[node2]);
+				if(!notConnected)	// 노드가 연결되어있는게 맞다면
+					node2 = chainParent[node2];// 다음 체인으로 점프
 				else
 					isPossible = false;
 			}
 			// 같은 헤더까지 반복해서 둘다 레벨을 낮춘다.
 			while(isPossible && chainHead[node1] != chainHead[node2])
 			{
-				int sum = query(1, 1, N, treeIdx[chainHead[node1]], treeIdx[node1]);
-				int ex = treeIdx[node1] - treeIdx[chainHead[node1]] + 1;// 예상 결과 값, 자기와 자기 부모노드 사이 간선 개수 카운팅
-				if(sum == ex)	// 노드가 연결되어있는게 맞다면
-					node1 = chainParent[node1];
-				else
-					isPossible = false;
+				boolean notConnected1 = query(1, 1, N, treeIdx[chainHead[node1]], treeIdx[node1]);
+				boolean notConnected2 = query(1, 1, N, treeIdx[chainHead[node2]], treeIdx[node2]);
 				
-				int sum1 = query(1, 1, N, treeIdx[chainHead[node2]], treeIdx[node2]);
-				int ex1 = treeIdx[node2] - treeIdx[chainHead[node2]] + 1;// 예상 결과 값, 자기와 자기 부모노드 사이 간선 개수 카운팅
-				if(sum1 == ex1)	// 노드가 연결되어있는게 맞다면
+				if(!notConnected1 && !notConnected2)	// 노드가 연결되어있는게 맞다면, 다음 체인으로 점프
+				{
+					node1 = chainParent[node1];
 					node2 = chainParent[node2];
+				}
 				else
 					isPossible = false;
 			}
@@ -180,29 +96,111 @@ class Main{
 					node2 = tmp;
 				}
 				
-				int ex = treeIdx[node2] - treeIdx[node1];
-				int sum = query(1, 1, N, treeIdx[node1] + 1, treeIdx[node2]);
-				if(ex != sum)
+				boolean notConnected = query(1, 1, N, treeIdx[node1] + 1, treeIdx[node2]);
+				if(notConnected)
 					isPossible = false;
 			}
 			
 			if(d == 1)// d가 1이면
 			{
-				int deleteNode = 0;
+				int deleteNode = c;// b와 c를 연결하는 통로가 없으면 c의 부모정점과 c를 연결하는 엣지 제거
+				
 				if(isPossible)	// b와 c를 연결하는 통로가 있으면 b의 부모 정점과 b연결 엣지 제거
 				{
 					deleteNode = b;
 				}
-				else			// b와 c를 연결하는 통로가 없으면 c의 부모정점과 c를 연결하는 엣지 제거
-				{
-					deleteNode = c;
-				}
-				update(1, 1, N, treeIdx[deleteNode], 0);
+				
+				update(1, 1, N, treeIdx[deleteNode], true);
 			}
 			
 			sb.append(isPossible ? "YES\n" : "NO\n");
 		}
 		System.out.print(sb);
+	}
+
+	static boolean query(int treeNode, int s, int e, int left, int right) {
+		
+		if(e < left || right < s)
+			return false;
+
+		if(left<=s && e<=right)
+			return tree[treeNode];
+		
+		int mid = (s + e) >> 1;
+		
+		return query(treeNode << 1, s, mid, left, right)
+				| query(treeNode << 1 | 1,mid + 1, e, left, right);
+	}
+	static void update(int treeNode, int s, int e, int targetIdx, boolean value) {
+		
+		if(e < targetIdx || targetIdx < s)
+			return;
+		
+		if(s == e)
+		{
+			tree[treeNode] = value;
+			return;
+		}
+		
+		int mid = (s + e) >> 1;
+		
+		update(treeNode << 1, s, mid, targetIdx, value);
+		update(treeNode << 1 | 1, mid + 1, e, targetIdx, value);
+		
+		tree[treeNode] = tree[treeNode << 1] | tree[treeNode << 1 | 1];
+	}
+	static void setHLD(int nowNode, int parentNode, int level) {
+		
+		treeIdx[nowNode] = ++idx;
+		chainLevel[nowNode] = level;
+		
+		for(int i=0; i<adNode[nowNode].size(); i++)
+		{
+			int child = adNode[nowNode].get(i);
+			
+			if(child == parentNode)
+				continue;
+			
+			if(i == 0)	// heavy인 경우, 체인 그대로 유지
+			{
+				chainHead[child]	= chainHead[nowNode];	// heavy는 부모의 값을 그대로 저장
+				chainParent[child]	= chainParent[nowNode];	// heavy는 부모의 값을 그대로 저장
+				setHLD(child, nowNode, level);
+			}
+			else		// light인 경우, 새로운 체인 시작
+			{
+				chainHead[child]	= child;				// head는 자기자신
+				chainParent[child]	= nowNode;				// 다음 점프할 노드는 자기의 부모노드로 세팅
+				setHLD(child, nowNode, level + 1);			// 레벨을 늘려 새로운 체인 시작
+			}
+		}
+	}
+	static void moveHeavyChildToFront(int nowNode, int parentNode, int[] size) {
+		
+		int heavySize = 0;
+		int heavyIdx = 0;
+		size[nowNode] = 1;
+		
+		for(int i=0; i<adNode[nowNode].size(); i++)
+		{
+			int child = adNode[nowNode].get(i);
+			
+			if(child == parentNode)
+				continue;
+			
+			moveHeavyChildToFront(child, nowNode, size);
+			
+			size[nowNode] += size[child];
+			
+			if(heavySize < size[child])
+			{
+				heavySize = size[child];
+				heavyIdx = i;
+			}
+		}
+		
+		if(adNode[nowNode].size() > 0)
+			Collections.swap(adNode[nowNode], 0, heavyIdx);
 	}
 }
 //3 4	// 트리의 정점개수(1 ≤ 200,000), 질의 개수(1 ≤ 200,000)
