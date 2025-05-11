91b9b634dd36ec9230f312d76e17266bd9190a24
YongJu Kim <kyjdummy@gmail.com>
2025-05-11 23:04:53 +0900

    [Platinum IV] Title: 노트북의 주인을 찾아서, Time: 156 ms, Memory: 15540 KB -BaekjoonHub

---
+++ 백준/Platinum/1298. 노트북의 주인을 찾아서/노트북의 주인을 찾아서.java
@@ -0,0 +1,78 @@
+//https://github.com/KimyongJ/algorithm
+//https://www.acmicpc.net/problem/1298
+//2초 128MB
+//5 13	// 사람 수 1<=100, 노트북 예상 수(0<=5,000)
+//1 2	// 1번 사람이 2번 노트북이 자신의 것이라 주장
+//1 3
+//2 2
+//2 3
+//3 1
+//3 2
+//4 1
+//4 2
+//5 1
+//5 2
+//5 3
+//5 4
+//5 5
+//답 : 4
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static int N, M;
+	static int[] match;						// idx : 노트북 번호, value : 매칭된 사람 번호
+	static boolean[] visit;					// idx : 노트북 번호, value : 해당 노트북을 방문했었는지 체크
+	static ArrayList<Integer>[] adList;
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N		= Integer.parseInt(st.nextToken());	// 사람 수 1<=100
+		M		= Integer.parseInt(st.nextToken());	// 노트북 예상 수(0<=5,000)
+		match	= new int[N + 1];
+		adList	= new ArrayList[N + 1];
+		
+		for(int i=0; i<=N; i++)
+			adList[i] = new ArrayList<>();
+		
+		for(int i=0; i<M; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adList[a].add(b);
+		}
+		
+		int cnt = 0;
+		
+		for(int i=1; i<=N; i++)
+		{
+			visit = new boolean[N + 1];
+			if(dfs(i))
+				++cnt;
+		}
+		
+		System.out.print(cnt);
+	}
+	static boolean dfs(int person) {
+		for(int noteBook : adList[person])
+		{
+			if(visit[noteBook])
+				continue;
+			
+			visit[noteBook] = true;
+			
+			if(match[noteBook] == 0 || dfs(match[noteBook]))
+			{
+				match[noteBook] = person;
+				return true;
+			}
+			
+		}
+		return false;
+	}
+}
