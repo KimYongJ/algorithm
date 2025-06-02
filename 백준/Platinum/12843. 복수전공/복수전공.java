34ca17e2c499f9e0ea5a54593a9e47a443b5fca1
YongJu Kim <kyjdummy@gmail.com>
2025-06-02 23:02:15 +0900

    [Platinum III] Title: 복수전공, Time: 544 ms, Memory: 185780 KB -BaekjoonHub

--- 백준/Platinum/12843. 복수전공/복수전공.java
+++ 백준/Platinum/12843. 복수전공/복수전공.java
@@ -11,8 +11,6 @@
 //3 4
 //4 5
 // 답 : 3
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -22,33 +20,31 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int N, M;
-	static int cIdx[];
-	static int sIdx[];
+	static boolean isComputer[];
 	static List<Integer> adList[];
+	
 	// 이분 매칭시 사용
 	static int time;
 	static int match[];
 	static int visitTime[];
+	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		adList = new ArrayList[N + 1];
-		cIdx = new int[N + 1];
-		sIdx = new int[N + 1];
-		for(int i=0; i<=N; i++)
-			adList[i] = new ArrayList<>();
+		isComputer = new boolean[N + 1];
 		
-		for(int i=0,idxC = 0, idxS = 0; i<N; i++)
+		for(int i=1; i<=N; i++)
 		{
-			st = new StringTokenizer(br.readLine());// 필요 없어서 버림
-			int node = Integer.parseInt(st.nextToken());
-			char c = st.nextToken().charAt(0);
-			if(c == 'c')
-				cIdx[node] = ++idxC;
-			else
-				sIdx[node] = ++idxS;
+			adList[i] = new ArrayList<>(); // 인접리스트 초기화
+			
+			st = new StringTokenizer(br.readLine());
+			
+			int node = Integer.parseInt(st.nextToken());// 노드번호
+			char c = st.nextToken().charAt(0);// 과목
+			isComputer[node] = c == 'c';
 		}
 		
 		for(int i=1;i<=M; i++)
@@ -56,10 +52,11 @@ class Main{
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if(cIdx[a] != 0)
-				adList[cIdx[a]].add(sIdx[b]);
+			// 컴퓨터 과목기준으로 인접 리스트 생성
+			if(isComputer[a])
+				adList[a].add(b);
 			else
-				adList[cIdx[b]].add(sIdx[a]);
+				adList[b].add(a);
 		}
 		
 		match = new int[N + 1];
