080bb099792255cb7c80dd624d86b67cede60020
YongJu Kim <kyjdummy@gmail.com>
2025-06-03 17:20:27 +0900

    [Platinum III] Title: System Engineer, Time: 2084 ms, Memory: 35176 KB -BaekjoonHub

---
+++ 백준/Platinum/3736. System Engineer/README.md
@@ -0,0 +1,32 @@
+# [Platinum III] System Engineer - 3736 
+
+[문제 링크](https://www.acmicpc.net/problem/3736) 
+
+### 성능 요약
+
+메모리: 35176 KB, 시간: 2084 ms
+
+### 분류
+
+이분 매칭
+
+### 제출 일자
+
+2025년 6월 3일 17:20:20
+
+### 문제 설명
+
+<p>Bob is a skilled system engineer. He is always facing challenging problems, and now he must solve a new one. He has to handle a set of servers with differing capabilities available to process job requests from persistent sources - jobs that need to be processed over a long or indefinite period of time. A sequence of persistent job requests arrives revealing a subset of servers capable of servicing their request. A job is processed on a single server and a server processes only one job. Bob has to schedule the maximum number of jobs on the servers. For example, if there are 2 jobs j<sub>1</sub>, j<sub>2</sub> and 2 servers s<sub>1</sub>, s<sub>2</sub>, job j<sub>1</sub> requiring the server s<sub>1</sub>, and job j<sub>2</sub> requiring also the server s<sub>1</sub> In this case Bob can schedule only one job. Can you help him?</p>
+
+<p>In the general case there are n jobs numbered from 0 to n-1, n servers numbered from n to 2 *n-1, and a sequence of job requests. The problem asks to find the maximum number of jobs that can be processed.</p>
+
+### 입력 
+
+ <p>The program input is from a text file (at most 1 MB). Each data set in the file stands for a particular set of jobs. A data set starts with the number n (n ≤ 10000) of jobs, followed by the list of required servers for each job, in the format: job<sub>number</sub>: (nr_servers) s1 … s<sub>nr_servers</sub> The program prints the maximum number of jobs that can be processed.</p>
+
+<p>White spaces can occur freely in the input. The input data are correct and terminate with an end of file.</p>
+
+### 출력 
+
+ <p>For each set of data the program prints the result to the standard output from the beginning of a line. An input/output sample is in the table bellow.</p>
+
---
+++ 백준/Platinum/3736. System Engineer/System Engineer.java
@@ -0,0 +1,102 @@
+//https://www.acmicpc.net/problem/3736
+//1초 128MB
+//2// 작업 개수
+//0: (1) 2// 0번 작업은 (1)개의 서버를 필요로하며 그 서버 번호가 2입니다.
+//1: (1) 2// 1번 작업은 (1)개의 서버를 필요로하며 그 서버 번호가 1입니다.
+//1// 반복..
+//0: (1) 1
+// 작업과 서버 최대매칭 수 출력( 서버는 하나의 작업만 처리 )
+//1
+//1
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Main{
+	
+	static final int MAX = 20_002;
+	static int N;
+	static int time;
+	static int match[];
+	static int visitTime[];
+	static List<Integer> adList[];
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		init();
+		
+		while(true)
+		{
+			String str = br.readLine();
+			if(str == null || "".equals(str))
+				break;
+			clear();
+			
+			N = Integer.parseInt(str);
+			
+			for(int i=0; i<N; i++)
+			{
+				st = new StringTokenizer(br.readLine());
+				String j = st.nextToken();
+				String c = st.nextToken();
+				int job = Integer.parseInt(j.substring(0, j.length() - 1));
+				int cnt = Integer.parseInt(c.substring(1, c.length() - 1));
+				
+				while(cnt-->0)
+				{
+					int server = Integer.parseInt(st.nextToken());
+					adList[job].add(server);
+				}
+			}
+			
+			int cnt = 0;
+			for(int i=0; i<N; i++)
+			{
+				++time;
+				if(dfs(i))
+					++cnt;
+			}
+			sb.append(cnt).append('\n');
+		}
+		System.out.print(sb);
+	}
+	static boolean dfs(int job)
+	{
+		for(int server : adList[job])
+		{
+			if(visitTime[server] == time)
+				continue;
+			
+			visitTime[server] = time;
+			
+			if(match[server] == -1 || dfs(match[server]))
+			{
+				match[server] = job;
+				return true;
+			}
+		}
+		return false;
+	}
+	static void init()
+	{
+		match = new int[MAX];
+		visitTime = new int[MAX];
+		adList = new ArrayList[MAX];
+		
+		for(int i=0; i<MAX; i++)
+			adList[i] = new ArrayList<>();
+	}
+	static void clear()
+	{
+		Arrays.fill(match, -1);
+		for(int i=0; i<MAX; i++)
+			adList[i].clear();
+	}
+}
