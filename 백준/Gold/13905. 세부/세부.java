bae9876992d105779ec49fc20767bf051f29ecbc
YongJu Kim <kyjdummy@gmail.com>
2025-06-05 23:44:26 +0900

    [Gold III] Title: 세부, Time: 360 ms, Memory: 31680 KB -BaekjoonHub

--- 백준/Gold/13905. 세부/세부.java
+++ 백준/Gold/13905. 세부/세부.java
@@ -1,33 +1,22 @@
 //https://www.acmicpc.net/problem/13905
 //1초 256MB
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main{
 
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());//집의 수(2≤N≤100,000)
-		int M = Integer.parseInt(st.nextToken());//다리의 수(1≤M≤300,000)
-		
-		st = new StringTokenizer(br.readLine());
-		int s = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
+		Reader in = new Reader();
+		int N = in.nextInt();//집의 수(2≤N≤100,000)
+		int M = in.nextInt();//다리의 수(1≤M≤300,000)
+		int s = in.nextInt();
+		int e = in.nextInt();
 		
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		UnionFind uf = new UnionFind(N);
 		
 		for(int i=0; i<M; i++)
-		{
-			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
-			int dist = Integer.parseInt(st.nextToken());
-			pq.add(new Node(n1, n2, dist));
-		}
+			pq.add(new Node(in.nextInt(),in.nextInt(),in.nextInt()));
 		
 		while(!pq.isEmpty())
 		{
@@ -81,5 +70,30 @@ class Main{
 		public int compareTo(Node o) {
 			return o.dist - dist;
 		}
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
