40056224bf6bfbfdc6bb430b86bba7aa14a89400
YongJu Kim <kyjdummy@gmail.com>
2025-06-06 19:55:21 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 156 ms, Memory: 17912 KB -BaekjoonHub

--- 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.java
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.java
@@ -1,16 +1,11 @@
 //https://www.acmicpc.net/problem/24391
 //2초 256MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		Reader in = new Reader();
+		int N = in.nextInt();
+		int M = in.nextInt();
 		int parent[] = new int[N + 1];
 		
 		for(int i=1; i<=N; i++)
@@ -18,9 +13,8 @@ class Main{
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int parent1 = find(Integer.parseInt(st.nextToken()), parent);
-			int parent2 = find(Integer.parseInt(st.nextToken()), parent);
+			int parent1 = find(in.nextInt(), parent);
+			int parent2 = find(in.nextInt(), parent);
 			if(parent1 == parent2)
 				continue;
 			
@@ -29,12 +23,12 @@ class Main{
 			else
 				parent[parent1] = parent2;
 		}
-		st = new StringTokenizer(br.readLine());
+		
 		int prevNode = 0;
 		int result = -1;
 		while(N-->0)
 		{
-			int parentNode = find(Integer.parseInt(st.nextToken()), parent);
+			int parentNode = find(in.nextInt(), parent);
 			
 			if(prevNode != parentNode)
 				++result;
@@ -46,5 +40,30 @@ class Main{
 	static int find(int node, int parent[]) {
 		if(parent[node] == node) return node;
 		return parent[node] = find(parent[node], parent);
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
