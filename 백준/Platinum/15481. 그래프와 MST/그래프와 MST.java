8fbc7b797fda10e6f7ed2c66ec60658b8ec02fe6
YongJu Kim <kyjdummy@gmail.com>
2025-06-08 21:35:58 +0900

    [Platinum I] Title: 그래프와 MST, Time: 1188 ms, Memory: 79424 KB -BaekjoonHub

--- 백준/Platinum/15481. 그래프와 MST/그래프와 MST.java
+++ 백준/Platinum/15481. 그래프와 MST/그래프와 MST.java
@@ -18,12 +18,10 @@
 //12
 //15
 //10
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -32,44 +30,43 @@ class Main{
 	static List<Node> adList[];
 	static DSU dsu;
 	static HLD hld;
+	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());//정점 개수(2 ≤ 200,000)
-		M = Integer.parseInt(st.nextToken());//간선 개수(N-1 ≤ 200,000)
+		Reader in = new Reader();
+		N = in.nextInt();//정점 개수(2 ≤ 200,000)
+		M = in.nextInt();//간선 개수(N-1 ≤ 200,000)
 		edgeList = new ArrayList<>();
 		adList = new ArrayList[N + 1];
-		dsu = new DSU(N);
-		
+		dsu = new DSU(N);// Union Find를 위한 클래스
+		// 인접 리스트 초기화
 		for(int i=0; i<=N; i++)
 			adList[i] = new ArrayList<>();
 		
 		for(int i=0; i<M; i++)
 		{
-			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
-			int dist = Integer.parseInt(st.nextToken());
-			edgeList.add(new Edge(n1, n2, dist, i));
+			int n1 = in.nextInt();
+			int n2 = in.nextInt();
+			int dist = in.nextInt();
+			edgeList.add(new Edge(n1, n2, dist, i));// 간선의 정보를 담음
 		}
-		
+		// 크루스칼 알고리즘을 위한 정렬
 		Collections.sort(edgeList);
 		
-		int edgeCnt = 1;
-		long edgeSum = 0;
+		int edgeCnt = 1;// 조기 종료를 위한 간선 카운팅 변수
+		long edgeSum = 0;// 최소 스패닝 트리의 총합
 		
 		for(int i=0; i<edgeList.size() && edgeCnt != N; i++)
 		{
 			Edge now = edgeList.get(i);
-			if(dsu.union(now.n1, now.n2))
+			if(dsu.union(now.n1, now.n2))// 처음 연결하는 경우만 이하 연산 실행
 			{
-				edgeCnt += 1;
-				edgeSum += now.dist;
-				adList[now.n1].add(new Node(now.n2, now.dist));
-				adList[now.n2].add(new Node(now.n1, now.dist));
+				edgeCnt += 1;// 간선 개수 추가
+				edgeSum += now.dist;// 최소 스패닝 트리 간선 가중치 추가
+				adList[now.n1].add(new Node(now.n2, now.dist));// 인접리스트 생성
+				adList[now.n2].add(new Node(now.n1, now.dist));// 인접리스트 생성
 			}
 		}
-		
+		//HLD 알고리즘을 위한 클래스 초기화
 		hld = new HLD(N, adList);
 		
 		long res[] = new long[M];
@@ -77,7 +74,7 @@ class Main{
 		for(int i=0; i<edgeList.size(); i++)
 		{
 			Edge now = edgeList.get(i);
-			
+			// 최소 스패닝 트리의 총합 + 현재 추가하려는 간선 - 트리상에서 n1,n2 노드 사이 가장 큰 간선
 			res[now.idx] = edgeSum + now.dist - hld.find(now.n1, now.n2);
 		}
 		
@@ -108,13 +105,15 @@ class Main{
 			this.chainParent = new int[N + 1];
 			this.maxTree = new int[N * 4];
 			this.chainHeader[1] = 1;
+			// HLD 알고리즘을 위해 무거운 자식노드를 인접리스트의 맨 앞으로 이동
 			setChild(1, 0, new int[N + 1]);
+			// HLD 알고리즘을 위해 체인 정보 입력 및 노드번호 -> seg인덱스 변환 진행
 			setHLD(1, 0);
 		}
-		int find(int n1 , int n2) {
-			
+		int find(int n1 , int n2)// n1, n2 노드 사이 가장큰 간선 가중치를 반환하는 함수
+		{
 			int max = 0;
-			
+			// header가 같아질 때 까지 레벨이 높은 것을 낮게 올린다.
 			while(chainHeader[n1] != chainHeader[n2])
 			{
 				if(chainLevel[n1] > chainLevel[n2])
@@ -133,12 +132,13 @@ class Main{
 				n1 = n2;
 				n2 = tmp;
 			}
-			
+			// 간선 가중치는 자식 노드에 저장되어 있으므로 LCA + 1부터 n2까지 탐색
 			return Math.max(max, query(1, 1, N, segIdx[n1] + 1, segIdx[n2]));
 		}
 		int query(int treeNode, int s, int e, int left, int right) {
 			if(e < left || right < s)
 				return 0;
+			
 			if(left <= s && e <= right)
 				return maxTree[treeNode];
 			
@@ -152,6 +152,7 @@ class Main{
 		void update(int treeNode, int s, int e, int targetIdx, int value) {
 			if(e < targetIdx || targetIdx < s)
 				return;
+			
 			if(s == e)
 			{
 				maxTree[treeNode] = value;
@@ -167,28 +168,30 @@ class Main{
 			
 			maxTree[treeNode] = Math.max(maxTree[treeNode << 1], maxTree[treeNode << 1 | 1]);
 		}
-		void setHLD(int nowNode, int level) {
+		void setHLD(int nowNode, int level)
+		{
 			segIdx[nowNode] = ++idx;
 			chainLevel[nowNode] = level;
 			for(int i=0; i<adList[nowNode].size(); i++)
 			{
 				Node next = adList[nowNode].get(i);
 				
-				if(chainHeader[next.node] != 0)
+				if(chainHeader[next.node] != 0)// 이미 방문한 경우 스킵
 					continue;
 				
-				if(i == 0)
+				if(i == 0)// 무거운 자식노드 인경우 체인 정보 유지
 				{
 					chainHeader[next.node] = chainHeader[nowNode];
 					chainParent[next.node] = chainParent[nowNode];
 					setHLD(next.node, level);
 				}
-				else
+				else// 가벼운 자식노드인 경우 체인 정보 새롭게 시작
 				{
-					chainHeader[next.node] = next.node;
-					chainParent[next.node] = nowNode;
-					setHLD(next.node, level + 1);
+					chainHeader[next.node] = next.node;// 체인의 헤더는 자기자신
+					chainParent[next.node] = nowNode;// 이전 체인 점프를 위한 이전 체인의 노드 정보 저장
+					setHLD(next.node, level + 1);// 레벨 증가
 				}
+				// 자식 노드에 간선의 정보 업데이트 처리
 				update(1, 1, N, segIdx[next.node], next.dist);
 			}
 		}
@@ -204,14 +207,16 @@ class Main{
 					continue;
 				
 				setChild(next.node, nowNode, size);
-				
+				// nowNode의 사이즈를 지속적으로 추가해나감
 				size[nowNode] += size[next.node];
+				// 무거운 자식노드의 인덱스를 구해나간다.
 				if(heavySize < size[next.node])
 				{
 					heavySize = size[next.node];
 					heavyIdx = i;
 				}
 			}
+			// 무거운 자식노드를 맨앞으로 교체(swap)
 			if(adList[nowNode].size() > 0)
 				Collections.swap(adList[nowNode],0, heavyIdx);
 		}
@@ -228,9 +233,10 @@ class Main{
 		boolean union(int n1, int n2) {
 			int parent1 = find(n1);
 			int parent2 = find(n2);
-			if(parent1 == parent2)
+			if(parent1 == parent2)// 이미 연결되어있으면 false
 				return false;
 			
+			// 새연결이면 true반환
 			if(parent[parent1] < parent[parent2])
 				parent[parent2] = parent1;
 			else
@@ -262,5 +268,31 @@ class Main{
 		public int compareTo(Edge o) {
 			return dist - o.dist;
 		}
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
