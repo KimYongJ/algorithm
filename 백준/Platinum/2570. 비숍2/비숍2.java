6d3cb7a2429aa34708845248f3e8c4c2e8cf4cac
YongJu Kim <kyjdummy@gmail.com>
2025-06-13 23:06:36 +0900

    [Platinum II] Title: 비숍2, Time: 108 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Platinum/2570. 비숍2/비숍2.java
+++ 백준/Platinum/2570. 비숍2/비숍2.java
@@ -4,11 +4,9 @@
 //1// 장애물 위치 개수
 //3 3// 장애물 좌표 y, x
 //답 : 10
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -25,10 +23,9 @@ class Main{
 	static int visitTime[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		B = Integer.parseInt(br.readLine());
+		Reader in = new Reader();
+		N = in.nextInt();
+		B = in.nextInt();
 		adList = new ArrayList[N * N + 1];
 		match = new int[adList.length];
 		visitTime = new int[adList.length];
@@ -43,9 +40,8 @@ class Main{
 		}
 		while(B-- >0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int y = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
+			int y = in.nextInt();
+			int x = in.nextInt();
 			isBlock[y][x] = true;
 		}
 		
@@ -56,37 +52,8 @@ class Main{
 				if(isBlock[y][x])
 					continue;
 				
-				if(leftDown[y][x] == 0)
-				{
-					leftDown[y][x] = ++idxL;
-					
-					int nextY = y + dxy[0][0];
-					int nextX = x + dxy[0][1];
-					
-					while(validate(nextY, nextX))
-					{
-						leftDown[nextY][nextX] = leftDown[y][x];
-						
-						nextY += dxy[0][0];
-						nextX += dxy[0][1];
-					}
-				}
-				
-				if(rightDown[y][x] == 0)
-				{
-					rightDown[y][x] = ++idxR;
-					int nextY = y + dxy[1][0];
-					int nextX = x + dxy[1][1];
-					
-					while(validate(nextY, nextX))
-					{
-						rightDown[nextY][nextX] = rightDown[y][x];
-						
-						nextY += dxy[1][0];
-						nextX += dxy[1][1];
-					}
-				}
-				
+				markLeftDown(y, x);
+				markRightDown(y, x);
 			}
 		}
 		
@@ -122,5 +89,66 @@ class Main{
 	}
 	static boolean validate(int y, int x) {
 		return 1<=y && 1<=x && y<=N && x<=N && !isBlock[y][x];
+	}
+	static void markRightDown(int y, int x) {
+		if(rightDown[y][x] == 0)
+		{
+			rightDown[y][x] = ++idxR;
+			int nextY = y + dxy[1][0];
+			int nextX = x + dxy[1][1];
+			
+			while(validate(nextY, nextX))
+			{
+				rightDown[nextY][nextX] = rightDown[y][x];
+				
+				nextY += dxy[1][0];
+				nextX += dxy[1][1];
+			}
+		}
+	}
+	static void markLeftDown(int y, int x) {
+		if(leftDown[y][x] == 0)
+		{
+			leftDown[y][x] = ++idxL;
+			
+			int nextY = y + dxy[0][0];
+			int nextX = x + dxy[0][1];
+			
+			while(validate(nextY, nextX))
+			{
+				leftDown[nextY][nextX] = leftDown[y][x];
+				
+				nextY += dxy[0][0];
+				nextX += dxy[0][1];
+			}
+		}
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
