ed1e68f86e868e7c4b985bd24a9fbdc63f9fea24
YongJu Kim <kyjdummy@gmail.com>
2025-06-22 11:08:25 +0900

    [Gold IV] Title: 어려운 모든 정점 쌍 최단 거리, Time: 192 ms, Memory: 19256 KB -BaekjoonHub

--- 백준/Gold/23324. 어려운 모든 정점 쌍 최단 거리/어려운 모든 정점 쌍 최단 거리.java
+++ 백준/Gold/23324. 어려운 모든 정점 쌍 최단 거리/어려운 모든 정점 쌍 최단 거리.java
@@ -6,10 +6,6 @@
 //4 5
 // 답 : 6
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N, M, K;
@@ -17,11 +13,10 @@ class Main{
 	static int parent[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());// 정점수(2<=100,000)
-		M = Integer.parseInt(st.nextToken());// 간선수(1<=200,000)
-		K = Integer.parseInt(st.nextToken());// 제외 정수 (1<=M)
+		Reader in = new Reader();
+		N = in.nextInt();// 정점수(2<=100,000)
+		M = in.nextInt();// 간선수(1<=200,000)
+		K = in.nextInt();// 제외 정수 (1<=M)
 		cnt = new int[N + 1];
 		parent = new int[N + 1];
 		
@@ -33,21 +28,19 @@ class Main{
 		
 		for(int i=1; i<=M; i++)
 		{
-			st = new StringTokenizer(br.readLine());
+			int p1 = find(in.nextInt());
+			int p2 = find(in.nextInt());
 			
-			if(K == i) continue;
-			
-			int p1 = find(Integer.parseInt(st.nextToken()));
-			int p2 = find(Integer.parseInt(st.nextToken()));
-			
-			if(p1 == p2)
+			if(K == i || p1 == p2)
 				continue;
 			
-			if(parent[p1] < parent[p2]) {
+			if(parent[p1] < parent[p2])
+			{
 				parent[p2] = p1;
 				cnt[p1] += cnt[p2];
 			}
-			else {
+			else
+			{
 				parent[p1] = p2;
 				cnt[p2] += cnt[p1];
 			}
@@ -73,5 +66,30 @@ class Main{
 			return node;
 		
 		return parent[node] = find(parent[node]);
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
