d0c79b3354066b194475d6ab5c675eedb558548c
YongJu Kim <kyjdummy@gmail.com>
2025-06-28 09:41:21 +0900

    [Gold III] Title: 슬슬 가지를 먹지 않으면 죽는다, Time: 276 ms, Memory: 38528 KB -BaekjoonHub

--- 백준/Gold/27945. 슬슬 가지를 먹지 않으면 죽는다/슬슬 가지를 먹지 않으면 죽는다.java
+++ 백준/Gold/27945. 슬슬 가지를 먹지 않으면 죽는다/슬슬 가지를 먹지 않으면 죽는다.java
@@ -1,10 +1,7 @@
 //https://www.acmicpc.net/problem/27945
 //1초 1024MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 class Main{
 	
@@ -12,48 +9,41 @@ class Main{
 	static int parent[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		Reader in = new Reader();
+		N = in.nextInt();
+		M = in.nextInt();
 		parent = new int[N + 1];
 		
 		for(int i=1; i<=N; i++)
 			parent[i] = i;
 		
 		while(M-->0)
-		{
-			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
-			int t = Integer.parseInt(st.nextToken());
-			pq.add(new Node(n1, n2, t));
-		}
+			pq.add(new Node(in.nextInt(),in.nextInt(),in.nextInt()));
+		
 		
 		int edgeCnt = 1;
 		int day = 0;
+		
 		while(!pq.isEmpty() && edgeCnt != N)
 		{
 			Node now = pq.poll();
 			int p1 = find(now.n1);
 			int p2 = find(now.n2);
-			int t = now.t;
 			
 			if(p1 == p2)
 				continue;
 			
+			if(day + 1 != now.t)
+				break;
+			
+			day++;
 			edgeCnt++;
 			
 			if(parent[p1] < parent[p2])
 				parent[p2] = p1;
 			else
 				parent[p1] = p2;
-			
-			if(day + 1 == t)
-				day++;
-			else
-				break;
 		}
 		
 		System.out.print(day + 1);
@@ -73,5 +63,30 @@ class Main{
 		public int compareTo(Node o) {
 			return t - o.t;
 		}
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
