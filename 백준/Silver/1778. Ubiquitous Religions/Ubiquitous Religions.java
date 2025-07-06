7c63a802ffce387af06352d6f61b4259870718c8
YongJu Kim <kyjdummy@gmail.com>
2025-07-06 13:50:57 +0900

    [Silver I] Title: Ubiquitous Religions, Time: 184 ms, Memory: 19320 KB -BaekjoonHub

--- 백준/Silver/1778. Ubiquitous Religions/Ubiquitous Religions.java
+++ 백준/Silver/1778. Ubiquitous Religions/Ubiquitous Religions.java
@@ -1,29 +1,24 @@
 //https://www.acmicpc.net/problem/1778
 //1초 128MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int N, M, cnt;
 	static int parent[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Reader in = new Reader();
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 		
 		parent = new int[50_001];
 		
 		int idx = 0;
 		while(true)
 		{
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
+			N = in.nextInt();
+			M = in.nextInt();
 			cnt = N;
+			
 			if(N == 0 && M == 0)
 				break;
 			
@@ -32,9 +27,8 @@ class Main{
 			
 			while(M-->0)
 			{
-				st = new StringTokenizer(br.readLine());
-				int p1 = find(Integer.parseInt(st.nextToken()));
-				int p2 = find(Integer.parseInt(st.nextToken()));
+				int p1 = find(in.nextInt());
+				int p2 = find(in.nextInt());
 				
 				if(p1 == p2)
 					continue;
@@ -54,5 +48,30 @@ class Main{
 	static int find(int node) {
 		if(parent[node] == node) return node;
 		return parent[node] = find(parent[node]);
+	}
+	static class Reader {
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
