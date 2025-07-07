300d2f156e5a7e83d2bc2c4949fc5ca0722fd1b3
YongJu Kim <kyjdummy@gmail.com>
2025-07-07 21:56:59 +0900

    [Silver I] Title: Money Matters, Time: 252 ms, Memory: 25724 KB -BaekjoonHub

--- 백준/Silver/5033. Money Matters/Money Matters.java
+++ 백준/Silver/5033. Money Matters/Money Matters.java
@@ -1,72 +1,66 @@
+//https://www.acmicpc.net/problem/5033
+//1초 128MB
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-
-    static int N, M;
-    static int[] parent;   // 유니온-파인드용
-    static int[] money;    // 그룹별 빚/받을 돈 합계 (루트 노드에만 의미 있음)
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());   // 친구 수
-        M = Integer.parseInt(st.nextToken());   // 남은 친구 관계 수
-
-        parent = new int[N];
-        money  = new int[N];
-
-        // 초기화
-        for (int i = 0; i < N; i++) {
-            parent[i] = i;                      // 자기 자신이 루트
-            money[i]  = Integer.parseInt(br.readLine());  // 개별 빚(+) / 받을 돈(-)
-        }
-
-        // 친구 관계(간선) 입력 및 합치기
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            union(a, b);
-        }
-
-        /* ------------- 판정 ------------- */
-        // 혹시 모를 경로 압축 누락 대비하여 한 번 더 find 수행
-        for (int i = 0; i < N; i++) find(i);
-
-        // 각 그룹(=대표 노드)의 돈 합계가 0이어야 가능
-        for (int i = 0; i < N; i++) {
-            if (parent[i] == i && money[i] != 0) {
-                System.out.println("IMPOSSIBLE");
-                return;
-            }
-        }
-        System.out.println("POSSIBLE");
-    }
-
-    /* --------- 유니온-파인드 ---------- */
-
-    // 경로 압축 find
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    // union: 두 집합을 합치면서 money 합산
-    static void union(int a, int b) {
-        int ra = find(a);
-        int rb = find(b);
-        if (ra == rb) return;                   // 이미 같은 그룹
-
-        // 인덱스가 작은 쪽을 새 루트로 삼음(임의 선택)
-        if (ra < rb) {
-            parent[rb] = ra;
-            money[ra] += money[rb];
-        } else {
-            parent[ra] = rb;
-            money[rb] += money[ra];
-        }
-    }
+class Main{
+	
+	static int N, M;
+	static int money[];
+	static int parent[];
+	
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		money = new int[N + 1];
+		parent = new int[N + 1];
+		
+		for(int i=0; i<N; i++)
+		{
+			parent[i] = i;
+			money[i] = Integer.parseInt(br.readLine());
+		}
+		
+		while(M-->0)
+		{
+			st = new StringTokenizer(br.readLine());
+			int p1 = find(Integer.parseInt(st.nextToken()));
+			int p2 = find(Integer.parseInt(st.nextToken()));
+			
+			if(p1 == p2)
+				continue;
+			
+			if(p1 < p2)
+			{
+				parent[p2] = p1;
+				money[p1] += money[p2];
+			}
+			else
+			{
+				parent[p1] = p2;
+				money[p2] += money[p1];
+			}
+		}
+		
+		
+		for(int i=0; i<N; i++)
+		{
+			if(parent[i] == i && money[i] != 0)
+			{
+				System.out.print("IMPOSSIBLE");
+				return;
+			}
+		}
+		
+		System.out.print("POSSIBLE");
+	}
+	static int find(int node) {
+		if(parent[node] == node) return node;
+		return parent[node] = find(parent[node]);
+	}
 }
