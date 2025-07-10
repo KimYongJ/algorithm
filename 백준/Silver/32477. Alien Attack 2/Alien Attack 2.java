b6248ba5be974688888ae44076405695e01c26e4
YongJu Kim <kyjdummy@gmail.com>
2025-07-10 21:11:13 +0900

    [Silver II] Title: Alien Attack 2, Time: 136 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Silver/32477. Alien Attack 2/Alien Attack 2.java
+++ 백준/Silver/32477. Alien Attack 2/Alien Attack 2.java
@@ -1,10 +1,6 @@
 //https://www.acmicpc.net/problem/32477
 //4초 1024
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	
 	static int res = 1;
@@ -13,10 +9,9 @@ class Main{
 	static int cnt[];
 	
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		Reader in = new Reader();
+		N = in.nextInt();
+		M = in.nextInt();
 		
 		parent = new int[N + 1];
 		cnt = new int[N + 1];
@@ -29,12 +24,12 @@ class Main{
 		
 		while(M-->0)
 		{
-			st = new StringTokenizer(br.readLine());
-			int p1 = find(Integer.parseInt(st.nextToken()));
-			int p2 = find(Integer.parseInt(st.nextToken()));
+			int p1 = find(in.nextInt());
+			int p2 = find(in.nextInt());
 			
 			if(p1 == p2)
 				continue;
+			
 			if(parent[p1] > parent[p2])
 			{
 				int tmp = p1;
@@ -53,5 +48,32 @@ class Main{
 	static int find(int node) {
 		if(parent[node] == node) return node;
 		return parent[node] = find(parent[node]);
+	}
+	
+	static class Reader
+	{
+	    final int SIZE = 1 << 13;
+	    byte[] buffer = new byte[SIZE];
+	    int index, size;
+	    int nextInt() throws Exception {
+	        int n = 0;
+	        byte c;
+	        boolean isMinus = false;
+	        while ((c = read()) <= 32) { if (size < 0) return -1; }
+	        if (c == 45) { c = read(); isMinus = true; }
+	        do n = (n << 3) + (n << 1) + (c & 15);
+	        while (isNumber(c = read()));
+	        return isMinus ? ~n + 1 : n;
+	    }
+	    boolean isNumber(byte c) {
+	        return 47 < c && c < 58;
+	    }
+	    byte read() throws Exception {
+	        if (index == size) {
+	            size = System.in.read(buffer, index = 0, SIZE);
+	            if (size < 0) buffer[0] = -1;
+	        }
+	        return buffer[index++];
+	    }
 	}
 }
