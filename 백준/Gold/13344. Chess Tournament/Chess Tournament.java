3f8cb1e4b5ae84615d4552ef2440fa5567c8832f
YongJu Kim <kyjdummy@gmail.com>
2025-07-13 16:11:31 +0900

    [Gold I] Title: Chess Tournament, Time: 480 ms, Memory: 87284 KB -BaekjoonHub

--- 백준/Gold/13344. Chess Tournament/Chess Tournament.java
+++ 백준/Gold/13344. Chess Tournament/Chess Tournament.java
@@ -1,93 +1,129 @@
-// https://www.acmicpc.net/problem/13344
-// 5초 512MB
+//https://www.acmicpc.net/problem/13344
+//5초 512MB
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-class Main {
+class Main{
+	
+	static int N, M;
+	static int parent[];
+	static int visitCount[];
+	static boolean onStack[];
+	static List<Node> list;
+	static List<Integer> adList[];
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N		= Integer.parseInt(st.nextToken());//선수 수 2<=50,000
+		M		= Integer.parseInt(st.nextToken());//보고된 경기 수 1<=250,000
+		parent	= new int[N];
+		visitCount = new int[N];
+		onStack = new boolean[N];
+		list	= new ArrayList<>();
+		adList	= new ArrayList[N];
+		
+		for(int i=0; i<N; i++)
+		{
+			parent[i] = i;
+			adList[i] = new ArrayList<>();
+		}
 
-    static int N, M;
-    static int[] parent;
+		while(M-->0)
+		{
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			char c = st.nextToken().charAt(0);
+			int b = Integer.parseInt(st.nextToken());
+			if(c == '=')
+			{
+				union(a,b);
+				continue;
+			}
+			else if(c == '<')
+			{
+				int tmp = b;
+				b = a;
+				a = tmp;
+			}
+			list.add(new Node(a, b));
+		}
+		
+		for(Node now : list)
+		{
+			int p1 = find(now.big);
+			int p2 = find(now.small);
+			
+			if(p1 == p2)
+			{
+				System.out.print("inconsistent");
+				return;
+			}
+			
+			adList[p1].add(p2);
+		}
+		
+		int flag = 0;
+		
+		for(int i=0; i<N; i++)
+		{
+			if(visitCount[i] == 0)
+			{
+				visitCount[i] = ++flag;
+				if(isCycle(i, flag))
+				{
+					System.out.print("inconsistent");
+					return;
+				}
+			}
+		}
 
-    @SuppressWarnings("unchecked")
-    public static void main(String[] args) throws Exception {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());   // 2 ≤ N ≤ 50 000
-        M = Integer.parseInt(st.nextToken());   // 1 ≤ M ≤ 250 000
-
-        parent = new int[N];
-        for (int i = 0; i < N; i++) parent[i] = i;
-
-        // 간선 정보 임시 저장
-        int[] from = new int[M];
-        int[] to   = new int[M];
-        int edgeCnt = 0;
-
-        // 1) 입력
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            char c = st.nextToken().charAt(0);
-            int b = Integer.parseInt(st.nextToken());
-
-            if (c == '=') {             // 동급 → 합집합
-                union(a, b);
-            } else {                    // ‘>’
-                from[edgeCnt] = a;
-                to[edgeCnt]   = b;
-                edgeCnt++;
-            }
-        }
-
-        // 2) 간선 정규화(대표 노드 기준) & 오류 체크
-        List<Integer>[] adj = new ArrayList[N];
-        int[] indeg = new int[N];
-
-        for (int i = 0; i < edgeCnt; i++) {
-            int u = find(from[i]);
-            int v = find(to[i]);
-            if (u == v) {               // 같은 집합에서 u > v ⇒ 모순
-                System.out.print("inconsistent");
-                return;
-            }
-            if (adj[u] == null) adj[u] = new ArrayList<>();
-            adj[u].add(v);
-            indeg[v]++;
-        }
-
-        // 3) Kahn 위상 정렬로 사이클 여부 판별
-        Deque<Integer> dq = new ArrayDeque<>();
-        int reps = 0;                   // 대표 노드 수
-        for (int i = 0; i < N; i++) {
-            if (parent[i] == i) {       // 대표 노드만 세면 충분
-                reps++;
-                if (indeg[i] == 0) dq.add(i);
-            }
-        }
-
-        int processed = 0;
-        while (!dq.isEmpty()) {
-            int cur = dq.poll();
-            processed++;
-            if (adj[cur] == null) continue;
-            for (int nxt : adj[cur]) {
-                if (--indeg[nxt] == 0) dq.add(nxt);
-            }
-        }
-
-        System.out.print(processed == reps ? "consistent"
-                                           : "inconsistent");
-    }
-
-    /* ---------- Union–Find ---------- */
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    static void union(int a, int b) {
-        int pa = find(a), pb = find(b);
-        if (pa != pb) parent[pb] = pa;
-    }
+		System.out.print("consistent");
+	}
+	static boolean isCycle(int now, int flag)
+	{
+		onStack[now] = true;
+		for(int next : adList[now])
+		{
+			if(onStack[next])
+				return true;
+			
+			if(visitCount[next] != 0)
+				continue;
+			
+			visitCount[next] = flag;
+			
+			if(isCycle(next, flag))
+				return true;
+		}
+		onStack[now] = false;
+		return false;
+	}
+	static void union(int a, int b) {
+		int p1 = find(a);
+		int p2 = find(b);
+		
+		if(p1 == p2)
+			return;
+		
+		if(parent[p1] < parent[p2])
+			parent[p2] = p1;
+		else
+			parent[p1] = p2;
+	}
+	static int find(int node) {
+		if(parent[node] == node) return node;
+		return parent[node] = find(parent[node]);
+	}
+	static class Node {
+		int big, small;
+		Node(int b, int s){
+			big = b;
+			small = s;
+		}
+	}
 }
