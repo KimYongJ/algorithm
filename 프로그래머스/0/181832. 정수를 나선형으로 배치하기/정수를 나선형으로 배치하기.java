cbf1ff7d8518598726d6b622066067a5529aac6b
YongJu Kim <kyjdummy@gmail.com>
2025-07-28 00:07:07 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.10 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -2,38 +2,25 @@
 //프로그래머스 : 정수를 나선형으로 배치하기
 class Solution {
     public int[][] solution(int n) {
+        final int dxy[][] = {{0,1},{1,0},{0,-1},{-1,0}};// 오른쪽, 아래, 왼쪽, 위
         int[][] ans = new int[n][n];
-        int limit = n*n;
-        int value = 0;
-        ans[0][0] = ++value;
         
-        int Y = 0;
-        int X = 0;
-        
-        int right = n - 1;// x만 증가 후 - 1
-        int down = n - 1;// y만 증가 후 - 1
-        int left = 0;// x만 감소 후 + 1
-        int up = 1;// y만 감소 후 + 1
-        
-        while(value != limit)
-        {
-            while(value != limit && X<right)
-                ans[Y][++X] = ++value;
-
-            while(value != limit && Y<down)
-                ans[++Y][X] = ++value;
-
-            while(value != limit && left < X)
-                ans[Y][--X] = ++value;
+        int x = 0, y = 0, dir = 0;
+        for(int value = 1; value<=n*n; value++){
+            ans[y][x] = value;
+            int ny = y + dxy[dir][0];
+            int nx = x + dxy[dir][1];
             
-            while(value != limit && up < Y)
-                ans[--Y][X] = ++value;
+            if(ny<0 || nx <0 || n<=ny || n<=nx || ans[ny][nx] != 0){
+                dir = (dir + 1) % 4;
+                ny = y + dxy[dir][0];
+                nx = x + dxy[dir][1];
+            }
             
-            --right;
-            --down;
-            ++left;
-            ++up;
+            x = nx;
+            y = ny;
         }
+        
         
         return ans;
     }
