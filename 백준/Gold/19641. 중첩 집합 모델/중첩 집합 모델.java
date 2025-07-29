a240e4f5066984b0adf56206cf7575431da11257
YongJu Kim <kyjdummy@gmail.com>
2025-07-29 22:26:14 +0900

    [Gold V] Title: 중첩 집합 모델, Time: 620 ms, Memory: 75884 KB -BaekjoonHub

--- 백준/Gold/19641. 중첩 집합 모델/중첩 집합 모델.java
+++ 백준/Gold/19641. 중첩 집합 모델/중첩 집합 모델.java
@@ -1,3 +1,14 @@
+//https://www.acmicpc.net/problem/19641
+//1초 1024MB
+//3// 정점 수 (2<=100,000)
+//2 1 3 -1 // 정점 번호가 먼저 주어지고, -1입력 전까지 해당 정점에 연결된 노드가 주어진다.
+//3 2 -1
+//1 2 -1
+//2// 루트노드 번호
+//출력 : 루트노드 부터 탐색을 시작하여 번호가 가장 낮은 노드부터 오름차순 방문해서 중첩 집합을 구성할 때, 각 노드의 번호 left 필드와 right 필드 출력
+//1 2 3// 정점 수 만큼 LEFT필드와 RIGHT필드를 출력
+//2 1 6
+//3 4 5
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -9,7 +20,6 @@ class Main{
 	static int idx;
 	static int order[][];
 	static int adNode[][];
-	static boolean visit[];
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +27,6 @@ class Main{
 		
 		N = Integer.parseInt(st.nextToken());
 		order = new int[N + 1][2];
-		visit = new boolean[N + 1];
 		adNode = new int[N + 1][];
 		
 		for(int i=0; i<N; i++)
@@ -33,10 +42,7 @@ class Main{
 			Arrays.sort(adNode[a]);
 		}
 		
-		int start = Integer.parseInt(br.readLine());
-		
-		visit[start] = true;
-		dfs(start);
+		dfs(Integer.parseInt(br.readLine()), 0);
 		
 		StringBuilder sb = new StringBuilder();
 		
@@ -46,17 +52,13 @@ class Main{
 		
 		System.out.print(sb);
 	}
-	static void dfs(int now) {
+	static void dfs(int now, int parent)
+	{
 		order[now][0] = ++idx;
 		
 		for(int next : adNode[now])
-		{
-			if(!visit[next])
-			{
-				visit[next] = true;
-				dfs(next);
-			}
-		}
+			if(next != parent)
+				dfs(next, now);
 		
 		order[now][1] = ++idx;
 	}
