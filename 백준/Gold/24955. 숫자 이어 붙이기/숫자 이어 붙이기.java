95fdf50d649086ba6cd94964751a764ce9027b5e
YongJu Kim <kyjdummy@gmail.com>
2025-08-01 22:02:06 +0900

    [Gold IV] Title: 숫자 이어 붙이기, Time: 212 ms, Memory: 20872 KB -BaekjoonHub

--- 백준/Gold/24955. 숫자 이어 붙이기/숫자 이어 붙이기.java
+++ 백준/Gold/24955. 숫자 이어 붙이기/숫자 이어 붙이기.java
@@ -12,7 +12,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -20,14 +19,10 @@ class Main{
 	
 	static final long mod = 1_000_000_007;
 	static int N, Q;
+	static int s, e;
 	static long num[];
-	static int idx;
-	static int chainIdx[];
-	static int chainLevel[];
-	static int chainParent[];
-	static int chainHeader[];
 	static List<Integer> adList[];
-	static List<Long> forward, reverse;
+	static StringBuilder sb;
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,16 +30,8 @@ class Main{
 		N = Integer.parseInt(st.nextToken());// 집의 수(2≤N≤1,000)
 		Q = Integer.parseInt(st.nextToken());// 놀이 횟수 (1≤Q≤1,000)
 		num = new long[N + 1];
-		chainIdx = new int[N + 1];
-		chainLevel = new int[N + 1];
-		chainParent = new int[N + 1];
-		chainHeader = new int[N + 1];
 		adList = new ArrayList[N + 1];
-		chainHeader[1] = 1;
-		chainParent[1] = 1;
-		chainLevel[1] = 1;
-		forward = new ArrayList<>();
-		reverse = new ArrayList<>();
+		sb = new StringBuilder();
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
@@ -62,64 +49,31 @@ class Main{
 			adList[b].add(a);
 		}
 		
-		setChild(1, 0, new int[N + 1]);
-		setHLD(1, 0, 1);
 		
-		StringBuilder sb = new StringBuilder();
 		while(Q-->0)
 		{
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
+			s = Integer.parseInt(st.nextToken());
+			e = Integer.parseInt(st.nextToken());
 			
-			sb.append(getAns(s, e)).append('\n');
+			dfs(s, 0,num[s]);
 		}
 		System.out.print(sb);
 	}
-	static long getAns(int s, int e)
-	{
-		forward.clear();
-		reverse.clear();
-		
-		while(chainHeader[s] != chainHeader[e])
+	static void dfs(int idx, int parent, long ans) {
+		if(idx == e)
 		{
-			if(chainLevel[s] < chainLevel[e])
-			{
-				reverse.add(0,num[e]);
-				e = chainParent[e];
-				continue;
-			}
-			forward.add(num[s]);
-			s = chainParent[s];
-		}
-		while(chainIdx[s] != chainIdx[e])
-		{
-			if(chainIdx[s] > chainIdx[e])
-			{
-				forward.add(num[s]);
-				s = chainParent[s];
-			}
-			else
-			{
-				reverse.add(0,num[e]);
-				e = chainParent[e];
-			}
+			sb.append(ans).append('\n');
+			return;
 		}
 		
-		forward.add(num[s]);
+		for(int next : adList[idx])
+			if(next != parent)
+				dfs(next, idx, getAns(ans, next));
 		
-		long res = 0;
-		
-		for(long now : forward)
-		{
-			res = (((res * getDigit(now)) % mod) + now) %mod;
-		}
-		for(long now : reverse)
-		{
-			res = (((res * getDigit(now)) % mod) + now) %mod;
-		}
-		
-		return res;
+	}
+	static long getAns(long ans, int idx) {
+		return (((ans * getDigit(num[idx]))%mod) + num[idx]) %mod;
 	}
 	static long getDigit(long now) {
 		long mul = 1;
@@ -130,49 +84,5 @@ class Main{
 			now /= 10;
 		}
 		return mul;
-	}
-	static void setHLD(int now, int parent, int level) {
-		chainIdx[now] = ++idx;
-		chainLevel[now] = level;
-		
-		for(int i=0; i<adList[now].size(); i++)
-		{
-			int next = adList[now].get(i);
-			if(next == parent)
-				continue;
-			
-			chainParent[next] = now;// 자기 이전 노드로 점프하기 위해 이전 노드 마킹
-			if(i == 0)// heavy child인 경우 체인 유지
-			{
-				chainHeader[next] = chainHeader[now];
-				setHLD(next, now, level);
-				continue;
-			}
-			// 가벼운 자식노드는 새로운 체인 시작
-			chainHeader[next] = next;// 체인의 머리는 자기 자신
-			setHLD(next, now, level + 1);// 레벨 추가
-		}
-	}
-	static void setChild(int now, int parent, int size[]) {
-		size[now] = 1;
-		int heavyIdx = 0;
-		int heavySize = 0;
-		for(int i=0; i<adList[now].size(); i++)
-		{
-			int next = adList[now].get(i);
-			if(next == parent)
-				continue;
-			
-			setChild(next, now, size);
-			
-			size[now] += size[next];
-			
-			if(heavySize < size[next])
-			{
-				heavySize = size[next];
-				heavyIdx = i;
-			}
-		}
-		Collections.swap(adList[now], 0, heavyIdx);
 	}
 }
