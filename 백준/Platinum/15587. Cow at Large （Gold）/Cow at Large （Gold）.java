5c892f7c6e798c5d213e1de3206a32a7ec36651e
YongJu Kim <kyjdummy@gmail.com>
2025-08-20 20:55:57 +0900

    [Platinum IV] Title: Cow at Large (Gold), Time: 440 ms, Memory: 49500 KB -BaekjoonHub

--- 백준/Platinum/15587. Cow at Large （Gold）/Cow at Large （Gold）.java
+++ 백준/Platinum/15587. Cow at Large （Gold）/Cow at Large （Gold）.java
@@ -10,8 +10,7 @@
 //베시를 잡기 위해 필요한 최소 농부수 : 3
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main{
@@ -20,7 +19,7 @@ class Main{
 	static int N, S;
 	static int distFromStart[];
 	static int distToNearestLeaf[];
-	static List<Integer> adList[];
+	static Node adNode[];
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,23 +28,20 @@ class Main{
 		S = Integer.parseInt(st.nextToken());
 		distFromStart = new int[N + 1];
 		distToNearestLeaf = new int[N + 1];
-		adList = new ArrayList[N + 1];
+		adNode = new Node[N + 1];
 		
-		for(int i=0; i<=N; i++)
+		Arrays.fill(distToNearestLeaf, 1<<30);
+		
+		for(int i=1; i<N; i++)
 		{
-			adList[i] = new ArrayList<>();
-			distToNearestLeaf[i] = 1<<30;
-		}
-		
-		for(int i=1; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			adList[a].add(b);
-			adList[b].add(a);
+			adNode[a] = new Node(b, adNode[a]);
+			adNode[b] = new Node(a, adNode[b]);
 		}
 		
-		mark(S, -1, 1);
+		mark(S, -1, 1);// 시작 노드로 부터 거리를 fromStart에 마킹한다.
 		
 		dfs(S, -1);
 		
@@ -59,27 +55,30 @@ class Main{
 			return;
 		}
 		
-		for(int next : adList[now])
-			if(next != prev)
-				dfs(next, now);
+		for(Node next=adNode[now]; next != null; next = next.next)
+			if(next.node != prev)
+				dfs(next.node, now);
 	}
 	
 	static int mark(int now, int prev, int depth) {
+		boolean isLeaf = true;
 		int min = 1<<30;// 리프노드로부터 현재노드까지의 거리
 		
 		distFromStart[now] = depth;// 시작 노드로부터의 현재까지 거리
 		
-		for(int next : adList[now])
+		for(Node next=adNode[now]; next != null; next = next.next)
 		{
-			if(next == prev)
+			if(next.node == prev)
 				continue;
 			
-			int l = mark(next, now, depth + 1);
+			int l = mark(next.node, now, depth + 1);
 			
 			min = Math.min(min,l);// 자기 자식 노드 중 리프노드까지 거리가 가장 짧은 것을 가져옴
+			
+			isLeaf = false;
 		}
 		
-		if(adList[now].size() <= 1) // 현재 노드가 리프노드일 경우 min을 0으로 세팅해서 추후 1이되도록함, 즉 리프노드일 경우 1저장됨
+		if(isLeaf) // 현재 노드가 리프노드일 경우 min을 0으로 세팅해서 추후 1이되도록함, 즉 리프노드일 경우 1저장됨
 			min = 0;
 		
 		min += 1; // 현재 노드는 이전 노드들의 거리보다 + 1이 되어야 함
@@ -87,5 +86,13 @@ class Main{
 		distToNearestLeaf[now] = min;// 리프노드부터 현재노드까지 거리 세팅
 		
 		return min;// 값 반환
+	}
+	static class Node{
+		int node;
+		Node next;
+		Node(int node, Node next){
+			this.node = node;
+			this.next = next;
+		}
 	}
 }
