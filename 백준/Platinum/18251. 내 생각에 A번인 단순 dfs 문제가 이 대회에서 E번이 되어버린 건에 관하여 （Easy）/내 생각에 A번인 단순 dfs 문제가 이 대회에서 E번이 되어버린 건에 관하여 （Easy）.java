b0325782f9ba27b3cb33afe260c76ff39d22a7be
YongJu Kim <kyjdummy@gmail.com>
2025-08-24 17:31:37 +0900

    [Platinum IV] Title: 내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 (Easy), Time: 376 ms, Memory: 61072 KB -BaekjoonHub

--- 백준/Platinum/18251. 내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）/내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）.java
+++ 백준/Platinum/18251. 내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）/내 생각에 A번인 단순 dfs 문제가 이 대회에서 E번이 되어버린 건에 관하여 （Easy）.java
@@ -1,3 +1,5 @@
+//https://www.acmicpc.net/problem/18251
+//2초 512MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -30,7 +32,7 @@ class Main{
 		
 		if(left <= N)
 			dfs(left, yPos + 1);
-		
+		// 중위 탐색으로 X좌표에 따른 Y좌표와 가중치를 저장
 		info[++xPos] = new Node(yPos, arr[node]);
 		
 		if(right <= N)
@@ -45,17 +47,18 @@ class Main{
 		int x = N; // 트리의 높이를 구하기 위해 사용
 		while(x > 0) {x>>=1; height++;}// 트리의 높이를 구함
 		
-		for(int s=1; s<=height; s++)
+		for(int s=1; s<=height; s++)// 모든 Y좌표 완전 탐색
 		{
-			for(int e=s; e<=height; e++)
+			for(int e=s; e<=height; e++)// 모든 Y좌표 완전 탐색
 			{
 				long cur = 0;
 				
-				for(int i=1; i<=N; i++)
+				for(int i=1; i<=N; i++)// X좌표를 1부터 끝까지 순차 탐색
 				{
-					if(info[i].yPos < s || e < info[i].yPos)
+					if(info[i].yPos < s || e < info[i].yPos)// 해당 X좌표에 따른 Y좌표가 범위밖이면 스킵
 						continue;
 					
+					// 이하 연속하는 최대 부분합을 구하기 위한 카데인 알고리즘 및 최종 결과 세팅
 					cur = Math.max(cur + info[i].val, info[i].val);
 					sum = Math.max(cur, sum);
 				}
