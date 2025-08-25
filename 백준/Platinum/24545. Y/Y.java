2298db321880145e5d76312f1b9baf51185a3910
YongJu Kim <kyjdummy@gmail.com>
2025-08-25 22:27:08 +0900

    [Platinum V] Title: Y, Time: 636 ms, Memory: 76452 KB -BaekjoonHub

--- 백준/Platinum/24545. Y/Y.java
+++ 백준/Platinum/24545. Y/Y.java
@@ -9,20 +9,19 @@ import java.util.StringTokenizer;
 class Main{
 	
 	static int N;
-	static int size;
 	static int idx;
 	static int maxDepth;
+	
 	static int max[];
 	static boolean visit[];
-	
 	static List<Integer> adList[];
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		adList = new ArrayList[N + 1];
 		max = new int[2];
 		visit = new boolean[N + 1];
+		adList = new ArrayList[N + 1];
 		
 		for(int i=0; i<=N; i++)
 			adList[i] = new ArrayList<>();
@@ -36,11 +35,14 @@ class Main{
 			adList[b].add(a);
 		}
 		
-		dfs1(1, -1, 0);// 트리 지름의 양끝 노드를 구하기 위한 첫번 째 dfs
+		dfs1(1, -1, 1);// 트리 지름의 양끝 노드를 구하기 위한 첫번 째 dfs
+		
 		idx += 1;
 		maxDepth = 0;
-		dfs1(max[0], -1, 0);// 트리 지름의 양끝 노드를 구하기 위한 두번 째 dfs
-		mark(max[0], -1, 1);// 트리 지름을 탐색하며 visit에 마킹
+		dfs1(max[0], -1, 1);// 트리 지름의 양끝 노드를 구하기 위한 두번 째 dfs
+		
+		visit[max[0]] = true;
+		mark(max[0], 1);// 트리 지름을 탐색하며 visit에 마킹
 		
 		int max = 0;
 		boolean validate = false;
@@ -60,43 +62,38 @@ class Main{
 			}
 		}
 		
-		System.out.print(validate ? size + max : 0);
+		System.out.print(validate ? maxDepth + max : 0);
 	}
 	static int dfs2(int now) {
 		visit[now] = true;
 		
 		int maxSize = 0;
 		
-		for(int next : adList[now]) {
+		for(int next : adList[now])
+		{
 			if(visit[next])
 				continue;
-			
-			visit[next] = true;
-			
+
 			int childSize = dfs2(next);
 			
 			maxSize = Math.max(maxSize, childSize);
 		}
 		return maxSize + 1;
 	}
-	static boolean mark(int now, int prev, int depth) {
-		visit[now] = true;
+	static boolean mark(int now, int depth) {
 		
-		if(max[1] == now)
-		{
-			size = depth;
+		if(max[1] == now)// 목표에 도달시 true 리턴
 			return true;
-		}
 		
 		for(int next : adList[now])
 		{
-			if(next != prev && !visit[next])
+			if(!visit[next])
 			{
 				visit[next] = true;
-				if(mark(next, now, depth + 1))
-				{
+				
+				if(mark(next, depth + 1))
 					return true;
-				}
+				
 				visit[next] = false;
 			}
 		}
