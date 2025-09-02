eb1044ee9471940b346f87be70ef336dfe41f51b
YongJu Kim <kyjdummy@gmail.com>
2025-09-02 23:08:54 +0900

    [Silver IV] Title: 빙고, Time: 64 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -2,7 +2,6 @@
 //1초 / 128MB
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 class Main{
@@ -10,36 +9,45 @@ class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
-		HashMap<Integer, int[]> idx = new HashMap<>();
+		int map[][] = new int[5][5];
+		
 		for(int y=0; y<5; y++)
 		{
 			st = new StringTokenizer(br.readLine());
 			for(int x=0; x<5; x++)
-			{
-				int num = Integer.parseInt(st.nextToken());
-				idx.put(num, new int[] {y,x});
-			}
+				map[y][x] = Integer.parseInt(st.nextToken());
+
 		}
 		
-		boolean map[][] = new boolean[5][5];
 		int cnt = 0;
 		
-		Loop:
 		while(cnt<25)
 		{
 			if(cnt % 5 == 0)
 				st = new StringTokenizer(br.readLine());
+			
+			mark(map, Integer.parseInt(st.nextToken()));
 
-			int xy[] = idx.get(Integer.parseInt(st.nextToken()));
-			map[xy[0]][xy[1]] = true;
 			if(check(map))
-				break Loop;
+			{
+				System.out.print(cnt + 1);
+				return;
+			}
 
 			++cnt;
 		}
-		System.out.print(cnt + 1);
+		System.out.print(0);
 	}
-	static boolean check(boolean[][] map) {
+	static void mark(int[][] map, int target) {
+		for(int y=0; y<5; y++)
+			for(int x=0; x<5; x++)
+				if(map[y][x] == target)
+				{
+					map[y][x] = 0;
+					return;
+				}
+	}
+	static boolean check(int[][] map) {
 		int line = 0;
 		int dig = 0;
 		for(int y=0; y<5; y++)
@@ -48,9 +56,9 @@ class Main{
 			int col = 0;
 			for(int x=0; x<5; x++)
 			{
-				if(map[y][x])row++;
-				if(map[x][y])col++;
-				if(y == x && map[y][x])
+				if(map[y][x] == 0)row++;
+				if(map[x][y] == 0)col++;
+				if(y == x && (map[y][x] == 0))
 					dig++;
 			}
 			if(row == 5) ++line;
@@ -59,7 +67,9 @@ class Main{
 		}
 		if(dig == 5) ++line;
 		
-		if(map[0][4] && map[1][3] && map[2][2] && map[3][1] && map[4][0])
+		int sum = map[0][4] + map[1][3] + map[2][2] + map[3][1] + map[4][0];
+		
+		if(sum == 0)
 			++line;
 		
 		return line>=3;
