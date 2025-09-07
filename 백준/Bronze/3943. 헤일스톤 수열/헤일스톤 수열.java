283fe3ac7a39662398e97b3effc5f88988b457c4
YongJu Kim <kyjdummy@gmail.com>
2025-09-07 17:10:04 +0900

    [Bronze II] Title: 헤일스톤 수열, Time: 356 ms, Memory: 27340 KB -BaekjoonHub

---
+++ 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.java
@@ -0,0 +1,31 @@
+//https://www.acmicpc.net/problem/3943
+//1초 128MB
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		while(T-->0)
+		{
+			int N = Integer.parseInt(br.readLine());
+			int M = N;
+			
+			while(N != 1)
+			{
+				if(N % 2 == 0)
+					N /= 2;
+				else
+					N = N * 3 + 1;
+				
+				M = Math.max(M, N);
+			}
+			
+			sb.append(M).append('\n');
+		}
+		System.out.print(sb);
+	}
+}
