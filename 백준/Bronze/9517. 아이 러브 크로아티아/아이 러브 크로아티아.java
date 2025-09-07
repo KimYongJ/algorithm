083600d4e0135f76d1dff1145079d1c4629d9466
YongJu Kim <kyjdummy@gmail.com>
2025-09-07 17:59:30 +0900

    [Bronze III] Title: 아이 러브 크로아티아, Time: 100 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/9517. 아이 러브 크로아티아/아이 러브 크로아티아.java
@@ -0,0 +1,31 @@
+//https://www.acmicpc.net/problem/9517
+//1초 128MB
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int time = 210;
+		
+		while(N-->0)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int t = Integer.parseInt(st.nextToken());
+			char c = st.nextToken().charAt(0);
+			time -= t;
+			
+			if(time < 0)
+				break;
+			
+			if(c == 'T' && ++K == 9)
+				K = 1;
+		}
+		
+		System.out.print(K);
+	}
+}
