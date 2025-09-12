a4484070391f8849da255ae11a8ae9f1cb216840
YongJu Kim <kyjdummy@gmail.com>
2025-09-12 23:10:35 +0900

    [Gold III] Title: 최대 유량, Time: 68 ms, Memory: 11792 KB -BaekjoonHub

--- 백준/Gold/6086. 최대 유량/최대 유량.java
+++ 백준/Gold/6086. 최대 유량/최대 유량.java
@@ -13,88 +13,95 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
 class Main{
 	
-	static final int len = 52;
-	static final int start = 1;// A를 의미
-	static final int end = 26;// Z를 의미
-	static int[][] ad = new int[len + 1][len + 1];
-	static int[] prev = new int[len + 1];
+	static final int MAX = 52;
+	static final int start = atoi('A');
+	static final int end = atoi('Z');
+	static int [][] pipe = new int[MAX + 1][MAX + 1];// 각
+	static int []prev = new int[MAX + 1];// A->Z 경로 탐색 중 이전 노드를 담을 배열
+	static int N;
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		
 		while(N-->0)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = atoi(st.nextToken().charAt(0));
 			int b = atoi(st.nextToken().charAt(0));
 			int u = Integer.parseInt(st.nextToken());
-			ad[a][b] += u;
-			ad[b][a] += u;
+			// “무방향 파이프”를 양방향 잔여용량으로 초기화, 중복 간선은 합쳐짐
+			pipe[a][b] += u;
+			pipe[b][a] += u;
 		}
 		
-		int sum = 0;
-        
+		int res = 0;
+		
 		while(true)
 		{
-			Arrays.fill(prev, -1); // 이전 탐색 노드를 담을 배열 초기화
-			
-			BFS();// A 에서 Z로 가는 방향을 마킹
-			
+			// 이전 노드 위치를 담을 배열 초기화
+			Arrays.fill(prev, -1);
+			// A->Z 까지의 경로를 prev 배열에 마킹
+			bfs();
+			// 종료 지점에 더이상 못 가면 종료
 			if(prev[end] < 0)
 				break;
-			
-			sum += flow();
+			// 최소 유량 확인 및 결과 갱신
+			res += flow();
 		}
 		
-		System.out.print(sum);
+		System.out.print(res);
 	}
 	static int flow() {
-		// end에서 start로 가며 최소 유량을 구한다.
 		int now = end;
-		int water = 1<<30;
+		int water = Integer.MAX_VALUE;// 최소 유량
+		
 		while(now != start)
 		{
-			water = Math.min(water, ad[prev[now]][now]);
+			// end -> start로 가면서(역방향) 정방향 방향(pipe[prev[now]][now])에 있는 가장 최소 유량을 구함
+			water = Math.min(water, pipe[prev[now]][now]);
 			now = prev[now];
 		}
+		
 		now = end;
+		
 		while(now != start)
 		{
-			ad[prev[now]][now] -= water;
-			ad[now][prev[now]] += water;
+			// 최소 유량(water)을 정방향에는 마이너스 처리
+			pipe[prev[now]][now] -= water;
+			// 역방향에는 최소 유량(water)를 플러스 해줌으로 추후 되돌림을 할 수 있게 한다.
+			pipe[now][prev[now]] += water;
 			now = prev[now];
 		}
+		
 		return water;
 	}
-	static void BFS() {
+	static void bfs() {
 		Queue<Integer> q = new ArrayDeque<>();
+		prev[start] = start;
 		q.add(start);
-		prev[start] = start;// 시작의 이전노드는 자기 자신
 		
 		while(!q.isEmpty())
 		{
 			int now = q.poll();
-			
 			if(now == end)
 				return;
-			
-			for(int next=1; next<=len; next++)
+			// 인접 노드 모두 탐색
+			for(int next = 1; next <= MAX; next++)
 			{
-				if(ad[now][next] > 0 && prev[next] == -1)
+				// 잔여 유량이 있고, 방문한적이 없다면 탐색
+				if(pipe[now][next] > 0 && prev[next] < 0)
 				{
+					prev[next] = now;// 이전노드마킹
 					q.add(next);
-					prev[next] = now;
 				}
 			}
-			
 		}
 	}
-	
-	static int atoi(char c) {
+	static int atoi(char c){
 		int res = c <= 'Z' ? c - 'A' : c - 'a' + 26;
-		return res + 1;// 1을 더해서 첫 시작이 1이되게함
+		return res + 1;
 	}
 }
