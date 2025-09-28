04c3e517e49a18d9f30efface318658d1d0f1840
YongJu Kim <kyjdummy@gmail.com>
2025-09-28 18:06:33 +0900

    [Bronze II] Title: 전구, Time: 136 ms, Memory: 16776 KB -BaekjoonHub

--- 백준/Bronze/21918. 전구/전구.java
+++ 백준/Bronze/21918. 전구/전구.java
@@ -3,19 +3,29 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main{
+	
+	static int N, M;
+	static int[] tree, lazy;
+	static boolean[] isXor;
+	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int arr[] = new int[N + 1];
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		tree = new int[N * 4];
+		lazy = new int[N * 4];
+		isXor = new boolean[N * 4];
+		
+		Arrays.fill(lazy, -1);
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+			updateRange(1, 1, N, i, i, Integer.parseInt(st.nextToken()), false);
 		
 		while(M-->0)
 		{
@@ -24,14 +34,104 @@ class Main{
 			int l = Integer.parseInt(st.nextToken());
 			int r = Integer.parseInt(st.nextToken());
 			
-			if(i == 1)		arr[l] = r;
-			else if(i == 2)	while(l<=r)arr[l++] ^= 1;
-			else if(i == 3)	while(l<=r)arr[l++] = 0;
-			else if(i == 4)	while(l<=r)arr[l++] = 1;
+			if(i == 1)		updateRange(1, 1, N, l, l, r, false);
+			else if(i == 2)	updateRange(1, 1, N, l, r, 0, true);
+			else if(i == 3)	updateRange(1, 1, N, l, r, 0, false);
+			else if(i == 4)	updateRange(1, 1, N, l, r, 1, false);
 		}
 		StringBuilder sb = new StringBuilder();
+
 		for(int i=1; i<=N; i++)
-			sb.append(arr[i]).append(' ');
+			sb.append(query(1, 1, N, i)).append(' ');
+		
 		System.out.print(sb);
+	}
+	static void lazy(int treeNode, int s, int e) {
+		if(lazy[treeNode] == -1 && !isXor[treeNode])
+			return;
+	
+		if(s == e)
+		{
+			if(lazy[treeNode] != -1) // 다이렉트 저장이 있다면 저장
+				tree[treeNode] = lazy[treeNode];
+			
+			if(isXor[treeNode])// xor도 있다면 xor 처리를 해줌
+				tree[treeNode] ^= 1;
+		}
+		else
+		{
+			int left = treeNode << 1;
+			int right = treeNode << 1 | 1;
+			
+			if(lazy[treeNode] != -1)
+			{
+				lazy[left] = lazy[treeNode];
+				lazy[right] = lazy[treeNode];
+				isXor[left] = false;
+				isXor[right] = false;
+			}
+			
+			if(isXor[treeNode])
+			{
+				if(lazy[left] != -1)
+					lazy[left] ^= 1;
+				else
+					isXor[left] = !isXor[left];
+				if(lazy[right] != -1)
+					lazy[right] ^= 1;
+				else
+					isXor[right] = !isXor[right];
+			}
+		}
+		lazy[treeNode] = -1;
+		isXor[treeNode] = false;
+		
+	}
+	static void updateRange(int treeNode, int s, int e, int left, int right, int val, boolean xor) {
+		
+		lazy(treeNode, s, e);
+		
+		if(e < left || right < s)
+			return;
+		
+		if(left <= s && e <= right)
+		{
+			if(xor)// 반전일 경우
+			{
+				if(lazy[treeNode] != -1)// lazy에 값이 있다면 lazy값을 변경함
+					lazy[treeNode] ^= 1;
+				else
+					isXor[treeNode] = !isXor[treeNode];// 아닌경우 xor을 저장
+			}
+			else
+			{
+				lazy[treeNode] = val;// 다이렉트 저장은 값을 그대로 대입
+				isXor[treeNode] = false;// xor은 false로 초기화
+			}
+			
+			lazy(treeNode, s, e);
+			return;
+		}
+		
+		int mid = (s + e) >> 1;
+		
+		updateRange(treeNode << 1, s, mid, left, right, val, xor);
+		updateRange(treeNode << 1 | 1, mid + 1, e, left, right, val, xor);
+	}
+	static int query(int treeNode, int s, int e, int targetIdx) {
+		lazy(treeNode, s, e);
+		
+		if(e<targetIdx || targetIdx < s)
+			return -1;
+		
+		if(s == e)
+			return tree[treeNode];
+		
+		int mid = (s + e) >> 1;
+		
+		if(targetIdx <= mid)
+			return query(treeNode << 1, s, mid, targetIdx);
+		
+		return query(treeNode << 1 | 1, mid + 1, e, targetIdx);
 	}
 }
