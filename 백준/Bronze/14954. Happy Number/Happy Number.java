df7b99ad1c9babf8f0c3157db6dfbee46a5234f5
YongJu Kim <kyjdummy@gmail.com>
2025-10-10 08:42:38 +0900

    [Bronze II] Title: Happy Number, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/14954. Happy Number/Happy Number.java
+++ 백준/Bronze/14954. Happy Number/Happy Number.java
@@ -4,7 +4,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 class Main{
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
