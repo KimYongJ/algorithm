ce9ed40d3a53db639b2313b5035b84069f2162a8
YongJu Kim <kyjdummy@gmail.com>
2025-12-06 23:34:06 +0900

    [Bronze I] Title: XORXORXOR, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,17 @@
+//https://www.acmicpc.net/problem/12833
+//0.2초 512
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		System.out.print(C % 2 == 0 ? A : A^B);
+	}
+}
