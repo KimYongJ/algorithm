f788c97d435522d884de1f265773c700438184af
YongJu Kim <kyjdummy@gmail.com>
2025-12-21 11:55:12 +0900

    [Silver III] Title: 데스스타, Time: 116 ms, Memory: 12016 KB -BaekjoonHub

--- 백준/Silver/11811. 데스스타/데스스타.java
+++ 백준/Silver/11811. 데스스타/데스스타.java
@@ -1,27 +1,25 @@
 //https://www.acmicpc.net/problem/11811
 //1초 256MB
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 class Main{
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N];
+		StringBuilder sb = new StringBuilder();
+		int N = read();
 		
 		for(int i=0; i<N; i++)
 		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
 			int bit = 0;
 			for(int j=0; j<N; j++)
-				bit |= Integer.parseInt(st.nextToken());
-			arr[i] = bit;
+				bit |= read();
+			sb.append(bit).append(' ');
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int a : arr) sb.append(a).append(' ');
+
 		System.out.print(sb);
+	}
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c = System.in.read()) > 32)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
 	}
 }
