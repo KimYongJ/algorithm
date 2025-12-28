086a20e224cba410f6d238925c95bc2d785b2036
YongJu Kim <kyjdummy@gmail.com>
2025-12-28 19:36:21 +0900

    [Silver I] Title: 오목, Time: 64 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/2072. 오목/오목.java
+++ 백준/Silver/2072. 오목/오목.java
@@ -6,28 +6,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main{
-	
-	static int pos[][][] = {
-			{{0,1},{0,-1}}, // 상하
-			{{1,0},{-1,0}}, // 좌우
-			{{-1,-1},{1,1}},// 왼쪽위에서 오른쪽 아래로
-			{{-1,1},{1,-1}},// 왼쪽아래에서 오른쪽 위로
-	};
+
 	public static void main(String[] args)throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final int pos[][][] = {
+				{{0,1},{0,-1}}, // 상하
+				{{1,0},{-1,0}}, // 좌우
+				{{-1,-1},{1,1}},// 왼쪽위에서 오른쪽 아래로
+				{{-1,1},{1,-1}},// 왼쪽아래에서 오른쪽 위로
+		};
 		int N = Integer.parseInt(br.readLine());
 		int map[][] = new int[22][22];
 		int res = -1;
-		boolean flag = true;
-		int color = 0;
 		
-		for(int i=1; i<=N; i++)
+		for(int i=1, color = 0; i<=N; i++)
 		{
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 			
-			map[y][x] = color = flag ? 1 : 2;
+			map[y][x] = color = (i & 1) == 1 ? 1 : 2;
 			
 			for(int dxy[][] : pos) // 상하좌우대각선을 탐색
 			{
@@ -43,13 +41,13 @@ class Main{
 						nextY += xy[1];
 					}
 				}
-				if(cnt == 5) {
+				if(cnt == 5)
+				{
 					System.out.print(i);
 					return;
 				}
 			}
 			
-			flag = !flag;
 		}
 		System.out.print(res);
 	}
